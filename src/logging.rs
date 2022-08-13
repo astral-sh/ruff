53ab2549592d1aad98a42e5c3cb94439487e24e2
Charles Marsh <charlie@springdisc.com>
2022-08-13 10:58:26 -0400

    Make cache configurable

---
+++ src/logging.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use fern;
+
+#[macro_export]
+macro_rules! timestamped_println {
+    ($($arg:tt)*) => {
+        println!(
+            "[{}] {}",
+            chrono::Local::now()
+                .format("%H:%M:%S %p")
+                .to_string()
+                .dimmed(),
+            format_args!($($arg)*)
+        )
+    }
+}
+
+pub fn set_up_logging(verbose: bool) -> Result<()> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(if verbose {
+            log::LevelFilter::Debug
+        } else {
+            log::LevelFilter::Info
+        })
+        .chain(std::io::stdout())
+        .apply()
+        .map_err(|e| e.into())
+}
