a3fb0d6c20155eb47345403d391f7df145d8005c
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-09-04 17:54:45 -0400

    Remove custom serialization for Location (#104)

--- src/message.rs
+++ src/message.rs
@@ -7,25 +7,9 @@ use serde::{Deserialize, Serialize};
 
 use crate::checks::CheckKind;
 
-#[derive(Serialize, Deserialize)]
-#[serde(remote = "Location")]
-struct LocationDef {
-    #[serde(getter = "Location::row")]
-    row: usize,
-    #[serde(getter = "Location::column")]
-    column: usize,
-}
-
-impl From<LocationDef> for Location {
-    fn from(def: LocationDef) -> Location {
-        Location::new(def.row, def.column)
-    }
-}
-
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Message {
     pub kind: CheckKind,
-    #[serde(with = "LocationDef")]
     pub location: Location,
     pub filename: String,
 }
