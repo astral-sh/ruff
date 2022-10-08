0aa76c3671c9dc60e507db73d2164816b8e80c07
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-10-08 17:36:15 -0400

    Use strum to iterate over all check codes

--- examples/generate_rules_table.rs
+++ examples/generate_rules_table.rs
@@ -1,13 +1,12 @@
 /// Generate a Markdown-compatible table of supported lint rules.
-use ruff::checks::{CheckCode, ALL_CHECK_CODES, DEFAULT_CHECK_CODES};
+use strum::IntoEnumIterator;
+
+use ruff::checks::{CheckCode, DEFAULT_CHECK_CODES};
 
 fn main() {
-    let mut check_codes: Vec<CheckCode> = ALL_CHECK_CODES.to_vec();
-    check_codes.sort();
-
     println!("| Code | Name | Message |     |     |");
     println!("| ---- | ---- | ------- | --- | --- |");
-    for check_code in check_codes {
+    for check_code in CheckCode::iter() {
         let check_kind = check_code.kind();
         let default_token = if DEFAULT_CHECK_CODES.contains(&check_code) {
             "✅"
