bc8a0961acd8b7bbb55d3ea69d2f3a9e4f676f32
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-10-08 21:13:12 -0400

    Create unified Expr for PEP 604 rewrites

--- src/plugins/use_pep604_annotation.rs
+++ src/plugins/use_pep604_annotation.rs
@@ -1,5 +1,4 @@
-use anyhow::{anyhow, Result};
-use rustpython_ast::{Expr, ExprKind};
+use rustpython_ast::{Constant, Expr, ExprKind, Operator};
 
 use crate::ast::helpers::match_name_or_attr;
 use crate::ast::types::Range;
@@ -8,15 +7,50 @@ use crate::check_ast::Checker;
 use crate::checks::{Check, CheckKind, Fix};
 use crate::code_gen::SourceGenerator;
 
+fn optional(expr: &Expr) -> Expr {
+    Expr::new(
+        Default::default(),
+        Default::default(),
+        ExprKind::BinOp {
+            left: Box::new(expr.clone()),
+            op: Operator::BitOr,
+            right: Box::new(Expr::new(
+                Default::default(),
+                Default::default(),
+                ExprKind::Constant {
+                    value: Constant::None,
+                    kind: None,
+                },
+            )),
+        },
+    )
+}
+
+fn union(elts: &[Expr]) -> Expr {
+    if elts.len() == 1 {
+        elts[0].clone()
+    } else {
+        Expr::new(
+            Default::default(),
+            Default::default(),
+            ExprKind::BinOp {
+                left: Box::new(union(&elts[..elts.len() - 1])),
+                op: Operator::BitOr,
+                right: Box::new(elts[elts.len() - 1].clone()),
+            },
+        )
+    }
+}
+
 pub fn use_pep604_annotation(checker: &mut Checker, expr: &Expr, value: &Expr, slice: &Expr) {
     if match_name_or_attr(value, "Optional") {
         let mut check = Check::new(CheckKind::UsePEP604Annotation, Range::from_located(expr));
         if matches!(checker.autofix, fixer::Mode::Generate | fixer::Mode::Apply) {
             let mut generator = SourceGenerator::new();
-            if let Ok(()) = generator.unparse_expr(slice, 0) {
+            if let Ok(()) = generator.unparse_expr(&optional(slice), 0) {
                 if let Ok(content) = generator.generate() {
                     check.amend(Fix {
-                        content: format!("{} | None", content),
+                        content,
                         location: expr.location,
                         end_location: expr.end_location,
                         applied: false,
@@ -33,27 +67,16 @@ pub fn use_pep604_annotation(checker: &mut Checker, expr: &Expr, value: &Expr, s
                     // Invalid type annotation.
                 }
                 ExprKind::Tuple { elts, .. } => {
-                    // Multiple arguments.
-                    let parts: Result<Vec<String>> = elts
-                        .iter()
-                        .map(|expr| {
-                            let mut generator = SourceGenerator::new();
-                            generator
-                                .unparse_expr(expr, 0)
-                                .map_err(|_| anyhow!("Failed to parse."))?;
-                            generator
-                                .generate()
-                                .map_err(|_| anyhow!("Failed to generate."))
-                        })
-                        .collect();
-                    if let Ok(parts) = parts {
-                        let content = parts.join(" | ");
-                        check.amend(Fix {
-                            content,
-                            location: expr.location,
-                            end_location: expr.end_location,
-                            applied: false,
-                        })
+                    let mut generator = SourceGenerator::new();
+                    if let Ok(()) = generator.unparse_expr(&union(elts), 0) {
+                        if let Ok(content) = generator.generate() {
+                            check.amend(Fix {
+                                content,
+                                location: expr.location,
+                                end_location: expr.end_location,
+                                applied: false,
+                            })
+                        }
                     }
                 }
                 _ => {
