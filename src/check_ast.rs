54cb2eb15be0f84a184309f84c9aa9397da2aebc
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-10-12 17:14:16 -0400

    Only run section checks when CheckCodes are enabled

--- src/check_ast.rs
+++ src/check_ast.rs
@@ -1973,8 +1973,23 @@ impl<'a> Checker<'a> {
             if self.settings.enabled.contains(&CheckCode::D418) {
                 docstring_checks::if_needed(self, &docstring);
             }
-
-            docstring_checks::check_sections(self, &docstring);
+            if self.settings.enabled.contains(&CheckCode::D407)
+                || self.settings.enabled.contains(&CheckCode::D414)
+                || self.settings.enabled.contains(&CheckCode::D407)
+                || self.settings.enabled.contains(&CheckCode::D212)
+                || self.settings.enabled.contains(&CheckCode::D408)
+                || self.settings.enabled.contains(&CheckCode::D409)
+                || self.settings.enabled.contains(&CheckCode::D414)
+                || self.settings.enabled.contains(&CheckCode::D412)
+                || self.settings.enabled.contains(&CheckCode::D414)
+                || self.settings.enabled.contains(&CheckCode::D405)
+                || self.settings.enabled.contains(&CheckCode::D413)
+                || self.settings.enabled.contains(&CheckCode::D410)
+                || self.settings.enabled.contains(&CheckCode::D411)
+                || self.settings.enabled.contains(&CheckCode::D406)
+            {
+                docstring_checks::check_sections(self, &docstring);
+            }
         }
     }
 
