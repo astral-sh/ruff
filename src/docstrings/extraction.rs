6407fd5a33ff90d30c6b6f2958816fce370ef5dd
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-10-14 12:34:35 -0400

    Re-arrange some docstring modules (#428)

---
+++ src/docstrings/extraction.rs
@@ -0,0 +1,82 @@
+//! Extract docstrings from an AST.
+
+use rustpython_ast::{Constant, Expr, ExprKind, Stmt, StmtKind};
+
+use crate::docstrings::types::{Definition, DefinitionKind, Documentable};
+use crate::visibility::{Modifier, VisibleScope};
+
+/// Extract a docstring from a function or class body.
+pub fn docstring_from(suite: &[Stmt]) -> Option<&Expr> {
+    if let Some(stmt) = suite.first() {
+        if let StmtKind::Expr { value } = &stmt.node {
+            if matches!(
+                &value.node,
+                ExprKind::Constant {
+                    value: Constant::Str(_),
+                    ..
+                }
+            ) {
+                return Some(value);
+            }
+        }
+    }
+    None
+}
+
+/// Extract a `Definition` from the AST node defined by a `Stmt`.
+pub fn extract<'a>(
+    scope: &VisibleScope,
+    stmt: &'a Stmt,
+    body: &'a [Stmt],
+    kind: &Documentable,
+) -> Definition<'a> {
+    let expr = docstring_from(body);
+    match kind {
+        Documentable::Function => match scope {
+            VisibleScope {
+                modifier: Modifier::Module,
+                ..
+            } => Definition {
+                kind: DefinitionKind::Function(stmt),
+                docstring: expr,
+            },
+            VisibleScope {
+                modifier: Modifier::Class,
+                ..
+            } => Definition {
+                kind: DefinitionKind::Method(stmt),
+                docstring: expr,
+            },
+            VisibleScope {
+                modifier: Modifier::Function,
+                ..
+            } => Definition {
+                kind: DefinitionKind::NestedFunction(stmt),
+                docstring: expr,
+            },
+        },
+        Documentable::Class => match scope {
+            VisibleScope {
+                modifier: Modifier::Module,
+                ..
+            } => Definition {
+                kind: DefinitionKind::Class(stmt),
+                docstring: expr,
+            },
+            VisibleScope {
+                modifier: Modifier::Class,
+                ..
+            } => Definition {
+                kind: DefinitionKind::NestedClass(stmt),
+                docstring: expr,
+            },
+            VisibleScope {
+                modifier: Modifier::Function,
+                ..
+            } => Definition {
+                kind: DefinitionKind::NestedClass(stmt),
+                docstring: expr,
+            },
+        },
+    }
+}
--- src/docstrings/sections.rs
+++ src/docstrings/sections.rs
@@ -1,176 +1,32 @@
-use itertools::Itertools;
 use std::collections::BTreeSet;
 
-use once_cell::sync::Lazy;
-use regex::Regex;
-use rustpython_ast::{Arg, Expr, Location, StmtKind};
+use itertools::Itertools;
+use rustpython_ast::{Arg, StmtKind};
 use titlecase::titlecase;
 
 use crate::ast::types::Range;
 use crate::check_ast::Checker;
 use crate::checks::{Check, CheckCode, CheckKind};
-use crate::docstrings::docstring_checks::range_for;
+use crate::docstrings::helpers;
+use crate::docstrings::helpers::range_for;
+use crate::docstrings::styles::SectionStyle;
 use crate::docstrings::types::{Definition, DefinitionKind};
 use crate::visibility::is_static;
 
-static NUMPY_SECTION_NAMES: Lazy<BTreeSet<&'static str>> = Lazy::new(|| {
-    BTreeSet::from([
-        "Short Summary",
-        "Extended Summary",
-        "Parameters",
-        "Returns",
-        "Yields",
-        "Other Parameters",
-        "Raises",
-        "See Also",
-        "Notes",
-        "References",
-        "Examples",
-        "Attributes",
-        "Methods",
-    ])
-});
-
-static LOWERCASE_NUMPY_SECTION_NAMES: Lazy<BTreeSet<&'static str>> = Lazy::new(|| {
-    BTreeSet::from([
-        "short summary",
-        "extended summary",
-        "parameters",
-        "returns",
-        "yields",
-        "other parameters",
-        "raises",
-        "see also",
-        "notes",
-        "references",
-        "examples",
-        "attributes",
-        "methods",
-    ])
-});
-
-static GOOGLE_SECTION_NAMES: Lazy<BTreeSet<&'static str>> = Lazy::new(|| {
-    BTreeSet::from([
-        "Args",
-        "Arguments",
-        "Attention",
-        "Attributes",
-        "Caution",
-        "Danger",
-        "Error",
-        "Example",
-        "Examples",
-        "Hint",
-        "Important",
-        "Keyword Args",
-        "Keyword Arguments",
-        "Methods",
-        "Note",
-        "Notes",
-        "Return",
-        "Returns",
-        "Raises",
-        "References",
-        "See Also",
-        "Tip",
-        "Todo",
-        "Warning",
-        "Warnings",
-        "Warns",
-        "Yield",
-        "Yields",
-    ])
-});
-
-static LOWERCASE_GOOGLE_SECTION_NAMES: Lazy<BTreeSet<&'static str>> = Lazy::new(|| {
-    BTreeSet::from([
-        "args",
-        "arguments",
-        "attention",
-        "attributes",
-        "caution",
-        "danger",
-        "error",
-        "example",
-        "examples",
-        "hint",
-        "important",
-        "keyword args",
-        "keyword arguments",
-        "methods",
-        "note",
-        "notes",
-        "return",
-        "returns",
-        "raises",
-        "references",
-        "see also",
-        "tip",
-        "todo",
-        "warning",
-        "warnings",
-        "warns",
-        "yield",
-        "yields",
-    ])
-});
-
-pub enum SectionStyle {
-    NumPy,
-    Google,
-}
-
-impl SectionStyle {
-    fn section_names(&self) -> &Lazy<BTreeSet<&'static str>> {
-        match self {
-            SectionStyle::NumPy => &NUMPY_SECTION_NAMES,
-            SectionStyle::Google => &GOOGLE_SECTION_NAMES,
-        }
-    }
-
-    fn lowercase_section_names(&self) -> &Lazy<BTreeSet<&'static str>> {
-        match self {
-            SectionStyle::NumPy => &LOWERCASE_NUMPY_SECTION_NAMES,
-            SectionStyle::Google => &LOWERCASE_GOOGLE_SECTION_NAMES,
-        }
-    }
-}
-
-fn indentation<'a>(checker: &'a mut Checker, docstring: &Expr) -> &'a str {
-    let range = range_for(docstring);
-    checker.locator.slice_source_code_range(&Range {
-        location: Location::new(range.location.row(), 1),
-        end_location: Location::new(range.location.row(), range.location.column()),
-    })
-}
-
-fn leading_space(line: &str) -> String {
-    line.chars()
-        .take_while(|char| char.is_whitespace())
-        .collect()
-}
-
-fn leading_words(line: &str) -> String {
-    line.trim()
-        .chars()
-        .take_while(|char| char.is_alphanumeric() || char.is_whitespace())
-        .collect()
+#[derive(Debug)]
+pub(crate) struct SectionContext<'a> {
+    pub(crate) section_name: String,
+    pub(crate) previous_line: &'a str,
+    pub(crate) line: &'a str,
+    pub(crate) following_lines: &'a [&'a str],
+    pub(crate) is_last_section: bool,
+    original_index: usize,
 }
 
 fn suspected_as_section(line: &str, style: &SectionStyle) -> bool {
     style
         .lowercase_section_names()
-        .contains(&leading_words(line).to_lowercase().as_str())
-}
-
-#[derive(Debug)]
-pub struct SectionContext<'a> {
-    section_name: String,
-    previous_line: &'a str,
-    line: &'a str,
-    following_lines: &'a [&'a str],
-    original_index: usize,
-    is_last_section: bool,
+        .contains(&helpers::leading_words(line).to_lowercase().as_str())
 }
 
 /// Check if the suspected context is really a section header.
@@ -201,7 +57,10 @@ fn is_docstring_section(context: &SectionContext) -> bool {
 }
 
 /// Extract all `SectionContext` values from a docstring.
-pub fn section_contexts<'a>(lines: &'a [&'a str], style: &SectionStyle) -> Vec<SectionContext<'a>> {
+pub(crate) fn section_contexts<'a>(
+    lines: &'a [&'a str],
+    style: &SectionStyle,
+) -> Vec<SectionContext<'a>> {
     let suspected_section_indices: Vec<usize> = lines
         .iter()
         .enumerate()
@@ -217,7 +76,7 @@ pub fn section_contexts<'a>(lines: &'a [&'a str], style: &SectionStyle) -> Vec<S
     let mut contexts = vec![];
     for lineno in suspected_section_indices {
         let context = SectionContext {
-            section_name: leading_words(lines[lineno]),
+            section_name: helpers::leading_words(lines[lineno]),
             previous_line: lines[lineno - 1],
             line: lines[lineno],
             following_lines: &lines[lineno + 1..],
@@ -335,7 +194,9 @@ fn check_blanks_and_section_underline(
         }
 
         if checker.settings.enabled.contains(&CheckCode::D215) {
-            if leading_space(non_empty_line).len() > indentation(checker, docstring).len() {
+            if helpers::leading_space(non_empty_line).len()
+                > helpers::indentation(checker, docstring).len()
+            {
                 checker.add_check(Check::new(
                     CheckKind::SectionUnderlineNotOverIndented(context.section_name.to_string()),
                     range_for(docstring),
@@ -378,7 +239,7 @@ fn check_blanks_and_section_underline(
     }
 }
 
-fn check_common_section(
+pub(crate) fn check_common_section(
     checker: &mut Checker,
     definition: &Definition,
     context: &SectionContext,
@@ -404,7 +265,9 @@ fn check_common_section(
     }
 
     if checker.settings.enabled.contains(&CheckCode::D214) {
-        if leading_space(context.line).len() > indentation(checker, docstring).len() {
+        if helpers::leading_space(context.line).len()
+            > helpers::indentation(checker, docstring).len()
+        {
             checker.add_check(Check::new(
                 CheckKind::SectionNotOverIndented(context.section_name.to_string()),
                 range_for(docstring),
@@ -443,9 +306,11 @@ fn check_common_section(
             ))
         }
     }
+
+    check_blanks_and_section_underline(checker, definition, context);
 }
 
-fn check_missing_args(
+pub(crate) fn check_missing_args(
     checker: &mut Checker,
     definition: &Definition,
     docstrings_args: &BTreeSet<&str>,
@@ -507,142 +372,6 @@ fn check_missing_args(
                     Range::from_located(parent),
                 ));
             }
-        }
-    }
-}
-
-fn check_parameters_section(
-    checker: &mut Checker,
-    definition: &Definition,
-    context: &SectionContext,
-) {
-    // Collect the list of arguments documented in the docstring.
-    let mut docstring_args: BTreeSet<&str> = Default::default();
-    let section_level_indent = leading_space(context.line);
-    for i in 1..context.following_lines.len() {
-        let current_line = context.following_lines[i - 1];
-        let current_leading_space = leading_space(current_line);
-        let next_line = context.following_lines[i];
-        if current_leading_space == section_level_indent
-            && (leading_space(next_line).len() > current_leading_space.len())
-            && !next_line.trim().is_empty()
-        {
-            let parameters = if let Some(semi_index) = current_line.find(':') {
-                // If the parameter has a type annotation, exclude it.
-                &current_line[..semi_index]
-            } else {
-                // Otherwise, it's just a list of parameters on the current line.
-                current_line.trim()
-            };
-            // Notably, NumPy lets you put multiple parameters of the same type on the same line.
-            for parameter in parameters.split(',') {
-                docstring_args.insert(parameter.trim());
-            }
-        }
-    }
-    // Validate that all arguments were documented.
-    check_missing_args(checker, definition, &docstring_args);
-}
-
-pub fn check_numpy_section(
-    checker: &mut Checker,
-    definition: &Definition,
-    context: &SectionContext,
-) {
-    check_common_section(checker, definition, context, &SectionStyle::NumPy);
-    check_blanks_and_section_underline(checker, definition, context);
-
-    if checker.settings.enabled.contains(&CheckCode::D406) {
-        let suffix = context
-            .line
-            .trim()
-            .strip_prefix(&context.section_name)
-            .unwrap();
-        if !suffix.is_empty() {
-            let docstring = definition
-                .docstring
-                .expect("Sections are only available for docstrings.");
-            checker.add_check(Check::new(
-                CheckKind::NewLineAfterSectionName(context.section_name.to_string()),
-                range_for(docstring),
-            ))
-        }
-    }
-
-    if checker.settings.enabled.contains(&CheckCode::D417) {
-        if titlecase(&context.section_name) == "Parameters" {
-            check_parameters_section(checker, definition, context);
-        }
-    }
-}
-
-// See: `GOOGLE_ARGS_REGEX` in `pydocstyle/checker.py`.
-static GOOGLE_ARGS_REGEX: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r"^\s*(\w+)\s*(\(.*?\))?\s*:\n?\s*.+").expect("Invalid regex"));
-
-fn check_args_section(checker: &mut Checker, definition: &Definition, context: &SectionContext) {
-    let mut args_sections: Vec<String> = vec![];
-    for line in textwrap::dedent(&context.following_lines.join("\n")).lines() {
-        if line
-            .chars()
-            .next()
-            .map(|char| char.is_whitespace())
-            .unwrap_or(true)
-        {
-            // This is a continuation of documentation for the last
-            // parameter because it does start with whitespace.
-            if let Some(current) = args_sections.last_mut() {
-                current.push_str(line);
-            }
-        } else {
-            // This line is the start of documentation for the next
-            // parameter because it doesn't start with any whitespace.
-            args_sections.push(line.to_string());
-        }
-    }
-
-    check_missing_args(
-        checker,
-        definition,
-        // Collect the list of arguments documented in the docstring.
-        &BTreeSet::from_iter(args_sections.iter().filter_map(|section| {
-            match GOOGLE_ARGS_REGEX.captures(section.as_str()) {
-                Some(caps) => caps.get(1).map(|arg_name| arg_name.as_str()),
-                None => None,
-            }
-        })),
-    )
-}
-
-pub fn check_google_section(
-    checker: &mut Checker,
-    definition: &Definition,
-    context: &SectionContext,
-) {
-    check_common_section(checker, definition, context, &SectionStyle::Google);
-    check_blanks_and_section_underline(checker, definition, context);
-
-    if checker.settings.enabled.contains(&CheckCode::D416) {
-        let suffix = context
-            .line
-            .trim()
-            .strip_prefix(&context.section_name)
-            .unwrap();
-        if suffix != ":" {
-            let docstring = definition
-                .docstring
-                .expect("Sections are only available for docstrings.");
-            checker.add_check(Check::new(
-                CheckKind::SectionNameEndsInColon(context.section_name.to_string()),
-                range_for(docstring),
-            ))
-        }
-    }
-
-    if checker.settings.enabled.contains(&CheckCode::D417) {
-        let capitalized_section_name = titlecase(&context.section_name);
-        if capitalized_section_name == "Args" || capitalized_section_name == "Arguments" {
-            check_args_section(checker, definition, context);
         }
     }
 }
