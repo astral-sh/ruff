cb551c5cf619b797cd102faa0d896fa9f3f37758
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-10-15 11:26:56 -0400

    Remove checkmark from rule table

--- examples/generate_rules_table.rs
+++ examples/generate_rules_table.rs
@@ -1,25 +1,19 @@
 /// Generate a Markdown-compatible table of supported lint rules.
 use strum::IntoEnumIterator;
 
-use ruff::checks::{CheckCode, DEFAULT_CHECK_CODES};
+use ruff::checks::CheckCode;
 
 fn main() {
-    println!("| Code | Name | Message |     |     |");
-    println!("| ---- | ---- | ------- | --- | --- |");
+    println!("| Code | Name | Message |     |");
+    println!("| ---- | ---- | ------- | --- |");
     for check_code in CheckCode::iter() {
         let check_kind = check_code.kind();
-        let default_token = if DEFAULT_CHECK_CODES.contains(&check_code) {
-            "✅"
-        } else {
-            ""
-        };
         let fix_token = if check_kind.fixable() { "🛠" } else { "" };
         println!(
-            "| {} | {} | {} | {} | {} |",
+            "| {} | {} | {} | {} |",
             check_kind.code().as_ref(),
             check_kind.as_ref(),
             check_kind.body().replace("|", r"\|"),
-            default_token,
             fix_token
         );
     }
