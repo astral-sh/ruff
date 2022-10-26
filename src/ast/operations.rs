3a59568aed5c8c1b4ffbc5242b76b1cd2cc16503
Suguru Yamamoto <sgryjp@gmail.com>
2022-10-26 12:15:17 +0900

    Apply cargo fmt

--- src/ast/operations.rs
+++ src/ast/operations.rs
@@ -196,7 +196,10 @@ mod tests {
         assert_eq!(locator.offsets.len(), 4);
         assert_eq!(locator.offsets[0], [0, 1, 2, 5]);
         assert_eq!(locator.offsets[1], [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
-        assert_eq!(locator.offsets[2], [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]);
+        assert_eq!(
+            locator.offsets[2],
+            [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+        );
         assert_eq!(locator.offsets[3], [29]);
     }
 }
