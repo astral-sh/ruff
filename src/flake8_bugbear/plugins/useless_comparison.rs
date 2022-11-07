c5e22c956e07eb51c67b9c1a4bc15a890d20dc77
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-11-07 16:34:21 -0500

    Upgrade RustPython

--- src/flake8_bugbear/plugins/useless_comparison.rs
+++ src/flake8_bugbear/plugins/useless_comparison.rs
@@ -5,10 +5,10 @@ use crate::check_ast::Checker;
 use crate::checks::{Check, CheckKind};
 
 pub fn useless_comparison(checker: &mut Checker, expr: &Expr) {
-    if let ExprKind::Compare { left, .. } = &expr.node {
+    if matches!(expr.node, ExprKind::Compare { .. }) {
         checker.add_check(Check::new(
             CheckKind::UselessComparison,
-            Range::from_located(left),
+            Range::from_located(expr),
         ));
     }
 }
