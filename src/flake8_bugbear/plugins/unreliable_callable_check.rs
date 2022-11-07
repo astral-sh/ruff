9c93bcfa7549340d39e0c269e935d267ddf1f4a3
harupy <hkawamura0130@gmail.com>
2022-11-07 23:22:24 +0900

    Fix clippy error

--- src/flake8_bugbear/plugins/unreliable_callable_check.rs
+++ src/flake8_bugbear/plugins/unreliable_callable_check.rs
@@ -9,14 +9,16 @@ pub fn unreliable_callable_check(checker: &mut Checker, expr: &Expr, func: &Expr
     if let ExprKind::Name { id, .. } = &func.node {
         if id == "getattr" || id == "hasattr" {
             if args.len() >= 2 {
-                if let ExprKind::Constant { value, .. } = &args[1].node {
-                    if let Constant::Str(s) = value {
-                        if s == "__call__" {
-                            checker.add_check(Check::new(
-                                CheckKind::UnreliableCallableCheck,
-                                Range::from_located(expr),
-                            ));
-                        }
+                if let ExprKind::Constant {
+                    value: Constant::Str(s),
+                    ..
+                } = &args[1].node
+                {
+                    if s == "__call__" {
+                        checker.add_check(Check::new(
+                            CheckKind::UnreliableCallableCheck,
+                            Range::from_located(expr),
+                        ));
                     }
                 }
             }
