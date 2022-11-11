bd3b40688fdaa9ca69219552616e415648c5d404
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-11-12 00:26:37 +0900

    Implement B010 (#683)

--- src/flake8_bugbear/plugins/setattr_with_constant.rs
+++ src/flake8_bugbear/plugins/setattr_with_constant.rs
@@ -6,11 +6,11 @@ use crate::checks::{Check, CheckKind};
 use crate::flake8_bugbear::constants::IDENTIFIER_REGEX;
 use crate::python::keyword::KWLIST;
 
-/// B009
-pub fn getattr_with_constant(checker: &mut Checker, expr: &Expr, func: &Expr, args: &[Expr]) {
+/// B010
+pub fn setattr_with_constant(checker: &mut Checker, expr: &Expr, func: &Expr, args: &[Expr]) {
     if let ExprKind::Name { id, .. } = &func.node {
-        if id == "getattr" {
-            if let [_, arg] = args {
+        if id == "setattr" {
+            if let [_, arg, _] = args {
                 if let ExprKind::Constant {
                     value: Constant::Str(value),
                     ..
