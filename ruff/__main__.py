c38adf73f97abe4cc8103098c37fc0f862c94485
Anders Kaseorg <andersk@mit.edu>
2022-11-11 12:23:30 -0800

    Add ruff.__main__ wrapper to allow invocation as ‘python -m ruff’
    Fixes #658.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

---
+++ ruff/__main__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import sysconfig
+
+if __name__ == "__main__":
+    ruff = os.path.join(sysconfig.get_path("scripts"), "ruff")
+    os.spawnv(os.P_WAIT, ruff, [ruff, *sys.argv[1:]])
