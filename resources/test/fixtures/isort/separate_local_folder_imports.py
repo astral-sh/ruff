048a13c7959ca404a3d2912675d09ca405b35c81
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-11-11 22:12:48 -0500

    Add a separate local folder category for imports (#690)

---
+++ resources/test/fixtures/isort/separate_local_folder_imports.py
@@ -0,0 +1,4 @@
+import sys
+import leading_prefix
+import os
+from . import leading_prefix
--- src/isort/categorize.rs
+++ src/isort/categorize.rs
@@ -12,16 +12,20 @@ pub enum ImportType {
     StandardLibrary,
     ThirdParty,
     FirstParty,
+    LocalFolder,
 }
 
 pub fn categorize(
     module_base: &str,
+    level: &Option<usize>,
     src: &[PathBuf],
     known_first_party: &BTreeSet<String>,
     known_third_party: &BTreeSet<String>,
     extra_standard_library: &BTreeSet<String>,
 ) -> ImportType {
-    if known_first_party.contains(module_base) {
+    if level.map(|level| level > 0).unwrap_or(false) {
+        ImportType::LocalFolder
+    } else if known_first_party.contains(module_base) {
         ImportType::FirstParty
     } else if known_third_party.contains(module_base) {
         ImportType::ThirdParty
@@ -31,12 +35,10 @@ pub fn categorize(
         import_type.clone()
     } else if KNOWN_STANDARD_LIBRARY.contains(module_base) {
         ImportType::StandardLibrary
+    } else if find_local(src, module_base) {
+        ImportType::FirstParty
     } else {
-        if find_local(src, module_base) {
-            ImportType::FirstParty
-        } else {
-            ImportType::ThirdParty
-        }
+        ImportType::ThirdParty
     }
 }
 
