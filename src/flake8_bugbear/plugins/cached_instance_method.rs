bdaf1f80c3e3862d095023720814c593cc3bd1f9
harupy <hkawamura0130@gmail.com>
2022-11-12 23:52:53 +0900

    Use match_name_or_attr_from_module

--- src/flake8_bugbear/plugins/cached_instance_method.rs
+++ src/flake8_bugbear/plugins/cached_instance_method.rs
@@ -1,16 +1,23 @@
-use rustpython_ast::Expr;
+use rustpython_ast::{Expr, ExprKind};
 
-use crate::ast::helpers::compose_call_path;
+use crate::ast::helpers::{compose_call_path, match_name_or_attr_from_module};
 use crate::ast::types::{Range, ScopeKind};
 use crate::check_ast::Checker;
 use crate::checks::{Check, CheckKind};
 
-const CACHE_FUNCTIONS: [&str; 4] = [
-    "functools.lru_cache",
-    "functools.cache",
-    "lru_cache",
-    "cache",
-];
+fn is_cache_func(checker: &Checker, expr: &Expr) -> bool {
+    match_name_or_attr_from_module(
+        expr,
+        "lru_cache",
+        "functools",
+        checker.from_imports.get("functools"),
+    ) || match_name_or_attr_from_module(
+        expr,
+        "cache",
+        "functools",
+        checker.from_imports.get("functools"),
+    )
+}
 
 pub fn cached_instance_method(checker: &mut Checker, decorator_list: &[Expr]) {
     if matches!(checker.current_scope().kind, ScopeKind::Class(_)) {
@@ -19,7 +26,11 @@ pub fn cached_instance_method(checker: &mut Checker, decorator_list: &[Expr]) {
                 if decorator_path == "classmethod" || decorator_path == "staticmethod" {
                     return;
                 }
-                if CACHE_FUNCTIONS.contains(&decorator_path.as_str()) {
+                let decorator_attr = match &decorator.node {
+                    ExprKind::Call { func, .. } => func,
+                    _ => decorator,
+                };
+                if is_cache(checker, decorator_attr) {
                     checker.add_check(Check::new(
                         CheckKind::CachedInstanceMethod,
                         Range::from_located(decorator),
