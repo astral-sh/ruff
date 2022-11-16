b4e15635178f2769c8d89825e944c2adcb2fad05
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-11-16 08:55:35 -0500

    Avoid allocations for binding values (#764)

--- src/ast/helpers.rs
+++ src/ast/helpers.rs
@@ -225,6 +225,18 @@ pub fn is_super_call_with_arguments(func: &Expr, args: &[Expr]) -> bool {
     }
 }
 
+/// Format the module name for a relative import.
+pub fn format_import_from(level: &Option<usize>, module: &Option<String>) -> String {
+    let mut module_name = String::with_capacity(16);
+    for _ in 0..level.unwrap_or_default() {
+        module_name.push('.');
+    }
+    if let Some(m) = module {
+        module_name.push_str(m);
+    }
+    module_name
+}
+
 /// Convert a location within a file (relative to `base`) to an absolute
 /// position.
 pub fn to_absolute(relative: &Location, base: &Location) -> Location {
