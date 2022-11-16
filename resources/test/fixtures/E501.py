343a1ec952e4dd43cd50dd08a4bb46d975a5d60d
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-11-16 11:56:57 -0500

    Fix off-by-one in noqa

--- resources/test/fixtures/E501.py
+++ resources/test/fixtures/E501.py
@@ -5,7 +5,7 @@
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 """
 
-_ = """Lorem ipsum dolor sit amet.
+_ = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 
     https://github.com/PyCQA/pycodestyle/pull/258/files#diff-841c622497a8033d10152bfdfb15b20b92437ecdea21a260944ea86b77b51533
 
--- src/noqa.rs
+++ src/noqa.rs
@@ -72,10 +72,7 @@ fn add_noqa_inner(
         // Grab the noqa (logical) line number for the current (physical) line.
         // If there are newlines at the end of the file, they won't be represented in
         // `noqa_line_for`, so fallback to the current line.
-        let noqa_lineno = noqa_line_for
-            .get(&lineno)
-            .map(|lineno| lineno - 1)
-            .unwrap_or(lineno);
+        let noqa_lineno = noqa_line_for.get(&(lineno + 1)).unwrap_or(&(lineno + 1)) - 1;
 
         if !codes.is_empty() {
             let matches = matches_by_line.entry(noqa_lineno).or_default();
