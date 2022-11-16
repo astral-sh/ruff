82fea36bb3120240413370502e15cc67fe2a74ac
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-11-15 22:02:52 -0500

    Preserve comments when sorting imports (#749)

---
+++ resources/test/fixtures/isort/fit_line_length_comment.py
@@ -0,0 +1,4 @@
+import a
+# Don't take this comment into account when determining whether the next import can fit on one line.
+from b import c
+from d import e  # Do take this comment into account when determining whether the next import can fit on one line.
--- src/isort/types.rs
+++ src/isort/types.rs
@@ -1,4 +1,6 @@
-use fnv::{FnvHashMap, FnvHashSet};
+use std::borrow::Cow;
+
+use fnv::FnvHashMap;
 
 #[derive(Debug, Hash, Ord, PartialOrd, Eq, PartialEq)]
 pub struct ImportFromData<'a> {
@@ -10,6 +12,12 @@ pub struct ImportFromData<'a> {
 pub struct AliasData<'a> {
     pub name: &'a str,
     pub asname: &'a Option<String>,
+}
+
+#[derive(Debug, Default)]
+pub struct CommentSet<'a> {
+    pub atop: Vec<Cow<'a, str>>,
+    pub inline: Vec<Cow<'a, str>>,
 }
 
 pub trait Importable {
@@ -50,17 +58,24 @@ impl Importable for ImportFromData<'_> {
 pub struct ImportBlock<'a> {
     // Set of (name, asname), used to track regular imports.
     // Ex) `import module`
-    pub import: FnvHashSet<AliasData<'a>>,
+    pub import: FnvHashMap<AliasData<'a>, CommentSet<'a>>,
     // Map from (module, level) to `AliasData`, used to track 'from' imports.
     // Ex) `from module import member`
-    pub import_from: FnvHashMap<ImportFromData<'a>, FnvHashSet<AliasData<'a>>>,
+    pub import_from:
+        FnvHashMap<ImportFromData<'a>, (CommentSet<'a>, FnvHashMap<AliasData<'a>, CommentSet<'a>>)>,
     // Set of (module, level, name, asname), used to track re-exported 'from' imports.
     // Ex) `from module import member as member`
-    pub import_from_as: FnvHashSet<(ImportFromData<'a>, AliasData<'a>)>,
+    pub import_from_as: FnvHashMap<(ImportFromData<'a>, AliasData<'a>), CommentSet<'a>>,
 }
+
+type AliasDataWithComments<'a> = (AliasData<'a>, CommentSet<'a>);
 
 #[derive(Debug, Default)]
 pub struct OrderedImportBlock<'a> {
-    pub import: Vec<AliasData<'a>>,
-    pub import_from: Vec<(ImportFromData<'a>, Vec<AliasData<'a>>)>,
+    pub import: Vec<AliasDataWithComments<'a>>,
+    pub import_from: Vec<(
+        ImportFromData<'a>,
+        CommentSet<'a>,
+        Vec<AliasDataWithComments<'a>>,
+    )>,
 }
