1a3d2ead41f62bf49ac90a2ba8f9b52eee4c8e4f
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-11-20 17:55:57 -0500

    Support PEP 562 (#841)

--- resources/test/fixtures/N807.py
+++ resources/test/fixtures/N807.py
@@ -10,6 +10,27 @@ def good__():
     pass
 
 
+def nested():
+    def __bad__():
+        pass
+
+    def __good():
+        pass
+
+    def good__():
+        pass
+
+
 class Class:
     def __good__(self):
         pass
+
+
+# https://peps.python.org/pep-0562/
+def __getattr__(name):
+    pass
+
+
+# https://peps.python.org/pep-0562/
+def __dir__():
+    pass
