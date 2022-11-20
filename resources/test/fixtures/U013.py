9467e98d89b5ce098d96f66237e2de2cf6bf6adb
andberger <and.bergsson@gmail.com>
2022-11-20 23:12:02 +0000

    Add with statement and snapshot file for test fixture #640.

--- resources/test/fixtures/U013.py
+++ resources/test/fixtures/U013.py
@@ -6,3 +6,20 @@ open("foo", "r")
 open("foo", "rt")
 open("f", "r", encoding="UTF-8")
 open("f", "wt")
+
+with open("foo", "U") as f:
+    pass
+with open("foo", "Ur") as f:
+    pass
+with open("foo", "Ub") as f:
+    pass
+with open("foo", "rUb") as f:
+    pass
+with open("foo", "r") as f:
+    pass
+with open("foo", "rt") as f:
+    pass
+with open("foo", "r", encoding="UTF-8") as f:
+    pass
+with open("foo", "wt") as f:
+    pass
