ab30c9b12d986fe2697f2f8274dbe25a239a23b0
olliemath <oliver.margetts@gmail.com>
2022-11-24 13:19:23 +0000

    F521: extra test cases

--- resources/test/fixtures/F521.py
+++ resources/test/fixtures/F521.py
@@ -1,6 +1,12 @@
 "{".format(1)
+"}".format(1)
+"{foo[}".format(foo=1)
 # too much string recursion (placeholder-in-placeholder)
 "{:{:{}}}".format(1, 2, 3)
+# ruff picks these issues up, but flake8 doesn't
+"{foo[]}".format(foo={"": 1})
+"{foo..}".format(foo=1)
+"{foo..bar}".format(foo=1)
 
 # "{} {1}".format(1, 2)  # F525
 # "{0} {}".format(1, 2)  # F525
@@ -13,6 +19,7 @@
 # The following are all "good" uses of .format
 "{.__class__}".format("")
 "{foo[bar]}".format(foo={"bar": "barv"})
+"{[bar]}".format({"bar": "barv"})
 "{:{}} {}".format(1, 15, 2)
 "{:2}".format(1)
 "{foo}-{}".format(1, foo=2)
