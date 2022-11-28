b83a6dd1962d209f3eb1be8b18d94fa10b16171f
Anders Kaseorg <andersk@mit.edu>
2022-11-27 21:59:23 -0800

    Fix clippy::manual_let_else (pedantic)
    This is part of nightly clippy::pedantic for MSRV ≥ 1.65.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- src/pyflakes/fixes.rs
+++ src/pyflakes/fixes.rs
@@ -22,14 +22,10 @@ pub fn remove_unused_imports(
     let module_text = locator.slice_source_code_range(&Range::from_located(stmt));
     let mut tree = match_module(&module_text)?;
 
-    let body = if let Some(Statement::Simple(body)) = tree.body.first_mut() {
-        body
-    } else {
+    let Some(Statement::Simple(body)) = tree.body.first_mut() else {
         return Err(anyhow::anyhow!("Expected node to be: Statement::Simple"));
     };
-    let body = if let Some(SmallStatement::Import(body)) = body.body.first_mut() {
-        body
-    } else {
+    let Some(SmallStatement::Import(body)) = body.body.first_mut() else {
         return Err(anyhow::anyhow!(
             "Expected node to be: SmallStatement::ImportFrom"
         ));
@@ -80,22 +76,16 @@ pub fn remove_unused_import_froms(
     let module_text = locator.slice_source_code_range(&Range::from_located(stmt));
     let mut tree = match_module(&module_text)?;
 
-    let body = if let Some(Statement::Simple(body)) = tree.body.first_mut() {
-        body
-    } else {
+    let Some(Statement::Simple(body)) = tree.body.first_mut() else {
         return Err(anyhow::anyhow!("Expected node to be: Statement::Simple"));
     };
-    let body = if let Some(SmallStatement::ImportFrom(body)) = body.body.first_mut() {
-        body
-    } else {
+    let Some(SmallStatement::ImportFrom(body)) = body.body.first_mut() else {
         return Err(anyhow::anyhow!(
             "Expected node to be: SmallStatement::ImportFrom"
         ));
     };
 
-    let aliases = if let ImportNames::Aliases(aliases) = &mut body.names {
-        aliases
-    } else {
+    let ImportNames::Aliases(aliases) = &mut body.names else {
         return Err(anyhow::anyhow!("Expected node to be: Aliases"));
     };
 
