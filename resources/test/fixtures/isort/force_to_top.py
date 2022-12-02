e6f8050764a963f644fe2df1eae03bbcdd04199b
Bhavika Tekwani <bhavika.tekwani@shopify.com>
2022-12-01 19:06:52 -0500

    Write test for force_to_top

---
+++ resources/test/fixtures/isort/force_to_top.py
@@ -0,0 +1,8 @@
+import os
+# This is a comment in the same section, so we need to add one newline.
+import sys
+import numpy as np
+# This is a comment, but it starts a new section, so we don't need to add a newline
+# before it.
+import leading_prefix
+import module_to_force_to_top
