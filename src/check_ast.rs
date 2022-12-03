54a6dcb1bd56db00f213ccd922eb2b031c0dce3b
harupy <hkawamura0130@gmail.com>
2022-12-03 15:34:03 +0900

    Add ;

--- src/check_ast.rs
+++ src/check_ast.rs
@@ -442,7 +442,7 @@ where
                 }
 
                 if self.settings.enabled.contains(&CheckCode::PLR0206) {
-                    pylint::plugins::property_with_parameters(self, stmt, decorator_list, args)
+                    pylint::plugins::property_with_parameters(self, stmt, decorator_list, args);
                 }
 
                 self.check_builtin_shadowing(name, Range::from_located(stmt), true);
