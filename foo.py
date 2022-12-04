f86f795217c3d21d75bb17d8977ba1dd3d0e8b86
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-12-04 13:29:39 -0500

    Fix D205+D400 autofix

---
+++ foo.py
@@ -0,0 +1,12 @@
+"""Stub."""
+
+
+class Ruffed:
+    """Stub."""
+
+    def __call__(self, a, b) -> None:
+        """
+        :param a: table rows
+        :param b: table rows
+        """
+        pass
