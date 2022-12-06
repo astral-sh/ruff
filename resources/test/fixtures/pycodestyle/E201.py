0240bbbc5bd12ddc0b2a63c40598a8ac17a9625b
Nick Anthony <nanthony007@gmail.com>
2022-12-06 15:33:38 -0500

    Add E201 fixture

---
+++ resources/test/fixtures/pycodestyle/E201.py
@@ -0,0 +1,23 @@
+# E201: Whitespace after '(', '[' or '{'
+
+def foo():
+    return (1, 2)
+
+x = [1, 2]
+x2 = [ 1, 2]
+
+y = {1: 2}
+y2 = { 1: 2}
+
+z = (1, 2)
+z2 = ( 1, 2)
+
+
+def fun( x, y):
+    pass
+
+fun(1, 2)
+fun( 1, 3)
+
+my_dict = {'key': 'value'}
+my_dict2 = { 'key': 'value' }
