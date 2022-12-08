e338d9acbe651c7912617bb0f2fc65eecc01db65
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-12-07 20:10:36 -0500

    Remove 'consider' language from check messages (#1135)

--- src/pylint/plugins/mod.rs
+++ src/pylint/plugins/mod.rs
@@ -1,19 +1,19 @@
 pub use await_outside_async::await_outside_async;
-pub use consider_merging_isinstance::consider_merging_isinstance;
-pub use consider_using_from_import::consider_using_from_import;
-pub use consider_using_sys_exit::consider_using_sys_exit;
+pub use merge_isinstance::merge_isinstance;
 pub use misplaced_comparison_constant::misplaced_comparison_constant;
 pub use property_with_parameters::property_with_parameters;
 pub use unnecessary_direct_lambda_call::unnecessary_direct_lambda_call;
+pub use use_from_import::use_from_import;
+pub use use_sys_exit::use_sys_exit;
 pub use useless_else_on_loop::useless_else_on_loop;
 pub use useless_import_alias::useless_import_alias;
 
 mod await_outside_async;
-mod consider_merging_isinstance;
-mod consider_using_from_import;
-mod consider_using_sys_exit;
+mod merge_isinstance;
 mod misplaced_comparison_constant;
 mod property_with_parameters;
 mod unnecessary_direct_lambda_call;
+mod use_from_import;
+mod use_sys_exit;
 mod useless_else_on_loop;
 mod useless_import_alias;
