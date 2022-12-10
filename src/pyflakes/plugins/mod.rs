fea7bb8941a7f6866de5feac229e0fb8c49c5e4d
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-12-10 16:41:24 -0500

    Move string formatting checks to plugins

--- src/pyflakes/plugins/mod.rs
+++ src/pyflakes/plugins/mod.rs
@@ -3,9 +3,18 @@ pub use if_tuple::if_tuple;
 pub use invalid_literal_comparisons::invalid_literal_comparison;
 pub use invalid_print_syntax::invalid_print_syntax;
 pub use raise_not_implemented::raise_not_implemented;
+pub(crate) use strings::{
+    percent_format_expected_mapping, percent_format_expected_sequence,
+    percent_format_extra_named_arguments, percent_format_missing_arguments,
+    percent_format_mixed_positional_and_named, percent_format_positional_count_mismatch,
+    percent_format_star_requires_sequence, string_dot_format_extra_named_arguments,
+    string_dot_format_extra_positional_arguments, string_dot_format_missing_argument,
+    string_dot_format_mixing_automatic,
+};
 
 mod assert_tuple;
 mod if_tuple;
 mod invalid_literal_comparisons;
 mod invalid_print_syntax;
 mod raise_not_implemented;
+mod strings;
