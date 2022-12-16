a4c8ccdb60555e8ede7ffdd12fa4f26ba2585a22
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-12-15 22:22:14 -0600

    test: Fix `flake8-errmsg` snapshots

--- resources/test/fixtures/flake8_errmsg/EM.py
+++ resources/test/fixtures/flake8_errmsg/EM.py
@@ -5,6 +5,10 @@ def f_a():
     raise RuntimeError("This is an example exception")
 
 
+def f_a_short():
+    raise RuntimeError("Error")
+
+
 def f_b():
     example = "example"
     raise RuntimeError(f"This is an {example} exception")
