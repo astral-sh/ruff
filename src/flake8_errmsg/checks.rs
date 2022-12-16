16960e0f7d4f515da215c481197322bfd2092012
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-12-15 21:50:58 -0600

    Do not use `unwrap`

--- src/flake8_errmsg/checks.rs
+++ src/flake8_errmsg/checks.rs
@@ -7,37 +7,38 @@ pub fn check_string_in_exception(exc: &Expr, max_string_length: usize) -> Vec<Ch
     let mut checks = vec![];
 
     if let ExprKind::Call { args, .. } = &exc.node {
-        let first = args.first().unwrap();
-        match &first.node {
-            // Check for string literals
-            ExprKind::Constant {
-                value: Constant::Str(string),
-                ..
-            } => {
-                if string.len() > max_string_length {
-                    checks.push(Check::new(
-                        CheckKind::RawStringInException,
-                        Range::from_located(first),
-                    ));
-                }
-            }
-            // Check for f-strings
-            ExprKind::JoinedStr { .. } => checks.push(Check::new(
-                CheckKind::FStringInException,
-                Range::from_located(first),
-            )),
-            // Check for .format() calls
-            ExprKind::Call { func, .. } => {
-                if let ExprKind::Attribute { value, attr, .. } = &func.node {
-                    if attr == "format" && matches!(value.node, ExprKind::Constant { .. }) {
+        if let Some(first) = args.first() {
+            match &first.node {
+                // Check for string literals
+                ExprKind::Constant {
+                    value: Constant::Str(string),
+                    ..
+                } => {
+                    if string.len() > max_string_length {
                         checks.push(Check::new(
-                            CheckKind::DotFormatInException,
+                            CheckKind::RawStringInException,
                             Range::from_located(first),
                         ));
                     }
                 }
+                // Check for f-strings
+                ExprKind::JoinedStr { .. } => checks.push(Check::new(
+                    CheckKind::FStringInException,
+                    Range::from_located(first),
+                )),
+                // Check for .format() calls
+                ExprKind::Call { func, .. } => {
+                    if let ExprKind::Attribute { value, attr, .. } = &func.node {
+                        if attr == "format" && matches!(value.node, ExprKind::Constant { .. }) {
+                            checks.push(Check::new(
+                                CheckKind::DotFormatInException,
+                                Range::from_located(first),
+                            ));
+                        }
+                    }
+                }
+                _ => {}
             }
-            _ => {}
         }
     }
 
