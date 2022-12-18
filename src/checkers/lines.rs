ae4367336e7ba97546160ab5269d0be284f961b0
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-12-18 18:02:58 -0500

    Only check commented lines

--- src/checkers/lines.rs
+++ src/checkers/lines.rs
@@ -6,7 +6,12 @@ use crate::pygrep_hooks::plugins::blanket_type_ignore;
 use crate::pyupgrade::checks::unnecessary_coding_comment;
 use crate::settings::{flags, Settings};
 
-pub fn check_lines(contents: &str, settings: &Settings, autofix: flags::Autofix) -> Vec<Check> {
+pub fn check_lines(
+    contents: &str,
+    commented_lines: &[usize],
+    settings: &Settings,
+    autofix: flags::Autofix,
+) -> Vec<Check> {
     let mut checks: Vec<Check> = vec![];
 
     let enforce_unnecessary_coding_comment = settings.enabled.contains(&CheckCode::UP009);
@@ -14,36 +19,41 @@ pub fn check_lines(contents: &str, settings: &Settings, autofix: flags::Autofix)
     let enforce_no_newline_at_end_of_file = settings.enabled.contains(&CheckCode::W292);
     let enforce_blanket_type_ignore = settings.enabled.contains(&CheckCode::PGH003);
 
-    for (lineno, line) in contents.lines().enumerate() {
-        // Enforce unnecessary coding comments (UP009).
-        if enforce_unnecessary_coding_comment {
-            if lineno < 2 {
-                if let Some(check) = unnecessary_coding_comment(
-                    lineno,
-                    line,
-                    matches!(autofix, flags::Autofix::Enabled)
-                        && settings.fixable.contains(&CheckCode::UP009),
-                ) {
-                    checks.push(check);
+    let mut commented_lines_iter = commented_lines.iter().peekable();
+    for (index, line) in contents.lines().enumerate() {
+        while commented_lines_iter
+            .next_if(|lineno| &(index + 1) == *lineno)
+            .is_some()
+        {
+            if enforce_unnecessary_coding_comment {
+                if index < 2 {
+                    if let Some(check) = unnecessary_coding_comment(
+                        index,
+                        line,
+                        matches!(autofix, flags::Autofix::Enabled)
+                            && settings.fixable.contains(&CheckCode::UP009),
+                    ) {
+                        checks.push(check);
+                    }
+                }
+            }
+
+            if enforce_blanket_type_ignore {
+                if commented_lines.contains(&(index + 1)) {
+                    if let Some(check) = blanket_type_ignore(index, line) {
+                        checks.push(check);
+                    }
                 }
             }
         }
 
-        // Enforce line length violations (E501).
         if enforce_line_too_long {
-            if let Some(check) = line_too_long(lineno, line, settings.line_length) {
-                checks.push(check);
-            }
-        }
-
-        if enforce_blanket_type_ignore {
-            if let Some(check) = blanket_type_ignore(lineno, line) {
+            if let Some(check) = line_too_long(index, line, settings.line_length) {
                 checks.push(check);
             }
         }
     }
 
-    // Enforce newlines at end of files (W292).
     if enforce_no_newline_at_end_of_file {
         if let Some(check) = no_newline_at_end_of_file(contents) {
             checks.push(check);
@@ -66,6 +76,7 @@ mod tests {
         let check_with_max_line_length = |line_length: usize| {
             check_lines(
                 line,
+                &[],
                 &Settings {
                     line_length,
                     ..Settings::for_rule(CheckCode::E501)
