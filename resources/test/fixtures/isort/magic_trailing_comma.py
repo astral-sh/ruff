c7ed221da29f7663cb0f0aecbb3d0406550273ee
colin99d <colin99delahunty@gmail.com>
2022-12-24 14:45:29 -0500

    Added requested test cases

--- resources/test/fixtures/isort/magic_trailing_comma.py
+++ resources/test/fixtures/isort/magic_trailing_comma.py
@@ -1,3 +1,4 @@
+# This has a magic trailing comma, will be sorted, but not rolled into one line
 from sys import (
     stderr,
     argv,
@@ -5,9 +6,29 @@ from sys import (
     exit,
 )
 
+# No magic comma, this will be rolled into one line
 from os import (
     path,
     environ,
     execl,
     execv
+)
+
+from os import (
+    path,
+    environ,
+    execl,
+    execv,  # Ends with a comment, should still treat as magic trailing comma.
+)
+
+# These will be combined, but without a trailing comma.
+from foo import bar
+from foo import baz
+
+
+# These will be combined, _with_ a trailing comma, I think? Follow `isort` here.
+from foo import bar
+from foo import (
+  baz,
+  bop,
 )
