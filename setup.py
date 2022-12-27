f094b23f94774a56850d31055035c368ba0cab25
Reiner Gerecke <me@reinergerecke.de>
2022-12-27 21:03:22 +0100

    Switch to setuptools-rust for building python package
    maturin doesn't yet support packaging both a binary and a library into
    the same package.
    
    https://github.com/PyO3/maturin/issues/368

--- setup.py
+++ setup.py
@@ -1,23 +1,13 @@
-import sys
-
 from setuptools import setup
+from setuptools_rust import Binding, RustExtension, RustBin, Strip
 
-sys.stderr.write(
-    """
-===============================
-Unsupported installation method
-===============================
-ruff no longer supports installation with `python setup.py install`.
-Please use `python -m pip install .` instead.
-"""
+setup(
+    name="ruff",
+    rust_extensions=[
+        RustExtension("ruff._ruff", binding=Binding.PyO3, strip=Strip.Debug, py_limited_api=True),
+        RustBin("ruff", strip=Strip.Debug),
+    ],
+    packages=["ruff"],
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
 )
-sys.exit(1)
-
-
-# The below code will never execute, however GitHub is particularly
-# picky about where it finds Python packaging metadata.
-# See: https://github.com/github/feedback/discussions/6456
-#
-# To be removed once GitHub catches up.
-
-setup(name="ruff", install_requires=[])
