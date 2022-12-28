9a0fba8bf4a36aa394680a3b8d6bdf952d970a87
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-12-28 16:21:18 -0500

    Use level

--- resources/test/fixtures/pyupgrade/UP023.py
+++ resources/test/fixtures/pyupgrade/UP023.py
@@ -1,12 +1,12 @@
-# These two imports have something after cElementTree, so they should be fixed
+# These two imports have something after cElementTree, so they should be fixed.
 from xml.etree.cElementTree import XML, Element, SubElement
 import xml.etree.cElementTree as ET
 
-# Weird spacing should not cause issues
+# Weird spacing should not cause issues.
 from   xml.etree.cElementTree    import  XML
 import    xml.etree.cElementTree       as      ET
 
-# Multi line imports should also work fine
+# Multi line imports should also work fine.
 from xml.etree.cElementTree import (
     XML,
     Element,
@@ -20,7 +20,7 @@ from xml.etree import cElementTree as ET
 
 import contextlib, xml.etree.cElementTree as ET
 
-# The below items shoudl NOT be changed
+# The below items should NOT be changed.
 import xml.etree.cElementTree
 
 from .xml.etree.cElementTree import XML
--- src/pyupgrade/plugins/rewrite_c_element_tree.rs
+++ src/pyupgrade/plugins/rewrite_c_element_tree.rs
@@ -1,63 +1,59 @@
-use rustpython_ast::{AliasData, Located, Stmt, StmtKind};
+use rustpython_ast::{Stmt, StmtKind};
 
 use crate::ast::types::Range;
 use crate::autofix::Fix;
 use crate::checkers::ast::Checker;
 use crate::checks::{Check, CheckKind};
 
-fn check_from_import(module: &Option<String>, names: &[Located<AliasData>]) -> bool {
-    if module == &Some("xml.etree.cElementTree".to_string()) {
-        return true;
-    } else if module == &Some("xml.etree".to_string()) {
-        let clean_names: Vec<&Located<AliasData>> = names
-            .iter()
-            .filter(|name| name.node.name == "cElementTree")
-            .collect();
-        let item = match clean_names.get(0) {
-            None => return false,
-            Some(thing) => thing,
-        };
-        if item.node.asname.is_some() {
-            return true;
-        }
-    }
-    false
-}
-
 /// UP023
 pub fn replace_c_element_tree(checker: &mut Checker, stmt: &Stmt) {
-    // Please note that for now this is an exact implementation that only does
-    // what pyupgrade does. In the future we could check for cElementTree
-    // anywhere as any type of stmt or expr, and replace it
     if match &stmt.node {
         StmtKind::Import { names } => {
-            let clean_names: Vec<&Located<AliasData>> = names
+            // Ex) `import xml.etree.cElementTree as ET`
+            if let Some(name) = names
                 .iter()
-                .filter(|name| name.node.name == "xml.etree.cElementTree")
-                .collect();
-            let item = match clean_names.get(0) {
-                None => return,
-                Some(thing) => thing,
-            };
-            item.node.asname.is_some()
+                .find(|name| name.node.name == "xml.etree.cElementTree")
+            {
+                name.node.asname.is_some()
+            } else {
+                false
+            }
         }
-        StmtKind::ImportFrom { module, names, .. } => check_from_import(module, names),
-        _ => false,
+        StmtKind::ImportFrom {
+            module,
+            names,
+            level,
+        } => {
+            if level.map_or(false, |level| level > 0) {
+                // Ex) `import .xml.etree.cElementTree as ET`
+                false
+            } else if let Some(module) = module {
+                if module == "xml.etree.cElementTree" {
+                    // Ex) `from xml.etree.cElementTree import XML`
+                    true
+                } else if module == "xml.etree" {
+                    // Ex) `from xml.etree import cElementTree as ET`
+                    if let Some(name) = names.iter().find(|name| name.node.name == "cElementTree") {
+                        name.node.asname.is_some()
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        }
+        _ => unreachable!("Expected StmtKind::Import | StmtKind::ImportFrom"),
     } {
-        let range = Some(Range::from_located(stmt));
-        let selected_contents = checker
-            .locator
-            .slice_source_code_range(&range.unwrap())
-            .to_string();
-        // This is a hacky way to check for a relative import, but the stmt
-        // variable is identical for absolute and relative imports
-        if selected_contents.contains(".xml") {
-            return;
-        }
-        let mut check = Check::new(CheckKind::RewriteCElementTree, range.unwrap());
+        let mut check = Check::new(CheckKind::RewriteCElementTree, Range::from_located(stmt));
         if checker.patch(check.kind.code()) {
+            let contents = checker
+                .locator
+                .slice_source_code_range(&Range::from_located(stmt));
             check.amend(Fix::replacement(
-                selected_contents.replace("cElementTree", "ElementTree"),
+                contents.replace("cElementTree", "ElementTree"),
                 stmt.location,
                 stmt.end_location.unwrap(),
             ));
