24162b97aecd01945184ed27c7a0191ff3def6b7
colin99d <colin99delahunty@gmail.com>
2022-12-29 18:52:56 -0500

    Added fixes

---
+++ resources/test/fixtures/pyupgrade/UP026.py
@@ -0,0 +1,16 @@
+# These should be changed
+if True:
+    import mock
+
+# This goes to from unitest import mock
+import mock.mock
+
+# Mock should go on a new line as `from unittest import mock`
+import contextlib, mock, sys
+
+# Mock should go on a new line as `from unittest import mock`
+import mock, sys
+x = "This code should be preserved one line below the mock"
+
+# Mock should go on a new line as `from unittest import mock`
+from mock import mock
--- src/pyupgrade/plugins/rewrite_mock_import.rs
+++ src/pyupgrade/plugins/rewrite_mock_import.rs
@@ -1,1 +1,5 @@
 use crate::checkers::ast::Checker;
+
+pub fn rewrite_mock_import(checker: &mut Checker) {
+
+}
