c415458a5c0abc3a7cb209254a487f1493b7c614
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-12-29 20:10:52 -0500

    Use strip_kind

--- src/pyupgrade/plugins/rewrite_unicode_literal.rs
+++ src/pyupgrade/plugins/rewrite_unicode_literal.rs
@@ -6,6 +6,17 @@ use crate::checkers::ast::Checker;
 use crate::checks::{Check, CheckKind};
 use crate::pydocstyle::helpers::leading_quote;
 
+/// Strip any leading kind prefixes (e..g. "u") from a quote string.
+fn strip_kind(leading_quote: &str) -> &str {
+    if let Some(index) = leading_quote.find('\'') {
+        &leading_quote[index..]
+    } else if let Some(index) = leading_quote.find('\"') {
+        &leading_quote[index..]
+    } else {
+        unreachable!("Expected docstring to start with a valid triple- or single-quote prefix")
+    }
+}
+
 pub fn rewrite_unicode_literal(
     checker: &mut Checker,
     expr: &Expr,
@@ -14,21 +25,22 @@ pub fn rewrite_unicode_literal(
 ) {
     if let Some(const_kind) = kind {
         if const_kind.to_lowercase() == "u" {
-            let range = Range::from_located(expr);
-            let content = checker.locator.slice_source_code_range(&range);
-            // We need to skip the first item since it is always a u
-            let quotes = leading_quote(&content).unwrap_or("u\"")[1..].to_string();
-            let mut check = Check::new(CheckKind::RewriteUnicodeLiteral, range);
+            let mut check = Check::new(CheckKind::RewriteUnicodeLiteral, Range::from_located(expr));
             if checker.patch(check.kind.code()) {
-                let mut contents = String::new();
-                contents.push_str(&quotes);
-                contents.push_str(value);
-                contents.push_str(&quotes);
-                check.amend(Fix::replacement(
-                    contents,
-                    expr.location,
-                    expr.end_location.unwrap(),
-                ));
+                let content = checker
+                    .locator
+                    .slice_source_code_range(&Range::from_located(expr));
+                if let Some(leading_quote) = leading_quote(&content).map(strip_kind) {
+                    let mut contents = String::with_capacity(value.len() + leading_quote.len() * 2);
+                    contents.push_str(leading_quote);
+                    contents.push_str(value);
+                    contents.push_str(leading_quote);
+                    check.amend(Fix::replacement(
+                        contents,
+                        expr.location,
+                        expr.end_location.unwrap(),
+                    ));
+                }
             }
             checker.add_check(check);
         }
