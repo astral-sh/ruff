25899b0692661800cf422181da831bf81abe7160
colin99d <colin99delahunty@gmail.com>
2022-12-30 11:15:30 -0500

    Added fix

--- src/pyupgrade/plugins/rewrite_unicode_literal.rs
+++ src/pyupgrade/plugins/rewrite_unicode_literal.rs
@@ -10,18 +10,12 @@ pub fn rewrite_unicode_literal(checker: &mut Checker, expr: &Expr, kind: &Option
         if const_kind.to_lowercase() == "u" {
             let mut check = Check::new(CheckKind::RewriteUnicodeLiteral, Range::from_located(expr));
             if checker.patch(check.kind.code()) {
-                let mut content = checker
-                    .locator
-                    .slice_source_code_range(&Range::from_located(expr))
-                    .to_string();
-                let first_char = content.remove(0);
-                if first_char == 'u' || first_char == 'U' {
-                    check.amend(Fix::replacement(
-                        content,
-                        expr.location,
-                        expr.end_location.unwrap(),
-                    ));
-                }
+                let mut new_location = expr.location.clone();
+                new_location.go_right();
+                check.amend(Fix::deletion(
+                    expr.location,
+                    new_location,
+                ));
             }
             checker.add_check(check);
         }
