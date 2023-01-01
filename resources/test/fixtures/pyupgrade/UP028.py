9aad274da9b7e8acb3ce2cf9c78402386c8f138b
colin99d <colin99delahunty@gmail.com>
2023-01-01 16:00:34 -0500

    Began adding tests

--- resources/test/fixtures/pyupgrade/UP028.py
+++ resources/test/fixtures/pyupgrade/UP028.py
@@ -6,3 +6,40 @@ def f():
 def g():
     for x, y in z:
         yield (x, y)
+
+
+def h():
+    for x in [1, 2, 3]:
+        yield x
+
+
+def i():
+    for x in {x for x in y}:
+        yield x
+
+
+def j():
+    for x in (1, 2, 3):
+        yield x
+
+
+def k():
+    for x, y in {3: "x", 6: "y"}:
+        yield x, y
+
+
+def f():  # Comment one\n'
+    # Comment two\n'
+    for x, y in {  # Comment three\n'
+        3: "x",  # Comment four\n'
+        # Comment five\n'
+        6: "y",  # Comment six\n'
+    }:  # Comment seven\n'
+        # Comment eight\n'
+        yield x, y  # Comment nine\n'
+        # Comment ten',
+
+
+def f():
+    for x, y in [{3: (3, [44, "long ss"]), 6: "y"}]:
+        yield x, y
