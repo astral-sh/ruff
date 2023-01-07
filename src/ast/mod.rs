d603b66e4b0f2efb51d22b5029acfbd48fca004e
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-01-07 16:06:21 -0500

    Comparable expr

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,6 +1,6 @@
 pub mod branch_detection;
 pub mod cast;
-pub mod cmp;
+pub mod comparable;
 pub mod function_type;
 pub mod helpers;
 pub mod operations;
--- src/pyflakes/plugins/repeated_keys.rs
+++ src/pyflakes/plugins/repeated_keys.rs
@@ -1,29 +1,25 @@
 use std::hash::{BuildHasherDefault, Hash};
 
-use rustc_hash::FxHashMap;
+use rustc_hash::{FxHashMap, FxHashSet};
 use rustpython_ast::{Expr, ExprKind};
 
-use crate::ast::cmp;
+use crate::ast::comparable::{ComparableConstant, ComparableExpr};
 use crate::ast::helpers::unparse_expr;
 use crate::ast::types::Range;
 use crate::autofix::Fix;
 use crate::checkers::ast::Checker;
 use crate::registry::{Check, CheckCode};
-use crate::source_code_style::SourceCodeStyleDetector;
 use crate::violations;
 
 #[derive(Debug, Eq, PartialEq, Hash)]
 enum DictionaryKey<'a> {
-    Constant(String),
+    Constant(ComparableConstant<'a>),
     Variable(&'a str),
 }
 
-fn into_dictionary_key<'a>(
-    expr: &'a Expr,
-    stylist: &SourceCodeStyleDetector,
-) -> Option<DictionaryKey<'a>> {
+fn into_dictionary_key(expr: &Expr) -> Option<DictionaryKey> {
     match &expr.node {
-        ExprKind::Constant { .. } => Some(DictionaryKey::Constant(unparse_expr(expr, stylist))),
+        ExprKind::Constant { value, .. } => Some(DictionaryKey::Constant(value.into())),
         ExprKind::Name { id, .. } => Some(DictionaryKey::Variable(id)),
         _ => None,
     }
@@ -32,23 +28,26 @@ fn into_dictionary_key<'a>(
 /// F601, F602
 pub fn repeated_keys(checker: &mut Checker, keys: &[Expr], values: &[Expr]) {
     // Generate a map from key to (index, value).
-    let mut seen: FxHashMap<DictionaryKey, Vec<&Expr>> =
+    let mut seen: FxHashMap<DictionaryKey, FxHashSet<ComparableExpr>> =
         FxHashMap::with_capacity_and_hasher(keys.len(), BuildHasherDefault::default());
 
     // Detect duplicate keys.
     for (i, key) in keys.iter().enumerate() {
-        if let Some(key) = into_dictionary_key(key, checker.style) {
+        if let Some(key) = into_dictionary_key(key) {
             if let Some(seen_values) = seen.get_mut(&key) {
                 match key {
-                    DictionaryKey::Constant(key) => {
+                    DictionaryKey::Constant(..) => {
                         if checker.settings.enabled.contains(&CheckCode::F601) {
-                            let repeated_value =
-                                seen_values.iter().any(|value| cmp::expr(value, &values[i]));
+                            let comparable_value: ComparableExpr = (&values[i]).into();
+                            let is_duplicate_value = seen_values.contains(&comparable_value);
                             let mut check = Check::new(
-                                violations::MultiValueRepeatedKeyLiteral(key, repeated_value),
+                                violations::MultiValueRepeatedKeyLiteral(
+                                    unparse_expr(&keys[i], checker.style),
+                                    is_duplicate_value,
+                                ),
                                 Range::from_located(&keys[i]),
                             );
-                            if repeated_value {
+                            if is_duplicate_value {
                                 if checker.patch(&CheckCode::F601) {
                                     check.amend(Fix::deletion(
                                         values[i - 1].end_location.unwrap(),
@@ -56,23 +55,23 @@ pub fn repeated_keys(checker: &mut Checker, keys: &[Expr], values: &[Expr]) {
                                     ));
                                 }
                             } else {
-                                seen_values.push(&values[i]);
+                                seen_values.insert(comparable_value);
                             }
                             checker.checks.push(check);
                         }
                     }
                     DictionaryKey::Variable(key) => {
                         if checker.settings.enabled.contains(&CheckCode::F602) {
-                            let repeated_value =
-                                seen_values.iter().any(|value| cmp::expr(value, &values[i]));
+                            let comparable_value: ComparableExpr = (&values[i]).into();
+                            let is_duplicate_value = seen_values.contains(&comparable_value);
                             let mut check = Check::new(
                                 violations::MultiValueRepeatedKeyVariable(
                                     key.to_string(),
-                                    repeated_value,
+                                    is_duplicate_value,
                                 ),
                                 Range::from_located(&keys[i]),
                             );
-                            if repeated_value {
+                            if is_duplicate_value {
                                 if checker.patch(&CheckCode::F602) {
                                     check.amend(Fix::deletion(
                                         values[i - 1].end_location.unwrap(),
@@ -80,14 +79,14 @@ pub fn repeated_keys(checker: &mut Checker, keys: &[Expr], values: &[Expr]) {
                                     ));
                                 }
                             } else {
-                                seen_values.push(&values[i]);
+                                seen_values.insert(comparable_value);
                             }
                             checker.checks.push(check);
                         }
                     }
                 }
             } else {
-                seen.insert(key, vec![&values[i]]);
+                seen.insert(key, FxHashSet::from_iter([(&values[i]).into()]));
             }
         }
     }
