24e99746a2a5980ee2909bf0a69f2648b6845a1b
Matthias Geier <Matthias.Geier@gmail.com>
2023-01-08 12:07:34 +0100

    Remove redundant __name__ == "__main__"
    The name of the module is "__main__" anyway.

--- ruff/__main__.py
+++ ruff/__main__.py
@@ -2,6 +2,5 @@ import os
 import sys
 import sysconfig
 
-if __name__ == "__main__":
-    ruff = os.path.join(sysconfig.get_path("scripts"), "ruff")
-    sys.exit(os.spawnv(os.P_WAIT, ruff, [ruff, *sys.argv[1:]]))
+ruff = os.path.join(sysconfig.get_path("scripts"), "ruff")
+sys.exit(os.spawnv(os.P_WAIT, ruff, [ruff, *sys.argv[1:]]))
