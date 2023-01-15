dbd5339d496f5b58ff97c0e4ace7c5731389cc70
Ran Benita <ran@unusedvar.com>
2023-01-15 10:49:45 +0200

    fmt

--- src/rules/flake8_commas/mod.rs
+++ src/rules/flake8_commas/mod.rs
@@ -18,7 +18,11 @@ mod tests {
             Path::new("./resources/test/fixtures/flake8_commas")
                 .join(path)
                 .as_path(),
-            &settings::Settings::for_rules(vec![RuleCode::COM812, RuleCode::COM818, RuleCode::COM819]),
+            &settings::Settings::for_rules(vec![
+                RuleCode::COM812,
+                RuleCode::COM818,
+                RuleCode::COM819,
+            ]),
         )?;
         insta::assert_yaml_snapshot!(snapshot, diagnostics);
         Ok(())
