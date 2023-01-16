0da754313472c5b1e210ec48ebacc7f259094e7e
colin99d <colin99delahunty@gmail.com>
2023-01-15 21:53:22 -0500

    Got all positive tests in

--- resources/test/fixtures/pyupgrade/UP032.py
+++ resources/test/fixtures/pyupgrade/UP032.py
@@ -1,1 +1,27 @@
-'{}{}{}{foo} {bar}'.format(x, y, z, foo=hello, bar=world)
+# These SHOULD change
+"{} {}".format(a, b)
+
+"{1} {0}".format(a, b)
+
+"{x.y}".format(x=z)
+
+"{.x} {.y}".format(a, b)
+
+"{} {}".format(a.b, c.d)
+
+"{}".format(a())
+
+"{}".format(a.b())
+
+"{}".format(a.b().c())
+
+"hello {}!".format(name)
+
+"{}{{}}{}".format(escaped, y)
+
+"{}{b}{}".format(a, c, b=b)
+
+"{}".format(0x0)
+
+# Waiting on Charlie solution to solve this
+# r'"\N{snowman} {}".format(a)'
