2ed1f78873a0655c9039de7a6fb2a2bcdaf3dac8
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-01-16 17:38:40 -0500

    Add benchmark scripts for no-IO (#1925)

---
+++ scripts/benchmarks/run_all.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+###
+# Benchmark Ruff's performance against a variety of similar tools, suppressing output as much as
+# possible (so as to reduce I/O overhead).
+###
+
+# Note: Flake8's `checker.py` requires the following variant of `mp_run`:
+#   def _mp_run(filename: str) -> tuple[str, Results, dict[str, int]]:
+#       try:
+#           return FileChecker(
+#               filename=filename, plugins=_mp_plugins, options=_mp_options
+#           ).run_checks()
+#       except:
+#           return (filename, [], {
+#               "files": 0,
+#               "logical lines": 0,
+#               "physical lines": 0,
+#               "tokens": 0,
+#           })
+
+hyperfine --ignore-failure --warmup 5 \
+  "./target/release/ruff ./resources/test/cpython/ --no-cache --silent --select ALL" \
+  "flake8 resources/test/cpython -qq --docstring-convention=all" \
+  "pycodestyle resources/test/cpython -qq" \
+  "pylint resources/test/cpython -j 0  --recursive=y --disable=E,W,C,R"
