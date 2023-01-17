a4862857de638274a6c753ecbc56459d90d3b0d1
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-01-16 19:29:14 -0500

    Update PIE796 fixture

--- resources/test/fixtures/flake8_pie/PIE796.py
+++ resources/test/fixtures/flake8_pie/PIE796.py
@@ -1,38 +1,43 @@
+import enum
+from enum import Enum, unique
+
+
 class FakeEnum(enum.Enum):
     A = "A"
     B = "B"
-    C = "B" # PIE796
+    C = "B"  # PIE796
 
 
 class FakeEnum2(Enum):
     A = 1
     B = 2
-    C = 2 # PIE796
+    C = 2  # PIE796
 
 
 class FakeEnum3(str, Enum):
     A = "1"
     B = "2"
-    C = "2" # PIE796
+    C = "2"  # PIE796
+
 
 class FakeEnum4(Enum):
     A = 1.0
     B = 2.5
-    C = 2.5 # PIE796
+    C = 2.5  # PIE796
 
 
 class FakeEnum5(Enum):
     A = 1.0
     B = True
     C = False
-    D = False # PIE796
+    D = False  # PIE796
 
 
 class FakeEnum6(Enum):
     A = 1
     B = 2
     C = None
-    D = None # PIE796
+    D = None  # PIE796
 
 
 @enum.unique
@@ -41,14 +46,15 @@ class FakeEnum7(enum.Enum):
     B = "B"
     C = "C"
 
+
 @unique
 class FakeEnum8(Enum):
     A = 1
     B = 2
-    C = 2 # PIE796
+    C = 2  # PIE796
+
 
 class FakeEnum9(enum.Enum):
     A = "A"
     B = "B"
     C = "C"
-
