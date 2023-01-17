c15a3543412528e1105e605f8deabf7dd0f1a05c
colin99d <colin99delahunty@gmail.com>
2023-01-16 20:10:11 -0500

    Added pyupgrade tests

--- resources/test/fixtures/pyupgrade/UP033.py
+++ resources/test/fixtures/pyupgrade/UP033.py
@@ -5,12 +5,36 @@ print(("hell((goodybe))o"))
 
 print((("foo")))
 
+print((((1))))
+
+print(("foo{}".format(1)))
+
+print(
+    ("foo{}".format(1))
+)
+
+print(
+    (
+        "foo"
+    )
+)
+
+def f():
+    x = int(((yield 1)))
+
 # These SHOULD NOT work
 print("foo")
+
+print((1, 2, 3))
 
 print(())
 
 print((1,))
+
+sum((block.code for block in blocks), [])
+
+def f():
+    x = int((yield 1))
 
 sum((i for i in range(3)), [])
 
