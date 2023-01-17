3e9f728df811b4f5d2d6bac26a8eacdf58cc61c8
colin99d <colin99delahunty@gmail.com>
2023-01-16 20:27:15 -0500

    Simplified to a broken curly_escape

--- src/rules/pyupgrade/helpers.rs
+++ src/rules/pyupgrade/helpers.rs
@@ -1,11 +1,3 @@
-use once_cell::sync::Lazy;
-use regex::Regex;
-
-// FOR REVIEWER: I had to rewrite this from the python implementation, so please
-// review carefully original regex was: (?<!\\)(?:\\\\)*(\\N\{[^}]+\})
-static NAMED_UNICODE_RE: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r"^(\\)(?:\\\\)*(\\N\{[^}]+\})").unwrap());
-
 const KEYWORDS: [&str; 35] = [
     "False", "None", "True", "and", "as", "assert", "async", "await", "break", "class", "continue",
     "def", "del", "elif", "else", "except", "finally", "for", "from", "global", "if", "import",
@@ -13,25 +5,12 @@ const KEYWORDS: [&str; 35] = [
     "with", "yield",
 ];
 
-/// Equivalent to the python regex fullmatch: <https://docs.python.org/3/library/re.html>
-fn full_match(regex: &Lazy<Regex>, string: &str) -> bool {
-    match regex.find(string) {
-        None => false,
-        Some(item) => item.as_str() == string,
-    }
-}
-
+/// This function currently does not work with python emoji synatx. For example "/N{dog}" will be
+/// converted to "/N{{dog}}" which is NOT correct and WILL cause issues. Please check for emoji
+/// syntax before using this command, and if it exists, issue a warning instead of attempting to
+/// fix
 pub fn curly_escape(string: &str) -> String {
-    let mut final_str = String::new();
-    let parts = NAMED_UNICODE_RE.split(string);
-    for part in parts {
-        if full_match(&NAMED_UNICODE_RE, part) {
-            final_str.push_str(part);
-        } else {
-            final_str.push_str(&part.replace('{', "{{").replace('}', "}}"));
-        }
-    }
-    final_str
+    string.replace('{', "{{").replace('}', "}}")
 }
 
 /// Whether or not a given string is a python keyword
