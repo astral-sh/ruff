e42661b3e046c8fc31d30af5d645b4124d7912c9
Zeddicus414 <zeddicus414@gmail.com>
2023-01-21 12:54:25 -0600

    Add some test data for ICN001.
    Next step, actually add real tests.

--- resources/test/fixtures/flake8_import_conventions/from_import.py
+++ resources/test/fixtures/flake8_import_conventions/from_import.py
@@ -1,2 +1,7 @@
+# Test absolute imports
 import xml.dom.minidom as wrong
 from xml.dom import minidom as wrong
+from xml.dom.minidom import parseString as wrong  # Ensure ICN001 works on function import.
+
+# Test relative imports
+from ..xml.dom import minidom as okay  # Ensure config "xml.dom.minidom" doesn't catch relative imports
