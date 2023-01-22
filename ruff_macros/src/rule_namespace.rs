991d3c1ef662e6dfb01fd3680caea1339fc2376b
Martin Fischer <martin@push-f.com>
2023-01-22 21:19:16 +0100

    refactor: Move Linter::url and Linter::name generation to proc macro
    This lets us get rid of the build.rs script and results
    in more developer-friendly compile error messages.

--- ruff_macros/src/rule_namespace.rs
+++ ruff_macros/src/rule_namespace.rs
@@ -13,6 +13,8 @@ pub fn derive_impl(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> 
     let mut parsed = Vec::new();
 
     let mut prefix_match_arms = quote!();
+    let mut name_match_arms = quote!(Self::Ruff => "Ruff-specific rules",);
+    let mut url_match_arms = quote!(Self::Ruff => None,);
 
     for variant in variants {
         let prefix_attrs: Vec<_> = variant
@@ -28,17 +30,33 @@ pub fn derive_impl(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> 
             ));
         }
 
+        let Some(doc_attr) = variant.attrs.iter().find(|a| a.path.is_ident("doc")) else {
+            return Err(Error::new(variant.span(), r#"expected a doc comment"#))
+        };
+
+        let variant_ident = variant.ident;
+
+        if variant_ident != "Ruff" {
+            let Ok(Meta::NameValue(MetaNameValue{lit: Lit::Str(doc_lit), ..})) = doc_attr.parse_meta() else {
+                return Err(Error::new(doc_attr.span(), r#"expected doc attribute to be in the form of [#doc = "..."]"#))
+            };
+            let doc_lit = doc_lit.value();
+            let Some((name, url)) = parse_markdown_link(doc_lit.trim()) else {
+                return Err(Error::new(doc_attr.span(), r#"expected doc comment to be in the form of `/// [name](https://example.com/)`"#))
+            };
+            name_match_arms.extend(quote! {Self::#variant_ident => #name,});
+            url_match_arms.extend(quote! {Self::#variant_ident => Some(#url),});
+        }
+
         let mut prefix_literals = Vec::new();
 
         for attr in prefix_attrs {
             let Ok(Meta::NameValue(MetaNameValue{lit: Lit::Str(lit), ..})) = attr.parse_meta() else {
                 return Err(Error::new(attr.span(), r#"expected attribute to be in the form of [#prefix = "..."]"#))
             };
-            parsed.push((lit.clone(), variant.ident.clone()));
+            parsed.push((lit.clone(), variant_ident.clone()));
             prefix_literals.push(lit);
         }
-
-        let variant_ident = variant.ident;
 
         prefix_match_arms.extend(quote! {
             Self::#variant_ident => &[#(#prefix_literals),*],
@@ -82,6 +100,14 @@ pub fn derive_impl(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> 
             fn prefixes(&self) -> &'static [&'static str] {
                 match self { #prefix_match_arms }
             }
+
+            fn name(&self) -> &'static str {
+                match self { #name_match_arms }
+            }
+
+            fn url(&self) -> Option<&'static str> {
+                match self { #url_match_arms }
+            }
         }
 
         impl IntoIterator for &#ident {
@@ -97,4 +123,8 @@ pub fn derive_impl(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> 
             }
         }
     })
+}
+
+fn parse_markdown_link(link: &str) -> Option<(&str, &str)> {
+    link.strip_prefix('[')?.strip_suffix(')')?.split_once("](")
 }
