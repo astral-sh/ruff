f811a842bb063e01cd92ed4cc6c6db1c6b9a6e57
colin99d <colin99delahunty@gmail.com>
2023-01-22 15:54:05 -0500

    Added more tests for six.moves

--- resources/test/fixtures/pyupgrade/UP035_1.py
+++ resources/test/fixtures/pyupgrade/UP035_1.py
@@ -1,3 +1,4 @@
+# These SHOULD change
 from six.moves import tkinter, lololol, queue
 
 from six.moves import tkinter as hello, lololol, queue
@@ -5,3 +6,24 @@ from six.moves import tkinter as hello, lololol, queue
 from six.moves import dbm_gnu
 
 from six.moves import dbm_gnu as gnud
+
+from six.moves import getcwd, StringIO
+
+if True:
+    from six.moves import queue, urllib_request
+
+from six.moves import urllib_request, filter, getcwd
+
+from six.moves import filter, getcwd
+
+from six.moves.queue import Queue
+
+from six.moves import queue
+
+from six.moves import queue, map, getcwd
+
+# These should NOT change
+if True: from six.moves import get_cwd, StringIO
+
+if True:from six.moves import get_cwd, StringIO
+
