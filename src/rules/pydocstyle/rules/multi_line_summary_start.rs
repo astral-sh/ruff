72095ff352b64cb22d9dadeb7fb1af2b9ab95efe
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-01-24 08:16:58 -0500

    Use checker.stylist.line_ending

--- src/rules/pydocstyle/rules/multi_line_summary_start.rs
+++ src/rules/pydocstyle/rules/multi_line_summary_start.rs
@@ -35,17 +35,18 @@ pub fn multi_line_summary_start(checker: &mut Checker, docstring: &Docstring) {
                 Range::from_located(docstring.expr),
             );
             if checker.patch(diagnostic.kind.rule()) {
-                let loc = docstring.expr.location;
-                let mut endl = loc.row() + 1;
+                let location = docstring.expr.location;
+                let mut end_row = location.row() + 1;
                 // Delete until first non-whitespace char.
                 for line in content_lines {
-                    if let Some(endc) = line.find(|c: char| !c.is_whitespace()) {
-                        let start = Location::new(loc.row(), loc.column() + first_line.len());
-                        let end = Location::new(endl, endc);
+                    if let Some(end_column) = line.find(|c: char| !c.is_whitespace()) {
+                        let start =
+                            Location::new(location.row(), location.column() + first_line.len());
+                        let end = Location::new(end_row, end_column);
                         diagnostic.amend(Fix::deletion(start, end));
                         break;
                     }
-                    endl += 1;
+                    end_row += 1;
                 }
             }
             checker.diagnostics.push(diagnostic);
@@ -62,41 +63,47 @@ pub fn multi_line_summary_start(checker: &mut Checker, docstring: &Docstring) {
             );
             if checker.patch(diagnostic.kind.rule()) {
                 let mut indentation = String::from(docstring.indentation);
-                let mut fix = true;
+                let mut fixable = true;
                 if !indentation.chars().all(char::is_whitespace) {
-                    fix = false;
+                    fixable = false;
+
+                    // If the docstring isn't on its own line, look at the parent indentation, and
+                    // add the default indentation to get the "right" level.
                     if let DefinitionKind::Class(parent)
                     | DefinitionKind::NestedClass(parent)
                     | DefinitionKind::Function(parent)
                     | DefinitionKind::NestedFunction(parent)
                     | DefinitionKind::Method(parent) = &docstring.kind
                     {
-                        let pindentation = checker.locator.slice_source_code_range(&Range::new(
-                            Location::new(parent.location.row(), 0),
-                            Location::new(parent.location.row(), parent.location.column()),
-                        ));
-                        if pindentation.chars().all(char::is_whitespace) {
+                        let parent_indentation =
+                            checker.locator.slice_source_code_range(&Range::new(
+                                Location::new(parent.location.row(), 0),
+                                Location::new(parent.location.row(), parent.location.column()),
+                            ));
+                        if parent_indentation.chars().all(char::is_whitespace) {
                             indentation.clear();
-                            indentation.push_str(pindentation);
+                            indentation.push_str(parent_indentation);
                             indentation.push_str(checker.stylist.indentation());
-                            fix = true;
+                            fixable = true;
                         }
                     };
                 }
-                if fix {
-                    let loc = docstring.expr.location;
+
+                if fixable {
+                    let location = docstring.expr.location;
                     let prefix = leading_quote(contents).unwrap();
                     // Use replacement instead of insert to trim possible whitespace between leading
                     // quote and text.
                     let repl = format!(
-                        "\n{}{}",
+                        "{}{}{}",
+                        checker.stylist.line_ending().as_str(),
                         indentation,
                         first_line.strip_prefix(prefix).unwrap().trim_start()
                     );
                     diagnostic.amend(Fix::replacement(
                         repl,
-                        Location::new(loc.row(), loc.column() + prefix.len()),
-                        Location::new(loc.row(), loc.column() + first_line.len()),
+                        Location::new(location.row(), location.column() + prefix.len()),
+                        Location::new(location.row(), location.column() + first_line.len()),
                     ));
                 }
             }
