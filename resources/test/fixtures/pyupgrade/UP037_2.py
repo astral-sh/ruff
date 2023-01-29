f153fded07d65644d776ac540098b14568f8f105
colin99d <colin99delahunty@gmail.com>
2023-01-29 17:38:44 -0500

    Added some negative tests

--- resources/test/fixtures/pyupgrade/UP037_2.py
+++ resources/test/fixtures/pyupgrade/UP037_2.py
@@ -38,3 +38,32 @@ else:
 
 if sys.version_info >= (3, 5):
     pass
+
+# These below tests should NOT change
+if six.PY2:
+    pass
+
+if True:
+    if six.PY2:
+        pass
+
+if six.PY2:
+    pass
+elif False:
+    pass
+
+if six.PY3:
+    pass
+elif False:
+    pass
+
+if sys.version_info[0] > "2":
+    3
+else:
+    2
+
+from .sys import version_info
+if version_info < (3,):
+    print(2)
+else:
+    print(3)
