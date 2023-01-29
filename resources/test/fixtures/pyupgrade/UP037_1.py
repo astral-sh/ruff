0092cd0e9f347c6cc52d045f00db4e32f297939d
colin99d <colin99delahunty@gmail.com>
2023-01-29 10:20:01 -0500

    Added more test cases

---
+++ resources/test/fixtures/pyupgrade/UP037_1.py
@@ -0,0 +1,64 @@
+import sys
+import six
+
+if sys.version_info == 2:
+    2
+else:
+    3
+
+if sys.version_info < (3,):
+    2
+else:
+    3
+
+if sys.version_info < (3,0):
+    2
+else:
+    3
+
+if sys.version_info == 3:
+    3
+else:
+    2
+
+if sys.version_info > (3,):
+    3
+else:
+    2
+
+if sys.version_info >= (3,):
+    3
+else:
+    2
+
+from sys import version_info
+
+if version_info > (3,):
+    3
+else:
+    2
+
+if True:
+    print(1)
+elif six.PY2:
+    print(2)
+else:
+    print(3)
+
+if True:
+    print(1)
+elif six.PY3:
+    print(3)
+else:
+    print(2)
+
+if True:
+    print(1)
+elif six.PY3:
+    print(3)
+
+def f():
+    if True:
+        print(1)
+    elif six.PY3:
+        print(3)
