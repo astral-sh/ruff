2746abb0137f29d126d003ba79c5caf01feebea0
colin99d <colin99delahunty@gmail.com>
2023-01-29 17:12:25 -0500

    Added second set of tests

---
+++ resources/test/fixtures/pyupgrade/UP037_2.py
@@ -0,0 +1,37 @@
+import sys
+from sys import version_info
+
+if sys.version_info > (3, 5):
+    3+6
+else:
+    3-5
+
+if version_info > (3, 5):
+    3+6
+else:
+    3-5
+
+if sys.version_info >= (3,6):
+    3+6
+else:
+    3-5
+
+if version_info >= (3,6):
+    3+6
+else:
+    3-5
+
+if sys.version_info < (3,6):
+    3-5
+else:
+    3+6
+
+if sys.version_info <= (3,5):
+    3-5
+else:
+    3+6
+
+if sys.version_info <= (3, 5):
+    3-5
+else:
+    3+6
