f9a1404e6f57a91ad3ec4c3a2d41bbb2be9609f9
colin99d <colin99delahunty@gmail.com>
2023-01-28 23:10:04 -0500

    Added more test cases

--- resources/test/fixtures/pyupgrade/UP037.py
+++ resources/test/fixtures/pyupgrade/UP037.py
@@ -1,5 +1,6 @@
 import sys
 import six
+from six import PY2, PY3
 
 if six.PY2:
     print("py2")
@@ -39,3 +40,42 @@ if not six.PY2:
     print("py3")
 else:
     print("py2")
+
+
+x = 1
+
+if not six.PY2:
+    print("py3")
+else:
+    print("py2")
+    # ohai
+
+x = 1
+
+if not six.PY2: print("py3")
+else: print("py2")
+
+if six.PY3:
+    print("py3")
+else:
+    print("py2")
+
+if True:
+    if six.PY3:
+        print("py3")
+    else:
+        print("py2")
+
+if not PY3:
+    print("py2")
+else:
+    print("py3")
+
+def f():
+    if six.PY2:
+        try:
+            yield
+        finally:
+            pass
+    else:
+        yield
