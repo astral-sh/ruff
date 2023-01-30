5ac5b69e9fc92d6094012a2a0935c00ed5fc44ed
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2023-01-30 15:36:19 -0500

    [`I001`] fix isort for files with tab-based indentation (#2361)
    This PR fixes two related issues with using isort on files using tabs for indentation:
    
    - Multiline imports are never considered correctly formatted, since the comparison with the generated code will always fail.
    - Using autofix generates code that can have mixed indentation in the same line, for imports that are within nested blocks.

---
+++ resources/test/fixtures/isort/preserve_tabs.py
@@ -0,0 +1,29 @@
+from numpy import (
+	cos,
+	int8,
+	int16,
+	int32,
+	int64,
+	sin,
+	tan,
+	uint8,
+	uint16,
+	uint32,
+	uint64,
+)
+
+if True:
+	# inside nested block
+	from numpy import (
+		cos,
+		int8,
+		int16,
+		int32,
+		int64,
+		sin,
+		tan,
+		uint8,
+		uint16,
+		uint32,
+		uint64,
+	)
