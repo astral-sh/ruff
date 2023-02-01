726f2f3ea70758b34a32dc385a565c30b2d26fa1
colin99d <colin99delahunty@gmail.com>
2023-01-31 20:06:15 -0500

    Going to conter to libcst

--- src/rules/pyupgrade/rules/quoted_annotations.rs
+++ src/rules/pyupgrade/rules/quoted_annotations.rs
@@ -46,6 +46,34 @@ fn remove_quotes(checker: &mut Checker, annotation: &Box<Expr>) {
     }
 }
 
+fn process_call(node: &Expr) -> Vec<&Expr> {
+    let to_add: Vec<&Expr> = vec![];
+    if let ExprKind::Call {
+        func,
+        args,
+        keywords,
+    } = &node.node
+    {
+        println!("{:?}", func);
+        return to_add;
+    }
+    to_add
+}
+
+fn replace_string_literal(checker: &mut Checker, annotation: &Box<Expr>) {
+    let mut nodes: Vec<&Expr> = vec![annotation.as_ref()];
+    while nodes.len() > 0 {
+        let node = match nodes.pop() {
+            Some(item) => item,
+            None => continue,
+        };
+        match node.node {
+            ExprKind::Call { .. } => nodes.extend(process_call(node)),
+            _ => continue,
+        }
+    }
+}
+
 /// UP037
 pub fn quoted_annotations_funcdef(
     checker: &mut Checker,
@@ -54,7 +82,7 @@ pub fn quoted_annotations_funcdef(
 ) {
     println!("{:?}", the_return);
     if let Some(return_item) = &the_return {
-        remove_quotes(checker, return_item);
+        replace_string_literal(checker, return_item);
     }
     let arg_list = argument_list(args);
     for argument in arg_list {
@@ -62,11 +90,12 @@ pub fn quoted_annotations_funcdef(
             Some(item) => item,
             None => continue,
         };
-        remove_quotes(checker, annotate);
+        replace_string_literal(checker, annotate);
     }
 }
 
 /// UP037
 pub fn quoted_annotations_annassign(checker: &mut Checker, annotation: &Box<Expr>) {
-    remove_quotes(checker, annotation);
+    println!("STARTING");
+    replace_string_literal(checker, annotation);
 }
