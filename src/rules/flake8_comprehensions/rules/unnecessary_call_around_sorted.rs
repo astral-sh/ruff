1d3646cb08cc7dca89d1916ace4f1e7a58e848ec
Aarni Koskela <akx@iki.fi>
2023-02-02 15:35:15 +0200

    Move flake8-comprehensions violations to rule files

---
+++ src/rules/flake8_comprehensions/rules/unnecessary_call_around_sorted.rs
@@ -0,0 +1,73 @@
+use super::helpers;
+use crate::ast::types::Range;
+use crate::checkers::ast::Checker;
+use crate::define_violation;
+use crate::registry::Diagnostic;
+use crate::rules::flake8_comprehensions::fixes;
+use crate::violation::AlwaysAutofixableViolation;
+use log::error;
+use ruff_macros::derive_message_formats;
+use rustpython_ast::{Expr, ExprKind};
+
+define_violation!(
+    pub struct UnnecessaryCallAroundSorted {
+        pub func: String,
+    }
+);
+impl AlwaysAutofixableViolation for UnnecessaryCallAroundSorted {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let UnnecessaryCallAroundSorted { func } = self;
+        format!("Unnecessary `{func}` call around `sorted()`")
+    }
+
+    fn autofix_title(&self) -> String {
+        let UnnecessaryCallAroundSorted { func } = self;
+        format!("Remove unnecessary `{func}` call")
+    }
+}
+
+/// C413
+pub fn unnecessary_call_around_sorted(
+    checker: &mut Checker,
+    expr: &Expr,
+    func: &Expr,
+    args: &[Expr],
+) {
+    let Some(outer) = helpers::function_name(func) else {
+        return;
+    };
+    if !(outer == "list" || outer == "reversed") {
+        return;
+    }
+    let Some(arg) = args.first() else {
+        return;
+    };
+    let ExprKind::Call { func, .. } = &arg.node else {
+        return;
+    };
+    let Some(inner) = helpers::function_name(func) else {
+        return;
+    };
+    if inner != "sorted" {
+        return;
+    }
+    if !checker.is_builtin(inner) || !checker.is_builtin(outer) {
+        return;
+    }
+    let mut diagnostic = Diagnostic::new(
+        UnnecessaryCallAroundSorted {
+            func: outer.to_string(),
+        },
+        Range::from_located(expr),
+    );
+    if checker.patch(diagnostic.kind.rule()) {
+        match fixes::fix_unnecessary_call_around_sorted(checker.locator, checker.stylist, expr) {
+            Ok(fix) => {
+                diagnostic.amend(fix);
+            }
+            Err(e) => error!("Failed to generate fix: {e}"),
+        }
+    }
+    checker.diagnostics.push(diagnostic);
+}
---
+++ src/rules/flake8_comprehensions/rules/unnecessary_double_cast_or_process.rs
@@ -0,0 +1,90 @@
+use super::helpers;
+use crate::ast::types::Range;
+use crate::checkers::ast::Checker;
+use crate::define_violation;
+use crate::registry::Diagnostic;
+
+use crate::violation::Violation;
+use ruff_macros::derive_message_formats;
+use rustpython_ast::{Expr, ExprKind};
+
+define_violation!(
+    pub struct UnnecessaryDoubleCastOrProcess {
+        pub inner: String,
+        pub outer: String,
+    }
+);
+impl Violation for UnnecessaryDoubleCastOrProcess {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let UnnecessaryDoubleCastOrProcess { inner, outer } = self;
+        format!("Unnecessary `{inner}` call within `{outer}()`")
+    }
+}
+
+/// C414
+pub fn unnecessary_double_cast_or_process(
+    checker: &mut Checker,
+    expr: &Expr,
+    func: &Expr,
+    args: &[Expr],
+) {
+    fn diagnostic(inner: &str, outer: &str, location: Range) -> Diagnostic {
+        Diagnostic::new(
+            UnnecessaryDoubleCastOrProcess {
+                inner: inner.to_string(),
+                outer: outer.to_string(),
+            },
+            location,
+        )
+    }
+
+    let Some(outer) = helpers::function_name(func) else {
+        return;
+    };
+    if !(outer == "list"
+        || outer == "tuple"
+        || outer == "set"
+        || outer == "reversed"
+        || outer == "sorted")
+    {
+        return;
+    }
+    let Some(arg) = args.first() else {
+        return;
+    };
+    let ExprKind::Call { func, .. } = &arg.node else {
+        return;
+    };
+    let Some(inner) = helpers::function_name(func) else {
+        return;
+    };
+    if !checker.is_builtin(inner) || !checker.is_builtin(outer) {
+        return;
+    }
+
+    // Ex) set(tuple(...))
+    if (outer == "set" || outer == "sorted")
+        && (inner == "list" || inner == "tuple" || inner == "reversed" || inner == "sorted")
+    {
+        checker
+            .diagnostics
+            .push(diagnostic(inner, outer, Range::from_located(expr)));
+        return;
+    }
+
+    // Ex) list(tuple(...))
+    if (outer == "list" || outer == "tuple") && (inner == "list" || inner == "tuple") {
+        checker
+            .diagnostics
+            .push(diagnostic(inner, outer, Range::from_located(expr)));
+        return;
+    }
+
+    // Ex) set(set(...))
+    if outer == "set" && inner == "set" {
+        checker
+            .diagnostics
+            .push(diagnostic(inner, outer, Range::from_located(expr)));
+    }
+}
