74bde39d247d74f99ded67d9a24fc6a094eb4586
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-03 09:05:43 -0500

    Move Clippy configuration to config.toml

--- flake8_to_ruff/src/main.rs
+++ flake8_to_ruff/src/main.rs
@@ -1,18 +1,4 @@
 //! Utility to generate Ruff's `pyproject.toml` section from a Flake8 INI file.
-#![forbid(unsafe_code)]
-#![warn(clippy::pedantic)]
-#![allow(
-    clippy::collapsible_else_if,
-    clippy::collapsible_if,
-    clippy::implicit_hasher,
-    clippy::match_same_arms,
-    clippy::missing_errors_doc,
-    clippy::missing_panics_doc,
-    clippy::module_name_repetitions,
-    clippy::must_use_candidate,
-    clippy::similar_names,
-    clippy::too_many_lines
-)]
 
 use std::path::PathBuf;
 
--- ruff_dev/src/main.rs
+++ ruff_dev/src/main.rs
@@ -1,20 +1,6 @@
 //! This crate implements an internal CLI for developers of Ruff.
 //!
 //! Within the ruff repository you can run it with `cargo dev`.
-#![forbid(unsafe_code)]
-#![warn(clippy::pedantic)]
-#![allow(
-    clippy::collapsible_else_if,
-    clippy::collapsible_if,
-    clippy::implicit_hasher,
-    clippy::match_same_arms,
-    clippy::missing_errors_doc,
-    clippy::missing_panics_doc,
-    clippy::module_name_repetitions,
-    clippy::must_use_candidate,
-    clippy::similar_names,
-    clippy::too_many_lines
-)]
 
 mod generate_all;
 mod generate_cli_help;
