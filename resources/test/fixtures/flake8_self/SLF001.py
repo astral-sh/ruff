4909acb7ca83fb2e5b89bb727a0f27d979a23538
Maksudul Haque <saad.mk112@gmail.com>
2023-02-03 16:42:29 +0600

    [flake8-self] Fix False Negative Issue on Rule SLF001

--- resources/test/fixtures/flake8_self/SLF001.py
+++ resources/test/fixtures/flake8_self/SLF001.py
@@ -1,12 +1,22 @@
+class Bar:
+    _private = True
+
+
 class Foo:
 
     def __init__(self):
         self.public_thing = "foo"
         self._private_thing = "bar"
         self.__really_private_thing = "baz"
+        self.bar = Bar()
 
     def __str__(self):
         return "foo"
+
+    def get_bar():
+        if self.bar._private:  # SLF001
+            return None
+        return self.bar
 
     def public_func(self):
         pass
@@ -29,3 +39,4 @@ print(foo._private_thing)  # SLF001
 print(foo.__really_private_thing)  # SLF001
 print(foo._private_func())  # SLF001
 print(foo.__really_private_func(1))  # SLF001
+print(foo.bar._private)  # SLF001
