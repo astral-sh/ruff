c43028623c1a620d5dd0b50c3c30cd76432dcff4
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-03 11:33:28 -0500

    Tweak message

--- ruff_cli/src/main.rs
+++ ruff_cli/src/main.rs
@@ -3,17 +3,18 @@ use std::path::PathBuf;
 use std::process::ExitCode;
 use std::sync::mpsc::channel;
 
-use ::ruff::logging::{set_up_logging, LogLevel};
-use ::ruff::resolver::PyprojectDiscovery;
-use ::ruff::settings::types::SerializationFormat;
-use ::ruff::{fix, fs, warn_user_once};
 use anyhow::Result;
-use args::{Args, CheckArgs, Command};
 use clap::{CommandFactory, Parser, Subcommand};
 use colored::Colorize;
 use notify::{recommended_watcher, RecursiveMode, Watcher};
+
+use ::ruff::logging::{set_up_logging, LogLevel};
+use ::ruff::resolver::PyprojectDiscovery;
+use ::ruff::settings::types::SerializationFormat;
+use ::ruff::settings::CliSettings;
+use ::ruff::{fix, fs, warn_user_once};
+use args::{Args, CheckArgs, Command};
 use printer::{Printer, Violations};
-use ruff::settings::CliSettings;
 
 pub(crate) mod args;
 mod cache;
@@ -256,7 +257,7 @@ fn check(args: CheckArgs, log_level: LogLevel) -> Result<ExitCode> {
 
         if update_check {
             warn_user_once!(
-                "update-check has been removed; setting it will be an error in a future version."
+                "update-check has been removed; setting it will cause an error in a future version."
             );
         }
 
