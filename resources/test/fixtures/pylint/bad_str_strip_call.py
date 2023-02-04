04fc7aa039a140c6f21795bbabc583972dc5a238
colin99d <colin99delahunty@gmail.com>
2023-02-04 11:47:55 -0500

    Got some tests fixed

--- resources/test/fixtures/pylint/bad_str_strip_call.py
+++ resources/test/fixtures/pylint/bad_str_strip_call.py
@@ -1,1 +1,12 @@
 "Hello World".strip("Hello")
+
+"Hello World".strip("Hello")
+
+"Hello World".strip("🤣🤣🤣🤣🙃👀😀")
+
+
+"Hello World".strip(
+    """
+there are a lot of characters I would like to strip today, including $ and @ and maybe even 9
+"""
+)
