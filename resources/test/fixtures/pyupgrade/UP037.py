8908e1e1a0c37c0c2e624dd834e0c0b4f85fb51d
colin99d <colin99delahunty@gmail.com>
2023-02-03 21:42:06 -0500

    Added all tests

--- resources/test/fixtures/pyupgrade/UP037.py
+++ resources/test/fixtures/pyupgrade/UP037.py
@@ -12,3 +12,69 @@ def foo(*args: "str", **kwargs: "int"):
 x: Tuple["MyClass"]
 
 x: Callable[["MyClass"], None]
+
+class Foo(NamedTuple):
+    x: "MyClass"
+
+class D(TypedDict):
+    E: TypedDict("E", foo="int", total=False)
+
+class D(typing.TypedDict):
+    E: TypedDict("E", {"foo": "int"})
+
+x: Annotated["str", "metadata"]
+
+x: Arg("str", "name")
+
+x: DefaultArg("str", "name")
+
+x: NamedArg("str", "name")
+
+x: DefaultNamedArg("str", "name")
+
+x: DefaultNamedArg("str", name="name")
+
+x: VarArg("str")
+
+x: List[List[List["MyClass"]]]
+
+x: NamedTuple("X", [("foo", "int"), ("bar", "str")])
+
+x: NamedTuple("X", fields=[("foo", "int"), ("bar", "str")])
+
+x: NamedTuple(typename="X", fields=[("foo", "int")])
+
+
+# These should NOT change
+class D(TypedDict):
+    E: TypedDict("E")
+
+
+x: Annotated[()]
+
+x: DefaultNamedArg(name="name", quox="str")
+
+x: DefaultNamedArg(name="name")
+
+x: NamedTuple("X", [("foo",), ("bar",)])
+
+x: NamedTuple("X", ["foo", "bar"])
+
+x: NamedTuple()
+
+x: Literal["foo", "bar"]
+
+x = TypeVar("x", "str")
+
+x = cast(x, "str")
+
+X = List["MyClass"]
+
+X: MyCallable("X")
+
+
+def foo(x, *args, **kwargs): ...
+
+def foo(*, inplace): ...
+
+x: Annotated[1:2] = ...
