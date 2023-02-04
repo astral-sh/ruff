a147283d79a5480c2443c3f3ae897cbf9f9c6fab
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-04 16:31:51 -0500

    Follow-up fixes for __main__.py

--- python/ruff/__main__.py
+++ python/ruff/__main__.py
@@ -3,23 +3,29 @@ import sys
 import sysconfig
 from pathlib import Path
 
-RUFF_PATHS = [
-    Path(sysconfig.get_config_var("userbase")) / "bin" / "ruff",
-    Path(sysconfig.get_path("scripts")) / "ruff",
-]
-
 
 def find_ruff_bin() -> Path:
     """Return the ruff binary path."""
-    for ruff_path in RUFF_PATHS:
+    ruff_path = Path(sysconfig.get_path("scripts")) / "ruff"
+    if ruff_path.is_file():
+        return ruff_path
+
+    if sys.version_info >= (3, 10):
+        ruff_path = (
+            Path(
+                sysconfig.get_path(
+                    "scripts",
+                    scheme=sysconfig.get_preferred_scheme("user"),
+                ),
+            )
+            / "ruff"
+        )
         if ruff_path.is_file():
             return ruff_path
+
     raise FileNotFoundError(ruff_path)
 
 
 if __name__ == "__main__":
-    try:
-        ruff = find_ruff_bin()
-    except FileNotFoundError as e:
-        raise FileNotFoundError(e) from e
+    ruff = find_ruff_bin()
     sys.exit(os.spawnv(os.P_WAIT, ruff, [ruff, *sys.argv[1:]]))
