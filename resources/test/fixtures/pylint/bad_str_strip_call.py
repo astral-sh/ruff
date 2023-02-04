2a415b90be9fc1a4f600291ec55ae96297e1fb70
colin99d <colin99delahunty@gmail.com>
2023-02-04 11:53:05 -0500

    Handled another edge case

--- resources/test/fixtures/pylint/bad_str_strip_call.py
+++ resources/test/fixtures/pylint/bad_str_strip_call.py
@@ -10,3 +10,7 @@
 there are a lot of characters I would like to strip today, including $ and @ and maybe even 9
 """
 )
+
+"Hello World".strip("can we get a stupidly long" \
+                    "string of characters to strip" \
+                    "please?")
