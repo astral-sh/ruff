54a7c3f0f858d99ec51a4d50746d65aa01527b8a
colin99d <colin99delahunty@gmail.com>
2023-02-04 23:06:22 -0500

    Added pylint tests

--- resources/test/fixtures/pylint/bad_str_strip_call.py
+++ resources/test/fixtures/pylint/bad_str_strip_call.py
@@ -1,3 +1,4 @@
+# These SHOULD have a warning
 "Hello World".strip("Hello")
 
 "Hello World".strip("Hello")
@@ -14,3 +15,13 @@ there are a lot of characters I would like to strip today, including $ and @ and
 "Hello World".strip("can we get a stupidly long" \
                     "string of characters to strip" \
                     "please?")
+
+u''.strip('http://')  # [bad-str-strip-call]
+u''.lstrip('http://')  # [bad-str-strip-call]
+b''.rstrip('http://')  # [bad-str-strip-call]
+
+
+# These should NOT have a warning
+''.strip('yo')
+''.strip()
+
