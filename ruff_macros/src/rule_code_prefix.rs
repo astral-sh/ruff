590d065ec005a37a6f92ece6701634d090a0cdb9
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-05 12:10:28 -0500

    Propagate attribute-like macros in define_rule_mapping

--- ruff_macros/src/rule_code_prefix.rs
+++ ruff_macros/src/rule_code_prefix.rs
@@ -1,22 +1,23 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 
 use proc_macro2::Span;
 use quote::quote;
-use syn::Ident;
+use syn::{Attribute, Ident};
 
 pub fn expand<'a>(
     rule_type: &Ident,
     prefix_ident: &Ident,
     variants: impl Iterator<Item = &'a Ident>,
     variant_name: impl Fn(&str) -> &'a Ident,
+    attr: impl Iterator<Item = &'a Vec<Attribute>>,
 ) -> proc_macro2::TokenStream {
     // Build up a map from prefix to matching RuleCodes.
-    let mut prefix_to_codes: BTreeMap<String, BTreeSet<String>> = BTreeMap::default();
+    let mut prefix_to_codes: BTreeMap<String, BTreeMap<String, Vec<Attribute>>> =
+        BTreeMap::default();
 
-    let mut all_codes = BTreeSet::new();
-    let mut pl_codes = BTreeSet::new();
+    let mut pl_codes = BTreeMap::new();
 
-    for variant in variants {
+    for (variant, attr) in variants.zip(attr) {
         let code_str = variant.to_string();
         let code_prefix_len = code_str
             .chars()
@@ -28,19 +29,21 @@ pub fn expand<'a>(
             prefix_to_codes
                 .entry(prefix)
                 .or_default()
-                .insert(code_str.clone());
+                .entry(code_str.clone())
+                .or_insert_with(|| attr.clone());
         }
         if code_str.starts_with("PL") {
-            pl_codes.insert(code_str.to_string());
+            pl_codes.insert(code_str, attr.clone());
         }
-        all_codes.insert(code_str);
     }
 
     prefix_to_codes.insert("PL".to_string(), pl_codes);
 
-    let prefix_variants = prefix_to_codes.keys().map(|prefix| {
+    let prefix_variants = prefix_to_codes.iter().map(|(prefix, codes)| {
         let prefix = Ident::new(prefix, Span::call_site());
+        let attr = if_all_same(codes.values().cloned()).unwrap_or_default();
         quote! {
+            #(#attr)*
             #prefix
         }
     });
@@ -74,24 +77,26 @@ pub fn expand<'a>(
 fn generate_impls<'a>(
     rule_type: &Ident,
     prefix_ident: &Ident,
-    prefix_to_codes: &BTreeMap<String, BTreeSet<String>>,
+    prefix_to_codes: &BTreeMap<String, BTreeMap<String, Vec<Attribute>>>,
     variant_name: impl Fn(&str) -> &'a Ident,
 ) -> proc_macro2::TokenStream {
     let into_iter_match_arms = prefix_to_codes.iter().map(|(prefix_str, codes)| {
-        let codes = codes.iter().map(|code| {
+        let prefix = Ident::new(prefix_str, Span::call_site());
+        let attr = if_all_same(codes.values().cloned()).unwrap_or_default();
+        let codes = codes.iter().map(|(code, attr)| {
             let rule_variant = variant_name(code);
             quote! {
+                #(#attr)*
                 #rule_type::#rule_variant
             }
         });
-        let prefix = Ident::new(prefix_str, Span::call_site());
-
         quote! {
+            #(#attr)*
             #prefix_ident::#prefix => vec![#(#codes),*].into_iter(),
         }
     });
 
-    let specificity_match_arms = prefix_to_codes.keys().map(|prefix_str| {
+    let specificity_match_arms = prefix_to_codes.iter().map(|(prefix_str, codes)| {
         let prefix = Ident::new(prefix_str, Span::call_site());
         let mut num_numeric = prefix_str.chars().filter(|char| char.is_numeric()).count();
         if prefix_str != "PL" && prefix_str.starts_with("PL") {
@@ -106,7 +111,9 @@ fn generate_impls<'a>(
             5 => quote! { Specificity::Code5Chars },
             _ => panic!("Invalid prefix: {prefix}"),
         };
+        let attr = if_all_same(codes.values().cloned()).unwrap_or_default();
         quote! {
+            #(#attr)*
             #prefix_ident::#prefix => #suffix_len,
         }
     });
@@ -134,5 +141,16 @@ fn generate_impls<'a>(
                 }
             }
         }
+    }
+}
+
+/// If all values in an iterator are the same, return that value. Otherwise, return `None`.
+fn if_all_same<T: PartialEq>(iter: impl Iterator<Item = T>) -> Option<T> {
+    let mut iter = iter.peekable();
+    let first = iter.next()?;
+    if iter.all(|x| x == first) {
+        Some(first)
+    } else {
+        None
     }
 }
