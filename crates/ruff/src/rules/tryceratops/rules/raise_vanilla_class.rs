16c3cf5a25763124b8c206408d6bb1e52d846f9f
nm-remarkable <nuno.mendes@remarkable.no>
2023-02-08 11:03:34 +0100

    doc: add documentation for TRY002

--- crates/ruff/src/rules/tryceratops/rules/raise_vanilla_class.rs
+++ crates/ruff/src/rules/tryceratops/rules/raise_vanilla_class.rs
@@ -7,6 +7,39 @@ use crate::registry::Diagnostic;
 use crate::violation::Violation;
 
 define_violation!(
+    /// ### What it does
+    /// Checks for bare exceptions.
+    ///
+    /// ## Why is this bad?
+    /// It's hard to capture generic exceptions making it hard for handling specific scenarios.
+    ///
+    ///## Example
+    ///```py
+    /// def main_function():
+    ///     if not cond:
+    ///         raise Exception()
+    /// def consumer_func():
+    ///     try:
+    ///         do_step()
+    ///         prepare()
+    ///         main_function()
+    ///     except Exception:
+    ///         logger.error("I have no idea what went wrong!!")
+    ///```
+    ///## How it should be
+    ///```py
+    /// def main_function():
+    ///     if not cond:
+    ///         raise CustomException()
+    /// def consumer_func():
+    ///     try:
+    ///         do_step()
+    ///         prepare()
+    ///         main_function()
+    ///     except CustomException:
+    ///         logger.error("Main function failed")
+    ///     except Exception:
+    ///         logger.error("I have no idea what went wrong!!")
     pub struct RaiseVanillaClass;
 );
 impl Violation for RaiseVanillaClass {
