6081f6388008debe9a90d67b0cd8aa02b880e241
colin99d <colin99delahunty@gmail.com>
2023-02-10 18:32:54 -0500

    Got tests working

--- crates/ruff/resources/test/fixtures/flake8_simplify/SIM114.py
+++ crates/ruff/resources/test/fixtures/flake8_simplify/SIM114.py
@@ -4,9 +4,57 @@ if a:
 elif c:
     b
 
-# These SHOULD NOT change
+if x == 1:
+    for _ in range(20):
+        print("hello")
+elif x == 2:
+    for _ in range(20):
+        print("hello")
 
-"""
+if x == 1:
+    if True:
+        for _ in range(20):
+            print("hello")
+elif x == 2:
+    if True:
+        for _ in range(20):
+            print("hello")
+
+if x == 1:
+    if True:
+        for _ in range(20):
+            print("hello")
+    elif False:
+        for _ in range(20):
+            print("hello")
+elif x == 2:
+    if True:
+        for _ in range(20):
+            print("hello")
+    elif False:
+        for _ in range(20):
+            print("hello")
+
+if (
+    x == 1
+    and y == 2
+    and z == 3
+    and a == 4
+    and b == 5
+    and c == 6
+    and d == 7
+    and e == 8
+    and f == 9
+    and g == 10
+    and h == 11
+    and i == 12
+    and j == 13
+    and k == 14
+):
+    pass
+elif 1 == 2: pass
+
+# These SHOULD NOT change
 def complicated_calc(*arg, **kwargs):
     return 42
 
@@ -18,6 +66,7 @@ def foo(p):
         return complicated_calc(microsecond=0, second=0)
     return None
 
+
 a = False
 b = True
 c = True
@@ -28,4 +77,3 @@ elif b:
     z = 2
 elif c:
     z = 1
-"""
