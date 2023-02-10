ae7cf235a10e7bc4fbddb7726c25c6782987f9d3
tomecki <tomek.dziopa@gmail.com>
2023-02-10 10:30:18 +0100

    fix merge issues

--- crates/ruff/src/rules/pylint/rules/init_is_generator.rs
+++ crates/ruff/src/rules/pylint/rules/init_is_generator.rs
@@ -1,10 +1,9 @@
-use ruff_macros::derive_message_formats;
-use rustpython_ast::Expr;
+use ruff_macros::{define_violation, derive_message_formats};
+use rustpython_parser::ast::Expr;
 
 use crate::{
     ast::types::{FunctionDef, Range, ScopeKind},
     checkers::ast::Checker,
-    define_violation,
     registry::Diagnostic,
     violation::Violation,
 };
