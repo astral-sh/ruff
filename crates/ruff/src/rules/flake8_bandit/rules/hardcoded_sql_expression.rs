57193ef17c08a14af9f949e22f52f2ba9ebc69ed
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-09 19:23:45 -0500

    Add docs

--- crates/ruff/src/rules/flake8_bandit/rules/hardcoded_sql_expression.rs
+++ crates/ruff/src/rules/flake8_bandit/rules/hardcoded_sql_expression.rs
@@ -16,6 +16,19 @@ static SQL_REGEX: Lazy<Regex> = Lazy::new(|| {
 });
 
 define_violation!(
+    /// ### What it does
+    /// Checks for strings that resemble SQL statements involved in some form
+    /// string building operation.
+    ///
+    /// ### Why is this bad?
+    /// SQL injection is a common attack vector for web applications. Unless care
+    /// is taken to sanitize and control the input data when building such
+    /// SQL statement strings, an injection attack becomes possible.
+    ///
+    /// ### Example
+    /// ```python
+    /// query = "DELETE FROM foo WHERE id = '%s'" % identifier
+    /// ```
     pub struct HardcodedSQLExpression {
         pub string: String,
     }
