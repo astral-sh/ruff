f623c28ad97caed631363c0e065e1daafc2cecee
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-10 19:37:17 -0500

    Use H2

--- crates/ruff/src/rules/flake8_tidy_imports/relative_imports.rs
+++ crates/ruff/src/rules/flake8_tidy_imports/relative_imports.rs
@@ -27,10 +27,10 @@ pub enum Strictness {
 }
 
 define_violation!(
-    /// ### What it does
+    /// ## What it does
     /// Checks for relative imports.
     ///
-    /// ### Why is this bad?
+    /// ## Why is this bad?
     /// According to [PEP8](https://peps.python.org/pep-0008/#imports):
     /// > Absolute imports are recommended, as they are usually more readable and tend to be better behaved.
     pub struct RelativeImports(pub Strictness);
--- docs/rules/relative-imports.md
+++ docs/rules/relative-imports.md
@@ -4,9 +4,9 @@ Derived from the **flake8-tidy-imports** linter.
 
 Autofix is sometimes available.
 
-### What it does
+## What it does
 Checks for relative imports.
 
-### Why is this bad?
+## Why is this bad?
 According to [PEP8](https://peps.python.org/pep-0008/#imports):
 > Absolute imports are recommended, as they are usually more readable and tend to be better behaved.
