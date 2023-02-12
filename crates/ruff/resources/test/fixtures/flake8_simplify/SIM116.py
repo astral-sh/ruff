b4b8c96392563c5d7ab5b8973188cf80b61810cf
colin99d <colin99delahunty@gmail.com>
2023-02-12 15:02:57 -0500

    Added a test

--- crates/ruff/resources/test/fixtures/flake8_simplify/SIM116.py
+++ crates/ruff/resources/test/fixtures/flake8_simplify/SIM116.py
@@ -10,6 +10,13 @@ elif a == "boo":
 else:
     return 42
 
+if a == 1:
+    return (1, 2, 3)
+elif a == 2:
+    return (4, 5, 6)
+elif a == 3:
+    return (7, 8, 9)
+
 # These Should NOT change
 if a == "foo":
     return "bar"
