c450e77f39cdcbf9f057efc694f46a52c2e68786
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-11 23:56:45 -0500

    Remove public re-export of commands

--- crates/ruff_cli/src/commands/mod.rs
+++ crates/ruff_cli/src/commands/mod.rs
@@ -1,18 +1,9 @@
-pub use add_noqa::add_noqa;
-pub use clean::clean;
-pub use linter::linter;
-pub use rule::rule;
-pub use run::run;
-pub use run_stdin::run_stdin;
-pub use show_files::show_files;
-pub use show_settings::show_settings;
-
-mod add_noqa;
-mod clean;
+pub mod add_noqa;
+pub mod clean;
 pub mod config;
-mod linter;
-mod rule;
-mod run;
-mod run_stdin;
-mod show_files;
-mod show_settings;
+pub mod linter;
+pub mod rule;
+pub mod run;
+pub mod run_stdin;
+pub mod show_files;
+pub mod show_settings;
