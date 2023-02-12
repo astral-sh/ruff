72ba884f6c09f7bb531628eed876bf7894880401
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-12 12:51:09 -0500

    Refactor fixtures

--- crates/ruff/resources/test/fixtures/flake8_pyi/PYI008.pyi
+++ crates/ruff/resources/test/fixtures/flake8_pyi/PYI008.pyi
@@ -1,11 +1,11 @@
 import sys
 
-if sys.platform == 'linus': ...  # Error: PYI008 Unrecognized platform `linus`
+if sys.platform == "linus": ...  # Error: PYI008 Unrecognized platform `linus`
 
-if sys.platform != 'linux': ...  # OK
+if sys.platform != "linux": ...  # OK
 
-if sys.platform == 'win32': ...  # OK
+if sys.platform == "win32": ...  # OK
 
-if sys.platform != 'darwin': ...  # OK
+if sys.platform != "darwin": ...  # OK
 
-if sys.platform == 'cygwin': ...  # OK
+if sys.platform == "cygwin": ...  # OK
