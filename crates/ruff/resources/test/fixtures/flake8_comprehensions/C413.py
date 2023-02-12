2dccb7611a23a6881bc90a691d8734f6a8e4876f
Simon Brugman <sbrugman@users.noreply.github.com>
2023-02-12 16:56:07 +0100

    [`flake8-comprehensions`] bugfix for C413 autofix (#2804)

--- crates/ruff/resources/test/fixtures/flake8_comprehensions/C413.py
+++ crates/ruff/resources/test/fixtures/flake8_comprehensions/C413.py
@@ -4,7 +4,9 @@ list(sorted(x))
 reversed(sorted(x))
 reversed(sorted(x, key=lambda e: e))
 reversed(sorted(x, reverse=True))
-
+reversed(sorted(x, key=lambda e: e, reverse=True))
+reversed(sorted(x, reverse=True, key=lambda e: e))
+reversed(sorted(x, reverse=False))
 
 def reversed(*args, **kwargs):
     return None
