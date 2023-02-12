fc4c927788c5ec6f4d62d74b36723ef06d792991
Martin Fischer <martin@push-f.com>
2023-02-12 06:21:56 +0100

    refactor: Introduce ConfigurationOptions::get method

--- crates/ruff_cli/src/commands/config.rs
+++ crates/ruff_cli/src/commands/config.rs
@@ -6,43 +6,31 @@ use ruff::settings::{
 use crate::ExitStatus;
 
 #[allow(clippy::print_stdout)]
-pub(crate) fn config(option: Option<&str>) -> ExitStatus {
-    let entries = Options::get_available_options();
-    let mut entries = &entries;
+pub(crate) fn config(key: Option<&str>) -> ExitStatus {
+    let Some(entry) = Options::get(key) else {
+        println!("Unknown option");
+        return ExitStatus::Error;
+    };
 
-    let mut parts_iter = option.iter().flat_map(|s| s.split('.'));
-
-    while let Some(part) = parts_iter.next() {
-        let Some((_, field)) = entries.iter().find(|(name, _)| *name == part) else {
-            println!("Unknown option");
-            return ExitStatus::Error;
-        };
-        match field {
-            OptionEntry::Field(OptionField {
-                doc,
-                default,
-                value_type,
-                example,
-            }) => {
-                if parts_iter.next().is_some() {
-                    println!("Unknown option");
-                    return ExitStatus::Error;
-                }
-
-                println!("{doc}");
-                println!();
-                println!("Default value: {default}");
-                println!("Type: {value_type}");
-                println!("Example usage:\n```toml\n{example}\n```");
-                return ExitStatus::Success;
-            }
-            OptionEntry::Group(fields) => {
-                entries = fields;
+    match entry {
+        OptionEntry::Field(OptionField {
+            doc,
+            default,
+            value_type,
+            example,
+        }) => {
+            println!("{doc}");
+            println!();
+            println!("Default value: {default}");
+            println!("Type: {value_type}");
+            println!("Example usage:\n```toml\n{example}\n```");
+        }
+        OptionEntry::Group(entries) => {
+            for (name, _) in entries {
+                println!("{name}");
             }
         }
     }
-    for (name, _) in entries {
-        println!("{name}");
-    }
+
     ExitStatus::Success
 }
