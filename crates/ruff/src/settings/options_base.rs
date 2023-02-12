25ba9eb43234d4e6005e917b0c3299607b605b0d
Martin Fischer <martin@push-f.com>
2023-02-12 06:21:56 +0100

    refactor: Introduce ConfigurationOptions::get method

--- crates/ruff/src/settings/options_base.rs
+++ crates/ruff/src/settings/options_base.rs
@@ -1,5 +1,30 @@
 pub trait ConfigurationOptions {
     fn get_available_options() -> Vec<(&'static str, OptionEntry)>;
+
+    /// Get an option entry by its fully-qualified name
+    /// (e.g. `foo.bar` refers to the `bar` option in the `foo` group).
+    fn get(name: Option<&str>) -> Option<OptionEntry> {
+        let mut entries = Self::get_available_options();
+
+        let mut parts_iter = name.into_iter().flat_map(|s| s.split('.'));
+
+        while let Some(part) = parts_iter.next() {
+            let (_, field) = entries.into_iter().find(|(name, _)| *name == part)?;
+            match field {
+                OptionEntry::Field(..) => {
+                    if parts_iter.next().is_some() {
+                        return None;
+                    }
+
+                    return Some(field);
+                }
+                OptionEntry::Group(fields) => {
+                    entries = fields;
+                }
+            }
+        }
+        Some(OptionEntry::Group(entries))
+    }
 }
 
 #[derive(Debug)]
