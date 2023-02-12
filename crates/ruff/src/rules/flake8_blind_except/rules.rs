399d2536f0a309ded82cb64ce48247bd44a7e041
Zeddicus414 <zeddicus414@gmail.com>
2023-02-11 22:49:41 -0600

    Fix trailing spaces

--- crates/ruff/src/rules/flake8_blind_except/rules.rs
+++ crates/ruff/src/rules/flake8_blind_except/rules.rs
@@ -11,10 +11,10 @@ use crate::violation::Violation;
 define_violation!(
     /// ## What it does
     /// Checks that `Exception` is not being caught in `try...except` statements.
-    /// 
+    ///
     /// ## Why is this bad?
     /// Catching `Exception` includes `AssertionError`, `ImportError`, `NameError`, `AttributeError`, `SyntaxError` and others that may indicate significant problems in your source code rather than the behavior of your application. Care should be taken to avoid catching broad exceptions that may include many unexpected errors.
-    /// 
+    ///
     /// ## Example
     /// ```python
     /// try:
@@ -22,7 +22,7 @@ define_violation!(
     /// except Exception:
     ///     print("But you will catch them.")
     /// ```
-    /// 
+    ///
     /// Use instead:
     /// ```python
     /// try:
@@ -30,7 +30,7 @@ define_violation!(
     /// except MoreSpecificException:
     ///     print("And now you won't.")
     /// ```
-    /// 
+    ///
     /// Or:
     /// ```python
     /// try:
@@ -39,7 +39,7 @@ define_violation!(
     ///    logger.Exception("Catch and log errors in a top level module.")
     ///    raise
     /// ```
-    /// 
+    ///
     /// ## References
     /// - [Python Exception Hierarchy](https://docs.python.org/3/library/exceptions.html#exception-hierarchy)
     /// - [Google Python Style Guide on Exceptions](https://google.github.io/styleguide/pyguide.html#24-exceptions)
