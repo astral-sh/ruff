bd4924b39b65beaf1f9538d2f1444262ad08ea61
Zeddicus414 <zeddicus414@gmail.com>
2023-02-11 22:56:16 -0600

    Fix trailing spaces

--- crates/ruff/src/rules/pandas_vet/rules/inplace_argument.rs
+++ crates/ruff/src/rules/pandas_vet/rules/inplace_argument.rs
@@ -10,22 +10,22 @@ use crate::violation::AlwaysAutofixableViolation;
 define_violation!(
     /// ## What it does
     /// Checks for `inplace=True` in code using the `pandas` library.
-    /// 
+    ///
     /// ## Why is this bad?
     /// - Many people expect `inplace=True` to be a performance benefit that prevents dataframe copies, but that's often not true.
     /// - It encourages mutation rather than immutable data, which is harder to reason about and may cause bugs.
     /// - It removes the ability to use the chaining style for `pandas` code.
-    /// 
+    ///
     /// ## Example
     /// ```python
     /// df.sort_values("col1", inplace=True)
     /// ```
-    /// 
+    ///
     /// Use instead:
     /// ```python
     /// sorted_df = df.sort_values("col1")
     /// ```
-    /// 
+    ///
     /// ## References
     /// - [Why You Should Probably Never Use pandas inplace=True](https://towardsdatascience.com/why-you-should-probably-never-use-pandas-inplace-true-9f9f211849e4)
     pub struct UseOfInplaceArgument;
