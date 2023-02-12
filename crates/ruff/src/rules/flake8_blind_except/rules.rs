b52da4cb19259ce0583aa56f78bbd97f42dcbad5
Zeddicus414 <zeddicus414@gmail.com>
2023-02-11 21:03:02 -0600

    add BLE001 documentation

--- crates/ruff/src/rules/flake8_blind_except/rules.rs
+++ crates/ruff/src/rules/flake8_blind_except/rules.rs
@@ -9,6 +9,31 @@ use crate::registry::Diagnostic;
 use crate::violation::Violation;
 
 define_violation!(
+    ///## What it does
+    ///Checks that `Exception` is not being caught in `try...except` statements.
+    ///
+    ///## Why is this bad?
+    ///Catching `Exception` includes `AssertionError`, `ImportError`, `NameError`, `AttributeError`, `SyntaxError` and others that may indicate significant problems in your source code rather than the behavior of your application. Care should be taken to avoid catching broad exceptions that may include many unexpected errors.
+    ///
+    ///## Example
+    ///```python
+    ///try:
+    ///    assert False, "You probably don't want to catch AssertionErrors."
+    ///except Exception:
+    ///    print("But you will catch them.")
+    ///```
+    ///
+    ///Use instead:
+    ///```python
+    ///try:
+    ///    assert False, "You probably don't want to catch AssertionErrors."
+    ///except MoreSpecificException:
+    ///    print("And now you won't.")
+    ///```
+    ///
+    ///## References
+    ///- [Python Exception Hierarchy](https://docs.python.org/3/library/exceptions.html#exception-hierarchy)
+    ///- [Google Python Style Guide on Exceptions](https://google.github.io/styleguide/pyguide.html#24-exceptions)
     pub struct BlindExcept {
         pub name: String,
     }
