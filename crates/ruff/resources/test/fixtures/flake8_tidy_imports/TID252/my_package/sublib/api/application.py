da58227018653d264a90f0ca8ee3910c3dec0e73
Simon Brugman <sfbbrugman@gmail.com>
2023-02-14 09:54:06 +0100

    [`flake8-tidy-imports`] autofix relative imports
    Closes #2768 and closes #2869

---
+++ crates/ruff/resources/test/fixtures/flake8_tidy_imports/TID252/my_package/sublib/api/application.py
@@ -0,0 +1,7 @@
+from typing import TYPE_CHECKING, Any, ClassVar
+ 
+import attrs
+
+from ..protocol import commands, definitions, responses
+from ..server import example
+from . import logger, models
