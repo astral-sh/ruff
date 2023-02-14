85a0d630a0d4f7fbec97b49aa56c3af43c069e21
Martin Fischer <martin@push-f.com>
2023-02-14 08:15:41 +0100

    Fix link relativization in generate_mkdocs.py

--- scripts/generate_mkdocs.py
+++ scripts/generate_mkdocs.py
@@ -40,11 +40,8 @@ def main() -> None:
         raise ValueError(msg)
     content = content.replace(DOCUMENTATION_LINK, "")
 
-    # Replace all GitHub links with relative links.
-    content = content.replace(
-        "https://github.com/charliermarsh/ruff/blob/main/docs/rules/",
-        "rules/",
-    )
+    # Make the documentation links in the README more relative.
+    content = content.replace("https://beta.ruff.rs", "")
 
     Path("docs").mkdir(parents=True, exist_ok=True)
 
