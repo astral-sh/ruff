fbfcaefcbdaa236704e4c35995d74aa6fc050f06
Sawbez <sawbez@users.noreply.github.com>
2023-02-14 21:07:09 -0800

    docs

--- crates/ruff/src/rules/flake8_self/rules/private_member_access.rs
+++ crates/ruff/src/rules/flake8_self/rules/private_member_access.rs
@@ -9,6 +9,42 @@ use crate::registry::Diagnostic;
 use crate::violation::Violation;
 
 define_violation!(
+    /// ## What it does
+    /// Checks for the access of a private member of a class.
+    ///
+    /// ## Why is this bad?
+    /// If a member of a class is declared private, the standard is that
+    /// the member in question generally shouldn't be accessed by anything
+    /// except for internally in the class. Using private variables can
+    /// also possibly cause problems if used incorrectly, and those errors
+    /// can be difficult to debug.
+    ///
+    /// Instead, the name of the member should be renamed to public
+    /// (no leading underscores) if possible. If this is not possible,
+    /// consider removing the usage or adding a `noqa` statement.
+    ///
+    /// ## Example
+    /// ```python
+    /// class MyClass:
+    ///     def __init__(self):
+    ///         self._private_member = "this is only supposed to be used internally"
+    ///
+    /// var_myclass = MyClass()
+    /// print(var_myclass._private_member)
+    /// ```
+    ///
+    /// Instead, use
+    /// ```python
+    /// class MyClass:
+    ///     def __init__(self):
+    ///         self.public_member = "public (underscore prefix is removed)"
+    ///
+    /// var_myclass = MyClass()
+    /// print(var_myclass.public_member)
+    /// ```
+    ///
+    /// * [What is the meaning of single or double underscores before an object name?_](https://stackoverflow.com/questions/1301346/what-is-the-meaning-of-single-and-double-underscore-before-an-object-name)
+    /// ```
     pub struct PrivateMemberAccess {
         pub access: String,
     }
