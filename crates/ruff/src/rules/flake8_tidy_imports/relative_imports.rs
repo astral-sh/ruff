a934d01bdb114e5d40075c8b849c46e0d8bdf2d4
Simon Brugman <sbrugman@users.noreply.github.com>
2023-02-17 20:35:28 +0100

    [`flake8-tidy-imports`] extend autofix of relative imports (#2990)
    This extends the autofix for TID252 to work with for relative imports without `module` (i.e. `from .. import`). Tested with `matplotlib` and `bokeh`.
    (Previously it would panic on unwrap of the module) 
    
    Note that pandas has [replaced](https://github.com/pandas-dev/pandas/commit/6057d7a93e6bb792508339dcf8e70998e7e78e1a) `absolufy-imports` with `ruff` now!

--- crates/ruff/src/rules/flake8_tidy_imports/relative_imports.rs
+++ crates/ruff/src/rules/flake8_tidy_imports/relative_imports.rs
@@ -92,33 +92,50 @@ fn fix_banned_relative_import(
     module_path: Option<&Vec<String>>,
     stylist: &Stylist,
 ) -> Option<Fix> {
-    // Only fix is the module path is known
+    // Only fix is the module path is known.
     if let Some(mut parts) = module_path.cloned() {
-        // Remove relative level from module path
+        // Remove relative level from module path.
         for _ in 0..*level? {
             parts.pop();
         }
 
-        let call_path = from_relative_import(&parts, module.unwrap());
-        let module_name = call_path.as_slice();
-
-        // Require import to be a valid PEP 8 module:
-        // https://python.org/dev/peps/pep-0008/#package-and-module-names
-        if module_name.iter().any(|f| !is_lower_with_underscore(f)) {
-            return None;
-        }
-
-        let content = match &stmt.node {
-            StmtKind::ImportFrom { names, .. } => unparse_stmt(
-                &create_stmt(StmtKind::ImportFrom {
-                    module: Some(module_name.join(".")),
-                    names: names.clone(),
-                    level: Some(0),
-                }),
-                stylist,
-            ),
-            _ => return None,
+        let module_name = if let Some(module) = module {
+            let call_path = from_relative_import(&parts, module);
+            // Require import to be a valid PEP 8 module:
+            // https://python.org/dev/peps/pep-0008/#package-and-module-names
+            if !call_path.iter().all(|part| is_lower_with_underscore(part)) {
+                return None;
+            }
+            call_path.as_slice().join(".")
+        } else if parts.len() > 1 {
+            let module = parts.last().unwrap();
+            let call_path = from_relative_import(&parts, module);
+            // Require import to be a valid PEP 8 module:
+            // https://python.org/dev/peps/pep-0008/#package-and-module-names
+            if !call_path.iter().all(|part| is_lower_with_underscore(part)) {
+                return None;
+            }
+            call_path.as_slice().join(".")
+        } else {
+            // Require import to be a valid PEP 8 module:
+            // https://python.org/dev/peps/pep-0008/#package-and-module-names
+            if !parts.iter().all(|part| is_lower_with_underscore(part)) {
+                return None;
+            }
+            parts.join(".")
         };
+
+        let StmtKind::ImportFrom { names, .. } = &stmt.node else {
+            unreachable!("Expected StmtKind::ImportFrom");
+        };
+        let content = unparse_stmt(
+            &create_stmt(StmtKind::ImportFrom {
+                module: Some(module_name),
+                names: names.clone(),
+                level: Some(0),
+            }),
+            stylist,
+        );
 
         Some(Fix::replacement(
             content,
