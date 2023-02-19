4c77a4211650214ec24e2e636d03d408a831ae5b
Matthew Lloyd <github@matthewlloyd.net>
2023-02-19 13:54:29 -0500

    [docs] redefined-loop-name

--- crates/ruff/src/rules/pylint/rules/redefined_loop_name.rs
+++ crates/ruff/src/rules/pylint/rules/redefined_loop_name.rs
@@ -11,6 +11,41 @@ use rustpython_parser::ast::{Expr, Stmt, StmtKind, Withitem};
 use std::iter::zip;
 
 define_violation!(
+    /// ## What it does
+    /// Checks for variables defined in `for` loops and `with` statements that get overwritten
+    /// within the body, for example by another `for` loop or `with` statement or by direct
+    /// assignment.
+    ///
+    /// ## Why is this bad?
+    /// Redefinition of a loop variable inside the loop's body causes its value to differ from
+    /// the original loop iteration for the remainder of the block, in a way that will likely
+    /// cause bugs.
+    ///
+    /// In Python, unlike many other languages, `for` loops and `with` statements don't define
+    /// their own scopes. Therefore, a nested loop that uses the same target variable name as
+    /// an outer loop will reuse the same actual variable, and the value from the last
+    /// iteration will "leak out" into the remainder of the enclosing loop.
+    ///
+    /// While this mistake is easy to spot in small examples, it can be hidden in larger
+    /// blocks of code where the definition and redefinition of the variable may not be
+    /// visible at the same time.
+    ///
+    /// ## Example
+    /// ```python
+    /// for i in range(10):
+    ///     i = 9
+    ///     print(i)  # prints 9 every iteration
+    ///
+    /// for i in range(10):
+    ///     for i in range(10):  # original value overwritten
+    ///         pass
+    ///     print(i)  # also prints 9 every iteration
+    ///
+    /// with path1.open() as f:
+    ///     with path2.open() as f:
+    ///         f = path2.open()
+    ///     print(f.readline())  # prints a line from path2
+    /// ```
     pub struct RedefinedLoopName {
         pub name: String,
     }
@@ -126,7 +161,9 @@ where
                 }
                 (name_ranges, visitor.name_ranges)
             }
-            _ => panic!("redefined_loop_name called Statement that is not With, For, or AsyncFor"),
+            _ => panic!(
+                "redefined_loop_name called on Statement that is not a With, For, or AsyncFor"
+            ),
         },
         Node::Expr(_) => panic!("redefined_loop_name called on Node that is not a Statement"),
     };
