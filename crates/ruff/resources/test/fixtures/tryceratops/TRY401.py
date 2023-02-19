a0cfc63718dc3d635b740e40531415e6f4a0d07b
colin99d <colin99delahunty@gmail.com>
2023-02-19 13:32:50 -0500

    Added tryceratops tests

--- crates/ruff/resources/test/fixtures/tryceratops/TRY401.py
+++ crates/ruff/resources/test/fixtures/tryceratops/TRY401.py
@@ -26,6 +26,32 @@ def main_function():
         logger.exception(f"Foud an error: {bad}")
 
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def func_fstr():
+    try:
+        ...
+    except Exception as ex:
+        logger.exception(f"log message {ex}")
+
+
+def func_concat():
+    try:
+        ...
+    except Exception as ex:
+        logger.exception("log message: " + str(ex))
+
+
+def func_comma():
+    try:
+        ...
+    except Exception as ex:
+        logger.exception("log message", ex)
+
+
 # These should NOT change
 
 
