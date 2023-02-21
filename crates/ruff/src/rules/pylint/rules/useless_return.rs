ecacccbd5e4ac37ca8c70ff8c3772461958952e5
tomecki <tomek.dziopa@gmail.com>
2023-02-21 20:39:33 +0100

    cleanup

--- crates/ruff/src/rules/pylint/rules/useless_return.rs
+++ crates/ruff/src/rules/pylint/rules/useless_return.rs
@@ -1,12 +1,15 @@
 use log::error;
 
-use rustpython_parser::ast::{Constant, ExprKind, Stmt, StmtKind};
+use rustpython_parser::ast::{Stmt, StmtKind};
 
 use ruff_macros::{define_violation, derive_message_formats};
 
 use crate::{
-    ast::{types::Range, helpers::is_const_none}, checkers::ast::Checker, fix::Fix, registry::Diagnostic,
-    violation::AlwaysAutofixableViolation, autofix::helpers::delete_stmt,
+    ast::{helpers::is_const_none, types::Range},
+    autofix::helpers::delete_stmt,
+    checkers::ast::Checker,
+    registry::Diagnostic,
+    violation::AlwaysAutofixableViolation,
 };
 
 define_violation!(
@@ -14,8 +17,6 @@ define_violation!(
     /// Checks if the last statement of a function or a method is `return` or
     ///  `return None`.
     /// ## Why is this bad?
-    /// The `__init__` method is the constructor for a given Python class,
-    /// responsible for initializing, rather than creating, new objects.
     ///
     /// Python implicitly assumes `None` return value at the end of a function.
     /// This statement can be safely removed.
@@ -51,7 +52,7 @@ pub fn useless_return(checker: &mut Checker, stmt: &Stmt) {
         Some(node) => matches!(node.node, StmtKind::FunctionDef { .. }),
         None => false,
     };
-    let is_last_statement = checker.current_sibling_stmt() == None && belongs_to_function_scope;
+    let is_last_statement = checker.current_sibling_stmt().is_none() && belongs_to_function_scope;
     if !is_last_statement {
         return;
     }
@@ -63,22 +64,22 @@ pub fn useless_return(checker: &mut Checker, stmt: &Stmt) {
     if is_bare_return_or_none {
         let mut diagnostic = Diagnostic::new(UselessReturn, Range::from_located(stmt));
         if checker.patch(diagnostic.kind.rule()) {
-                // Fix::deletion(stmt.location, stmt.end_location.unwrap())
-                match delete_stmt(
-                    stmt,
-                    None,
-                    &[],
-                    checker.locator,
-                    checker.indexer,
-                    checker.stylist,
-                ) {
-                    Ok(fix) => {
-                        diagnostic.amend(fix);
-                    }
-                    Err(e) => {
-                        error!("Failed to delete `return` statement: {}", e);
-                    }
-                };
+            // Fix::deletion(stmt.location, stmt.end_location.unwrap())
+            match delete_stmt(
+                stmt,
+                None,
+                &[],
+                checker.locator,
+                checker.indexer,
+                checker.stylist,
+            ) {
+                Ok(fix) => {
+                    diagnostic.amend(fix);
+                }
+                Err(e) => {
+                    error!("Failed to delete `return` statement: {}", e);
+                }
+            };
         }
         checker.diagnostics.push(diagnostic);
     }
