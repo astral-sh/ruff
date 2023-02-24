d77223c4be1753612de22c11edeeb70825fa9d59
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-02-23 19:50:36 -0500

    Handle end-of-line comments on excepthandlers

--- crates/ruff_python_formatter/src/format/alias.rs
+++ crates/ruff_python_formatter/src/format/alias.rs
@@ -2,9 +2,11 @@ use ruff_formatter::prelude::*;
 use ruff_formatter::write;
 use ruff_text_size::TextSize;
 
+use crate::builders::literal;
 use crate::context::ASTFormatContext;
 use crate::cst::Alias;
 use crate::shared_traits::AsFormat;
+use crate::trivia::{Relationship, TriviaKind};
 
 pub struct FormatAlias<'a> {
     item: &'a Alias,
@@ -26,6 +28,25 @@ impl Format<ASTFormatContext<'_>> for FormatAlias<'_> {
         if let Some(asname) = &alias.node.asname {
             write!(f, [text(" as ")])?;
             write!(f, [dynamic_text(asname, TextSize::default())])?;
+        }
+
+        // Format any end-of-line comments.
+        let mut first = true;
+        for range in alias.trivia.iter().filter_map(|trivia| {
+            if matches!(trivia.relationship, Relationship::Trailing) {
+                if let TriviaKind::EndOfLineComment(range) = trivia.kind {
+                    Some(range)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        }) {
+            if std::mem::take(&mut first) {
+                write!(f, [line_suffix(&text("  "))])?;
+            }
+            write!(f, [line_suffix(&literal(range))])?;
         }
 
         Ok(())
