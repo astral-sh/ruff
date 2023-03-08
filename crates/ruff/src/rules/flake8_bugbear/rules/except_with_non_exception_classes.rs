73601b4b2c29ebd5df8e60a7c8589c7746135444
Aaron Cunningham <aaron.cu@enervee.com>
2023-03-08 01:10:11 -0800

    move some code left

--- crates/ruff/src/rules/flake8_bugbear/rules/except_with_non_exception_classes.rs
+++ crates/ruff/src/rules/flake8_bugbear/rules/except_with_non_exception_classes.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use rustpython_parser::ast::{Excepthandler, ExcepthandlerKind, Expr, ExprKind, Located};
 
 use ruff_macros::{derive_message_formats, violation};
@@ -22,19 +24,17 @@ impl Violation for ExceptWithNonExceptionClasses {
 /// warning for B029).
 fn flatten_starred_iterables(exprs: &[Expr]) -> Vec<&Expr> {
     let mut flattened_exprs: Vec<&Expr> = Vec::new();
-    let mut expr_lists_to_flatten = vec![exprs];
-    while !expr_lists_to_flatten.is_empty() {
-        let expr_list = expr_lists_to_flatten.pop().unwrap();
-        for expr in expr_list {
-            match &expr.node {
-                ExprKind::Starred { value, .. } => match &value.as_ref().node {
-                    ExprKind::Tuple { elts, .. } | ExprKind::List { elts, .. } => {
-                        expr_lists_to_flatten.push(elts.as_slice());
-                    }
-                    _ => flattened_exprs.push(expr),
-                },
+    let mut exprs_to_process: VecDeque<&Expr> = exprs.iter().collect();
+    while !exprs_to_process.is_empty() {
+        let expr = exprs_to_process.pop_front().unwrap();
+        match &expr.node {
+            ExprKind::Starred { value, .. } => match &value.as_ref().node {
+                ExprKind::Tuple { elts, .. } | ExprKind::List { elts, .. } => {
+                    exprs_to_process.append(&mut elts.iter().collect());
+                }
                 _ => flattened_exprs.push(expr),
-            }
+            },
+            _ => flattened_exprs.push(expr),
         }
     }
 
