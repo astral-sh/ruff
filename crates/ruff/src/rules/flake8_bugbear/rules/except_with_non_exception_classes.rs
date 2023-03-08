d5efa289c11efea4811dbfbfe422b68b7f8b060d
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-03-08 15:36:59 -0500

    Rebase; use let-else

--- crates/ruff/src/rules/flake8_bugbear/rules/except_with_non_exception_classes.rs
+++ crates/ruff/src/rules/flake8_bugbear/rules/except_with_non_exception_classes.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use rustpython_parser::ast::{Excepthandler, ExcepthandlerKind, Expr, ExprKind, Located};
+use rustpython_parser::ast::{Excepthandler, ExcepthandlerKind, Expr, ExprKind};
 
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::types::Range;
@@ -15,20 +15,22 @@ pub struct ExceptWithNonExceptionClasses;
 impl Violation for ExceptWithNonExceptionClasses {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Except handlers should only be exception classes or tuples of exception classes")
+        format!("`except` handlers should only be exception classes or tuples of exception classes")
     }
 }
 
-/// Given a set of Expr, finds any that are starred and flattens them.
+/// Given an [`Expr`], flatten any [`ExprKind::Starred`] expressions.
 /// This should leave any unstarred iterables alone (subsequently raising a
 /// warning for B029).
-fn flatten_starred_iterables(exprs: &[Expr]) -> Vec<&Expr> {
-    let mut flattened_exprs: Vec<&Expr> = Vec::new();
-    let mut exprs_to_process: VecDeque<&Expr> = exprs.iter().collect();
-    while !exprs_to_process.is_empty() {
-        let expr = exprs_to_process.pop_front().unwrap();
+fn flatten_starred_iterables(expr: &Expr) -> Vec<&Expr> {
+    let ExprKind::Tuple { elts, .. } = &expr.node else {
+        return vec![expr];
+    };
+    let mut flattened_exprs: Vec<&Expr> = Vec::with_capacity(elts.len());
+    let mut exprs_to_process: VecDeque<&Expr> = elts.iter().collect();
+    while let Some(expr) = exprs_to_process.pop_front() {
         match &expr.node {
-            ExprKind::Starred { value, .. } => match &value.as_ref().node {
+            ExprKind::Starred { value, .. } => match &value.node {
                 ExprKind::Tuple { elts, .. } | ExprKind::List { elts, .. } => {
                     exprs_to_process.append(&mut elts.iter().collect());
                 }
@@ -37,30 +39,21 @@ fn flatten_starred_iterables(exprs: &[Expr]) -> Vec<&Expr> {
             _ => flattened_exprs.push(expr),
         }
     }
-
     flattened_exprs
 }
 
 /// B030
 pub fn except_with_non_exception_classes(checker: &mut Checker, excepthandler: &Excepthandler) {
     let ExcepthandlerKind::ExceptHandler { type_, .. } = &excepthandler.node;
-    if type_.is_none() {
+    let Some(type_) = type_ else {
         return;
-    }
-    let exprs: Vec<&Expr> = match type_.as_ref().unwrap().as_ref() {
-        Located {
-            node: ExprKind::Tuple { elts, .. },
-            ..
-        } => flatten_starred_iterables(elts),
-        any => vec![any],
     };
-
-    for expr in exprs {
+    for expr in flatten_starred_iterables(type_) {
         match expr.node {
             ExprKind::Attribute { .. } | ExprKind::Name { .. } | ExprKind::Call { .. } => (),
             _ => checker.diagnostics.push(Diagnostic::new(
                 ExceptWithNonExceptionClasses,
-                Range::from_located(expr),
+                Range::from(expr),
             )),
         }
     }
