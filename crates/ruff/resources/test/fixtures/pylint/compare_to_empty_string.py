2f3734dd2292f47e7785254f3fc5aefa91a3aa42
AreamanM <aryaman.marathe@gmail.com>
2023-03-08 22:47:04 +0000

    update test for plc1901

--- crates/ruff/resources/test/fixtures/pylint/compare_to_empty_string.py
+++ crates/ruff/resources/test/fixtures/pylint/compare_to_empty_string.py
@@ -1,8 +1,11 @@
-x = ""
-y = "hello"
+x = "a string"
+y = "another string"
 
-if x == "":  # [compare-to-empty-string]
+if x is "" or x == "":
     print("x is an empty string")
 
-if y != "":  # [compare-to-empty-string]
+if y is not "" or y != "":
     print("y is not an empty string")
+
+if x and not y:
+    print("x is not an empty string, but y is an empty string")
