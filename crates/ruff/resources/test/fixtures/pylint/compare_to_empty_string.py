56e8a4fd149db62a6394ae93f8144392952baea1
AreamanM <aryaman.marathe@gmail.com>
2023-03-09 00:08:20 +0000

    cleanup plc1901 impl

--- crates/ruff/resources/test/fixtures/pylint/compare_to_empty_string.py
+++ crates/ruff/resources/test/fixtures/pylint/compare_to_empty_string.py
@@ -1,11 +1,13 @@
 x = "a string"
 y = "another string"
 
-if x is "" or x == "":
-    print("x is an empty string")
+def should_raise_lint_warning():
+    if x is "" or x == "":
+        print("x is an empty string")
 
-if y is not "" or y != "":
-    print("y is not an empty string")
+    if y is not "" or y != "":
+        print("y is not an empty string")
 
-if x and not y:
-    print("x is not an empty string, but y is an empty string")
+def should_not_raise_lint_warning():
+    if x and not y:
+        print("x is not an empty string, but y is an empty string")
--- crates/ruff/src/rules/pylint/rules/compare_to_empty_string.rs
+++ crates/ruff/src/rules/pylint/rules/compare_to_empty_string.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use itertools::Itertools;
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::helpers::{unparse_constant, unparse_expr};
@@ -5,27 +6,59 @@ use rustpython_parser::ast::{Cmpop, Constant, Expr, ExprKind, Located};
 
 use crate::{checkers::ast::Checker, registry::Diagnostic, violation::Violation};
 
-use super::comparison_of_constant::ViolationsCmpop;
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum EmptyStringViolationsCmpop {
+    Is,
+    IsNot,
+    Eq,
+    NotEq,
+}
+
+impl TryFrom<&Cmpop> for EmptyStringViolationsCmpop {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &Cmpop) -> Result<Self, Self::Error> {
+        match value {
+            Cmpop::Is => Ok(Self::Is),
+            Cmpop::IsNot => Ok(Self::IsNot),
+            Cmpop::Eq => Ok(Self::Eq),
+            Cmpop::NotEq => Ok(Self::NotEq),
+            _ => Err(anyhow!(
+                "{value:?} cannot be converted to EmptyStringViolationsCmpop"
+            )),
+        }
+    }
+}
+
+impl std::fmt::Display for EmptyStringViolationsCmpop {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let repr = match self {
+            Self::Is => "is",
+            Self::IsNot => "is not",
+            Self::Eq => "==",
+            Self::NotEq => "!=",
+        };
+        write!(f, "{repr}")
+    }
+}
 
 #[violation]
 pub struct CompareToEmptyString {
     pub lhs: String,
-    pub op: ViolationsCmpop,
+    pub op: EmptyStringViolationsCmpop,
     pub rhs: String,
 }
 
 impl Violation for CompareToEmptyString {
     #[derive_message_formats]
     fn message(&self) -> String {
-        let cond = match self.op {
-            ViolationsCmpop::Is | ViolationsCmpop::Eq => "",
-            // the assumption is that this message will only ever be shown
-            // if op is `in`, `=`, `not in`, `!=`
-            _ => "not",
+        let prefix = match self.op {
+            EmptyStringViolationsCmpop::Is | EmptyStringViolationsCmpop::Eq => "",
+            EmptyStringViolationsCmpop::IsNot | EmptyStringViolationsCmpop::NotEq => "not ",
         };
         format!(
-            "{} {} {} can be simplified to {} {} as strings are falsey",
-            self.lhs, self.op, self.rhs, cond, self.lhs
+            "`{} {} {}` can be simplified to `{}{}` as an empty string is falsey",
+            self.lhs, self.op, self.rhs, prefix, self.lhs
         )
     }
 }
@@ -48,7 +81,10 @@ pub fn compare_to_empty_string(
                         let diag = Diagnostic::new(
                             CompareToEmptyString {
                                 lhs: unparse_expr(lhs, checker.stylist),
-                                op: op.into(),
+                                // we know `op` can be safely converted into a
+                                // EmptyStringViolationCmpop due to the first if statement being
+                                // true in this branch
+                                op: op.try_into().unwrap(),
                                 rhs: unparse_constant(value, checker.stylist),
                             },
                             lhs.into(),
