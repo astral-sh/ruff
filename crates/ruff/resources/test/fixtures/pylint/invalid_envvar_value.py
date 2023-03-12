ebfdc59ddf621abdaf205134028a859cee0bf978
Jacob Latonis <latonis@wisc.edu>
2023-03-12 13:53:21 -0500

    one more test

--- crates/ruff/resources/test/fixtures/pylint/invalid_envvar_value.py
+++ crates/ruff/resources/test/fixtures/pylint/invalid_envvar_value.py
@@ -6,6 +6,7 @@ os.getenv("test")
 os.getenv(key="testingAgain")
 os.getenv(key=11)  # [invalid-envvar-value]
 os.getenv(["hello"])  # [invalid-envvar-value]
+os.getenv(key="foo", default="bar")
 
 AA = "aa"
 os.getenv(AA)
