177f2eb52ce121ac2c0fc4883d19441e35f6bdbd
Micha Reiser <micha@reiser.io>
2023-03-17 18:47:37 +0100

    Fix test case urls (must use raw) and target directory resolution

--- crates/ruff_benchmark/benches/linter.rs
+++ crates/ruff_benchmark/benches/linter.rs
@@ -26,12 +26,12 @@ static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 fn create_test_cases() -> Result<Vec<TestCase>, TestFileDownloadError> {
     Ok(vec![
-        TestCase::fast(TestFile::try_download("numpy/globals.py", "https://github.com/numpy/numpy/blob/89d64415e349ca75a25250f22b874aa16e5c0973/numpy/_globals.py")?),
+        TestCase::fast(TestFile::try_download("numpy/globals.py", "https://raw.githubusercontent.com/numpy/numpy/89d64415e349ca75a25250f22b874aa16e5c0973/numpy/_globals.py")?),
         TestCase::normal(TestFile::try_download(
             "pydantic/types.py",
-            "https://raw.githubusercontent.com/pydantic/pydantic/main/pydantic/types.py",
+            "https://raw.githubusercontent.com/pydantic/pydantic/83b3c49e99ceb4599d9286a3d793cea44ac36d4b/pydantic/types.py",
         )?),
-        TestCase::normal(TestFile::try_download("numpy/ctypeslib.py", "https://github.com/numpy/numpy/blob/main/numpy/ctypeslib.py")?),
+        TestCase::normal(TestFile::try_download("numpy/ctypeslib.py", "https://raw.githubusercontent.com/numpy/numpy/e42c9503a14d66adfd41356ef5640c6975c45218/numpy/ctypeslib.py")?),
         TestCase::slow(TestFile::try_download(
             "large/dataset.py",
             "https://raw.githubusercontent.com/DHI/mikeio/b7d26418f4db2909b0aa965253dbe83194d7bb5b/tests/test_dataset.py",
@@ -55,14 +55,17 @@ fn benchmark_linter(mut group: BenchmarkGroup<WallTime>, settings: &Settings) {
             &case,
             |b, case| {
                 b.iter(|| {
-                    lint_only(
+                    let result = lint_only(
                         case.code(),
                         &case.path(),
                         None,
                         settings,
                         flags::Noqa::Enabled,
                         flags::Autofix::Enabled,
-                    )
+                    );
+
+                    // Assert that file contains no parse errors
+                    assert_eq!(result.error, None);
                 });
             },
         );
