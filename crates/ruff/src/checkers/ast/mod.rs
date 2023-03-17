eba2552eb15d716ac49af378d5d3d86acc8d4e20
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-03-17 15:15:31 -0400

    Truncate message; move to documentation

--- crates/ruff/src/checkers/ast/mod.rs
+++ crates/ruff/src/checkers/ast/mod.rs
@@ -2456,7 +2456,7 @@ where
                 {
                     flake8_bugbear::rules::zip_without_explicit_strict(self, expr, func, keywords);
                 }
-                if self.settings.rules.enabled(&Rule::NoExplicitStacklevel) {
+                if self.settings.rules.enabled(Rule::NoExplicitStacklevel) {
                     flake8_bugbear::rules::no_explicit_stacklevel(self, func, args, keywords);
                 }
 
