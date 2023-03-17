ac36c9c082c9b9756132a8782fde3ac5ae5096b7
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-03-16 23:35:32 -0400

    Avoid steps

--- crates/ruff/resources/test/fixtures/ruff/RUF007.py
+++ crates/ruff/resources/test/fixtures/ruff/RUF007.py
@@ -7,9 +7,11 @@ zip(input, otherInput[1:])  # different inputs
 zip(input, input[2:])  # not successive
 zip(input[:-1], input[2:])  # not successive
 list(zip(input, otherInput))  # nested call
+zip(input, input[1::2])  # not successive
 
 # Errors
 zip(input, input[1:])
+zip(input, input[1::1])
 zip(input[:-1], input[1:])
 zip(input[1:], input[2:])
 zip(input[1:-1], input[2:])
