8a33a399c99bb512f5b4d0600957165fa46c8d0d
jordan-verschoor <JBLDSKY@gmail.com>
2023-03-26 17:08:28 +0200

    Updated fixtures

--- crates/ruff/resources/test/fixtures/flake8_pyi/PYI012.pyi
+++ crates/ruff/resources/test/fixtures/flake8_pyi/PYI012.pyi
@@ -1,3 +1,62 @@
-class Foo:
+# Violations
+
+class Foo:  # PYI012
     pass
+
+class Foo:  # PYI012
+    """buzz"""
+
+    pass
+
+class Bar: pass  # PYI012
+
+class Baz:  # PYI012
+    """
+    My body only contains pass.
+    """
+    pass
+
+
+class Error(Exception):  # PYI012
+    pass
+
+
+class BadWithInit:  # PYI012 
+    value: int
+    pass
+    def __init__():
+        prop: str = "property"
+
+class OneAttr:  # PYI012
+    value: int
+    pass
+
+class OneAttrRev:  # PYI012
+    pass
+    value: int
+
+
+
+
+# Not violations
+
+class Dog: 
+    eyes: int = 2
+
+
+class Pass: 
+    ...
+
+class Pazz: ...
+
+class WithInit:  
+    value: int = 0
+    def __init__():
+        prop: str = "property"
+        pass
+
+def function():
+    pass
+
+pass
 
