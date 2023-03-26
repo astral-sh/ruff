f113b98682d485908fa886a30d9b23e94623b6b5
jordan-verschoor <JBLDSKY@gmail.com>
2023-03-26 21:58:12 +0200

    Fixed a major misconception
    I was under the impression that every occurrence of the pass statement in every class was supposed to be diagnosed. However, upon examining the original rule more closely, that was not the case. See the new updated test for more information.

--- crates/ruff/src/rules/flake8_pyi/rules/pass_in_class_body.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/pass_in_class_body.rs
@@ -1,9 +1,11 @@
-use ruff_diagnostics::{Diagnostic, AlwaysAutofixableViolation};
+use crate::autofix::helpers::delete_stmt;
+use ruff_diagnostics::{AlwaysAutofixableViolation, Diagnostic};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::types::Range;
 
 use crate::checkers::ast::Checker;
 
+use crate::registry::AsRule;
 use rustpython_parser::ast::{Stmt, StmtKind};
 
 #[violation]
@@ -17,19 +19,36 @@ impl AlwaysAutofixableViolation for PassInClassBody {
 
     fn autofix_title(&self) -> String {
         format!("Remove `pass` from the class body")
-
     }
 }
 
 /// PYI012
 pub fn pass_in_class_body(checker: &mut Checker, body: &[Stmt]) {
+        // pass is required in these situations
+    if body.len() < 2 {
+        return;
+    }
+
     // Loops through all the Located in a ClassDef body and checks for
-    // any nodes to be of StmtKind::Pass
+    // nodes to match StmtKind::Pass
     for located in body {
         if matches!(located.node, StmtKind::Pass) {
-            checker
-                .diagnostics
-                .push(Diagnostic::new(PassInClassBody, Range::from(located)));
+            let mut diagnostic = Diagnostic::new(PassInClassBody, Range::from(located));
+
+            if checker.patch(diagnostic.kind.rule()) {
+                diagnostic.try_amend(|| {
+                    delete_stmt(
+                        located,
+                        None,
+                        &[],
+                        checker.locator,
+                        checker.indexer,
+                        checker.stylist,
+                    )
+                });
+            };
+
+            checker.diagnostics.push(diagnostic);
         }
     }
 }
