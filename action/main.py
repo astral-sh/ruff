c1218b3ec9924a664ebef25ed71cf2712e475e36
brucearctor <5032356+brucearctor@users.noreply.github.com>
2023-03-28 10:15:51 -0700

    pipx and formatting

--- action/main.py
+++ action/main.py
@@ -5,13 +5,10 @@ from pathlib import Path
 from subprocess import PIPE, STDOUT, run
 
 ACTION_PATH = Path(os.environ["GITHUB_ACTION_PATH"])
-ENV_PATH = ACTION_PATH / ".ruff-env"
-ENV_BIN = ENV_PATH / ("Scripts" if sys.platform == "win32" else "bin")
+ENV_BIN = ACTION_PATH / ".ruff-env" / ("Scripts" if sys.platform == "win32" else "bin")
 OPTIONS = os.getenv("INPUT_OPTIONS", default="")
 SRC = os.getenv("INPUT_SRC", default="")
 VERSION = os.getenv("INPUT_VERSION", default="")
-
-run([sys.executable, "-m", "venv", str(ENV_PATH)], check=True)
 
 version_specifier=""
 # TODO: some form of validation for user input VERSION
@@ -19,8 +16,9 @@ if VERSION != "":
     version_specifier = f"=={VERSION}"
 
 req = f"ruff{version_specifier}"
+
 pip_proc = run(
-    [str(ENV_BIN / "python"), "-m", "pip", "install", req],
+    ["pipx", "install", "venv", req],
     stdout=PIPE,
     stderr=STDOUT,
     encoding="utf-8",
@@ -30,9 +28,8 @@ if pip_proc.returncode:
     print("::error::Failed to install Ruff.", flush=True)
     sys.exit(pip_proc.returncode)
 
-
 base_cmd = [str(ENV_BIN / "ruff")]
 
-proc = run([*base_cmd, *shlex.split(OPTIONS), *shlex.split(SRC)])
+proc = run([*base_cmd, *shlex.split(OPTIONS), *shlex.split(SRC), "--format github"])
 
 sys.exit(proc.returncode)
