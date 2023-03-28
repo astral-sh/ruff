71b3c9c245f38239f86d476539faab108eea38b2
brucearctor <5032356+brucearctor@users.noreply.github.com>
2023-03-28 15:33:39 -0700

    regex

--- action/main.py
+++ action/main.py
@@ -13,8 +13,8 @@ VERSION = os.getenv("INPUT_VERSION", default="")
 version_specifier=""
 # TODO: some form of validation for user input VERSION
 if VERSION != "":
-    if not re.match('[v?0-9\.]', VERSION):
-        print("VERSION is incorrect")
+    if not re.match('v?\d\.\d{1,3}\.\d{1,3}$', VERSION):
+        print("VERSION does not match expected pattern")
         sys.exit(1)
     version_specifier = f"=={VERSION}"
 
