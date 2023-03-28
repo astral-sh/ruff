29126e96341716de7a2abd675f8e1fa60a714917
brucearctor <5032356+brucearctor@users.noreply.github.com>
2023-03-28 10:53:08 -0700

    update

--- action/main.py
+++ action/main.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from subprocess import PIPE, STDOUT, run
 
 ACTION_PATH = Path(os.environ["GITHUB_ACTION_PATH"])
-ENV_BIN = ACTION_PATH / ".ruff-env" / ("Scripts" if sys.platform == "win32" else "bin")
 OPTIONS = os.getenv("INPUT_OPTIONS", default="")
 SRC = os.getenv("INPUT_SRC", default="")
 VERSION = os.getenv("INPUT_VERSION", default="")
@@ -28,8 +27,6 @@ if pip_proc.returncode:
     print("::error::Failed to install Ruff.", flush=True)
     sys.exit(pip_proc.returncode)
 
-base_cmd = [str(ENV_BIN / "ruff")]
-
-proc = run([*base_cmd, *shlex.split(OPTIONS), *shlex.split(SRC), "--format github"])
+proc = run(["ruff", *shlex.split(OPTIONS), *shlex.split(SRC), "--format github"])
 
 sys.exit(proc.returncode)
