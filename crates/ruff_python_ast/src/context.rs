3887a675f738e4e9e316f28071fe73cc7e2c9f84
Hashem Nasarat <hashem@hudson-trading.com>
2023-03-30 09:56:55 -0400

    When checking module visibility, don't check entire ancestry
    I have a network system mounted on a directory that starts with an
    underscore, e.g. `/mnt/_foo/home/timothy/projects/my_package/foo.py`
    
    I found that ruff wasn't showing lints for undocumented public classes,
    functions or modules in foo.py.
    
    When checking for module visibility, we should stop after the module
    path. (In ruff, unlike pydocstyle, the module visibility overrides any
    class or function visibility.)
    
    This should fix the bug where an unrelated parent directory affects the
    visibility checks.

--- crates/ruff_python_ast/src/context.rs
+++ crates/ruff_python_ast/src/context.rs
@@ -61,6 +61,7 @@ impl<'a> Context<'a> {
         path: &'a Path,
         module_path: Option<Vec<String>>,
     ) -> Self {
+        let visibility = module_visibility(module_path.as_deref(), path);
         Self {
             typing_modules,
             module_path,
@@ -77,7 +78,7 @@ impl<'a> Context<'a> {
             body_index: 0,
             visible_scope: VisibleScope {
                 modifier: Modifier::Module,
-                visibility: module_visibility(path),
+                visibility,
             },
             in_annotation: false,
             in_type_definition: false,
