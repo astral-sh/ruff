2f1aa4bd0acbabb0c3071c102324213fe73cf143
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-04-03 18:50:25 -0400

    Allow starred arguments in B030

--- crates/ruff/src/rules/flake8_bugbear/rules/except_with_empty_tuple.rs
+++ crates/ruff/src/rules/flake8_bugbear/rules/except_with_empty_tuple.rs
@@ -13,17 +13,17 @@ pub struct ExceptWithEmptyTuple;
 impl Violation for ExceptWithEmptyTuple {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Using except (): with an empty tuple does not handle/catch anything. Add exceptions to handle.")
+        format!("Using `except ():` with an empty tuple does not catch anything; add exceptions to handle")
     }
 }
 
 /// B029
 pub fn except_with_empty_tuple(checker: &mut Checker, excepthandler: &Excepthandler) {
     let ExcepthandlerKind::ExceptHandler { type_, .. } = &excepthandler.node;
-    if type_.is_none() {
+    let Some(type_) = type_ else {
         return;
-    }
-    let ExprKind::Tuple { elts, .. } = &type_.as_ref().unwrap().node else {
+    };
+    let ExprKind::Tuple { elts, .. } = &type_.node else {
         return;
     };
     if elts.is_empty() {
