177578a4f1d6e6bdb2e3055c36f3855d446cf83c
USER5 <us3r5@hotmail.com>
2023-04-10 17:45:30 +0930

    Use more efficient algorithm

--- crates/ruff/src/rules/flake8_pyi/rules/duplicate_types_in_union.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/duplicate_types_in_union.rs
@@ -23,45 +23,47 @@ impl AlwaysAutofixableViolation for DuplicateTypesInUnion {
 }
 
 ///PYI016
-pub fn duplicate_types_in_union(checker: &mut Checker, left: &Expr, right: &Expr) {
+pub fn duplicate_types_in_union(checker: &mut Checker, left_expr: &Expr, potential_dup: &Expr) {
     // The union data structure always works like so:
     // a | b | c | d -> (((a | b) | c) | d).
     // This function gets called on each pair of brackets, so it's safe to only check if the
     // right is the duplicate (since we will have already checked the others in other invocations)
 
-    // Collapse down the left side of the left expression into a vector of nodes
-    let mut left_nodes: Vec<ExprKind> = Vec::new();
-    let mut left_tree = left.node.clone();
-    loop {
+    let mut working_tree = left_expr.node.clone();
+
+    if loop {
         if let ExprKind::BinOp {
             op: Operator::BitOr,
-            left,
-            right,
-        } = left_tree
+            left: left_node,
+            right: right_node,
+        } = working_tree
         {
-            left_nodes.push(right.node);
-            left_tree = left.node;
+            if right_node.node == potential_dup.node {
+                // Early exit - we only care if there _is_ a duplicate, not how many
+                break true;
+            } else {
+                working_tree = left_node.node;
+                continue;
+            }
         } else {
             // We found a non-union node.
-            // Tree traversal stops here but add this node to the vec
-            left_nodes.push(left_tree);
-            break;
+            // Tree traversal stops here, but still check this node
+            break working_tree == potential_dup.node;
         }
-    }
-
-    if left_nodes.contains(&right.node) {
+    } {
+        // If we broke the loop with true, create a violation
         let mut diagnostic = Diagnostic::new(
             DuplicateTypesInUnion {
-                duplicate_name: right.node.name().to_string(),
+                duplicate_name: potential_dup.node.name().to_string(),
             },
-            Range::from(right),
+            Range::from(potential_dup),
         );
         if checker.patch(diagnostic.kind.rule()) {
             // We want to delete the "|" character as well as the duplicate
             // value, so delete from the end of "left" to the end of "right"
             diagnostic.set_fix(Edit::deletion(
-                left.end_location.unwrap(),
-                right.end_location.unwrap(),
+                left_expr.end_location.unwrap(),
+                potential_dup.end_location.unwrap(),
             ));
         }
         checker.diagnostics.push(diagnostic);
