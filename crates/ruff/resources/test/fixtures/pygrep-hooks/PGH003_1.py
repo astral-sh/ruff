299a955c44700c0eb1e7f43acf36e7701a01baba
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-04-11 22:51:25 -0400

    Support pyright: ignore comments

---
+++ crates/ruff/resources/test/fixtures/pygrep-hooks/PGH003_1.py
@@ -0,0 +1,16 @@
+x = 1  # pyright: ignore
+x = 1  # pyright:ignore
+x = 1  # pyright: ignore[attr-defined]  # pyright: ignore
+
+x = 1
+x = 1  # pyright ignore
+x = 1  # pyright ignore  # noqa
+x = 1  # pyright: ignore[attr-defined]
+x = 1  # pyright: ignore[attr-defined, name-defined]
+x = 1  # pyright: ignore[attr-defined]  # pyright: ignore[type-mismatch]
+x = 1  # pyright: ignore[type-mismatch]  # noqa
+x = 1  # pyright: ignore [attr-defined]
+x = 1  # pyright: ignore [attr-defined, name-defined]
+x = 1  # pyright: ignore [type-mismatch]  # noqa
+x = 1  # pyright: Union[int, str]
+x = 1  # pyright: ignoreme
