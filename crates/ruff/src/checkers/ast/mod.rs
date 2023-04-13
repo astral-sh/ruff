e160a52bfddb9e43398cf5f01b8c49956e0b71f3
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-04-12 23:59:20 -0400

    Raise percent-format upgrade rule (`UP031`) for hanging modulos (#3953)

--- crates/ruff/src/checkers/ast/mod.rs
+++ crates/ruff/src/checkers/ast/mod.rs
@@ -3234,7 +3234,7 @@ where
                     }
 
                     if self.settings.rules.enabled(Rule::PrintfStringFormatting) {
-                        pyupgrade::rules::printf_string_formatting(self, expr, left, right);
+                        pyupgrade::rules::printf_string_formatting(self, expr, right);
                     }
                     if self.settings.rules.enabled(Rule::BadStringFormatType) {
                         pylint::rules::bad_string_format_type(self, expr, right);
