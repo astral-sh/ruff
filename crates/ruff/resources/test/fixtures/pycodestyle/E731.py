c89bc50166492fe94f065d39ec29b13d5e9bd929
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-04-16 11:21:47 +0530

    Preserve type annotations when fixing `E731`

--- crates/ruff/resources/test/fixtures/pycodestyle/E731.py
+++ crates/ruff/resources/test/fixtures/pycodestyle/E731.py
@@ -21,3 +21,30 @@ f = []
 f.append(lambda x: x**2)
 f = g = lambda x: x**2
 lambda: "no-op"
+
+# Annotated
+from typing import Callable, ParamSpec
+
+P = ParamSpec("P")
+
+# ParamSpec cannot be used in this context, so do not preserve the annotation.
+f: Callable[P, int] = lambda *args: len(args)
+f: Callable[[], None] = lambda: None
+f: Callable[..., None] = lambda a, b: None
+f: Callable[[int], int] = lambda x: 2 * x
+
+# Let's use the `Callable` type from `collections.abc` instead.
+from collections.abc import Callable
+
+f: Callable[[str, int], str] = lambda a, b: a * b
+f: Callable[[str, int], tuple[str, int]] = lambda a, b: (a, b)
+f: Callable[[str, int, list[str]], list[str]] = lambda a, b, /, c: [*c, a * b]
+
+
+# Override `Callable`
+class Callable:
+    pass
+
+
+# Do not copy the annotation from here on out.
+f: Callable[[str, int], str] = lambda a, b: a * b
