b3f8f2a5c115ce77301ee5bafe0b4995bd685d54
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-04-19 15:28:56 -0400

    Remove TODO in `handle_node_store` (#4031)

--- crates/ruff/src/checkers/ast/mod.rs
+++ crates/ruff/src/checkers/ast/mod.rs
@@ -4488,7 +4488,6 @@ impl<'a> Checker<'a> {
             return;
         }
 
-        // TODO(charlie): Include comprehensions here.
         if matches!(
             parent.node,
             StmtKind::For { .. } | StmtKind::AsyncFor { .. }
