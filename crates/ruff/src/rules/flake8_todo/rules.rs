fa6983296b32b9af581e71a4e811753bbf492592
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-04-23 10:12:35 -0500

    Minor fixes

--- crates/ruff/src/rules/flake8_todo/rules.rs
+++ crates/ruff/src/rules/flake8_todo/rules.rs
@@ -2,7 +2,7 @@ use std::iter::Peekable;
 
 use once_cell::sync::Lazy;
 
-use regex::{CaptureMatches, Regex};
+use regex::{CaptureMatches, Regex, RegexSet};
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::types::Range;
@@ -217,15 +217,19 @@ impl Violation for MissingSpaceAfterColonInTodo {
 //
 // Note: Regexes taken from https://github.com/orsinium-labs/flake8-todos/blob/master/flake8_todos/_rules.py#L12.
 static TODO_REGEX: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r"^#\s*(?P<tag>[tT][oO][dD][oO]|BUG|FIXME|XXX)( {0,1}\(.*\))?(:)?( )?(.+)?$").unwrap()
+    Regex::new(r"^#\s*(?P<tag>[tT][oO][dD][oO]|BUG|FIXME|XXX)( {0,1}\(.*\))?(:)?( )?(.+)?$")
+        .unwrap()
 });
 
-// Issue code: TDO-003
-static ISSUE_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r#"^#\s*[A-Z]{1,6}\-?\d+$"#).unwrap());
-// Issue code: 003
-static TICKET_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r#"^#\s*\d+$"#).unwrap());
-// Link to issue
-static LINK_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r#"^#\s*(http|https)://.*"#).unwrap());
+static ISSUE_LINK_REGEX_SET: Lazy<RegexSet> = Lazy::new(|| {
+    RegexSet::new([
+        r#"^#\s*(http|https)://.*"#, // issue link
+        r#"^#\s*\d+$"#,              // issue code - like "003"
+        r#"^#\s*[A-Z]{1,6}\-?\d+$"#, // issue code - like "TDO-003"
+    ])
+    .unwrap()
+});
+
 static NUM_CAPTURE_GROUPS: usize = 5usize;
 
 pub fn check_rules(tokens: &[LexResult]) -> Vec<Diagnostic> {
@@ -242,13 +246,10 @@ pub fn check_rules(tokens: &[LexResult]) -> Vec<Diagnostic> {
 
         // The previous token is a TODO, so let's check if this token is a link
         if prev_token_is_todo {
-            if ISSUE_REGEX.is_match(comment)
-                || TICKET_REGEX.is_match(comment)
-                || LINK_REGEX.is_match(comment)
-            {
+            if ISSUE_LINK_REGEX_SET.is_match(comment) {
                 prev_token_is_todo = false;
-                // continuing here avoids an expensive call to captures_iter() - we know that this
-                // line, if it's a link, can't be another TODO
+                // continuing here avoids an expensive call to captures_iter(). if this line is a
+                // link, we can skip checking if it's a TODO
                 continue;
             }
 
