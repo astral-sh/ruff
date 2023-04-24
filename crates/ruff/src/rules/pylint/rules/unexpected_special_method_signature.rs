f8fbb4adc856f5214af5ce1696593c3644b3e75f
Trevor McCulloch <mccullocht@gmail.com>
2023-04-24 12:17:44 -0700

    address review comments

--- crates/ruff/src/rules/pylint/rules/unexpected_special_method_signature.rs
+++ crates/ruff/src/rules/pylint/rules/unexpected_special_method_signature.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeInclusive;
+use std::cmp::Ordering;
 
 use rustpython_parser::ast::{Arguments, Expr, Stmt};
 
@@ -11,10 +11,74 @@ use ruff_python_semantic::scope::ScopeKind;
 
 use crate::checkers::ast::Checker;
 
+#[derive(Debug, Eq, PartialEq)]
+pub enum ExpectedParams {
+    Fixed(usize),
+    Range(usize, usize),
+}
+
+impl ExpectedParams {
+    fn from_method(name: &str, is_staticmethod: bool) -> Option<ExpectedParams> {
+        let p = match name {
+            "__del__" | "__repr__" | "__str__" | "__bytes__" | "__hash__" | "__bool__"
+            | "__dir__" | "__len__" | "__length_hint__" | "__iter__" | "__reversed__"
+            | "__neg__" | "__pos__" | "__abs__" | "__invert__" | "__complex__" | "__int__"
+            | "__float__" | "__index__" | "__trunc__" | "__floor__" | "__ceil__" | "__enter__"
+            | "__aenter__" | "__getnewargs_ex__" | "__getnewargs__" | "__getstate__"
+            | "__reduce__" | "__copy__" | "__unicode__" | "__nonzero__" | "__await__"
+            | "__aiter__" | "__anext__" | "__fspath__" | "__subclasses__" => {
+                Some(ExpectedParams::Fixed(0))
+            }
+            "__format__" | "__lt__" | "__le__" | "__eq__" | "__ne__" | "__gt__" | "__ge__"
+            | "__getattr__" | "__getattribute__" | "__delattr__" | "__delete__"
+            | "__instancecheck__" | "__subclasscheck__" | "__getitem__" | "__missing__"
+            | "__delitem__" | "__contains__" | "__add__" | "__sub__" | "__mul__"
+            | "__truediv__" | "__floordiv__" | "__rfloordiv__" | "__mod__" | "__divmod__"
+            | "__lshift__" | "__rshift__" | "__and__" | "__xor__" | "__or__" | "__radd__"
+            | "__rsub__" | "__rmul__" | "__rtruediv__" | "__rmod__" | "__rdivmod__"
+            | "__rpow__" | "__rlshift__" | "__rrshift__" | "__rand__" | "__rxor__" | "__ror__"
+            | "__iadd__" | "__isub__" | "__imul__" | "__itruediv__" | "__ifloordiv__"
+            | "__imod__" | "__ilshift__" | "__irshift__" | "__iand__" | "__ixor__" | "__ior__"
+            | "__ipow__" | "__setstate__" | "__reduce_ex__" | "__deepcopy__" | "__cmp__"
+            | "__matmul__" | "__rmatmul__" | "__imatmul__" | "__div__" => {
+                Some(ExpectedParams::Fixed(1))
+            }
+            "__setattr__" | "__get__" | "__set__" | "__setitem__" | "__set_name__" => {
+                Some(ExpectedParams::Fixed(2))
+            }
+            "__exit__" | "__aexit__" => Some(ExpectedParams::Fixed(3)),
+            "__round__" => Some(ExpectedParams::Range(0, 1)),
+            "__pow__" => Some(ExpectedParams::Range(1, 2)),
+            _ => None,
+        }?;
+
+        Some(if is_staticmethod {
+            p
+        } else {
+            match p {
+                ExpectedParams::Fixed(n) => ExpectedParams::Fixed(n + 1),
+                ExpectedParams::Range(min, max) => ExpectedParams::Range(min + 1, max + 1),
+            }
+        })
+    }
+
+    fn message(&self) -> String {
+        match self {
+            ExpectedParams::Fixed(n) if *n == 1 => "1 parameter".to_string(),
+            ExpectedParams::Fixed(n) => {
+                format!("{} parameters", *n)
+            }
+            ExpectedParams::Range(min, max) => {
+                format!("between {} and {} parameters", *min, *max)
+            }
+        }
+    }
+}
+
 #[violation]
 pub struct UnexpectedSpecialMethodSignature {
     pub method_name: String,
-    pub expected_params: std::ops::RangeInclusive<usize>,
+    pub expected_params: ExpectedParams,
     pub actual_params: usize,
 }
 
@@ -26,61 +90,14 @@ impl Violation for UnexpectedSpecialMethodSignature {
         } else {
             "was"
         };
-        if self.expected_params.end() - self.expected_params.start() > 1 {
-            format!(
-                "The special method '{}' expects between {} and {} param(s), {} {} given",
-                self.method_name,
-                self.expected_params.start(),
-                self.expected_params.end(),
-                self.actual_params,
-                verb,
-            )
-        } else {
-            format!(
-                "The special method '{}' expects {} param(s), {} {} given",
-                self.method_name,
-                self.expected_params.start(),
-                self.actual_params,
-                verb,
-            )
-        }
+        format!(
+            "The special method '{}' expects {}, {} {} given",
+            self.method_name,
+            self.expected_params.message(),
+            self.actual_params,
+            verb
+        )
     }
-}
-
-fn expected_params(name: &str, is_staticmethod: bool) -> Option<RangeInclusive<usize>> {
-    match name {
-        "__del__" | "__repr__" | "__str__" | "__bytes__" | "__hash__" | "__bool__" | "__dir__"
-        | "__len__" | "__length_hint__" | "__iter__" | "__reversed__" | "__neg__" | "__pos__"
-        | "__abs__" | "__invert__" | "__complex__" | "__int__" | "__float__" | "__index__"
-        | "__trunc__" | "__floor__" | "__ceil__" | "__enter__" | "__aenter__"
-        | "__getnewargs_ex__" | "__getnewargs__" | "__getstate__" | "__reduce__" | "__copy__"
-        | "__unicode__" | "__nonzero__" | "__await__" | "__aiter__" | "__anext__"
-        | "__fspath__" | "__subclasses__" => Some(0..=0),
-        "__format__" | "__lt__" | "__le__" | "__eq__" | "__ne__" | "__gt__" | "__ge__"
-        | "__getattr__" | "__getattribute__" | "__delattr__" | "__delete__"
-        | "__instancecheck__" | "__subclasscheck__" | "__getitem__" | "__missing__"
-        | "__delitem__" | "__contains__" | "__add__" | "__sub__" | "__mul__" | "__truediv__"
-        | "__floordiv__" | "__rfloordiv__" | "__mod__" | "__divmod__" | "__lshift__"
-        | "__rshift__" | "__and__" | "__xor__" | "__or__" | "__radd__" | "__rsub__"
-        | "__rmul__" | "__rtruediv__" | "__rmod__" | "__rdivmod__" | "__rpow__" | "__rlshift__"
-        | "__rrshift__" | "__rand__" | "__rxor__" | "__ror__" | "__iadd__" | "__isub__"
-        | "__imul__" | "__itruediv__" | "__ifloordiv__" | "__imod__" | "__ilshift__"
-        | "__irshift__" | "__iand__" | "__ixor__" | "__ior__" | "__ipow__" | "__setstate__"
-        | "__reduce_ex__" | "__deepcopy__" | "__cmp__" | "__matmul__" | "__rmatmul__"
-        | "__imatmul__" | "__div__" => Some(1..=1),
-        "__setattr__" | "__get__" | "__set__" | "__setitem__" | "__set_name__" => Some(2..=2),
-        "__exit__" | "__aexit__" => Some(3..=3),
-        "__round__" => Some(0..=1),
-        "__pow__" => Some(1..=2),
-        _ => return None,
-    }
-    .map(|r| {
-        if is_staticmethod {
-            r
-        } else {
-            RangeInclusive::new(r.start() + 1, r.end() + 1)
-        }
-    })
 }
 
 /// PLE0302
@@ -105,28 +122,29 @@ pub fn unexpected_special_method_signature(
     let optional_params = args.defaults.len();
     let mandatory_params = actual_params - optional_params;
 
-    if let Some(expected_params) =
-        expected_params(name, is_staticmethod(&checker.ctx, decorator_list))
-    {
-        let emit = if expected_params.end() - expected_params.start() > 1 {
-            !expected_params.contains(&actual_params)
-        } else if *expected_params.start() < mandatory_params {
-            true
-        } else if *expected_params.start() > mandatory_params {
-            args.vararg.is_none() && optional_params < (*expected_params.start() - mandatory_params)
-        } else {
-            false
-        };
+    let Some(expected_params) = ExpectedParams::from_method(name, is_staticmethod(&checker.ctx, decorator_list)) else {
+        return;
+    };
 
-        if emit {
-            checker.diagnostics.push(Diagnostic::new(
-                UnexpectedSpecialMethodSignature {
-                    method_name: name.to_owned(),
-                    expected_params,
-                    actual_params,
-                },
-                identifier_range(stmt, locator),
-            ));
-        }
+    let emit = match expected_params {
+        ExpectedParams::Range(min, max) => !(min..=max).contains(&actual_params),
+        ExpectedParams::Fixed(expected) => match expected.cmp(&mandatory_params) {
+            Ordering::Less => true,
+            Ordering::Greater => {
+                args.vararg.is_none() && optional_params < (expected - mandatory_params)
+            }
+            Ordering::Equal => false,
+        },
+    };
+
+    if emit {
+        checker.diagnostics.push(Diagnostic::new(
+            UnexpectedSpecialMethodSignature {
+                method_name: name.to_owned(),
+                expected_params,
+                actual_params,
+            },
+            identifier_range(stmt, locator),
+        ));
     }
 }
