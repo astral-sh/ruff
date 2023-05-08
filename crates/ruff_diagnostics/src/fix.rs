dcbb469e0df3e84fc5479f9e3f5d7cf5b047261c
Zanie <contact@zanie.dev>
2023-05-08 13:13:22 -0500

    Rename `Applicability` members to `Automatic`, `Suggested`, `Manual`

--- crates/ruff_diagnostics/src/fix.rs
+++ crates/ruff_diagnostics/src/fix.rs
@@ -9,15 +9,21 @@ use crate::edit::Edit;
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[non_exhaustive]
 pub enum Applicability {
-    /// The suggestion is definitely what the user intended, or maintains the exact meaning of the code.
-    /// This suggestion should be automatically applied.
-    Safe,
+    /// The fix is definitely what the user intended, or maintains the exact meaning of the code.
+    /// This fix should be automatically applied.
+    Automatic,
 
-    /// The suggestion may be what the user intended, but it is uncertain.
-    /// The suggestion should result in valid code if it is applied.
-    MaybeIncorrect,
+    /// The fix may be what the user intended, but it is uncertain.
+    /// The fix should result in valid code if it is applied.
+    /// The fix can be applied with user opt-in.
+    Suggested,
 
-    /// The applicability of the suggestion is unknown.
+    /// The fix has a good chance of being incorrect or the code be incomplete.
+    /// The fix may result in invalid code if it is applied.
+    /// The fix should only be manually applied by the user.
+    Manual,
+
+    /// The applicability of the fix is unknown.
     #[default]
     Unspecified,
 }
@@ -47,37 +53,55 @@ impl Fix {
         }
     }
 
-    /// Create a new [`Fix`] with safe applicability from an [`Edit`] element.
-    pub fn safe(edit: Edit) -> Self {
+    /// Create a new [`Fix`] with automatic applicability from an [`Edit`] element.
+    pub fn automatic(edit: Edit) -> Self {
         Self {
             edits: vec![edit],
-            applicability: Applicability::safe,
+            applicability: Applicability::Automatic,
         }
     }
 
-    /// Create a new [`Fix`] with safe applicability from multiple [`Edit`] elements.
-    pub fn safe_edits(edit: Edit, rest: impl IntoIterator<Item = Edit>) -> Self {
+    /// Create a new [`Fix`] with automatic applicability from multiple [`Edit`] elements.
+    pub fn automatic_edits(edit: Edit, rest: impl IntoIterator<Item = Edit>) -> Self {
         Self {
             edits: std::iter::once(edit).chain(rest.into_iter()).collect(),
-            applicability: Applicability::safe,
+            applicability: Applicability::Automatic,
         }
     }
 
-    /// Create a new [`Fix`] with maybe incorrect applicability from an [`Edit`] element.
-    pub fn maybe_incorrect(edit: Edit) -> Self {
+    /// Create a new [`Fix`] with suggsted applicability from an [`Edit`] element.
+    pub fn suggested(edit: Edit) -> Self {
         Self {
             edits: vec![edit],
-            applicability: Applicability::MaybeIncorrect,
+            applicability: Applicability::Suggested,
         }
     }
 
-    /// Create a new [`Fix`] with maybe incorrect applicability from multiple [`Edit`] elements.
-    pub fn maybe_incorrect_edits(edit: Edit, rest: impl IntoIterator<Item = Edit>) -> Self {
+    /// Create a new [`Fix`] with suggsted applicability from multiple [`Edit`] elements.
+    pub fn suggested_edits(edit: Edit, rest: impl IntoIterator<Item = Edit>) -> Self {
         Self {
             edits: std::iter::once(edit).chain(rest.into_iter()).collect(),
-            applicability: Applicability::MaybeIncorrect,
+            applicability: Applicability::Suggested,
         }
     }
+
+
+    /// Create a new [`Fix`] with manual applicability from an [`Edit`] element.
+    pub fn manual(edit: Edit) -> Self {
+        Self {
+            edits: vec![edit],
+            applicability: Applicability::Manual,
+        }
+    }
+
+    /// Create a new [`Fix`] with manual applicability from multiple [`Edit`] elements.
+    pub fn manual_edits(edit: Edit, rest: impl IntoIterator<Item = Edit>) -> Self {
+        Self {
+            edits: std::iter::once(edit).chain(rest.into_iter()).collect(),
+            applicability: Applicability::Manual,
+        }
+    }
+
 
     /// Return the [`TextSize`] of the first [`Edit`] in the [`Fix`].
     pub fn min_start(&self) -> Option<TextSize> {
