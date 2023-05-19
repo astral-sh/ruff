ad577310a13805be436dedd91967c747f20f0c09
Hoel Bagard <hoel.bagard@gmail.com>
2023-05-19 12:27:42 +0900

    Updated to reflect upstream changes.

--- crates/ruff/src/rules/pylint/rules/duplicate_values.rs
+++ crates/ruff/src/rules/pylint/rules/duplicate_values.rs
@@ -1,5 +1,4 @@
 use ruff_python_ast::comparable::ComparableExpr;
-use ruff_python_ast::helpers::unparse_constant;
 use rustc_hash::FxHashSet;
 use rustpython_parser::ast::{self, Expr, Ranged};
 use std::hash::BuildHasherDefault;
@@ -30,14 +29,12 @@ pub(crate) fn duplicate_values(checker: &mut Checker, elts: &Vec<Expr>) {
 
     for elt in elts {
         if let Expr::Constant(ast::ExprConstant { value, .. }) = elt {
-            // dbg!(value);
             let comparable_value: ComparableExpr = elt.into();
-            // dbg!(&comparable_value);
 
             if seen_values.contains(&comparable_value) {
                 checker.diagnostics.push(Diagnostic::new(
                     DuplicateValues {
-                        value: unparse_constant(value, checker.stylist),
+                        value: checker.generator().constant(value),
                     },
                     elt.range(),
                 ));
