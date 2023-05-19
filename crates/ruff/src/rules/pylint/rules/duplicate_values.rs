01e59e14b53f7d04f97eba3bd97fcff679c79724
Hoel Bagard <hoel.bagard@gmail.com>
2023-05-19 16:26:35 +0900

    Avoid avoid allocating capacity to the HashSet when the elts do not contain a constant.

--- crates/ruff/src/rules/pylint/rules/duplicate_values.rs
+++ crates/ruff/src/rules/pylint/rules/duplicate_values.rs
@@ -1,7 +1,6 @@
 use ruff_python_ast::comparable::ComparableExpr;
 use rustc_hash::FxHashSet;
 use rustpython_parser::ast::{self, Expr, Ranged};
-use std::hash::BuildHasherDefault;
 
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
@@ -24,8 +23,7 @@ impl Violation for DuplicateValues {
 /// PLW0130
 /// "This message is emitted when a set contains the same value two or more times.",
 pub(crate) fn duplicate_values(checker: &mut Checker, elts: &Vec<Expr>) {
-    let mut seen_values: FxHashSet<ComparableExpr> =
-        FxHashSet::with_capacity_and_hasher(elts.len(), BuildHasherDefault::default());
+    let mut seen_values: FxHashSet<ComparableExpr> = FxHashSet::default();
 
     for elt in elts {
         if let Expr::Constant(ast::ExprConstant { value, .. }) = elt {
