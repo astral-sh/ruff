9a078654a6502145d6eae6860b8ac0cfcebd372e
Hoel Bagard <hoel.bagard@gmail.com>
2023-05-20 23:01:50 +0900

    Added problematic case to the fixture.

--- crates/ruff/resources/test/fixtures/ruff/RUF005.py
+++ crates/ruff/resources/test/fixtures/ruff/RUF005.py
@@ -4,6 +4,7 @@ class Fun:
     def yay(self):
         return self.words
 
+
 yay = Fun().yay
 
 foo = [4, 5, 6]
@@ -13,11 +14,11 @@ quux = (7, 8, 9) + zoob
 spam = quux + (10, 11, 12)
 spom = list(spam)
 eggs = spom + [13, 14, 15]
-elatement = ("we all say", ) + yay()
-excitement = ("we all think", ) + Fun().yay()
-astonishment = ("we all feel", ) + Fun.words
+elatement = ("we all say",) + yay()
+excitement = ("we all think",) + Fun().yay()
+astonishment = ("we all feel",) + Fun.words
 
-chain = ['a', 'b', 'c'] + eggs + list(('yes', 'no', 'pants') + zoob)
+chain = ["a", "b", "c"] + eggs + list(("yes", "no", "pants") + zoob)
 
 baz = () + zoob
 
@@ -38,11 +39,11 @@ second = first + [
     6,
 ]
 
-[] + foo + [
-]
+[] + foo + []
 
-[] + foo + [  # This will be preserved, but doesn't prevent the fix
-]
+[] + foo + []  # This will be preserved, but doesn't prevent the fix
 
 # Uses the non-preferred quote style, which should be retained.
 f"{[*a(), 'b']}"
+
+pylint_call = [sys.executable, "-m", "pylint"] + args + [path]
