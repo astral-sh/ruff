3b3ebc58410ae1b18c37551e54ebb571f2f401b4
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2023-05-22 01:51:45 +0200

    Update flake8-implicit-str-concat

--- crates/ruff/src/rules/flake8_implicit_str_concat/rules/implicit.rs
+++ crates/ruff/src/rules/flake8_implicit_str_concat/rules/implicit.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
 use ruff_text_size::TextRange;
-use rustpython_parser::ast::{self, Constant, Expr, Operator, Ranged};
 use rustpython_parser::lexer::LexResult;
 use rustpython_parser::Tok;
 
@@ -84,40 +83,6 @@ impl Violation for MultiLineImplicitStringConcatenation {
     }
 }
 
-/// ## What it does
-/// Checks for string literals that are explicitly concatenated (using the
-/// `+` operator).
-///
-/// ## Why is this bad?
-/// For string literals that wrap across multiple lines, implicit string
-/// concatenation within parentheses is preferred over explicit
-/// concatenation using the `+` operator, as the former is more readable.
-///
-/// ## Example
-/// ```python
-/// z = (
-///     "The quick brown fox jumps over the lazy "
-///     + "dog"
-/// )
-/// ```
-///
-/// Use instead:
-/// ```python
-/// z = (
-///     "The quick brown fox jumps over the lazy "
-///     "dog"
-/// )
-/// ```
-#[violation]
-pub struct ExplicitStringConcatenation;
-
-impl Violation for ExplicitStringConcatenation {
-    #[derive_message_formats]
-    fn message(&self) -> String {
-        format!("Explicitly concatenated string should be implicitly concatenated")
-    }
-}
-
 /// ISC001, ISC002
 pub(crate) fn implicit(
     tokens: &[LexResult],
@@ -149,36 +114,4 @@ pub(crate) fn implicit(
         }
     }
     diagnostics
-}
-
-/// ISC003
-pub(crate) fn explicit(expr: &Expr) -> Option<Diagnostic> {
-    if let Expr::BinOp(ast::ExprBinOp {
-        left,
-        op,
-        right,
-        range: _,
-    }) = expr
-    {
-        if matches!(op, Operator::Add) {
-            if matches!(
-                left.as_ref(),
-                Expr::JoinedStr(_)
-                    | Expr::Constant(ast::ExprConstant {
-                        value: Constant::Str(..) | Constant::Bytes(..),
-                        ..
-                    })
-            ) && matches!(
-                right.as_ref(),
-                Expr::JoinedStr(_)
-                    | Expr::Constant(ast::ExprConstant {
-                        value: Constant::Str(..) | Constant::Bytes(..),
-                        ..
-                    })
-            ) {
-                return Some(Diagnostic::new(ExplicitStringConcatenation, expr.range()));
-            }
-        }
-    }
-    None
 }
