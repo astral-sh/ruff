f9ab054d42650570fefcd63e6ea8c323578c032e
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2023-05-22 23:52:04 +0200

    Remove old architecture from scripts

--- scripts/test_add_rule.py
+++ scripts/test_add_rule.py
@@ -5,10 +5,4 @@ from add_rule import add_rule
 
 if __name__ == "__main__":
     for index, linter in enumerate(get_linters()):
-        add_rule(
-            name=f"DoTheThing{index}",
-            prefix="Z",
-            code=str(index),
-            linter=linter,
-            should_create=False,
-        )
+        add_rule(name=f"DoTheThing{index}", prefix="Z", code=str(index), linter=linter)
