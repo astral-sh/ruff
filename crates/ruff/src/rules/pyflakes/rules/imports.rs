27f1182be453a380ce8b28d87fef5099a2697757
Tom Kuson <12143269+tjkuson@users.noreply.github.com>
2023-05-22 22:26:32 +0100

    Add F4 rules

--- crates/ruff/src/rules/pyflakes/rules/imports.rs
+++ crates/ruff/src/rules/pyflakes/rules/imports.rs
@@ -15,6 +15,54 @@ pub(crate) enum UnusedImportContext {
     Init,
 }
 
+/// ## What it does
+/// Checks for imports that are never used.
+///
+/// ## Why is this bad?
+/// Unused imports confuse the reader and may impact performance negatively.
+/// They should be removed.
+///
+/// If the unused import is used to check for availability of a module,
+/// consider using `importlib.util.find_spec` instead.
+///
+/// ## Example
+/// ```python
+/// import numpy as np  # unused import
+///
+///
+/// def area(radius):
+///     return 3.14 * radius**2
+/// ```
+///
+/// Use instead:
+/// ```python
+/// def area(radius):
+///     return 3.14 * radius**2
+/// ```
+///
+/// ## Example (checking for availability of a module)
+/// ```python
+/// try:
+///     import numpy as np
+/// except ImportError:
+///     print("numpy is not installed")
+/// else:
+///     print("numpy is installed")
+/// ```
+///
+/// Use instead:
+/// ```python
+/// from importlib.util import find_spec
+///
+/// if find_spec("numpy") is not None:
+///     print("numpy is installed")
+/// else:
+///     print("numpy is not installed")
+/// ```
+///
+/// ## References
+/// - [Python documentation](https://docs.python.org/3/reference/simple_stmts.html#the-import-statement)
+/// - [Python documentation](https://docs.python.org/3/library/importlib.html#importlib.util.find_spec)
 #[violation]
 pub struct UnusedImport {
     pub(crate) name: String,
@@ -54,6 +102,36 @@ impl Violation for UnusedImport {
         })
     }
 }
+
+/// ## What it does
+/// Checks for imports that are shadowed by a loop variable.
+///
+/// ## Why is this bad?
+/// Shadowing imports with loop variables makes the code harder to read.
+///
+/// Consider using a different name for the loop variable.
+///
+/// ## Example
+/// ```python
+/// import email
+///
+///
+/// for email in emails:
+///     ...
+/// ```
+///
+/// Use instead:
+/// ```python
+/// import email
+///
+///
+/// for e in emails:
+///     ...
+/// ```
+///
+/// ## References
+/// - [Python documentation](https://docs.python.org/3/reference/simple_stmts.html#the-import-statement)
+/// - [Python documentation](https://docs.python.org/3/reference/compound_stmts.html#the-for-statement)
 #[violation]
 pub struct ImportShadowedByLoopVar {
     pub(crate) name: String,
@@ -68,6 +146,36 @@ impl Violation for ImportShadowedByLoopVar {
     }
 }
 
+/// ## What it does
+/// Checks for `from module import *` statements.
+///
+/// ## Why is this bad?
+/// Wildcard imports makes it hard to determine from where names are imported.
+/// They also pollute the namespace, making it harder to determine which names
+/// are defined in the current module.
+///
+/// ## Example
+/// ```python
+/// from math import *
+///
+///
+/// def area(radius):
+///     return pi * radius**2
+/// ```
+///
+/// Use instead:
+/// ```python
+/// from math import pi
+///
+///
+/// def area(radius):
+///     return pi * radius**2
+/// ```
+///
+/// ## References
+/// - [PEP 8](https://peps.python.org/pep-0008/#imports)
+/// - [Python documentation](https://docs.python.org/3/reference/simple_stmts.html#the-import-statement)
+/// - [Python documentation](https://docs.python.org/3/tutorial/modules.html#importing-from-a-package)
 #[violation]
 pub struct UndefinedLocalWithImportStar {
     pub(crate) name: String,
@@ -81,6 +189,37 @@ impl Violation for UndefinedLocalWithImportStar {
     }
 }
 
+/// ## What it does
+/// Checks for `__future__` imports that are not at the beginning of the file.
+///
+/// ## Why is this bad?
+/// Imports from `__future__` must be at the beginning of the file before any
+/// other code, except docstrings.
+///
+/// ## Example
+/// ```python
+/// """Example docstring."""
+///
+///
+/// from pathlib import Path
+///
+/// from __future__ import annotations
+/// ```
+///
+/// Use instead:
+/// ```python
+/// """Example docstring."""
+///
+///
+/// from __future__ import annotations
+///
+/// from pathlib import Path
+/// ```
+///
+/// ## References
+/// - [PEP 8](https://peps.python.org/pep-0008/#module-level-dunder-names)
+/// - [Python documentation](https://docs.python.org/3/reference/simple_stmts.html#future-statements)
+/// - [Python documentation](https://docs.python.org/3/library/__future__.html)
 #[violation]
 pub struct LateFutureImport;
 
@@ -91,6 +230,59 @@ impl Violation for LateFutureImport {
     }
 }
 
+/// ## What it does
+/// Checks for names that could be undefined but could be defined in a wildcard
+/// import.
+///
+/// ## Why is this bad?
+/// If the name is defined in a wildcard import, it should be imported
+/// explicitly to avoid confusion.
+///
+/// If the name is not defined in a wildcard import and is undefined, it should
+/// be defined or imported.
+///
+/// ## Example (name in wildcard import)
+/// ```python
+/// from math import *
+///
+///
+/// def area(radius):
+///     return pi * radius**2
+/// ```
+///
+/// Use instead:
+/// ```python
+/// from math import pi
+///
+///
+/// def area(radius):
+///     return pi * radius**2
+/// ```
+///
+/// ## Example (name undefined)
+/// ```python
+/// from math import *
+///
+///
+/// def weight_on_earth(mass):
+///     return mass * G  # G is undefined
+/// ```
+///
+/// Use instead:
+/// ```python
+/// from math import *
+///
+/// G = 9.8
+///
+///
+/// def weight_on_earth(mass):
+///     return mass * G
+/// ```
+///
+/// ## References
+/// - [PEP 8](https://peps.python.org/pep-0008/#imports)
+/// - [Python documentation](https://docs.python.org/3/reference/simple_stmts.html#the-import-statement)
+/// - [Python documentation](https://docs.python.org/3/tutorial/modules.html#importing-from-a-package)
 #[violation]
 pub struct UndefinedLocalWithImportStarUsage {
     pub(crate) name: String,
@@ -109,6 +301,36 @@ impl Violation for UndefinedLocalWithImportStarUsage {
     }
 }
 
+/// ## What it does
+/// Check for wildcard imports not in the module-level namespace.
+///
+/// ## Why is this bad?
+/// Wildcard imports at non-module level are confusing and can shadow names
+/// imported in the module-level namespace.
+///
+/// If you are going to use wildcard imports, they should be at the module level
+/// and not nested inside functions or classes.
+///
+/// ## Example
+/// ```python
+/// def foo():
+///     from math import *
+///     ...
+/// ```
+///
+/// Use instead:
+/// ```python
+/// from math import *
+///
+///
+/// def foo():
+///     ...
+/// ```
+///
+/// ## References
+/// - [PEP 8](https://peps.python.org/pep-0008/#imports)
+/// - [Python documentation](https://docs.python.org/3/reference/simple_stmts.html#the-import-statement)
+/// - [Python documentation](https://docs.python.org/3/tutorial/modules.html#importing-from-a-package)
 #[violation]
 pub struct UndefinedLocalWithNestedImportStarUsage {
     pub(crate) name: String,
@@ -122,6 +344,30 @@ impl Violation for UndefinedLocalWithNestedImportStarUsage {
     }
 }
 
+/// ## What it does
+/// Checks for `__future__` imports that are not defined.
+///
+/// ## Why is this bad?
+/// `__future__` imports must be defined in the Python version you are using.
+///
+/// To fix this, check the Python version you are using and make sure the
+/// `__future__` import is defined in that version. If it is not, remove the
+/// import.
+///
+/// ## Example
+/// ```python
+/// # using Python <3.7
+/// from __future__ import annotations
+/// ```
+///
+/// Use instead:
+/// ```python
+/// # using Python >=3.7
+/// from __future__ import annotations
+/// ```
+///
+/// ## References
+/// - [Python documentation](https://docs.python.org/3/library/__future__.html)
 #[violation]
 pub struct FutureFeatureNotDefined {
     name: String,
