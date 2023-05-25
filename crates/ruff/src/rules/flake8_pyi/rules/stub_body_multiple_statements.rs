efbc46e512b6ab0fe850bc0d2b0a7df5ffa217a8
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-05-25 15:53:49 -0400

    Change 1 to one

--- crates/ruff/src/rules/flake8_pyi/rules/stub_body_multiple_statements.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/stub_body_multiple_statements.rs
@@ -1,10 +1,11 @@
 use rustpython_parser::ast::Stmt;
 
-use crate::checkers::ast::Checker;
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::helpers;
 use ruff_python_ast::helpers::is_docstring_stmt;
+
+use crate::checkers::ast::Checker;
 
 #[violation]
 pub struct StubBodyMultipleStatements;
@@ -12,29 +13,25 @@ pub struct StubBodyMultipleStatements;
 impl Violation for StubBodyMultipleStatements {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Function body must contain exactly 1 statement")
+        format!("Function body must contain exactly one statement")
     }
 }
 
 /// PYI010
 pub(crate) fn stub_body_multiple_statements(checker: &mut Checker, stmt: &Stmt, body: &[Stmt]) {
-    match body.len() {
-        1 => (),
-        2 => {
-            // If 2 statements and one is a docstring. Skip as this is covered by PYI021
-            if is_docstring_stmt(&body[0]) {
-                return;
-            }
-            checker.diagnostics.push(Diagnostic::new(
-                StubBodyMultipleStatements,
-                helpers::identifier_range(stmt, checker.locator),
-            ));
-        }
-        _ => {
-            checker.diagnostics.push(Diagnostic::new(
-                StubBodyMultipleStatements,
-                helpers::identifier_range(stmt, checker.locator),
-            ));
-        }
+    // If the function body consists of exactly one statement, abort.
+    if body.len() == 1 {
+        return;
     }
+
+    // If the function body consists of exactly two statements, and the first is a
+    // docstring, abort (this is covered by PYI021).
+    if body.len() == 2 && is_docstring_stmt(&body[0]) {
+        return;
+    }
+
+    checker.diagnostics.push(Diagnostic::new(
+        StubBodyMultipleStatements,
+        helpers::identifier_range(stmt, checker.locator),
+    ));
 }
