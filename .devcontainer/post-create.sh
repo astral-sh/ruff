f0885aa6c86ae7203a884b03f760c3d4e7e95aa1
Vadim Suharnikov <vsuharnikov@gmail.com>
2023-05-27 10:34:38 +0000

    Add devcontainer support (#4676)

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rustup default < rust-toolchain
+rustup component add clippy rustfmt
+cargo install cargo-insta
+cargo fetch
+
+pip install maturin pre-commit
+
+pre-commit install
