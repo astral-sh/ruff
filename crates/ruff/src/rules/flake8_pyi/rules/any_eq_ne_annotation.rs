3e0e6c86e392a02d2698c6ef9ce6713f3e23303a
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-05-28 18:31:59 -0400

    Tweak docs; shuffle around some calls

--- crates/ruff/src/rules/flake8_pyi/rules/any_eq_ne_annotation.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/any_eq_ne_annotation.rs
@@ -1,4 +1,4 @@
-use rustpython_parser::ast::Arguments;
+use rustpython_parser::ast::{Arguments, Ranged};
 
 use ruff_diagnostics::{AlwaysAutofixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
@@ -7,20 +7,22 @@ use crate::checkers::ast::Checker;
 use crate::registry::AsRule;
 
 /// ## What it does
-/// Checks for `Any` type annotations for the second parameter in `__ne__` and `__eq__` methods
+/// Checks for `__eq__` and `__ne__` implementations that use `typing.Any` as
+/// the type annotation for the `obj` parameter.
 ///
 /// ## Why is this bad?
-/// From the Python docs: `Use object to indicate that a value could be any type in a typesafe
-/// manner. Use Any to indicate that a value is dynamically typed.` For `__eq__` and `__ne__` method
-/// we want to do the former.
+/// The Python documentation recommends the use of `object` to "indicate that a
+/// value could be any type in a typesafe manner", while `Any` should be used to
+/// "indicate that a value is dynamically typed."
+///
+/// The semantics of `__eq__` and `__ne__` are such that the `obj` parameter
+/// should be any type, as opposed to a dynamically typed value. Therefore, the
+/// `object` type annotation is more appropriate.
 ///
 /// ## Example
 /// ```python
 /// class Foo:
-///     def __eq__(self, obj: Any):
-///         ...
-///
-///     def __ne__(self, obj: typing.Any):
+///     def __eq__(self, obj: typing.Any):
 ///         ...
 /// ```
 ///
@@ -29,13 +31,10 @@ use crate::registry::AsRule;
 /// class Foo:
 ///     def __eq__(self, obj: object):
 ///         ...
-///
-///     def __ne__(self, obj: object):
-///         ...
 /// ```
 /// ## References
-/// - [Python Docs](https://docs.python.org/3/library/typing.html#the-any-type)
-/// - [Mypy Docs](https://mypy.readthedocs.io/en/latest/dynamic_typing.html#any-vs-object)
+/// - [Python documentation](https://docs.python.org/3/library/typing.html#the-any-type)
+/// - [Mypy documentation](https://mypy.readthedocs.io/en/latest/dynamic_typing.html#any-vs-object)
 #[violation]
 pub struct AnyEqNeAnnotation {
     method_name: String,
@@ -45,56 +44,51 @@ impl AlwaysAutofixableViolation for AnyEqNeAnnotation {
     #[derive_message_formats]
     fn message(&self) -> String {
         let AnyEqNeAnnotation { method_name } = self;
-        format!("Prefer `object` to `Any` for the second parameter in {method_name}")
+        format!("Prefer `object` to `Any` for the second parameter to `{method_name}`")
     }
 
     fn autofix_title(&self) -> String {
-        format!("Replace `object` with `Any`")
+        format!("Replace with `object`")
     }
 }
 
 /// PYI032
 pub(crate) fn any_eq_ne_annotation(checker: &mut Checker, name: &str, args: &Arguments) {
+    if !matches!(name, "__eq__" | "__ne__") {
+        return;
+    }
+
+    if args.args.len() != 2 {
+        return;
+    }
+
+    let Some(annotation) = &args.args[1].annotation else {
+        return;
+    };
+
     if !checker.semantic_model().scope().kind.is_class() {
         return;
     }
 
-    // Ignore non `__eq__` and non `__ne__` methods
-    if name != "__eq__" && name != "__ne__" {
-        return;
-    }
-
-    // Different numbers of arguments than 2 are handled by other rules
-    if args.args.len() == 2 {
-        if let Some(annotation) = &args.args[1].annotation {
-            if checker
-                .semantic_model()
-                .match_typing_expr(annotation, "Any")
-            {
-                let mut diagnostic = Diagnostic::new(
-                    AnyEqNeAnnotation {
-                        method_name: name.to_string(),
-                    },
-                    args.args[1].range,
-                );
-                if checker.patch(diagnostic.kind.rule()) {
-                    // def __eq__(self, arg2: Any): ...
-                    if let Some(name) = annotation.as_name_expr() {
-                        diagnostic.set_fix(Fix::automatic(Edit::range_replacement(
-                            format!("object"),
-                            name.range,
-                        )));
-                    }
-                    // def __eq__(self, arg2: typing.Any): ...
-                    if let Some(attr) = annotation.as_attribute_expr() {
-                        diagnostic.set_fix(Fix::automatic(Edit::range_replacement(
-                            format!("object"),
-                            attr.range,
-                        )));
-                    }
-                }
-                checker.diagnostics.push(diagnostic);
+    if checker
+        .semantic_model()
+        .match_typing_expr(annotation, "Any")
+    {
+        let mut diagnostic = Diagnostic::new(
+            AnyEqNeAnnotation {
+                method_name: name.to_string(),
+            },
+            annotation.range(),
+        );
+        if checker.patch(diagnostic.kind.rule()) {
+            // Ex) `def __eq__(self, obj: Any): ...`
+            if checker.semantic_model().is_builtin("object") {
+                diagnostic.set_fix(Fix::automatic(Edit::range_replacement(
+                    "object".to_string(),
+                    annotation.range(),
+                )));
             }
         }
+        checker.diagnostics.push(diagnostic);
     }
 }
