06bcb85f81790a8dc720fdb258b1ce130aab7fb0
Micha Reiser <micha@reiser.io>
2023-05-31 08:27:23 +0200

    formatter: Remove CST and old formatting (#4730)

--- crates/ruff_python_formatter/src/prelude.rs
+++ crates/ruff_python_formatter/src/prelude.rs
@@ -1,3 +1,4 @@
 #[allow(unused_imports)]
 pub(crate) use crate::{ASTFormatContext, AsFormat, FormattedIterExt as _, IntoFormat};
+#[allow(unused_imports)]
 pub(crate) use ruff_formatter::prelude::*;
