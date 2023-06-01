08940d455a6e982322d7eec2574b462d337705d4
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-06-01 23:51:15 +0530

    Add autofix for `PLR1701` (repeated-isinstance-calls)

--- crates/ruff/src/rules/pylint/rules/repeated_isinstance_calls.rs
+++ crates/ruff/src/rules/pylint/rules/repeated_isinstance_calls.rs
@@ -2,11 +2,13 @@ use itertools::Itertools;
 use rustc_hash::{FxHashMap, FxHashSet};
 use rustpython_parser::ast::{self, Boolop, Expr, Ranged};
 
-use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_diagnostics::{AlwaysAutofixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::hashable::HashableExpr;
 
 use crate::checkers::ast::Checker;
+use crate::registry::AsRule;
+use crate::settings::types::PythonVersion;
 
 /// ## What it does
 /// Checks for repeated `isinstance` calls on the same object.
@@ -38,16 +40,31 @@ use crate::checkers::ast::Checker;
 /// - [Python documentation](https://docs.python.org/3/library/functions.html#isinstance)
 #[violation]
 pub struct RepeatedIsinstanceCalls {
-    obj: String,
-    types: Vec<String>,
+    expr: String,
 }
 
-impl Violation for RepeatedIsinstanceCalls {
+impl AlwaysAutofixableViolation for RepeatedIsinstanceCalls {
     #[derive_message_formats]
     fn message(&self) -> String {
-        let RepeatedIsinstanceCalls { obj, types } = self;
-        let types = types.join(", ");
-        format!("Merge these isinstance calls: `isinstance({obj}, ({types}))`")
+        let RepeatedIsinstanceCalls { expr } = self;
+        format!("Consider merging these isinstance calls to `{expr}`")
+    }
+
+    fn autofix_title(&self) -> String {
+        let RepeatedIsinstanceCalls { expr } = self;
+        format!("Replace with `{expr}`")
+    }
+}
+
+fn merged_isinstance_call(
+    obj: &str,
+    types: impl IntoIterator<Item = String>,
+    target_version: PythonVersion,
+) -> String {
+    if target_version >= PythonVersion::Py310 {
+        format!("isinstance({}, {})", obj, types.into_iter().join(" | "))
+    } else {
+        format!("isinstance({}, ({}))", obj, types.into_iter().join(", "))
     }
 }
 
@@ -91,18 +108,21 @@ pub(crate) fn repeated_isinstance_calls(
 
     for (obj, (num_calls, types)) in obj_to_types {
         if num_calls > 1 && types.len() > 1 {
-            checker.diagnostics.push(Diagnostic::new(
-                RepeatedIsinstanceCalls {
-                    obj: checker.generator().expr(obj.as_expr()),
-                    types: types
-                        .iter()
-                        .map(HashableExpr::as_expr)
-                        .map(|expr| checker.generator().expr(expr))
-                        .sorted()
-                        .collect(),
-                },
-                expr.range(),
-            ));
+            let call = merged_isinstance_call(
+                &checker.generator().expr(obj.as_expr()),
+                types
+                    .iter()
+                    .map(HashableExpr::as_expr)
+                    .map(|expr| checker.generator().expr(expr))
+                    .sorted(),
+                checker.settings.target_version,
+            );
+            let mut diagnostic =
+                Diagnostic::new(RepeatedIsinstanceCalls { expr: call.clone() }, expr.range());
+            if checker.patch(diagnostic.kind.rule()) {
+                diagnostic.set_fix(Fix::automatic(Edit::range_replacement(call, expr.range())));
+            }
+            checker.diagnostics.push(diagnostic);
         }
     }
 }
