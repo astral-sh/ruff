d4027d8b65d2fc88e6474ba1387ecf30cde658ba
konstin <konstin@mailbox.org>
2023-06-01 11:55:04 +0200

    Use new formatter infrastructure in CLI and test (#4767)
    * Use dummy verbatim formatter for all nodes
    
    * Use new formatter infrastructure in CLI and test
    
    * Expose the new formatter in the CLI
    
    * Merge import blocks

--- crates/ruff_python_formatter/src/main.rs
+++ crates/ruff_python_formatter/src/main.rs
@@ -6,12 +6,10 @@ use clap::Parser as ClapParser;
 use ruff_python_formatter::cli::Cli;
 use ruff_python_formatter::format_module;
 
+#[allow(clippy::print_stdout)]
 fn main() -> Result<()> {
     let cli = Cli::parse();
     let contents = fs::read_to_string(cli.file)?;
-    #[allow(clippy::print_stdout)]
-    {
-        println!("{}", format_module(&contents)?.as_code());
-    }
+    println!("{}", format_module(&contents)?.as_code());
     Ok(())
 }
