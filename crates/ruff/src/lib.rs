1ab2ee6271076d264e7893e1183e867d50252867
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-06-02 14:07:05 -0400

    Remove regex

--- crates/ruff/src/lib.rs
+++ crates/ruff/src/lib.rs
@@ -5,6 +5,8 @@
 //!
 //! [Ruff]: https://github.com/charliermarsh/ruff
 
+extern crate core;
+
 pub use ruff_python_ast::source_code::round_trip;
 pub use rule_selector::RuleSelector;
 pub use rules::pycodestyle::rules::IOError;
--- crates/ruff/src/rules/flake8_bandit/rules/shell_injection.rs
+++ crates/ruff/src/rules/flake8_bandit/rules/shell_injection.rs
@@ -1,6 +1,5 @@
 //! Checks relating to shell injection.
 
-use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
 use rustpython_parser::ast::{self, Constant, Expr, Keyword, Ranged};
@@ -15,8 +14,6 @@ use crate::{
 };
 
 static FULL_PATH_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([A-Za-z]:|[\\/.])").unwrap());
-static WILDCARD_COMMAND_REGEX: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r"(^|[\s/])(chown|chmod|tar|rsync)\s.*\*").unwrap());
 
 #[violation]
 pub struct SubprocessPopenWithShellEqualsTrue {
@@ -98,7 +95,121 @@ pub struct UnixCommandWildcardInjection;
 impl Violation for UnixCommandWildcardInjection {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Possible wildcard injection in call")
+        format!("Possible wildcard injection in call due to `*` usage")
+    }
+}
+
+/// S602, S603, S604, S605, S606, S607, S609
+pub(crate) fn shell_injection(
+    checker: &mut Checker,
+    func: &Expr,
+    args: &[Expr],
+    keywords: &[Keyword],
+) {
+    let call_kind = get_call_kind(func, checker.semantic_model());
+    let mut subprocess_with_shell = false;
+
+    if matches!(call_kind, Some(CallKind::Subprocess)) {
+        if let Some(arg) = args.first() {
+            match find_shell_keyword(checker.semantic_model(), keywords) {
+                // S602
+                Some(ShellKeyword {
+                    truthiness: Truthiness::Truthy,
+                    keyword,
+                }) => {
+                    subprocess_with_shell = true;
+                    if checker.enabled(Rule::SubprocessPopenWithShellEqualsTrue) {
+                        checker.diagnostics.push(Diagnostic::new(
+                            SubprocessPopenWithShellEqualsTrue {
+                                seems_safe: shell_call_seems_safe(arg),
+                            },
+                            keyword.range(),
+                        ));
+                    }
+                }
+                // S603
+                Some(ShellKeyword {
+                    truthiness: Truthiness::Falsey | Truthiness::Unknown,
+                    keyword,
+                }) => {
+                    if checker.enabled(Rule::SubprocessWithoutShellEqualsTrue) {
+                        checker.diagnostics.push(Diagnostic::new(
+                            SubprocessWithoutShellEqualsTrue,
+                            keyword.range(),
+                        ));
+                    }
+                }
+                // S603
+                None => {
+                    if checker.enabled(Rule::SubprocessWithoutShellEqualsTrue) {
+                        checker.diagnostics.push(Diagnostic::new(
+                            SubprocessWithoutShellEqualsTrue,
+                            arg.range(),
+                        ));
+                    }
+                }
+            }
+        }
+    } else if let Some(ShellKeyword {
+        truthiness: Truthiness::Truthy,
+        keyword,
+    }) = find_shell_keyword(checker.semantic_model(), keywords)
+    {
+        // S604
+        if checker.enabled(Rule::CallWithShellEqualsTrue) {
+            checker
+                .diagnostics
+                .push(Diagnostic::new(CallWithShellEqualsTrue, keyword.range()));
+        }
+    }
+
+    // S605
+    if checker.enabled(Rule::StartProcessWithAShell) {
+        if matches!(call_kind, Some(CallKind::Shell)) {
+            if let Some(arg) = args.first() {
+                checker.diagnostics.push(Diagnostic::new(
+                    StartProcessWithAShell {
+                        seems_safe: shell_call_seems_safe(arg),
+                    },
+                    arg.range(),
+                ));
+            }
+        }
+    }
+
+    // S606
+    if checker.enabled(Rule::StartProcessWithNoShell) {
+        if matches!(call_kind, Some(CallKind::NoShell)) {
+            checker
+                .diagnostics
+                .push(Diagnostic::new(StartProcessWithNoShell, func.range()));
+        }
+    }
+
+    // S607
+    if checker.enabled(Rule::StartProcessWithPartialPath) {
+        if call_kind.is_some() {
+            if let Some(arg) = args.first() {
+                if is_partial_path(arg) {
+                    checker
+                        .diagnostics
+                        .push(Diagnostic::new(StartProcessWithPartialPath, arg.range()));
+                }
+            }
+        }
+    }
+
+    // S609
+    if checker.enabled(Rule::UnixCommandWildcardInjection) {
+        if matches!(call_kind, Some(CallKind::Shell)) || subprocess_with_shell {
+            if let Some(arg) = args.first() {
+                if is_wildcard_command(arg) {
+                    checker
+                        .diagnostics
+                        .push(Diagnostic::new(UnixCommandWildcardInjection, func.range()));
+                }
+            }
+        }
     }
 }
 
@@ -176,139 +287,50 @@ fn shell_call_seems_safe(arg: &Expr) -> bool {
     )
 }
 
-/// Return the [`Expr`] as a string literal, if it's a string or a list of strings.
-fn try_string_literal(expr: &Expr) -> Option<&str> {
-    match expr {
-        Expr::List(ast::ExprList { elts, .. }) => {
-            if elts.is_empty() {
-                None
-            } else {
-                string_literal(&elts[0])
-            }
-        }
+/// Return `true` if the [`Expr`] is a string literal or list of string literals that starts with a
+/// partial path.
+fn is_partial_path(expr: &Expr) -> bool {
+    let string_literal = match expr {
+        Expr::List(ast::ExprList { elts, .. }) => elts.first().and_then(string_literal),
         _ => string_literal(expr),
-    }
+    };
+    string_literal.map_or(false, |text| !FULL_PATH_REGEX.is_match(text))
 }
 
-/// S602, S603, S604, S605, S606, S607, S609
-pub(crate) fn shell_injection(
-    checker: &mut Checker,
-    func: &Expr,
-    args: &[Expr],
-    keywords: &[Keyword],
-) {
-    let call_kind = get_call_kind(func, checker.semantic_model());
-    let mut subprocess_with_shell = false;
-
-    if matches!(call_kind, Some(CallKind::Subprocess)) {
-        if let Some(arg) = args.first() {
-            match find_shell_keyword(checker.semantic_model(), keywords) {
-                // S602
-                Some(ShellKeyword {
-                    truthiness: Truthiness::Truthy,
-                    keyword,
-                }) => {
-                    subprocess_with_shell = true;
-                    if checker.enabled(Rule::SubprocessPopenWithShellEqualsTrue) {
-                        checker.diagnostics.push(Diagnostic::new(
-                            SubprocessPopenWithShellEqualsTrue {
-                                seems_safe: shell_call_seems_safe(arg),
-                            },
-                            keyword.range(),
-                        ));
-                    }
-                }
-                // S603
-                Some(ShellKeyword {
-                    truthiness: Truthiness::Falsey | Truthiness::Unknown,
-                    keyword,
-                }) => {
-                    if checker.enabled(Rule::SubprocessWithoutShellEqualsTrue) {
-                        checker.diagnostics.push(Diagnostic::new(
-                            SubprocessWithoutShellEqualsTrue,
-                            keyword.range(),
-                        ));
-                    }
-                }
-                // S603
-                None => {
-                    if checker.enabled(Rule::SubprocessWithoutShellEqualsTrue) {
-                        checker.diagnostics.push(Diagnostic::new(
-                            SubprocessWithoutShellEqualsTrue,
-                            arg.range(),
-                        ));
-                    }
-                }
+/// Return `true` if the [`Expr`] is a wildcard command.
+///
+/// ## Examples
+/// ```python
+/// import subprocess
+///
+/// subprocess.Popen("/bin/chown root: *", shell=True)
+/// subprocess.Popen(["/usr/local/bin/rsync", "*", "some_where:"], shell=True)
+/// ```
+fn is_wildcard_command(expr: &Expr) -> bool {
+    if let Expr::List(ast::ExprList { elts, .. }) = expr {
+        let mut has_star = false;
+        let mut has_command = false;
+        for elt in elts.iter() {
+            if let Some(text) = string_literal(elt) {
+                has_star |= text.contains('*');
+                has_command |= text.contains("chown")
+                    || text.contains("chmod")
+                    || text.contains("tar")
+                    || text.contains("rsync");
+            }
+            if has_star && has_command {
+                break;
             }
         }
-    } else if let Some(ShellKeyword {
-        truthiness: Truthiness::Truthy,
-        keyword,
-    }) = find_shell_keyword(checker.semantic_model(), keywords)
-    {
-        // S604
-        if checker.enabled(Rule::CallWithShellEqualsTrue) {
-            checker
-                .diagnostics
-                .push(Diagnostic::new(CallWithShellEqualsTrue, keyword.range()));
-        }
-    }
-
-    // S605
-    if matches!(call_kind, Some(CallKind::Shell)) {
-        if let Some(arg) = args.first() {
-            if checker.enabled(Rule::StartProcessWithAShell) {
-                checker.diagnostics.push(Diagnostic::new(
-                    StartProcessWithAShell {
-                        seems_safe: shell_call_seems_safe(arg),
-                    },
-                    arg.range(),
-                ));
-            }
-        }
-    }
-
-    // S606
-    if matches!(call_kind, Some(CallKind::NoShell)) {
-        if checker.enabled(Rule::StartProcessWithNoShell) {
-            checker
-                .diagnostics
-                .push(Diagnostic::new(StartProcessWithNoShell, func.range()));
-        }
-    }
-
-    // S607
-    if call_kind.is_some() {
-        if let Some(arg) = args.first() {
-            if checker.enabled(Rule::StartProcessWithPartialPath) {
-                if let Some(value) = try_string_literal(arg) {
-                    if FULL_PATH_REGEX.find(value).is_none() {
-                        checker
-                            .diagnostics
-                            .push(Diagnostic::new(StartProcessWithPartialPath, arg.range()));
-                    }
-                }
-            }
-        }
-    }
-
-    // S609
-    if checker.enabled(Rule::UnixCommandWildcardInjection)
-        && (matches!(call_kind, Some(CallKind::Shell)) || subprocess_with_shell)
-    {
-        if let Some(cmd) = args.first() {
-            let cmd_string = match cmd {
-                Expr::List(ast::ExprList { elts, .. }) => elts
-                    .iter()
-                    .map(|elt| string_literal(elt).unwrap_or_default())
-                    .join(" "),
-                _ => String::from(string_literal(cmd).unwrap_or_default()),
-            };
-            if WILDCARD_COMMAND_REGEX.is_match(cmd_string.as_str()) {
-                checker
-                    .diagnostics
-                    .push(Diagnostic::new(UnixCommandWildcardInjection, func.range()));
-            }
-        }
+        has_star && has_command
+    } else {
+        let string_literal = string_literal(expr);
+        string_literal.map_or(false, |text| {
+            text.contains('*')
+                && (text.contains("chown")
+                    || text.contains("chmod")
+                    || text.contains("tar")
+                    || text.contains("rsync"))
+        })
     }
 }
