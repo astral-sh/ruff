ae34d629ac47d7818c06ad997ce2d4855d0d5b4e
konstin <konstin@mailbox.org>
2023-06-02 14:08:04 +0200

    Add a formatter CLI for debugging
    This adds a ruff_python_formatter cli modelled aber `rustfmt` that i use for debugging

--- crates/ruff_python_formatter/src/cli.rs
+++ crates/ruff_python_formatter/src/cli.rs
@@ -1,11 +1,24 @@
 use std::path::PathBuf;
 
-use clap::{command, Parser};
+use clap::{command, Parser, ValueEnum};
+
+#[derive(ValueEnum, Clone, Debug)]
+pub enum Emit {
+    /// Write back to the original files
+    Files,
+    /// Write to stdout
+    Stdout,
+}
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    /// Python file to round-trip.
-    #[arg(required = true)]
-    pub file: PathBuf,
+    /// Python files to format. If there are none, stdin will be used. `-` as stdin is not supported
+    pub files: Vec<PathBuf>,
+    #[clap(long)]
+    pub emit: Option<Emit>,
+    /// Run in 'check' mode. Exits with 0 if input is formatted correctly. Exits with 1 and prints
+    /// a diff if formatting is required.
+    #[clap(long)]
+    pub check: bool,
 }
