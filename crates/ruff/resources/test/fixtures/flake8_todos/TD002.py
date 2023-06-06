e23fe73bba27717713043775299cc91944cef2a9
mayrholu <34736939+mayrholu@users.noreply.github.com>
2023-06-06 15:49:52 +0200

    Add tests for new TD002 author format

--- crates/ruff/resources/test/fixtures/flake8_todos/TD002.py
+++ crates/ruff/resources/test/fixtures/flake8_todos/TD002.py
@@ -1,6 +1,8 @@
 # T002 - accepted
 # TODO (evanrittenhouse): this has an author
 # TODO(evanrittenhouse): this also has an author
+# TODO@someauthor: this has an author
+# TODO @someauthor: this also has an author
 # T002 - errors
 # TODO: this has no author
 # FIXME: neither does this
