3a5b9ffaab122200de68a33cda16b6136af06617
qdegraaf <querijndegraaf@gmail.com>
2023-06-06 23:38:32 +0200

    Fix docs

--- crates/ruff/src/rules/flake8_slots/rules/no_slots_in_namedtuple_subclass.rs
+++ crates/ruff/src/rules/flake8_slots/rules/no_slots_in_namedtuple_subclass.rs
@@ -22,7 +22,7 @@ use crate::rules::flake8_slots::rules::helpers::has_slots;
 /// ```python
 /// from collections import namedtuple
 ///
-/// class Foo(namedtuple("foo", ["name", "age"]):
+/// class Foo(namedtuple("foo", ["name", "age"])):
 ///     pass
 /// ```
 ///
@@ -30,7 +30,7 @@ use crate::rules::flake8_slots::rules::helpers::has_slots;
 /// ```python
 /// from collections import namedtuple
 ///
-/// class Foo(namedtuple("foo", ["name", "age"]):
+/// class Foo(namedtuple("foo", ["name", "age"])):
 ///     __slots__ = ()
 /// ```
 /// ## References
