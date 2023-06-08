0766f5f85d0dd47d4f4d126cf924e5fd45ea1b6a
konstin <konstin@mailbox.org>
2023-06-08 15:20:02 +0200

    Implement StmtPass
    This implements StmtPass as `pass`.
    
    The snapshot diff is small because pass mainly occurs in bodies and function (#4951) and if/for bodies.

--- crates/ruff_python_formatter/src/statement/stmt_pass.rs
+++ crates/ruff_python_formatter/src/statement/stmt_pass.rs
@@ -1,12 +1,13 @@
-use crate::{not_yet_implemented, FormatNodeRule, PyFormatter};
-use ruff_formatter::{write, Buffer, FormatResult};
+use crate::{FormatNodeRule, PyFormatter};
+use ruff_formatter::prelude::text;
+use ruff_formatter::{Format, FormatResult};
 use rustpython_parser::ast::StmtPass;
 
 #[derive(Default)]
 pub struct FormatStmtPass;
 
 impl FormatNodeRule<StmtPass> for FormatStmtPass {
-    fn fmt_fields(&self, item: &StmtPass, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(f, [not_yet_implemented(item)])
+    fn fmt_fields(&self, _item: &StmtPass, f: &mut PyFormatter) -> FormatResult<()> {
+        text("pass").fmt(f)
     }
 }
