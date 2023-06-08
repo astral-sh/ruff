1c8655efce6d876f13165a10f53c894c4005d0f5
qdegraaf <querijndegraaf@gmail.com>
2023-06-08 11:54:59 +0200

    Add extra test cases

--- crates/ruff/resources/test/fixtures/perflint/W8102.py
+++ crates/ruff/resources/test/fixtures/perflint/W8102.py
@@ -16,6 +16,30 @@ for weird_arg_name, _ in some_dict.items():  # W8102
     print(weird_arg_name)
 
 
+for name, (_, _) in some_dict.items():  # W8102
+    pass
+
+
+for name, (value1, _) in some_dict.items():  # OK
+    pass
+
+
+for ((key1, _), (_, _)) in some_dict.items():  # W8102
+    pass
+
+
+for ((_, _), (value, _)) in some_dict.items():  # W8102
+    pass
+
+
+for ((_, key2), (value1, _)) in some_dict.items():  # OK
+    pass
+
+
+for ((_, _, _, variants), (r_language, _, _, _)) in some_dict.items():  # OK
+    pass
+
+
 for key, value in some_dict.items():  # OK
     print(key, value)
 
