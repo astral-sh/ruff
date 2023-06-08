d4a22baf92bb7fede493aa89117b32bea8d029a3
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-06-07 22:06:50 -0400

    Tweak test suite

--- crates/ruff/resources/test/fixtures/flake8_bugbear/B905.py
+++ crates/ruff/resources/test/fixtures/flake8_bugbear/B905.py
@@ -1,5 +1,6 @@
 from itertools import count, cycle, repeat
 
+# Errors
 zip()
 zip(range(3))
 zip("a", "b")
@@ -7,17 +8,18 @@ zip("a", "b", *zip("c"))
 zip(zip("a"), strict=False)
 zip(zip("a", strict=True))
 
+# OK
 zip(range(3), strict=True)
 zip("a", "b", strict=False)
 zip("a", "b", "c", strict=True)
 
-# Ignore infinite iterators.
+# OK (infinite iterators).
 zip([1, 2, 3], cycle("ABCDEF"))
 zip([1, 2, 3], count())
 zip([1, 2, 3], repeat(1))
 zip([1, 2, 3], repeat(1, None))
 zip([1, 2, 3], repeat(1, times=None))
 
-# Unless a limit is specified.
+# Errors (limited iterators).
 zip([1, 2, 3], repeat(1, 1))
 zip([1, 2, 3], repeat(1, times=4))
