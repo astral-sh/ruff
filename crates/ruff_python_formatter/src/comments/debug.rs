68d52da43b73a570b50c37c5b324eaabdc8d8c69
Micha Reiser <micha@reiser.io>
2023-06-09 11:09:45 +0200

    Track formatted comments (#4979)

--- crates/ruff_python_formatter/src/comments/debug.rs
+++ crates/ruff_python_formatter/src/comments/debug.rs
@@ -206,32 +206,26 @@ break;
 
         comments_map.push_leading(
             continue_statement.as_ref().into(),
-            SourceComment {
-                slice: source_code.slice(TextRange::at(TextSize::new(0), TextSize::new(17))),
-                #[cfg(debug_assertions)]
-                formatted: std::cell::Cell::new(false),
-                position: CommentTextPosition::OwnLine,
-            },
+            SourceComment::new(
+                source_code.slice(TextRange::at(TextSize::new(0), TextSize::new(17))),
+                CommentTextPosition::OwnLine,
+            ),
         );
 
         comments_map.push_trailing(
             continue_statement.as_ref().into(),
-            SourceComment {
-                slice: source_code.slice(TextRange::at(TextSize::new(28), TextSize::new(10))),
-                #[cfg(debug_assertions)]
-                formatted: std::cell::Cell::new(false),
-                position: CommentTextPosition::EndOfLine,
-            },
+            SourceComment::new(
+                source_code.slice(TextRange::at(TextSize::new(28), TextSize::new(10))),
+                CommentTextPosition::EndOfLine,
+            ),
         );
 
         comments_map.push_leading(
             break_statement.as_ref().into(),
-            SourceComment {
-                slice: source_code.slice(TextRange::at(TextSize::new(39), TextSize::new(15))),
-                #[cfg(debug_assertions)]
-                formatted: std::cell::Cell::new(false),
-                position: CommentTextPosition::OwnLine,
-            },
+            SourceComment::new(
+                source_code.slice(TextRange::at(TextSize::new(39), TextSize::new(15))),
+                CommentTextPosition::OwnLine,
+            ),
         );
 
         let comments = Comments::new(comments_map);
