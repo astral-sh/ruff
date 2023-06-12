fc22481bd27344dcec85ae09c2a09cf1bdbc2659
mayrholu <34736939+mayrholu@users.noreply.github.com>
2023-06-12 13:18:06 +0200

    Update TD002 tests

--- crates/ruff/resources/test/fixtures/flake8_todos/TD002.py
+++ crates/ruff/resources/test/fixtures/flake8_todos/TD002.py
@@ -1,8 +1,12 @@
 # T002 - accepted
 # TODO (evanrittenhouse): this has an author
-# TODO(evanrittenhouse): this also has an author
-# TODO@someauthor: this has an author
-# TODO @someauthor: this also has an author
+# TODO(evanrittenhouse): this has an author
+# TODO (evanrittenhouse) and more: this has an author
+# TODO(evanrittenhouse) and more: this has an author
+# TODO@mayrholu: this has an author
+# TODO @mayrholu: this has an author
+# TODO@mayrholu and more: this has an author
+# TODO @mayrholu and more: this has an author
 # T002 - errors
 # TODO: this has no author
 # FIXME: neither does this
