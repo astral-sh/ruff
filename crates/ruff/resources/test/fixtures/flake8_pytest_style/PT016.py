cbd4c10fddd33d3afb735d7aed1b77d8ece2ed88
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-06-12 20:54:07 -0400

    Support 'reason' argument to `pytest.fail` (#5040)
    ## Summary
    
    Per the [API
    reference](https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest.fail),
    `reason` was added in version 7, and is equivalent to `msg` (but
    preferred going forward).
    
    I also grepped for `msg` usages in `flake8_pytest_style`, but found no
    others (apart from those that reference `unittest` APIs.)
    
    Closes #3387.

--- crates/ruff/resources/test/fixtures/flake8_pytest_style/PT016.py
+++ crates/ruff/resources/test/fixtures/flake8_pytest_style/PT016.py
@@ -1,17 +1,25 @@
 import pytest
 
 
-def test_xxx():
-    pytest.fail("this is a failure")  # Test OK arg
+# OK
+def f():
+    pytest.fail("this is a failure")
 
 
-def test_xxx():
-    pytest.fail(msg="this is a failure")  # Test OK kwarg
+def f():
+    pytest.fail(msg="this is a failure")
 
 
-def test_xxx():  # Error
+def f():
+    pytest.fail(reason="this is a failure")
+
+
+# Errors
+def f():
     pytest.fail()
     pytest.fail("")
     pytest.fail(f"")
     pytest.fail(msg="")
     pytest.fail(msg=f"")
+    pytest.fail(reason="")
+    pytest.fail(reason=f"")
