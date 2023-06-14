f55e28e5531e61de2533611404716000ec71f248
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-06-14 14:19:55 -0400

    Improve names and documentation on scope API

--- crates/ruff/src/checkers/ast/mod.rs
+++ crates/ruff/src/checkers/ast/mod.rs
@@ -4815,7 +4815,7 @@ impl<'a> Checker<'a> {
         let exports: Vec<(&str, TextRange)> = {
             self.semantic_model
                 .global_scope()
-                .bindings_for_name("__all__")
+                .get_all("__all__")
                 .map(|binding_id| &self.semantic_model.bindings[binding_id])
                 .filter_map(|binding| match &binding.kind {
                     BindingKind::Export(Export { names }) => {
@@ -5076,7 +5076,7 @@ impl<'a> Checker<'a> {
         let exports: Option<Vec<&str>> = {
             let global_scope = self.semantic_model.global_scope();
             global_scope
-                .bindings_for_name("__all__")
+                .get_all("__all__")
                 .map(|binding_id| &self.semantic_model.bindings[binding_id])
                 .filter_map(|binding| match &binding.kind {
                     BindingKind::Export(Export { names }) => Some(names.iter().copied()),
