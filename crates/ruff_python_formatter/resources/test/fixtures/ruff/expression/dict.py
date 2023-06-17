b4a4f03c485c846ff87267cb60cf97aea9e791a0
David Szotten <davidszotten@gmail.com>
2023-06-17 21:06:46 +0100

    basic formatting for ExprDict

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/expression/dict.py
@@ -0,0 +1,26 @@
+# before
+{  # open
+ key# key
+ : # colon
+ value# value
+} # close
+# after
+
+
+{**d}
+
+{}
+
+{1:2,}
+
+{1:2,
+ 3:4,}
+
+{asdfsadfalsdkjfhalsdkjfhalskdjfhlaksjdfhlaskjdfhlaskjdfhlaksdjfh: 1, adsfadsflasdflasdfasdfasdasdf: 2}
+
+mapping = {
+    A: 0.25 * (10.0 / 12),
+    B: 0.1 * (10.0 / 12),
+    C: 0.1 * (10.0 / 12),
+    D: 0.1 * (10.0 / 12),
+}
