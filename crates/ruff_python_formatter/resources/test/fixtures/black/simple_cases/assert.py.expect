7739dd2bbc61c9e3fd7ffa8e3c6b9dd11719b677
Chris Pryer <cnpryer@gmail.com>
2023-06-18 11:07:25 -0400

    Move new assert fixture into ruff fixtures

--- crates/ruff_python_formatter/resources/test/fixtures/black/simple_cases/assert.py.expect
+++
@@ -1,6 +0,0 @@
-assert (
-    # Some comment
-    True  # Some comment
-    # Some comment
-), "Some string"  # Some comment
-# Some comment
---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/assert.py
@@ -0,0 +1,23 @@
+assert (
+    # Asserted
+)
+
+# Some assert
+assert (
+    # Let's go inside
+    True  # Go around this
+    # Below that    
+), "Some string"  # Outside this way
+# Passed all that
+
+# Dangle this here
+
+# Some assert
+assert (
+    # Let's go inside
+    True  # Go around this
+    # Below that
+
+    # Dangle that there
+), "Some string"  # Outside this way
+# Passed all that
