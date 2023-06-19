6a0cd2eb74b4cea15cb3f87d60e3cf9afdfa1b6a
Tom Kuson <mail@tjkuson.me>
2023-06-19 20:38:22 +0100

    Tweak

--- crates/ruff/src/rules/pylint/rules/repeated_equality_comparison_target.rs
+++ crates/ruff/src/rules/pylint/rules/repeated_equality_comparison_target.rs
@@ -1,24 +1,33 @@
 use rustpython_parser::ast::{Boolop, Expr, ExprBoolOp, ExprCompare};
 
-use ruff_diagnostics::{Violation};
+use ruff_diagnostics::Violation;
 use ruff_macros::{derive_message_formats, violation};
 
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for operations that compare a name to itself.
+/// Checks for equality comparisons that can rewritten as a membership test.
 ///
 /// ## Why is this bad?
-/// Comparing a name to itself always results in the same value, and is likely
-/// a mistake.
+/// Instead, to check if a variable is equal to one of many values, combine the
+/// values into a collection and use the `in` operator. This is faster and less
+/// verbose. If the items are hashable, use a `set` instead of a `list`, as
+/// membership tests are faster for sets.
 ///
 /// ## Example
 /// ```python
-/// foo == foo
+/// foo == "bar" or foo == "baz" or foo == "qux"
+/// ```
+///
+/// Use instead:
+/// ```python
+/// foo in {"bar", "baz", "qux"}
 /// ```
 ///
 /// ## References
 /// - [Python documentation: Comparisons](https://docs.python.org/3/reference/expressions.html#comparisons)
+/// - [Python documentation: Membership test operations](https://docs.python.org/3/reference/expressions.html#membership-test-operations)
+/// - [Python documentation: `set`](https://docs.python.org/3/library/stdtypes.html#set)
 #[violation]
 pub struct RepeatedEqualityComparisonTarget;
 
@@ -26,12 +35,12 @@ impl Violation for RepeatedEqualityComparisonTarget {
     #[derive_message_formats]
     fn message(&self) -> String {
         format!(
+            // TODO: Improve the message.
             "Consider merging multiple comparisons with ???. \
             Use a `set` if the elements are hashable."
         )
     }
 }
-
 
 /// PLR0124
 pub(crate) fn repeated_equality_comparison_target(
@@ -39,62 +48,16 @@ pub(crate) fn repeated_equality_comparison_target(
     op: Boolop,
     values: &[Expr],
 ) {
-    // Ignore if the operators are not `or`.
+    // Ignore if not all `or`, these cannot be rewritten as an `in` expression.
     if op != Boolop::Or {
         return;
     }
     for value in values {
-         if let Expr::BoolOp(ExprBoolOp { op, .. }) = value {
-             if *op != Boolop::Or {
-                 return;
-             }
-         }
-    }
-
-    println!("Found an expression linked by `or`");
-
-    // Iterate though `values` ExprCompare type elements.
-    // If the left value is the same in every element of `values`, then
-    // we have a repeated comparison target.
-    let mut prev_names = Vec::new();
-    let mut prev_consts = Vec::new();
-    // For each item in `values`, check if prev_names or prev_const exist in `left` or `comparator`
-    // elements. If there is any element in prev_names or prev_consts that does not exst in the
-    // current value being checked, remove it from prev_names or prev_consts. If the last element
-    // in prev_names or prev_consts is removed, return. If prev_names or prev_consts is empty,
-    // that means it is the start of the loop, so add all the elements in `left` and `comparator`
-    // to prev_names and prev_consts.
-    for value in values {
-        let current_left = match value {
-            Expr::Compare(ExprCompare { left, .. }) => left,
-            _ => return,
-        };
-        let current_comparators = match value {
-            Expr::Compare(ExprCompare { comparators, .. }) => comparators,
-            _ => return,
-        };
-        // If prev_names and prev_consts are empty, add all the elements in `left` and `comparator`
-        // to prev_names and prev_consts. Then continue to the next value in `values`.
-        if prev_names.is_empty() && prev_consts.is_empty() {
-            if let Expr::Name(name) = current_left {
-                prev_names.push(name);
-            } else if let Expr::Constant(constant) = current_left {
-                prev_consts.push(constant);
+        if let Expr::BoolOp(ExprBoolOp { op, .. }) = value {
+            if *op != Boolop::Or {
+                return;
             }
-            for comparator in current_comparators {
-                if let Expr::Name(name) = comparator {
-                    prev_names.push(name);
-                } else if let Expr::Constant(constant) = comparator {
-                    prev_consts.push(constant);
-                }
-            }
-            // Print the contents of prev_names and prev_consts for debugging.
-            println!("prev_names: {:?}", prev_names);
-            println!("prev_consts: {:?}", prev_consts);
-            continue;
         }
     }
-
-    println!("Found a repeated comparison target");
-
+    // TODO: Check if the expression can be rewritten as a membership test.
 }
