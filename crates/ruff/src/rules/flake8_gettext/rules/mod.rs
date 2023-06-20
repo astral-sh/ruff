4cc3cdba16846955a1b7a9f4abf7f83fddd26359
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-06-19 23:21:08 -0400

    Use some more wildcard imports in rules (#5201)

--- crates/ruff/src/rules/flake8_gettext/rules/mod.rs
+++ crates/ruff/src/rules/flake8_gettext/rules/mod.rs
@@ -1,6 +1,6 @@
 pub(crate) use f_string_in_gettext_func_call::*;
 pub(crate) use format_in_gettext_func_call::*;
-pub(crate) use is_gettext_func_call::is_gettext_func_call;
+pub(crate) use is_gettext_func_call::*;
 pub(crate) use printf_in_gettext_func_call::*;
 
 mod f_string_in_gettext_func_call;
