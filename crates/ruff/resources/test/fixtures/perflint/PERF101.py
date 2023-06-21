a2c5b06a719dd275aeba08d61222e5455ab7e9d8
qdegraaf <querijndegraaf@gmail.com>
2023-06-21 17:18:13 +0200

    Add few more testcases review snapshot

--- crates/ruff/resources/test/fixtures/perflint/PERF101.py
+++ crates/ruff/resources/test/fixtures/perflint/PERF101.py
@@ -1,10 +1,8 @@
-from typing import List, Dict, Any
-
 foo_tuple = (1, 2, 3)
 foo_list = [1, 2, 3]
 foo_set = {1, 2, 3}
 foo_dict = {1: 2, 3: 4}
-
+foo_int = 123
 
 for i in list(foo_tuple):  # PERF101
     pass
@@ -26,16 +24,12 @@ for i in list({1, 2, 3}):  # PERF101
     pass
 
 
-def foo(items: List[int]):
-    for item in list(items):  # PERF101
-        pass
-
-
-def bar(items: Dict[str, Any]):
-    for item in list(items):  # Ok
-        pass
-
-
 for i in list(foo_dict):  # Ok
     pass
 
+
+for i in list(1):  # Ok
+    pass
+
+for i in list(foo_int):  # Ok
+    pass
