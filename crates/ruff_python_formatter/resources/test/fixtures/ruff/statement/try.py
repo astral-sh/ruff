5e010f4abd7f3494c691b2f05d70920356628ab7
David Szotten <davidszotten@gmail.com>
2023-06-21 16:52:07 +0100

    snapshot test

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/try.py
@@ -0,0 +1,46 @@
+try:
+    ...
+except:
+    ...
+
+
+try: # try
+    ...
+    # end of body
+# before except
+except (Exception, ValueError) as exc:  # except line
+    ...
+# before except 2
+except (KeyError) as key:  # except line 2
+    ...
+    # in body 2
+# before else
+else:
+    ...
+# before finally
+finally:
+    ...
+
+
+
+# with line breaks
+try: # try
+    ...
+    # end of body
+
+# before except
+except (Exception, ValueError) as exc:  # except line
+    ...
+
+# before except 2
+except (KeyError) as key:  # except line 2
+    ...
+    # in body 2
+
+# before else
+else:
+    ...
+
+# before finally
+finally:
+    ...
