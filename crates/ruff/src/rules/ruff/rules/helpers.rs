11ed64df533f6d358b3b5b95b92d46fb9ffdb40e
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-06-21 19:50:48 -0400

    Allow typing.Final for mutable-class-default annotations (RUF012)

--- crates/ruff/src/rules/ruff/rules/helpers.rs
+++ crates/ruff/src/rules/ruff/rules/helpers.rs
@@ -18,6 +18,14 @@ pub(super) fn is_class_var_annotation(annotation: &Expr, semantic: &SemanticMode
     semantic.match_typing_expr(value, "ClassVar")
 }
 
+/// Returns `true` if the given [`Expr`] is a `typing.Final` annotation.
+pub(super) fn is_final_annotation(annotation: &Expr, semantic: &SemanticModel) -> bool {
+    let Expr::Subscript(ast::ExprSubscript { value, .. }) = &annotation else {
+        return false;
+    };
+    semantic.match_typing_expr(value, "Final")
+}
+
 /// Returns `true` if the given class is a dataclass.
 pub(super) fn is_dataclass(class_def: &ast::StmtClassDef, semantic: &SemanticModel) -> bool {
     class_def.decorator_list.iter().any(|decorator| {
