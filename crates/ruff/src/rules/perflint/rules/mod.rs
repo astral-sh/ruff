a3fef72976413e0745bc25b99b75ce40bd251c3d
qdegraaf <querijndegraaf@gmail.com>
2023-06-22 23:39:22 +0200

    Fix mod.rs

--- crates/ruff/src/rules/perflint/rules/mod.rs
+++ crates/ruff/src/rules/perflint/rules/mod.rs
@@ -3,7 +3,9 @@ pub(crate) use slow_filtered_list_creation::{
     slow_filtered_list_creation, SlowFilteredListCreation,
 };
 pub(crate) use slow_list_copy::{slow_list_copy, SlowListCopy};
+pub(crate) use unnecessary_list_cast::{unnecessary_list_cast, UnnecessaryListCast};
 
 mod incorrect_dict_iterator;
 mod slow_filtered_list_creation;
 mod slow_list_copy;
+mod unnecessary_list_cast;
