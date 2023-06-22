c51e57a2c6206c29d47d4f8ea0724d8c0360e59e
qdegraaf <querijndegraaf@gmail.com>
2023-06-22 21:23:30 +0200

    Add PERF403

--- crates/ruff/src/rules/perflint/rules/mod.rs
+++ crates/ruff/src/rules/perflint/rules/mod.rs
@@ -3,9 +3,11 @@ pub(crate) use manual_list_comprehension::*;
 pub(crate) use manual_list_copy::*;
 pub(crate) use try_except_in_loop::*;
 pub(crate) use unnecessary_list_cast::*;
+pub(crate) use slow_dict_creation::{slow_dict_creation, SlowDictCreation};
 
 mod incorrect_dict_iterator;
 mod manual_list_comprehension;
 mod manual_list_copy;
 mod try_except_in_loop;
 mod unnecessary_list_cast;
+mod slow_dict_creation;
