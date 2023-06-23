6f694a948743fc77b1dd50d0a56daa6ba4a3e2ca
konstin <konstin@mailbox.org>
2023-06-23 11:21:45 +0200

    Fix attribute chain own line comments

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/expression/attribute.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/expression/attribute.py
@@ -27,3 +27,63 @@
 
 
 aaaaaaaaaaaaaaaaaaaaa.lllllllllllllllllllllllllllloooooooooong.chaaaaaaaaaaaaaaaaaaaaaaiiiiiiiiiiiiiiiiiiiiiiinnnnnnnn.ooooooooooooooooooooooooofffffffff.aaaaaaaaaattr
+
+
+# Test that we add parentheses around the outermost attribute access in an attribute
+# chain if and only if we need them, that is if there are own line comments inside
+# the chain.
+x1 = (
+    a1
+    .a2
+    # a
+    .  # b
+    # c
+    a3
+    .a4
+)
+x20 = (
+    a
+    .b
+)
+x21 = (
+    a
+    # trailing name own line
+    .b
+)
+x22 = (
+    ""
+    # leading
+    .b
+)
+x23 = (
+    # leading
+    a
+    .b
+)
+x24 = (
+    a # trailing name end-of-line
+    .b
+)
+x31 = (
+    a
+    .# a
+    b
+)
+x32 = (
+    a
+    .# a
+    b
+    .c
+)
+x4 = (
+    a.
+    # a
+    b
+)
+x5 = (
+    a.b.c
+)
+x61 = askjdfahdlskjflsajfadhsaf.akjdsf.aksjdlfadhaljsashdfljaf.askjdflhasfdlashdlfaskjfd.asdkjfksahdfkjafs
+x62 = (
+    askjdfahdlskjflsajfadhsaf.akjdsf.aksjdlfadhaljsashdfljaf.askjdflhasfdlashdlfaskjfd.asdkjfksahdfkjafs
+)
