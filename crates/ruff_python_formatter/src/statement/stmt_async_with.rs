d8faa6e1a7459f3cf70b9dae2488cbf578f0e9d0
David Szotten <davidszotten@gmail.com>
2023-06-26 17:31:29 +0100

    format StmtAsyncWith
    refactor `FormatStmtWith` and share impl with `FormatStmtAsyncWith`

--- crates/ruff_python_formatter/src/statement/stmt_async_with.rs
+++ crates/ruff_python_formatter/src/statement/stmt_async_with.rs
@@ -1,12 +1,32 @@
-use crate::{not_yet_implemented, FormatNodeRule, PyFormatter};
-use ruff_formatter::{write, Buffer, FormatResult};
-use rustpython_parser::ast::StmtAsyncWith;
+use rustpython_parser::ast::{Stmt, StmtAsyncWith, WithItem};
+
+use ruff_formatter::FormatResult;
+
+use super::stmt_with::FormatWithLike;
+use crate::{FormatNodeRule, PyFormatter};
 
 #[derive(Default)]
 pub struct FormatStmtAsyncWith;
 
+impl<'ast> FormatWithLike<'ast> for StmtAsyncWith {
+    const ASYNC: bool = true;
+
+    fn destruct(&self) -> (&Vec<WithItem>, &Vec<Stmt>) {
+        (&self.items, &self.body)
+    }
+}
+
 impl FormatNodeRule<StmtAsyncWith> for FormatStmtAsyncWith {
     fn fmt_fields(&self, item: &StmtAsyncWith, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(f, [not_yet_implemented(item)])
+        item.fmt_with(f)
+    }
+
+    fn fmt_dangling_comments(
+        &self,
+        _node: &StmtAsyncWith,
+        _f: &mut PyFormatter,
+    ) -> FormatResult<()> {
+        // Handled in `fmt_fields`
+        Ok(())
     }
 }
--- crates/ruff_python_formatter/src/statement/stmt_with.rs
+++ crates/ruff_python_formatter/src/statement/stmt_with.rs
@@ -1,29 +1,30 @@
-use rustpython_parser::ast::StmtWith;
-
 use ruff_formatter::{write, Buffer, FormatResult};
-use ruff_python_ast::node::AstNode;
+use rustpython_parser::ast::{StmtWith, WithItem};
 
 use crate::builders::optional_parentheses;
 use crate::comments::trailing_comments;
 use crate::prelude::*;
+use crate::statement::Stmt;
 use crate::{FormatNodeRule, PyFormatter};
 
-#[derive(Default)]
-pub struct FormatStmtWith;
+pub(super) trait FormatWithLike<'ast>: ruff_python_ast::node::AstNode {
+    // whether this is `with` or `async with`
+    const ASYNC: bool;
 
-impl FormatNodeRule<StmtWith> for FormatStmtWith {
-    fn fmt_fields(&self, item: &StmtWith, f: &mut PyFormatter) -> FormatResult<()> {
-        let StmtWith {
-            range: _,
-            items,
-            body,
-            type_comment: _,
-        } = item;
+    // extract the items and the body
+    fn destruct(&self) -> (&Vec<WithItem>, &Vec<Stmt>);
+
+    fn fmt_with(&self, f: &mut PyFormatter) -> FormatResult<()> {
+        let (items, body) = self.destruct();
 
         let comments = f.context().comments().clone();
-        let dangling_comments = comments.dangling_comments(item.as_any_node_ref());
+        let dangling_comments = comments.dangling_comments(self.as_any_node_ref());
 
         let joined_items = format_with(|f| f.join_comma_separated().nodes(items.iter()).finish());
+
+        if Self::ASYNC {
+            write!(f, [text("async"), space()])?;
+        }
 
         write!(
             f,
@@ -36,6 +37,23 @@ impl FormatNodeRule<StmtWith> for FormatStmtWith {
                 block_indent(&body.format())
             ]
         )
+    }
+}
+
+#[derive(Default)]
+pub struct FormatStmtWith;
+
+impl<'ast> FormatWithLike<'ast> for StmtWith {
+    const ASYNC: bool = false;
+
+    fn destruct(&self) -> (&Vec<WithItem>, &Vec<Stmt>) {
+        (&self.items, &self.body)
+    }
+}
+
+impl FormatNodeRule<StmtWith> for FormatStmtWith {
+    fn fmt_fields(&self, item: &StmtWith, f: &mut PyFormatter) -> FormatResult<()> {
+        item.fmt_with(f)
     }
 
     fn fmt_dangling_comments(&self, _node: &StmtWith, _f: &mut PyFormatter) -> FormatResult<()> {
