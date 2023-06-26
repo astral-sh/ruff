8b52a7810ff666d6480ce55e1b2342be95800b3b
jgberry <jgbcode@gmail.com>
2023-06-26 04:14:47 -0400

    Added custom graph implementation

--- crates/ruff/src/rules/ssort/graph.rs
+++ crates/ruff/src/rules/ssort/graph.rs
@@ -1,51 +1,77 @@
-use itertools::Itertools;
-use petgraph::algo::tarjan_scc;
-use petgraph::graph::EdgeReference;
-use petgraph::visit::EdgeRef;
-use petgraph::Graph;
-use std::collections::HashSet;
+use indexmap::{IndexMap, IndexSet};
+use std::collections::hash_map::Keys;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 
-pub(super) fn break_cycles<'a, N, E: 'a, F, K>(graph: &'a mut Graph<N, E>, edge_cost: F)
+#[derive(Debug)]
+pub(super) struct Graph<N, E> {
+    outgoing_edges: HashMap<N, HashMap<N, E>>,
+    incoming_edges: HashMap<N, HashMap<N, E>>,
+}
+
+impl<N, E> Graph<N, E>
 where
-    F: Fn(&EdgeReference<E>) -> K,
-    K: Ord,
+    N: Clone + Eq + Hash + PartialEq,
+    E: Clone,
 {
-    loop {
-        let mut found_cycle = false;
-        for strongly_connected_component in tarjan_scc(&*graph) {
-            if strongly_connected_component.len() < 2 {
-                continue;
-            }
+    fn new() -> Self {
+        Graph {
+            outgoing_edges: HashMap::new(),
+            incoming_edges: HashMap::new(),
+        }
+    }
 
-            found_cycle = true;
+    fn insert_node(&mut self, node: N) {
+        self.outgoing_edges.entry(node.clone()).or_default();
+        self.incoming_edges.entry(node).or_default();
+    }
 
-            let nodes: HashSet<_> = strongly_connected_component.iter().copied().collect();
+    fn remove_node(&mut self, node: &N) {
+        let outgoing_edges = self.outgoing_edges.remove(node).unwrap_or_default();
+        let incoming_edges = self.incoming_edges.remove(node).unwrap_or_default();
 
-            let edges_to_remove = strongly_connected_component
-                .into_iter()
-                .flat_map(|node| {
-                    graph
-                        .edges(node)
-                        .filter(|edge| nodes.contains(&edge.target()))
-                        .group_by(|edge| edge.target())
-                        .into_iter()
-                        .map(|(_, edges)| edges.collect::<Vec<_>>())
-                        .collect::<Vec<_>>()
-                })
-                .max_by_key(|edges| edges.iter().map(&edge_cost).min().unwrap())
-                .unwrap()
-                .into_iter()
-                .map(|edge| edge.id())
-                .collect::<Vec<_>>();
-
-            for edge in edges_to_remove {
-                graph.remove_edge(edge);
-            }
+        for outgoing_node in outgoing_edges.keys() {
+            self.incoming_edges
+                .get_mut(outgoing_node)
+                .map(|edges| edges.remove(node));
         }
 
-        if !found_cycle {
-            break;
+        for incoming_node in incoming_edges.keys() {
+            self.outgoing_edges
+                .get_mut(incoming_node)
+                .map(|edges| edges.remove(node));
         }
+    }
+
+    fn contains_node(&self, node: &N) -> bool {
+        self.outgoing_edges.contains_key(node) || self.incoming_edges.contains_key(node)
+    }
+
+    fn insert_edge(&mut self, source: N, target: N, edge: E) {
+        self.outgoing_edges
+            .entry(source.clone())
+            .or_default()
+            .insert(target.clone(), edge.clone());
+        self.incoming_edges
+            .entry(target)
+            .or_default()
+            .insert(source, edge);
+    }
+
+    fn remove_edge(&mut self, source: &N, target: &N) {
+        self.outgoing_edges
+            .get_mut(source)
+            .map(|edges| edges.remove(target));
+        self.incoming_edges
+            .get_mut(target)
+            .map(|edges| edges.remove(source));
+    }
+
+    fn edge(&self, source: &N, target: &N) -> Option<&E> {
+        self.outgoing_edges
+            .get(source)
+            .map(|edges| edges.get(target))
+            .flatten()
     }
 }
 
@@ -55,44 +81,46 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn test_break_cycles_with_two_nodes() {
+    fn test_graph_insert_node() {
         let mut graph = Graph::<&str, i32>::new();
-        let a = graph.add_node("a");
-        let b = graph.add_node("b");
-        graph.extend_with_edges(&[(a, b, 1), (b, a, 2)]);
-        assert!(graph.contains_edge(a, b));
-        assert!(graph.contains_edge(b, a));
-        break_cycles(&mut graph, |edge| *edge.weight());
-        assert!(graph.contains_edge(a, b));
-        assert!(!graph.contains_edge(b, a));
+        graph.insert_node("a");
+        assert!(graph.contains_node(&"a"));
     }
 
     #[test]
-    fn test_break_cycles_with_three_nodes() {
+    fn test_graph_insert_edge() {
         let mut graph = Graph::<&str, i32>::new();
-        let a = graph.add_node("a");
-        let b = graph.add_node("b");
-        let c = graph.add_node("c");
-        graph.extend_with_edges(&[
-            (a, b, 1),
-            (a, c, 2),
-            (b, a, 4),
-            (b, c, 5),
-            (c, a, 6),
-            (c, b, 7),
-        ]);
-        assert!(graph.contains_edge(a, b));
-        assert!(graph.contains_edge(a, c));
-        assert!(graph.contains_edge(b, a));
-        assert!(graph.contains_edge(b, c));
-        assert!(graph.contains_edge(c, a));
-        assert!(graph.contains_edge(c, b));
-        break_cycles(&mut graph, |edge| *edge.weight());
-        assert!(graph.contains_edge(a, b));
-        assert!(graph.contains_edge(a, c));
-        assert!(!graph.contains_edge(b, a));
-        assert!(graph.contains_edge(b, c));
-        assert!(!graph.contains_edge(c, a));
-        assert!(!graph.contains_edge(c, b));
+        graph.insert_edge("a", "b", 1);
+        assert_eq!(graph.edge(&"a", &"b"), Some(&1));
+    }
+
+    #[test]
+    fn test_graph_remove_node() {
+        let mut graph = Graph::<&str, i32>::new();
+        graph.insert_edge("a", "b", 1);
+        graph.insert_edge("a", "c", 2);
+        graph.insert_edge("b", "c", 3);
+        graph.remove_node(&"b");
+        assert!(graph.contains_node(&"a"));
+        assert!(!graph.contains_node(&"b"));
+        assert!(graph.contains_node(&"c"));
+        assert_eq!(graph.edge(&"a", &"b"), None);
+        assert_eq!(graph.edge(&"a", &"c"), Some(&2));
+        assert_eq!(graph.edge(&"b", &"c"), None);
+    }
+
+    #[test]
+    fn test_graph_remove_edge() {
+        let mut graph = Graph::<&str, i32>::new();
+        graph.insert_edge("a", "b", 1);
+        graph.insert_edge("a", "c", 2);
+        graph.insert_edge("b", "c", 3);
+        graph.remove_edge(&"b", &"c");
+        assert!(graph.contains_node(&"a"));
+        assert!(graph.contains_node(&"b"));
+        assert!(graph.contains_node(&"c"));
+        assert_eq!(graph.edge(&"a", &"b"), Some(&1));
+        assert_eq!(graph.edge(&"a", &"c"), Some(&2));
+        assert_eq!(graph.edge(&"b", &"c"), None);
     }
 }
