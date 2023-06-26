190bed124f1a1753dc009749ffd7a7fba146718a
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-06-26 12:34:37 -0500

    [`perflint`] Implement `try-except-in-loop` (`PERF203`) (#5166)
    ## Summary
    
    Implements PERF203 from #4789, which throws if a `try/except` block is
    inside of a loop. Not sure if we want to extend the diagnostic to the
    `except` as well, but I thought that that may get a little messy. We may
    also want to just throw on the word `try` - open to suggestions though.
    
    ## Test Plan
    `cargo test`

--- crates/ruff/src/rules/perflint/rules/mod.rs
+++ crates/ruff/src/rules/perflint/rules/mod.rs
@@ -1,5 +1,7 @@
 pub(crate) use incorrect_dict_iterator::*;
+pub(crate) use try_except_in_loop::*;
 pub(crate) use unnecessary_list_cast::*;
 
 mod incorrect_dict_iterator;
+mod try_except_in_loop;
 mod unnecessary_list_cast;
