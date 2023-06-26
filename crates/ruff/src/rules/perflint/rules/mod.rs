9e13b805e3f35146af97173d3482383cb8a8483b
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-06-25 22:01:59 -0500

    Export

--- crates/ruff/src/rules/perflint/rules/mod.rs
+++ crates/ruff/src/rules/perflint/rules/mod.rs
@@ -1,5 +1,7 @@
 pub(crate) use incorrect_dict_iterator::*;
+pub(crate) use loop_try_except_usage::*;
 pub(crate) use unnecessary_list_cast::*;
 
 mod incorrect_dict_iterator;
+mod loop_try_except_usage;
 mod unnecessary_list_cast;
