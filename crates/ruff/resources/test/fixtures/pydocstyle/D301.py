astral-sh/ruff

commit 18c73c1f9bf97562f4a9b3afe6dfad482d11937f

Improve backslash-detection rule for docstrings (#5360)

crates/ruff/src/rules/pydocstyle/rules/backslashes.rs
@@ -1,5 +1,4 @@
-use once_cell::sync::Lazy;
-use regex::Regex;
+use memchr::memchr_iter;
 
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
@@ -53,18 +52,22 @@ impl Violation for EscapeSequenceInDocstring {
     }
 }
 
-static BACKSLASH_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"\\[^(\r\n|\n)uN]").unwrap());
-
 /// D301
 pub(crate) fn backslashes(checker: &mut Checker, docstring: &Docstring) {
-    let contents = docstring.contents;
+    let body = docstring.body();
 
     // Docstring is already raw.
-    if contents.starts_with('r') || contents.starts_with("ur") {
+    if body.starts_with('r') || body.starts_with("ur") {
         return;
     }
 
-    if BACKSLASH_REGEX.is_match(contents) {
+    // Docstring contains at least one backslash.
+    let bytes = body.as_bytes();
+    if memchr_iter(b'\\', bytes).any(|position| {
+        let escaped_char = bytes.get(position.saturating_add(1));
+        // Allow continuations (backslashes followed by newlines) and Unicode escapes.
+        !matches!(escaped_char, Some(b'\r' | b'\n' | b'u' | b'N'))
+    }) {
         checker.diagnostics.push(Diagnostic::new(
             EscapeSequenceInDocstring,
             docstring.range(),

crates/ruff/src/docstrings/mod.rs
@@ -18,7 +18,6 @@ pub(crate) struct Docstring<'a> {
     pub(crate) expr: &'a Expr,
     /// The content of the docstring, including the leading and trailing quotes.
     pub(crate) contents: &'a str,
-
     /// The range of the docstring body (without the quotes). The range is relative to [`Self::contents`].
     pub(crate) body_range: TextRange,
     pub(crate) indentation: &'a str,

crates/ruff/Cargo.toml
@@ -42,6 +42,7 @@ is-macro = { workspace = true }
 itertools = { workspace = true }
 libcst = { workspace = true }
 log = { workspace = true }
+memchr = { workspace = true }
 natord = { version = "1.0.9" }
 nohash-hasher = { workspace = true }
 num-bigint = { workspace = true }

crates/ruff/src/rules/pydocstyle/rules/non_imperative_mood.rs
@@ -17,6 +17,19 @@ use crate::rules::pydocstyle::helpers::normalize_word;
 
 static MOOD: Lazy<Mood> = Lazy::new(Mood::new);
 
+#[violation]
+pub struct NonImperativeMood {
+    first_line: String,
+}
+
+impl Violation for NonImperativeMood {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let NonImperativeMood { first_line } = self;
+        format!("First line of docstring should be in imperative mood: \"{first_line}\"")
+    }
+}
+
 /// D401
 pub(crate) fn non_imperative_mood(
     checker: &mut Checker,
@@ -52,31 +65,26 @@ pub(crate) fn non_imperative_mood(
     let body = docstring.body();
 
     // Find first line, disregarding whitespace.
-    let line = match body.trim().universal_newlines().next() {
+    let first_line = match body.trim().universal_newlines().next() {
         Some(line) => line.as_str().trim(),
         None => return,
     };
+
     // Find the first word on that line and normalize it to lower-case.
-    let first_word_norm = match line.split_whitespace().next() {
+    let first_word_norm = match first_line.split_whitespace().next() {
         Some(word) => normalize_word(word),
         None => return,
     };
     if first_word_norm.is_empty() {
         return;
     }
-    if let Some(false) = MOOD.is_imperative(&first_word_norm) {
-        let diagnostic = Diagnostic::new(NonImperativeMood(line.to_string()), docstring.range());
-        checker.diagnostics.push(diagnostic);
-    }
-}
 
-#[violation]
-pub struct NonImperativeMood(pub String);
-
-impl Violation for NonImperativeMood {
-    #[derive_message_formats]
-    fn message(&self) -> String {
-        let NonImperativeMood(first_line) = self;
-        format!("First line of docstring should be in imperative mood: \"{first_line}\"")
+    if matches!(MOOD.is_imperative(&first_word_norm), Some(false)) {
+        checker.diagnostics.push(Diagnostic::new(
+            NonImperativeMood {
+                first_line: first_line.to_string(),
+            },
+            docstring.range(),
+        ));
     }
 }

crates/ruff/src/rules/pydocstyle/rules/triple_quotes.rs
@@ -1,5 +1,6 @@
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast::source_code::Quote;
 
 use crate::checkers::ast::Checker;
 use crate::docstrings::Docstring;
@@ -30,32 +31,47 @@ use crate::docstrings::Docstring;
 /// - [NumPy Style Guide](https://numpydoc.readthedocs.io/en/latest/format.html)
 /// - [Google Python Style Guide - Docstrings](https://google.github.io/styleguide/pyguide.html#38-comments-and-docstrings)
 #[violation]
-pub struct TripleSingleQuotes;
+pub struct TripleSingleQuotes {
+    expected_quote: Quote,
+}
 
 impl Violation for TripleSingleQuotes {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!(r#"Use triple double quotes `"""`"#)
+        let TripleSingleQuotes { expected_quote } = self;
+        match expected_quote {
+            Quote::Double => format!(r#"Use triple double quotes `"""`"#),
+            Quote::Single => format!(r#"Use triple single quotes `'''`"#),
+        }
     }
 }
 
 /// D300
 pub(crate) fn triple_quotes(checker: &mut Checker, docstring: &Docstring) {
-    let body = docstring.body();
+    let leading_quote = docstring.leading_quote();
 
-    let leading_quote = docstring.leading_quote().to_ascii_lowercase();
-
-    let starts_with_triple = if body.contains("\"\"\"") {
-        matches!(leading_quote.as_str(), "'''" | "u'''" | "r'''" | "ur'''")
+    let expected_quote = if docstring.body().contains("\"\"\"") {
+        Quote::Single
     } else {
-        matches!(
-            leading_quote.as_str(),
-            "\"\"\"" | "u\"\"\"" | "r\"\"\"" | "ur\"\"\""
-        )
+        Quote::Double
     };
-    if !starts_with_triple {
-        checker
-            .diagnostics
-            .push(Diagnostic::new(TripleSingleQuotes, docstring.range()));
+
+    match expected_quote {
+        Quote::Single => {
+            if !leading_quote.ends_with("'''") {
+                checker.diagnostics.push(Diagnostic::new(
+                    TripleSingleQuotes { expected_quote },
+                    docstring.range(),
+                ));
+            }
+        }
+        Quote::Double => {
+            if !leading_quote.ends_with("\"\"\"") {
+                checker.diagnostics.push(Diagnostic::new(
+                    TripleSingleQuotes { expected_quote },
+                    docstring.range(),
+                ));
+            }
+        }
     }
 }

commit dce6a046b09570ab121e254ec6845b4fd1fe48ce

Add tests for escape-sequence-in-docstring (#5362)
## Summary

Looks like I added a regression in #5360. This PR fixes it and adds
dedicated tests to avoid it in the future.

crates/ruff/src/rules/pydocstyle/mod.rs
@@ -82,6 +82,7 @@ mod tests {
     #[test_case(Rule::SectionUnderlineNotOverIndented, Path::new("sections.py"))]
     #[test_case(Rule::OverloadWithDocstring, Path::new("D.py"))]
     #[test_case(Rule::EscapeSequenceInDocstring, Path::new("D.py"))]
+    #[test_case(Rule::EscapeSequenceInDocstring, Path::new("D301.py"))]
     #[test_case(Rule::TripleSingleQuotes, Path::new("D.py"))]
     fn rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!("{}_{}", rule_code.noqa_code(), path.to_string_lossy());

crates/ruff/src/rules/pydocstyle/rules/backslashes.rs
@@ -54,14 +54,14 @@ impl Violation for EscapeSequenceInDocstring {
 
 /// D301
 pub(crate) fn backslashes(checker: &mut Checker, docstring: &Docstring) {
-    let body = docstring.body();
-
     // Docstring is already raw.
-    if body.starts_with('r') || body.starts_with("ur") {
+    let contents = docstring.contents;
+    if contents.starts_with('r') || contents.starts_with("ur") {
         return;
     }
 
     // Docstring contains at least one backslash.
+    let body = docstring.body();
     let bytes = body.as_bytes();
     if memchr_iter(b'\\', bytes).any(|position| {
         let escaped_char = bytes.get(position.saturating_add(1));

crates/ruff/src/rules/pydocstyle/snapshots/ruff__rules__pydocstyle__tests__D301_D301.py.snap
@@ -1,0 +1,18 @@
+---
+source: crates/ruff/src/rules/pydocstyle/mod.rs
+---
+D301.py:2:5: D301 Use `r"""` if any backslashes in a docstring
+  |
+1 | def double_quotes_backslash():
+2 |     """Sum\\mary."""
+  |     ^^^^^^^^^^^^^^^^ D301
+  |
+
+D301.py:10:5: D301 Use `r"""` if any backslashes in a docstring
+   |
+ 9 | def double_quotes_backslash_uppercase():
+10 |     R"""Sum\\mary."""
+   |     ^^^^^^^^^^^^^^^^^ D301
+   |
+
+

crates/ruff/resources/test/fixtures/pydocstyle/D301.py
@@ -1,0 +1,29 @@
+def double_quotes_backslash():
+    """Sum\\mary."""
+
+
+def double_quotes_backslash_raw():
+    r"""Sum\mary."""
+
+
+def double_quotes_backslash_uppercase():
+    R"""Sum\\mary."""
+
+
+def make_unique_pod_id(pod_id: str) -> str | None:
+    r"""
+    Generate a unique Pod name.
+
+    Kubernetes pod names must consist of one or more lowercase
+    rfc1035/rfc1123 labels separated by '.' with a maximum length of 253
+    characters.
+
+    Name must pass the following regex for validation
+    ``^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$``
+
+    For more details, see:
+    https://github.com/kubernetes/kubernetes/blob/release-1.1/docs/design/identifiers.md
+
+    :param pod_id: requested pod name
+    :return: ``str`` valid Pod name of appropriate length
+    """