7eeb5297f25121b07a4c5c1d6122a0f592313136
jgberry <jgbcode@gmail.com>
2023-06-26 20:26:35 -0400

    Add find_cycle

--- crates/ruff/src/rules/ssort/graph.rs
+++ crates/ruff/src/rules/ssort/graph.rs
@@ -1,6 +1,6 @@
 use indexmap::{IndexMap, IndexSet};
 use std::collections::hash_map::Keys;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::hash::Hash;
 
 #[derive(Debug)]
@@ -9,23 +9,19 @@ pub(super) struct Graph<N, E> {
     incoming_edges: HashMap<N, HashMap<N, E>>,
 }
 
-impl<N, E> Graph<N, E>
-where
-    N: Clone + Eq + Hash + PartialEq,
-    E: Clone,
-{
+impl<N, E> Graph<N, E> {
     fn new() -> Self {
         Graph {
             outgoing_edges: HashMap::new(),
             incoming_edges: HashMap::new(),
         }
     }
+}
 
-    fn insert_node(&mut self, node: N) {
-        self.outgoing_edges.entry(node.clone()).or_default();
-        self.incoming_edges.entry(node).or_default();
-    }
-
+impl<N, E> Graph<N, E>
+where
+    N: Eq + Hash + PartialEq,
+{
     fn remove_node(&mut self, node: &N) {
         let outgoing_edges = self.outgoing_edges.remove(node).unwrap_or_default();
         let incoming_edges = self.incoming_edges.remove(node).unwrap_or_default();
@@ -44,18 +40,15 @@ where
     }
 
     fn contains_node(&self, node: &N) -> bool {
-        self.outgoing_edges.contains_key(node) || self.incoming_edges.contains_key(node)
+        self.outgoing_edges.contains_key(node)
     }
 
-    fn insert_edge(&mut self, source: N, target: N, edge: E) {
-        self.outgoing_edges
-            .entry(source.clone())
-            .or_default()
-            .insert(target.clone(), edge.clone());
-        self.incoming_edges
-            .entry(target)
-            .or_default()
-            .insert(source, edge);
+    fn nodes(&self) -> impl Iterator<Item = &N> {
+        self.outgoing_edges.keys()
+    }
+
+    fn neighbors(&self, node: &N) -> Option<impl Iterator<Item = &N>> {
+        self.outgoing_edges.get(node).map(|edges| edges.keys())
     }
 
     fn remove_edge(&mut self, source: &N, target: &N) {
@@ -73,6 +66,95 @@ where
             .map(|edges| edges.get(target))
             .flatten()
     }
+}
+
+impl<N, E> Graph<N, E>
+where
+    N: Clone + Eq + Hash + PartialEq,
+    E: Clone,
+{
+    fn insert_node(&mut self, node: N) {
+        self.outgoing_edges.entry(node.clone()).or_default();
+        self.incoming_edges.entry(node).or_default();
+    }
+
+    fn insert_edge(&mut self, source: N, target: N, edge: E) {
+        self.outgoing_edges
+            .entry(source.clone())
+            .or_default()
+            .insert(target.clone(), edge.clone());
+        self.incoming_edges
+            .entry(target.clone())
+            .or_default()
+            .insert(source, edge);
+
+        // outgoing_edges is used to track all nodes, so make sure the target exists there.
+        self.outgoing_edges.entry(target).or_default();
+    }
+}
+
+fn find_cycle<N, E>(graph: &Graph<N, E>) -> Option<Vec<&N>>
+where
+    N: Eq + Hash + PartialEq,
+{
+    let mut visited = HashSet::new();
+    find_cycle_with_visited(graph, &mut visited)
+}
+
+fn find_cycle_with_visited<'a, N, E>(
+    graph: &'a Graph<N, E>,
+    visited: &mut HashSet<&'a N>,
+) -> Option<Vec<&'a N>>
+where
+    N: Eq + Hash + PartialEq,
+{
+    for node in graph.nodes() {
+        if visited.contains(node) {
+            continue;
+        }
+
+        let mut path: IndexSet<&'a N> = IndexSet::new();
+
+        if let Some(path) = find_cycle_with_path(graph, visited, &mut path, node) {
+            return Some(path);
+        }
+    }
+
+    None
+}
+
+fn find_cycle_with_path<'a, N, E>(
+    graph: &'a Graph<N, E>,
+    visited: &mut HashSet<&'a N>,
+    path: &mut IndexSet<&'a N>,
+    node: &'a N,
+) -> Option<Vec<&'a N>>
+where
+    N: Eq + Hash + PartialEq,
+{
+    visited.insert(node);
+    path.insert(node);
+
+    for neighbor in graph.neighbors(node).unwrap() {
+        if !visited.contains(neighbor) {
+            if let Some(cycle) = find_cycle_with_path(graph, visited, path, neighbor) {
+                visited.remove(node);
+                return Some(cycle);
+            }
+        } else if let Some(index) = path.get_index_of(neighbor) {
+            visited.remove(node);
+            return Some(
+                path.get_range(index..)
+                    .unwrap()
+                    .into_iter()
+                    .copied()
+                    .collect(),
+            );
+        }
+    }
+
+    path.pop();
+    None
 }
 
 #[cfg(test)]
@@ -122,5 +204,22 @@ mod tests {
         assert_eq!(graph.edge(&"a", &"b"), Some(&1));
         assert_eq!(graph.edge(&"a", &"c"), Some(&2));
         assert_eq!(graph.edge(&"b", &"c"), None);
+    }
+
+    #[test]
+    fn test_find_cycle() {
+        let mut graph = Graph::<&str, i32>::new();
+        graph.insert_edge("a", "b", 1);
+        graph.insert_edge("b", "c", 1);
+        graph.insert_edge("c", "d", 1);
+        graph.insert_edge("d", "e", 1);
+        graph.insert_edge("e", "a", 1);
+        assert_eq!(
+            find_cycle(&graph)
+                .unwrap()
+                .into_iter()
+                .collect::<HashSet<_>>(),
+            HashSet::from_iter(&["a", "b", "c", "d", "e"])
+        );
     }
 }
