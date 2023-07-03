6acc316d19ad4a9d24a1ed46b6020b84c5a3c28a
Aarni Koskela <akx@iki.fi>
2023-07-04 02:35:16 +0300

    Turn Linters', etc. implicit `into_iter()`s into explicit `rules()` (#5436)
    ## Summary
    
    As discussed on ~IRC~ Discord, this will make it easier for e.g. the
    docs generation stuff to get all rules for a linter (using
    `all_rules()`) instead of just non-nursery ones, and it also makes it
    more Explicit Is Better Than Implicit to iterate over linter rules.
    
    Grepping for `Item = Rule` reveals some remaining implicit
    `IntoIterator`s that I didn't feel were necessarily in scope for this
    (and honestly, iterating over a `RuleSet` makes sense).

--- crates/ruff/src/rules/pandas_vet/mod.rs
+++ crates/ruff/src/rules/pandas_vet/mod.rs
@@ -353,8 +353,10 @@ mod tests {
         "PD901_fail_df_var"
     )]
     fn contents(contents: &str, snapshot: &str) {
-        let diagnostics =
-            test_snippet(contents, &settings::Settings::for_rules(&Linter::PandasVet));
+        let diagnostics = test_snippet(
+            contents,
+            &settings::Settings::for_rules(Linter::PandasVet.rules()),
+        );
         assert_messages!(snapshot, diagnostics);
     }
 
