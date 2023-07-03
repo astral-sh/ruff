d63956d900d517604d1ae352564196a06664d4b8
konstin <konstin@mailbox.org>
2023-07-03 21:49:06 +0200

    Format import statements

--- crates/ruff_python_formatter/src/statement/stmt_import_from.rs
+++ crates/ruff_python_formatter/src/statement/stmt_import_from.rs
@@ -1,12 +1,49 @@
-use crate::{not_yet_implemented, FormatNodeRule, PyFormatter};
-use ruff_formatter::{write, Buffer, FormatResult};
+use crate::builders::{optional_parentheses, PyFormatterExtensions};
+use crate::{AsFormat, FormatNodeRule, PyFormatter};
+use ruff_formatter::prelude::{dynamic_text, format_with, space, text};
+use ruff_formatter::{write, Buffer, Format, FormatResult};
 use rustpython_parser::ast::StmtImportFrom;
+use std::iter;
 
 #[derive(Default)]
 pub struct FormatStmtImportFrom;
 
 impl FormatNodeRule<StmtImportFrom> for FormatStmtImportFrom {
     fn fmt_fields(&self, item: &StmtImportFrom, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(f, [not_yet_implemented(item)])
+        let StmtImportFrom {
+            module,
+            names,
+            range: _,
+            level,
+        } = item;
+
+        let level_str = level
+            .map(|level| iter::repeat('.').take(level.to_usize()).collect())
+            .unwrap_or(String::default());
+
+        write!(
+            f,
+            [
+                text("from"),
+                space(),
+                dynamic_text(&level_str, None),
+                module.as_ref().map(|module| module.format()),
+                space(),
+                text("import"),
+                space(),
+            ]
+        )?;
+        if let [name] = names.as_slice() {
+            // star can't be surrounded by parentheses
+            if name.name.as_str() == "*" {
+                return text("*").fmt(f);
+            }
+        }
+        let names = format_with(|f| {
+            f.join_comma_separated()
+                .entries(names.iter().map(|name| (name, name.format())))
+                .finish()
+        });
+        optional_parentheses(&names).fmt(f)
     }
 }
