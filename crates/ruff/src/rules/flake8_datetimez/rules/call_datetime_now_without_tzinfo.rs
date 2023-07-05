b819e098a255060eeeb53e9dc47356a719aebecd
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-07-05 08:34:01 +0530

    Do not flag DTZ001-006 when using `.astimezone()`

--- crates/ruff/src/rules/flake8_datetimez/rules/call_datetime_now_without_tzinfo.rs
+++ crates/ruff/src/rules/flake8_datetimez/rules/call_datetime_now_without_tzinfo.rs
@@ -7,6 +7,8 @@ use ruff_python_ast::helpers::{has_non_none_keyword, is_const_none};
 
 use crate::checkers::ast::Checker;
 
+use super::helpers;
+
 #[violation]
 pub struct CallDatetimeNowWithoutTzinfo;
 
@@ -32,6 +34,10 @@ pub(crate) fn call_datetime_now_without_tzinfo(
             matches!(call_path.as_slice(), ["datetime", "datetime", "now"])
         })
     {
+        return;
+    }
+
+    if helpers::parent_expr_is_astimezone(checker) {
         return;
     }
 
