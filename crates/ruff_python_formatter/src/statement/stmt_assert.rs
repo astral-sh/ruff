9a87a00d36265838d731b93c953eee497298da1f
Chris Pryer <cnpryer@gmail.com>
2023-07-08 20:57:07 -0400

    Revert "Format assert statement"
    This reverts commit 239daa747795e68c8889d47b21699fa7ed3f1a04.

--- crates/ruff_python_formatter/src/statement/stmt_assert.rs
+++ crates/ruff_python_formatter/src/statement/stmt_assert.rs
@@ -1,6 +1,4 @@
-use crate::expression::parentheses::Parenthesize;
-use crate::{AsFormat, FormatNodeRule, PyFormatter};
-use ruff_formatter::prelude::{space, text};
+use crate::{not_yet_implemented, FormatNodeRule, PyFormatter};
 use ruff_formatter::{write, Buffer, FormatResult};
 use rustpython_parser::ast::StmtAssert;
 
@@ -9,32 +7,6 @@ pub struct FormatStmtAssert;
 
 impl FormatNodeRule<StmtAssert> for FormatStmtAssert {
     fn fmt_fields(&self, item: &StmtAssert, f: &mut PyFormatter) -> FormatResult<()> {
-        let StmtAssert {
-            range: _,
-            test,
-            msg,
-        } = item;
-        if let Some(msg) = msg {
-            write!(
-                f,
-                [
-                    text("assert"),
-                    space(),
-                    test.format().with_options(Parenthesize::IfBreaks),
-                    text(","),
-                    space(),
-                    msg.format().with_options(Parenthesize::IfBreaks)
-                ]
-            )
-        } else {
-            write!(
-                f,
-                [
-                    text("assert"),
-                    space(),
-                    test.format().with_options(Parenthesize::IfBreaks)
-                ]
-            )
-        }
+        write!(f, [not_yet_implemented(item)])
     }
 }
