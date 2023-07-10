5d6eac5982e096eee7245b8ccd52da257ccb1320
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-10 11:18:21 -0400

    Tweak some logic

--- crates/ruff/src/rules/pylint/rules/type_name_incorrect_variance.rs
+++ crates/ruff/src/rules/pylint/rules/type_name_incorrect_variance.rs
@@ -10,14 +10,13 @@ use crate::checkers::ast::Checker;
 use crate::rules::pylint::helpers::type_param_name;
 
 /// ## What it does
-/// Checks for type names that do not match the variance of the type parameter.
+/// Checks for type names that do not match the variance of their associated
+/// type parameter.
 ///
 /// ## Why is this bad?
-/// [PEP 484](https://www.python.org/dev/peps/pep-0484/) recommends using the
-/// suffixes `_co` for covariant type parameters and `_contra` for contravariant
-/// type parameters. Invariant type parameters should not have a suffix. This
-/// helps to distinguish between invariant, covariant, and contravariant type
-/// parameters.
+/// [PEP 484] recommends the use of the `_co` and `_contra` suffixes for
+/// covariant and contravariant type parameters, respectively (while invariant
+/// type parameters should not have any such suffix).
 ///
 /// ## Example
 /// ```python
@@ -41,6 +40,8 @@ use crate::rules::pylint::helpers::type_param_name;
 /// - [Python documentation: `typing` — Support for type hints](https://docs.python.org/3/library/typing.html)
 /// - [PEP 483 – The Theory of Type Hints: Covariance and Contravariance](https://peps.python.org/pep-0483/#covariance-and-contravariance)
 /// - [PEP 484 – Type Hints: Covariance and contravariance](https://peps.python.org/pep-0484/#covariance-and-contravariance)
+///
+/// [PEP 484]: https://www.python.org/dev/peps/pep-0484/
 #[violation]
 pub struct TypeNameIncorrectVariance {
     kind: VarKind,
@@ -55,6 +56,7 @@ impl Violation for TypeNameIncorrectVariance {
     }
 }
 
+/// PLC0105
 pub(crate) fn type_name_incorrect_variance(checker: &mut Checker, value: &Expr) {
     let Expr::Call(ast::ExprCall {
         func,
@@ -65,7 +67,11 @@ pub(crate) fn type_name_incorrect_variance(checker: &mut Checker, value: &Expr) 
     else {
         return;
     };
-    let Some(param_name) = type_param_name(args, keywords) else { return; };
+
+    let Some(param_name) = type_param_name(args, keywords) else {
+        return;
+    };
+
     let covariant = keywords
         .iter()
         .find(|keyword| {
@@ -75,6 +81,7 @@ pub(crate) fn type_name_incorrect_variance(checker: &mut Checker, value: &Expr) 
                 .map_or(false, |keyword| keyword.as_str() == "covariant")
         })
         .map(|keyword| &keyword.value);
+
     let contravariant = keywords
         .iter()
         .find(|keyword| {
@@ -84,62 +91,51 @@ pub(crate) fn type_name_incorrect_variance(checker: &mut Checker, value: &Expr) 
                 .map_or(false, |keyword| keyword.as_str() == "contravariant")
         })
         .map(|keyword| &keyword.value);
-    if mismatch(param_name, covariant, contravariant) {
-        let Some(kind) = checker
-            .semantic()
-            .resolve_call_path(func)
-            .and_then(|call_path| {
-                if checker
-                    .semantic()
-                    .match_typing_call_path(&call_path, "ParamSpec")
-                {
-                    Some(VarKind::ParamSpec)
-                } else if checker
-                    .semantic()
-                    .match_typing_call_path(&call_path, "TypeVar")
-                {
-                    Some(VarKind::TypeVar)
-                } else {
-                    None
-                }
-            })
-            else {
-                return;
-            };
-        checker.diagnostics.push(Diagnostic::new(
-            TypeNameIncorrectVariance {
-                kind,
-                param_name: param_name.to_string(),
-            },
-            func.range(),
-        ));
+
+    if !mismatch(param_name, covariant, contravariant) {
+        return;
+    }
+
+    let Some(kind) = checker
+        .semantic()
+        .resolve_call_path(func)
+        .and_then(|call_path| {
+            if checker
+                .semantic()
+                .match_typing_call_path(&call_path, "ParamSpec")
+            {
+                Some(VarKind::ParamSpec)
+            } else if checker
+                .semantic()
+                .match_typing_call_path(&call_path, "TypeVar")
+            {
+                Some(VarKind::TypeVar)
+            } else {
+                None
+            }
+        })
+    else {
+        return;
     };
+
+    checker.diagnostics.push(Diagnostic::new(
+        TypeNameIncorrectVariance {
+            kind,
+            param_name: param_name.to_string(),
+        },
+        func.range(),
+    ));
 }
 
+/// Returns `true` if the parameter name does not match its type variance.
 fn mismatch(param_name: &str, covariant: Option<&Expr>, contravariant: Option<&Expr>) -> bool {
     if param_name.ends_with("_co") {
-        match covariant {
-            Some(covariant) if !is_const_true(covariant) => true,
-            None => true,
-            _ => false,
-        }
+        covariant.map_or(true, |covariant| !is_const_true(covariant))
     } else if param_name.ends_with("_contra") {
-        match contravariant {
-            Some(contravariant) if !is_const_true(contravariant) => true,
-            None => true,
-            _ => false,
-        }
+        contravariant.map_or(true, |contravariant| !is_const_true(contravariant))
     } else {
-        match (covariant, contravariant) {
-            (Some(covariant), None) if is_const_true(covariant) => true,
-            (None, Some(contravariant)) if is_const_true(contravariant) => true,
-            (Some(covariant), Some(contravariant))
-                if is_const_true(covariant) || is_const_true(contravariant) =>
-            {
-                true
-            }
-            _ => false,
-        }
+        covariant.map_or(false, |covariant| is_const_true(covariant))
+            || contravariant.map_or(false, |contravariant| is_const_true(contravariant))
     }
 }
 
