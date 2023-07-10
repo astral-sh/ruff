b2b979ab15d4dfb1a1812fd3da2c3941d5f5252f
Tom Kuson <mail@tjkuson.me>
2023-07-10 16:44:16 +0100

    Format

--- crates/ruff/src/rules/pylint/rules/type_name_incorrect_variance.rs
+++ crates/ruff/src/rules/pylint/rules/type_name_incorrect_variance.rs
@@ -134,8 +134,7 @@ fn mismatch(param_name: &str, covariant: Option<&Expr>, contravariant: Option<&E
     } else if param_name.ends_with("_contra") {
         contravariant.map_or(true, |contravariant| !is_const_true(contravariant))
     } else {
-        covariant.map_or(false, is_const_true)
-            || contravariant.map_or(false, is_const_true)
+        covariant.map_or(false, is_const_true) || contravariant.map_or(false, is_const_true)
     }
 }
 
