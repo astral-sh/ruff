4cac75bc271104b1b9b80a979b9cb737f5ce5153
Tom Kuson <mail@tjkuson.me>
2023-07-10 16:45:36 +0100

    Add documentation to `pandas-vet` rules (#5629)
    ## Summary
    
    Completes all the documentation for the `pandas-vet` rules, except for
    `pandas-use-of-dot-read-table` as I am unclear of the rule's motivation
    (see #5628).
    
    Related to #2646.
    
    ## Test Plan
    
    `python scripts/check_docs_formatted.py && mkdocs serve`

--- crates/ruff/src/rules/pandas_vet/rules/assignment_to_df.rs
+++ crates/ruff/src/rules/pandas_vet/rules/assignment_to_df.rs
@@ -3,6 +3,29 @@ use rustpython_parser::ast::{self, Expr, Ranged};
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 
+/// ## What it does
+/// Checks for assignments to the variable `df`.
+///
+/// ## Why is this bad?
+/// Although `df` is a common variable name for a Pandas DataFrame, it's not a
+/// great variable name for production code, as it's non-descriptive and
+/// prone to name conflicts.
+///
+/// Instead, use a more descriptive variable name.
+///
+/// ## Example
+/// ```python
+/// import pandas as pd
+///
+/// df = pd.read_csv("animals.csv")
+/// ```
+///
+/// Use instead:
+/// ```python
+/// import pandas as pd
+///
+/// animals = pd.read_csv("animals.csv")
+/// ```
 #[violation]
 pub struct PandasDfVariableName;
 
