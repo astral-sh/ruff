d19839fe0fc2b3d4b2dbe40227734baf0103ab6a
Zanie <contact@zanie.dev>
2023-07-10 12:11:54 -0500

    Add support for `Union` declarations without `|` to PYI016 (#5598)
    Previously, PYI016 only supported reporting violations for unions
    defined with `|`. Now, union declarations with `typing.Union` are
    supported.

--- crates/ruff/src/rules/flake8_pyi/rules/unnecessary_literal_union.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/unnecessary_literal_union.rs
@@ -1,11 +1,11 @@
-use ruff_python_semantic::SemanticModel;
-use rustpython_parser::ast::{self, Expr, Operator, Ranged};
+use rustpython_parser::ast::{self, Expr, Ranged};
 
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use smallvec::SmallVec;
 
 use crate::checkers::ast::Checker;
+use crate::rules::flake8_pyi::helpers::traverse_union;
 
 /// ## What it does
 /// Checks for the presence of multiple literal types in a union.
@@ -47,7 +47,7 @@ pub(crate) fn unnecessary_literal_union<'a>(checker: &mut Checker, expr: &'a Exp
     let mut literal_exprs = SmallVec::<[&Box<Expr>; 1]>::new();
 
     // Adds a member to `literal_exprs` if it is a `Literal` annotation
-    let mut collect_literal_expr = |expr: &'a Expr| {
+    let mut collect_literal_expr = |expr: &'a Expr, _| {
         if let Expr::Subscript(ast::ExprSubscript { value, slice, .. }) = expr {
             if checker.semantic().match_typing_expr(value, "Literal") {
                 literal_exprs.push(slice);
@@ -56,7 +56,7 @@ pub(crate) fn unnecessary_literal_union<'a>(checker: &mut Checker, expr: &'a Exp
     };
 
     // Traverse the union, collect all literal members
-    traverse_union(&mut collect_literal_expr, expr, checker.semantic());
+    traverse_union(&mut collect_literal_expr, checker.semantic(), expr, None);
 
     // Raise a violation if more than one
     if literal_exprs.len() > 1 {
@@ -72,49 +72,4 @@ pub(crate) fn unnecessary_literal_union<'a>(checker: &mut Checker, expr: &'a Exp
 
         checker.diagnostics.push(diagnostic);
     }
-}
-
-/// Traverse a "union" type annotation, calling `func` on each expression in the union.
-fn traverse_union<'a, F>(func: &mut F, expr: &'a Expr, semantic: &SemanticModel)
-where
-    F: FnMut(&'a Expr),
-{
-    // Ex) x | y
-    if let Expr::BinOp(ast::ExprBinOp {
-        op: Operator::BitOr,
-        left,
-        right,
-        range: _,
-    }) = expr
-    {
-        // The union data structure usually looks like this:
-        //  a | b | c -> (a | b) | c
-        //
-        // However, parenthesized expressions can coerce it into any structure:
-        //  a | (b | c)
-        //
-        // So we have to traverse both branches in order (left, then right), to report members
-        // in the order they appear in the source code.
-
-        // Traverse the left then right arms
-        traverse_union(func, left, semantic);
-        traverse_union(func, right, semantic);
-        return;
-    }
-
-    // Ex) Union[x, y]
-    if let Expr::Subscript(ast::ExprSubscript { value, slice, .. }) = expr {
-        if semantic.match_typing_expr(value, "Union") {
-            if let Expr::Tuple(ast::ExprTuple { elts, .. }) = slice.as_ref() {
-                // Traverse each element of the tuple within the union recursively to handle cases
-                // such as `Union[..., Union[...]]
-                elts.iter()
-                    .for_each(|elt| traverse_union(func, elt, semantic));
-                return;
-            }
-        }
-    }
-
-    // Otherwise, call the function on expression
-    func(expr);
 }
