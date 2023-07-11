e65e9ba3a424c14359b350a07a71250081958b95
Zanie <contact@zanie.dev>
2023-07-11 12:05:27 -0500

    Refactor for readability; add handling of slice steps

--- crates/ruff/src/rules/ruff/rules/invalid_index_type.rs
+++ crates/ruff/src/rules/ruff/rules/invalid_index_type.rs
@@ -2,6 +2,7 @@ use rustpython_parser::ast::{Constant, Expr, ExprConstant, ExprSlice, ExprSubscr
 
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
+use std::fmt;
 
 use crate::checkers::ast::Checker;
 
@@ -25,22 +26,24 @@ use crate::checkers::ast::Checker;
 /// ```
 #[violation]
 pub struct InvalidIndexType {
-    var_type: String,
-    idx_type: String,
-    slice_bound: bool,
+    value_type: String,
+    index_type: String,
+    is_slice: bool,
 }
 
 impl Violation for InvalidIndexType {
     #[derive_message_formats]
     fn message(&self) -> String {
         let InvalidIndexType {
-            var_type, idx_type, ..
+            value_type,
+            index_type,
+            ..
         } = self;
-        if self.slice_bound {
-            format!("Slice in indexed access to type `{var_type}` uses type `{idx_type}` as bound instead of an integer.")
+        if self.is_slice {
+            format!("Slice in indexed access to type `{value_type}` uses type `{index_type}` instead of an integer.")
         } else {
             format!(
-                "Indexed access to type `{var_type}` uses type `{idx_type}` instead of an integer or slice."
+                "Indexed access to type `{value_type}` uses type `{index_type}` instead of an integer or slice."
             )
         }
     }
@@ -48,10 +51,14 @@ impl Violation for InvalidIndexType {
 
 /// RUF015
 pub(crate) fn invalid_index_type<'a>(checker: &mut Checker, expr: &'a ExprSubscript) {
-    let ExprSubscript { value, slice, .. } = expr;
+    let ExprSubscript {
+        value,
+        slice: index,
+        ..
+    } = expr;
 
-    // If the value being indexed is a list, tuple, string, f-string, bytes, or comprehension
-    if matches!(
+    // Check the value being indexed is a list, tuple, string, f-string, bytes, or comprehension
+    if !matches!(
         value.as_ref(),
         Expr::List(_)
             | Expr::ListComp(_)
@@ -62,94 +69,126 @@ pub(crate) fn invalid_index_type<'a>(checker: &mut Checker, expr: &'a ExprSubscr
                 ..
             })
     ) {
-        // Then check the contents of the index
-        match slice.as_ref() {
-            // If the index is a const, only allow integers
-            Expr::Constant(ExprConstant {
-                value: index_value, ..
-            }) => {
-                if !index_value.is_int() {
+        return;
+    }
+
+    // The types supported by this rule should always be checkable
+    let value_type = CheckableExprType::try_from(&value)
+        .expect("Expected indexed expression to be a checkable type.");
+
+    // If the index is not a checkable type then we can't easily determine if there is a violation
+    let Some(index_type) = CheckableExprType::try_from(&index) else {
+        return;
+    };
+
+    // Then check the contents of the index
+    match index.as_ref() {
+        Expr::Constant(ExprConstant {
+            value: index_value, ..
+        }) => {
+            // If the index is a constant, require an integer
+            if !index_value.is_int() {
+                checker.diagnostics.push(Diagnostic::new(
+                    InvalidIndexType {
+                        value_type: value_type.to_string(),
+                        index_type: constant_type_name(index_value).to_string(),
+                        is_slice: false,
+                    },
+                    index.range(),
+                ));
+            }
+        }
+        Expr::Slice(ExprSlice {
+            lower, upper, step, ..
+        }) => {
+            // If the index is a slice, require integer or null bounds
+            for is_slice in [lower, upper, step].into_iter().flatten() {
+                if let Expr::Constant(ExprConstant {
+                    value: index_value, ..
+                }) = is_slice.as_ref()
+                {
+                    if !(index_value.is_int() || index_value.is_none()) {
+                        checker.diagnostics.push(Diagnostic::new(
+                            InvalidIndexType {
+                                value_type: value_type.to_string(),
+                                index_type: constant_type_name(index_value).to_string(),
+                                is_slice: true,
+                            },
+                            is_slice.range(),
+                        ));
+                    }
+                } else if let Some(is_slice_type) = CheckableExprType::try_from(is_slice.as_ref()) {
                     checker.diagnostics.push(Diagnostic::new(
                         InvalidIndexType {
-                            var_type: expression_type_name(value)
-                                .expect("Failed to cast parent expression to type name")
-                                .to_string(),
-                            idx_type: constant_type_name(index_value).to_string(),
-                            slice_bound: false,
+                            value_type: value_type.to_string(),
+                            index_type: is_slice_type.to_string(),
+                            is_slice: true,
                         },
-                        slice.range(),
+                        is_slice.range(),
                     ));
                 }
             }
-            // If the index is a slice, check for integer or null bounds
-            Expr::Slice(ExprSlice { lower, upper, .. }) => {
-                for slice_bound in [lower, upper].into_iter().flatten() {
-                    if let Expr::Constant(ExprConstant {
-                        value: index_value, ..
-                    }) = slice_bound.as_ref()
-                    {
-                        if !(index_value.is_int() || index_value.is_none()) {
-                            checker.diagnostics.push(Diagnostic::new(
-                                InvalidIndexType {
-                                    var_type: expression_type_name(value)
-                                        .expect("Failed to cast parent expression to type name")
-                                        .to_string(),
-                                    idx_type: constant_type_name(index_value).to_string(),
-                                    slice_bound: true,
-                                },
-                                slice_bound.range(),
-                            ));
-                        }
-                    } else if matches!(
-                        slice_bound.as_ref(),
-                        Expr::Tuple(_)
-                            | Expr::List(_)
-                            | Expr::Set(_)
-                            | Expr::Dict(_)
-                            | Expr::ListComp(_)
-                            | Expr::SetComp(_)
-                            | Expr::DictComp(_)
-                            | Expr::JoinedStr(_)
-                    ) {
-                        checker.diagnostics.push(Diagnostic::new(
-                            InvalidIndexType {
-                                var_type: expression_type_name(value)
-                                    .expect("Failed to cast parent expression to type name")
-                                    .to_string(),
-                                idx_type: expression_type_name(slice_bound)
-                                    .expect("Failed to slice bound expression to type name")
-                                    .to_string(),
-                                slice_bound: true,
-                            },
-                            slice_bound.range(),
-                        ));
-                    }
-                }
-            }
-            // If it's some other data type, it's a violation
-            Expr::Tuple(_)
-            | Expr::List(_)
-            | Expr::Set(_)
-            | Expr::Dict(_)
-            | Expr::ListComp(_)
-            | Expr::SetComp(_)
-            | Expr::DictComp(_)
-            | Expr::JoinedStr(_) => {
-                checker.diagnostics.push(Diagnostic::new(
-                    InvalidIndexType {
-                        var_type: expression_type_name(value)
-                            .expect("Failed to cast parent expression to type name")
-                            .to_string(),
-                        idx_type: expression_type_name(slice)
-                            .expect("Failed to cast index expression to type name")
-                            .to_string(),
-                        slice_bound: false,
-                    },
-                    slice.range(),
-                ));
-            }
-            // If it's anything else, it's too hard to tell if it's a violation
-            _ => (),
+        }
+        _ => {
+            // If it's some other checkable data type, it's a violation
+            checker.diagnostics.push(Diagnostic::new(
+                InvalidIndexType {
+                    value_type: value_type.to_string(),
+                    index_type: index_type.to_string(),
+                    is_slice: false,
+                },
+                index.range(),
+            ));
+        }
+    }
+}
+
+/// An expression that can be checked for type compatibility
+#[derive(Debug)]
+enum CheckableExprType<'a> {
+    Constant(&'a Constant),
+    JoinedStr,
+    List,
+    ListComp,
+    SetComp,
+    DictComp,
+    Set,
+    Dict,
+    Tuple,
+    Slice,
+}
+
+impl fmt::Display for CheckableExprType<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Constant(constant) => f.write_str(&constant_type_name(constant)),
+            Self::JoinedStr => f.write_str("str"),
+            Self::List => f.write_str("list"),
+            Self::SetComp => f.write_str("set comprehension"),
+            Self::ListComp => f.write_str("list comprehension"),
+            Self::DictComp => f.write_str("dict comprehension"),
+            Self::Set => f.write_str("set"),
+            Self::Slice => f.write_str("slice"),
+            Self::Dict => f.write_str("dict"),
+            Self::Tuple => f.write_str("tuple"),
+        }
+    }
+}
+
+impl<'a> CheckableExprType<'a> {
+    fn try_from(expr: &'a Expr) -> Option<Self> {
+        match expr {
+            Expr::Constant(ExprConstant { value, .. }) => Some(Self::Constant(value)),
+            Expr::JoinedStr(_) => Some(Self::JoinedStr),
+            Expr::List(_) => Some(Self::List),
+            Expr::ListComp(_) => Some(Self::ListComp),
+            Expr::SetComp(_) => Some(Self::SetComp),
+            Expr::DictComp(_) => Some(Self::DictComp),
+            Expr::Set(_) => Some(Self::Set),
+            Expr::Dict(_) => Some(Self::Dict),
+            Expr::Tuple(_) => Some(Self::Tuple),
+            Expr::Slice(_) => Some(Self::Slice),
+            _ => None,
         }
     }
 }
@@ -165,22 +204,5 @@ fn constant_type_name(constant: &Constant) -> &'static str {
         Constant::Float(_) => "float",
         Constant::Complex { .. } => "complex",
         Constant::Ellipsis => "ellipsis",
-    }
-}
-
-/// Utility for casting expressions to their Python type name
-/// Does not cover all cases, only implements expressions needed in RUF015
-fn expression_type_name(expr: &Expr) -> Option<&'static str> {
-    match expr {
-        Expr::Constant(ExprConstant { value, .. }) => Some(constant_type_name(value)),
-        Expr::JoinedStr(_) => Some("str"),
-        Expr::Tuple(_) => Some("tuple"),
-        Expr::List(_) => Some("list"),
-        Expr::Set(_) => Some("set"),
-        Expr::Dict(_) => Some("dict"),
-        Expr::ListComp(_) => Some("list comprehension"),
-        Expr::SetComp(_) => Some("set comprehension"),
-        Expr::DictComp(_) => Some("dict comprehension"),
-        _ => None,
     }
 }
