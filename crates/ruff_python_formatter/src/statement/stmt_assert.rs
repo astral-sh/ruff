411f5cee673db58f467a6bc8eb1085d42f631bdd
Chris Pryer <cnpryer@gmail.com>
2023-07-11 20:12:26 -0400

    Use group if msg

--- crates/ruff_python_formatter/src/statement/stmt_assert.rs
+++ crates/ruff_python_formatter/src/statement/stmt_assert.rs
@@ -1,6 +1,6 @@
 use crate::expression::parentheses::Parenthesize;
 use crate::{AsFormat, FormatNodeRule, PyFormatter};
-use ruff_formatter::prelude::{space, text};
+use ruff_formatter::prelude::{format_args, group, space, text};
 use ruff_formatter::{write, Buffer, FormatResult};
 use rustpython_parser::ast::StmtAssert;
 
@@ -15,24 +15,20 @@ impl FormatNodeRule<StmtAssert> for FormatStmtAssert {
             msg,
         } = item;
 
-        write!(
-            f,
-            [
-                text("assert"),
-                space(),
-                test.format().with_options(Parenthesize::IfBreaks)
-            ]
-        )?;
+        write!(f, [text("assert"), space()])?;
 
         if let Some(msg) = msg {
             write!(
                 f,
-                [
+                [group(&format_args![
+                    test.format().with_options(Parenthesize::IfBreaks),
                     text(","),
                     space(),
-                    msg.format().with_options(Parenthesize::IfBreaks)
-                ]
+                    &msg.format().with_options(Parenthesize::IfBreaks)
+                ])]
             )?;
+        } else {
+            write!(f, [test.format().with_options(Parenthesize::IfBreaks)])?;
         }
 
         Ok(())
