709c4a70f6d4ba6ce91d12a71cde94dee34789a9
Chris Pryer <cnpryer@gmail.com>
2023-07-13 11:51:28 -0400

    Don't use group

--- crates/ruff_python_formatter/src/statement/stmt_assert.rs
+++ crates/ruff_python_formatter/src/statement/stmt_assert.rs
@@ -1,7 +1,7 @@
 use crate::expression::maybe_parenthesize_expression;
 use crate::expression::parentheses::Parenthesize;
 use crate::{FormatNodeRule, PyFormatter};
-use ruff_formatter::prelude::{format_args, group, space, text};
+use ruff_formatter::prelude::{space, text};
 use ruff_formatter::{write, Buffer, FormatResult};
 use rustpython_parser::ast::StmtAssert;
 
@@ -16,26 +16,23 @@ impl FormatNodeRule<StmtAssert> for FormatStmtAssert {
             msg,
         } = item;
 
-        write!(f, [text("assert"), space()])?;
+        write!(
+            f,
+            [
+                text("assert"),
+                space(),
+                maybe_parenthesize_expression(test, item, Parenthesize::IfBreaks)
+            ]
+        )?;
 
         if let Some(msg) = msg {
             write!(
                 f,
-                [group(&format_args![
-                    maybe_parenthesize_expression(test, item, Parenthesize::IfBreaks),
+                [
                     text(","),
                     space(),
                     maybe_parenthesize_expression(msg, item, Parenthesize::IfBreaks),
-                ])]
-            )?;
-        } else {
-            write!(
-                f,
-                [maybe_parenthesize_expression(
-                    test,
-                    item,
-                    Parenthesize::IfBreaks
-                )]
+                ]
             )?;
         }
 
