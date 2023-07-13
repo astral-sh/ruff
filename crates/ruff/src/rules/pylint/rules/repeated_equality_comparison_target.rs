572116897a5aaff4df49148fe86377b3d44e30d2
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-12 21:20:15 -0400

    Tweaks

--- crates/ruff/src/rules/pylint/rules/repeated_equality_comparison_target.rs
+++ crates/ruff/src/rules/pylint/rules/repeated_equality_comparison_target.rs
@@ -1,23 +1,30 @@
-use crate::checkers::ast::Checker;
-use itertools::any;
+use std::hash::BuildHasherDefault;
+use std::ops::Deref;
+
+use itertools::{any, Itertools};
+use rustc_hash::FxHashMap;
+use rustpython_parser::ast::{BoolOp, CmpOp, Expr, ExprBoolOp, ExprCompare, Ranged};
+
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::hashable::HashableExpr;
-use rustc_hash::FxHashMap;
-use rustpython_parser::ast::{BoolOp, CmpOp, Expr, ExprBoolOp, ExprCompare, Ranged};
-use std::ops::Deref;
+use ruff_python_ast::source_code::Locator;
+
+use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for equality comparisons that can rewritten as a membership test.
+/// Checks for repeated equality comparisons that can rewritten as a membership
+/// test.
 ///
 /// ## Why is this bad?
-/// Instead, to check if a variable is equal to one of many values, combine the
-/// values into a collection and use the `in` operator. This is faster and less
-/// verbose.
+/// To check if a variable is equal to one of many values, it is common to
+/// write a series of equality comparisons (e.g.,
+/// `foo == "bar" or foo == "baz").
 ///
-/// If the items are hashable, use a `set` instead of a `tuple`. Membership
-/// using the `in` operator is more efficient for sets than for non-hashable
-/// collections like lists and tuples.
+/// Instead, prefer to combine the values into a collection and use the `in`
+/// operator to check for membership, which is more performant and succinct.
+/// If the items are hashable, use a `set` for efficiency; otherwise, use a
+/// `tuple`.
 ///
 /// ## Example
 /// ```python
@@ -43,100 +50,102 @@ impl Violation for RepeatedEqualityComparisonTarget {
     fn message(&self) -> String {
         let RepeatedEqualityComparisonTarget { expr } = self;
         format!(
-            "Consider merging multiple comparisons: `{expr}`. \
-            Use a `set` if the elements are hashable."
+            "Consider merging multiple comparisons: `{expr}`. Use a `set` if the elements are hashable."
         )
     }
 }
 
 /// PLR1714
 pub(crate) fn repeated_equality_comparison_target(checker: &mut Checker, bool_op: &ExprBoolOp) {
-    if any(bool_op.values.iter(), |v| !is_allowed_value(bool_op.op, v)) {
+    if bool_op
+        .values
+        .iter()
+        .any(|value| !is_allowed_value(bool_op.op, value))
+    {
         return;
     }
-    let mut left_to_comparators: FxHashMap<HashableExpr, (usize, Vec<HashableExpr>)> =
-        FxHashMap::default();
+
+    let mut left_to_comparators: FxHashMap<HashableExpr, (usize, Vec<&Expr>)> =
+        FxHashMap::with_capacity_and_hasher(bool_op.values.len(), BuildHasherDefault::default());
     for value in &bool_op.values {
-        match value {
-            Expr::Compare(ExprCompare {
-                left, comparators, ..
-            }) => {
-                let (count, matches) = left_to_comparators
-                    .entry(left.deref().into())
-                    .or_insert_with(|| (0, Vec::new()));
-                *count += 1;
-                matches.extend(comparators.iter().map(HashableExpr::from));
-            }
-            _ => continue,
+        if let Expr::Compare(ExprCompare {
+            left, comparators, ..
+        }) = value
+        {
+            let (count, matches) = left_to_comparators
+                .entry(left.deref().into())
+                .or_insert_with(|| (0, Vec::new()));
+            *count += 1;
+            matches.extend(comparators);
         }
     }
+
     for (left, (count, comparators)) in left_to_comparators {
-        if count < 2 {
-            continue;
+        if count > 1 {
+            checker.diagnostics.push(Diagnostic::new(
+                RepeatedEqualityComparisonTarget {
+                    expr: merged_membership_test(
+                        left.as_expr(),
+                        bool_op.op,
+                        &comparators,
+                        checker.locator,
+                    ),
+                },
+                bool_op.range(),
+            ));
         }
-        let membership_expr = merged_membership_test(
-            &checker.generator().expr(left.as_expr()),
-            comparators
-                .iter()
-                .map(|c| checker.generator().expr(c.as_expr()))
-                .collect::<Vec<String>>(),
-            bool_op.op,
-        );
-        checker.diagnostics.push(Diagnostic::new(
-            RepeatedEqualityComparisonTarget {
-                expr: membership_expr,
-            },
-            bool_op.range(),
-        ));
     }
 }
 
-// Eq can be joined with Or; NotEq can be joined with And.
+/// Return `true` if the given expression is compatible with a membership test.
+/// E.g., `==` operators can be joined with `or` and `!=` operators can be
+/// joined with `and`.
 fn is_allowed_value(bool_op: BoolOp, value: &Expr) -> bool {
-    match value {
-        Expr::Compare(ExprCompare {
-            left,
-            ops,
-            comparators,
-            ..
-        }) => {
-            for cmp_op in ops {
-                if match bool_op {
-                    BoolOp::Or => !matches!(cmp_op, CmpOp::Eq),
-                    BoolOp::And => !matches!(cmp_op, CmpOp::NotEq),
-                } {
-                    return false;
-                }
-                if left.is_call_expr() {
-                    return false;
-                }
-                if any(comparators.iter(), Expr::is_call_expr) {
-                    return false;
-                }
-            }
-            true
+    let Expr::Compare(ExprCompare {
+        left,
+        ops,
+        comparators,
+        ..
+    }) = value
+    else {
+        return false;
+    };
+
+    ops.iter().all(|op| {
+        if match bool_op {
+            BoolOp::Or => !matches!(op, CmpOp::Eq),
+            BoolOp::And => !matches!(op, CmpOp::NotEq),
+        } {
+            return false;
         }
-        _ => false,
-    }
+
+        if left.is_call_expr() {
+            return false;
+        }
+
+        if any(comparators.iter(), Expr::is_call_expr) {
+            return false;
+        }
+
+        true
+    })
 }
 
-// Produces a string like `obj in (a, b, c)` or `obj not in (a, b, c)`.
+/// Generate a string like `obj in (a, b, c)` or `obj not in (a, b, c)`.
 fn merged_membership_test(
-    obj: &str,
-    collection_items: impl IntoIterator<Item = String>,
+    left: &Expr,
     op: BoolOp,
+    comparators: &[&Expr],
+    locator: &Locator,
 ) -> String {
-    let membership_op = match op {
+    let op = match op {
         BoolOp::Or => "in",
         BoolOp::And => "not in",
     };
-    format!(
-        "{} {} ({})",
-        obj,
-        membership_op,
-        collection_items
-            .into_iter()
-            .collect::<Vec<String>>()
-            .join(", ")
-    )
+    let left = locator.slice(left.range());
+    let members = comparators
+        .iter()
+        .map(|comparator| locator.slice(comparator.range()))
+        .join(", ");
+    format!("{left} {op} ({members})",)
 }
