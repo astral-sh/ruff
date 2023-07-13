c6234bb1adc92a4e4120434b48af478c33f03454
konstin <konstin@mailbox.org>
2023-07-13 12:30:05 +0200

    PEP8 on the test file

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/ann_assign.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/ann_assign.py
@@ -1,6 +1,7 @@
 # Regression test: Don't forget the parentheses in the value when breaking
 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: int = a + 1 * a
 
+
 # Regression test: Don't forget the parentheses in the annotation when breaking
-class DefaultRunner():
+class DefaultRunner:
     task_runner_cls: TaskRunnerProtocol | typing.Callable[[], typing.Any] = DefaultTaskRunner
