f0567032d3d59cf1c2282862a02904c307f271db
harupy <hkawamura0130@gmail.com>
2023-07-15 20:17:27 +0900

    Ignore directories when collecting files to lint

--- crates/ruff/src/resolver.rs
+++ crates/ruff/src/resolver.rs
@@ -330,9 +330,13 @@ pub fn python_files_in_path(
             }
 
             if result.as_ref().map_or(true, |entry| {
+                // Ignore directories
+                if entry.file_type().map_or(true, |ft| ft.is_dir()) {
+                    return false;
+                }
                 if entry.depth() == 0 {
                     // Accept all files that are passed-in directly.
-                    entry.file_type().map_or(false, |ft| ft.is_file())
+                    true
                 } else {
                     // Otherwise, check if the file is included.
                     let path = entry.path();
