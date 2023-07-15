8ccd697020b543e606d559d8a7c58d6dc3ae372f
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-15 15:37:34 -0400

    Expand scope of `quoted-annotation` rule (#5766)
    ## Summary
    
    Previously, the `quoted-annotation` rule only removed quotes when `from
    __future__ import annotations` was present. However, there are some
    other cases in which this is also safe -- for example:
    
    ```python
    def foo():
        x: "MyClass"
    ```
    
    We already model these in the semantic model, so this PR just expands
    the scope of the rule to handle those.

--- crates/ruff/src/checkers/ast/mod.rs
+++ crates/ruff/src/checkers/ast/mod.rs
@@ -4581,7 +4581,7 @@ impl<'a> Checker<'a> {
 
                     self.semantic.restore(snapshot);
 
-                    if self.semantic.in_annotation() && self.semantic.future_annotations() {
+                    if self.semantic.in_typing_only_annotation() {
                         if self.enabled(Rule::QuotedAnnotation) {
                             pyupgrade::rules::quoted_annotation(self, value, range);
                         }
