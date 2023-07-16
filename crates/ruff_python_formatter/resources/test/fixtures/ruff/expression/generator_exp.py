e78acd07db5e96c018029a4bac94f770327c3dba
David Szotten <davidszotten@gmail.com>
2023-07-16 13:37:21 +0100

    found accepted black bug to remove parens

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/expression/generator_exp.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/expression/generator_exp.py
@@ -6,7 +6,7 @@ len(a for b in c)
 # parens around generator expression required
 sum((a for b in c), start=0)
 
-# parens not needed but keep since they are in the source
+# black keeps these atm: https://github.com/psf/black/issues/2943
 f((1 for _ in a))
 
 # make sure source parenthesis detection isn't fooled by these
