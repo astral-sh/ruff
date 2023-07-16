25294a5da25c0d321b2614c39fd638f931dd1aca
harupy <hkawamura0130@gmail.com>
2023-07-16 23:19:27 +0900

    Fix test cases

--- crates/ruff/resources/test/fixtures/pyupgrade/UP014.py
+++ crates/ruff/resources/test/fixtures/pyupgrade/UP014.py
@@ -4,20 +4,8 @@ import typing
 # with complex annotations
 MyType = NamedTuple("MyType", [("a", int), ("b", tuple[str, ...])])
 
-# with default values as list
-MyType = NamedTuple(
-    "MyType",
-    [("a", int), ("b", str), ("c", list[bool])],
-)
-
 # with namespace
 MyType = typing.NamedTuple("MyType", [("a", int), ("b", str)])
-
-# too many default values (OK)
-MyType = NamedTuple(
-    "MyType",
-    [("a", int), ("b", str)],
-)
 
 # invalid identifiers (OK)
 MyType = NamedTuple("MyType", [("x-y", int), ("b", tuple[str, ...])])
@@ -30,3 +18,7 @@ MyType = typing.NamedTuple("MyType", [])
 
 # keywords
 MyType = typing.NamedTuple("MyType", a=int, b=tuple[str, ...])
+
+# unfixable
+MyType = typing.NamedTuple("MyType", [("a", int)], [("b", str)])
+MyType = typing.NamedTuple("MyType", [("a", int)], b=str)
