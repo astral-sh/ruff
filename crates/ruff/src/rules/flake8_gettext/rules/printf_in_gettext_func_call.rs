ac9f36d1e166be009789a0a9cc50898fdcad52a0
Tom Kuson <mail@tjkuson.me>
2023-07-16 22:39:48 +0100

    Add docs

--- crates/ruff/src/rules/flake8_gettext/rules/printf_in_gettext_func_call.rs
+++ crates/ruff/src/rules/flake8_gettext/rules/printf_in_gettext_func_call.rs
@@ -4,6 +4,42 @@ use crate::checkers::ast::Checker;
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 
+/// ## What it does
+/// Checks for printf-style formatted strings in `gettext` function calls.
+///
+/// ## Why is this bad?
+/// In the `gettext` API, the `gettext` function (usually aliased to `_`)
+/// returns a translation of the given string by looking it up in a translation
+/// catalogue.
+///
+/// Formatting strings in the function call means the formatted string will be
+/// passed to the function, which will then look it up in the translation
+/// catalogue.
+///
+/// This is likely unintended. Even if such behavior is intended, it is
+/// error-prone as the translation catalogue may not contain the formatted
+/// string.
+///
+/// Instead, consider formatting the result of the function call.
+///
+/// ## Example
+/// ```python
+/// from gettext import gettext as _
+///
+/// name = "Maria"
+/// _("Hello, {}!".format(name))  # Looks for "Hello, Maria!".
+/// ```
+///
+/// Use instead:
+/// ```python
+/// from gettext import gettext as _
+///
+/// name = "Maria"
+/// _("Hello, %s!") % name  # Looks for "Hello, %s!".
+/// ```
+///
+/// ## References
+/// - [Python documentation: gettext](https://docs.python.org/3/library/gettext.html)
 #[violation]
 pub struct PrintfInGetTextFuncCall;
 
