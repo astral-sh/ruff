f008a7f7fffd9926beaa587d3ad2fd98a9ddc2f1
Tom Kuson <mail@tjkuson.me>
2023-07-16 15:12:29 +0100

    Add docs

--- crates/ruff/src/rules/pandas_vet/rules/read_table.rs
+++ crates/ruff/src/rules/pandas_vet/rules/read_table.rs
@@ -6,6 +6,34 @@ use ruff_macros::{derive_message_formats, violation};
 
 use crate::checkers::ast::Checker;
 
+/// ## What it does
+/// Checks for uses of `.read_table` to read CSV files.
+///
+/// ## Why is this bad?
+/// In the Pandas API, `.read_csv` and `.read_table` are equivalent apart from
+/// the default separator; `.read_csv` uses a comma (`,`) as the default
+/// separator, while `.read_table` uses a tab (`\t`) as the default separator.
+///
+/// Prefer `.read_csv` over `.read_table` to read CSV files, since it is
+/// clearer and more idiomatic.
+///
+/// ## Example
+/// ```python
+/// import pandas as pd
+///
+/// cities_df = pd.read_table("cities.csv", sep=",")
+/// ```
+///
+/// Use instead:
+/// ```python
+/// import pandas as pd
+///
+/// cities_df = pd.read_csv("cities.csv")
+/// ```
+///
+/// ## References
+/// - [Pandas documentation: `read_csv`](https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html#pandas.read_csv)
+/// - [Pandas documentation: `read_table`](https://pandas.pydata.org/docs/reference/api/pandas.read_table.html#pandas.read_table)
 #[violation]
 pub struct PandasUseOfDotReadTable;
 
