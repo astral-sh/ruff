bacf71356b7c397564b0f1f74e04406e6e034260
Chris Pryer <cnpryer@gmail.com>
2023-07-18 20:26:44 -0400

    Handle lambda Arguments dangling comments

--- crates/ruff_python_formatter/src/expression/expr_lambda.rs
+++ crates/ruff_python_formatter/src/expression/expr_lambda.rs
@@ -1,3 +1,4 @@
+use crate::comments::dangling_comments;
 use crate::context::PyFormatContext;
 use crate::expression::parentheses::{NeedsParentheses, OptionalParentheses};
 use crate::other::arguments::ArgumentsParentheses;
@@ -5,7 +6,7 @@ use crate::AsFormat;
 use crate::{FormatNodeRule, PyFormatter};
 use ruff_formatter::prelude::{space, text};
 use ruff_formatter::{write, Buffer, FormatResult};
-use ruff_python_ast::node::AnyNodeRef;
+use ruff_python_ast::node::{AnyNodeRef, AstNode};
 use rustpython_parser::ast::ExprLambda;
 
 #[derive(Default)]
@@ -19,7 +20,17 @@ impl FormatNodeRule<ExprLambda> for FormatExprLambda {
             body,
         } = item;
 
+        // It's possible for some `Arguments` of `lambda`s to be assigned dangling comments.
+        //
+        // a = (
+        //     lambda  # Dangling
+        //     : 1
+        // )
+        let comments = f.context().comments().clone();
+        let dangling = comments.dangling_comments(args.as_any_node_ref());
+
         write!(f, [text("lambda")])?;
+
         if !args.args.is_empty() {
             write!(
                 f,
@@ -30,7 +41,16 @@ impl FormatNodeRule<ExprLambda> for FormatExprLambda {
                 ]
             )?;
         }
-        write!(f, [text(":"), space(), body.format()])
+
+        write!(
+            f,
+            [
+                text(":"),
+                space(),
+                body.format(),
+                dangling_comments(dangling)
+            ]
+        )
     }
 }
 
