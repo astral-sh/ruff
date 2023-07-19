3ddff0b9f51beaf88cd1b3907cc4c82f98c5d908
Chris Pryer <cnpryer@gmail.com>
2023-07-19 07:56:06 -0400

    Remove TODO

--- crates/ruff_python_formatter/src/expression/expr_tuple.rs
+++ crates/ruff_python_formatter/src/expression/expr_tuple.rs
@@ -14,7 +14,6 @@ use crate::prelude::*;
 
 #[derive(Eq, PartialEq, Debug, Default)]
 pub enum TupleParentheses {
-    /// TODO(cnpryer): Why not `Never`?
     /// Black omits parentheses for tuples inside of comprehensions.
     Comprehension,
 
