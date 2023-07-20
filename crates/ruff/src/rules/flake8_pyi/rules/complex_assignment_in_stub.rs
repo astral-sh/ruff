3ac97b23962315eb186442453c2c26085682f771
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-20 12:29:00 -0400

    Tweak docs

--- crates/ruff/src/rules/flake8_pyi/rules/complex_assignment_in_stub.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/complex_assignment_in_stub.rs
@@ -6,13 +6,17 @@ use ruff_macros::{derive_message_formats, violation};
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for assignments with multiple or non-name targets
+/// Checks for assignments with multiple or non-name targets in stub files.
 ///
 /// ## Why is this bad?
-/// Stub files are not intended to ever be executed. As such, it's useful to enforce that only a
-/// subset of Python syntax is allowed in a stub file, to ensure that everything in the stub is
-/// 100% unambiguous when it comes to how the type checker is supposed to interpret it. Only
-/// allowing simple assignments is one such restriction.
+/// In general, stub files should be thought of as "data files" for a type
+/// checker, and are not intended to be executed. As such, it's useful to
+/// enforce that only a subset of Python syntax is allowed in a stub file, to
+/// ensure that everything in the stub is unambiguous for the type checker.
+///
+/// The need to perform multi-assignment, or assignment to a non-name target,
+/// likely indicates a misunderstanding of how stub files are intended to be
+/// used.
 ///
 /// ## Example
 /// ```python
