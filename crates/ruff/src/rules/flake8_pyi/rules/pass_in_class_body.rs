dbe5f8ec7c28ac1117a753eb6fc5dba6de9e17a5
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-20 22:36:13 -0400

    Remove some unnecessary lifetime annotations

--- crates/ruff/src/rules/flake8_pyi/rules/pass_in_class_body.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/pass_in_class_body.rs
@@ -22,11 +22,7 @@ impl AlwaysAutofixableViolation for PassInClassBody {
 }
 
 /// PYI012
-pub(crate) fn pass_in_class_body<'a>(
-    checker: &mut Checker<'a>,
-    parent: &'a Stmt,
-    body: &'a [Stmt],
-) {
+pub(crate) fn pass_in_class_body(checker: &mut Checker, parent: &Stmt, body: &[Stmt]) {
     // `pass` is required in these situations (or handled by `pass_statement_stub_body`).
     if body.len() < 2 {
         return;
