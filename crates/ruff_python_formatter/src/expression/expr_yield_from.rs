76bc00f4545265605e2237f95c3523038553963a
qdegraaf <querijndegraaf@gmail.com>
2023-07-21 12:51:21 +0200

    Add comments to `needs_parentheses()` funcs

--- crates/ruff_python_formatter/src/expression/expr_yield_from.rs
+++ crates/ruff_python_formatter/src/expression/expr_yield_from.rs
@@ -33,6 +33,12 @@ impl NeedsParentheses for ExprYieldFrom {
         parent: AnyNodeRef,
         _context: &PyFormatContext,
     ) -> OptionalParentheses {
+        // According to https://docs.python.org/3/reference/grammar.html There are two situations
+        // where we do not want to always parenthesize a yield expression:
+        //  1. Right hand side of an assignment, e.g. `x = yield y`
+        //  2. Yield statement, e.g. `def foo(): yield y`
+        // We catch situation 1 below. Situation 2 does not need to be handled here as
+        // FormatStmtExpr, does not add parenthesis
         if parent.is_stmt_assign() || parent.is_stmt_ann_assign() || parent.is_stmt_aug_assign() {
             OptionalParentheses::Multiline
         } else {
