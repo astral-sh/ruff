265fa70ba74e0a904dacf99b08fcf3a5327318a0
Simon Brugman <sbrugman@users.noreply.github.com>
2023-07-22 14:44:09 +0200

    Add test case

--- crates/ruff/resources/test/fixtures/flake8_use_pathlib/PTH207.py
+++ crates/ruff/resources/test/fixtures/flake8_use_pathlib/PTH207.py
@@ -7,4 +7,5 @@ extensions_dir = "./extensions"
 
 # PTH207
 glob.glob(os.path.join(extensions_dir, "ops", "autograd", "*.cpp"))
+list(glob.iglob(os.path.join(extensions_dir, "ops", "autograd", "*.cpp")))
 search("*.png")
