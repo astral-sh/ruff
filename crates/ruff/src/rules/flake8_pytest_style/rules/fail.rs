941bba89e844cd6006d69eb684a05d07ab4f41f0
Harutaka Kawamura <hkawamura0130@gmail.com>
2023-07-23 16:30:21 +0900

    Apply suggestions from code review
    Co-authored-by: Dhruv Manilawala <dhruvmanila@gmail.com>

--- crates/ruff/src/rules/flake8_pytest_style/rules/fail.rs
+++ crates/ruff/src/rules/flake8_pytest_style/rules/fail.rs
@@ -9,7 +9,7 @@ use crate::checkers::ast::Checker;
 use super::helpers::{is_empty_or_null_string, is_pytest_fail};
 
 /// ## What it does
-/// Checks for ``pytest.fail`` calls without a message.
+/// Checks for `pytest.fail` calls without a message.
 ///
 /// ## Why is this bad?
 /// `pytest.fail` calls without a message make it harder to understand and debug test failures.
@@ -45,7 +45,7 @@ use super::helpers::{is_empty_or_null_string, is_pytest_fail};
 /// ```
 ///
 /// ## References
-/// - [API Reference: pytest.fail](https://docs.pytest.org/en/latest/reference/reference.html#pytest-fail)
+/// - [API Reference: `pytest.fail`](https://docs.pytest.org/en/latest/reference/reference.html#pytest-fail)
 #[violation]
 pub struct PytestFailWithoutMessage;
 
