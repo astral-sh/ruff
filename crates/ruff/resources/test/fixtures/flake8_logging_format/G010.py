7605082ef952380240e65bb8ce1d27b2b690d046
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-24 00:51:08 -0400

    Fix logging rules with whitespace around dot

--- crates/ruff/resources/test/fixtures/flake8_logging_format/G010.py
+++ crates/ruff/resources/test/fixtures/flake8_logging_format/G010.py
@@ -6,3 +6,5 @@ from logging_setup import logger
 logging.warn("Hello World!")
 log.warn("Hello world!")  # This shouldn't be considered as a logger candidate
 logger.warn("Hello world!")
+
+logging . warn("Hello World!")
