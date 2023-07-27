323074667b9fc852c6c0e115928e46ae30b6d440
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-27 14:19:19 -0400

    Make nursery

--- crates/ruff/src/rules/pylint/rules/eq_without_hash.rs
+++ crates/ruff/src/rules/pylint/rules/eq_without_hash.rs
@@ -1,4 +1,4 @@
-use rustpython_parser::ast::{self, Ranged, Stmt};
+use ruff_python_ast::{self as ast, Ranged, Stmt};
 
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
@@ -6,11 +6,16 @@ use ruff_macros::{derive_message_formats, violation};
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks that a class that implements a `__eq__` also implements `__hash__`.
+/// Checks for classes that implement `__eq__` but not `__hash__`.
 ///
 /// ## Why is this bad?
-/// A class that overrides eq() and does not define hash() will have its hash()
-/// implicitly set to None.
+/// A class that implements `__eq__` but not `__hash__` will have its hash
+/// method implicitly set to `None`. This will cause the class to be
+/// unhashable, will in turn cause issues when using the class as a key in a
+/// dictionary or a member of a set.
+///
+/// ## Known problems
+/// Does not check for `__hash__` implementations in superclasses.
 ///
 /// ## Example
 /// ```python
@@ -49,26 +54,24 @@ pub(crate) fn object_without_hash_method(
     checker: &mut Checker,
     ast::StmtClassDef { name, body, .. }: &ast::StmtClassDef,
 ) {
-    if !has_eq_without_hash(body) {
-        return;
+    if has_eq_without_hash(body) {
+        checker
+            .diagnostics
+            .push(Diagnostic::new(EqWithoutHash, name.range()));
     }
-    checker
-        .diagnostics
-        .push(Diagnostic::new(EqWithoutHash, name.range()));
 }
 
 fn has_eq_without_hash(body: &[Stmt]) -> bool {
     let mut has_hash = false;
     let mut has_eq = false;
-
     for statement in body {
         let Stmt::FunctionDef(ast::StmtFunctionDef { name, .. }) = statement else {
             continue;
         };
-        if name == "__hash__" {
-            has_hash = true;
-        } else if name == "__eq__" {
-            has_eq = true;
+        match name.as_str() {
+            "__hash__" => has_hash = true,
+            "__eq__" => has_eq = true,
+            _ => {}
         }
     }
     has_eq && !has_hash
