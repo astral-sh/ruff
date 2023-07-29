d67178fd457c80b58ee15c7cd7147625d99ac630
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-29 09:01:39 -0400

    Break global and nonlocal statements over continuation lines

--- crates/ruff_python_formatter/src/statement/stmt_global.rs
+++ crates/ruff_python_formatter/src/statement/stmt_global.rs
@@ -1,4 +1,5 @@
 use ruff_formatter::{format_args, write};
+use ruff_python_ast::node::AstNode;
 use ruff_python_ast::StmtGlobal;
 
 use crate::prelude::*;
@@ -9,10 +10,44 @@ pub struct FormatStmtGlobal;
 
 impl FormatNodeRule<StmtGlobal> for FormatStmtGlobal {
     fn fmt_fields(&self, item: &StmtGlobal, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(f, [text("global"), space()])?;
+        // Join the `global` names, breaking across continuation lines if necessary, unless the
+        // `global` statement has a trailing comment, in which case, breaking the names would
+        // move the comment "off" of the `global` statement.
+        if f.context()
+            .comments()
+            .has_trailing_comments(item.as_any_node_ref())
+        {
+            let joined = format_with(|f| {
+                f.join_with(format_args![text(","), space()])
+                    .entries(item.names.iter().formatted())
+                    .finish()
+            });
 
-        f.join_with(format_args![text(","), space()])
-            .entries(item.names.iter().formatted())
-            .finish()
+            write!(f, [text("global"), space(), &joined])
+        } else {
+            let joined = format_with(|f| {
+                f.join_with(&format_args![
+                    text(","),
+                    space(),
+                    if_group_breaks(&text("\\")),
+                    soft_line_break(),
+                ])
+                .entries(item.names.iter().formatted())
+                .finish()
+            });
+
+            write!(
+                f,
+                [
+                    text("global"),
+                    space(),
+                    &group(&format_args!(
+                        if_group_breaks(&text("\\")),
+                        soft_line_break(),
+                        &soft_block_indent(&joined)
+                    ))
+                ]
+            )
+        }
     }
 }
--- crates/ruff_python_formatter/src/statement/stmt_nonlocal.rs
+++ crates/ruff_python_formatter/src/statement/stmt_nonlocal.rs
@@ -1,4 +1,5 @@
 use ruff_formatter::{format_args, write};
+use ruff_python_ast::node::AstNode;
 use ruff_python_ast::StmtNonlocal;
 
 use crate::prelude::*;
@@ -9,10 +10,44 @@ pub struct FormatStmtNonlocal;
 
 impl FormatNodeRule<StmtNonlocal> for FormatStmtNonlocal {
     fn fmt_fields(&self, item: &StmtNonlocal, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(f, [text("nonlocal"), space()])?;
+        // Join the `nonlocal` names, breaking across continuation lines if necessary, unless the
+        // `nonlocal` statement has a trailing comment, in which case, breaking the names would
+        // move the comment "off" of the `nonlocal` statement.
+        if f.context()
+            .comments()
+            .has_trailing_comments(item.as_any_node_ref())
+        {
+            let joined = format_with(|f| {
+                f.join_with(format_args![text(","), space()])
+                    .entries(item.names.iter().formatted())
+                    .finish()
+            });
 
-        f.join_with(format_args![text(","), space()])
-            .entries(item.names.iter().formatted())
-            .finish()
+            write!(f, [text("nonlocal"), space(), &joined])
+        } else {
+            let joined = format_with(|f| {
+                f.join_with(&format_args![
+                    text(","),
+                    space(),
+                    if_group_breaks(&text("\\")),
+                    soft_line_break(),
+                ])
+                .entries(item.names.iter().formatted())
+                .finish()
+            });
+
+            write!(
+                f,
+                [
+                    text("nonlocal"),
+                    space(),
+                    &group(&format_args!(
+                        if_group_breaks(&text("\\")),
+                        soft_line_break(),
+                        &soft_block_indent(&joined)
+                    ))
+                ]
+            )
+        }
     }
 }
