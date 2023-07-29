76741cac77111d6243f12706d1f1f565e0337527
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-07-29 10:39:42 -0400

    Add `global` and `nonlocal` formatting (#6170)
    ## Summary
    
    Adds `global` and `nonlocal` formatting, without the "deviation from
    black" outlined in the linked issue, which I'll do separately.
    
    See: https://github.com/astral-sh/ruff/issues/4798.
    
    ## Test Plan
    
    Added a fixture in the Ruff-specific directory since the Black fixtures
    don't seem to cover this.

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/nonlocal.py
@@ -0,0 +1,12 @@
+def f():
+    nonlocal x, y, z
+
+
+def f():
+    # leading comment
+    nonlocal x, y, z # end-of-line comment
+    # trailing comment
+
+
+def f():
+    nonlocal analyze_featuremap_layer, analyze_featuremapcompression_layer, analyze_latencies_post, analyze_motions_layer, analyze_size_model
