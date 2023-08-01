0b98920c6a661f812f85e2db88501b9b38558ef5
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-08-01 00:13:02 -0400

    Tweak logic

--- crates/ruff/src/rules/flake8_pyi/rules/redundant_literal_union.rs
+++ crates/ruff/src/rules/flake8_pyi/rules/redundant_literal_union.rs
@@ -1,9 +1,11 @@
+use rustc_hash::FxHashSet;
+use std::fmt;
+
 use ast::{Constant, Ranged};
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::{self as ast, Expr};
-use rustc_hash::FxHashSet;
-use smallvec::SmallVec;
+use ruff_python_semantic::SemanticModel;
 
 use crate::{checkers::ast::Checker, rules::flake8_pyi::helpers::traverse_union};
 
@@ -12,22 +14,26 @@ use crate::{checkers::ast::Checker, rules::flake8_pyi::helpers::traverse_union};
 /// types in an union.
 ///
 /// ## Why is this bad?
+/// The use of `Literal` types in a union with the builtin super type of one of
+/// its literal members is redundant, as the super type is strictly more
+/// general than the `Literal` type.
 ///
+/// For example, `Literal["A"] | str` is equivalent to `str`, and
+/// `Literal[1] | int` is equivalent to `int`, as `str` and `int` are the super
+/// types of `"A"` and `1` respectively.
 ///
 /// ## Example
 /// ```python
-/// from typing import Literal, Union
+/// from typing import Literal
 ///
-/// Redundancy: Literal["foo"] | str
-/// Redundancy2: Union[Literal[2], int]
+/// A: Literal["A"] | str
 /// ```
 ///
 /// Use instead:
 /// ```python
-/// from typing import Literal, TypeAlias
+/// from typing import Literal
 ///
-/// WithoutRedundancy: Literal["foo"]
-/// WithoutRedundancy2: Literal[2]
+/// A: Literal["A"]
 /// ```
 #[violation]
 pub struct RedundantLiteralUnion {
@@ -42,29 +48,30 @@ impl Violation for RedundantLiteralUnion {
             literal,
             builtin_type,
         } = self;
-        format!(
-            "`Literal[{literal}]` is redundant in an union with `{}`",
-            builtin_type.as_str()
-        )
+        format!("`Literal[{literal}]` is redundant in a union with `{builtin_type}`",)
     }
 }
 
 /// PYI051
 pub(crate) fn redundant_literal_union<'a>(checker: &mut Checker, union: &'a Expr) {
-    let mut literal_exprs = SmallVec::<[&Expr; 1]>::new();
+    let mut literal_exprs = Vec::new();
     let mut builtin_types_in_union = FxHashSet::default();
 
-    // Adds a member to `literal_exprs` if it is a `Literal` annotation and
-    // adds the type of `expr` to `builtin_types_in_union`.
+    // Adds a member to `literal_exprs` for each value in a `Literal`, and any builtin types
+    // to `builtin_types_in_union`.
     let mut func = |expr: &'a Expr, _| {
         if let Expr::Subscript(ast::ExprSubscript { value, slice, .. }) = expr {
             if checker.semantic().match_typing_expr(value, "Literal") {
-                literal_exprs.push(slice);
+                if let Expr::Tuple(ast::ExprTuple { elts, .. }) = slice.as_ref() {
+                    literal_exprs.extend(elts.iter());
+                } else {
+                    literal_exprs.push(slice);
+                }
             }
             return;
         }
 
-        let Some(builtin_type) = get_expr_type(expr) else {
+        let Some(builtin_type) = match_builtin_type(expr, checker.semantic()) else {
             return;
         };
         builtin_types_in_union.insert(builtin_type);
@@ -73,85 +80,19 @@ pub(crate) fn redundant_literal_union<'a>(checker: &mut Checker, union: &'a Expr
     traverse_union(&mut func, checker.semantic(), union, None);
 
     for literal_expr in literal_exprs {
-        if let Expr::Tuple(ast::ExprTuple { elts, .. }) = literal_expr {
-            handle_literal_with_multiple_members(checker, literal_expr, elts);
-            continue;
-        }
-
-        let Some(literal_type) = get_expr_type(literal_expr) else {
+        let Some(constant_type) = match_constant_type(literal_expr) else {
             continue;
         };
 
-        if builtin_types_in_union.contains(&literal_type) {
+        if builtin_types_in_union.contains(&constant_type) {
             checker.diagnostics.push(Diagnostic::new(
                 RedundantLiteralUnion {
                     literal: checker.locator().slice(literal_expr.range()).to_string(),
-                    builtin_type: literal_type,
+                    builtin_type: constant_type,
                 },
                 literal_expr.range(),
             ));
         }
-    }
-}
-
-/// Handles redundancy for `Literal`s with multiple members.
-///
-/// For example:
-/// ```python
-/// from typing import TypeAlias, Union, Literal
-///
-/// A: TypeAlias = Union[Literal["foo", "bar"], str]
-/// ```
-/// This function allows to show in the diagnostic the full expression,
-/// e.g. `Literal["foo", "bar"]`, if its members are all of the same type.
-///
-/// If the members of `Literal` have different types like in the following,
-/// example:
-/// ```python
-/// from typing import TypeAlias, Union, Literal
-///
-/// A: TypeAlias = Union[Literal[b"foo", "bar"], str]
-/// ```
-/// Only the `Literal` with the redundancy will be shown, e.g. `Literal["bar"]`.
-fn handle_literal_with_multiple_members(
-    checker: &mut Checker,
-    literal: &Expr,
-    literal_members: &[Expr],
-) {
-    let literal_members_types: Vec<ExprType> =
-        literal_members.iter().filter_map(get_expr_type).collect();
-
-    // Check if all `ExprType`s in `literal_members_types` is the same,
-    // if it's true, handle `Literal` with members of the same type
-    let literal_type = literal_members_types[0];
-    if literal_members_types
-        .iter()
-        .all(|&element_type| element_type == literal_type)
-    {
-        checker.diagnostics.push(Diagnostic::new(
-            RedundantLiteralUnion {
-                literal: checker.locator().slice(literal.range()).to_string(),
-                builtin_type: literal_type,
-            },
-            literal.range(),
-        ));
-
-        return;
-    }
-
-    // Handle `Literal` with different members types
-    for literal_member in literal_members {
-        let Some(literal_type) = get_expr_type(literal_member) else {
-            continue;
-        };
-
-        checker.diagnostics.push(Diagnostic::new(
-            RedundantLiteralUnion {
-                literal: checker.locator().slice(literal_member.range()).to_string(),
-                builtin_type: literal_type,
-            },
-            literal_member.range(),
-        ));
     }
 }
 
@@ -165,46 +106,50 @@ enum ExprType {
     Complex,
 }
 
-impl ExprType {
-    fn as_str(&self) -> &str {
+impl fmt::Display for ExprType {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            ExprType::Int => "int",
-            ExprType::Str => "str",
-            ExprType::Bool => "bool",
-            ExprType::Float => "float",
-            ExprType::Bytes => "bytes",
-            ExprType::Complex => "complex",
+            Self::Int => fmt.write_str("int"),
+            Self::Str => fmt.write_str("str"),
+            Self::Bool => fmt.write_str("bool"),
+            Self::Float => fmt.write_str("float"),
+            Self::Bytes => fmt.write_str("bytes"),
+            Self::Complex => fmt.write_str("complex"),
         }
     }
 }
 
-/// Return type of `expr` if it is a literal type ( e.g. `1`, `b"bytes"`, `"str"`,
-/// `True`, `3.14` and `1J`) or it is a builtin type (e.g. `int`, `bool`, `float`, `str`,
-/// `bytes` and `complex`).
-fn get_expr_type(expr: &Expr) -> Option<ExprType> {
-    if let Expr::Constant(ast::ExprConstant { value, .. }) = expr {
-        return Some(match value {
-            Constant::Bool(_) => ExprType::Bool,
-            Constant::Str(_) => ExprType::Str,
-            Constant::Bytes(_) => ExprType::Bytes,
-            Constant::Int(_) => ExprType::Int,
-            Constant::Float(_) => ExprType::Float,
-            Constant::Complex { .. } => ExprType::Complex,
-            _ => return None,
-        });
+/// Return the [`ExprType`] of an [`Expr]` if it is a builtin type (e.g. `int`, `bool`, `float`,
+/// `str`, `bytes`, or `complex`).
+fn match_builtin_type(expr: &Expr, model: &SemanticModel) -> Option<ExprType> {
+    let name = expr.as_name_expr()?;
+    let result = match name.id.as_str() {
+        "int" => ExprType::Int,
+        "bool" => ExprType::Bool,
+        "str" => ExprType::Str,
+        "float" => ExprType::Float,
+        "bytes" => ExprType::Bytes,
+        "complex" => ExprType::Complex,
+        _ => return None,
+    };
+    if !model.is_builtin(name.id.as_str()) {
+        return None;
     }
+    Some(result)
+}
 
-    if let Expr::Name(ast::ExprName { id, .. }) = expr {
-        return Some(match id.as_str() {
-            "int" => ExprType::Int,
-            "bool" => ExprType::Bool,
-            "str" => ExprType::Str,
-            "float" => ExprType::Float,
-            "bytes" => ExprType::Bytes,
-            "complex" => ExprType::Complex,
-            _ => return None,
-        });
-    }
-
-    None
+/// Return the [`ExprType`] of an [`Expr]` if it is a constant (e.g., an `int`, like `1`, or a
+/// `bool`, like `True`).
+fn match_constant_type(expr: &Expr) -> Option<ExprType> {
+    let constant = expr.as_constant_expr()?;
+    let result = match constant.value {
+        Constant::Bool(_) => ExprType::Bool,
+        Constant::Str(_) => ExprType::Str,
+        Constant::Bytes(_) => ExprType::Bytes,
+        Constant::Int(_) => ExprType::Int,
+        Constant::Float(_) => ExprType::Float,
+        Constant::Complex { .. } => ExprType::Complex,
+        _ => return None,
+    };
+    Some(result)
 }
