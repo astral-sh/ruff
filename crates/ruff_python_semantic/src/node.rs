8a5bc93fdd621aec745b6b86f7a4b880a5342625
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-08-03 23:57:15 -0400

    Make the `Nodes` vector generic on node type (#6328)

--- crates/ruff_python_semantic/src/node.rs
+++ crates/ruff_python_semantic/src/node.rs
@@ -1,25 +1,24 @@
 use std::ops::{Index, IndexMut};
 
 use ruff_index::{newtype_index, IndexVec};
-use ruff_python_ast::Stmt;
 use rustc_hash::FxHashMap;
 
 use ruff_python_ast::types::RefEquality;
 
-/// Id uniquely identifying a statement in a program.
+/// Id uniquely identifying an AST node in a program.
 ///
 /// Using a `u32` is sufficient because Ruff only supports parsing documents with a size of max `u32::max`
-/// and it is impossible to have more statements than characters in the file. We use a `NonZeroU32` to
+/// and it is impossible to have more nodes than characters in the file. We use a `NonZeroU32` to
 /// take advantage of memory layout optimizations.
 #[newtype_index]
 #[derive(Ord, PartialOrd)]
 pub struct NodeId;
 
-/// A [`Node`] represents a statement in a program, along with a pointer to its parent (if any).
+/// A [`Node`] represents an AST node in a program, along with a pointer to its parent (if any).
 #[derive(Debug)]
-struct Node<'a> {
-    /// The statement this node represents.
-    stmt: &'a Stmt,
+struct Node<'a, T> {
+    /// A pointer to the AST node.
+    node: &'a T,
     /// The ID of the parent of this node, if any.
     parent: Option<NodeId>,
     /// The depth of this node in the tree.
@@ -27,23 +26,32 @@ struct Node<'a> {
 }
 
 /// The nodes of a program indexed by [`NodeId`]
-#[derive(Debug, Default)]
-pub struct Nodes<'a> {
-    nodes: IndexVec<NodeId, Node<'a>>,
-    node_to_id: FxHashMap<RefEquality<'a, Stmt>, NodeId>,
+#[derive(Debug)]
+pub struct Nodes<'a, T> {
+    nodes: IndexVec<NodeId, Node<'a, T>>,
+    node_to_id: FxHashMap<RefEquality<'a, T>, NodeId>,
 }
 
-impl<'a> Nodes<'a> {
+impl<'a, T> Default for Nodes<'a, T> {
+    fn default() -> Self {
+        Self {
+            nodes: IndexVec::default(),
+            node_to_id: FxHashMap::default(),
+        }
+    }
+}
+
+impl<'a, T> Nodes<'a, T> {
     /// Inserts a new node into the node tree and returns its unique id.
     ///
     /// Panics if a node with the same pointer already exists.
-    pub(crate) fn insert(&mut self, stmt: &'a Stmt, parent: Option<NodeId>) -> NodeId {
+    pub(crate) fn insert(&mut self, node: &'a T, parent: Option<NodeId>) -> NodeId {
         let next_id = self.nodes.next_index();
-        if let Some(existing_id) = self.node_to_id.insert(RefEquality(stmt), next_id) {
+        if let Some(existing_id) = self.node_to_id.insert(RefEquality(node), next_id) {
             panic!("Node already exists with id {existing_id:?}");
         }
         self.nodes.push(Node {
-            stmt,
+            node,
             parent,
             depth: parent.map_or(0, |parent| self.nodes[parent].depth + 1),
         })
@@ -51,7 +59,7 @@ impl<'a> Nodes<'a> {
 
     /// Returns the [`NodeId`] of the given node.
     #[inline]
-    pub fn node_id(&self, node: &'a Stmt) -> Option<NodeId> {
+    pub fn node_id(&self, node: &'a T) -> Option<NodeId> {
         self.node_to_id.get(&RefEquality(node)).copied()
     }
 
@@ -62,7 +70,7 @@ impl<'a> Nodes<'a> {
     }
 
     /// Return the parent of the given node.
-    pub fn parent(&self, node: &'a Stmt) -> Option<&'a Stmt> {
+    pub fn parent(&self, node: &'a T) -> Option<&'a T> {
         let node_id = self.node_to_id.get(&RefEquality(node))?;
         let parent_id = self.nodes[*node_id].parent?;
         Some(self[parent_id])
@@ -80,18 +88,18 @@ impl<'a> Nodes<'a> {
     }
 }
 
-impl<'a> Index<NodeId> for Nodes<'a> {
-    type Output = &'a Stmt;
+impl<'a, T> Index<NodeId> for Nodes<'a, T> {
+    type Output = &'a T;
 
     #[inline]
     fn index(&self, index: NodeId) -> &Self::Output {
-        &self.nodes[index].stmt
+        &self.nodes[index].node
     }
 }
 
-impl<'a> IndexMut<NodeId> for Nodes<'a> {
+impl<'a, T> IndexMut<NodeId> for Nodes<'a, T> {
     #[inline]
     fn index_mut(&mut self, index: NodeId) -> &mut Self::Output {
-        &mut self.nodes[index].stmt
+        &mut self.nodes[index].node
     }
 }
