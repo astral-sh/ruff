87a54fdb8eaa65c2e05750dfd767d8337de5e0c5
Tom Kuson <mail@tjkuson.me>
2023-08-11 14:18:00 +0100

    Check stub file fixtures

--- crates/ruff_python_formatter/tests/fixtures.rs
+++ crates/ruff_python_formatter/tests/fixtures.rs
@@ -161,7 +161,7 @@ fn format() {
         });
     };
 
-    insta::glob!("../resources", "test/fixtures/ruff/**/*.py", test_file);
+    insta::glob!("../resources", "test/fixtures/ruff/**/*.py*", test_file);
 }
 
 /// Format another time and make sure that there are no changes anymore
