f165f8305552537983b448c000d0c8bc19630192
Tom Kuson <mail@tjkuson.me>
2023-08-11 14:14:47 +0100

    Add tests

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/top_level.py
@@ -0,0 +1,14 @@
+class A:
+    def __init__(self):
+        pass
+
+class B:
+    def __init__(self):
+        pass
+
+def foo():
+    pass
+
+class Del(expr_context): ...
+class Load(expr_context): ...
+class Store(expr_context): ...
---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/top_level.pyi
@@ -0,0 +1,24 @@
+class A:
+    def __init__(self):
+        pass
+
+
+class B:
+    def __init__(self):
+        pass
+
+
+def foo():
+    pass
+
+
+class Del(expr_context):
+    ...
+
+
+class Load(expr_context):
+    ...
+
+
+class Store(expr_context):
+    ...
