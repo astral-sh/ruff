da3af6e0829b6c3022eb62f62697aa02683f8d78
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-08-14 13:32:55 -0400

    Clarify FBT documentation and refine rule names

--- crates/ruff/src/rules/flake8_boolean_trap/rules/boolean_default_value_positional_argument.rs
+++ crates/ruff/src/rules/flake8_boolean_trap/rules/boolean_default_value_positional_argument.rs
@@ -1,32 +1,34 @@
-use ruff_python_ast::{
-    self as ast, Constant, Decorator, Expr, ParameterWithDefault, Parameters, Ranged,
-};
-
-use ruff_diagnostics::Diagnostic;
-use ruff_diagnostics::Violation;
+use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::call_path::collect_call_path;
+use ruff_python_ast::{Decorator, ParameterWithDefault, Parameters, Ranged};
 
 use crate::checkers::ast::Checker;
-use crate::rules::flake8_boolean_trap::helpers::is_allowed_func_def;
+use crate::rules::flake8_boolean_trap::helpers::{is_allowed_func_def, is_boolean};
 
 /// ## What it does
-/// Checks for boolean positional arguments in function definitions.
+/// Checks for the use of boolean positional arguments in function definitions,
+/// as determined by the presence of a boolean default value.
 ///
 /// ## Why is this bad?
 /// Calling a function with boolean positional arguments is confusing as the
-/// meaning of the boolean value is not clear to the caller, and to future
+/// meaning of the boolean value is not clear to the caller and to future
 /// readers of the code.
 ///
 /// The use of a boolean will also limit the function to only two possible
 /// behaviors, which makes the function difficult to extend in the future.
+///
+/// Instead, consider refactoring into separate implementations for the
+/// `True` and `False` cases, using an `Enum`, or making the argument a
+/// keyword-only argument, to force callers to be explicit when providing
+/// the argument.
 ///
 /// ## Example
 /// ```python
 /// from math import ceil, floor
 ///
 ///
-/// def round_number(number: float, up: bool) -> int:
+/// def round_number(number, up=True):
 ///     return ceil(number) if up else floor(number)
 ///
 ///
@@ -39,11 +41,11 @@ use crate::rules::flake8_boolean_trap::helpers::is_allowed_func_def;
 /// from math import ceil, floor
 ///
 ///
-/// def round_up(number: float) -> int:
+/// def round_up(number):
 ///     return ceil(number)
 ///
 ///
-/// def round_down(number: float) -> int:
+/// def round_down(number):
 ///     return floor(number)
 ///
 ///
@@ -61,24 +63,37 @@ use crate::rules::flake8_boolean_trap::helpers::is_allowed_func_def;
 ///     DOWN = 2
 ///
 ///
-/// def round_number(value: float, method: RoundingMethod) -> float:
+/// def round_number(value, method):
 ///     ...
+/// ```
+///
+/// Or, make the argument a keyword-only argument:
+/// ```python
+/// from math import ceil, floor
+///
+///
+/// def round_number(number, *, up=True):
+///     return ceil(number) if up else floor(number)
+///
+///
+/// round_number(1.5, up=True)
+/// round_number(1.5, up=False)
 /// ```
 ///
 /// ## References
 /// - [Python documentation: Calls](https://docs.python.org/3/reference/expressions.html#calls)
 /// - [_How to Avoid “The Boolean Trap”_ by Adam Johnson](https://adamj.eu/tech/2021/07/10/python-type-hints-how-to-avoid-the-boolean-trap/)
 #[violation]
-pub struct BooleanPositionalArgInFunctionDefinition;
+pub struct BooleanDefaultValuePositionalArgument;
 
-impl Violation for BooleanPositionalArgInFunctionDefinition {
+impl Violation for BooleanDefaultValuePositionalArgument {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Boolean positional arg in function definition")
+        format!("Boolean default positional argument in function definition")
     }
 }
 
-pub(crate) fn check_positional_boolean_in_def(
+pub(crate) fn boolean_default_value_positional_argument(
     checker: &mut Checker,
     name: &str,
     decorator_list: &[Decorator],
@@ -97,32 +112,15 @@ pub(crate) fn check_positional_boolean_in_def(
 
     for ParameterWithDefault {
         parameter,
-        default: _,
+        default,
         range: _,
     } in parameters.posonlyargs.iter().chain(&parameters.args)
     {
-        if parameter.annotation.is_none() {
-            continue;
+        if default.as_ref().is_some_and(|default| is_boolean(default)) {
+            checker.diagnostics.push(Diagnostic::new(
+                BooleanDefaultValuePositionalArgument,
+                parameter.name.range(),
+            ));
         }
-        let Some(expr) = &parameter.annotation else {
-            continue;
-        };
-
-        // check for both bool (python class) and 'bool' (string annotation)
-        let hint = match expr.as_ref() {
-            Expr::Name(name) => &name.id == "bool",
-            Expr::Constant(ast::ExprConstant {
-                value: Constant::Str(ast::StringConstant { value, .. }),
-                ..
-            }) => value == "bool",
-            _ => false,
-        };
-        if !hint {
-            continue;
-        }
-        checker.diagnostics.push(Diagnostic::new(
-            BooleanPositionalArgInFunctionDefinition,
-            parameter.range(),
-        ));
     }
 }
