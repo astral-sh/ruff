e3ecbe660e97b1647c93cc0d40795cee99b00ae8
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-08-16 22:13:05 -0500

    [`ruff`] Implement `quadratic-list-summation` rule (`RUF017`) (#6489)
    ## Summary
    
    Adds `RUF017`. Closes #5073 
    
    ## Test Plan
    
    `cargo t`

--- crates/ruff/src/codes.rs
+++ crates/ruff/src/codes.rs
@@ -816,6 +816,7 @@ pub fn code_to_rule(linter: Linter, code: &str) -> Option<(RuleGroup, Rule)> {
         (Ruff, "014") => (RuleGroup::Nursery, rules::ruff::rules::UnreachableCode),
         (Ruff, "015") => (RuleGroup::Unspecified, rules::ruff::rules::UnnecessaryIterableAllocationForFirstElement),
         (Ruff, "016") => (RuleGroup::Unspecified, rules::ruff::rules::InvalidIndexType),
+        (Ruff, "017") => (RuleGroup::Nursery, rules::ruff::rules::QuadraticListSummation),
         (Ruff, "100") => (RuleGroup::Unspecified, rules::ruff::rules::UnusedNOQA),
         (Ruff, "200") => (RuleGroup::Unspecified, rules::ruff::rules::InvalidPyprojectToml),
 
