68fe3f1522a40633de03786d4d0c7ec723b20154
Tom Kuson <mail@tjkuson.me>
2023-08-18 15:45:46 +0100

    Expand tests

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/ellipsis.pyi
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/ellipsis.pyi
@@ -3,15 +3,48 @@
 while True:
     ...
 
+if True:
+    # comment
+    ...
+
+with True:
+    ...  # comment
+
 for i in []:
     ...
+
+for i in []:
+    # comment
+    ...
+
+for i in []:
+    ... # comment
 
 if True:
     ...
 
+if True:
+    # comment
+    ...
+
+if True:
+    ... # comment
+
 with True:
     ...
+
+with True:
+    # comment
+    ...
+
+with True:
+    ... # comment
 
 match x:
     case 1:
         ...
+    case 2:
+        # comment
+        ...
+    case 3:
+        ... # comment
