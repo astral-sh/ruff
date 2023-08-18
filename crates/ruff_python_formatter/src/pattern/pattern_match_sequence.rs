b9514b31fabf5f5c998ca84e669b3722c793f0d8
harupy <hkawamura0130@gmail.com>
2023-08-18 23:35:28 +0900

    Format FormatPatternSequence

--- crates/ruff_python_formatter/src/pattern/pattern_match_sequence.rs
+++ crates/ruff_python_formatter/src/pattern/pattern_match_sequence.rs
@@ -1,19 +1,30 @@
-use ruff_formatter::{write, Buffer, FormatResult};
+use ruff_formatter::prelude::format_with;
+use ruff_formatter::Format;
+use ruff_formatter::FormatResult;
 use ruff_python_ast::PatternMatchSequence;
 
-use crate::{not_yet_implemented_custom_text, FormatNodeRule, PyFormatter};
+use crate::builders::PyFormatterExtensions;
+use crate::expression::parentheses::{empty_parenthesized, parenthesized};
+use crate::{FormatNodeRule, PyFormatter};
 
 #[derive(Default)]
 pub struct FormatPatternMatchSequence;
 
 impl FormatNodeRule<PatternMatchSequence> for FormatPatternMatchSequence {
     fn fmt_fields(&self, item: &PatternMatchSequence, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(
-            f,
-            [not_yet_implemented_custom_text(
-                "[NOT_YET_IMPLEMENTED_PatternMatchSequence, 2]",
-                item
-            )]
-        )
+        let PatternMatchSequence { patterns, range } = item;
+        let comments = f.context().comments().clone();
+        let dangling = comments.dangling(item);
+        if patterns.is_empty() {
+            return empty_parenthesized("[", dangling, "]").fmt(f);
+        }
+        let items = format_with(|f| {
+            f.join_comma_separated(range.end())
+                .nodes(patterns.iter())
+                .finish()
+        });
+        parenthesized("[", &items, "]")
+            .with_dangling_comments(dangling)
+            .fmt(f)
     }
 }
