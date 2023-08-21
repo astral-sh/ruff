16fba95bb97868a95cf920aadf6184a0fa88349d
harupy <hkawamura0130@gmail.com>
2023-08-21 18:40:33 +0900

    wip

--- crates/ruff_python_formatter/src/pattern/pattern_match_sequence.rs
+++ crates/ruff_python_formatter/src/pattern/pattern_match_sequence.rs
@@ -10,21 +10,45 @@ use crate::{FormatNodeRule, PyFormatter};
 #[derive(Default)]
 pub struct FormatPatternMatchSequence;
 
+#[derive(Debug)]
+enum SequenceType {
+    Tuple,
+    TupleWithoutParentheses,
+    List,
+}
+
 impl FormatNodeRule<PatternMatchSequence> for FormatPatternMatchSequence {
     fn fmt_fields(&self, item: &PatternMatchSequence, f: &mut PyFormatter) -> FormatResult<()> {
         let PatternMatchSequence { patterns, range } = item;
+        let sequence_type = match &f.context().source()[*range].chars().next() {
+            Some('(') => SequenceType::Tuple,
+            Some('[') => SequenceType::List,
+            _ => SequenceType::TupleWithoutParentheses,
+        };
         let comments = f.context().comments().clone();
         let dangling = comments.dangling(item);
         if patterns.is_empty() {
-            return empty_parenthesized("[", dangling, "]").fmt(f);
+            return match sequence_type {
+                SequenceType::Tuple => empty_parenthesized("(", dangling, ")").fmt(f),
+                SequenceType::List => empty_parenthesized("[", dangling, "]").fmt(f),
+                SequenceType::TupleWithoutParentheses => {
+                    unreachable!("If empty, it should be either tuple or list")
+                }
+            };
         }
         let items = format_with(|f| {
             f.join_comma_separated(range.end())
                 .nodes(patterns.iter())
                 .finish()
         });
-        parenthesized("[", &items, "]")
-            .with_dangling_comments(dangling)
-            .fmt(f)
+        match sequence_type {
+            SequenceType::Tuple => parenthesized("(", &items, ")")
+                .with_dangling_comments(dangling)
+                .fmt(f),
+            SequenceType::List => parenthesized("[", &items, "]")
+                .with_dangling_comments(dangling)
+                .fmt(f),
+            SequenceType::TupleWithoutParentheses => items.fmt(f),
+        }
     }
 }
