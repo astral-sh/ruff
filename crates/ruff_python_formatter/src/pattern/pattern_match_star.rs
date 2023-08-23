84ed8e81b9e4aa69a6f69f24dc6014b977cfbfd8
harupy <hkawamura0130@gmail.com>
2023-08-23 18:55:22 +0900

    Format PatternMatchStar

--- crates/ruff_python_formatter/src/pattern/pattern_match_star.rs
+++ crates/ruff_python_formatter/src/pattern/pattern_match_star.rs
@@ -1,19 +1,18 @@
-use ruff_formatter::{write, Buffer, FormatResult};
+use ruff_formatter::{prelude::text, write, Buffer, FormatResult};
 use ruff_python_ast::PatternMatchStar;
 
-use crate::{not_yet_implemented_custom_text, FormatNodeRule, PyFormatter};
+use crate::AsFormat;
+use crate::{FormatNodeRule, PyFormatter};
 
 #[derive(Default)]
 pub struct FormatPatternMatchStar;
 
 impl FormatNodeRule<PatternMatchStar> for FormatPatternMatchStar {
     fn fmt_fields(&self, item: &PatternMatchStar, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(
-            f,
-            [not_yet_implemented_custom_text(
-                "*NOT_YET_IMPLEMENTED_PatternMatchStar",
-                item
-            )]
-        )
+        let PatternMatchStar { name, .. } = item;
+        match name {
+            Some(name) => write!(f, [text("*"), name.format()]),
+            None => write!(f, [text("*_")]),
+        }
     }
 }
