71c25e4f9d138cbc0b3463ec4eadf911f15299a4
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-08-23 10:01:14 -0400

    Implement `FormatPatternMatchValue` (#6799)
    ## Summary
    
    This is effectively #6608, but with additional tests.
    
    We aren't properly handling parenthesized patterns, but that needs to be
    dealt with separately as it's somewhat involved.
    
    Closes #6555

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/match.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/match.py
@@ -236,3 +236,30 @@ match foo:
         "b",
     ]:
         pass
+
+
+match foo:
+    case 1:
+        y = 0
+    case (1):
+        y = 1
+    case (("a")):
+        y = 1
+    case (  # comment
+        1
+    ):
+        y = 1
+    case (
+        # comment
+        1
+    ):
+        y = 1
+    case (
+        1  # comment
+    ):
+        y = 1
+    case (
+        1
+        # comment
+    ):
+        y = 1
