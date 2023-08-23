923f86adf8e5aa395424682d78dd21db8bb8d448
Simon Høxbro Hansen <simon.hansen@me.com>
2023-08-23 22:20:13 +0200

    Add more tests

--- crates/ruff/resources/test/fixtures/flake8_bugbear/B006_B008.py
+++ crates/ruff/resources/test/fixtures/flake8_bugbear/B006_B008.py
@@ -310,9 +310,15 @@ def import_module_wrong(value: dict[str, str] = {}):
     import os
 
 
+def import_module_with_values_wrong(value: dict[str, str] = {}):
+    import os
+    return 2
+
+
 def import_modules_wrong(value: dict[str, str] = {}):
     import os
     import sys
+    import itertools
 
 
 def from_import_module_wrong(value: dict[str, str] = {}):
@@ -324,6 +330,11 @@ def from_imports_module_wrong(value: dict[str, str] = {}):
     from sys import version_info
 
 
-def from_imports_module_wrong(value: dict[str, str] = {}):
+def import_and_from_imports_module_wrong(value: dict[str, str] = {}):
     import os
     from sys import version_info
+
+
+def import_docstring_module_wrong(value: dict[str, str] = {}):
+    """Docstring"""
+    import os
