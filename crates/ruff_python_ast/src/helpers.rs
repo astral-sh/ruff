7c480236e0efb9cf8cb0169aef05c93d10516e43
Micha Reiser <micha@reiser.io>
2023-08-27 15:54:01 +0200

    Use dyn dispatch for `any_over_*` (#6912)

--- crates/ruff_python_ast/src/helpers.rs
+++ crates/ruff_python_ast/src/helpers.rs
@@ -114,10 +114,7 @@ where
 
 /// Call `func` over every `Expr` in `expr`, returning `true` if any expression
 /// returns `true`..
-pub fn any_over_expr<F>(expr: &Expr, func: &F) -> bool
-where
-    F: Fn(&Expr) -> bool,
-{
+pub fn any_over_expr(expr: &Expr, func: &dyn Fn(&Expr) -> bool) -> bool {
     if func(expr) {
         return true;
     }
@@ -248,10 +245,7 @@ where
     }
 }
 
-pub fn any_over_type_param<F>(type_param: &TypeParam, func: &F) -> bool
-where
-    F: Fn(&Expr) -> bool,
-{
+pub fn any_over_type_param(type_param: &TypeParam, func: &dyn Fn(&Expr) -> bool) -> bool {
     match type_param {
         TypeParam::TypeVar(ast::TypeParamTypeVar { bound, .. }) => bound
             .as_ref()
@@ -261,10 +255,7 @@ where
     }
 }
 
-pub fn any_over_pattern<F>(pattern: &Pattern, func: &F) -> bool
-where
-    F: Fn(&Expr) -> bool,
-{
+pub fn any_over_pattern(pattern: &Pattern, func: &dyn Fn(&Expr) -> bool) -> bool {
     match pattern {
         Pattern::MatchValue(ast::PatternMatchValue { value, range: _ }) => {
             any_over_expr(value, func)
@@ -300,10 +291,7 @@ where
     }
 }
 
-pub fn any_over_stmt<F>(stmt: &Stmt, func: &F) -> bool
-where
-    F: Fn(&Expr) -> bool,
-{
+pub fn any_over_stmt(stmt: &Stmt, func: &dyn Fn(&Expr) -> bool) -> bool {
     match stmt {
         Stmt::FunctionDef(ast::StmtFunctionDef {
             parameters,
@@ -526,10 +514,7 @@ where
     }
 }
 
-pub fn any_over_body<F>(body: &[Stmt], func: &F) -> bool
-where
-    F: Fn(&Expr) -> bool,
-{
+pub fn any_over_body(body: &[Stmt], func: &dyn Fn(&Expr) -> bool) -> bool {
     body.iter().any(|stmt| any_over_stmt(stmt, func))
 }
 
