7bf68e4ef15a9ede931c726f6c3265062a2d09ff
Anselm Hahn <Anselm.Hahn@gmail.com>
2023-08-29 19:31:44 +0200

    docs: :memo: Updating the example for `assert(S101)`
    Rewriting the `if`-comparison to focus on the meaning of rule ` assert S101`.
    
    Fixes #6984

--- crates/ruff/src/rules/flake8_bandit/rules/assert_used.rs
+++ crates/ruff/src/rules/flake8_bandit/rules/assert_used.rs
@@ -23,6 +23,9 @@ use ruff_text_size::Ranged;
 ///
 /// Use instead:
 /// ```python
+/// if x not > 0:
+///     raise ValueError("Expected positive value.")
+/// # or even better:
 /// if x <= 0:
 ///     raise ValueError("Expected positive value.")
 /// ```
