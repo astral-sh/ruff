adb48692d6ea53d29c60500a75c236c51a72a011
Micha Reiser <micha@reiser.io>
2023-08-29 08:30:05 +0200

    Use optional parentheses for tuples in return statements (#6875)

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/statement/return.py
@@ -0,0 +1,16 @@
+
+return len(self.nodeseeeeeeeee), sum(
+    len(node.parents) for node in self.node_map.values()
+)
+
+
+return len(self.nodeseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee), sum(
+    len(node.parents) for node in self.node_map.values()
+)
+
+
+return (
+    len(self.nodeseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee), sum(
+    len(node.parents) for node in self.node_map.values()
+    )
+)
