0b55fbd574dd30007f1ed0e7359e72f971a11c1d
David Szotten <davidszotten@gmail.com>
2023-08-31 15:33:15 +0100

    add some tests (one unstable atm)

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/parentheses/best_fit.py
@@ -0,0 +1,14 @@
+UNUSABLE_PASSWORD_SUFFIX_LENGTH = 40  # number of random chars to add after UNUSABLE_PASSWORD_PREFIX
+
+
+UNUSABLE_PASSWORD_SUFFIX_LENGTH = (
+    40
+)  # number of random chars to add after UNUSABLE_PASSWORD_PREFIX
+
+
+
+UNUSABLE_PASSWORD_SUFFIX_LENGTH = (
+    40  # number of random chars to add after UNUSABLE_PASSWORD_PREFIX
+)
+
+
