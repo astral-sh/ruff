716248b7dce94c40cce54512b1f71c008de5c8b8
konstin <konstin@mailbox.org>
2023-09-05 14:23:28 +0200

    Use line length in formatter

--- crates/ruff_python_formatter/src/lib.rs
+++ crates/ruff_python_formatter/src/lib.rs
@@ -273,10 +273,7 @@ for converter in connection.ops.get_db_converters(
         struct FormatString<'a>(&'a str);
 
         impl Format<SimpleFormatContext> for FormatString<'_> {
-            fn fmt(
-                &self,
-                f: &mut ruff_formatter::formatter::Formatter<SimpleFormatContext>,
-            ) -> FormatResult<()> {
+            fn fmt(&self, f: &mut Formatter<SimpleFormatContext>) -> FormatResult<()> {
                 let format_str = format_with(|f| {
                     write!(f, [token("\"")])?;
 
