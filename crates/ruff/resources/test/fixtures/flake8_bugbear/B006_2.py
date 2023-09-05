76ab6b68d4aa74b608efe8b580949cbe39d7ed0f
Zanie <contact@zanie.dev>
2023-09-05 10:01:03 -0500

    Fix B006 when function docstring is followed by whitespace but no newline

---
+++ crates/ruff/resources/test/fixtures/flake8_bugbear/B006_2.py
@@ -0,0 +1,7 @@
+# Regression test for https://github.com/astral-sh/ruff/issues/7155
+
+def readLinkagesByLineFile(linkages_file, 
+                           column_mapping={
+                           }):    
+    """
+    """                           
