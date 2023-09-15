224364e769696f8e31f3b81d25c363a386abd81c
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-09-14 21:11:02 -0400

    Update docs

--- crates/ruff/src/rules/perflint/rules/manual_dict_comprehension.rs
+++ crates/ruff/src/rules/perflint/rules/manual_dict_comprehension.rs
@@ -15,8 +15,8 @@ use crate::checkers::ast::Checker;
 /// When creating or extending a dictionary in a for-loop, prefer a dictionary
 /// comprehension. Comprehensions are more readable and more performant.
 ///
-/// Using the below as an example, the list comprehension is ~5% faster on
-/// Python 3.10.
+/// For example, when comparing `{x: x for x in list(range(1000))}` to the `for`
+/// loop version, the comprehension is ~10% faster on Python 3.11.
 ///
 /// Note that, as with all `perflint` rules, this is only intended as a
 /// micro-optimization, and will have a negligible impact on performance in
