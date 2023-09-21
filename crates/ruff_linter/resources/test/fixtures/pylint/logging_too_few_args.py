b531f91839831004379157445866b8073319c10b
qdegraaf <querijndegraaf@gmail.com>
2023-09-21 12:37:32 +0200

    Add Expr::Name check to PLE1205 and PLE1206

--- crates/ruff_linter/resources/test/fixtures/pylint/logging_too_few_args.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/logging_too_few_args.py
@@ -26,3 +26,29 @@ logging.info(msg="Hello %s %s")
 import warning
 
 warning.warning("Hello %s %s", "World!")
+
+
+from logging import error, info, warning
+
+warning("Hello %s %s", "World!")  # [logging-too-few-args]
+
+# do not handle calls with kwargs (like pylint)
+warning("Hello %s", "World!", "again", something="else")
+
+warning("Hello %s", "World!")
+
+# do not handle calls without any args
+info("100% dynamic")
+
+# do not handle calls with *args
+error("Example log %s, %s", "foo", "bar", "baz", *args)
+
+# do not handle calls with **kwargs
+error("Example log %s, %s", "foo", "bar", "baz", **kwargs)
+
+# do not handle keyword arguments
+error("%(objects)d modifications: %(modifications)d errors: %(errors)d")
+
+info(msg="Hello %s")
+
+info(msg="Hello %s %s")
