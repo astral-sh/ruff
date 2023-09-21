ec1373c69737d683c88149a9fb4723f7930eea47
qdegraaf <querijndegraaf@gmail.com>
2023-09-21 13:22:45 +0200

    Add Expr::Name check to tryceratops helpers and verbose_log_message

--- crates/ruff_linter/src/rules/tryceratops/helpers.rs
+++ crates/ruff_linter/src/rules/tryceratops/helpers.rs
@@ -4,6 +4,7 @@ use ruff_python_ast::visitor;
 use ruff_python_ast::visitor::Visitor;
 use ruff_python_semantic::analyze::logging;
 use ruff_python_semantic::SemanticModel;
+use ruff_python_stdlib::logging::LoggingLevel;
 
 /// Collect `logging`-like calls from an AST.
 pub(super) struct LoggerCandidateVisitor<'a, 'b> {
@@ -25,8 +26,28 @@ impl<'a, 'b> LoggerCandidateVisitor<'a, 'b> {
 impl<'a, 'b> Visitor<'b> for LoggerCandidateVisitor<'a, 'b> {
     fn visit_expr(&mut self, expr: &'b Expr) {
         if let Expr::Call(call) = expr {
-            if logging::is_logger_candidate(&call.func, self.semantic, self.logger_objects) {
-                self.calls.push(call);
+            match call.func.as_ref() {
+                Expr::Attribute(_) => {
+                    if logging::is_logger_candidate(&call.func, self.semantic, self.logger_objects)
+                    {
+                        self.calls.push(call);
+                    }
+                }
+                Expr::Name(_) => {
+                    let Some(call_path) = self.semantic.resolve_call_path(call.func.as_ref()) else {
+                        return;
+                    };
+                    let ["logging", attribute] = call_path.as_slice() else {
+                        return;
+                    };
+                    let Some(_) = LoggingLevel::from_attribute(attribute) else {
+                        return;
+                    };
+                    {
+                        self.calls.push(call);
+                    }
+                }
+                _ => {}
             }
         }
         visitor::walk_expr(self, expr);
