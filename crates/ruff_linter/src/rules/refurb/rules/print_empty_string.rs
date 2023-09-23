5a2fb01a0fba26f19a0a38331f4101cefb23f45d
Tom Kuson <mail@tjkuson.me>
2023-09-23 14:58:27 +0100

    Check also for empty seperators

--- crates/ruff_linter/src/rules/refurb/rules/print_empty_string.rs
+++ crates/ruff_linter/src/rules/refurb/rules/print_empty_string.rs
@@ -72,51 +72,65 @@ pub(crate) fn print_empty_string(checker: &mut Checker, call: &ast::ExprCall) {
         // If the print call does not have precisely one positional argument,
         // do not trigger (more than one positional empty string argument is
         // not equivalent to no positional arguments due to the `sep` argument).
-        if call.arguments.args.len() != 1 {
+        let sep_value_is_empty_string = call
+            .arguments
+            .find_keyword("sep")
+            .map_or(false, |keyword| is_const_empty_string(&keyword.value));
+
+        if call.arguments.args.len() != 1 && !sep_value_is_empty_string {
             return;
         }
-        // Check if the one positional argument is an empty string.
-        if let Expr::Constant(ast::ExprConstant {
-            value: Constant::Str(s),
-            ..
-        }) = &call.arguments.args[0]
-        {
-            if s.is_empty() {
-                let sep_index = call.arguments.keywords.iter().position(|keyword| {
-                    keyword
-                        .arg
-                        .clone()
-                        .is_some_and(|arg| arg.to_string() == "sep")
-                });
-                let suggestion = generate_suggestion(&call.clone(), sep_index, checker.generator());
-                let mut diagnostic = Diagnostic::new(
-                    PrintEmptyString {
-                        suggestion: suggestion.clone(),
-                        redundant_sep: sep_index.is_some(),
-                    },
-                    call.range(),
-                );
-                if checker.patch(diagnostic.kind.rule()) {
-                    diagnostic.set_fix(Fix::suggested(Edit::replacement(
-                        suggestion,
-                        call.start(),
-                        call.end(),
-                    )));
-                }
-                checker.diagnostics.push(diagnostic);
+        // Check if the positional arguments is are all empty string.
+        let is_empty = call.arguments.args.iter().all(is_const_empty_string);
+
+        if is_empty {
+            let sep_index = call.arguments.keywords.iter().position(|keyword| {
+                keyword
+                    .arg
+                    .clone()
+                    .is_some_and(|arg| arg.to_string() == "sep")
+            });
+            let suggestion = generate_suggestion(&call.clone(), sep_index, checker.generator());
+            let mut diagnostic = Diagnostic::new(
+                PrintEmptyString {
+                    suggestion: suggestion.clone(),
+                    redundant_sep: sep_index.is_some(),
+                },
+                call.range(),
+            );
+            if checker.patch(diagnostic.kind.rule()) {
+                diagnostic.set_fix(Fix::suggested(Edit::replacement(
+                    suggestion,
+                    call.start(),
+                    call.end(),
+                )));
             }
+            checker.diagnostics.push(diagnostic);
         }
     }
 }
 
+/// Check if an expression is a constant empty string.
+fn is_const_empty_string(expr: &Expr) -> bool {
+    matches!(
+        expr,
+        Expr::Constant(ast::ExprConstant {
+            value: Constant::Str(s),
+            ..
+        }) if s.is_empty()
+    )
+}
+
+/// Generate a suggestion to replace a `print` call with `print` call with no
+/// positional arguments, and no `sep` keyword argument.
 fn generate_suggestion(
     call: &ast::ExprCall,
     sep_index: Option<usize>,
     generator: Generator,
 ) -> String {
     let mut suggestion = call.clone();
-    // Remove the empty string argument.
-    suggestion.arguments.args.remove(0);
+    // Remove all positional arguments.
+    suggestion.arguments.args.clear();
     // If there is a `sep` keyword argument, remove it too (the separator is
     // not needed if there are no objects to separate) by finding its index.
     if let Some(index) = sep_index {
