33f0413b64214f327d35dd17dbd068ed9deee644
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-09-23 18:50:32 -0400

    Treat form feed as whitespace in SimpleTokenizer

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/form_feed.py
@@ -0,0 +1,6 @@
+# Regression test for: https://github.com/astral-sh/ruff/issues/7624
+if symbol is not None:
+    request["market"] = market["id"]
+      #             "remaining_volume": "0.0",
+else:
+    pass
