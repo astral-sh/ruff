cd17b8c539673c5ea2ce7c4246bbc0cbc0a5baa1
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-24 19:41:13 +0200

    Fix duplicate tests

--- crates/ruff_linter/resources/test/fixtures/flake8_bugbear/B006_5.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_bugbear/B006_5.py
@@ -35,38 +35,3 @@ def import_and_from_imports_module_wrong(value: dict[str, str] = {}):
 def import_docstring_module_wrong(value: dict[str, str] = {}):
     """Docstring"""
     import os
-
-
-def import_module_wrong(value: dict[str, str] = {}):
-    import os
-
-
-def import_module_with_values_wrong(value: dict[str, str] = {}):
-    import os
-
-    return 2
-
-
-def import_modules_wrong(value: dict[str, str] = {}):
-    import os
-    import sys
-    import itertools
-
-
-def from_import_module_wrong(value: dict[str, str] = {}):
-    from os import path
-
-
-def from_imports_module_wrong(value: dict[str, str] = {}):
-    from os import path
-    from sys import version_info
-
-
-def import_and_from_imports_module_wrong(value: dict[str, str] = {}):
-    import os
-    from sys import version_info
-
-
-def import_docstring_module_wrong(value: dict[str, str] = {}):
-    """Docstring"""
-    import os
