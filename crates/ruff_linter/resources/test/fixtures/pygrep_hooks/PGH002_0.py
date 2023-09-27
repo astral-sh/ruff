0a6449ca0cc7ac66daaa80ee4baff8f9683589f8
qdegraaf <querijndegraaf@gmail.com>
2023-09-27 16:03:50 +0200

    Expand DeprecatedLogWarn to check for Expr::Atrribute

--- crates/ruff_linter/resources/test/fixtures/pygrep_hooks/PGH002_0.py
+++ crates/ruff_linter/resources/test/fixtures/pygrep_hooks/PGH002_0.py
@@ -5,3 +5,6 @@ from warnings import warn
 warnings.warn("this is ok")
 warn("by itself is also ok")
 logging.warning("this is fine")
+
+logger = logging.getLogger(__name__)
+logger.warning("this is fine")
