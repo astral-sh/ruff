75aff5b8deb93323e56bd3cc2d5c045a090942e1
Arborym <dparizher@gmail.com>
2023-09-28 19:49:37 -0400

    improve test file

--- crates/ruff_linter/resources/test/fixtures/refurb/FURB177.py
+++ crates/ruff_linter/resources/test/fixtures/refurb/FURB177.py
@@ -1,16 +1,22 @@
+import pathlib
 from pathlib import Path
 
 # Errors
 cwd = Path().resolve()
+cwd = pathlib.Path().resolve()
 
 current_directory = Path().resolve()
+current_directory = pathlib.Path().resolve()
 
 dir = Path().resolve()
-
+dir = pathlib.Path().resolve()
 
 # OK
 cwd = Path.cwd()
+cwd = pathlib.Path.cwd()
 
 current_directory = Path.cwd()
+current_directory = pathlib.Path.cwd()
 
 dir = Path.cwd()
+dir = pathlib.Path.cwd()
