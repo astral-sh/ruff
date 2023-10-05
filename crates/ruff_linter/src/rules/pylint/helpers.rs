2662cf704af74f5f695ba7737e733a47b816ce3d
Jinwoo Park <danbi2990@gmail.com>
2023-10-05 12:01:07 +0900

    move to helpers

--- crates/ruff_linter/src/rules/pylint/helpers.rs
+++ crates/ruff_linter/src/rules/pylint/helpers.rs
@@ -1,7 +1,8 @@
 use std::fmt;
 
 use ruff_python_ast as ast;
-use ruff_python_ast::{Arguments, CmpOp, Constant, Expr};
+use ruff_python_ast::{Arguments, BoolOp, CmpOp, Constant, Expr, ExprBoolOp};
+use ruff_python_parser::parse_expression;
 use ruff_python_semantic::analyze::function_type;
 use ruff_python_semantic::{ScopeKind, SemanticModel};
 
@@ -81,4 +82,54 @@ impl fmt::Display for CmpOpExt {
         };
         write!(f, "{representation}")
     }
+}
+
+/// Returns `true` if `bool_op` is `condition and true_value or false_value` form.
+pub(super) fn is_and_or_ternary(bool_op: &ExprBoolOp) -> bool {
+    let and_op = &bool_op.values[0];
+    if !and_op.is_bool_op_expr() {
+        return false
+    }
+    let false_value = &bool_op.values[1];
+    let and_values = &and_op.as_bool_op_expr().unwrap().values;
+    let true_value = &and_values[1];
+    bool_op.op == BoolOp::Or
+        && bool_op.values.len() == 2
+        && !false_value.is_bool_op_expr()
+        && and_op.as_bool_op_expr().unwrap().op == BoolOp::And
+        && !true_value.is_bool_op_expr()
+        && and_values.len() == 2
+}
+
+#[allow(dead_code)]
+fn parse_bool_op(s: &str) -> Option<ExprBoolOp> {
+    if let Ok(expr) = parse_expression(s, "<embedded>") {
+        expr.as_bool_op_expr().cloned()
+    } else {
+        None
+    }
+}
+
+#[allow(dead_code)]
+fn is_str_and_or_ternary(s: &str) -> bool {
+    if let Some(bool_op) = parse_bool_op(s) {
+        is_and_or_ternary(&bool_op)
+    } else {
+        false
+    }
+}
+
+#[test]
+fn test_is_and_or_ternary() {
+    // positive
+    assert!(is_str_and_or_ternary("1<2 and 'a' or 'b'"));
+
+    // negative
+    assert!(!is_str_and_or_ternary("1<2 and 'a' or 'b' and 'd'")); // 'a' if 1<2 else 'b' and 'd'
+    assert!(!is_str_and_or_ternary("1<2 and 'a' or 'b' or 'd'")); // 'a' if 1<2 else 'b' or 'd'
+    assert!(!is_str_and_or_ternary("1<2 and 'a'"));
+    assert!(!is_str_and_or_ternary("1<2 or 'a'"));
+    assert!(!is_str_and_or_ternary("2>1 or 'a' and 'b'"));
+    assert!(!is_str_and_or_ternary("2>1"));
+    assert!(!is_str_and_or_ternary("'string'"));
 }
