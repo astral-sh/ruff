c3f67287f02cb5e912457b5c09ab86da56948b02
Jinwoo Park <danbi2990@gmail.com>
2023-10-11 10:25:46 +0900

    add test

--- crates/ruff_linter/resources/test/fixtures/pylint/and_or_ternary.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/and_or_ternary.py
@@ -30,6 +30,18 @@ docid, token = None, None
 vendor, os_version = 'darwin', '14'
 vendor == "debian" and os_version in ["12"] or vendor == "ubuntu" and os_version in []
 
+# No emit if parent is `if` statement
+if (task_id in task_dict and task_dict[task_id] is not task) \
+        or task_id in used_group_ids:
+    pass
+
+no_target, is_x64, target = True, False, 'target'
+if (no_target and not is_x64) or target == 'ARM_APPL_RUST_TARGET':
+    pass
+
+# No emit if parent is bool_op expression
+isinstance(val, str) and ((len(val) == 7 and val[0] == "#") or val in enums.NamedColor)
+
 # Ternary (Error)
 
 1<2 and 'a' or 'b'
