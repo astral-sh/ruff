87ac9402523b0b715977d8fa9f37289d4256f025
Steve C <diceroll123@gmail.com>
2023-10-18 02:13:45 -0400

    add the other assignments

--- crates/ruff_linter/resources/test/fixtures/pylint/unnecessary_list_index_lookup.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/unnecessary_list_index_lookup.py
@@ -5,6 +5,8 @@ def fix_these():
     for index, letter in enumerate(letters):
         print(letters[index])  # PLR1736
         blah = letters[index]  # PLR1736
+        letters[index]: str = letters[index]  # PLR1736 (on the right hand)
+        letters[index] += letters[index]  # PLR1736 (on the right hand)
         letters[index] = letters[index]  # PLR1736 (on the right hand)
 
 
@@ -17,5 +19,7 @@ def value_intentionally_unused():
     for index, _ in enumerate(letters):
         print(letters[index])  # Ok
         blah = letters[index]  # Ok
+        letters[index]: str = letters[index]  # Ok
+        letters[index] += letters[index]  # Ok
         letters[index] = letters[index]  # Ok
         letters[index] = "d"  # Ok
