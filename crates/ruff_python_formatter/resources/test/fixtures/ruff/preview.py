b137e2a0f56d7d1689a1b968d398be5ad5b9c4c2
konstin <konstin@mailbox.org>
2023-10-18 13:10:06 +0200

    Add preview test cases based on black

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/preview.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/preview.py
@@ -31,3 +31,36 @@ class RemoveNewlineBeforeClassDocstring:
 
     """Black's `Preview.no_blank_line_before_class_docstring`"""
 
+
+def f():
+    """Black's `Preview.prefer_splitting_right_hand_side_of_assignments`"""
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa[
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    ] = cccccccc.ccccccccccccc.cccccccc
+
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa[
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    ] = cccccccc.ccccccccccccc().cccccccc
+
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa[
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    ] = cccccccc.ccccccccccccc(d).cccccccc
+
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa[bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb] = (
+        cccccccc.ccccccccccccc(d).cccccccc + e
+    )
+
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa[bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb] = (
+        cccccccc.ccccccccccccc.cccccccc + e
+    )
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa[bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb] = (
+        cccccccc.ccccccccccccc.cccccccc
+        + eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    )
+
+    self._cache: dict[
+        DependencyCacheKey, list[list[DependencyPackage]]
+    ] = collections.defaultdict(list)
+    self._cached_dependencies_by_level: dict[
+        int, list[DependencyCacheKey]
+    ] = collections.defaultdict(list)
