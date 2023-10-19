f6c9a45efcf3525ae87214844dd4114ac21b80e0
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-10-19 00:40:15 -0400

    Tweak docs

--- crates/ruff_linter/src/rules/pylint/rules/global_at_module_level.rs
+++ crates/ruff_linter/src/rules/pylint/rules/global_at_module_level.rs
@@ -1,17 +1,19 @@
-use ast::Stmt;
-use ruff_python_ast::{self as ast};
-
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast::Stmt;
 use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for the use of `global` at the top level of a module.
+/// Checks for uses of the `global` keyword at the module level.
 ///
 /// ## Why is this bad?
-/// `global` at the top level of a module is redundant.
+/// The `global` keyword is used within functions to indicate that a name
+/// refers to a global variable, rather than a local variable.
+///
+/// At the module level, all names are global by default, so the `global`
+/// keyword is redundant.
 #[violation]
 pub struct GlobalAtModuleLevel;
 
@@ -24,11 +26,9 @@ impl Violation for GlobalAtModuleLevel {
 
 /// PLW0604
 pub(crate) fn global_at_module_level(checker: &mut Checker, stmt: &Stmt) {
-    if !checker.semantic().current_scope().kind.is_module() {
-        return;
+    if checker.semantic().current_scope().kind.is_module() {
+        checker
+            .diagnostics
+            .push(Diagnostic::new(GlobalAtModuleLevel, stmt.range()));
     }
-
-    checker
-        .diagnostics
-        .push(Diagnostic::new(GlobalAtModuleLevel, stmt.range()));
 }
