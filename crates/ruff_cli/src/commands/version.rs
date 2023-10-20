996ddf1871a53ebd088f219d161e0e7f181aa314
Zanie <contact@zanie.dev>
2023-10-20 10:36:37 -0500

    Add ruff prefix

--- crates/ruff_cli/src/commands/version.rs
+++ crates/ruff_cli/src/commands/version.rs
@@ -11,7 +11,7 @@ pub(crate) fn version(output_format: HelpFormat) -> Result<()> {
 
     match output_format {
         HelpFormat::Text => {
-            writeln!(stdout, "{}", &version_info)?;
+            writeln!(stdout, "ruff {}", &version_info)?;
         }
         HelpFormat::Json => {
             serde_json::to_writer_pretty(stdout, &version_info)?;
