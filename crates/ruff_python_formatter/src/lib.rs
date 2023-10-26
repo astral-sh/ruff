6b356e8d1dabf55a993467cb57f0386c4a053f5b
Micha Reiser <micha@reiser.io>
2023-10-26 12:39:18 +0900

    Discard changes to crates/ruff_python_formatter/src/lib.rs

--- crates/ruff_python_formatter/src/lib.rs
+++ crates/ruff_python_formatter/src/lib.rs
@@ -206,11 +206,14 @@ if True:
     #[test]
     fn quick_test() {
         let source = r#"
-def foo():
-    while (
-        aaaaaaaaaaaaaaaaaaaaa(bbbbbbbb, cccccccccccccc) and dddddddddd < eeeeeeeeeeeeeee
-    ):
-        pass
+def main() -> None:
+    if True:
+        some_very_long_variable_name_abcdefghijk = Foo()
+        some_very_long_variable_name_abcdefghijk = some_very_long_variable_name_abcdefghijk[
+            some_very_long_variable_name_abcdefghijk.some_very_long_attribute_name
+            == "This is a very long string abcdefghijk"
+        ]
+
 "#;
         let source_type = PySourceType::Python;
         let (tokens, comment_ranges) = tokens_and_ranges(source, source_type).unwrap();
