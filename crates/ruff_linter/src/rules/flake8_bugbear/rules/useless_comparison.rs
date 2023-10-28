5ea0316493c30a7912cf9f4b034538b3bdadf9c6
harupy <hkawamura0130@gmail.com>
2023-10-28 16:11:32 +0900

    Address comments

--- crates/ruff_linter/src/rules/flake8_bugbear/rules/useless_comparison.rs
+++ crates/ruff_linter/src/rules/flake8_bugbear/rules/useless_comparison.rs
@@ -32,8 +32,8 @@ impl Violation for UselessComparison {
     #[derive_message_formats]
     fn message(&self) -> String {
         format!(
-            "Pointless comparison. This comparison does nothing. Did you mean to assign a value? \
-             Either prepend `assert` or remove it."
+            "Pointless comparison. Did you mean to assign a value? \
+             Otherwise, prepend `assert` or remove it."
         )
     }
 }
