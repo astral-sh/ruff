f483ed42406ef6f4cabfe138bcbd1df31602c92e
konsti <konstin@mailbox.org>
2023-10-30 10:58:33 +0100

    Byte strings aren't docstrings (#8350)
    We previously incorrectly treated byte strings in docstring position as
    docstrings because black does so
    (https://github.com/astral-sh/ruff/pull/8283#discussion_r1375682931,
    https://github.com/psf/black/issues/4002), even CPython doesn't
    recognize them:
    
    ```console
    $ python3.12
    Python 3.12.0 (main, Oct  6 2023, 17:57:44) [GCC 11.4.0] on linux
    Type "help", "copyright", "credits" or "license" for more information.
    >>> def f():
    ...     b""" a"""
    ...
    >>> print(str(f.__doc__))
    None
    ```
    
    <!--
    Thank you for contributing to Ruff! To help us out with reviewing,
    please consider the following:
    
    - Does this pull request include a summary of the change? (See below.)
    - Does this pull request include a descriptive title?
    - Does this pull request include references to any relevant issues?
    -->

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/docstring.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/docstring.py
@@ -93,6 +93,10 @@ def docstring_that_ends_with_quote_and_a_line_break3():
     """
 
 
+class ByteDocstring:
+    b"""   has leading whitespace"""
+    first_statement = 1
+
 class TabbedIndent:
 	def tabbed_indent(self):
 		"""check for correct tabbed formatting
