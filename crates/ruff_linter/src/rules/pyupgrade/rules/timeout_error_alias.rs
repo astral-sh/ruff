8aa53ca1d88dbf57a5ea8f144f0c1279f00a0465
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-11-03 13:19:43 -0400

    Tweaks to docs; move to preview

--- crates/ruff_linter/src/rules/pyupgrade/rules/timeout_error_alias.rs
+++ crates/ruff_linter/src/rules/pyupgrade/rules/timeout_error_alias.rs
@@ -17,17 +17,17 @@ use crate::settings::types::PythonVersion;
 /// `TimeoutError` is the builtin error type used for exceptions when a system
 /// function timed out at the system level.
 ///
-/// In Python 3.10, `socket.timeout` was aliased to `TimeoutError`.
-/// In Python 3.11, `asyncio.TimeoutError` was aliased to `TimeoutError`.
+/// In Python 3.10, `socket.timeout` was aliased to `TimeoutError`. In Python
+/// 3.11, `asyncio.TimeoutError` was aliased to `TimeoutError`.
 ///
-/// These aliass remain in place for compatibility with
-/// older versions of Python, but may be removed in future versions.
+/// These aliases remain in place for compatibility with older versions of
+/// Python, but may be removed in future versions.
 ///
 /// Prefer using `TimeoutError` directly, as it is more idiomatic and future-proof.
 ///
 /// ## Example
 /// ```python
-/// raise asyncio.TimeoutError.
+/// raise asyncio.TimeoutError
 /// ```
 ///
 /// Use instead:
