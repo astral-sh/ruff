3ed4a2f92b74893f7d60792debf499257d63105a
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-11-10 02:34:20 +0530

    Preserve trailing semicolon for Notebooks

--- crates/ruff_python_formatter/src/statement/stmt_ann_assign.rs
+++ crates/ruff_python_formatter/src/statement/stmt_ann_assign.rs
@@ -2,9 +2,11 @@ use ruff_formatter::write;
 use ruff_python_ast::StmtAnnAssign;
 
 use crate::comments::{SourceComment, SuppressionKind};
+
 use crate::expression::maybe_parenthesize_expression;
 use crate::expression::parentheses::Parenthesize;
 use crate::prelude::*;
+use crate::statement::trailing_semicolon;
 
 #[derive(Default)]
 pub struct FormatStmtAnnAssign;
@@ -34,6 +36,14 @@ impl FormatNodeRule<StmtAnnAssign> for FormatStmtAnnAssign {
                     maybe_parenthesize_expression(value, item, Parenthesize::IfBreaks)
                 ]
             )?;
+        }
+
+        if f.options().source_type().is_ipynb()
+            && f.context().node_level().is_last_top_level_statement()
+            && target.is_name_expr()
+            && trailing_semicolon(item.into(), f.context().source()).is_some()
+        {
+            token(";").fmt(f)?;
         }
 
         Ok(())
