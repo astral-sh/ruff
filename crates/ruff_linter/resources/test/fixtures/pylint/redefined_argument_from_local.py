d5bb19d2a05ddb5df176cca1dfc555af29ad0a4c
Jake Park <danbi2990@gmail.com>
2023-11-10 16:19:33 +0900

    edit test cases

--- crates/ruff_linter/resources/test/fixtures/pylint/redefined_argument_from_local.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/redefined_argument_from_local.py
@@ -6,59 +6,65 @@ def foo(a):
         ...
 
 def foo(a):
-    try:
-        ...
-    except Exception as e:
+    for a in range(1):  # unused
         ...
 
 def foo(a):
-    with open('', ) as f:
+    try:
+        ...
+    except ValueError as e:
+        ...
+    except KeyError as a:
+        ...
+
+def foo(a):
+    with open('foo.py', ) as f, open('bar.py') as a:
         ...
 
 if True:
     def foo(a):
         ...
 else:
-    for a in range(1):
-        ...
+    for a in range(1):  # diff branch
+        print(a)
 
 # Errors
 
 def foo(i):
     for i in range(10):
-        ...
+        print(i)
 
 def foo(e):
     try:
         ...
     except Exception as e:
-        ...
+        print(e)
 
 def foo(f):
     with open('', ) as f:
-        ...
+        print(f)
 
 def foo(a, b):
     with context() as (a, b, c):
-        ...
+        print(a, b, c)
 
 def foo(a, b):
     with context() as [a, b, c]:
-        ...
+        print(a, b, c)
 
 def foo(a):
     def bar(b):
         for a in range(1):
-            ...
+            print(a)
 
 def foo(a):
     def bar(b):
         for b in range(1):
-            ...
+            print(b)
 
 def foo(a=1):
     def bar(b=2):
         for a in range(1):
-            ...
+            print(a)
         for b in range(1):
-            ...
+            print(b)
