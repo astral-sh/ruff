70f491d31e7235b21bac81dd9104e337f3741ba9
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-11-12 12:40:33 -0800

    Omit unrolled augmented assignments in `PIE794` (#8634)
    Closes https://github.com/astral-sh/ruff/issues/8497.

--- crates/ruff_linter/src/rules/flake8_pie/rules/duplicate_class_field_definition.rs
+++ crates/ruff_linter/src/rules/flake8_pie/rules/duplicate_class_field_definition.rs
@@ -3,6 +3,7 @@ use rustc_hash::FxHashSet;
 use ruff_diagnostics::Diagnostic;
 use ruff_diagnostics::{AlwaysFixableViolation, Fix};
 use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast::helpers::any_over_expr;
 use ruff_python_ast::{self as ast, Expr, Stmt};
 use ruff_text_size::Ranged;
 
@@ -55,14 +56,14 @@ pub(crate) fn duplicate_class_field_definition(checker: &mut Checker, body: &[St
         // Extract the property name from the assignment statement.
         let target = match stmt {
             Stmt::Assign(ast::StmtAssign { targets, .. }) => {
-                if let [Expr::Name(ast::ExprName { id, .. })] = targets.as_slice() {
+                if let [Expr::Name(id)] = targets.as_slice() {
                     id
                 } else {
                     continue;
                 }
             }
             Stmt::AnnAssign(ast::StmtAnnAssign { target, .. }) => {
-                if let Expr::Name(ast::ExprName { id, .. }) = target.as_ref() {
+                if let Expr::Name(id) = target.as_ref() {
                     id
                 } else {
                     continue;
@@ -71,10 +72,31 @@ pub(crate) fn duplicate_class_field_definition(checker: &mut Checker, body: &[St
             _ => continue,
         };
 
-        if !seen_targets.insert(target) {
+        // If this is an unrolled augmented assignment (e.g., `x = x + 1`), skip it.
+        match stmt {
+            Stmt::Assign(ast::StmtAssign { value, .. }) => {
+                if any_over_expr(value.as_ref(), &|expr| {
+                    expr.as_name_expr().is_some_and(|name| name.id == target.id)
+                }) {
+                    continue;
+                }
+            }
+            Stmt::AnnAssign(ast::StmtAnnAssign {
+                value: Some(value), ..
+            }) => {
+                if any_over_expr(value.as_ref(), &|expr| {
+                    expr.as_name_expr().is_some_and(|name| name.id == target.id)
+                }) {
+                    continue;
+                }
+            }
+            _ => continue,
+        }
+
+        if !seen_targets.insert(target.id.as_str()) {
             let mut diagnostic = Diagnostic::new(
                 DuplicateClassFieldDefinition {
-                    name: target.to_string(),
+                    name: target.id.to_string(),
                 },
                 stmt.range(),
             );
