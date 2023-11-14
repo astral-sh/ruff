1acd749bff62670e9496043c626a3d3c5505e2a6
Alan Du <alanhdu@gmail.com>
2023-11-13 23:05:41 -0500

    Add autofix for PIE800
    Whenever we see a **{...} inside another dictionary literal, just
    delete the `**{` and `}` to inline the key-value pairs

--- crates/ruff_linter/src/rules/flake8_pie/rules/unnecessary_spread.rs
+++ crates/ruff_linter/src/rules/flake8_pie/rules/unnecessary_spread.rs
@@ -1,9 +1,8 @@
 use ruff_python_ast::Expr;
 
-use ruff_diagnostics::Diagnostic;
-use ruff_diagnostics::Violation;
+use ruff_diagnostics::{Diagnostic, Edit, Fix, FixAvailability, Violation};
 use ruff_macros::{derive_message_formats, violation};
-use ruff_text_size::Ranged;
+use ruff_text_size::{Ranged, TextSize};
 
 use crate::checkers::ast::Checker;
 
@@ -32,9 +31,15 @@ use crate::checkers::ast::Checker;
 pub struct UnnecessarySpread;
 
 impl Violation for UnnecessarySpread {
+    const FIX_AVAILABILITY: FixAvailability = FixAvailability::Sometimes;
+
     #[derive_message_formats]
     fn message(&self) -> String {
         format!("Unnecessary spread `**`")
+    }
+
+    fn fix_title(&self) -> Option<String> {
+        Some(format!("Remove unnecessary dict"))
     }
 }
 
@@ -45,7 +50,22 @@ pub(crate) fn unnecessary_spread(checker: &mut Checker, keys: &[Option<Expr>], v
             // We only care about when the key is None which indicates a spread `**`
             // inside a dict.
             if let Expr::Dict(_) = value {
-                let diagnostic = Diagnostic::new(UnnecessarySpread, value.range());
+                let mut diagnostic = Diagnostic::new(UnnecessarySpread, value.range());
+                if checker.settings.preview.is_enabled() {
+                    let range = value.range();
+                    // unwrap -- `item.0 == None` iff this is a spread operator
+                    // which means there *must* be a `**` here
+                    let doublestar = checker.locator().up_to(range.start()).rfind("**").unwrap();
+                    diagnostic.set_fix(Fix::safe_edits(
+                        // delete the `**{`
+                        Edit::deletion(
+                            TextSize::from(doublestar as u32),
+                            range.start() + TextSize::from(1),
+                        ),
+                        // delete the `}`
+                        [Edit::deletion(range.end() - TextSize::from(1), range.end())],
+                    ));
+                }
                 checker.diagnostics.push(diagnostic);
             }
         }
