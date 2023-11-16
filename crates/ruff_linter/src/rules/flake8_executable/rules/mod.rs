a9a4520f2905782c20e35dcf47533044ad32c0be
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-11-15 21:23:21 -0500

    Avoid missing namespace violations in scripts with shebangs

--- crates/ruff_linter/src/rules/flake8_executable/rules/mod.rs
+++ crates/ruff_linter/src/rules/flake8_executable/rules/mod.rs
@@ -1,9 +1,7 @@
 use std::path::Path;
 
-use ruff_python_parser::lexer::LexResult;
-use ruff_python_parser::Tok;
-
 use ruff_diagnostics::Diagnostic;
+use ruff_python_index::Indexer;
 use ruff_source_file::Locator;
 pub(crate) use shebang_leading_whitespace::*;
 pub(crate) use shebang_missing_executable_file::*;
@@ -20,32 +18,31 @@ mod shebang_not_executable;
 mod shebang_not_first_line;
 
 pub(crate) fn from_tokens(
-    tokens: &[LexResult],
+    diagnostics: &mut Vec<Diagnostic>,
     path: &Path,
     locator: &Locator,
-    diagnostics: &mut Vec<Diagnostic>,
+    indexer: &Indexer,
 ) {
     let mut has_any_shebang = false;
-    for (tok, range) in tokens.iter().flatten() {
-        if let Tok::Comment(comment) = tok {
-            if let Some(shebang) = ShebangDirective::try_extract(comment) {
-                has_any_shebang = true;
+    for range in indexer.comment_ranges() {
+        let comment = locator.slice(*range);
+        if let Some(shebang) = ShebangDirective::try_extract(comment) {
+            has_any_shebang = true;
 
-                if let Some(diagnostic) = shebang_missing_python(*range, &shebang) {
-                    diagnostics.push(diagnostic);
-                }
+            if let Some(diagnostic) = shebang_missing_python(*range, &shebang) {
+                diagnostics.push(diagnostic);
+            }
 
-                if let Some(diagnostic) = shebang_not_executable(path, *range) {
-                    diagnostics.push(diagnostic);
-                }
+            if let Some(diagnostic) = shebang_not_executable(path, *range) {
+                diagnostics.push(diagnostic);
+            }
 
-                if let Some(diagnostic) = shebang_leading_whitespace(*range, locator) {
-                    diagnostics.push(diagnostic);
-                }
+            if let Some(diagnostic) = shebang_leading_whitespace(*range, locator) {
+                diagnostics.push(diagnostic);
+            }
 
-                if let Some(diagnostic) = shebang_not_first_line(*range, locator) {
-                    diagnostics.push(diagnostic);
-                }
+            if let Some(diagnostic) = shebang_not_first_line(*range, locator) {
+                diagnostics.push(diagnostic);
             }
         }
     }
