d0cdd585823e738d7906bd13c000f64edd00b598
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-11-18 18:50:43 -0500

    Extend dict-get-with-none-default (SIM910) to non-literals

--- crates/ruff_linter/src/rules/flake8_simplify/mod.rs
+++ crates/ruff_linter/src/rules/flake8_simplify/mod.rs
@@ -57,6 +57,7 @@ mod tests {
 
     #[test_case(Rule::InDictKeys, Path::new("SIM118.py"))]
     #[test_case(Rule::IfElseBlockInsteadOfDictGet, Path::new("SIM401.py"))]
+    #[test_case(Rule::DictGetWithNoneDefault, Path::new("SIM910.py"))]
     fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!(
             "preview__{}_{}",
