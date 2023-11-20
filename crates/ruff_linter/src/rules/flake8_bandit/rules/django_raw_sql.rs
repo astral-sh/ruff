2ab2876bdedadf9749cb11381a79646bb5df1f67
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-11-20 12:09:49 +0000

    Tweak logic

--- crates/ruff_linter/src/rules/flake8_bandit/rules/django_raw_sql.rs
+++ crates/ruff_linter/src/rules/flake8_bandit/rules/django_raw_sql.rs
@@ -1,27 +1,27 @@
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
-use ruff_python_ast::{self as ast};
+use ruff_python_ast::{self as ast, Expr};
 use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for Django that use `RawSQL` function.
+/// Checks for uses of Django's `RawSQL` function.
 ///
 /// ## Why is this bad?
-/// Django `RawSQL` function can cause SQL injection attack.
+/// Django's `RawSQL` function can be used to execute arbitrary SQL queries,
+/// which can in turn lead to SQL injection vulnerabilities.
 ///
 /// ## Example
 /// ```python
 /// from django.db.models.expressions import RawSQL
 /// from django.contrib.auth.models import User
 ///
-/// User.objects.annotate(val=RawSQL("%secure" % "nos", []))
+/// User.objects.annotate(val=("%secure" % "nos", []))
 /// ```
 ///
 /// ## References
-/// - [Django documentation: API](https://docs.djangoproject.com/en/dev/ref/models/expressions/#django.db.models.expressions.RawSQL)
-/// - [Django documentation: sql injection protection](https://docs.djangoproject.com/en/dev/topics/security/#sql-injection-protection)
+/// - [Django documentation: SQL injection protection](https://docs.djangoproject.com/en/dev/topics/security/#sql-injection-protection)
 /// - [Common Weakness Enumeration: CWE-89](https://cwe.mitre.org/data/definitions/89.html)
 #[violation]
 pub struct DjangoRawSql;
@@ -29,7 +29,7 @@ pub struct DjangoRawSql;
 impl Violation for DjangoRawSql {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Use of RawSQL potential SQL attack vector.")
+        format!("Use of `RawSQL` can lead to SQL injection vulnerabilities")
     }
 }
 
@@ -45,13 +45,11 @@ pub(crate) fn django_raw_sql(checker: &mut Checker, call: &ast::ExprCall) {
             )
         })
     {
-        let sql = if let Some(arg) = call.arguments.find_argument("sql", 0) {
-            arg
-        } else {
-            &call.arguments.find_keyword("sql").unwrap().value
-        };
-
-        if !sql.is_string_literal_expr() {
+        if !call
+            .arguments
+            .find_argument("sql", 0)
+            .is_some_and(Expr::is_string_literal_expr)
+        {
             checker
                 .diagnostics
                 .push(Diagnostic::new(DjangoRawSql, call.func.range()));
