fab5932c26562a822a9c28afcf6868dcc8aabe37
Zanie <contact@zanie.dev>
2023-11-20 12:59:53 -0600

    Attempt to fix Windows tests

--- crates/ruff_cli/tests/resolve_files.rs
+++ crates/ruff_cli/tests/resolve_files.rs
@@ -7,6 +7,14 @@ use std::str;
 use insta_cmd::{assert_cmd_snapshot, get_cargo_bin};
 const BIN_NAME: &str = "ruff";
 
+#[cfg(not(target_os = "windows"))]
+const TEST_FILTERS: &[(&str, &str)] = &[(".*/resources/test/fixtures/", "[BASEPATH]/")];
+#[cfg(target_os = "windows")]
+const TEST_FILTERS: &[(&str, &str)] = &[
+    (r".*\\resources\\test\\fixtures\\", "[BASEPATH]\\"),
+    (r"\\", "/"),
+];
+
 #[test]
 fn check_project_include_defaults() {
     // Defaults to checking the current working directory
@@ -18,7 +26,7 @@ fn check_project_include_defaults() {
     // The nested project should all be checked instead of respecting the parent includes
 
     insta::with_settings!({
-        filters => vec![(".*/resources/test/fixtures/", "[BASEPATH]/")]
+        filters => TEST_FILTERS.to_vec()
     }, {
         assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
         .args(["check", "--show-files"]).current_dir(Path::new("./resources/test/fixtures/include-test")), @r###"
@@ -40,7 +48,7 @@ fn check_project_respects_direct_paths() {
     // Given a direct path not included in the project `includes`, it should be checked
 
     insta::with_settings!({
-        filters => vec![(".*/resources/test/fixtures/", "[BASEPATH]/")]
+        filters => TEST_FILTERS.to_vec()
     }, {
         assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
         .args(["check", "--show-files", "b.py"]).current_dir(Path::new("./resources/test/fixtures/include-test")), @r###"
@@ -59,7 +67,7 @@ fn check_project_respects_subdirectory_includes() {
     // Given a direct path to a subdirectory, the include should be respected
 
     insta::with_settings!({
-        filters => vec![(".*/resources/test/fixtures/", "[BASEPATH]/")]
+        filters => TEST_FILTERS.to_vec()
     }, {
         assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
         .args(["check", "--show-files", "subdirectory"]).current_dir(Path::new("./resources/test/fixtures/include-test")), @r###"
@@ -78,7 +86,7 @@ fn check_project_from_project_subdirectory_respects_includes() {
     // Run from a project subdirectory, the include specified in the parent directory should be respected
 
     insta::with_settings!({
-        filters => vec![(".*/resources/test/fixtures/", "[BASEPATH]/")]
+        filters => TEST_FILTERS.to_vec()
     }, {
         assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
         .args(["check", "--show-files"]).current_dir(Path::new("./resources/test/fixtures/include-test/subdirectory")), @r###"
