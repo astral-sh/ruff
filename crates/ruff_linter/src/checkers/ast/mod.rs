9fb238a63f92322ba3addcabae4c314fcabe985a
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-11-28 18:25:12 -0600

    Remove from preview rule

--- crates/ruff_linter/src/checkers/ast/mod.rs
+++ crates/ruff_linter/src/checkers/ast/mod.rs
@@ -273,17 +273,14 @@ where
 
         // For Jupyter Notebooks, we'll reset the `IMPORT_BOUNDARY` flag when
         // we encounter a cell boundary.
-        if self.settings.preview.is_enabled()
-            && self.source_type.is_ipynb()
+        if self.source_type.is_ipynb()
             && self.semantic.at_top_level()
             && self.semantic.seen_import_boundary()
             && self.cell_offsets.is_some_and(|cell_offsets| {
                 cell_offsets.has_cell_boundary(TextRange::new(self.last_stmt_end, stmt.start()))
             })
         {
-            self.semantic
-                .flags
-                .remove(SemanticModelFlags::IMPORT_BOUNDARY);
+            self.semantic.flags -= SemanticModelFlags::IMPORT_BOUNDARY;
         }
 
         // Track whether we've seen docstrings, non-imports, etc.
--- crates/ruff_linter/src/rules/pycodestyle/mod.rs
+++ crates/ruff_linter/src/rules/pycodestyle/mod.rs
@@ -37,6 +37,7 @@ mod tests {
     #[test_case(Rule::MixedSpacesAndTabs, Path::new("E101.py"))]
     #[test_case(Rule::ModuleImportNotAtTopOfFile, Path::new("E40.py"))]
     #[test_case(Rule::ModuleImportNotAtTopOfFile, Path::new("E402.py"))]
+    #[test_case(Rule::ModuleImportNotAtTopOfFile, Path::new("E402.ipynb"))]
     #[test_case(Rule::MultipleImportsOnOneLine, Path::new("E40.py"))]
     #[test_case(Rule::MultipleStatementsOnOneLineColon, Path::new("E70.py"))]
     #[test_case(Rule::MultipleStatementsOnOneLineSemicolon, Path::new("E70.py"))]
@@ -66,7 +67,6 @@ mod tests {
 
     #[test_case(Rule::IsLiteral, Path::new("constant_literals.py"))]
     #[test_case(Rule::TypeComparison, Path::new("E721.py"))]
-    #[test_case(Rule::ModuleImportNotAtTopOfFile, Path::new("E402.ipynb"))]
     fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!(
             "preview__{}_{}",
