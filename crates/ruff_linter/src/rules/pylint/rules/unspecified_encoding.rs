64c2535e28a121d67ad0f162ef35d31b7b3c2290
qdegraaf <34540841+qdegraaf@users.noreply.github.com>
2023-12-01 19:23:56 +0100

    [`pylint`] Add `add_argument` utility and autofix for `PLW1514` (#8928)
    ## Summary
    
    - Adds `add_argument` similar to existing `remove_argument` utility to
    safely add arguments to functions.
    - Adds autofix for `PLW1514` as per specs requested in
    https://github.com/astral-sh/ruff/issues/8883 as a test
    
    ## Test Plan
    
    Checks on existing fixtures as well as additional test and fixture for
    Python 3.9 and lower fix
    
    ## Issue Link
    
    Closes: https://github.com/astral-sh/ruff/issues/8883

--- crates/ruff_linter/src/rules/pylint/rules/unspecified_encoding.rs
+++ crates/ruff_linter/src/rules/pylint/rules/unspecified_encoding.rs
@@ -1,10 +1,16 @@
-use ruff_diagnostics::{Diagnostic, Violation};
+use anyhow::Result;
+
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast as ast;
 use ruff_python_ast::call_path::{format_call_path, CallPath};
-use ruff_text_size::Ranged;
+use ruff_python_ast::Expr;
+use ruff_text_size::{Ranged, TextRange};
 
 use crate::checkers::ast::Checker;
+use crate::fix::edits::add_argument;
+use crate::importer::ImportRequest;
+use crate::settings::types::PythonVersion;
 
 /// ## What it does
 /// Checks for uses of `open` and related calls without an explicit `encoding`
@@ -15,7 +21,9 @@ use crate::checkers::ast::Checker;
 /// non-portable code, with differing behavior across platforms.
 ///
 /// Instead, consider using the `encoding` parameter to enforce a specific
-/// encoding.
+/// encoding. [PEP 597] recommends using `locale.getpreferredencoding(False)`
+/// as the default encoding on versions earlier than Python 3.10, and
+/// `encoding="locale"` on Python 3.10 and later.
 ///
 /// ## Example
 /// ```python
@@ -29,13 +37,15 @@ use crate::checkers::ast::Checker;
 ///
 /// ## References
 /// - [Python documentation: `open`](https://docs.python.org/3/library/functions.html#open)
+///
+/// [PEP 597]: https://peps.python.org/pep-0597/
 #[violation]
 pub struct UnspecifiedEncoding {
     function_name: String,
     mode: Mode,
 }
 
-impl Violation for UnspecifiedEncoding {
+impl AlwaysFixableViolation for UnspecifiedEncoding {
     #[derive_message_formats]
     fn message(&self) -> String {
         let UnspecifiedEncoding {
@@ -51,6 +61,10 @@ impl Violation for UnspecifiedEncoding {
                 format!("`{function_name}` without explicit `encoding` argument")
             }
         }
+    }
+
+    fn fix_title(&self) -> String {
+        format!("Add explicit `encoding` argument")
     }
 }
 
@@ -70,17 +84,63 @@ pub(crate) fn unspecified_encoding(checker: &mut Checker, call: &ast::ExprCall) 
         return;
     };
 
-    checker.diagnostics.push(Diagnostic::new(
+    let mut diagnostic = Diagnostic::new(
         UnspecifiedEncoding {
             function_name,
             mode,
         },
         call.func.range(),
-    ));
+    );
+
+    if checker.settings.target_version >= PythonVersion::Py310 {
+        diagnostic.set_fix(generate_keyword_fix(checker, call));
+    } else {
+        diagnostic.try_set_fix(|| generate_import_fix(checker, call));
+    }
+
+    checker.diagnostics.push(diagnostic);
+}
+
+/// Generate an [`Edit`] for Python 3.10 and later.
+fn generate_keyword_fix(checker: &Checker, call: &ast::ExprCall) -> Fix {
+    Fix::unsafe_edit(add_argument(
+        &format!(
+            "encoding={}",
+            checker
+                .generator()
+                .expr(&Expr::StringLiteral(ast::ExprStringLiteral {
+                    value: ast::StringLiteralValue::single(ast::StringLiteral {
+                        value: "locale".to_string(),
+                        unicode: false,
+                        range: TextRange::default(),
+                    }),
+                    range: TextRange::default(),
+                }))
+        ),
+        &call.arguments,
+        checker.indexer().comment_ranges(),
+        checker.locator().contents(),
+    ))
+}
+
+/// Generate an [`Edit`] for Python 3.9 and earlier.
+fn generate_import_fix(checker: &Checker, call: &ast::ExprCall) -> Result<Fix> {
+    let (import_edit, binding) = checker.importer().get_or_import_symbol(
+        &ImportRequest::import("locale", "getpreferredencoding"),
+        call.start(),
+        checker.semantic(),
+    )?;
+    let argument_edit = add_argument(
+        &format!("encoding={binding}(False)"),
+        &call.arguments,
+        checker.indexer().comment_ranges(),
+        checker.locator().contents(),
+    );
+    Ok(Fix::unsafe_edits(import_edit, [argument_edit]))
 }
 
 /// Returns `true` if the given expression is a string literal containing a `b` character.
-fn is_binary_mode(expr: &ast::Expr) -> Option<bool> {
+fn is_binary_mode(expr: &Expr) -> Option<bool> {
     Some(
         expr.as_string_literal_expr()?
             .value
@@ -92,12 +152,7 @@ fn is_binary_mode(expr: &ast::Expr) -> Option<bool> {
 /// Returns `true` if the given call lacks an explicit `encoding`.
 fn is_violation(call: &ast::ExprCall, call_path: &CallPath) -> bool {
     // If we have something like `*args`, which might contain the encoding argument, abort.
-    if call
-        .arguments
-        .args
-        .iter()
-        .any(ruff_python_ast::Expr::is_starred_expr)
-    {
+    if call.arguments.args.iter().any(Expr::is_starred_expr) {
         return false;
     }
     // If we have something like `**kwargs`, which might contain the encoding argument, abort.
