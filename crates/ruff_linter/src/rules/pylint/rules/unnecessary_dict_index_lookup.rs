b32cc3979454e86a175d079ca5cf16e4b90c661e
Steve C <diceroll123@gmail.com>
2023-11-30 22:51:47 -0500

    Make suggested changes

--- crates/ruff_linter/src/rules/pylint/rules/unnecessary_dict_index_lookup.rs
+++ crates/ruff_linter/src/rules/pylint/rules/unnecessary_dict_index_lookup.rs
@@ -10,10 +10,12 @@ use ruff_text_size::TextRange;
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for access of a dict value at the current index when iterating through the dict items.
+/// Checks for index-based dict accesses during iterations through
+/// the `dict.items()`.
 ///
 /// ## Why is this bad?
-/// It is more succinct to use the variable for the value at the current index which is already in scope from the iterator.
+/// It is more succinct to use the variable for the value at the current
+/// index which is already in scope from the iterator.
 ///
 /// ## Example
 /// ```python
@@ -40,102 +42,7 @@ impl AlwaysFixableViolation for UnnecessaryDictIndexLookup {
     }
 
     fn fix_title(&self) -> String {
-        format!("Use existing item variable instead")
-    }
-}
-
-struct SubscriptVisitor<'a> {
-    dict_name: &'a str,
-    index_name: &'a str,
-    diagnostic_ranges: Vec<TextRange>,
-    is_subcript_modified: bool,
-}
-
-impl<'a> SubscriptVisitor<'a> {
-    fn new(dict_name: &'a str, index_name: &'a str) -> Self {
-        Self {
-            dict_name,
-            index_name,
-            diagnostic_ranges: Vec::new(),
-            is_subcript_modified: false,
-        }
-    }
-}
-
-fn check_target_for_assignment(expr: &Expr, dict_name: &str, index_name: &str) -> bool {
-    // if we see the sequence subscript being modified, we'll stop emitting diagnostics
-    match expr {
-        Expr::Subscript(ast::ExprSubscript { value, slice, .. }) => {
-            let Expr::Name(ast::ExprName { id, .. }) = value.as_ref() else {
-                return false;
-            };
-            if id == dict_name {
-                let Expr::Name(ast::ExprName { id, .. }) = slice.as_ref() else {
-                    return false;
-                };
-                if id == index_name {
-                    return true;
-                }
-            }
-            false
-        }
-        _ => false,
-    }
-}
-
-impl<'a> Visitor<'_> for SubscriptVisitor<'a> {
-    fn visit_expr(&mut self, expr: &Expr) {
-        if self.is_subcript_modified {
-            return;
-        }
-        match expr {
-            Expr::Subscript(ast::ExprSubscript {
-                value,
-                slice,
-                range,
-                ..
-            }) => {
-                let Expr::Name(ast::ExprName { id, .. }) = value.as_ref() else {
-                    return;
-                };
-                if id == self.dict_name {
-                    let Expr::Name(ast::ExprName { id, .. }) = slice.as_ref() else {
-                        return;
-                    };
-                    if id == self.index_name {
-                        self.diagnostic_ranges.push(*range);
-                    }
-                }
-            }
-            _ => visitor::walk_expr(self, expr),
-        }
-    }
-
-    fn visit_stmt(&mut self, stmt: &Stmt) {
-        if self.is_subcript_modified {
-            return;
-        }
-        match stmt {
-            Stmt::Assign(ast::StmtAssign { targets, value, .. }) => {
-                self.is_subcript_modified = targets.iter().any(|target| {
-                    check_target_for_assignment(target, self.dict_name, self.index_name)
-                });
-                self.visit_expr(value);
-            }
-            Stmt::AnnAssign(ast::StmtAnnAssign { target, value, .. }) => {
-                if let Some(value) = value {
-                    self.is_subcript_modified =
-                        check_target_for_assignment(target, self.dict_name, self.index_name);
-                    self.visit_expr(value);
-                }
-            }
-            Stmt::AugAssign(ast::StmtAugAssign { target, value, .. }) => {
-                self.is_subcript_modified =
-                    check_target_for_assignment(target, self.dict_name, self.index_name);
-                self.visit_expr(value);
-            }
-            _ => visitor::walk_stmt(self, stmt),
-        }
+        format!("Use existing variable")
     }
 }
 
@@ -146,7 +53,7 @@ pub(crate) fn unnecessary_dict_index_lookup(checker: &mut Checker, stmt_for: &St
         return;
     };
 
-    let mut visitor = SubscriptVisitor::new(&dict_name, &index_name);
+    let mut visitor = SubscriptVisitor::new(dict_name, index_name);
 
     visitor.visit_body(&stmt_for.body);
     visitor.visit_body(&stmt_for.orelse);
@@ -155,7 +62,7 @@ pub(crate) fn unnecessary_dict_index_lookup(checker: &mut Checker, stmt_for: &St
         let mut diagnostic = Diagnostic::new(UnnecessaryDictIndexLookup, range);
 
         diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-            value_name.clone(),
+            value_name.to_string(),
             range,
         )));
 
@@ -165,52 +72,53 @@ pub(crate) fn unnecessary_dict_index_lookup(checker: &mut Checker, stmt_for: &St
 
 /// PLR1733
 pub(crate) fn unnecessary_dict_index_lookup_comprehension(checker: &mut Checker, expr: &Expr) {
-    match expr {
-        Expr::GeneratorExp(ast::ExprGeneratorExp {
-            elt, generators, ..
-        })
-        | Expr::DictComp(ast::ExprDictComp {
-            value: elt,
-            generators,
-            ..
-        })
-        | Expr::SetComp(ast::ExprSetComp {
-            elt, generators, ..
-        })
-        | Expr::ListComp(ast::ExprListComp {
-            elt, generators, ..
-        }) => {
-            for comp in generators {
-                let Some((dict_name, index_name, value_name)) =
-                    dict_items(&comp.iter, &comp.target)
-                else {
-                    continue;
-                };
+    let (Expr::GeneratorExp(ast::ExprGeneratorExp {
+        elt, generators, ..
+    })
+    | Expr::DictComp(ast::ExprDictComp {
+        value: elt,
+        generators,
+        ..
+    })
+    | Expr::SetComp(ast::ExprSetComp {
+        elt, generators, ..
+    })
+    | Expr::ListComp(ast::ExprListComp {
+        elt, generators, ..
+    })) = expr
+    else {
+        return;
+    };
 
-                let mut visitor = SubscriptVisitor::new(&dict_name, &index_name);
+    for comp in generators {
+        let Some((dict_name, index_name, value_name)) = dict_items(&comp.iter, &comp.target) else {
+            continue;
+        };
 
-                visitor.visit_expr(elt.as_ref());
-                for expr in &comp.ifs {
-                    visitor.visit_expr(expr);
-                }
+        let mut visitor = SubscriptVisitor::new(dict_name, index_name);
 
-                for range in visitor.diagnostic_ranges {
-                    let mut diagnostic = Diagnostic::new(UnnecessaryDictIndexLookup, range);
-
-                    diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-                        value_name.clone(),
-                        range,
-                    )));
-
-                    checker.diagnostics.push(diagnostic);
-                }
-            }
+        visitor.visit_expr(elt.as_ref());
+        for expr in &comp.ifs {
+            visitor.visit_expr(expr);
         }
-        _ => (),
+
+        for range in visitor.diagnostic_ranges {
+            let mut diagnostic = Diagnostic::new(UnnecessaryDictIndexLookup, range);
+
+            diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
+                value_name.to_string(),
+                range,
+            )));
+
+            checker.diagnostics.push(diagnostic);
+        }
     }
 }
 
-fn dict_items(call_expr: &Expr, tuple_expr: &Expr) -> Option<(String, String, String)> {
+fn dict_items<'a>(
+    call_expr: &'a Expr,
+    tuple_expr: &'a Expr,
+) -> Option<(&'a str, &'a str, &'a str)> {
     let ast::ExprCall {
         func, arguments, ..
     } = call_expr.as_call_expr()?;
@@ -250,5 +158,98 @@ fn dict_items(call_expr: &Expr, tuple_expr: &Expr) -> Option<(String, String, St
         return None;
     }
 
-    Some((dict_name.clone(), index_name.clone(), value_name.clone()))
+    Some((dict_name, index_name, value_name))
+}
+
+#[derive(Debug)]
+struct SubscriptVisitor<'a> {
+    dict_name: &'a str,
+    index_name: &'a str,
+    diagnostic_ranges: Vec<TextRange>,
+    modified: bool,
+}
+
+impl<'a> SubscriptVisitor<'a> {
+    fn new(dict_name: &'a str, index_name: &'a str) -> Self {
+        Self {
+            dict_name,
+            index_name,
+            diagnostic_ranges: Vec::new(),
+            modified: false,
+        }
+    }
+}
+
+impl SubscriptVisitor<'_> {
+    fn check_target(&self, expr: &Expr) -> bool {
+        match expr {
+            Expr::Subscript(ast::ExprSubscript { value, slice, .. }) => {
+                let Expr::Name(ast::ExprName { id, .. }) = value.as_ref() else {
+                    return false;
+                };
+                if id == self.dict_name {
+                    let Expr::Name(ast::ExprName { id, .. }) = slice.as_ref() else {
+                        return false;
+                    };
+                    if id == self.index_name {
+                        return true;
+                    }
+                }
+                false
+            }
+            _ => false,
+        }
+    }
+}
+
+impl<'a> Visitor<'_> for SubscriptVisitor<'a> {
+    fn visit_stmt(&mut self, stmt: &Stmt) {
+        if self.modified {
+            return;
+        }
+        match stmt {
+            Stmt::Assign(ast::StmtAssign { targets, value, .. }) => {
+                self.modified = targets.iter().any(|target| self.check_target(target));
+                self.visit_expr(value);
+            }
+            Stmt::AnnAssign(ast::StmtAnnAssign { target, value, .. }) => {
+                if let Some(value) = value {
+                    self.modified = self.check_target(target);
+                    self.visit_expr(value);
+                }
+            }
+            Stmt::AugAssign(ast::StmtAugAssign { target, value, .. }) => {
+                self.modified = self.check_target(target);
+                self.visit_expr(value);
+            }
+            _ => visitor::walk_stmt(self, stmt),
+        }
+    }
+
+    fn visit_expr(&mut self, expr: &Expr) {
+        if self.modified {
+            return;
+        }
+        match expr {
+            Expr::Subscript(ast::ExprSubscript {
+                value,
+                slice,
+                range,
+                ..
+            }) => {
+                let Expr::Name(ast::ExprName { id, .. }) = value.as_ref() else {
+                    return;
+                };
+                if id == self.dict_name {
+                    let Expr::Name(ast::ExprName { id, .. }) = slice.as_ref() else {
+                        return;
+                    };
+                    if id == self.index_name {
+                        self.diagnostic_ranges.push(*range);
+                    }
+                }
+            }
+            _ => visitor::walk_expr(self, expr),
+        }
+    }
 }
