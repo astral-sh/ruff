1dabc98ea7b05931a1ad1f3f7682b38451553afb
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-11-30 19:37:30 -0500

    Apply some minor changes to unnecessary-list-index-lookup

--- crates/ruff_linter/src/rules/pylint/rules/unnecessary_list_index_lookup.rs
+++ crates/ruff_linter/src/rules/pylint/rules/unnecessary_list_index_lookup.rs
@@ -4,15 +4,18 @@ use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::visitor;
 use ruff_python_ast::visitor::Visitor;
+use ruff_python_semantic::SemanticModel;
 use ruff_text_size::TextRange;
 
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for access of a list item at the current index when using enumeration.
+/// Checks for index-based list accesses during `enumerate` iterations.
 ///
 /// ## Why is this bad?
-/// It is more succinct to use the variable for the value at the current index which is already in scope from the iterator.
+/// When iterating over a list with `enumerate`, the current item is already
+/// available alongside its index. Using the index to look up the item is
+/// unnecessary.
 ///
 /// ## Example
 /// ```python
@@ -39,10 +42,127 @@ impl AlwaysFixableViolation for UnnecessaryListIndexLookup {
     }
 
     fn fix_title(&self) -> String {
-        format!("Use existing item variable instead")
+        format!("Use existing variable")
     }
 }
 
+/// PLR1736
+pub(crate) fn unnecessary_list_index_lookup(checker: &mut Checker, stmt_for: &StmtFor) {
+    let Some((sequence, index_name, value_name)) =
+        enumerate_items(&stmt_for.iter, &stmt_for.target, checker.semantic())
+    else {
+        return;
+    };
+
+    let ranges = {
+        let mut visitor = SubscriptVisitor::new(sequence, index_name);
+        visitor.visit_body(&stmt_for.body);
+        visitor.visit_body(&stmt_for.orelse);
+        visitor.diagnostic_ranges
+    };
+
+    for range in ranges {
+        let mut diagnostic = Diagnostic::new(UnnecessaryListIndexLookup, range);
+        diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
+            value_name.to_string(),
+            range,
+        )));
+        checker.diagnostics.push(diagnostic);
+    }
+}
+
+/// PLR1736
+pub(crate) fn unnecessary_list_index_lookup_comprehension(checker: &mut Checker, expr: &Expr) {
+    let (Expr::GeneratorExp(ast::ExprGeneratorExp {
+        elt, generators, ..
+    })
+    | Expr::DictComp(ast::ExprDictComp {
+        value: elt,
+        generators,
+        ..
+    })
+    | Expr::SetComp(ast::ExprSetComp {
+        elt, generators, ..
+    })
+    | Expr::ListComp(ast::ExprListComp {
+        elt, generators, ..
+    })) = expr
+    else {
+        return;
+    };
+
+    for comp in generators {
+        let Some((sequence, index_name, value_name)) =
+            enumerate_items(&comp.iter, &comp.target, checker.semantic())
+        else {
+            return;
+        };
+
+        let ranges = {
+            let mut visitor = SubscriptVisitor::new(sequence, index_name);
+            visitor.visit_expr(elt.as_ref());
+            visitor.diagnostic_ranges
+        };
+
+        for range in ranges {
+            let mut diagnostic = Diagnostic::new(UnnecessaryListIndexLookup, range);
+            diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
+                value_name.to_string(),
+                range,
+            )));
+            checker.diagnostics.push(diagnostic);
+        }
+    }
+}
+
+fn enumerate_items<'a>(
+    call_expr: &'a Expr,
+    tuple_expr: &'a Expr,
+    semantic: &SemanticModel,
+) -> Option<(&'a str, &'a str, &'a str)> {
+    let ast::ExprCall {
+        func, arguments, ..
+    } = call_expr.as_call_expr()?;
+
+    // Check that the function is the `enumerate` builtin.
+    if !semantic
+        .resolve_call_path(func.as_ref())
+        .is_some_and(|call_path| matches!(call_path.as_slice(), ["builtins" | "", "enumerate"]))
+    {
+        return None;
+    }
+
+    let Expr::Tuple(ast::ExprTuple { elts, .. }) = tuple_expr else {
+        return None;
+    };
+    let [index, value] = elts.as_slice() else {
+        return None;
+    };
+
+    // Grab the variable names.
+    let Expr::Name(ast::ExprName { id: index_name, .. }) = index else {
+        return None;
+    };
+
+    let Expr::Name(ast::ExprName { id: value_name, .. }) = value else {
+        return None;
+    };
+
+    // If either of the variable names are intentionally ignored by naming them `_`, then don't
+    // emit.
+    if index_name == "_" || value_name == "_" {
+        return None;
+    }
+
+    // Get the first argument of the enumerate call.
+    let Some(Expr::Name(ast::ExprName { id: sequence, .. })) = arguments.args.first() else {
+        return None;
+    };
+
+    Some((sequence, index_name, value_name))
+}
+
+#[derive(Debug)]
 struct SubscriptVisitor<'a> {
     sequence_name: &'a str,
     index_name: &'a str,
@@ -61,29 +181,60 @@ impl<'a> SubscriptVisitor<'a> {
     }
 }
 
-fn check_target_for_assignment(expr: &Expr, sequence_name: &str, index_name: &str) -> bool {
-    // if we see the sequence, a subscript, or the index being modified, we'll stop emitting diagnostics
-    match expr {
-        Expr::Name(ast::ExprName { id, .. }) => id == sequence_name || id == index_name,
-        Expr::Subscript(ast::ExprSubscript { value, slice, .. }) => {
-            let Expr::Name(ast::ExprName { id, .. }) = value.as_ref() else {
-                return false;
-            };
-            if id == sequence_name {
-                let Expr::Name(ast::ExprName { id, .. }) = slice.as_ref() else {
+impl SubscriptVisitor<'_> {
+    fn is_assignment(&self, expr: &Expr) -> bool {
+        // If we see the sequence, a subscript, or the index being modified, we'll stop emitting
+        // diagnostics.
+        match expr {
+            Expr::Name(ast::ExprName { id, .. }) => {
+                id == self.sequence_name || id == self.index_name
+            }
+            Expr::Subscript(ast::ExprSubscript { value, slice, .. }) => {
+                let Expr::Name(ast::ExprName { id, .. }) = value.as_ref() else {
                     return false;
                 };
-                if id == index_name {
-                    return true;
+                if id == self.sequence_name {
+                    let Expr::Name(ast::ExprName { id, .. }) = slice.as_ref() else {
+                        return false;
+                    };
+                    if id == self.index_name {
+                        return true;
+                    }
                 }
+                false
             }
-            false
+            _ => false,
         }
-        _ => false,
     }
 }
 
 impl<'a> Visitor<'_> for SubscriptVisitor<'a> {
+    fn visit_stmt(&mut self, stmt: &Stmt) {
+        if self.modified {
+            return;
+        }
+        match stmt {
+            Stmt::Assign(ast::StmtAssign { targets, value, .. }) => {
+                self.modified = targets.iter().any(|target| self.is_assignment(target));
+                self.visit_expr(value);
+            }
+            Stmt::AnnAssign(ast::StmtAnnAssign { target, value, .. }) => {
+                if let Some(value) = value {
+                    self.modified = self.is_assignment(target);
+                    self.visit_expr(value);
+                }
+            }
+            Stmt::AugAssign(ast::StmtAugAssign { target, value, .. }) => {
+                self.modified = self.is_assignment(target);
+                self.visit_expr(value);
+            }
+            Stmt::Delete(ast::StmtDelete { targets, .. }) => {
+                self.modified = targets.iter().any(|target| self.is_assignment(target));
+            }
+            _ => visitor::walk_stmt(self, stmt),
+        }
+    }
+
     fn visit_expr(&mut self, expr: &Expr) {
         if self.modified {
             return;
@@ -110,171 +261,4 @@ impl<'a> Visitor<'_> for SubscriptVisitor<'a> {
             _ => visitor::walk_expr(self, expr),
         }
     }
-
-    fn visit_stmt(&mut self, stmt: &Stmt) {
-        if self.modified {
-            return;
-        }
-        match stmt {
-            Stmt::Assign(ast::StmtAssign { targets, value, .. }) => {
-                self.modified = targets.iter().any(|target| {
-                    check_target_for_assignment(target, self.sequence_name, self.index_name)
-                });
-                self.visit_expr(value);
-            }
-            Stmt::AnnAssign(ast::StmtAnnAssign { target, value, .. }) => {
-                if let Some(value) = value {
-                    self.modified =
-                        check_target_for_assignment(target, self.sequence_name, self.index_name);
-                    self.visit_expr(value);
-                }
-            }
-            Stmt::AugAssign(ast::StmtAugAssign { target, value, .. }) => {
-                self.modified =
-                    check_target_for_assignment(target, self.sequence_name, self.index_name);
-                self.visit_expr(value);
-            }
-            Stmt::Delete(ast::StmtDelete { targets, .. }) => {
-                self.modified = targets.iter().any(|target| match target {
-                    Expr::Name(ast::ExprName { id, .. }) => {
-                        id == self.sequence_name || id == self.index_name
-                    }
-                    Expr::Subscript(ast::ExprSubscript { value, slice, .. }) => {
-                        let Expr::Name(ast::ExprName { id, .. }) = value.as_ref() else {
-                            return false;
-                        };
-                        if id == self.sequence_name {
-                            let Expr::Name(ast::ExprName { id, .. }) = slice.as_ref() else {
-                                return false;
-                            };
-                            if id == self.index_name {
-                                return true;
-                            }
-                        }
-                        false
-                    }
-                    _ => false,
-                });
-            }
-            _ => visitor::walk_stmt(self, stmt),
-        }
-    }
-}
-
-/// PLR1736
-pub(crate) fn unnecessary_list_index_lookup(checker: &mut Checker, stmt_for: &StmtFor) {
-    let Some((sequence, index_name, value_name)) =
-        enumerate_items(checker, &stmt_for.iter, &stmt_for.target)
-    else {
-        return;
-    };
-
-    let mut visitor = SubscriptVisitor::new(&sequence, &index_name);
-
-    visitor.visit_body(&stmt_for.body);
-    visitor.visit_body(&stmt_for.orelse);
-
-    for range in visitor.diagnostic_ranges {
-        let mut diagnostic = Diagnostic::new(UnnecessaryListIndexLookup, range);
-
-        diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-            value_name.clone(),
-            range,
-        )));
-
-        checker.diagnostics.push(diagnostic);
-    }
-}
-
-/// PLR1736
-pub(crate) fn unnecessary_list_index_lookup_comprehension(checker: &mut Checker, expr: &Expr) {
-    match expr {
-        Expr::GeneratorExp(ast::ExprGeneratorExp {
-            elt, generators, ..
-        })
-        | Expr::DictComp(ast::ExprDictComp {
-            value: elt,
-            generators,
-            ..
-        })
-        | Expr::SetComp(ast::ExprSetComp {
-            elt, generators, ..
-        })
-        | Expr::ListComp(ast::ExprListComp {
-            elt, generators, ..
-        }) => {
-            for comp in generators {
-                let Some((sequence, index_name, value_name)) =
-                    enumerate_items(checker, &comp.iter, &comp.target)
-                else {
-                    return;
-                };
-
-                let mut visitor = SubscriptVisitor::new(&sequence, &index_name);
-
-                visitor.visit_expr(elt.as_ref());
-
-                for range in visitor.diagnostic_ranges {
-                    let mut diagnostic = Diagnostic::new(UnnecessaryListIndexLookup, range);
-
-                    diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-                        value_name.clone(),
-                        range,
-                    )));
-
-                    checker.diagnostics.push(diagnostic);
-                }
-            }
-        }
-        _ => (),
-    }
-}
-
-fn enumerate_items(
-    checker: &mut Checker,
-    call_expr: &Expr,
-    tuple_expr: &Expr,
-) -> Option<(String, String, String)> {
-    let ast::ExprCall {
-        func, arguments, ..
-    } = call_expr.as_call_expr()?;
-
-    // Check that the function is the `enumerate` builtin.
-    let Some(call_path) = checker.semantic().resolve_call_path(func.as_ref()) else {
-        return None;
-    };
-
-    match call_path.as_slice() {
-        ["", "enumerate"] => (),
-        ["builtins", "enumerate"] => (),
-        _ => return None,
-    }
-
-    let Expr::Tuple(ast::ExprTuple { elts, .. }) = tuple_expr else {
-        return None;
-    };
-    let [index, value] = elts.as_slice() else {
-        return None;
-    };
-
-    // Grab the variable names
-    let Expr::Name(ast::ExprName { id: index_name, .. }) = index else {
-        return None;
-    };
-
-    let Expr::Name(ast::ExprName { id: value_name, .. }) = value else {
-        return None;
-    };
-
-    // If either of the variable names are intentionally ignored by naming them `_`, then don't emit
-    if index_name == "_" || value_name == "_" {
-        return None;
-    }
-
-    // Get the first argument of the enumerate call
-    let Some(Expr::Name(ast::ExprName { id: sequence, .. })) = arguments.args.first() else {
-        return None;
-    };
-
-    Some((sequence.clone(), index_name.clone(), value_name.clone()))
 }
