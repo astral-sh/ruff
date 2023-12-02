d8ad85396a1a6dad813eef8959acee4cdf393062
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-12-02 13:11:08 -0500

    Avoid unnecessary index diagnostics when value is modified

--- crates/ruff_linter/resources/test/fixtures/pylint/unnecessary_list_index_lookup.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/unnecessary_list_index_lookup.py
@@ -12,7 +12,7 @@ def fix_these():
         print(letters[index])  # PLR1736
         blah = letters[index]  # PLR1736
         assert letters[index]  == "d"  # PLR1736
-    
+
     for index, letter in builtins.enumerate(letters):
         print(letters[index])  # PLR1736
         blah = letters[index]  # PLR1736
@@ -22,38 +22,43 @@ def fix_these():
 def dont_fix_these():
     # once there is an assignment to the sequence[index], we stop emitting diagnostics
     for index, letter in enumerate(letters):
-        letters[index] = "d"  # Ok
-        letters[index] += "e"  # Ok
-        assert letters[index] == "de"  # Ok
-    
+        letters[index] = "d"  # OK
+        letters[index] += "e"  # OK
+        assert letters[index] == "de"  # OK
+
     # once there is an assignment to the index, we stop emitting diagnostics
     for index, letter in enumerate(letters):
-        index += 1  # Ok
-        print(letters[index])  # Ok
-    
+        index += 1  # OK
+        print(letters[index])  # OK
+
     # once there is an assignment to the sequence, we stop emitting diagnostics
     for index, letter in enumerate(letters):
-        letters = ["d", "e", "f"]  # Ok
-        print(letters[index])  # Ok
+        letters = ["d", "e", "f"]  # OK
+        print(letters[index])  # OK
+
+    # once there is an assignment to the value, we stop emitting diagnostics
+    for index, letter in enumerate(letters):
+        letter = "d"
+        print(letters[index])  # OK
 
     # once there is an deletion from or of the sequence or index, we stop emitting diagnostics
     for index, letter in enumerate(letters):
-        del letters[index]  # Ok
-        print(letters[index])  # Ok
+        del letters[index]  # OK
+        print(letters[index])  # OK
     for index, letter in enumerate(letters):
-        del letters  # Ok
-        print(letters[index])  # Ok
+        del letters  # OK
+        print(letters[index])  # OK
     for index, letter in enumerate(letters):
-        del index  # Ok
-        print(letters[index])  # Ok
+        del index  # OK
+        print(letters[index])  # OK
 
 
 def value_intentionally_unused():
-    [letters[index] for index, _ in enumerate(letters)]  # Ok
-    {letters[index] for index, _ in enumerate(letters)}  # Ok
-    {index: letters[index] for index, _ in enumerate(letters)}  # Ok
+    [letters[index] for index, _ in enumerate(letters)]  # OK
+    {letters[index] for index, _ in enumerate(letters)}  # OK
+    {index: letters[index] for index, _ in enumerate(letters)}  # OK
 
     for index, _ in enumerate(letters):
-        print(letters[index])  # Ok
-        blah = letters[index]  # Ok
-        letters[index] = "d"  # Ok
+        print(letters[index])  # OK
+        blah = letters[index]  # OK
+        letters[index] = "d"  # OK
