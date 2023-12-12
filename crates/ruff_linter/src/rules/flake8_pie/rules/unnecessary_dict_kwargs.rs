3f2fd98511811a042a4a1497da01b08f7a5f00e4
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-12-12 18:12:20 -0500

    Avoid fix for any duplicates

--- crates/ruff_linter/src/rules/flake8_pie/rules/unnecessary_dict_kwargs.rs
+++ crates/ruff_linter/src/rules/flake8_pie/rules/unnecessary_dict_kwargs.rs
@@ -3,7 +3,7 @@ use std::hash::BuildHasherDefault;
 use itertools::Itertools;
 use rustc_hash::FxHashSet;
 
-use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
+use ruff_diagnostics::{Diagnostic, Edit, Fix, FixAvailability, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::{self as ast, Expr};
 use ruff_python_stdlib::identifiers::is_identifier;
@@ -37,47 +37,31 @@ use crate::fix::edits::{remove_argument, Parentheses};
 /// print(foo(bar=2))  # prints 3
 /// ```
 ///
-/// ## Fix safety
-/// This rule's fix is marked as safe, but there are some scenarios
-/// that fix causes the TypeError at runtime:
-///
-/// ```python
-/// def foo(bar):
-///     return bar + 1
-///
-///
-/// # TypeError: foo() got multiple values for keyword argument 'bar'
-/// foo(bar=2, **{"bar": 3})
-/// ```
-///
 /// ## References
 /// - [Python documentation: Dictionary displays](https://docs.python.org/3/reference/expressions.html#dictionary-displays)
 /// - [Python documentation: Calls](https://docs.python.org/3/reference/expressions.html#calls)
 #[violation]
 pub struct UnnecessaryDictKwargs;
 
-impl AlwaysFixableViolation for UnnecessaryDictKwargs {
+impl Violation for UnnecessaryDictKwargs {
+    const FIX_AVAILABILITY: FixAvailability = FixAvailability::Sometimes;
+
     #[derive_message_formats]
     fn message(&self) -> String {
         format!("Unnecessary `dict` kwargs")
     }
 
-    fn fix_title(&self) -> String {
-        format!("Remove unnecessary kwargs")
+    fn fix_title(&self) -> Option<String> {
+        Some(format!("Remove unnecessary kwargs"))
     }
 }
 
 /// PIE804
 pub(crate) fn unnecessary_dict_kwargs(checker: &mut Checker, call: &ast::ExprCall) {
-    let mut seen = FxHashSet::with_capacity_and_hasher(
-        call.arguments.keywords.len(),
-        BuildHasherDefault::default(),
-    );
-
+    let mut duplicate_keywords = None;
     for keyword in &call.arguments.keywords {
-        // keyword is a spread operator (indicated by None)
-        if let Some(name) = &keyword.arg {
-            seen.insert(name.as_str());
+        // keyword is a spread operator (indicated by None).
+        if keyword.arg.is_some() {
             continue;
         }
 
@@ -87,7 +71,7 @@ pub(crate) fn unnecessary_dict_kwargs(checker: &mut Checker, call: &ast::ExprCal
 
         // Ex) `foo(**{**bar})`
         if matches!(keys.as_slice(), [None]) {
-            let mut diagnostic = Diagnostic::new(UnnecessaryDictKwargs, call.range());
+            let mut diagnostic = Diagnostic::new(UnnecessaryDictKwargs, keyword.range());
 
             diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
                 format!("**{}", checker.locator().slice(values[0].range())),
@@ -103,13 +87,12 @@ pub(crate) fn unnecessary_dict_kwargs(checker: &mut Checker, call: &ast::ExprCal
         let kwargs = keys
             .iter()
             .filter_map(|key| key.as_ref().and_then(as_kwarg))
-            .filter(|name| seen.insert(name))
             .collect::<Vec<_>>();
         if kwargs.len() != keys.len() {
             continue;
         }
 
-        let mut diagnostic = Diagnostic::new(UnnecessaryDictKwargs, call.range());
+        let mut diagnostic = Diagnostic::new(UnnecessaryDictKwargs, keyword.range());
 
         if values.is_empty() {
             diagnostic.try_set_fix(|| {
@@ -122,20 +105,62 @@ pub(crate) fn unnecessary_dict_kwargs(checker: &mut Checker, call: &ast::ExprCal
                 .map(Fix::safe_edit)
             });
         } else {
-            diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-                kwargs
+            // Compute the set of duplicate keywords (lazily).
+            if duplicate_keywords.is_none() {
+                duplicate_keywords = Some(duplicates(call));
+            }
+
+            // Avoid fixing if doing so could introduce a duplicate keyword argument.
+            if let Some(duplicate_keywords) = duplicate_keywords.as_ref() {
+                if kwargs
                     .iter()
-                    .zip(values.iter())
-                    .map(|(kwarg, value)| {
-                        format!("{}={}", kwarg, checker.locator().slice(value.range()))
-                    })
-                    .join(", "),
-                keyword.range(),
-            )));
+                    .all(|kwarg| !duplicate_keywords.contains(kwarg))
+                {
+                    diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
+                        kwargs
+                            .iter()
+                            .zip(values.iter())
+                            .map(|(kwarg, value)| {
+                                format!("{}={}", kwarg, checker.locator().slice(value.range()))
+                            })
+                            .join(", "),
+                        keyword.range(),
+                    )));
+                }
+            }
         }
 
         checker.diagnostics.push(diagnostic);
     }
+}
+
+/// Determine the set of keywords that appear in multiple positions (either directly, as in
+/// `func(x=1)`, or indirectly, as in `func(**{"x": 1})`).
+fn duplicates(call: &ast::ExprCall) -> FxHashSet<&str> {
+    let mut seen = FxHashSet::with_capacity_and_hasher(
+        call.arguments.keywords.len(),
+        BuildHasherDefault::default(),
+    );
+    let mut duplicates = FxHashSet::with_capacity_and_hasher(
+        call.arguments.keywords.len(),
+        BuildHasherDefault::default(),
+    );
+    for keyword in &call.arguments.keywords {
+        if let Some(name) = &keyword.arg {
+            if !seen.insert(name.as_str()) {
+                duplicates.insert(name.as_str());
+            }
+        } else if let Expr::Dict(ast::ExprDict { keys, .. }) = &keyword.value {
+            for key in keys {
+                if let Some(name) = key.as_ref().and_then(as_kwarg) {
+                    if !seen.insert(name) {
+                        duplicates.insert(name);
+                    }
+                }
+            }
+        }
+    }
+    duplicates
 }
 
 /// Return `Some` if a key is a valid keyword argument name, or `None` otherwise.
