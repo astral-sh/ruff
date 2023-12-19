2c8c450d0aec3219cc892ce8ca1aecf9b3476cda
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-12-19 00:11:11 -0600

    Add test case for docstring with comments

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/blank_line_before_class_docstring.py
@@ -0,0 +1,38 @@
+class NormalDocstring:
+
+    """This is a docstring."""
+
+
+class DocstringWithComment0:
+    # This is a comment
+    """This is a docstring."""
+
+
+class DocstringWithComment1:
+    # This is a comment
+
+    """This is a docstring."""
+
+
+class DocstringWithComment2:
+
+    # This is a comment
+    """This is a docstring."""
+
+
+class DocstringWithComment3:
+
+    # This is a comment
+
+    """This is a docstring."""
+
+
+class DocstringWithComment4:
+
+
+    # This is a comment
+
+
+    """This is a docstring."""
+
+
