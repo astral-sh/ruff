5d41c84ef71d0581d2e2a63cb7b768269ee53c9b
asafamr-mm <112866104+asafamr-mm@users.noreply.github.com>
2023-12-20 22:26:07 +0200

    SIM300: CONSTANT_CASE variables are improperly flagged for yoda violation (#9164)
    ## Summary
    
    fixes #6956 
    details in issue
    
    Following an advice in
    https://github.com/astral-sh/ruff/issues/6956#issuecomment-1817672585,
    this change separates expressions to 3 levels of "constant likelihood":
    *  literals, empty dict and tuples... (definitely constant, level 2)
    *  CONSTANT_CASE vars (probably constant, 1)
    * all other expressions (0)
    
    a comparison is marked yoda if the level is strictly higher on its left
    hand side
    
    following
    https://github.com/astral-sh/ruff/issues/6956#issuecomment-1697107822
    marking compound expressions of literals (e.g. `60 * 60` ) as constants
    this change current behaviour on
    `SomeClass().settings.SOME_CONSTANT_VALUE > (60 * 60)` in the fixture
    from error to ok

--- crates/ruff_linter/resources/test/fixtures/flake8_simplify/SIM300.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_simplify/SIM300.py
@@ -1,6 +1,5 @@
 # Errors
 "yoda" == compare  # SIM300
-"yoda" == compare  # SIM300
 42 == age  # SIM300
 ("a", "b") == compare  # SIM300
 "yoda" <= compare  # SIM300
@@ -13,9 +12,16 @@ YODA > age  # SIM300
 YODA >= age  # SIM300
 JediOrder.YODA == age  # SIM300
 0 < (number - 100)  # SIM300
-SomeClass().settings.SOME_CONSTANT_VALUE > (60 * 60)  # SIM300
 B<A[0][0]or B
 B or(B)<A[0][0]
+
+# Errors in preview
+['upper'] == UPPER_LIST
+{} == DummyHandler.CONFIG
+
+# Errors in stable
+UPPER_LIST == ['upper']
+DummyHandler.CONFIG == {}
 
 # OK
 compare == "yoda"
@@ -31,3 +37,6 @@ age <= YODA
 YODA == YODA
 age == JediOrder.YODA
 (number - 100) > 0
+SECONDS_IN_DAY == 60 * 60 * 24 # Error in 0.1.8
+SomeClass().settings.SOME_CONSTANT_VALUE > (60 * 60) # Error in 0.1.8
+{"non-empty-dict": "is-ok"} == DummyHandler.CONFIG
