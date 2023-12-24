a5153cbb8036111489fe0c6560a4673f39245dd1
Steve C <diceroll123@gmail.com>
2023-12-24 17:54:25 -0500

    [pylint] - implement W0128/redeclared-assigned-name
    Update redeclared_assigned_name.rs

---
+++ crates/ruff_linter/src/rules/pylint/rules/redeclared_assigned_name.rs
@@ -0,0 +1,59 @@
+use ruff_python_ast::{self as ast, Expr};
+
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, violation};
+use ruff_text_size::Ranged;
+
+use crate::checkers::ast::Checker;
+
+/// ## What it does
+/// Checks for declared assignments to the same variable multiple times
+/// in the same assignment.
+///
+/// ## Why is this bad?
+/// Assigning a variable multiple times in the same assignment is redundant,
+/// as the final assignment to the variable is what the value will be.
+///
+#[violation]
+pub struct RedeclaredAssignedName {
+    name: String,
+}
+
+impl Violation for RedeclaredAssignedName {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let RedeclaredAssignedName { name } = self;
+        format!("Redeclared variable `{name}` in assignment")
+    }
+}
+
+/// PLW0128
+pub(crate) fn redeclared_assigned_name(checker: &mut Checker, targets: &Vec<Expr>) {
+    let mut names: Vec<String> = Vec::new();
+
+    for target in targets {
+        check_expr(checker, target, &mut names);
+    }
+}
+
+fn check_expr(checker: &mut Checker, expr: &Expr, names: &mut Vec<String>) {
+    match expr {
+        Expr::Tuple(ast::ExprTuple { elts, .. }) => {
+            for target in elts {
+                check_expr(checker, target, names);
+            }
+        }
+        Expr::Name(ast::ExprName { id, .. }) => {
+            if names.contains(id) {
+                checker.diagnostics.push(Diagnostic::new(
+                    RedeclaredAssignedName {
+                        name: id.to_string(),
+                    },
+                    expr.range(),
+                ));
+            }
+            names.push(id.to_string());
+        }
+        _ => {}
+    }
+}
