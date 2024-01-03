2745f771006eee226a43d8885dce753df76c4ae4
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-01-03 10:57:44 -0500

    Update fixtures

--- crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI058.pyi
+++ crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI058.pyi
@@ -1,68 +1,128 @@
-import collections.abc
-import typing
-import typing_extensions
-from collections.abc import AsyncGenerator, Generator
-from typing import Any
+def scope():
+    from collections.abc import Generator
 
-class IteratorReturningSimpleGenerator1:
-    def __iter__(self) -> Generator: ...  # PYI058 (use `Iterator`)
+    class IteratorReturningSimpleGenerator1:
+        def __iter__(self) -> Generator: ...  # PYI058 (use `Iterator`)
 
-class IteratorReturningSimpleGenerator2:
-    def __iter__(self) -> typing.Generator: ...  # PYI058 (use `Iterator`)
+def scope():
+    import typing
 
-class IteratorReturningSimpleGenerator3:
-    def __iter__(self) -> collections.abc.Generator: ...  # PYI058 (use `Iterator`)
+    class IteratorReturningSimpleGenerator2:
+        def __iter__(self) -> typing.Generator: ...  # PYI058 (use `Iterator`)
 
-class IteratorReturningSimpleGenerator4:
-    def __iter__(self, /) -> collections.abc.Generator[str, Any, None]: ...  # PYI058 (use `Iterator`)
+def scope():
+    import collections.abc
 
-class IteratorReturningSimpleGenerator5:
-    def __iter__(self, /) -> collections.abc.Generator[str, None, typing.Any]: ...  # PYI058 (use `Iterator`)
+    class IteratorReturningSimpleGenerator3:
+        def __iter__(self) -> collections.abc.Generator: ...  # PYI058 (use `Iterator`)
 
-class IteratorReturningSimpleGenerator6:
-    def __iter__(self, /) -> Generator[str, None, None]: ...  # PYI058 (use `Iterator`)
+def scope():
+    import collections.abc
+    from typing import Any
 
-class AsyncIteratorReturningSimpleAsyncGenerator1:
-    def __aiter__(self) -> typing_extensions.AsyncGenerator: ...  # PYI058 (Use `AsyncIterator`)
+    class IteratorReturningSimpleGenerator4:
+        def __iter__(self, /) -> collections.abc.Generator[str, Any, None]: ...  # PYI058 (use `Iterator`)
 
-class AsyncIteratorReturningSimpleAsyncGenerator2:
-    def __aiter__(self, /) -> collections.abc.AsyncGenerator[str, Any]: ...  # PYI058 (Use `AsyncIterator`)
+def scope():
+    import collections.abc
+    import typing
 
-class AsyncIteratorReturningSimpleAsyncGenerator3:
-    def __aiter__(self, /) -> collections.abc.AsyncGenerator[str, None]: ...  # PYI058 (Use `AsyncIterator`)
+    class IteratorReturningSimpleGenerator5:
+        def __iter__(self, /) -> collections.abc.Generator[str, None, typing.Any]: ...  # PYI058 (use `Iterator`)
 
-class CorrectIterator:
-    def __iter__(self) -> Iterator[str]: ...  # OK
+def scope():
+    from collections.abc import Generator
 
-class CorrectAsyncIterator:
-    def __aiter__(self) -> collections.abc.AsyncIterator[int]: ...  # OK
+    class IteratorReturningSimpleGenerator6:
+        def __iter__(self, /) -> Generator[str, None, None]: ...  # PYI058 (use `Iterator`)
 
-class Fine:
-    def __iter__(self) -> typing.Self: ...  # OK
+def scope():
+    import typing_extensions
 
-class StrangeButWeWontComplainHere:
-    def __aiter__(self) -> list[bytes]: ...  # OK
+    class AsyncIteratorReturningSimpleAsyncGenerator1:
+        def __aiter__(self,) -> typing_extensions.AsyncGenerator: ...  # PYI058 (Use `AsyncIterator`)
 
-def __iter__(self) -> Generator: ...  # OK (not in class scope)
-def __aiter__(self) -> AsyncGenerator: ...  # OK (not in class scope)
+def scope():
+    import collections.abc
 
-class IteratorReturningComplexGenerator:
-    def __iter__(self) -> Generator[str, int, bytes]: ...  # OK
+    class AsyncIteratorReturningSimpleAsyncGenerator3:
+        def __aiter__(self, /) -> collections.abc.AsyncGenerator[str, None]:
+            ...  # PYI058 (Use `AsyncIterator`)
 
-class AsyncIteratorReturningComplexAsyncGenerator:
-    def __aiter__(self) -> AsyncGenerator[str, int]: ...  # OK
+def scope():
+    import collections.abc
 
-class ClassWithInvalidAsyncAiterMethod:
-    async def __aiter__(self) -> AsyncGenerator: ...  # OK
+    class AsyncIteratorReturningSimpleAsyncGenerator3:
+        def __aiter__(self, /) -> collections.abc.AsyncGenerator[str, None]: ...  # PYI058 (Use `AsyncIterator`)
 
-class IteratorWithUnusualParameters1:
-    def __iter__(self, foo) -> Generator: ...  # OK
+def scope():
+    from typing import Iterator
 
-class IteratorWithUnusualParameters2:
-    def __iter__(self, *, bar) -> Generator: ...  # OK
+    class CorrectIterator:
+        def __iter__(self) -> Iterator[str]: ...  # OK
 
-class IteratorWithUnusualParameters3:
-    def __iter__(self, *args) -> Generator: ...  # OK
+def scope():
+    import collections.abc
 
-class IteratorWithUnusualParameters4:
-    def __iter__(self, **kwargs) -> Generator: ...  # OK
+    class CorrectAsyncIterator:
+        def __aiter__(self) -> collections.abc.AsyncIterator[int]: ...  # OK
+
+def scope():
+    import typing
+
+    class Fine:
+        def __iter__(self) -> typing.Self: ...  # OK
+
+def scope():
+    class StrangeButWeWontComplainHere:
+        def __aiter__(self) -> list[bytes]: ...  # OK
+
+def scope():
+    from collections.abc import Generator
+    def __iter__(self) -> Generator: ...  # OK (not in class scope)
+
+def scope():
+    from collections.abc import AsyncGenerator
+    def __aiter__(self) -> AsyncGenerator: ...  # OK (not in class scope)
+
+def scope():
+    from collections.abc import Generator
+
+    class IteratorReturningComplexGenerator:
+        def __iter__(self) -> Generator[str, int, bytes]: ...  # OK
+
+def scope():
+    from collections.abc import AsyncGenerator
+
+    class AsyncIteratorReturningComplexAsyncGenerator:
+        def __aiter__(self) -> AsyncGenerator[str, int]: ...  # OK
+
+def scope():
+    from collections.abc import AsyncGenerator
+
+    class ClassWithInvalidAsyncAiterMethod:
+        async def __aiter__(self) -> AsyncGenerator: ...  # OK
+
+def scope():
+    from collections.abc import Generator
+
+    class IteratorWithUnusualParameters1:
+        def __iter__(self, foo) -> Generator: ...  # OK
+
+def scope():
+    from collections.abc import Generator
+
+    class IteratorWithUnusualParameters2:
+        def __iter__(self, *, bar) -> Generator: ...  # OK
+
+def scope():
+    from collections.abc import Generator
+
+    class IteratorWithUnusualParameters3:
+        def __iter__(self, *args) -> Generator: ...  # OK
+
+def scope():
+    from collections.abc import Generator
+
+    class IteratorWithUnusualParameters4:
+        def __iter__(self, **kwargs) -> Generator: ...  # OK
