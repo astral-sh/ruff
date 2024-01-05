e832dd735a80f3332dabfff77b2bebc83ee2a9b3
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-01-04 20:32:26 -0500

    Tweaks

--- crates/ruff_linter/src/rules/flake8_bandit/rules/ssl_with_bad_defaults.rs
+++ crates/ruff_linter/src/rules/flake8_bandit/rules/ssl_with_bad_defaults.rs
@@ -1,22 +1,21 @@
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::{self as ast, Expr, StmtFunctionDef};
-use ruff_text_size::TextRange;
 
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for function definitions which have default arguments set to broken SSL/TLS
-/// protocol versions.
+/// Checks for function definitions with default arguments set to insecure SSL
+/// and TLS protocol versions.
 ///
 /// ## Why is this bad?
-/// Several highly publicized exploitable flaws have been discovered in all versions of SSL and
-/// early versions of TLS. It is strongly recommended that use of the following known broken
-/// protocol versions be avoided:
-///     - SSL v2
-///     - SSL v3
-///     - TLS v1
-///     - TLS v1.1
+/// Several highly publicized exploitable flaws have been discovered in all
+/// versions of SSL and early versions of TLS. The following versions are
+/// considered insecure, and should be avoided:
+/// - SSL v2
+/// - SSL v3
+/// - TLS v1
+/// - TLS v1.1
 ///
 /// ## Example
 /// ```python
@@ -43,24 +42,10 @@ pub struct SslWithBadDefaults {
 impl Violation for SslWithBadDefaults {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!(
-            "Argument default set to insecure SSL protocol: {}",
-            self.protocol
-        )
+        let SslWithBadDefaults { protocol } = self;
+        format!("Argument default set to insecure SSL protocol: `{protocol}`")
     }
 }
-
-const INSECURE_SSL_PROTOCOLS: &[&str] = &[
-    "PROTOCOL_SSLv2",
-    "PROTOCOL_SSLv3",
-    "PROTOCOL_TLSv1",
-    "PROTOCOL_TLSv1_1",
-    "SSLv2_METHOD",
-    "SSLv23_METHOD",
-    "SSLv3_METHOD",
-    "TLSv1_METHOD",
-    "TLSv1_1_METHOD",
-];
 
 /// S503
 pub(crate) fn ssl_with_bad_defaults(checker: &mut Checker, function_def: &StmtFunctionDef) {
@@ -79,14 +64,24 @@ pub(crate) fn ssl_with_bad_defaults(checker: &mut Checker, function_def: &StmtFu
             if let Some(default) = &param.default {
                 match default.as_ref() {
                     Expr::Name(ast::ExprName { id, range, .. }) => {
-                        check_default_arg_for_insecure_ssl_violation(id.as_str(), checker, *range);
+                        if is_insecure_protocol(id.as_str()) {
+                            checker.diagnostics.push(Diagnostic::new(
+                                SslWithBadDefaults {
+                                    protocol: id.to_string(),
+                                },
+                                *range,
+                            ));
+                        }
                     }
                     Expr::Attribute(ast::ExprAttribute { attr, range, .. }) => {
-                        check_default_arg_for_insecure_ssl_violation(
-                            attr.as_str(),
-                            checker,
-                            *range,
-                        );
+                        if is_insecure_protocol(attr.as_str()) {
+                            checker.diagnostics.push(Diagnostic::new(
+                                SslWithBadDefaults {
+                                    protocol: attr.to_string(),
+                                },
+                                *range,
+                            ));
+                        }
                     }
                     _ => {}
                 }
@@ -94,13 +89,18 @@ pub(crate) fn ssl_with_bad_defaults(checker: &mut Checker, function_def: &StmtFu
         });
 }
 
-fn check_default_arg_for_insecure_ssl_violation(id: &str, checker: &mut Checker, range: TextRange) {
-    if INSECURE_SSL_PROTOCOLS.contains(&id) {
-        checker.diagnostics.push(Diagnostic::new(
-            SslWithBadDefaults {
-                protocol: id.to_string(),
-            },
-            range,
-        ));
-    }
+/// Returns `true` if the given protocol name is insecure.
+fn is_insecure_protocol(name: &str) -> bool {
+    matches!(
+        name,
+        "PROTOCOL_SSLv2"
+            | "PROTOCOL_SSLv3"
+            | "PROTOCOL_TLSv1"
+            | "PROTOCOL_TLSv1_1"
+            | "SSLv2_METHOD"
+            | "SSLv23_METHOD"
+            | "SSLv3_METHOD"
+            | "TLSv1_METHOD"
+            | "TLSv1_1_METHOD"
+    )
 }
