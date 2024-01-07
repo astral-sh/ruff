b31a9852cd591ea24019e88746ca8582def9f9e4
AlexWaygood <alex.waygood@gmail.com>
2024-01-07 17:26:54 +0000

    [flake8-pyi] PYI053: Exclude string literals that are the first argument to `warnings.deprecated` or `typing_extensions.deprecated`

--- crates/ruff_linter/src/rules/flake8_pyi/rules/string_or_bytes_too_long.rs
+++ crates/ruff_linter/src/rules/flake8_pyi/rules/string_or_bytes_too_long.rs
@@ -49,6 +49,16 @@ pub(crate) fn string_or_bytes_too_long(checker: &mut Checker, string: StringLike
         return;
     }
 
+    if let Some(depr_message) =
+        warnings_dot_deprecated_message(checker.semantic().current_expression_parent(), checker)
+    {
+        if let StringLike::StringLiteral(literal) = string {
+            if depr_message == literal {
+                return;
+            }
+        }
+    }
+
     let length = match string {
         StringLike::StringLiteral(ast::ExprStringLiteral { value, .. }) => value.chars().count(),
         StringLike::BytesLiteral(ast::ExprBytesLiteral { value, .. }) => value.len(),
@@ -66,4 +76,41 @@ pub(crate) fn string_or_bytes_too_long(checker: &mut Checker, string: StringLike
         string.range(),
     )));
     checker.diagnostics.push(diagnostic);
+}
+
+fn warnings_dot_deprecated_message<'a>(
+    expr: Option<&'a ast::Expr>,
+    checker: &Checker,
+) -> Option<&'a ast::ExprStringLiteral> {
+    // If `expr` represents a call to `warnings.deprecated()`
+    // (or the `typing_extensions`` backport), return the string-literal
+    // deprecation message passed as the first argument to the call.
+    // Else, return `None`.
+    let Some(ast::Expr::Call(ast::ExprCall {
+        func,
+        arguments: ast::Arguments { args, .. },
+        ..
+    })) = expr
+    else {
+        return None;
+    };
+    let [ast::Expr::StringLiteral(depr_message @ ast::ExprStringLiteral { .. }), ..] =
+        &args.as_slice()
+    else {
+        return None;
+    };
+    if checker
+        .semantic()
+        .resolve_call_path(func)
+        .is_some_and(|call_path| {
+            matches!(
+                call_path.as_slice(),
+                ["warnings" | "typing_extensions", "deprecated"]
+            )
+        })
+    {
+        Some(depr_message)
+    } else {
+        None
+    }
 }
