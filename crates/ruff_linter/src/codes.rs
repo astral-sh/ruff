6ce42ca6935615332db1c0a78145ff8a92a34b0e
AlexWaygood <alex.waygood@gmail.com>
2024-01-08 15:17:02 +0000

    Add rule to enforce parentheses in `a or b and c`

--- crates/ruff_linter/src/codes.rs
+++ crates/ruff_linter/src/codes.rs
@@ -923,6 +923,7 @@ pub fn code_to_rule(linter: Linter, code: &str) -> Option<(RuleGroup, Rule)> {
         (Ruff, "018") => (RuleGroup::Preview, rules::ruff::rules::AssignmentInAssert),
         (Ruff, "019") => (RuleGroup::Preview, rules::ruff::rules::UnnecessaryKeyCheck),
         (Ruff, "020") => (RuleGroup::Preview, rules::ruff::rules::NeverUnion),
+        (Ruff, "021") => (RuleGroup::Preview, rules::ruff::rules::ParenthesizeChainedOperators),
         (Ruff, "100") => (RuleGroup::Stable, rules::ruff::rules::UnusedNOQA),
         (Ruff, "200") => (RuleGroup::Stable, rules::ruff::rules::InvalidPyprojectToml),
 
---
+++ crates/ruff_linter/src/rules/ruff/rules/parenthesize_logical_operators.rs
@@ -0,0 +1,81 @@
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast as ast;
+use ruff_python_ast::parenthesize::parenthesized_range;
+use ruff_text_size::Ranged;
+
+use crate::checkers::ast::Checker;
+
+/// ## What it does
+/// Checks for chained operators where adding parentheses could improve the
+/// clarity of the code.
+///
+/// ## Why is this bad?
+/// `and` always binds more tightly than `or` when chaining the two together,
+/// but this can be hard to remember (and sometimes surprising).
+/// Adding parentheses in these situations can greatly improve code readability,
+/// with no change to semantics or performance.
+///
+/// For example:
+/// ```python
+/// a, b, c = 1, 0, 2
+/// x = a or b and c
+///
+/// d, e, f = 0, 1, 2
+/// y = d and e or f
+/// ```
+/// Use instead:
+/// ```python
+/// a, b, c = 1, 0, 2
+/// x = a or (b and c)
+///
+/// d, e, f = 0, 1, 2
+/// y = (d and e) or f
+
+#[violation]
+pub struct ParenthesizeChainedOperators;
+
+impl Violation for ParenthesizeChainedOperators {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        format!(
+            "Parenthesize `foo and bar` expressions when chaining `and` and `or` together, to make the precedence clear"
+        )
+    }
+}
+
+pub(crate) fn parenthesize_chained_logical_operators(
+    checker: &mut Checker,
+    expr: &ast::ExprBoolOp,
+) {
+    for condition in &expr.values {
+        match condition {
+            // If we find a BoolOp expressions inside BoolOp expressions,
+            // it means a different operator is being used for the subexpression
+            // than in the superexpression:
+            // `a or b or c` => `BoolOp(values=[Name('a'), Name('b'), Name('c')], op=Or)`
+            // `a or b and c` => `BoolOp(values=[Name('a'), BoolOp(values=[Name('b'), Name('c')], op=And)], op=Or)`
+            ast::Expr::BoolOp(
+                bool_op @ ast::ExprBoolOp {
+                    op: ast::BoolOp::And,
+                    ..
+                },
+            ) => {
+                if parenthesized_range(
+                    ast::ExpressionRef::BoolOp(bool_op),
+                    ast::AnyNodeRef::ExprBoolOp(expr),
+                    checker.indexer().comment_ranges(),
+                    checker.locator().contents(),
+                )
+                .is_none()
+                {
+                    checker.diagnostics.push(Diagnostic::new(
+                        ParenthesizeChainedOperators {},
+                        bool_op.range(),
+                    ));
+                }
+            }
+            _ => continue,
+        };
+    }
+}
