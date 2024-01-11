eb9c1fbf5c984d41730cfa2077d18536cc4efdbc
Jane Lewis <me@jane.engineering>
2024-01-11 11:44:51 -0800

    Add `quoted` formatter to `display_settings` and document the `display_settings` macro

--- crates/ruff_linter/src/settings/mod.rs
+++ crates/ruff_linter/src/settings/mod.rs
@@ -41,6 +41,71 @@ pub mod flags;
 pub mod rule_table;
 pub mod types;
 
+/// `display_settings!` is a macro that can display and format struct fields in a readable,
+/// namespaced format. It's particularly useful at generating `Display` implementations
+/// for types used in settings.
+///
+/// # Example
+/// ```
+/// use std::fmt;
+/// use ruff_linter::display_settings;
+/// #[derive(Default)]
+/// struct Settings {
+///     option_a: bool,
+///     sub_settings: SubSettings,
+///     option_b: String,
+/// }
+///
+/// struct SubSettings {
+///     name: String
+/// }
+///
+/// impl Default for SubSettings {
+///     fn default() -> Self {
+///         Self { name: "Default Name".into() }
+///     }
+///
+/// }
+///
+/// impl fmt::Display for SubSettings {
+///     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+///         display_settings! {
+///             formatter = f,
+///             namespace = "sub_settings",
+///             fields = [
+///                 self.name | quoted
+///             ]
+///         }
+///         Ok(())
+///     }
+///
+/// }
+///
+/// impl fmt::Display for Settings {
+///     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+///         display_settings! {
+///             formatter = f,
+///             fields = [
+///                 self.option_a,
+///                 self.sub_settings | nested,
+///                 self.option_b | quoted,
+///             ]
+///         }
+///         Ok(())
+///     }
+///
+/// }
+///
+/// const EXPECTED_OUTPUT: &str = r#"option_a = false
+/// sub_settings.name = "Default Name"
+/// option_b = ""
+/// "#;
+///
+/// fn main() {
+///     let settings = Settings::default();
+///     assert_eq!(format!("{settings}"), EXPECTED_OUTPUT);
+/// }
+/// ```
 #[macro_export]
 macro_rules! display_settings {
     (formatter = $fmt:ident, namespace = $namespace:literal, fields = [$($settings:ident.$field:ident $(| $modifier:tt)?),* $(,)?]) => {
@@ -61,6 +126,9 @@ macro_rules! display_settings {
     };
     (@field $fmt:ident, $prefix:ident, $settings:ident.$field:ident | debug) => {
         writeln!($fmt, "{}{} = {:?}", $prefix, stringify!($field), $settings.$field)?;
+    };
+    (@field $fmt:ident, $prefix:ident, $settings:ident.$field:ident | quoted) => {
+        writeln!($fmt, "{}{} = \"{}\"", $prefix, stringify!($field), $settings.$field)?;
     };
     (@field $fmt:ident, $prefix:ident, $settings:ident.$field:ident | nested) => {
         write!($fmt, "{}", $settings.$field)?;
