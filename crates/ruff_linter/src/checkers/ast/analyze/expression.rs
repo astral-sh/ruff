260ad1dc77a3626aac1e56708d45e9e62b4d5706
Steve C <diceroll123@gmail.com>
2024-01-14 13:37:14 -0500

    more logic tweaks

--- crates/ruff_linter/src/checkers/ast/analyze/expression.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/expression.rs
@@ -980,9 +980,9 @@ pub(crate) fn expression(expr: &Expr, checker: &mut Checker) {
                 flake8_pie::rules::unnecessary_spread(checker, dict);
             }
         }
-        Expr::Set(ast::ExprSet { elts, range }) => {
+        Expr::Set(_) => {
             if checker.enabled(Rule::DuplicateValue) {
-                flake8_bugbear::rules::duplicate_value(checker, elts, *range);
+                flake8_bugbear::rules::duplicate_value(checker, expr);
             }
         }
         Expr::Yield(_) => {
--- crates/ruff_linter/src/rules/flake8_bugbear/rules/duplicate_value.rs
+++ crates/ruff_linter/src/rules/flake8_bugbear/rules/duplicate_value.rs
@@ -1,10 +1,13 @@
+use anyhow::{Context, Result};
+
 use ruff_python_ast::{Expr, ExprSet};
+use ruff_python_trivia::{SimpleTokenKind, SimpleTokenizer};
 use rustc_hash::FxHashSet;
 
 use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::comparable::ComparableExpr;
-use ruff_text_size::{Ranged, TextRange};
+use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
 
@@ -44,7 +47,11 @@ impl AlwaysFixableViolation for DuplicateValue {
 }
 
 /// B033
-pub(crate) fn duplicate_value(checker: &mut Checker, elts: &[Expr], range: TextRange) {
+pub(crate) fn duplicate_value(checker: &mut Checker, expr: &Expr) {
+    let Expr::Set(ExprSet { elts, .. }) = expr else {
+        return;
+    };
+
     let mut seen_values: FxHashSet<ComparableExpr> = FxHashSet::default();
     let mut duplicate_indices: Vec<usize> = Vec::new();
     let mut unique_indices: Vec<usize> = Vec::new();
@@ -63,23 +70,9 @@ pub(crate) fn duplicate_value(checker: &mut Checker, elts: &[Expr], range: TextR
         }
     }
 
-    if duplicate_indices.is_empty() {
-        return;
-    }
-
-    let fix = Fix::safe_edit(Edit::range_replacement(
-        checker.generator().expr(&Expr::Set(ExprSet {
-            elts: unique_indices
-                .into_iter()
-                .map(|index| elts[index].clone())
-                .collect(),
-            range: TextRange::default(),
-        })),
-        range,
-    ));
-
     for index in duplicate_indices {
         let elt = &elts[index];
+
         let mut diagnostic = Diagnostic::new(
             DuplicateValue {
                 value: checker.generator().expr(elt),
@@ -87,8 +80,52 @@ pub(crate) fn duplicate_value(checker: &mut Checker, elts: &[Expr], range: TextR
             elt.range(),
         );
 
-        diagnostic.set_fix(fix.clone());
+        diagnostic.try_set_fix(|| {
+            remove_member(elt, elts, checker.locator().contents()).map(Fix::safe_edit)
+        });
 
         checker.diagnostics.push(diagnostic);
+    }
+}
+
+fn remove_member(expr: &Expr, elts: &[Expr], source: &str) -> Result<Edit> {
+    let (before, after): (Vec<_>, Vec<_>) = elts
+        .iter()
+        .map(Ranged::range)
+        .filter(|range| expr.range() != *range)
+        .partition(|range| range.start() < expr.start());
+
+    if !after.is_empty() {
+        // Case 1: expr is _not_ the last node, so delete from the start of the
+        // expr to the end of the subsequent comma.
+        let mut tokenizer = SimpleTokenizer::starts_at(expr.end(), source);
+
+        // Find the trailing comma.
+        tokenizer
+            .find(|token| token.kind == SimpleTokenKind::Comma)
+            .context("Unable to find trailing comma")?;
+
+        // Find the next non-whitespace token.
+        let next = tokenizer
+            .find(|token| {
+                token.kind != SimpleTokenKind::Whitespace && token.kind != SimpleTokenKind::Newline
+            })
+            .context("Unable to find next token")?;
+
+        Ok(Edit::deletion(expr.start(), next.start()))
+    } else if let Some(previous) = before.iter().map(Ranged::end).max() {
+        // Case 2: expr is the last node, so delete from the start of the
+        // previous comma to the end of the expr.
+        let mut tokenizer = SimpleTokenizer::starts_at(previous, source);
+
+        // Find the trailing comma.
+        let comma = tokenizer
+            .find(|token| token.kind == SimpleTokenKind::Comma)
+            .context("Unable to find trailing comma")?;
+
+        Ok(Edit::deletion(comma.start(), expr.end()))
+    } else {
+        // Case 3: expr is the only node, so delete it
+        Ok(Edit::range_deletion(expr.range()))
     }
 }
