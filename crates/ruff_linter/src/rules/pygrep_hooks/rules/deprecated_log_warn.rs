6f853082fde83b2ed20bdce4e09f0214f1f4b15a
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-01-15 21:46:47 -0500

    Fix names

--- crates/ruff_linter/src/rules/pygrep_hooks/rules/deprecated_log_warn.rs
+++ crates/ruff_linter/src/rules/pygrep_hooks/rules/deprecated_log_warn.rs
@@ -6,6 +6,7 @@ use ruff_python_stdlib::logging::LoggingLevel;
 use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
+use crate::importer::ImportRequest;
 
 /// ## What it does
 /// Check for usages of the deprecated `warn` method from the `logging` module.
@@ -81,11 +82,25 @@ pub(crate) fn deprecated_log_warn(checker: &mut Checker, call: &ast::ExprCall) {
 
     let mut diagnostic = Diagnostic::new(DeprecatedLogWarn, call.func.range());
     if checker.settings.preview.is_enabled() {
-        if let Expr::Attribute(ast::ExprAttribute { attr, .. }) = call.func.as_ref() {
-            diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-                "warning".to_string(),
-                attr.range(),
-            )));
+        match call.func.as_ref() {
+            Expr::Attribute(ast::ExprAttribute { attr, .. }) => {
+                diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
+                    "warning".to_string(),
+                    attr.range(),
+                )));
+            }
+            Expr::Name(_) => {
+                diagnostic.try_set_fix(|| {
+                    let (import_edit, binding) = checker.importer().get_or_import_symbol(
+                        &ImportRequest::import("logging", "warning"),
+                        call.start(),
+                        checker.semantic(),
+                    )?;
+                    let name_edit = Edit::range_replacement(binding, call.func.range());
+                    Ok(Fix::safe_edits(import_edit, [name_edit]))
+                });
+            }
+            _ => {}
         }
     }
     checker.diagnostics.push(diagnostic);
