57313d9d63af023b7c61c971ef13ead9fb803f34
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2024-01-24 11:50:22 +0900

    [`pylint`] Implement `assigning-non-slot` (`E0237`) (#9623)
    ## Summary
    
    Implement [assigning-non-slot /
    E0237](https://pylint.readthedocs.io/en/latest/user_guide/messages/error/assigning-non-slot.html)
    
    related #970
    
    ## Test Plan
    
    `cargo test`

--- crates/ruff_linter/src/checkers/ast/analyze/statement.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/statement.rs
@@ -507,6 +507,9 @@ pub(crate) fn statement(stmt: &Stmt, checker: &mut Checker) {
             if checker.enabled(Rule::NoSlotsInNamedtupleSubclass) {
                 flake8_slots::rules::no_slots_in_namedtuple_subclass(checker, stmt, class_def);
             }
+            if checker.enabled(Rule::NonSlotAssignment) {
+                pylint::rules::non_slot_assignment(checker, class_def);
+            }
             if checker.enabled(Rule::SingleStringSlots) {
                 pylint::rules::single_string_slots(checker, class_def);
             }
