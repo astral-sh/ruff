2cdcb48f4c2e0fdbc8118e729b6feb3971fe68da
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-01-26 12:28:02 -0500

    Brush up docs

--- crates/ruff_linter/src/rules/ruff/rules/defaultdict_with_default_factory_as_kwarg.rs
+++ crates/ruff_linter/src/rules/ruff/rules/defaultdict_with_default_factory_as_kwarg.rs
@@ -9,27 +9,36 @@ use ruff_text_size::{Ranged, TextRange};
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for unnecessary `default_factory` keyword argument when initializing
-/// `defaultdict`.
+/// Checks for incorrect usages of `default_factory` as a keyword argument when
+/// initializing a `defaultdict`.
 ///
 /// ## Why is this bad?
-/// Type checkers can't reliably infer the type of the default value when using
-/// `default_factory` keyword argument.
+/// The `defaultdict` constructor accepts a callable as its first argument.
+/// For example, it's common to initialize a `defaultdict` with `int` or `list`
+/// via `defaultdict(int)` or `defaultdict(list)`, to create a dictionary that
+/// returns `0` or `[]` respectively when a key is missing.
 ///
-/// Prefer `defaultdict(callable)` over `defaultdict(default_factory=callable)`
+/// The default factory _must_ be provided as a positional argument, as all
+/// keyword arguments to `defaultdict` are interpreted as initial entries in
+/// the dictionary. For example, `defaultdict(foo=1, bar=2)` will create a
+/// dictionary with `{"foo": 1, "bar": 2}` as its initial entries.
+///
+/// As such, `defaultdict(default_factory=list)` will create a dictionary with
+/// `{"default_factory": list}` as its initial entry, instead of a dictionary
+/// that returns `[]` when a key is missing. Specifying a `default_factory`
+/// keyword argument is almost always a mistake, and one that type checkers
+/// can't reliably detect.
 ///
 /// ## Examples
 /// ```python
 /// defaultdict(default_factory=int)
 /// defaultdict(default_factory=list)
-/// defaultdict(default_factory=any_callable)
 /// ```
 ///
 /// Use instead:
 /// ```python
 /// defaultdict(int)
 /// defaultdict(list)
-/// defaultdict(any_callable)
 /// ```
 #[violation]
 pub struct DefaultDictWithDefaultFactoryAsKwArg {
