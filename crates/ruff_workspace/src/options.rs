5dba038de1f18b2984f2728ab9ef3686547fee02
Micha Reiser <micha@reiser.io>
2024-01-29 18:52:49 +0100

    Add deprecation message for top-level lint settings (#9582)

--- crates/ruff_workspace/src/options.rs
+++ crates/ruff_workspace/src/options.rs
@@ -481,9 +481,7 @@ pub struct LintOptions {
 /// Newtype wrapper for [`LintCommonOptions`] that allows customizing the JSON schema and omitting the fields from the [`OptionsMetadata`].
 #[derive(Debug, PartialEq, Eq, Default, Serialize, Deserialize)]
 #[serde(transparent)]
-pub struct DeprecatedTopLevelLintOptions {
-    pub common: LintCommonOptions,
-}
+pub struct DeprecatedTopLevelLintOptions(pub LintCommonOptions);
 
 impl OptionsMetadata for DeprecatedTopLevelLintOptions {
     fn record(_visit: &mut dyn Visit) {
