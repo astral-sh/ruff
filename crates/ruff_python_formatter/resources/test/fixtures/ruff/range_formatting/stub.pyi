1ff938a7dc92decd5a7bbb828a34a53637d60b06
Micha Reiser <micha@reiser.io>
2024-01-30 11:34:35 +0100

    Add stub file tests

---
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/range_formatting/stub.pyi
@@ -0,0 +1,11 @@
+class Test:
+    <RANGE_START>...<RANGE_END>
+
+class Test2: <RANGE_START>pass<RANGE_END>
+
+class Test3:    <RANGE_START>...<RANGE_END>
+
+class Test4:
+    # leading comment
+    <RANGE_START>...<RANGE_END>
+    # trailing comment
