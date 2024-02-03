5c10e8a19e890918212d8a2ff03239ec3213fe33
Anders Steen <anders.steen@knowit.no>
2024-02-03 15:03:32 +0100

    Trivial test passing

--- crates/ruff_linter/src/rules/flake8_cognitive_complexity/rules/function_is_too_cognitive_complex.rs
+++ crates/ruff_linter/src/rules/flake8_cognitive_complexity/rules/function_is_too_cognitive_complex.rs
@@ -1,6 +1,6 @@
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
-use ruff_python_ast::{identifier::Identifier, Stmt};
+use ruff_python_ast::{self as ast, identifier::Identifier, Stmt};
 
 #[violation]
 pub struct CognitiveComplexStructure {
@@ -21,8 +21,28 @@ impl Violation for CognitiveComplexStructure {
     }
 }
 
-fn get_cognitive_complexity_number(body: &[Stmt]) -> usize {
-    42
+fn get_cognitive_complexity_number(stmts: &[Stmt], nesting: usize) -> usize {
+    let mut complexity: usize = 0;
+    for stmt in stmts {
+        match stmt {
+            Stmt::If(ast::StmtIf {
+                body,
+                elif_else_clauses,
+                ..
+            }) => {
+                complexity += 1 + nesting;
+                complexity += get_cognitive_complexity_number(body, nesting + 1);
+                for clause in elif_else_clauses {
+                    if clause.test.is_some() {
+                        complexity += 1;
+                    }
+                    complexity += get_cognitive_complexity_number(&clause.body, nesting + 1);
+                }
+            }
+            _ => {}
+        }
+    }
+    complexity
 }
 
 pub(crate) fn function_is_too_cognitive_complex(
@@ -31,7 +51,7 @@ pub(crate) fn function_is_too_cognitive_complex(
     body: &[Stmt],
     max_cognitive_complexity: usize,
 ) -> Option<Diagnostic> {
-    let complexity = get_cognitive_complexity_number(body) + 1;
+    let complexity = get_cognitive_complexity_number(body, 1);
     if complexity > max_cognitive_complexity {
         Some(Diagnostic::new(
             CognitiveComplexStructure {
@@ -61,7 +81,7 @@ def trivial():
     pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_cognitive_complexity_number(&stmts), 1);
+        assert_eq!(get_cognitive_complexity_number(&stmts, 0), 0);
         Ok(())
     }
 }
