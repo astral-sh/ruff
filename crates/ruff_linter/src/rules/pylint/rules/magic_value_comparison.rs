5d7ed2799715d7ede0c049bd288e0f3908d0db9e
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-02-12 20:12:12 -0500

    Fix clippy

--- crates/ruff_linter/src/rules/pylint/rules/magic_value_comparison.rs
+++ crates/ruff_linter/src/rules/pylint/rules/magic_value_comparison.rs
@@ -86,9 +86,10 @@ fn is_magic_value(literal_expr: LiteralExpressionRef, allowed_types: &[ConstantT
             !matches!(value.to_str(), "" | "__main__")
         }
         LiteralExpressionRef::NumberLiteral(ast::ExprNumberLiteral { value, .. }) => match value {
-            ast::Number::Int(value) => !matches!(*value, Int::ZERO | Int::ONE),
+            #[allow(clippy::float_cmp)]
             ast::Number::Float(value) => !(*value == 0.0 || *value == 1.0),
-            _ => true,
+            ast::Number::Int(value) => !matches!(*value, Int::ZERO | Int::ONE),
+            ast::Number::Complex { .. } => true,
         },
         LiteralExpressionRef::BytesLiteral(_) => true,
     }
