a69fd7f189decea27d2b66e103c149cdcd26288f
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-02-29 21:10:28 -0500

    Reformat fixture

--- crates/ruff_linter/resources/test/fixtures/pylint/useless_exception_statement.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/useless_exception_statement.py
@@ -1,110 +1,106 @@
-
-# Violation test cases: PLW0133
-
 # Test case 1: Useless exception statement
 from abc import ABC, abstractmethod
 from contextlib import suppress
 
 
-def test():
+def func():
     AssertionError("This is an assertion error")  # PLW0133
 
-# Test case 2: Useless exception statement in try-except block
 
-def test():
+# Test case 2: Useless exception statement in try-except block
+def func():
     try:
-        Exception("This is an exception") # PLW0133
-    except Exception as e:
-        pass  # PL
+        Exception("This is an exception")  # PLW0133
+    except Exception as err:
+        pass
+
 
 # Test case 3: Useless exception statement in if statement
-
-def test():
+def func():
     if True:
         RuntimeError("This is an exception")  # PLW0133
 
-# Test case 4: Useless exception statement in class
 
-def test():
-    class Test:
+# Test case 4: Useless exception statement in class
+def func():
+    class Class:
         def __init__(self):
-            TypeError("This is an exception") # PLW0133
+            TypeError("This is an exception")  # PLW0133
+
 
 # Test case 5: Useless exception statement in function
+def func():
+    def inner():
+        IndexError("This is an exception")  # PLW0133
 
-def test():
-    def f():
-        IndexError("This is an exception") # PLW0133
-    f()
+    inner()
+
 
 # Test case 6: Useless exception statement in while loop
-    
-def test():
+def func():
     while True:
-        KeyError("This is an exception") # PLW0133
+        KeyError("This is an exception")  # PLW0133
+
 
 # Test case 7: Useless exception statement in abstract class
-        
-def test():
-    class Test(ABC):
+def func():
+    class Class(ABC):
         @abstractmethod
-        def test(self):
-            NotImplementedError("This is an exception") # PLW0133
+        def method(self):
+            NotImplementedError("This is an exception")  # PLW0133
 
 
 # Test case 8: Useless exception statement inside context manager
-            
-def test():
+def func():
     with suppress(AttributeError):
-        AttributeError("This is an exception") # PLW0133
+        AttributeError("This is an exception")  # PLW0133
 
 
 # Non-violation test cases: PLW0133
-            
-# Test case 1: Used exception statement in try-except block
 
-def test():
+
+# Test case 1: Used exception statement in try-except block
+def func():
     raise Exception("This is an exception")  # OK
 
-# Test case 2: Used exception statement in if statement
 
-def test():
+# Test case 2: Used exception statement in if statement
+def func():
     if True:
         raise ValueError("This is an exception")  # OK
-    
+
+
 # Test case 3: Used exception statement in class
-    
-def test():
-    class Test:
+def func():
+    class Class:
         def __init__(self):
             raise TypeError("This is an exception")  # OK
-        
-# Test case 4: Exception statement used in list comprehension
 
-def test():
+
+# Test case 4: Exception statement used in list comprehension
+def func():
     [ValueError("This is an exception") for i in range(10)]  # OK
 
 
 # Test case 5: Exception statement used when initializing a dictionary
-            
-def test():
+def func():
     {i: TypeError("This is an exception") for i in range(10)}  # OK
 
+
 # Test case 6: Exception statement used in function
-    
-def test():
-    def f():
+def func():
+    def inner():
         raise IndexError("This is an exception")  # OK
-    f()
+
+    inner()
+
 
 # Test case 7: Exception statement used in variable assignment
-
-def test():
-    e = KeyError("This is an exception")  # OK
+def func():
+    err = KeyError("This is an exception")  # OK
 
 
 # Test case 8: Exception statement inside context manager
-            
-def test():
+def func():
     with suppress(AttributeError):
-        raise AttributeError("This is an exception") # OK
+        raise AttributeError("This is an exception")  # OK
