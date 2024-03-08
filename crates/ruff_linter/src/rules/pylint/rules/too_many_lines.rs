38b3f2e0500019afb8057b9fb514c5e8c517848d
augustelalande <auguste.lalande@gmail.com>
2024-03-07 23:12:06 -0500

    reformat docs

--- crates/ruff_linter/src/rules/pylint/rules/too_many_lines.rs
+++ crates/ruff_linter/src/rules/pylint/rules/too_many_lines.rs
@@ -20,21 +20,21 @@ use crate::settings::LinterSettings;
 /// ## Example
 /// ```python
 /// def is_palindrome(string):  # [too-many-lines]
-///    left_pos = 0
-///    right_pos = len(string) - 1
-///    while right_pos >= left_pos:
-///        if not string[left_pos] == string[right_pos]:
-///            return False
-///        left_pos += 1
-///        right_pos -= 1
-///    return True
+///     left_pos = 0
+///     right_pos = len(string) - 1
+///     while right_pos >= left_pos:
+///         if not string[left_pos] == string[right_pos]:
+///             return False
+///         left_pos += 1
+///         right_pos -= 1
+///     return True
 ///
 ///
-///def main():
-///    print(is_palindrome("aza"))
-///    print(is_palindrome("racecar"))
-///    print(is_palindrome("trigger"))
-///    print(is_palindrome("ogre"))
+/// def main():
+///     print(is_palindrome("aza"))
+///     print(is_palindrome("racecar"))
+///     print(is_palindrome("trigger"))
+///     print(is_palindrome("ogre"))
 /// ```
 ///
 /// Use instead:
