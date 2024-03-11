8c587e983333ddfd481cb0b3ba9c74b29cf490ed
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-11 16:24:47 +0530

    Move parser quick test to pretty print code

--- crates/ruff_python_parser/src/parser/tests.rs
+++ crates/ruff_python_parser/src/parser/tests.rs
@@ -1,7 +1,3 @@
-use crate::parser::Parser;
-use crate::token_source::TokenSource;
-use crate::Mode;
-
 mod parser;
 mod suite;
 
@@ -16,29 +12,4 @@ fn ok_ipy_escape_command() {
     let tokens = crate::lexer::lex(src, Mode::Ipython).collect();
     let ast = crate::parse_tokens(tokens, src, Mode::Ipython);
     insta::assert_debug_snapshot!(ast);
-}
-
-// Test that is intentionally ignored by default.
-// Use it for quickly debugging a parser issue.
-#[test]
-#[ignore]
-fn parser_quick_test() {
-    let src = r"if True:
-    1
-    ...
-if x < 1:
-    ...
-else:
-    pass
-
-if a:
-    pass
-elif b:
-    ...
-";
-
-    let tokens = crate::lexer::lex(src, Mode::Module).collect();
-    let ast = Parser::new(src, Mode::Module, TokenSource::new(tokens)).parse_program();
-
-    assert_eq!(&ast.parse_errors, &[]);
 }
