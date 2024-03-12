94cc5f2e135b9e1eb31f07fda558983911643d5c
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-12 13:53:36 +0530

    Remove `FStringElement::Invalid`, improve parsing logic (#10327)
    This PR does the following around f-string parsing:
    1. Removes the `FStringElement::Invalid` variant
    2. Move the parsing of f-string elements to use list parsing logic
    3. Add error recovery for f-string elements
    
    - [x] All tests pass
    - [x] No performance regression

--- crates/ruff_python_codegen/src/generator.rs
+++ crates/ruff_python_codegen/src/generator.rs
@@ -1363,8 +1363,6 @@ impl<'a> Generator<'a> {
                 *conversion,
                 format_spec.as_deref(),
             ),
-            #[allow(deprecated)]
-            ast::FStringElement::Invalid(ast::FStringInvalidElement { value, .. }) => self.p(value),
         }
     }
 
