eb9559cd82e7f8db61f2849fa746b8e4bbd757ff
Alex Waygood <alex.waygood@gmail.com>
2024-03-13 22:33:50 +0000

    .

--- crates/ruff_python_semantic/src/scope.rs
+++ crates/ruff_python_semantic/src/scope.rs
@@ -20,34 +20,41 @@ use crate::star_import::StarImport;
 struct Bindings<'a>(FxHashMap<Cow<'a, str>, BindingId>);
 
 impl<'a> Bindings<'a> {
+    #[inline]
     fn get(&self, name: &str) -> Option<&BindingId> {
-        if name.is_ascii() {
-            self.0.get(name)
-        } else {
-            self.0.get(&*name.nfkc().collect::<String>())
-        }
+        self.0.get(name).or({
+            if name.is_ascii() {
+                None
+            } else {
+                self.0.get(&*name.nfkc().collect::<String>())
+            }
+        })
     }
 
+    #[inline]
     fn insert(&mut self, name: &'a str, value: BindingId) -> Option<BindingId> {
-        if name.is_ascii() {
-            self.0.insert(Cow::Borrowed(name), value)
-        } else {
-            self.0.insert(Cow::Owned(name.nfkc().collect()), value)
-        }
+        let name = {
+            if name.is_ascii() {
+                Cow::Borrowed(name)
+            } else {
+                Cow::Owned(name.nfkc().collect())
+            }
+        };
+        self.0.insert(name, value)
     }
 
+    #[inline]
     fn contains_key(&self, key: &str) -> bool {
-        if key.is_ascii() {
-            self.0.contains_key(key)
-        } else {
-            self.0.contains_key(&*key.nfkc().collect::<String>())
-        }
+        self.0.contains_key(key)
+            || (!key.is_ascii() && self.0.contains_key(&*key.nfkc().collect::<String>()))
     }
 
+    #[inline]
     fn values(&self) -> impl Iterator<Item = &BindingId> {
         self.0.values()
     }
 
+    #[inline]
     fn iter(&self) -> impl Iterator<Item = (&Cow<'a, str>, &BindingId)> {
         self.0.iter()
     }
