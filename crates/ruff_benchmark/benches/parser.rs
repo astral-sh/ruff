59889adaeda3132fb43d0567a2652d6bdbac7ecd
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-13 21:36:06 +0530

    Set new parser for benchmarks

--- crates/ruff_benchmark/benches/parser.rs
+++ crates/ruff_benchmark/benches/parser.rs
@@ -4,7 +4,7 @@ use ruff_benchmark::criterion::{
 use ruff_benchmark::{TestCase, TestFile, TestFileDownloadError};
 use ruff_python_ast::statement_visitor::{walk_stmt, StatementVisitor};
 use ruff_python_ast::Stmt;
-use ruff_python_parser::parse_suite;
+use ruff_python_parser::{parse_suite, set_new_parser};
 
 #[cfg(target_os = "windows")]
 #[global_allocator]
@@ -50,6 +50,8 @@ impl<'a> StatementVisitor<'a> for CountVisitor {
 }
 
 fn benchmark_parser(criterion: &mut Criterion<WallTime>) {
+    set_new_parser(true);
+
     let test_cases = create_test_cases().unwrap();
     let mut group = criterion.benchmark_group("parser");
 
