59a4898f017ce343c66c21790bafe0f629e2f41d
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-14 08:46:09 +0530

    Add dummy test case

--- crates/ruff_python_parser/resources/invalid/expressions/unclosed_fstring_lbrace.py
+++ crates/ruff_python_parser/resources/invalid/expressions/unclosed_fstring_lbrace.py
@@ -1,4 +1,6 @@
-# TODO(dhruvmanila): Uncomment when this is fixed. See PR #10372
+# TODO(dhruvmanila): Remove the dummy test case and uncomment the others when this is fixed. See PR #10372
+x +
+
 # f'{'
 # f'{foo!r'
 # f'{foo='
