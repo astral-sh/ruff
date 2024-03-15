a3d8cf421b83238e00d1def4db57cbd37379bb49
boolean-light <61526956+boolean-light@users.noreply.github.com>
2024-03-15 16:45:16 +0900

    Extended C400 and short-circuited fixation

--- crates/ruff_linter/src/rules/flake8_comprehensions/rules/unnecessary_generator_list.rs
+++ crates/ruff_linter/src/rules/flake8_comprehensions/rules/unnecessary_generator_list.rs
@@ -1,3 +1,4 @@
+use ast::{comparable::ComparableExpr, ExprGenerator};
 use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast as ast;
@@ -9,37 +10,49 @@ use super::helpers;
 
 /// ## What it does
 /// Checks for unnecessary generators that can be rewritten as `list`
-/// comprehensions.
+/// comprehensions or conversion to `list` using the builtin function.
 ///
 /// ## Why is this bad?
 /// It is unnecessary to use `list` around a generator expression, since
 /// there are equivalent comprehensions for these types. Using a
-/// comprehension is clearer and more idiomatic.
+/// comprehension/conversion to `list` is clearer and more idiomatic.
 ///
 /// ## Examples
 /// ```python
 /// list(f(x) for x in foo)
+/// list(x for x in foo)
 /// ```
 ///
 /// Use instead:
 /// ```python
 /// [f(x) for x in foo]
+/// list(foo)
 /// ```
 ///
 /// ## Fix safety
 /// This rule's fix is marked as unsafe, as it may occasionally drop comments
 /// when rewriting the call. In most cases, though, comments will be preserved.
 #[violation]
-pub struct UnnecessaryGeneratorList;
+pub struct UnnecessaryGeneratorList {
+    shortened_to_list_conversion: bool,
+}
 
 impl AlwaysFixableViolation for UnnecessaryGeneratorList {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Unnecessary generator (rewrite as a `list` comprehension)")
+        if self.shortened_to_list_conversion {
+            format!("Unnecessary generator (rewrite using `list()`")
+        } else {
+            format!("Unnecessary generator (rewrite as a `list` comprehension)")
+        }
     }
 
     fn fix_title(&self) -> String {
-        "Rewrite as a `list` comprehension".to_string()
+        if self.shortened_to_list_conversion {
+            "Rewrite using `list()`".to_string()
+        } else {
+            "Rewrite as a `list` comprehension".to_string()
+        }
     }
 }
 
@@ -56,28 +69,70 @@ pub(crate) fn unnecessary_generator_list(checker: &mut Checker, call: &ast::Expr
     if !checker.semantic().is_builtin("list") {
         return;
     }
-    if argument.is_generator_expr() {
-        let mut diagnostic = Diagnostic::new(UnnecessaryGeneratorList, call.range());
-
-        // Convert `list(x for x in y)` to `[x for x in y]`.
-        diagnostic.set_fix({
-            // Replace `list(` with `[`.
-            let call_start = Edit::replacement(
-                "[".to_string(),
-                call.start(),
-                call.arguments.start() + TextSize::from(1),
+    if let Some(generator_expr) = argument.clone().generator_expr() {
+        // Short-circuit the case for list(x for x in y) -> list(y)
+        let mut shortcircuit_completed = false;
+        'shortcircuit: {
+            let mut diagnostic = Diagnostic::new(
+                UnnecessaryGeneratorList {
+                    shortened_to_list_conversion: true,
+                },
+                call.range(),
             );
 
-            // Replace `)` with `]`.
-            let call_end = Edit::replacement(
-                "]".to_string(),
-                call.arguments.end() - TextSize::from(1),
-                call.end(),
+            let ExprGenerator {
+                range: _,
+                elt,
+                generators,
+                parenthesized: _,
+            } = generator_expr;
+
+            let [generator] = &generators[..] else {
+                break 'shortcircuit;
+            };
+            if !generator.ifs.is_empty() || generator.is_async {
+                break 'shortcircuit;
+            };
+            if ComparableExpr::from(&elt) != ComparableExpr::from(&generator.target) {
+                break 'shortcircuit;
+            };
+
+            let iterator = format!("list({})", checker.locator().slice(generator.iter.range()));
+            diagnostic.set_fix(Fix::unsafe_edit(Edit::range_replacement(
+                iterator,
+                call.range(),
+            )));
+            checker.diagnostics.push(diagnostic);
+            shortcircuit_completed = true;
+        }
+
+        // Convert `list(f(x) for x in y)` to `[f(x) for x in y]`.
+        if !shortcircuit_completed {
+            let mut diagnostic = Diagnostic::new(
+                UnnecessaryGeneratorList {
+                    shortened_to_list_conversion: false,
+                },
+                call.range(),
             );
+            diagnostic.set_fix({
+                // Replace `list(` with `[`.
+                let call_start = Edit::replacement(
+                    "[".to_string(),
+                    call.start(),
+                    call.arguments.start() + TextSize::from(1),
+                );
 
-            Fix::unsafe_edits(call_start, [call_end])
-        });
+                // Replace `)` with `]`.
+                let call_end = Edit::replacement(
+                    "]".to_string(),
+                    call.arguments.end() - TextSize::from(1),
+                    call.end(),
+                );
 
-        checker.diagnostics.push(diagnostic);
+                Fix::unsafe_edits(call_start, [call_end])
+            });
+
+            checker.diagnostics.push(diagnostic);
+        }
     }
 }
