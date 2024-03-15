ab12b93f8454ff090b0d7de0a248be564f1fb0a7
Steve C <diceroll123@gmail.com>
2024-03-14 23:47:47 -0400

    update snapshot

--- crates/ruff_linter/resources/test/fixtures/pylint/redeclared_assigned_name.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/redeclared_assigned_name.py
@@ -3,4 +3,4 @@ FIRST, (FIRST, SECOND) = (1, (1, 2))  # PLW0128
 FIRST, (FIRST, SECOND, (THIRD, FIRST)) = (1, (1, 2))  # PLW0128
 FIRST, SECOND, THIRD, FIRST, SECOND = (1, 2, 3, 4)  # PLW0128
 
-FIRST, SECOND, _, _ = (1, 2, 3, 4)  # OK
+FIRST, SECOND, _, _, _ignored = (1, 2, 3, 4, 5)  # OK
