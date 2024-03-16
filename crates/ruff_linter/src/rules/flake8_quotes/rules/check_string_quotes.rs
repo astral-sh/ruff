0ebeb2df93a5eea13a56089e1fa1dcef91469cb6
Robin Caloudis <robin.caloudis@gmx.de>
2024-03-16 14:22:43 +0100

    Fix diagnostic

--- crates/ruff_linter/src/rules/flake8_quotes/rules/check_string_quotes.rs
+++ crates/ruff_linter/src/rules/flake8_quotes/rules/check_string_quotes.rs
@@ -241,7 +241,7 @@ impl<'a> From<&'a str> for Trivia<'a> {
     }
 }
 
-fn text_ends_at_quote(locator: &Locator, range: TextRange, settings: &LinterSettings) -> bool {
+fn text_ends_at_quote(locator: &Locator, range: TextRange, quote: &Quote) -> bool {
     let trivia_of_next_char: Trivia = locator
         .slice(TextRange::new(
             range.end(),
@@ -250,7 +250,7 @@ fn text_ends_at_quote(locator: &Locator, range: TextRange, settings: &LinterSett
         .into();
     trivia_of_next_char
         .raw_text
-        .contains(good_docstring(settings.flake8_quotes.docstring_quotes))
+        .contains(good_docstring(*quote))
 }
 
 /// Q002
@@ -259,12 +259,14 @@ fn docstring(locator: &Locator, range: TextRange, settings: &LinterSettings) -> 
 
     let text = locator.slice(range);
     let trivia: Trivia = text.into();
-    if trivia.has_empty_text() && text_ends_at_quote(locator, range, settings) {
+    if trivia.has_empty_text()
+        && text_ends_at_quote(locator, range, &settings.flake8_quotes.docstring_quotes)
+    {
         // Cannot fix. Fix would result in an one-sided multi-line docstring,
         // which would introduce an error.
         let diagnostic = Diagnostic::new(
             BadQuotesDocstring {
-                preferred_quote: quotes_settings.multiline_quotes,
+                preferred_quote: quotes_settings.docstring_quotes,
             },
             range,
         );
@@ -377,7 +379,9 @@ fn strings(
             // If we're not using the preferred type, only allow use to avoid escapes.
             && !relax_quote
         {
-            if trivia.has_empty_text() && text_ends_at_quote(locator, *range, settings) {
+            if trivia.has_empty_text()
+                && text_ends_at_quote(locator, *range, &settings.flake8_quotes.inline_quotes)
+            {
                 // Cannot fix. Fix would result in an one-sided multi-line docstring,
                 // which would introduce an error.
                 let diagnostic = Diagnostic::new(
