e49bbd20ffafbcf3a70bbba05fa51dc1c3a456f8
Ottavio Hartman <omh1280@gmail.com>
2024-03-17 10:51:44 -0400

    cleanup

--- crates/ruff_linter/src/rules/flake8_bugbear/rules/except_with_non_exception_classes.rs
+++ crates/ruff_linter/src/rules/flake8_bugbear/rules/except_with_non_exception_classes.rs
@@ -44,22 +44,18 @@ impl Violation for ExceptWithNonExceptionClasses {
     }
 }
 
-/// Given an [`Expr`], flatten any [`Expr::Starred`] expressions.
+/// Given an [`Expr`], flatten any [`Expr::Starred`] expressions and any
+/// [`Expr::BinOp`] expressions into a flat list of expressions.
 /// This should leave any unstarred iterables alone (subsequently raising a
 /// warning for B029).
-fn flatten_starred_iterables(expr: &Expr) -> Vec<&Expr> {
-    // let Expr::Tuple(ast::ExprTuple { elts, .. }) = expr else {
-    //     return vec![expr];
-    // };
-    // let mut exprs_to_process: VecDeque<&Expr> = elts.iter().collect();
+fn flatten_starred_iterables_and_binops(expr: &Expr) -> Vec<&Expr> {
+    // The top-level expression should be a tuple or a binop, else return as-is.
     let mut exprs_to_process: VecDeque<&Expr> = match expr {
         Expr::Tuple(ast::ExprTuple { elts, .. }) => elts.iter().collect(),
-        Expr::BinOp(ast::ExprBinOp { .. }) => {
-            // vec![left.as_ref(), right.as_ref()].into()
-            vec![expr].into()
-        }
+        Expr::BinOp(ast::ExprBinOp { .. }) => vec![expr].into(),
         _ => return vec![expr],
     };
+
     let mut flattened_exprs: Vec<&Expr> = Vec::new();
     while let Some(expr) = exprs_to_process.pop_front() {
         match expr {
@@ -72,48 +68,21 @@ fn flatten_starred_iterables(expr: &Expr) -> Vec<&Expr> {
             },
             Expr::BinOp(ast::ExprBinOp { left, right, .. }) => {
                 // if left or right are tuples, we should flatten them
-                match left.as_ref() {
-                    Expr::Tuple(ast::ExprTuple { elts, .. }) => {
-                        exprs_to_process.append(&mut elts.iter().collect());
+                for expr in [left, right] {
+                    match expr.as_ref() {
+                        Expr::Tuple(ast::ExprTuple { elts, .. }) => {
+                            exprs_to_process.append(&mut elts.iter().collect());
+                        }
+                        Expr::Starred(ast::ExprStarred { value, .. }) => {
+                            exprs_to_process.push_back(value);
+                        }
+                        _ => flattened_exprs.push(expr),
                     }
-                    Expr::Starred(ast::ExprStarred { value, .. }) => {
-                        exprs_to_process.push_back(value);
-                    }
-                    _ => flattened_exprs.push(left),
-                }
-                match right.as_ref() {
-                    Expr::Tuple(ast::ExprTuple { elts, .. }) => {
-                        exprs_to_process.append(&mut elts.iter().collect());
-                    }
-                    Expr::Starred(ast::ExprStarred { value, .. }) => {
-                        exprs_to_process.push_back(value);
-                    }
-                    _ => flattened_exprs.push(right),
                 }
             }
             _ => flattened_exprs.push(expr),
         }
     }
-    flattened_exprs
-}
-
-/// Given an [`Expr`], flatten any binary operations.
-fn flatten_bin_op(expr: &Expr) -> Vec<&Expr> {
-    let mut stack = vec![expr];
-    let mut flattened_exprs = Vec::new();
-
-    while let Some(expr) = stack.pop() {
-        if let Expr::BinOp(ast::ExprBinOp { left, right, .. }) = expr {
-            stack.push(left);
-            stack.push(right);
-        // } else if let Expr::Tuple(ast::ExprTuple { elts, .. }) = expr {
-        //     stack.extend(elts.iter());
-        } else {
-            println!("Pushing expression: {:#?}", expr);
-            flattened_exprs.push(expr);
-        }
-    }
-
     flattened_exprs
 }
 
@@ -127,15 +96,11 @@ pub(crate) fn except_with_non_exception_classes(
     let Some(type_) = type_ else {
         return;
     };
-    // let flattened_exprs = flatten_starred_iterables(type_);
-    // println!("{:#?}", flattened_exprs);
-    // let flattened_exprs = flattened_exprs.iter().flat_map(|expr| flatten_bin_op(expr));
-    for expr in flatten_starred_iterables(type_) {
+    for expr in flatten_starred_iterables_and_binops(type_) {
         if !matches!(
             expr,
             Expr::Subscript(_) | Expr::Attribute(_) | Expr::Name(_) | Expr::Call(_),
         ) {
-            println!("Adding diagnostic for expression: {:#?}", expr);
             checker
                 .diagnostics
                 .push(Diagnostic::new(ExceptWithNonExceptionClasses, expr.range()));
