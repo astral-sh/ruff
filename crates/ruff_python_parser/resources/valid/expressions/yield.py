16a80fd526acc142875c11178b82092c9f2ad397
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-19 18:16:53 +0530

    Move parser tests to new valid/invalid framework (#10471)
    This PR is just a mechanical change to move the existing parser tests to
    use the new valid/invalid framework. It also removes duplicate test
    cases and rearranges wherever feasible.
    
    The with item and IPython syntax tests aren't ported yet. The former
    will be done once the #10418 PR is merged while the latter will be done
    later.
    
    The snapshots were updated and old snapshots were removed.
    
    `cargo test`

---
+++ crates/ruff_python_parser/resources/valid/expressions/yield.py
@@ -0,0 +1,11 @@
+yield *y
+yield x
+yield x + 1
+yield a and b
+yield f()
+yield [1, 2]
+yield {3, 4}
+yield {i: 5}
+yield 7, 8
+yield (9, 10)
+yield 1 == 1
---
+++ crates/ruff_python_parser/resources/valid/expressions/yield_from.py
@@ -0,0 +1,9 @@
+yield from x
+yield from x + 1
+yield from a and b
+yield from f()
+yield from [1, 2]
+yield from {3, 4}
+yield from {i: 5}
+yield from (9, 10)
+yield from 1 == 1
