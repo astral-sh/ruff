01e9707ba8b913780f30373bf05e61fc6dbcc6ad
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-19 17:43:40 +0530

    Move parser tests to new valid/invalid framework

--- crates/ruff_python_parser/resources/valid/expressions/list_comprehension.py
+++ crates/ruff_python_parser/resources/valid/expressions/list_comprehension.py
@@ -1,1 +1,16 @@
 x = [y for y in (1, 2, 3)]
+
+[x for i in range(5)]
+[b for c in d if x in w if y and yy if z]
+[a for b in c if d and e for f in j if k > h]
+[a for b in c if d and e async for f in j if k > h]
+[1 for i in x in a]
+[a for a, b in G]
+[
+    await x for a, b in C
+]
+[i for i in await x if entity is not None]
+[x for x in (l if True else L) if T]
+[i for i in (await x if True else X) if F]
+[i for i in await (x if True else X) if F]
+[f for f in c(x if True else [])]
--- crates/ruff_python_parser/resources/valid/statement/for.py
+++ crates/ruff_python_parser/resources/valid/statement/for.py
@@ -1,2 +1,34 @@
-for x in (1, 2, 3):
+for target in iter:
+    pass
+
+for target in (1, 2, 3):
+    pass
+
+for target.attr in call():
+    pass
+
+for 1 + 2 in x.attr:
+    pass
+
+for target in x <= y:
+    pass
+
+for target in a and b:
+    pass
+
+for a, b, c, in iter:
+    pass
+
+for (a, b) in iter:
+    pass
+
+for target in *x.attr:
+    pass
+
+for -target in [1, 2]:
+    pass
+
+for *target in a, b, c,:
+    pass
+else:
     pass
