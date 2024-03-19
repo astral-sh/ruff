bc9b4571eb1b4c51ed860b35798b74f40831517a
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-03-19 14:01:33 -0400

    Avoid failures due to non-deterministic binding ordering (#10478)
    ## Summary
    
    We're seeing failures in https://github.com/astral-sh/ruff/issues/10470
    because `resolve_qualified_import_name` isn't guaranteed to return a
    specific import if a symbol is accessible in two ways (e.g., you have
    both `import logging` and `from logging import error` in scope, and you
    want `logging.error`). This PR breaks up the failing tests such that the
    imports aren't in the same scope.
    
    Closes https://github.com/astral-sh/ruff/issues/10470.
    
    ## Test Plan
    
    I added a `bindings.reverse()` to `resolve_qualified_import_name` to
    ensure that the tests pass regardless of the binding order.

--- crates/ruff_linter/resources/test/fixtures/refurb/FURB163.py
+++ crates/ruff_linter/resources/test/fixtures/refurb/FURB163.py
@@ -1,9 +1,6 @@
 import math
 
-from math import e as special_e
-from math import log as special_log
-
-# Errors.
+# Errors
 math.log(1, 2)
 math.log(1, 10)
 math.log(1, math.e)
@@ -11,15 +8,10 @@ foo = ...
 math.log(foo, 2)
 math.log(foo, 10)
 math.log(foo, math.e)
-math.log(1, special_e)
-special_log(1, 2)
-special_log(1, 10)
-special_log(1, math.e)
-special_log(1, special_e)
 math.log(1, 2.0)
 math.log(1, 10.0)
 
-# Ok.
+# OK
 math.log2(1)
 math.log10(1)
 math.log(1)
@@ -39,6 +31,7 @@ math.log2(1, 10)  # math.log2 takes only one argument.
 math.log10(1, 2)  # math.log10 takes only one argument.
 
 math.log(1, base=2)  # math.log does not accept keyword arguments.
+
 
 def log(*args):
     print(f"Logging: {args}")
