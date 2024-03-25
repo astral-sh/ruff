dbc6207598d56b9ab6f72f0fb61ee6d798eb40e6
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-25 08:37:52 +0530

    Add tests for list expression parsing

---
+++ crates/ruff_python_parser/resources/invalid/expressions/list/star_expression_precedence.py
@@ -0,0 +1,10 @@
+# For list expression, the minimum binding power of star expression is bitwise or.
+
+[(*x), y]
+[*x in y, z]
+[*not x, z]
+[*x and y, z]
+[*x or y, z]
+[*x if True else y, z]
+[*lambda x: x, z]
+[*x := 2, z]
