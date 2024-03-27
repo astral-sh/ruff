4544bb4eeace31119d07f8bb1285d0ea0a650eea
Robin Caloudis <robin.caloudis@gmx.de>
2024-03-27 23:01:07 +0100

    Test compound level as well

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/notebook_docstring.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/notebook_docstring.py
@@ -4,3 +4,14 @@
 """;
 
 "another normal string"
+
+
+def raw_docstring():
+
+    a = """
+    This looks like a docstring but is not in a notebook because notebooks can't be imported as a module.
+    Ruff should leave it as is
+    """;
+
+    b = "another normal string"
+    pass
