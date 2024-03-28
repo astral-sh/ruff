764b0879c5e125ab148714b29d9bbfc903f2e3a0
boolean-light <61526956+boolean-light@users.noreply.github.com>
2024-03-28 23:06:14 +0900

    Fixed message for violation

--- crates/ruff_linter/src/rules/refurb/rules/verbose_decimal_fraction_construction.rs
+++ crates/ruff_linter/src/rules/refurb/rules/verbose_decimal_fraction_construction.rs
@@ -54,7 +54,7 @@ impl Violation for VerboseDecimalFractionConstruction {
 
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("Verbose expression in constructing `{}`", self.constructor)
+        format!("Verbose method `{}` when constructing `{}`", self.method_name, self.constructor)
     }
 
     fn fix_title(&self) -> Option<String> {
