78d6d0c51d27068e960c62edda6c4843f6954be3
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-29 13:33:31 +0530

    Add tests for list expression parsing (#10554)
    ## Summary
    
    This PR updates the existing valid test case for list expression and
    adds some invalid ones to check how the parser is recovering. It also
    updates the parsing function used to match the grammar.
    
    The goal is mainly to check if the list parsing is correct or not as per
    the grammar.
    
    There are test cases for unclosed bracket where the parser sometimes
    doesn't recover well or doesn't provide useful error messages. I think
    these will be improved by feeding the information back to the lexer. The
    lexer could then emit `Newline` where appropriate instead of
    `NonLogicalNewline` and the recovery set could include the `Newline` as
    the list terminator to recover from it.

--- crates/ruff_python_parser/resources/valid/expressions/list.py
+++ crates/ruff_python_parser/resources/valid/expressions/list.py
@@ -1,5 +1,34 @@
-[1 + i, [1, 2, 3, 4], (a, i + x, y), {a, b, c}, {a: 1}]
-[1, 2, 3]
+# Simple lists
 []
 [1]
-[f(g(attr.H()) for c in l)]
+[1,]
+[1, 2, 3]
+[1, 2, 3,]
+
+# Mixed with indentations
+[
+]
+[
+        1
+]
+[
+    1,
+        2,
+]
+
+# Nested
+[[[1]]]
+[[1, 2], [3, 4]]
+
+# Named expression
+[x := 2]
+[x := 2,]
+[1, x := 2, 3]
+
+# Star expression
+[1, *x, 3]
+[1, *x | y, 3]
+
+# Random expressions
+[1 + 2, [1, 2, 3, 4], (a, b + c, d), {a, b, c}, {a: 1}, x := 2]
+[call1(call2(value.attr()) for element in iter)]
