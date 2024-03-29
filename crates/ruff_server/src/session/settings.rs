72d7267352f0d8e0fb940c8914a11932460cb583
Jane Lewis <jane@astral.sh>
2024-03-28 22:13:21 -0700

    Check support for code action data fields when checking support for deferred code action resolution

--- crates/ruff_server/src/session/settings.rs
+++ crates/ruff_server/src/session/settings.rs
@@ -7,13 +7,19 @@ pub(crate) struct ResolvedClientCapabilities {
 
 impl ResolvedClientCapabilities {
     pub(super) fn new(client_capabilities: &ClientCapabilities) -> Self {
+        let code_action_settings = client_capabilities
+            .text_document
+            .as_ref()
+            .and_then(|doc_settings| doc_settings.code_action.as_ref());
+        let code_action_data_support = code_action_settings
+            .and_then(|code_action_settings| code_action_settings.data_support)
+            .unwrap_or_default();
+        let code_action_edit_resolution = code_action_settings
+            .and_then(|code_action_settings| code_action_settings.resolve_support.as_ref())
+            .is_some_and(|resolve_support| resolve_support.properties.contains(&"edit".into()));
         Self {
-            code_action_deferred_edit_resolution: client_capabilities
-                .text_document
-                .as_ref()
-                .and_then(|doc_settings| doc_settings.code_action.as_ref())
-                .and_then(|code_action_settings| code_action_settings.resolve_support.as_ref())
-                .is_some_and(|resolve_support| resolve_support.properties.contains(&"edit".into())),
+            code_action_deferred_edit_resolution: code_action_data_support
+                && code_action_edit_resolution,
         }
     }
 }
