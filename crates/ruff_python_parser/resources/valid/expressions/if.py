cbdb0558f0d2086c438ec8e6e9ebe6e7941ee974
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-04-03 13:59:37 +0530

    Add tests for conditional expression (#10660)
    ## Summary
    
    This PR adds test cases for conditional expressions.
    
    For context, the grammar is:
    ```
    expression:
        | disjunction 'if' disjunction 'else' expression 
        | disjunction
        | lambdef
    ```
    
    `disjunction` is the start of operator precedence parsing with `or`
    operator.

--- crates/ruff_python_parser/resources/valid/expressions/if.py
+++ crates/ruff_python_parser/resources/valid/expressions/if.py
@@ -6,3 +6,16 @@ a and b if x else False
 x <= y if y else x
 True if a and b else False
 1, 1 if a else c
+
+# Lambda is allowed in orelse expression
+x if True else lambda y: y
+
+# These test expression are only allowed when parenthesized
+x if (yield x) else y
+x if (yield from x) else y
+x if (lambda x: x) else y
+
+# Split across multiple lines
+(x
+if y
+else z)
