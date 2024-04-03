5491268bbe65824aaed782d5c25e0de8072d621e
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-04-03 15:17:13 +0530

    Add tests for function call arguments (#10733)
    ## Summary
    
    This PR adds tests for call arguments.

--- crates/ruff_python_parser/resources/invalid/expressions/call_invalid_argument_ordering.py
+++
@@ -1,6 +0,0 @@
-f(b=20, c)
-
-f(**b, *c)
-
-# Duplicate keyword argument
-f(a=20, a=30)
---
+++ crates/ruff_python_parser/resources/valid/expressions/arguments.py
@@ -0,0 +1,50 @@
+# This only tests the call arguments and not the expression before the opening parenthesis.
+
+# Simple
+call()
+call(x, y)
+call(x, y,)  # Trailing comma
+call(x=1, y=2)
+call(*x)
+call(**x)
+
+# Order
+call(x, y=1)
+call(x, *y)
+call(x, **y)
+call(x=1, *y)
+call(x=1, **y)
+call(*x, **y)
+call(*x, y, z)
+call(**x, y=1, z=2)
+call(*x1, *x2, **y1, **y2)
+call(x=1, **y, z=1)
+
+# Keyword expression
+call(x=1 if True else 2)
+call(x=await y)
+call(x=lambda y: y)
+call(x=(y := 1))
+
+# Yield expression
+call((yield x))
+call((yield from x))
+
+# Named expression
+call(x := 1)
+call(x := 1 for x in iter)
+
+# Starred expressions
+call(*x and y)
+call(*x | y)
+call(*await x)
+call(*lambda x: x)
+call(*x if True else y)
+
+# Double starred
+call(**x)
+call(**x and y)
+call(**await x)
+call(**x if True else y)
+call(**(yield x))
+call(**lambda x: x)
