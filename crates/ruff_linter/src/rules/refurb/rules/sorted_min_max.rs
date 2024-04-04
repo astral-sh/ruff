0e94732584d146de358d8d7be2f29800d31e86cd
Ottavio Hartman <omh1280@gmail.com>
2024-04-03 22:22:59 -0400

    impl nearly done

--- crates/ruff_linter/src/rules/refurb/rules/sorted_min_max.rs
+++ crates/ruff_linter/src/rules/refurb/rules/sorted_min_max.rs
@@ -1,11 +1,16 @@
+use ruff_diagnostics::Diagnostic;
+use ruff_diagnostics::Edit;
+use ruff_diagnostics::Fix;
 use ruff_diagnostics::FixAvailability;
 use ruff_diagnostics::Violation;
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::Expr;
 use ruff_python_ast::ExprSubscript;
+use ruff_python_semantic::SemanticModel;
 
 use crate::checkers::ast::Checker;
 use ruff_python_ast::Number;
+use ruff_text_size::Ranged;
 
 /// ## What it does
 /// Checks for uses of `sorted()` to get the min and max values of a list.
@@ -54,32 +59,44 @@ pub(crate) fn sorted_min_max(checker: &mut Checker, subscript: &ExprSubscript) {
         return;
     }
 
-    let Some(name) = match_sorted_min_max(subscript) else {
+    let Some(name) = match_sorted_min_max(subscript, checker.semantic()) else {
         return;
     };
 
-    // let diagnostic = Diagnostic::new(SortedMinMax, TextRange::default());
-    // checker.diagnostics.push(diagnostic);
+    let mut diagnostic = Diagnostic::new(SortedMinMax, subscript.range());
+    diagnostic.set_fix(Fix::safe_edit(Edit::replacement(
+        name.to_string(),
+        subscript.start(),
+        subscript.end(),
+    )));
+    checker.diagnostics.push(diagnostic);
 }
 
-pub(crate) fn match_sorted_min_max<'a>(
-    ExprSubscript { value, slice, .. }: &'a ExprSubscript,
-) -> Option<&'a str> {
-    match slice.as_ref() {
-        Expr::NumberLiteral(number_literal) => {
+pub(crate) fn match_sorted_min_max(
+    ExprSubscript { value, slice, .. }: &ExprSubscript,
+    semantic: &SemanticModel,
+) -> Option<i8> {
+    // Early return if index is not 0 or 1
+    let index = {
+        if let Expr::NumberLiteral(number_literal) = slice.as_ref() {
             if let Number::Int(number) = &number_literal.value {
-                if !number
+                return number
                     .as_i8()
-                    .is_some_and(|number| number == 0 || number == 1)
-                {
-                    return None;
-                }
-            } else {
-                return None;
+                    .and_then(|n| if n == 0 || n == 1 { Some(n) } else { None });
             }
         }
-        _ => return None,
+        None
     };
+
+    // Check if the value is a call to `sorted()`
+    if let Expr::Call(call) = value.as_ref() {
+        if let Expr::Name(name) = call.func.as_ref() {
+            if name.id == "sorted" && semantic.is_builtin(name.id.as_str()) {
+                return index;
+            }
+        }
+    }
+
     None
 }
 
