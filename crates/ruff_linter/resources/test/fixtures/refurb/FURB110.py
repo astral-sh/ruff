a52c531e14f410cccb15cca2c23b45f8cbb237a8
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-04-06 12:30:14 -0400

    Tweak message, add more tests

--- crates/ruff_linter/resources/test/fixtures/refurb/FURB110.py
+++ crates/ruff_linter/resources/test/fixtures/refurb/FURB110.py
@@ -6,3 +6,25 @@ z = x \
 z = x if x \
     else  \
         y  # FURB110
+
+z = x() if x() else y()  # FURB110
+
+# FURB110
+z = x if (
+    # Test for x.
+    x
+) else (
+    # Test for y.
+    y
+)
+
+# FURB110
+z = (
+    x if (
+        # Test for x.
+        x
+    ) else (
+        # Test for y.
+        y
+    )
+)
--- crates/ruff_linter/src/checkers/ast/analyze/expression.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/expression.rs
@@ -1363,8 +1363,8 @@ pub(crate) fn expression(expr: &Expr, checker: &mut Checker) {
             if checker.enabled(Rule::IfExprMinMax) {
                 refurb::rules::if_expr_min_max(checker, if_exp);
             }
-            if checker.enabled(Rule::OrOper) {
-                refurb::rules::or_oper(checker, if_exp);
+            if checker.enabled(Rule::IfExpInsteadOfOrOperator) {
+                refurb::rules::if_exp_instead_of_or_operator(checker, if_exp);
             }
         }
         Expr::ListComp(
