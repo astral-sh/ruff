f75c4ae38fd366a97470f182a570d2f03c053d31
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-04-10 08:21:17 +0530

    Move IPython test snapshots

--- crates/ruff_python_parser/src/parser/tests.rs
+++ crates/ruff_python_parser/src/parser/tests.rs
@@ -1,18 +1,7 @@
 mod parser;
 mod suite;
 
-use crate::{parse, parse_suite, Mode};
-
-// This is a sanity test for what looks like an ipython directive being
-// assigned to. Although this doesn't actually parse as an assignment
-// statement, but rather, a directive whose value is `foo = 42`.
-#[test]
-fn ok_ipy_escape_command() {
-    let source = r"!foo = 42";
-    let ast = parse(source, Mode::Ipython);
-
-    insta::assert_debug_snapshot!(ast);
-}
+use crate::{lex, parse, parse_suite, parse_tokens, Mode};
 
 #[test]
 fn test_modes() {
@@ -29,4 +18,96 @@ fn test_unicode_aliases() {
     let parse_ast = parse_suite(source).unwrap();
 
     insta::assert_debug_snapshot!(parse_ast);
+}
+
+#[test]
+fn test_ipython_escape_commands() {
+    let parse_ast = parse(
+        r"
+# Normal Python code
+(
+    a
+    %
+    b
+)
+
+# Dynamic object info
+??a.foo
+?a.foo
+?a.foo?
+??a.foo()??
+
+# Line magic
+%timeit a = b
+%timeit foo(b) % 3
+%alias showPath pwd && ls -a
+%timeit a =\
+  foo(b); b = 2
+%matplotlib --inline
+%matplotlib \
+    --inline
+
+# System shell access
+!pwd && ls -a | sed 's/^/\    /'
+!pwd \
+  && ls -a | sed 's/^/\\    /'
+!!cd /Users/foo/Library/Application\ Support/
+
+# Let's add some Python code to make sure that earlier escapes were handled
+# correctly and that we didn't consume any of the following code as a result
+# of the escapes.
+def foo():
+    return (
+        a
+        !=
+        b
+    )
+
+# Transforms into `foo(..)`
+/foo 1 2
+;foo 1 2
+,foo 1 2
+
+# Indented escape commands
+for a in range(5):
+    !ls
+
+p1 = !pwd
+p2: str = !pwd
+foo = %foo \
+    bar
+
+% foo
+foo = %foo  # comment
+
+# Help end line magics
+foo?
+foo.bar??
+foo.bar.baz?
+foo[0]??
+foo[0][1]?
+foo.bar[0].baz[1]??
+foo.bar[0].baz[2].egg??
+"
+        .trim(),
+        Mode::Ipython,
+    )
+    .unwrap();
+    insta::assert_debug_snapshot!(parse_ast);
+}
+
+#[test]
+fn test_ipython_escape_command_parse_error() {
+    let source = r"
+a = 1
+%timeit a == 1
+    "
+    .trim();
+    let lxr = lex(source, Mode::Ipython);
+    let parse_err = parse_tokens(lxr.collect(), source, Mode::Module).unwrap_err();
+    assert_eq!(
+        parse_err.to_string(),
+        "IPython escape commands are only allowed in `Mode::Ipython` at byte range 6..20"
+            .to_string()
+    );
 }
--- crates/ruff_python_parser/src/parser/tests/suite.rs
+++ crates/ruff_python_parser/src/parser/tests/suite.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::{lexer, parse, parse_suite, parse_tokens, Mode};
+    use crate::parse_suite;
 
     #[test]
     fn test_with_statement() {
@@ -87,97 +87,5 @@ with ((yield from a)): pass
         // TODO(dhruvmanila): Check error recovery for these cases
         assert!(parse_suite("a: type X = int").is_err());
         assert!(parse_suite("lambda: type X = int").is_err());
-    }
-
-    #[test]
-    fn test_ipython_escape_commands() {
-        let parse_ast = parse(
-            r"
-# Normal Python code
-(
-    a
-    %
-    b
-)
-
-# Dynamic object info
-??a.foo
-?a.foo
-?a.foo?
-??a.foo()??
-
-# Line magic
-%timeit a = b
-%timeit foo(b) % 3
-%alias showPath pwd && ls -a
-%timeit a =\
-  foo(b); b = 2
-%matplotlib --inline
-%matplotlib \
-    --inline
-
-# System shell access
-!pwd && ls -a | sed 's/^/\    /'
-!pwd \
-  && ls -a | sed 's/^/\\    /'
-!!cd /Users/foo/Library/Application\ Support/
-
-# Let's add some Python code to make sure that earlier escapes were handled
-# correctly and that we didn't consume any of the following code as a result
-# of the escapes.
-def foo():
-    return (
-        a
-        !=
-        b
-    )
-
-# Transforms into `foo(..)`
-/foo 1 2
-;foo 1 2
-,foo 1 2
-
-# Indented escape commands
-for a in range(5):
-    !ls
-
-p1 = !pwd
-p2: str = !pwd
-foo = %foo \
-    bar
-
-% foo
-foo = %foo  # comment
-
-# Help end line magics
-foo?
-foo.bar??
-foo.bar.baz?
-foo[0]??
-foo[0][1]?
-foo.bar[0].baz[1]??
-foo.bar[0].baz[2].egg??
-"
-            .trim(),
-            Mode::Ipython,
-        )
-        .unwrap();
-        insta::assert_debug_snapshot!(parse_ast);
-    }
-
-    #[test]
-    fn test_ipython_escape_command_parse_error() {
-        let source = r"
-a = 1
-%timeit a == 1
-    "
-        .trim();
-        let lxr = lexer::lex(source, Mode::Ipython);
-        let parse_err = parse_tokens(lxr.collect(), source, Mode::Module).unwrap_err();
-        assert_eq!(
-            parse_err.to_string(),
-            "IPython escape commands are only allowed in `Mode::Ipython` at byte range 6..20"
-                .to_string()
-        );
     }
 }
