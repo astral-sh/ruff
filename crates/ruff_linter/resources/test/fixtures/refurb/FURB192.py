b212e8716c656bebec0ed88debc3b135d9300370
Ottavio Hartman <omh1280@gmail.com>
2024-04-10 19:32:14 -0400

    updated rules

--- crates/ruff_linter/resources/test/fixtures/refurb/FURB192.py
+++ crates/ruff_linter/resources/test/fixtures/refurb/FURB192.py
@@ -4,12 +4,22 @@ sorted(l)[0]
 
 sorted(l)[-1]
 
-sorted(l)[0]
+sorted(l, reverse=True)[0]
 
-sorted(l)[0]
+sorted(l, reverse=True)[-1]
+
+sorted(l, reverse=False)[-1]
 
 sorted(l, key=lambda x: x)[0]
+
+sorted(l, key=key_fn, reverse=True)[-1]
+
+sorted(l, key=key_fn)[0]
 
 # Non-errors
 
 sorted(l, reverse=foo())[0]
+
+sorted([1, 2, 3])[0]
+
+sorted(l)[1]
