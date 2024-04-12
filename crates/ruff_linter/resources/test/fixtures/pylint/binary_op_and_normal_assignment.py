6fca9c0cfcbf6852a100f57117c72134452c502e
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-04-11 22:21:37 -0400

    Reformat fixture

--- crates/ruff_linter/resources/test/fixtures/pylint/binary_op_and_normal_assignment.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/binary_op_and_normal_assignment.py
@@ -1,44 +1,53 @@
-some_string = "some string" # PLR6104
-index, a_number, to_multiply, to_divide, to_cube, timeDiffSeconds, flags = 0, 1, 2, 3, 4, 5, 0x3 # PLR6104
-a_list = [1,2] # PLR6104
-some_set = {"elem"} # PLR6104
-mat1, mat2 = None, None # PLR6104
+# Errors
+some_string = "some string"
+index, a_number, to_multiply, to_divide, to_cube, timeDiffSeconds, flags = (
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    0x3,
+)
+a_list = [1, 2]
+some_set = {"elem"}
+mat1, mat2 = None, None
 
-some_string = (
-  some_string
-  + "a very long end of string"
-) # PLR6104
-index = index - 1 # PLR6104
-a_list = a_list + ["to concat"] # PLR6104
-some_set = some_set | {"to concat"} # PLR6104
-to_multiply = to_multiply * 5 # PLR6104
-to_divide = to_divide / 5 # PLR6104
-to_divide = to_divide // 5 # PLR6104
-to_cube = to_cube ** 3 # PLR6104
-timeDiffSeconds = timeDiffSeconds % 60 # PLR6104
-flags = flags & 0x1 # PLR6104
-flags = flags | 0x1 # PLR6104
-flags = flags ^ 0x1 # PLR6104
-flags = flags << 1 # PLR6104
-flags = flags >> 1 # PLR6104
-mat1 = mat1 @ mat2 # PLR6104
-a_list[1] = a_list[1] + 1 # PLR6104
+some_string = some_string + "a very long end of string"
+index = index - 1
+a_list = a_list + ["to concat"]
+some_set = some_set | {"to concat"}
+to_multiply = to_multiply * 5
+to_divide = to_divide / 5
+to_divide = to_divide // 5
+to_cube = to_cube**3
+timeDiffSeconds = timeDiffSeconds % 60
+flags = flags & 0x1
+flags = flags | 0x1
+flags = flags ^ 0x1
+flags = flags << 1
+flags = flags >> 1
+mat1 = mat1 @ mat2
+a_list[1] = a_list[1] + 1
 
-a_list[0:2] = a_list[0:2] * 3 # PLR6104
-a_list[:2] = a_list[:2] * 3 # PLR6104
-a_list[1:] = a_list[1:] * 3 # PLR6104
-a_list[:] = a_list[:] * 3 # PLR6104
+a_list[0:2] = a_list[0:2] * 3
+a_list[:2] = a_list[:2] * 3
+a_list[1:] = a_list[1:] * 3
+a_list[:] = a_list[:] * 3
 
-index = index * (index + 10)  # PLR6104
+index = index * (index + 10)
+
 
 class T:
     def t(self):
-        self.a = self.a + 1 # PLR6104
+        self.a = self.a + 1
+
 
 obj = T()
-obj.a = obj.a + 1 # PLR6104
+obj.a = obj.a + 1
 
-a_list[0] = a_list[:] * 3 # OK
-index = a_number = a_number + 1 # OK
-a_number = index = a_number + 1 # OK
-index = index * index + 10 # OK
+# OK
+a_list[0] = a_list[:] * 3
+index = a_number = a_number + 1
+a_number = index = a_number + 1
+index = index * index + 10
