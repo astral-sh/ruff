65edbfe62ffc0a692c2b5a01f448f5e4e9d19dbf
plredmond <51248199+plredmond@users.noreply.github.com>
2024-04-16 10:32:29 -0700

    Detect unneeded `async` keywords on functions (#9966)
    ## Summary
    
    This change adds a rule to detect functions declared `async` but lacking
    any of `await`, `async with`, or `async for`. This resolves #9951.
    
    ## Test Plan
    
    This change was tested by following
    https://docs.astral.sh/ruff/contributing/#rule-testing-fixtures-and-snapshots
    and adding positive and negative cases for each of `await` vs nothing,
    `async with` vs `with`, and `async for` vs `for`.

--- crates/ruff_linter/src/checkers/ast/analyze/statement.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/statement.rs
@@ -348,6 +348,9 @@ pub(crate) fn statement(stmt: &Stmt, checker: &mut Checker) {
             if checker.enabled(Rule::SslWithBadDefaults) {
                 flake8_bandit::rules::ssl_with_bad_defaults(checker, function_def);
             }
+            if checker.enabled(Rule::UnusedAsync) {
+                ruff::rules::unused_async(checker, function_def);
+            }
         }
         Stmt::Return(_) => {
             if checker.enabled(Rule::ReturnOutsideFunction) {
