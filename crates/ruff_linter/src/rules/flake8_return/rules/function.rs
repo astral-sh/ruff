ec68233db5ff449553f3e1a3cb9641ab70c3e715
Jonathan Plasse <jonathan.plasse@live.fr>
2024-04-21 22:50:14 +0200

    Set diagnostic range to be the entire function

--- crates/ruff_linter/src/rules/flake8_return/rules/function.rs
+++ crates/ruff_linter/src/rules/flake8_return/rules/function.rs
@@ -440,8 +440,8 @@ fn is_noreturn_func(func: &Expr, semantic: &SemanticModel) -> bool {
     semantic.match_typing_qualified_name(&qualified_name, "NoReturn")
 }
 
-fn add_return_none(checker: &mut Checker, stmt: &Stmt) {
-    let mut diagnostic = Diagnostic::new(ImplicitReturn, stmt.range());
+fn add_return_none(checker: &mut Checker, stmt: &Stmt, range: TextRange) {
+    let mut diagnostic = Diagnostic::new(ImplicitReturn, range);
     if let Some(indent) = indentation(checker.locator(), stmt) {
         let mut content = String::new();
         content.push_str(checker.stylist().line_ending().as_str());
@@ -488,7 +488,7 @@ fn has_implicit_return(checker: &mut Checker, stmt: &Stmt) -> bool {
                 None | Some(ast::ElifElseClause { test: Some(_), .. })
             ) {
                 if checker.settings.preview.is_disabled() {
-                    add_return_none(checker, stmt);
+                    add_return_none(checker, stmt, stmt.range());
                 }
                 return true;
             }
@@ -501,7 +501,7 @@ fn has_implicit_return(checker: &mut Checker, stmt: &Stmt) -> bool {
                 has_implicit_return(checker, last_stmt)
             } else {
                 if checker.settings.preview.is_disabled() {
-                    add_return_none(checker, stmt);
+                    add_return_none(checker, stmt, stmt.range());
                 }
                 true
             }
@@ -540,7 +540,7 @@ fn has_implicit_return(checker: &mut Checker, stmt: &Stmt) -> bool {
         }
         _ => {
             if checker.settings.preview.is_disabled() {
-                add_return_none(checker, stmt);
+                add_return_none(checker, stmt, stmt.range());
             }
             true
         }
@@ -548,9 +548,9 @@ fn has_implicit_return(checker: &mut Checker, stmt: &Stmt) -> bool {
 }
 
 /// RET503
-fn implicit_return(checker: &mut Checker, stmt: &Stmt) {
+fn implicit_return(checker: &mut Checker, function_def: &ast::StmtFunctionDef, stmt: &Stmt) {
     if has_implicit_return(checker, stmt) && checker.settings.preview.is_enabled() {
-        add_return_none(checker, stmt);
+        add_return_none(checker, stmt, function_def.range());
     }
 }
 
@@ -753,7 +753,12 @@ fn superfluous_elif_else(checker: &mut Checker, stack: &Stack) {
 }
 
 /// Run all checks from the `flake8-return` plugin.
-pub(crate) fn function(checker: &mut Checker, body: &[Stmt], returns: Option<&Expr>) {
+pub(crate) fn function(
+    checker: &mut Checker,
+    function_def: &ast::StmtFunctionDef,
+    body: &[Stmt],
+    returns: Option<&Expr>,
+) {
     // Find the last statement in the function.
     let Some(last_stmt) = body.last() else {
         // Skip empty functions.
@@ -799,7 +804,7 @@ pub(crate) fn function(checker: &mut Checker, body: &[Stmt], returns: Option<&Ex
             implicit_return_value(checker, &stack);
         }
         if checker.enabled(Rule::ImplicitReturn) {
-            implicit_return(checker, last_stmt);
+            implicit_return(checker, function_def, last_stmt);
         }
 
         if checker.enabled(Rule::UnnecessaryAssign) {
