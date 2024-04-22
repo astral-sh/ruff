8f2f738b5ba9c03b12fdff9fd0f1e22672e463c6
Micha Reiser <micha@reiser.io>
2024-04-22 14:05:58 +0200

    Remove salsa, introduce Db trait

--- crates/red_knot/src/parse.rs
+++ crates/red_knot/src/parse.rs
@@ -1,25 +1,50 @@
-#[allow(dead_code)]
-use ruff_python_ast::{self as ast};
+use std::sync::Arc;
+
+use crate::files::FileId;
+use ruff_python_ast as ast;
 use ruff_python_parser::{Mode, ParseError};
 use ruff_text_size::{Ranged, TextRange};
 
-pub(crate) struct SourceText {
-    pub text: String,
+use crate::source::Source;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Parsed {
+    inner: Arc<ParsedInner>,
 }
 
-pub(crate) struct Parsed {
-    pub ast: ast::ModModule,
-    pub errors: Vec<ParseError>,
+#[derive(Debug, PartialEq)]
+struct ParsedInner {
+    file_id: FileId,
+    ast: ast::ModModule,
+    errors: Vec<ParseError>,
 }
 
 impl Parsed {
-    fn new(ast: ast::ModModule, errors: Vec<ParseError>) -> Self {
-        Self { ast, errors }
+    fn new(file_id: FileId, ast: ast::ModModule, errors: Vec<ParseError>) -> Self {
+        Self {
+            inner: Arc::new(ParsedInner {
+                file_id,
+                ast,
+                errors,
+            }),
+        }
+    }
+
+    pub fn ast(&self) -> &ast::ModModule {
+        &self.inner.ast
+    }
+
+    pub fn file(&self) -> FileId {
+        self.inner.file_id
+    }
+
+    pub fn errors(&self) -> &[ParseError] {
+        &self.inner.errors
     }
 }
 
-pub(crate) fn parse(source: &SourceText) -> Parsed {
-    let result = ruff_python_parser::parse(&source.text, Mode::Module);
+pub(crate) fn parse(source: &Source) -> Parsed {
+    let result = ruff_python_parser::parse(source.text(), Mode::Module);
 
     let (module, errors) = match result {
         Ok(ast::Mod::Module(module)) => (module, vec![]),
@@ -42,5 +67,5 @@ pub(crate) fn parse(source: &SourceText) -> Parsed {
         ),
     };
 
-    Parsed::new(module, errors)
+    Parsed::new(source.file(), module, errors)
 }
--- crates/red_knot/src/program.rs
+++ crates/red_knot/src/program.rs
@@ -1,39 +1,32 @@
 #![allow(dead_code)]
-use crate::module::{ModuleId, ModuleName, ModuleResolver};
-use crate::parse::{parse, Parsed, SourceText};
-use crate::symbols::Symbols;
-use crate::FxDashMap;
+
+use crate::cache::{Cache, MapCache};
+use crate::{Db, ModuleDb, SourceDb};
 use anyhow::Result;
-use std::fs;
+use std::path::Path;
 use std::sync::Arc;
 
-struct Program {
+use crate::files::{FileId, Files};
+use crate::module::{Module, ModuleId, ModuleName, ModuleResolver, ModuleSearchPath};
+use crate::parse::{parse, Parsed};
+use crate::source::Source;
+
+#[derive(Debug)]
+pub struct Program {
     module_resolver: ModuleResolver,
-    parsed: FxDashMap<ModuleId, Arc<Parsed>>,
+    files: Files,
+    sources: MapCache<FileId, Source>,
+    parsed: MapCache<FileId, Parsed>,
 }
 
 impl Program {
-    pub fn new(module_resolver: ModuleResolver) -> Self {
+    pub fn new(module_search_paths: Vec<ModuleSearchPath>, files: Files) -> Self {
         Self {
-            module_resolver,
-            parsed: FxDashMap::default(),
+            module_resolver: ModuleResolver::new(module_search_paths, files.clone()),
+            files,
+            sources: Default::default(),
+            parsed: Default::default(),
         }
-    }
-
-    fn load_source(&self, module_id: ModuleId) -> Result<SourceText> {
-        // TODO filesystem access doesn't belong here, this is temporary until we have VFS
-        let path = self.module_resolver.path(module_id);
-        let contents = fs::read_to_string(path)?;
-        Ok(SourceText { text: contents })
-    }
-
-    fn parse(&self, module_id: ModuleId) -> Result<Arc<Parsed>> {
-        let parsed = parse(&self.load_source(module_id)?);
-        Ok(self
-            .parsed
-            .entry(module_id)
-            .insert(Arc::new(parsed))
-            .clone())
     }
 
     // TODO figure out the ownership to make this work
@@ -41,17 +34,75 @@ impl Program {
     //        Ok(Symbols::from_ast(&self.parse(module_id)?.ast))
     //    }
 
-    fn analyze_imports(&self, name: ModuleName) -> Result<Vec<String>> {
+    fn analyze_imports(&self, _name: ModuleName) -> Result<Vec<String>> {
         // TODO
         Ok(Vec::new())
     }
+
+    pub fn file_changed(&mut self, path: &Path) {
+        let Some(file_id) = self.files.try_get(path) else {
+            return;
+        };
+
+        self.module_resolver.remove_module(path);
+        self.sources.remove(&file_id);
+        self.parsed.remove(&file_id);
+    }
 }
+
+impl SourceDb for Program {
+    fn file_id(&self, path: &Path) -> FileId {
+        self.files.intern(path)
+    }
+
+    fn file_path(&self, file_id: FileId) -> Arc<Path> {
+        self.files.path(file_id)
+    }
+
+    fn source(&self, file_id: FileId) -> Source {
+        self.sources.get(&file_id, |file_id| {
+            let path = self.file_path(*file_id);
+
+            let source_text = std::fs::read_to_string(&path).unwrap_or_else(|err| {
+                tracing::error!(
+                    "Failed to read file '{path:?}: {err}'. Falling back to empty text"
+                );
+                String::new()
+            });
+
+            Source::new(*file_id, source_text)
+        })
+    }
+
+    fn parse(&self, source: &Source) -> Parsed {
+        // TODO still performs two lookups instead of just one.
+        // Can be avoided by lifting the requirement to pass the source text to the caller.
+        // But could make for more awkward code.
+        self.parsed.get(&source.file(), |_| parse(&source))
+    }
+}
+
+impl ModuleDb for Program {
+    fn resolve_module(&self, name: ModuleName) -> Option<ModuleId> {
+        // FIXME: The fact that `resolve_module` returns the id only is ab it annoying.
+        // What salsa does is that Module is only an id and the
+        // `path` or `name` methods call into the database
+        self.module_resolver.resolve(name)
+    }
+
+    fn module(&self, module_id: ModuleId) -> Module {
+        self.module_resolver.module(module_id)
+    }
+}
+
+impl Db for Program {}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::files::Files;
     use crate::module::{ModuleSearchPath, ModuleSearchPathKind};
+
+    use super::*;
 
     #[test]
     fn resolve_imports() -> std::io::Result<()> {
@@ -61,14 +112,14 @@ mod tests {
         let mod2 = root.join("mod2.py");
         std::fs::write(&mod1, "class C: pass")?;
         std::fs::write(&mod2, "from mod1 import C")?;
-        let resolver = ModuleResolver::new(
+
+        let program = Program::new(
             vec![ModuleSearchPath::new(
                 root,
                 ModuleSearchPathKind::FirstParty,
             )],
             Files::default(),
         );
-        let program = Program::new(resolver);
         let imported_symbol_names = program.analyze_imports(ModuleName::new("mod2")).unwrap();
 
         assert_eq!(imported_symbol_names, vec!["C"]);
