5ce1d23de3d6ea2a32a66cf3d6d9f511277ef9a0
Tomer Bin <tomerbin98@gmail.com>
2024-04-23 14:58:45 +0300

    add pathlib open test cases

--- crates/ruff_linter/resources/test/fixtures/pylint/open_without_with.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/open_without_with.py
@@ -1,30 +1,43 @@
+from pathlib import Path
+
+
 def foo(f):
     pass
 
 
 # Correct usage
-
 with open('path', "wb"):
     pass
 
+with open('path', "wb") as f:
+    pass
+
+with open('path', "wb") as f:
+    foo(f)
+
 foo('a')
 
-# Bad usage
+with Path('path').open("wb"):
+    pass
 
+with Path('path').open("wb") as f:
+    pass
+
+
+
+
+# Bad usage
+open()  # [open-without-with]
 
 open('path', "wb")  # [open-without-with]
 
-f1 = open('path', "wb")  # [open-without-with]
-
 f = open('path', "wb")  # [open-without-with]
+
 f.close()
 
 foo(open('path', "wb"))  # [open-without-with]
 
-
 with foo(open('path', "wb")) as f:  # [open-without-with]
     pass
 
-
-
-
+Path('path').open()  # [open-without-with]
