c8d315003471018ed0e021a0813da62087b3fc95
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-05-04 01:20:24 +0530

    Add test for nested literals

--- crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI062.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI062.py
@@ -4,6 +4,9 @@ x: Literal[True, False, True, False]  # PYI062 twice here
 
 y: Literal[1, print("hello"), 3, Literal[4, 1]]  # PYI062 on the last 1
 
-z: Literal[{1, 3, 5}, "foobar", {1,3,5}]  # PY062 on the set literal
+z: Literal[{1, 3, 5}, "foobar", {1,3,5}]  # PYI062 on the set literal
+
+# Ensure issue is only raised once, even on nested literals
+MyType = Literal["foo", Literal[True, False, True], "bar"]  # PYI062
 
 n: Literal["No", "duplicates", "here", 1, "1"]
--- crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI062.pyi
+++ crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI062.pyi
@@ -6,4 +6,7 @@ y: Literal[1, print("hello"), 3, Literal[4, 1]]  # PY062 on the last 1
 
 z: Literal[{1, 3, 5}, "foobar", {1,3,5}]  # PY062 on the set literal
 
+# Ensure issue is only raised once, even on nested literals
+MyType = Literal["foo", Literal[True, False, True], "bar"]  # PYI062
+
 n: Literal["No", "duplicates", "here", 1, "1"]
