ed30b1a1c7b981c3f2e5a752c23cc320d3310b51
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-05-03 17:11:01 +0530

    i thought i set a pre commit hook for clippy

--- crates/ruff_linter/src/rules/flake8_pyi/rules/generic_not_last_base_class.rs
+++ crates/ruff_linter/src/rules/flake8_pyi/rules/generic_not_last_base_class.rs
@@ -2,7 +2,7 @@ use ruff_diagnostics::{Diagnostic, Edit, Fix, FixAvailability, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::helpers::map_subscript;
 use ruff_python_ast::identifier::Identifier;
-use ruff_python_ast::{self as ast, Arguments, Expr, StmtClassDef};
+use ruff_python_ast::{Arguments, Expr, StmtClassDef};
 use ruff_python_semantic::SemanticModel;
 use ruff_source_file::Locator;
 use ruff_text_size::Ranged;
