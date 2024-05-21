cfb4131812e842bf77705ee055938c6d32b17b92
Evan Kohilas <ekohilas@users.noreply.github.com>
2024-05-21 17:17:35 -0400

    cleaned up test

--- crates/ruff_linter/resources/test/fixtures/flake8_async/ASYNC116.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_async/ASYNC116.py
@@ -4,46 +4,48 @@ import math
 from math import inf
 
 import trio
+from trio import sleep
 
 
 async def foo():
     # These examples are probably not meant to ever wake up:
-    await trio.sleep(100000)  # error: 10, "trio"
-
-    # and these ones _definitely_ never wake up:
-    await trio.sleep(float("inf"))  # error: 10, "trio"
-    await trio.sleep(math.inf)  # error: 10, "trio"
-    await trio.sleep(inf)  # error: 10, "trio"
+    await trio.sleep(100000)  # error: 116, "async"
 
     # 'inf literal' overflow trick
-    await trio.sleep(1e999)  # error: 10, "trio"
+    await trio.sleep(1e999)  # error: 116, "async"
 
     await trio.sleep(86399)
     await trio.sleep(86400)
-    await trio.sleep(86400.01)  # error: 10, "trio"
-    await trio.sleep(86401)  # error: 10, "trio"
+    await trio.sleep(86400.01)  # error: 116, "async"
+    await trio.sleep(86401)  # error: 116, "async"
 
-    await trio.sleep(-1)  # will raise a runtime error from Trio
+    # catch from import 
+    await sleep(86401)  # error: 116, "async"
+
+    await trio.sleep(-1)  # will raise a runtime error
     await trio.sleep(0)  # handled by different check
 
-    # don't evaluate expressions
-    await trio.sleep(10**10)
+    # these ones _definitely_ never wake up (TODO)
+    await trio.sleep(float("inf"))
+    await trio.sleep(math.inf)
+    await trio.sleep(inf)
+
+    # don't require inf to be in math (TODO)
+    await trio.sleep(np.inf)
+
+    # don't evaluate expressions (TODO)
+    one_day = 86401 
     await trio.sleep(86400 + 1)
+    await trio.sleep(60 * 60 * 24 + 1)
     await trio.sleep(foo())
-    await trio.sleep(86400 + foo())  # type: ignore
-    await trio.sleep(86400 + ...)  # type: ignore
+    await trio.sleep(one_day)
+    await trio.sleep(86400 + foo()) 
+    await trio.sleep(86400 + ...)
     await trio.sleep("hello")
     await trio.sleep(...)
 
-    # don't require inf to be in math #103
-    await trio.sleep(np.inf)  # error: 10, "trio"
-    await trio.sleep(anything.inf)  # error: 10, "trio"
-    await trio.sleep(anything.anything.inf)  # error: 10, "trio"
-    await trio.sleep(inf.inf)  # error: 10, "trio"
-    await trio.sleep(inf.anything)
-
 
 # does not require the call to be awaited, nor in an async fun
-trio.sleep(86401)  # error: 0, "trio"
+trio.sleep(86401)  # error: 116, "async" 
 # also checks that we don't break visit_Call
-trio.run(trio.sleep(86401))  # error: 9, "trio"
+trio.run(trio.sleep(86401))  # error: 116, "async"
