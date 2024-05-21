1b1639105d5803b47e38353d9af73c192a6e73f6
Evan Kohilas <ekohilas@users.noreply.github.com>
2024-05-21 15:48:24 -0400

    add docs

--- crates/ruff_linter/src/rules/flake8_async/rules/sleep_forever_call.rs
+++ crates/ruff_linter/src/rules/flake8_async/rules/sleep_forever_call.rs
@@ -4,15 +4,28 @@ use ruff_macros::{derive_message_formats, violation};
 use crate::checkers::ast::Checker;
 
 /// ## What it does
+/// Checks for uses of `trio.sleep()` with >24 hour interval.
 ///
 /// ## Why is this bad?
+/// `trio.sleep()` with a >24 hour interval is usually intended to sleep indefintely.
+/// This intent is be better conveyed using `trio.sleep_forever()`.
 ///
 /// ## Example
 /// ```python
+/// import trio
+///
+///
+/// async def func():
+///     await trio.sleep(86401)
 /// ```
 ///
 /// Use instead:
 /// ```python
+/// import trio
+///
+///
+/// async def func():
+///     await trio.sleep_forever()
 /// ```
 #[violation]
 pub struct SleepForeverCall;
@@ -20,7 +33,7 @@ pub struct SleepForeverCall;
 impl Violation for SleepForeverCall {
     #[derive_message_formats]
     fn message(&self) -> String {
-        format!("TODO: write message: {}", todo!("implement message"))
+        format!("`trio.sleep()` with >24 hour interval should usually be `trio.sleep_forever()`.")
     }
 }
 
