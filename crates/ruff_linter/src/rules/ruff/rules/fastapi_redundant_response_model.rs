a62b0bf4cd7561dc453592ab1167c661de5470d3
Tomer Bin <tomerbin98@gmail.com>
2024-05-28 10:10:40 +0300

    refactor

--- crates/ruff_linter/src/rules/ruff/rules/fastapi_redundant_response_model.rs
+++ crates/ruff_linter/src/rules/ruff/rules/fastapi_redundant_response_model.rs
@@ -1,6 +1,7 @@
 use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Fix, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast as ast;
+use ruff_python_ast::{Decorator, ExprCall, Keyword, StmtFunctionDef};
 use ruff_python_semantic::analyze::typing::resolve_assignment;
 use ruff_text_size::Ranged;
 
@@ -66,46 +67,10 @@ pub(crate) fn fastapi_redundant_response_model(
 ) {
     // Check if the function has a fast api app.post decorator
     for decorator in &function_def.decorator_list {
-        let Some(call) = decorator.expression.as_call_expr() else {
-            continue;
+        let (call, response_model_arg) = match check_decorator(checker, &function_def, decorator) {
+            Some(value) => value,
+            None => continue,
         };
-        let Some(decorator_method) = call.func.as_attribute_expr() else {
-            continue;
-        };
-        let method_name = &decorator_method.attr;
-
-        let route_methods = [
-            "get", "post", "put", "delete", "patch", "options", "head", "trace",
-        ];
-        if !route_methods.contains(&method_name.as_str()) {
-            continue;
-        }
-        let ra = resolve_assignment(&*decorator_method.value, checker.semantic());
-        if !ra.is_some_and(|qualified_name| {
-            matches!(
-                qualified_name.segments(),
-                ["fastapi", "FastAPI"] | ["fastapi", "APIRouter"]
-            )
-        }) {
-            continue;
-        }
-        let Some(response_model_arg) = call.arguments.find_keyword("response_model") else {
-            continue;
-        };
-        let Some(return_value) = &(function_def.returns) else {
-            continue;
-        };
-        let Some(response_mode_name_expr) = response_model_arg.value.as_name_expr() else {
-            continue;
-        };
-        let Some(return_value_name_expr) = return_value.as_name_expr() else {
-            continue;
-        };
-        let is_response_model_redundant = checker.semantic().resolve_name(response_mode_name_expr)
-            == checker.semantic().resolve_name(return_value_name_expr);
-        if !is_response_model_redundant {
-            continue;
-        }
         let mut diagnostic =
             Diagnostic::new(FastApiRedundantResponseModel, response_model_arg.range());
         diagnostic.try_set_fix(|| {
@@ -119,4 +84,42 @@ pub(crate) fn fastapi_redundant_response_model(
         });
         checker.diagnostics.push(diagnostic)
     }
+}
+
+fn check_decorator<'a>(
+    checker: &'a Checker,
+    function_def: &&StmtFunctionDef,
+    decorator: &'a Decorator,
+) -> Option<(&'a ExprCall, &'a Keyword)> {
+    let call = decorator.expression.as_call_expr()?;
+    let decorator_method = call.func.as_attribute_expr()?;
+    let method_name = &decorator_method.attr;
+
+    let route_methods = [
+        "get", "post", "put", "delete", "patch", "options", "head", "trace",
+    ];
+    if !route_methods.contains(&method_name.as_str()) {
+        return None;
+    }
+    let ra = resolve_assignment(&*decorator_method.value, checker.semantic());
+    if !ra.is_some_and(|qualified_name| {
+        matches!(
+            qualified_name.segments(),
+            ["fastapi", "FastAPI"] | ["fastapi", "APIRouter"]
+        )
+    }) {
+        return None;
+    }
+    let response_model_arg = call.arguments.find_keyword("response_model")?;
+    let Some(return_value) = &(function_def.returns) else {
+        return None;
+    };
+    let response_mode_name_expr = response_model_arg.value.as_name_expr()?;
+    let return_value_name_expr = return_value.as_name_expr()?;
+    let is_response_model_redundant = checker.semantic().resolve_name(response_mode_name_expr)
+        == checker.semantic().resolve_name(return_value_name_expr);
+    if !is_response_model_redundant {
+        return None;
+    }
+    Some((call, response_model_arg))
 }
