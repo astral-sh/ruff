5f0e87543a7865824426c55ffd17367c380d37c0
Tomer Bin <tomerbin98@gmail.com>
2024-05-28 09:02:56 +0300

    Add tests

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF102.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF102.py
@@ -1,45 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 
 app = FastAPI()
+router = APIRouter()
 class Item(BaseModel):
     name: str
 
-
+# Errors.
 @app.post("/items/", response_model=Item)
-async def create_item0(item: Item) -> Item:
+async def create_item(item: Item) -> Item:
+    return item
+
+@app.post("/items/", response_model=str)
+async def create_item(item: Item) -> str:
+    return item
+
+@app.get("/items/", response_model=Item)
+async def create_item(item: Item) -> Item:
     return item
 
 
-async def create_item1(item: Item) -> Item:
+@app.get("/items/", response_model=Item)
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Item:
+    return item
+
+@router.get("/items/", response_model=Item)
+async def create_item(item: Item) -> Item:
+    return item
+
+# Ok.
+async def create_item(item: Item) -> Item:
     return item
 
 
 @app("/items/", response_model=Item)
-async def create_item2(item: Item) -> Item:
+async def create_item(item: Item) -> Item:
     return item
 
 @cache
-async def create_item3(item: Item) -> Item:
-    return item
-
-
-@app.get("/items/", response_model=Item)
-async def create_item4(item: Item) -> Item:
-    return item
-
-
-@app.get("/items/", response_model=Item)
-@app.post("/items/", response_model=Item)
-async def create_item5(item: Item) -> Item:
+async def create_item(item: Item) -> Item:
     return item
 
 
 @app.post("/items/", response_model=str)
-async def create_item6(item: Item) -> Item:
+async def create_item(item: Item) -> Item:
     return item
 
 
 @app.post("/items/")
-async def create_item7(item: Item) -> Item:
+async def create_item(item: Item) -> Item:
+    return item
+
+
+@app.post("/items/", response_model=str)
+async def create_item(item: Item):
+    return item
+
+app = None
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Item:
     return item
