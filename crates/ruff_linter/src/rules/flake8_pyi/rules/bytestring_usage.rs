2f01144f6439f9fee5bd59690e90f6b49f66a2c6
Tomas R <tomas.roun8@gmail.com>
2024-05-28 16:05:24 +0200

    Improve documentation
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- crates/ruff_linter/src/rules/flake8_pyi/rules/bytestring_usage.rs
+++ crates/ruff_linter/src/rules/flake8_pyi/rules/bytestring_usage.rs
@@ -6,13 +6,13 @@ use ruff_text_size::Ranged;
 use crate::checkers::ast::Checker;
 
 /// ## What it does
-/// Checks for the uses of `typing.ByteString` and `collections.abc.ByteString`.
+/// Checks for uses of `typing.ByteString` or `collections.abc.ByteString`.
 ///
 /// ## Why is this bad?
 /// `ByteString` has been deprecated since Python 3.9 and will be removed in
 /// Python 3.14. The Python documentation recommends using either
-/// `collections.abc.Buffer` (for Python >=3.12) or a union like
-/// `bytes | bytearray | memoryview` instead.
+/// `collections.abc.Buffer` (or the `typing_extensions` backport
+/// on Python <3.12) or a union like `bytes | bytearray | memoryview` instead.
 ///
 /// ## Example
 /// ```python
