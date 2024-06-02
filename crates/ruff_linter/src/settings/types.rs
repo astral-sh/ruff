9f3e609278f1a2b59e565c2af814bbec5eec8229
Alex Waygood <Alex.Waygood@Gmail.com>
2024-06-02 14:06:04 +0100

    Make tests aware that py313 is the latest supported Python version (#11690)

--- crates/ruff_linter/src/settings/types.rs
+++ crates/ruff_linter/src/settings/types.rs
@@ -50,6 +50,8 @@ pub enum PythonVersion {
     Py311,
     Py312,
     Py313,
+    // Remember to update the `latest()` function
+    // when adding new versions here!
 }
 
 impl From<PythonVersion> for Pep440Version {
@@ -62,7 +64,7 @@ impl From<PythonVersion> for Pep440Version {
 impl PythonVersion {
     /// Return the latest supported Python version.
     pub const fn latest() -> Self {
-        Self::Py312
+        Self::Py313
     }
 
     pub const fn as_tuple(&self) -> (u8, u8) {
