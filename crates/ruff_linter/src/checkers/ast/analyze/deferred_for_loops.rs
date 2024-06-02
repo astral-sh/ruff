b13f16d0e553e9e4b916fccf71adbfdf5b765641
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-06-02 00:20:19 -0500

    Fix test

--- crates/ruff_linter/src/checkers/ast/analyze/deferred_for_loops.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/deferred_for_loops.rs
@@ -2,7 +2,7 @@ use ruff_python_ast::Stmt;
 
 use crate::checkers::ast::Checker;
 use crate::codes::Rule;
-use crate::rules::{flake8_bugbear, flake8_simplify, perflint, pyupgrade, refurb};
+use crate::rules::{flake8_bugbear, flake8_simplify, perflint, pylint, pyupgrade, refurb};
 
 /// Run lint rules over all deferred for-loops in the [`SemanticModel`].
 pub(crate) fn deferred_for_loops(checker: &mut Checker) {
@@ -32,6 +32,9 @@ pub(crate) fn deferred_for_loops(checker: &mut Checker) {
             }
             if checker.enabled(Rule::LoopIteratorMutation) {
                 flake8_bugbear::rules::loop_iterator_mutation(checker, stmt_for);
+            }
+            if checker.enabled(Rule::ConsiderDictItems) {
+                pylint::rules::consider_dict_items(checker, stmt_for);
             }
         }
     }
