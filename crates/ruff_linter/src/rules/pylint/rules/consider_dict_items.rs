f41494d6353f4cc022e45ea818d83ee0343600c3
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-06-01 23:53:17 -0500

    implement-consider-dict-items

---
+++ crates/ruff_linter/src/rules/pylint/rules/consider_dict_items.rs
@@ -0,0 +1,108 @@
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast::{self as ast};
+
+use crate::checkers::ast::Checker;
+
+/// ## What it does
+/// Checks for usages where
+/// a value is extracted through the key of dictionary in an iteration, when it simply be extracted using `.items()`
+///
+/// ## Why is this bad?
+/// Instead of unnecsarily indexing the the dictionary, it's more semantically clear to extract the value
+/// one-per-one with the key, increasing readability.
+///
+///
+/// ## Example
+/// ```python
+/// ORCHESTRA = {
+///     "violin": "strings",
+///     "oboe": "woodwind",
+///     "tuba": "brass",
+///     "gong": "percussion",
+/// }
+///
+/// for instrument in ORCHESTRA:
+///     print(f"{instrument}: {ORCHESTRA[instrument]}")
+/// ```
+///
+/// Use instead:
+/// ```python
+/// ORCHESTRA = {
+///     "violin": "strings",
+///     "oboe": "woodwind",
+///     "tuba": "brass",
+///     "gong": "percussion",
+/// }
+///
+/// for instrument, section in ORCHESTRA.items():
+///     print(f"{instrument}: {section}")
+/// ```
+
+#[violation]
+pub struct ConsiderDictItems;
+
+impl Violation for ConsiderDictItems {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        format!("Extracting value from dictionary in iteration without calling `.items()`")
+    }
+}
+
+/// PLC206
+pub(crate) fn consider_dict_items(checker: &mut Checker, stmt_for: &ast::StmtFor) {
+    let ast::StmtFor {
+        target,
+        iter,
+        body,
+        orelse,
+        is_async: _,
+        range: _,
+    } = stmt_for;
+
+    let Some(iter_obj_name) = iter.as_name_expr() else {
+        return;
+    };
+
+    for child in body {
+        if let ast::Expr::Subscript(ast::ExprSubscript {
+            value,
+            slice,
+            ctx: _,
+            range: _,
+        }) = child
+        {
+            if !value.is_name_expr() && !value.is_attribute_expr() {
+                return;
+            }
+
+            // Check that the sliced value is the same as the target of the for loop.
+            let slice_name = slice.as_name_expr();
+            let target_name = target.as_name_expr();
+
+            if slice_name.is_none() || target_name.is_none() {
+                return;
+            }
+
+            let slice_name = slice_name.unwrap();
+            let target_name = target_name.unwrap();
+
+            if slice_name.id != target_name.id {
+                return;
+            }
+
+            // Check that the sliced dict name is the same as the iterated object name.
+            if !(slice
+                .as_name_expr()
+                .is_some_and(|name| name.id == iter_obj_name.id))
+            {
+                return;
+            }
+
+            let mut diagnostic = Diagnostic::new(ConsiderDictItems, stmt_for.range);
+            checker.diagnostics.push(diagnostic);
+        } else {
+            return;
+        }
+    }
+}
