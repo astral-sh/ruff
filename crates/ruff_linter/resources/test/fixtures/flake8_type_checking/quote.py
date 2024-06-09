65a38e9c1c2df7ab26b045a0665a3cb7240a2fd7
Glyphack <sh.hooshyari@gmail.com>
2024-06-09 19:25:17 +0200

    test: add test case for auto quoting

--- crates/ruff_linter/resources/test/fixtures/flake8_type_checking/quote.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_type_checking/quote.py
@@ -97,3 +97,10 @@ def f():
 
     def func(self) -> DataFrame | list[Series]:
         pass
+
+def f():
+    from django.contrib.auth.models import AbstractBaseUser
+
+    def foo(self, user: AbstractBaseUser["int"], view: "type[CondorBaseViewSet]"):
+        pass
+
