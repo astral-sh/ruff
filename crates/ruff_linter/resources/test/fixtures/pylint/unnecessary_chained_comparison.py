fbf605e6ee4c4c1bc175e7003a3951debe6ea68f
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-06-08 23:01:55 -0500

    Snapshot

--- crates/ruff_linter/resources/test/fixtures/pylint/unnecessary_chained_comparison.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/unnecessary_chained_comparison.py
@@ -5,17 +5,25 @@ c = int(input())
 if a < b and b < c:
     pass
 
+while a < b and b < c:
+    pass
+
 x = int(input())
 y = int(input())
 z = int(input())
 if x <= y and y < z:
     print("In mixed order")
 
+while x <= y and y < z:
+    print("In mixed order")
 
 i = int(input())
 j = int(input())
 k = int(input())
 if i > j and j > k:
+    print("Descending order")
+
+while i > j and j > k:
     print("Descending order")
 
 # OK
@@ -25,14 +33,23 @@ c = int(input())
 if a < b < c:
     pass
 
+while a < b < c:
+    pass
+
 x = int(input())
 y = int(input())
 z = int(input())
 if x <= y < z:
     print("In mixed order")
 
+while x <= y < z:
+    print("In mixed order")
+
 i = int(input())
 j = int(input())
 k = int(input())
 if i > j > k:
+    print("Descending order")
+
+while i > j > k:
     print("Descending order")
