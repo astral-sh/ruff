b00696c4038510876b187b1e17bbc33565f80597
adrinjalali <adrin.jalali@gmail.com>
2024-06-18 16:45:47 +0200

    add note to docs

--- crates/ruff_linter/src/rules/flake8_copyright/rules/missing_copyright_notice.rs
+++ crates/ruff_linter/src/rules/flake8_copyright/rules/missing_copyright_notice.rs
@@ -8,6 +8,8 @@ use crate::settings::LinterSettings;
 /// ## What it does
 /// Checks for the absence of copyright notices within Python files.
 ///
+/// Note that this check only searches within the first 4096 bytes of the file.
+///
 /// ## Why is this bad?
 /// In some codebases, it's common to have a license header at the top of every
 /// file. This rule ensures that the license header is present.
