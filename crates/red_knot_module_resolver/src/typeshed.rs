1a06d42a7107da1af4e1a86e6c038c93ce9b0015
Alex Waygood <alex.waygood@gmail.com>
2024-06-21 17:28:32 +0100

    Add better error messages so they will be less terrifying if they start failing in CI

--- crates/red_knot_module_resolver/src/typeshed.rs
+++ crates/red_knot_module_resolver/src/typeshed.rs
@@ -3,27 +3,15 @@ pub(crate) mod versions;
 #[cfg(test)]
 mod tests {
     use std::io::{self, Read};
-    use std::path::{Path, PathBuf};
-    use std::str::FromStr;
-
-    use once_cell::sync::Lazy;
+    use std::path::Path;
 
     use ruff_db::vendored::VendoredFileSystem;
     use ruff_db::vfs::VendoredPath;
-
-    use crate::module::ModuleName;
-
-    use super::versions::*;
-
-    const TYPESHED_STDLIB_DIR: &str = "stdlib";
 
     // The file path here is hardcoded in this crate's `build.rs` script.
     // Luckily this crate will fail to build if this file isn't available at build time.
     const TYPESHED_ZIP_BYTES: &[u8] =
         include_bytes!(concat!(env!("OUT_DIR"), "/zipped_typeshed.zip"));
-
-    static VENDORED_TYPESHED_DIR: Lazy<PathBuf> =
-        Lazy::new(|| Path::new("vendor/typeshed").canonicalize().unwrap());
 
     #[test]
     fn typeshed_zip_created_at_build_time() {
@@ -46,62 +34,49 @@ mod tests {
 
     #[test]
     fn typeshed_vfs_consistent_with_vendored_stubs() {
+        let vendored_typeshed_dir = Path::new("vendor/typeshed").canonicalize().unwrap();
         let vendored_typeshed_stubs = VendoredFileSystem::new(TYPESHED_ZIP_BYTES).unwrap();
 
         let mut empty_iterator = true;
-        for entry in walkdir::WalkDir::new(&*VENDORED_TYPESHED_DIR).min_depth(1) {
+        for entry in walkdir::WalkDir::new(&vendored_typeshed_dir).min_depth(1) {
             empty_iterator = false;
             let entry = entry.unwrap();
             let absolute_path = entry.path();
             let file_type = entry.file_type();
 
-            let relative_path = absolute_path.strip_prefix(&*VENDORED_TYPESHED_DIR).unwrap();
-            let vendored_path = VendoredPath::new(relative_path.to_str().unwrap());
+            let relative_path = absolute_path
+                .strip_prefix(&vendored_typeshed_dir)
+                .unwrap_or_else(|_| {
+                    panic!("Expected {absolute_path:?} to be a child of {vendored_typeshed_dir:?}")
+                });
 
-            assert!(vendored_typeshed_stubs.exists(vendored_path));
+            let vendored_path =
+                VendoredPath::new(relative_path.to_str().unwrap_or_else(|| {
+                    panic!("Expected {relative_path:?} to be a valid UTF-8 path")
+                }));
+
+            assert!(
+                vendored_typeshed_stubs.exists(vendored_path),
+                "Expected {vendored_path:?} to exist in the `VendoredFileSystem`!"
+            );
 
             let vendored_path_kind = vendored_typeshed_stubs
                 .metadata(vendored_path)
-                .unwrap()
+                .unwrap_or_else(|| {
+                    panic!("Expected metadata for {vendored_path:?} to be retrievable from the `VendoredFileSystem!")
+                })
                 .kind();
-            assert_eq!(vendored_path_kind.is_directory(), file_type.is_dir());
-        }
-        assert!(!empty_iterator);
-    }
 
-    #[test]
-    fn typeshed_versions_consistent_with_vendored_stubs() {
-        const VERSIONS_DATA: &str = include_str!("../vendor/typeshed/stdlib/VERSIONS");
-        let vendored_typeshed_versions = TypeshedVersions::from_str(VERSIONS_DATA).unwrap();
-
-        let mut empty_iterator = true;
-        for entry in std::fs::read_dir(VENDORED_TYPESHED_DIR.join(TYPESHED_STDLIB_DIR)).unwrap() {
-            empty_iterator = false;
-            let entry = entry.unwrap();
-            let absolute_path = entry.path();
-            let relative_path = absolute_path
-                .strip_prefix(&*VENDORED_TYPESHED_DIR)
-                .and_then(|path| path.strip_prefix(TYPESHED_STDLIB_DIR))
-                .unwrap();
-            if relative_path == Path::new("VERSIONS") {
-                continue;
-            }
-            let mut top_level_module = relative_path
-                .components()
-                .next()
-                .unwrap()
-                .as_os_str()
-                .to_str()
-                .unwrap();
-            if let Some(extension) = absolute_path.extension() {
-                top_level_module = top_level_module
-                    .strip_suffix(extension.to_str().unwrap())
-                    .and_then(|string| string.strip_suffix('.'))
-                    .unwrap();
-            }
-            let top_level_module = ModuleName::new(top_level_module).unwrap();
-            assert!(vendored_typeshed_versions.contains_module(&top_level_module));
+            assert_eq!(
+                vendored_path_kind.is_directory(),
+                file_type.is_dir(),
+                "{vendored_path:?} had type {vendored_path_kind:?}, inconsistent with fs path {relative_path:?}: {file_type:?}"
+            );
         }
-        assert!(!empty_iterator);
+
+        assert!(
+            !empty_iterator,
+            "Expected there to be at least one file or directory in the vendored typeshed stubs!"
+        );
     }
 }
