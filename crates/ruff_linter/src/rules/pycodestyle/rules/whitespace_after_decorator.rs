17e902d3078a1f2d6319b6ab9a0fd7dfebe4ff01
Javier Kauer <kauer.javier01@gmail.com>
2024-06-23 18:20:17 +0200

    Feat: implementaiton of the function whitespace_after_decorator

--- crates/ruff_linter/src/rules/pycodestyle/rules/whitespace_after_decorator.rs
+++ crates/ruff_linter/src/rules/pycodestyle/rules/whitespace_after_decorator.rs
@@ -1,2 +1,15 @@
+use ruff_diagnostic::AlwaysFixableViolation;
+
 #[violation]
 pub struct WhitespaceAfterDecorator;
+
+impl AlwaysFixableViolation for WhitespaceAfterDecorator {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        format!("Whitespace after decorator")
+    }
+
+    fn fix_title(&self) -> String {
+        "Remove whitespace after decorator".to_string()
+    }
+}
