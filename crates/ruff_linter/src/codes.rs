e9b7d7507a393e7cf47fac22466db635fa22f55d
Alex Waygood <Alex.Waygood@Gmail.com>
2024-06-25 20:46:02 +0100

    Stabilise `django-extra` (`S610`) for release 0.5 (#12029)
    The motivation for this rule is solid; it's been in preview for a long
    time; the implementation and tests seem sound; there are no open issues
    regarding it, and as far as I can tell there never have been any.
    
    The only issue I see is that the docs don't really describe the rule
    accurately right now; I fix that in this PR.

--- crates/ruff_linter/src/codes.rs
+++ crates/ruff_linter/src/codes.rs
@@ -660,7 +660,7 @@ pub fn code_to_rule(linter: Linter, code: &str) -> Option<(RuleGroup, Rule)> {
         (Flake8Bandit, "607") => (RuleGroup::Stable, rules::flake8_bandit::rules::StartProcessWithPartialPath),
         (Flake8Bandit, "608") => (RuleGroup::Stable, rules::flake8_bandit::rules::HardcodedSQLExpression),
         (Flake8Bandit, "609") => (RuleGroup::Stable, rules::flake8_bandit::rules::UnixCommandWildcardInjection),
-        (Flake8Bandit, "610") => (RuleGroup::Preview, rules::flake8_bandit::rules::DjangoExtra),
+        (Flake8Bandit, "610") => (RuleGroup::Stable, rules::flake8_bandit::rules::DjangoExtra),
         (Flake8Bandit, "611") => (RuleGroup::Stable, rules::flake8_bandit::rules::DjangoRawSql),
         (Flake8Bandit, "612") => (RuleGroup::Stable, rules::flake8_bandit::rules::LoggingConfigInsecureListen),
         (Flake8Bandit, "701") => (RuleGroup::Stable, rules::flake8_bandit::rules::Jinja2AutoescapeFalse),
