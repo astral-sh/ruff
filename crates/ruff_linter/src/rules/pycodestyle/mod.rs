4e41f5103a2bccdd173c623fe25a6e6dffb46dbe
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-06-26 11:24:01 -0400

    Stabilize allowance of os.environ modifications between imports (#12047)
    ## Summary
    
    See: https://github.com/astral-sh/ruff/pull/10066.

--- crates/ruff_linter/src/rules/pycodestyle/mod.rs
+++ crates/ruff_linter/src/rules/pycodestyle/mod.rs
@@ -69,7 +69,6 @@ mod tests {
     }
 
     #[test_case(Rule::IsLiteral, Path::new("constant_literals.py"))]
-    #[test_case(Rule::ModuleImportNotAtTopOfFile, Path::new("E402_2.py"))]
     #[test_case(Rule::RedundantBackslash, Path::new("E502.py"))]
     #[test_case(Rule::TooManyNewlinesAtEndOfFile, Path::new("W391_0.py"))]
     #[test_case(Rule::TooManyNewlinesAtEndOfFile, Path::new("W391_1.py"))]
