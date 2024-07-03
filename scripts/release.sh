8d6f1f5060e18f757a0160c6a7678ddb9782a956
Zanie Blue <contact@zanie.dev>
2024-07-02 23:41:29 -0500

    Update to latest changes

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,12 +4,15 @@
 # All additional options are passed to `rooster release`
 set -eu
 
+export UV_PREVIEW=1
+
 script_root="$(realpath "$(dirname "$0")")"
 project_root="$(dirname "$script_root")"
 
 echo "Updating metadata with rooster..."
 cd "$project_root"
-uv tool run --from rooster-blue --isolated -- rooster release "$@"
+uv tool run --from 'rooster-blue>=0.0.7' --python 3.12 --isolated -- \
+    rooster release "$@"
 
 echo "Updating lockfile..."
 cargo update -p ruff
@@ -17,4 +20,5 @@ cargo update -p ruff
 echo "Generating contributors list..."
 echo ""
 echo ""
-uv tool run --from rooster-blue --isolated -- rooster contributors --quiet
+uv tool run --from 'rooster-blue>=0.0.7' --python 3.12 --isolated -- \
+    rooster contributors --quiet
