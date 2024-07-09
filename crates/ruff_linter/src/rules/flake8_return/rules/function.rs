9e0853851c053bce2d3ac0ec50348a98ef7f23c0
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-07-08 19:23:56 -0700

    Move gate

--- crates/ruff_linter/src/rules/flake8_return/rules/function.rs
+++ crates/ruff_linter/src/rules/flake8_return/rules/function.rs
@@ -365,15 +365,6 @@ impl Violation for SuperfluousElseBreak {
 
 /// RET501
 fn unnecessary_return_none(checker: &mut Checker, decorator_list: &[Decorator], stack: &Stack) {
-    // Skip properties.
-    let semantic = checker.semantic();
-    if decorator_list
-        .iter()
-        .any(|decorator| semantic.match_builtin_expr(&decorator.expression, "property"))
-    {
-        return;
-    }
-
     for stmt in &stack.returns {
         let Some(expr) = stmt.value.as_deref() else {
             continue;
@@ -381,7 +372,17 @@ fn unnecessary_return_none(checker: &mut Checker, decorator_list: &[Decorator], 
         if !expr.is_none_literal_expr() {
             continue;
         }
-        let mut diagnostic = Diagnostic::new(UnnecessaryReturnNone, stmt.range);
+
+        // Skip properties.
+        if decorator_list.iter().any(|decorator| {
+            checker
+                .semantic()
+                .match_builtin_expr(&decorator.expression, "property")
+        }) {
+            return;
+        }
+
+        let mut diagnostic = Diagnostic::new(UnnecessaryReturnNone, stmt.range());
         diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
             "return".to_string(),
             stmt.range(),
@@ -396,10 +397,10 @@ fn implicit_return_value(checker: &mut Checker, stack: &Stack) {
         if stmt.value.is_some() {
             continue;
         }
-        let mut diagnostic = Diagnostic::new(ImplicitReturnValue, stmt.range);
+        let mut diagnostic = Diagnostic::new(ImplicitReturnValue, stmt.range());
         diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
             "return None".to_string(),
-            stmt.range,
+            stmt.range(),
         )));
         checker.diagnostics.push(diagnostic);
     }
