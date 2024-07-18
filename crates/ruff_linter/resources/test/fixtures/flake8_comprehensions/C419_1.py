9b9d70150001da91d6eec55590c117c38ad679c9
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-07-18 08:37:28 -0400

    Allow additional arguments for sum and max comprehensions (#12364)
    ## Summary
    
    These can have other arguments, so it seems wrong to gate on single
    argument here.
    
    Closes https://github.com/astral-sh/ruff/issues/12358.

--- crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C419_1.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C419_1.py
@@ -1,8 +1,10 @@
 sum([x.val for x in bar])
 min([x.val for x in bar])
 max([x.val for x in bar])
+sum([x.val for x in bar], 0)
 
 # Ok
 sum(x.val for x in bar)
 min(x.val for x in bar)
 max(x.val for x in bar)
+sum(x.val for x in bar, 0)
