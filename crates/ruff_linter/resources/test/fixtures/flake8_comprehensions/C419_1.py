3e4e3ee11773c7352c3b914816f0dc2e283998d7
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-07-20 12:37:48 -0400

    Insert parentheses for multi-argument generators

--- crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C419_1.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C419_1.py
@@ -3,8 +3,18 @@ min([x.val for x in bar])
 max([x.val for x in bar])
 sum([x.val for x in bar], 0)
 
-# Ok
+# OK
 sum(x.val for x in bar)
 min(x.val for x in bar)
 max(x.val for x in bar)
 sum(x.val for x in bar, 0)
+
+# Multi-line
+sum(
+    [
+        delta
+        for delta in timedelta_list
+        if delta
+    ],
+    dt.timedelta(),
+)
