8cd1d0ab7bec7d0915b0b57b6925758fbf8fb735
augustelalande <auguste.lalande@gmail.com>
2024-07-27 00:52:43 -0400

    add tests

---
+++ crates/ruff_linter/resources/test/fixtures/pydoclint/DOC403_google.py
@@ -0,0 +1,50 @@
+# OK
+def foo(num: int) -> str:
+    """
+    Do something
+
+    Args:
+        num (int): A number
+    """
+    print('test')
+
+
+# DOC403
+def foo(num: int) -> str:
+    """
+    Do something
+
+    Args:
+        num (int): A number
+
+    Yields:
+        str: A string
+    """
+    print('test')
+
+
+class Bar:
+
+    # DOC403
+    def foo(self) -> str:
+        """
+        Do something
+
+        Args:
+            num (int): A number
+
+        Yields:
+            str: A string
+        """
+        print('test')
+
+
+    # OK
+    def bar(self) -> str:
+        """
+        Do something
+
+        Args:
+            num (int): A number
+        """
+        print('test')
