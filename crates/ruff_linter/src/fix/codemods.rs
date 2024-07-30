aaa56eb0bd7f2740c83587d03198bc12c8e69648
Alex Waygood <Alex.Waygood@Gmail.com>
2024-07-30 10:54:35 +0100

    Fix NFKC normalization bug when removing unused imports (#12571)

--- crates/ruff_linter/src/fix/codemods.rs
+++ crates/ruff_linter/src/fix/codemods.rs
@@ -1,13 +1,16 @@
 //! Interface for editing code snippets. These functions take statements or expressions as input,
 //! and return the modified code snippet as output.
+use std::borrow::Cow;
+
 use anyhow::{bail, Result};
 use libcst_native::{
     Codegen, CodegenState, Expression, ImportNames, NameOrAttribute, ParenthesizableWhitespace,
     SmallStatement, Statement,
 };
-use ruff_python_ast::name::UnqualifiedName;
 use smallvec::{smallvec, SmallVec};
+use unicode_normalization::UnicodeNormalization;
 
+use ruff_python_ast::name::UnqualifiedName;
 use ruff_python_ast::Stmt;
 use ruff_python_codegen::Stylist;
 use ruff_source_file::Locator;
@@ -167,39 +170,55 @@ pub(crate) fn retain_imports(
     Ok(tree.codegen_stylist(stylist))
 }
 
-fn collect_segments<'a>(expr: &'a Expression, parts: &mut SmallVec<[&'a str; 8]>) {
-    match expr {
-        Expression::Call(expr) => {
-            collect_segments(&expr.func, parts);
-        }
-        Expression::Attribute(expr) => {
-            collect_segments(&expr.value, parts);
-            parts.push(expr.attr.value);
-        }
-        Expression::Name(expr) => {
-            parts.push(expr.value);
-        }
-        _ => {}
-    }
-}
-
-fn unqualified_name_from_expression<'a>(expr: &'a Expression<'a>) -> Option<UnqualifiedName<'a>> {
-    let mut segments = smallvec![];
-    collect_segments(expr, &mut segments);
-    if segments.is_empty() {
-        None
-    } else {
-        Some(segments.into_iter().collect())
-    }
-}
-
+/// Create an NFKC-normalized qualified name from a libCST node.
 fn qualified_name_from_name_or_attribute(module: &NameOrAttribute) -> String {
-    match module {
-        NameOrAttribute::N(name) => name.value.to_string(),
+    fn collect_segments<'a>(expr: &'a Expression, parts: &mut SmallVec<[&'a str; 8]>) {
+        match expr {
+            Expression::Call(expr) => {
+                collect_segments(&expr.func, parts);
+            }
+            Expression::Attribute(expr) => {
+                collect_segments(&expr.value, parts);
+                parts.push(expr.attr.value);
+            }
+            Expression::Name(expr) => {
+                parts.push(expr.value);
+            }
+            _ => {}
+        }
+    }
+
+    /// Attempt to create an [`UnqualifiedName`] from a libCST expression.
+    ///
+    /// Strictly speaking, the `UnqualifiedName` returned by this function may be invalid,
+    /// since it hasn't been NFKC-normalized. In order for an `UnqualifiedName` to be
+    /// comparable to one constructed from a `ruff_python_ast` node, it has to undergo
+    /// NFKC normalization. As a local function, however, this is fine;
+    /// the outer function always performs NFKC normalization before returning the
+    /// qualified name to the caller.
+    fn unqualified_name_from_expression<'a>(
+        expr: &'a Expression<'a>,
+    ) -> Option<UnqualifiedName<'a>> {
+        let mut segments = smallvec![];
+        collect_segments(expr, &mut segments);
+        if segments.is_empty() {
+            None
+        } else {
+            Some(segments.into_iter().collect())
+        }
+    }
+
+    let unnormalized = match module {
+        NameOrAttribute::N(name) => Cow::Borrowed(name.value),
         NameOrAttribute::A(attr) => {
             let name = attr.attr.value;
             let prefix = unqualified_name_from_expression(&attr.value);
-            prefix.map_or_else(|| name.to_string(), |prefix| format!("{prefix}.{name}"))
+            prefix.map_or_else(
+                || Cow::Borrowed(name),
+                |prefix| Cow::Owned(format!("{prefix}.{name}")),
+            )
         }
-    }
+    };
+
+    unnormalized.nfkc().collect()
 }
