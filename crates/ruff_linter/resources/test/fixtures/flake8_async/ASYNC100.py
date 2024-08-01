d774a3bd48bbf8b697190b7d68251195cdc6c64b
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-07-31 22:12:43 -0400

    Avoid unused async when context manager includes `TaskGroup` (#12605)
    ## Summary
    
    Closes https://github.com/astral-sh/ruff/issues/12354.

--- crates/ruff_linter/resources/test/fixtures/flake8_async/ASYNC100.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_async/ASYNC100.py
@@ -4,56 +4,61 @@ import trio
 
 
 async def func():
-    with trio.fail_after():
+    async with trio.fail_after():
         ...
 
 
 async def func():
-    with trio.fail_at():
+    async with trio.fail_at():
         await ...
 
 
 async def func():
-    with trio.move_on_after():
+    async with trio.move_on_after():
         ...
 
 
 async def func():
-    with trio.move_at():
+    async with trio.move_at():
         await ...
 
 
 async def func():
-    with trio.move_at():
-        async with trio.open_nursery() as nursery:
+    async with trio.move_at():
+        async with trio.open_nursery():
             ...
 
 
 async def func():
-    with anyio.move_on_after():
+    async with anyio.move_on_after(delay=0.2):
         ...
 
 
 async def func():
-    with anyio.fail_after():
+    async with anyio.fail_after():
         ...
 
 
 async def func():
-    with anyio.CancelScope():
+    async with anyio.CancelScope():
         ...
 
 
 async def func():
-    with anyio.CancelScope():
+    async with anyio.CancelScope():
         ...
 
 
 async def func():
-    with asyncio.timeout():
+    async with asyncio.timeout(delay=0.2):
         ...
 
 
 async def func():
-    with asyncio.timeout_at():
+    async with asyncio.timeout_at(when=0.2):
+        ...
+
+
+async def func():
+    async with asyncio.timeout(delay=0.2), asyncio.TaskGroup():
         ...
