e34be273158e7d755e6192749c76f36d47b6c41a
Alex Waygood <alex.waygood@gmail.com>
2024-08-02 18:47:17 +0100

    [`pydoclint`] Teach rules to understand reraised exceptions as being explicitly raised

--- crates/ruff_linter/resources/test/fixtures/pydoclint/DOC501_google.py
+++ crates/ruff_linter/resources/test/fixtures/pydoclint/DOC501_google.py
@@ -190,3 +190,45 @@ def foo(bar: int):
         something.SomeError: Wow.
     """
     raise something.SomeError
+
+
+# DOC501
+def calculate_speed(distance: float, time: float) -> float:
+    """Calculate speed as distance divided by time.
+
+    Args:
+        distance: Distance traveled.
+        time: Time spent traveling.
+
+    Returns:
+        Speed as distance divided by time.
+
+    Raises:
+        TypeError: if you didn't pass a number for both parameters
+    """
+    try:
+        return distance / time
+    except ZeroDivisionError:
+        print("Oh no, why would you divide something by zero?")
+        raise
+    except TypeError:
+        print("Not a number? Shame on you!")
+        raise
+
+
+# This is fine
+def calculate_speed(distance: float, time: float) -> float:
+    """Calculate speed as distance divided by time.
+
+    Args:
+        distance: Distance traveled.
+        time: Time spent traveling.
+
+    Returns:
+        Speed as distance divided by time.
+    """
+    try:
+        return distance / time
+    except Exception as e:
+        print(f"Oh no, we encountered {e}")
+        raise
--- crates/ruff_linter/resources/test/fixtures/pydoclint/DOC502_google.py
+++ crates/ruff_linter/resources/test/fixtures/pydoclint/DOC502_google.py
@@ -56,3 +56,28 @@ def calculate_speed(distance: float, time: float) -> float:
         return distance / time
     except ZeroDivisionError as exc:
         raise FasterThanLightError from exc
+
+
+# This is fine
+def calculate_speed(distance: float, time: float) -> float:
+    """Calculate speed as distance divided by time.
+
+    Args:
+        distance: Distance traveled.
+        time: Time spent traveling.
+
+    Returns:
+        Speed as distance divided by time.
+
+    Raises:
+        ZeroDivisionError: If you pass `0` for the time
+        TypeError: if you didn't pass a number for both parameters
+    """
+    try:
+        return distance / time
+    except ZeroDivisionError:
+        print("Oh no, why would you divide something by zero?")
+        raise
+    except TypeError:
+        print("Not a number? Shame on you!")
+        raise
