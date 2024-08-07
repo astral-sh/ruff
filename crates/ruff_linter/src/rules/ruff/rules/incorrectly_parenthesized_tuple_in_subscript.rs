4ba055dd5596b2ef06e0b862cb845febb1ed13d2
Alex Waygood <alex.waygood@gmail.com>
2024-08-07 14:01:05 +0100

    final nits

--- crates/ruff_linter/src/rules/ruff/rules/incorrectly_parenthesized_tuple_in_subscript.rs
+++ crates/ruff_linter/src/rules/ruff/rules/incorrectly_parenthesized_tuple_in_subscript.rs
@@ -58,24 +58,25 @@ impl AlwaysFixableViolation for IncorrectlyParenthesizedTupleInSubscript {
 /// RUF031
 pub(crate) fn subscript_with_parenthesized_tuple(checker: &mut Checker, subscript: &ExprSubscript) {
     let prefer_parentheses = checker.settings.ruff.parenthesize_tuple_in_subscript;
-    let Some(tuple_index) = subscript.slice.as_tuple_expr() else {
+    let Some(tuple_subscript) = subscript.slice.as_tuple_expr() else {
         return;
     };
-    if tuple_index.parenthesized != prefer_parentheses {
-        let locator = checker.locator();
-        let source_range = subscript.slice.range();
-        let new_source = if prefer_parentheses {
-            format!("({})", locator.slice(source_range))
-        } else {
-            locator.slice(source_range)[1..source_range.len().to_usize() - 1].to_string()
-        };
-        let edit = Edit::range_replacement(new_source, source_range);
-        checker.diagnostics.push(
-            Diagnostic::new(
-                IncorrectlyParenthesizedTupleInSubscript { prefer_parentheses },
-                source_range,
-            )
-            .with_fix(Fix::safe_edit(edit)),
-        );
+    if tuple_subscript.parenthesized == prefer_parentheses {
+        return;
     }
+    let locator = checker.locator();
+    let source_range = subscript.slice.range();
+    let new_source = if prefer_parentheses {
+        format!("({})", locator.slice(source_range))
+    } else {
+        locator.slice(source_range)[1..source_range.len().to_usize() - 1].to_string()
+    };
+    let edit = Edit::range_replacement(new_source, source_range);
+    checker.diagnostics.push(
+        Diagnostic::new(
+            IncorrectlyParenthesizedTupleInSubscript { prefer_parentheses },
+            source_range,
+        )
+        .with_fix(Fix::safe_edit(edit)),
+    );
 }
