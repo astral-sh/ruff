7fc39ad624c0df95d62066d758fdb6fdcc6de2e3
Jonathan Plasse <jonathan.plasse@live.fr>
2024-08-14 09:47:45 +0200

    [flake8-return] Only add return None at end of function (RET503) (#11074)
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_linter/src/checkers/ast/analyze/statement.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/statement.rs
@@ -229,12 +229,7 @@ pub(crate) fn statement(stmt: &Stmt, checker: &mut Checker) {
                 Rule::SuperfluousElseContinue,
                 Rule::SuperfluousElseBreak,
             ]) {
-                flake8_return::rules::function(
-                    checker,
-                    body,
-                    decorator_list,
-                    returns.as_ref().map(AsRef::as_ref),
-                );
+                flake8_return::rules::function(checker, function_def);
             }
             if checker.enabled(Rule::UselessReturn) {
                 pylint::rules::useless_return(
