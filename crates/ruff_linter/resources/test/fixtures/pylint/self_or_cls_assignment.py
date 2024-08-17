77f170b97c220a0babe8c83489f7c47215bb10c7
Steve C <diceroll123@gmail.com>
2024-08-17 16:06:39 -0400

    [`pylint`] - remove AugAssign errors from `self-cls-assignment` (`W0642`)

--- crates/ruff_linter/resources/test/fixtures/pylint/self_or_cls_assignment.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/self_or_cls_assignment.py
@@ -3,7 +3,6 @@ class Fruit:
     def list_fruits(cls) -> None:
         cls = "apple"  # PLW0642
         cls: Fruit = "apple"  # PLW0642
-        cls += "orange"  # PLW0642
         *cls = "banana"  # PLW0642
         cls, blah = "apple", "orange"  # PLW0642
         blah, (cls, blah2) = "apple", ("orange", "banana")  # PLW0642
@@ -16,7 +15,6 @@ class Fruit:
     def print_color(self) -> None:
         self = "red"  # PLW0642
         self: Self = "red"  # PLW0642
-        self += "blue"  # PLW0642
         *self = "blue"  # PLW0642
         self, blah = "red", "blue"  # PLW0642
         blah, (self, blah2) = "apple", ("orange", "banana")  # PLW0642
