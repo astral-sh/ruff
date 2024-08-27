81cd438d883dafb7c51cf2ec9b8e8dcec5cf1f6b
Chris Krycho <hello@chriskrycho.com>
2024-08-27 13:52:53 -0600

    red-knot: infer and display ellipsis type (#13124)
    ## Summary
    
    Just what it says on the tin: adds basic `EllipsisType` inference for
    any time `...` appears in the AST.
    
    ## Test Plan
    
    Test that `x = ...` produces exactly what we would expect.
    
    ---------
    
    Co-authored-by: Carl Meyer <carl@oddbird.net>

--- crates/red_knot_python_semantic/src/types/infer.rs
+++ crates/red_knot_python_semantic/src/types/infer.rs
@@ -1337,8 +1337,7 @@ impl<'db> TypeInferenceBuilder<'db> {
         &mut self,
         _literal: &ast::ExprEllipsisLiteral,
     ) -> Type<'db> {
-        // TODO Ellipsis
-        Type::Unknown
+        builtins_symbol_ty_by_name(self.db, "Ellipsis")
     }
 
     fn infer_tuple_expression(&mut self, tuple: &ast::ExprTuple) -> Type<'db> {
@@ -2466,6 +2465,24 @@ mod tests {
         assert_public_ty(&db, "src/a.py", "x", "Literal[b\"hello\"]");
         assert_public_ty(&db, "src/a.py", "y", "Literal[b\"world!\"]");
         assert_public_ty(&db, "src/a.py", "z", "Literal[b\"\\xff\\x00\"]");
+
+        Ok(())
+    }
+
+    #[test]
+    fn ellipsis_type() -> anyhow::Result<()> {
+        let mut db = setup_db();
+
+        db.write_dedented(
+            "src/a.py",
+            "
+            x = ...
+            ",
+        )?;
+
+        // TODO: update this once `infer_ellipsis_literal_expression` correctly
+        // infers `types.EllipsisType`.
+        assert_public_ty(&db, "src/a.py", "x", "Unknown | Literal[EllipsisType]");
 
         Ok(())
     }
