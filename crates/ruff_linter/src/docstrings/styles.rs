281e6d9791576aaf5c6eae0f13a06272fb8adc9c
Alex Waygood <Alex.Waygood@Gmail.com>
2024-08-29 16:33:18 +0100

    [`pydocstyle`] Improve heuristics for detecting Google-style docstrings (#13142)

--- crates/ruff_linter/src/docstrings/styles.rs
+++ crates/ruff_linter/src/docstrings/styles.rs
@@ -2,7 +2,7 @@ use crate::docstrings::google::GOOGLE_SECTIONS;
 use crate::docstrings::numpy::NUMPY_SECTIONS;
 use crate::docstrings::sections::SectionKind;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, is_macro::Is)]
 pub(crate) enum SectionStyle {
     Numpy,
     Google,
