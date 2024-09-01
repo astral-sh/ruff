46fafea11941c03946a1d7102999a5405128d16c
Tom Kuson <mail@tjkuson.me>
2024-09-01 19:06:11 +0100

    Add a fix

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF033.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF033.py
@@ -38,3 +38,30 @@ def __post_init__(foo: bool = True) -> None: ...
 # OK
 class Foo:
     def __post_init__(self, x="hmm") -> None: ...
+
+
+# RUF033
+@dataclass
+class Foo:
+    def __post_init__(self, bar: int = 11, baz: Something[Whatever | None] = 11) -> None: ...
+
+
+# RUF033
+@dataclass
+class Foo:
+    """A very helpful docstring.
+
+    Docstrings are very important and totally not a waste of time.
+    """
+
+    ping = "pong"
+
+    def __post_init__(self, bar: int = 11, baz: int = 12) -> None: ...
+
+
+# RUF033
+@dataclass
+class Foo:
+    bar = "should've used attrs"
+
+    def __post_init__(self, bar: str = "ahhh", baz: str = "hmm") -> None: ...
--- crates/ruff_linter/src/rules/ruff/rules/post_init_default.rs
+++ crates/ruff_linter/src/rules/ruff/rules/post_init_default.rs
@@ -1,9 +1,12 @@
-use ruff_diagnostics::{Diagnostic, FixAvailability, Violation};
+use ruff_diagnostics::{Diagnostic, Edit, Fix, FixAvailability, Violation};
 use ruff_macros::{derive_message_formats, violation};
-use ruff_python_ast::{self as ast, ParameterWithDefault, Stmt};
-use ruff_text_size::Ranged;
+use ruff_python_ast::{
+    self as ast, helpers::is_docstring_stmt, Expr, Parameter, ParameterWithDefault, Stmt,
+};
+use ruff_python_trivia::{indentation_at_offset, textwrap};
+use ruff_text_size::{Ranged, TextRange};
 
-use crate::checkers::ast::Checker;
+use crate::{checkers::ast::Checker, importer::ImportRequest};
 
 use super::helpers::is_dataclass;
 
@@ -88,19 +91,142 @@ pub(crate) fn post_init_default(checker: &mut Checker, class_def: &ast::StmtClas
                 continue;
             }
 
+            let mut stopped_fixes = false;
+
             for ParameterWithDefault {
-                parameter: _,
+                parameter,
                 default,
-                range: _,
+                range,
             } in function_def.parameters.iter_non_variadic_params()
             {
                 let Some(default) = default else {
                     continue;
                 };
-                checker
-                    .diagnostics
-                    .push(Diagnostic::new(PostInitDefault, default.range()));
+                let mut diagnostic = Diagnostic::new(PostInitDefault, default.range());
+
+                // If the name is already bound on the class, no fix is available.
+                if !already_bound(parameter, class_def) && !stopped_fixes {
+                    if let Some(fix) = use_initvar(class_def, parameter, default, *range, checker) {
+                        diagnostic.set_fix(fix);
+                    }
+                } else {
+                    // Need to stop fixes as soon as there is a parameter we
+                    // cannot fix. Otherwise, we risk a syntax error (a
+                    // parameter without a default following parameter with a
+                    // default).
+                    stopped_fixes = true;
+                }
+
+                checker.diagnostics.push(diagnostic);
             }
         }
     }
+}
+
+/// Generate a [`Fix`] to transform a `__post_init__` default argument into a
+/// `dataclasses.InitVar` pseudo-field.
+fn use_initvar(
+    class_def: &ast::StmtClassDef,
+    parameter: &Parameter,
+    default: &Expr,
+    range: TextRange,
+    checker: &Checker,
+) -> Option<Fix> {
+    // Ensure that `dataclasses.InitVar` is accessible. For example,
+    // + `from dataclasses import InitVar`
+    let (import_edit, binding) = checker
+        .importer()
+        .get_or_import_symbol(
+            &ImportRequest::import("dataclasses", "InitVar"),
+            default.start(),
+            checker.semantic(),
+        )
+        .ok()?;
+
+    // Delete the default value. For example,
+    // - def __post_init__(self, foo: int = 0) -> None: ...
+    // + def __post_init__(self, foo: int) -> None: ...
+    let (default_start, default_end) = match &parameter.annotation {
+        Some(annotation) => (annotation.range().end(), range.end()),
+        None => (parameter.range().end(), range.end()),
+    };
+    let default_edit = Edit::deletion(default_start, default_end);
+
+    // Add `dataclasses.InitVar` field to class body.
+    let mut body = class_def.body.iter().peekable();
+    let statement = body.peek()?;
+    let mut content = String::new();
+    match &parameter.annotation {
+        Some(annotation) => {
+            content.push_str(&format!(
+                "{}: {}[{}] = {}",
+                checker.locator().slice(&parameter.name),
+                binding,
+                checker.locator().slice(annotation.range()),
+                checker.locator().slice(default)
+            ));
+        }
+        None => {
+            content.push_str(&format!(
+                "{}: {} = {}",
+                checker.locator().slice(&parameter.name),
+                binding,
+                checker.locator().slice(default)
+            ));
+        }
+    }
+    content.push_str(checker.stylist().line_ending().as_str());
+    let indentation = indentation_at_offset(statement.start(), checker.locator())?;
+    let content = textwrap::indent(&content, indentation).to_string();
+
+    // Find the position after any docstring to insert the field.
+    let mut pos = checker.locator().line_start(statement.start());
+    while let Some(statement) = body.next() {
+        if is_docstring_stmt(statement) {
+            if let Some(next) = body.peek() {
+                if checker
+                    .indexer()
+                    .in_multi_statement_line(statement, checker.locator())
+                {
+                    continue;
+                }
+                pos = checker.locator().line_start(next.start());
+            } else {
+                pos = checker.locator().full_line_end(statement.end());
+                break;
+            }
+        } else {
+            break;
+        };
+    }
+    let initvar_edit = Edit::insertion(content, pos);
+
+    Some(Fix::unsafe_edits(import_edit, [default_edit, initvar_edit]))
+}
+
+/// Check if a name is already bound as a class variable.
+fn already_bound(parameter: &Parameter, class_def: &ast::StmtClassDef) -> bool {
+    for statement in &class_def.body {
+        let target = match statement {
+            Stmt::Assign(ast::StmtAssign { targets, .. }) => {
+                if let [Expr::Name(id)] = targets.as_slice() {
+                    id
+                } else {
+                    continue;
+                }
+            }
+            Stmt::AnnAssign(ast::StmtAnnAssign { target, .. }) => {
+                if let Expr::Name(id) = target.as_ref() {
+                    id
+                } else {
+                    continue;
+                }
+            }
+            _ => continue,
+        };
+        if *target.id == *parameter.name {
+            return true;
+        }
+    }
+    false
 }
