6b340dc4d916e82a4e78ce9a817348d4462a47db
brucearctor <5032356+brucearctor@users.noreply.github.com>
2024-09-03 18:51:24 +0000

    git and precommit

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,8 +7,8 @@ cargo fetch
 
 pip install maturin pre-commit
 
-# ensure pre-commit is configured to run
-pre-commit install
-
 # configure git to be able to run from the devcontainer
 git config --global --add safe.directory /workspaces/ruff
+
+# ensure pre-commit is configured to run
+pre-commit install
