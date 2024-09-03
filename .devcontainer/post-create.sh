22fd748613fb857d634ff8d5631760a4eed28a77
brucearctor <5032356+brucearctor@users.noreply.github.com>
2024-09-03 18:29:09 +0000

    autoprecommit-in-devcontainer-workspace

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,3 +6,9 @@ cargo install cargo-insta
 cargo fetch
 
 pip install maturin pre-commit
+
+# ensure pre-commit is configured to run
+pre-commit install
+
+# configure git to be able to run from the devcontainer
+git config --global --add safe.directory /workspaces/ruff
