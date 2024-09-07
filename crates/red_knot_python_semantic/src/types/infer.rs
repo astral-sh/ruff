db3f2aae12e8e20b99bc7f922e9545e27ff1efbc
dylwil3 <dylwil3@gmail.com>
2024-09-07 11:06:57 -0500

    test multiply literal string

--- crates/red_knot_python_semantic/src/types/infer.rs
+++ crates/red_knot_python_semantic/src/types/infer.rs
@@ -2913,6 +2913,26 @@ mod tests {
     }
 
     #[test]
+    fn multiplied_literal_string() -> anyhow::Result<()> {
+        let mut db = setup_db();
+        let content = format!(
+            r#"
+        v = "{y}"
+        w = 10*"{y}"
+        x = "{y}"*10
+        "#,
+            y = "a".repeat(TypeInferenceBuilder::MAX_STRING_LITERAL_SIZE + 1),
+        );
+        db.write_dedented("src/a.py", &content)?;
+
+        assert_public_ty(&db, "src/a.py", "v", "LiteralString");
+        assert_public_ty(&db, "src/a.py", "w", "LiteralString");
+        assert_public_ty(&db, "src/a.py", "x", "LiteralString");
+
+        Ok(())
+    }
+
+    #[test]
     fn truncated_string_literals_become_literal_string() -> anyhow::Result<()> {
         let mut db = setup_db();
         let content = format!(
