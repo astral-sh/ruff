d6b7e0c114ad202de87a3800f28ad7ac473f1a9c
dylwil3 <dylwil3@gmail.com>
2024-09-09 13:33:50 -0500

    clippy

--- crates/ruff_python_ast/src/nodes.rs
+++ crates/ruff_python_ast/src/nodes.rs
@@ -2196,7 +2196,7 @@ impl<'a> From<&'a BytesLiteralValue> for Cow<'a, [u8]> {
             BytesLiteralValueInner::Concatenated(bytes_literal_vec) => Cow::Owned(
                 bytes_literal_vec
                     .iter()
-                    .flat_map(|bytes_literal| bytes_literal.value.to_owned())
+                    .flat_map(|bytes_literal| bytes_literal.value.clone())
                     .collect::<Vec<u8>>(),
             ),
         }
