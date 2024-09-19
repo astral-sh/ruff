a8c1523ece33db8d6d458bfd46444848bec62e53
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2024-09-19 21:47:47 +0100

    fix

--- crates/ruff_linter/src/rules/pydocstyle/rules/ends_with_period.rs
+++ crates/ruff_linter/src/rules/pydocstyle/rules/ends_with_period.rs
@@ -104,7 +104,7 @@ pub(crate) fn ends_with_period(checker: &mut Checker, docstring: &Docstring) {
         if !trimmed.ends_with('.') {
             let mut diagnostic = Diagnostic::new(EndsInPeriod, docstring.range());
             // Best-effort fix: avoid adding a period after other punctuation marks.
-            if !trimmed.ends_with([':', ';']) {
+            if !trimmed.ends_with([':', ';', '?', '!']) {
                 diagnostic.set_fix(Fix::unsafe_edit(Edit::insertion(
                     ".".to_string(),
                     line.start() + trimmed.text_len(),
