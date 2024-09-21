3582a5e1b01a723266722078e1439e971e91cbc2
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-09-21 09:27:32 -0400

    Use file

--- crates/ruff_graph/src/resolver.rs
+++ crates/ruff_graph/src/resolver.rs
@@ -1,36 +1,37 @@
-use red_knot_python_semantic::resolve_module;
+use red_knot_python_semantic::SemanticModel;
 use ruff_db::files::FilePath;
 
 use crate::collector::CollectedImport;
-use crate::ModuleDb;
 
 /// Collect all imports for a given Python file.
 pub(crate) struct Resolver<'a> {
-    db: &'a ModuleDb,
+    semantic: &'a SemanticModel<'a>,
 }
 
 impl<'a> Resolver<'a> {
-    /// Initialize a [`Resolver`] with a given [`ModuleDb`].
-    pub(crate) fn new(db: &'a ModuleDb) -> Self {
-        Self { db }
+    /// Initialize a [`Resolver`] with a given [`SemanticModel`].
+    pub(crate) fn new(semantic: &'a SemanticModel<'a>) -> Self {
+        Self { semantic }
     }
 
     /// Resolve the [`CollectedImport`] into a [`FilePath`].
     pub(crate) fn resolve(&self, import: CollectedImport) -> Option<&'a FilePath> {
         match import {
-            CollectedImport::Import(import) => {
-                resolve_module(self.db, import).map(|module| module.file().path(self.db))
-            }
+            CollectedImport::Import(import) => self
+                .semantic
+                .resolve_module(import)
+                .map(|module| module.file().path(self.semantic.db())),
             CollectedImport::ImportFrom(import) => {
                 // Attempt to resolve the member (e.g., given `from foo import bar`, look for `foo.bar`).
                 let parent = import.parent();
-
-                resolve_module(self.db, import)
-                    .map(|module| module.file().path(self.db))
+                self.semantic
+                    .resolve_module(import)
+                    .map(|module| module.file().path(self.semantic.db()))
                     .or_else(|| {
                         // Attempt to resolve the module (e.g., given `from foo import bar`, look for `foo`).
-
-                        resolve_module(self.db, parent?).map(|module| module.file().path(self.db))
+                        self.semantic
+                            .resolve_module(parent?)
+                            .map(|module| module.file().path(self.semantic.db()))
                     })
             }
         }
