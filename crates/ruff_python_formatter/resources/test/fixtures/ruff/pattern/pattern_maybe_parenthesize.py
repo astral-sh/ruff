2584f8e3b64e5dd0e719bdd7a89309e3b3718eeb
Micha Reiser <micha@reiser.io>
2024-09-25 17:14:59 +0200

    Implement `can_omit_parentheses` for patterns

--- crates/ruff_python_formatter/resources/test/fixtures/ruff/pattern/pattern_maybe_parenthesize.py
+++ crates/ruff_python_formatter/resources/test/fixtures/ruff/pattern/pattern_maybe_parenthesize.py
@@ -99,3 +99,194 @@ match x:
         pass
 
 
+## Patterns ending with a sequence, mapping, class, or parenthesized pattern should break the parenthesized-like pattern first
+match x:
+    case A | [
+        aaaaaa,
+        bbbbbbbbbbbbbbbb,
+        cccccccccccccccccc,
+        ddddddddddddddddddddddddddd,
+    ]:
+        pass
+
+match x:
+    case A | (
+        aaaaaa,
+        bbbbbbbbbbbbbbbb,
+        cccccccccccccccccc,
+        ddddddddddddddddddddddddddd,
+    ):
+        pass
+
+
+match x:
+    case A | {
+        "a": aaaaaa,
+        "b": bbbbbbbbbbbbbbbb,
+        "c": cccccccccccccccccc,
+        "d": ddddddddddddddddddddddddddd,
+    }:
+        pass
+
+
+match x:
+    case A | Class(
+        aaaaaa,
+        bbbbbbbbbbbbbbbb,
+        cccccccccccccccccc,
+        ddddddddddddddddddddddddddd,
+    ):
+        pass
+
+
+
+match x:
+    case A | (
+        aaaaaaaaaaaaaaaaaaa.bbbbbbbbbbbbbbbbbbbbbbb.cccccccccccccccccccccccccccc.ddddddddddddddddddddddd
+    ):
+        pass
+
+
+## Patterns starting with a sequence, mapping, class, or parenthesized pattern should break the parenthesized-like pattern first
+match x:
+    case [
+         aaaaaa,
+         bbbbbbbbbbbbbbbb,
+         cccccccccccccccccc,
+         ddddddddddddddddddddddddddd,
+     ] | A:
+        pass
+
+match x:
+    case (
+         aaaaaa,
+         bbbbbbbbbbbbbbbb,
+         cccccccccccccccccc,
+         ddddddddddddddddddddddddddd,
+     ) | A:
+        pass
+
+
+match x:
+    case {
+         "a": aaaaaa,
+         "b": bbbbbbbbbbbbbbbb,
+         "c": cccccccccccccccccc,
+         "d": ddddddddddddddddddddddddddd,
+         } | A:
+        pass
+
+
+match x:
+    case Class(
+        aaaaaa,
+        bbbbbbbbbbbbbbbb,
+        cccccccccccccccccc,
+        ddddddddddddddddddddddddddd,
+    ):
+        pass
+
+
+## Not for non-parenthesized sequence patterns
+match x:
+    case (
+        (1) | aaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+        ccccccccccccccccccccccccccccccccc,
+    ):
+        pass
+
+## Parenthesize patterns that start with a token
+match x:
+    case (
+    A(
+        aaaaaaaaaaaaaaaaaaa.bbbbbbbbbbbbbbbbbbbbbbb.cccccccccccccccccccccccccccc.ddddddddddddddddddddddd
+    )
+    | B
+    ):
+        pass
+
+
+## Always use parentheses for implicitly concatenated strings
+match x:
+    case (
+        "implicit"
+        "concatenated"
+        "string"
+        | [aaaaaa, bbbbbbbbbbbbbbbb, cccccccccccccccccc, ddddddddddddddddddddddddddd]
+    ):
+        pass
+
+
+match x:
+    case (
+        b"implicit"
+        b"concatenated"
+        b"string"
+        | [aaaaaa, bbbbbbbbbbbbbbbb, cccccccccccccccccc, ddddddddddddddddddddddddddd]
+    ):
+        pass
+
+
+match x:
+    case (
+         f"implicit"
+         "concatenated"
+         "string"
+         | [aaaaaa, bbbbbbbbbbbbbbbb, cccccccccccccccccc, ddddddddddddddddddddddddddd]
+    ):
+        pass
+
+
+## Complex number expressions and unary expressions
+
+match x:
+    case 4 - 3j | [
+        aaaaaaaaaaaaaaaaaaaaaaaa,
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+        cccccccccccccccccccccccccccccccccccccccc,
+    ]:
+        pass
+
+
+match x:
+    case 4 + 3j | [
+        aaaaaaaaaaaaaaaaaaaaaaaa,
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+        cccccccccccccccccccccccccccccccccccccccc,
+    ]:
+        pass
+
+
+match x:
+    case -1 | [
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+        ccccccccccccccccccccccccccccccccc,
+    ]:
+        pass
+
+
+
+### Parenthesized patterns
+match x:
+    case (1) | [
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+        ccccccccccccccccccccccccccccccccc,
+    ]:
+        pass
+
+
+match x:
+    case ( # comment
+         1
+     ) | [
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+        ccccccccccccccccccccccccccccccccc,
+    ]:
+        pass
+
+
+
