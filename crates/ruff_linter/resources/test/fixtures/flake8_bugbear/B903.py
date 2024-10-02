ae5a829d9153ad605d3b084e5cf140e5141ad8e7
Ayush Gupta <ayush.gpt8@gmail.com>
2024-10-02 13:04:44 +0000

    add B907.py and B903.py

---
+++ crates/ruff_linter/resources/test/fixtures/flake8_bugbear/B903.py
@@ -0,0 +1,26 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+
+class Square:
+    def __init__(self, side):
+        self.side = side
---
+++ crates/ruff_linter/resources/test/fixtures/flake8_bugbear/B907.py
@@ -0,0 +1,8 @@
+foo = "bar"
+f"'{foo}'"
+
+baz = "qux"
+f"'{baz}'"
+
+quux = "corge"
+f"'{quux}'"
