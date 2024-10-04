fc653d53e624819c35e2752c48aafd3e09705fff
Alex Waygood <alex.waygood@gmail.com>
2024-10-04 17:25:04 +0100

    Add passing test for `finally` branch with no `except`s

--- crates/red_knot_python_semantic/src/types/infer.rs
+++ crates/red_knot_python_semantic/src/types/infer.rs
@@ -6592,6 +6592,49 @@ mod tests {
     }
 
     #[test]
+    fn exception_handler_control_flow_no_except_with_finally() -> anyhow::Result<()> {
+        let mut db = setup_db();
+
+        db.write_dedented(
+            "src/a.py",
+            "
+            from typing_extensions import reveal_type
+
+            def could_raise_returns_str() -> str: ...
+
+            x = 1
+
+            try:
+                reveal_type(x)
+                x = could_raise_returns_str()
+                reveal_type(x)
+            finally:
+                reveal_type(x)
+                x = 2
+                reveal_type(x)
+
+            # `finally` is *always* executed, so this should be the same
+            # as at the end of the `finally` block (`Literal[2]`)
+            reveal_type(x)
+            ",
+        )?;
+
+        assert_file_diagnostics(
+            &db,
+            "src/a.py",
+            &[
+                "Revealed type is `Literal[1]`",
+                "Revealed type is `str`",
+                "Revealed type is `Literal[1] | str`",
+                "Revealed type is `Literal[2]`",
+                "Revealed type is `Literal[2]`",
+            ],
+        );
+
+        Ok(())
+    }
+
+    #[test]
     fn basic_comprehension() -> anyhow::Result<()> {
         let mut db = setup_db();
 
