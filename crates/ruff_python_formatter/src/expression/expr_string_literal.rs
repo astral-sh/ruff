ae9c6a9255294cb5cc884726fcbfbcd441f2f76c
Micha Reiser <micha@reiser.io>
2024-10-07 16:47:04 +0200

    All tests are passing

--- crates/ruff_python_formatter/src/expression/expr_string_literal.rs
+++ crates/ruff_python_formatter/src/expression/expr_string_literal.rs
@@ -1,4 +1,4 @@
-use ruff_formatter::FormatRuleWithOptions;
+use ruff_formatter::{FormatRuleWithOptions, GroupId};
 use ruff_python_ast::{AnyNodeRef, ExprStringLiteral, StringLike};
 
 use crate::expression::parentheses::{
@@ -10,14 +10,29 @@ use crate::string::{FormatImplicitConcatenatedString, StringLikeExtensions};
 
 #[derive(Default)]
 pub struct FormatExprStringLiteral {
-    kind: StringLiteralKind,
+    layout: ExprStringLiteralLayout,
+}
+
+#[derive(Default)]
+pub struct ExprStringLiteralLayout {
+    pub kind: StringLiteralKind,
+    pub implicit_group_id: Option<GroupId>,
+}
+
+impl ExprStringLiteralLayout {
+    pub const fn docstring() -> Self {
+        Self {
+            kind: StringLiteralKind::Docstring,
+            implicit_group_id: None,
+        }
+    }
 }
 
 impl FormatRuleWithOptions<ExprStringLiteral, PyFormatContext<'_>> for FormatExprStringLiteral {
-    type Options = StringLiteralKind;
+    type Options = ExprStringLiteralLayout;
 
     fn with_options(mut self, options: Self::Options) -> Self {
-        self.kind = options;
+        self.layout = options;
         self
     }
 }
@@ -27,15 +42,23 @@ impl FormatNodeRule<ExprStringLiteral> for FormatExprStringLiteral {
         let ExprStringLiteral { value, .. } = item;
 
         match value.as_slice() {
-            [string_literal] => string_literal.format().with_options(self.kind).fmt(f),
+            [string_literal] => string_literal
+                .format()
+                .with_options(self.layout.kind)
+                .fmt(f),
             _ => {
                 // This is just a sanity check because [`DocstringStmt::try_from_statement`]
                 // ensures that the docstring is a *single* string literal.
-                assert!(!self.kind.is_docstring());
+                assert!(!self.layout.kind.is_docstring());
 
-                in_parentheses_only_group(&FormatImplicitConcatenatedString::new(item))
+                match self.layout.implicit_group_id {
+                    Some(group_id) => group(&FormatImplicitConcatenatedString::new(item))
+                        .with_group_id(Some(group_id))
+                        .fmt(f),
+                    None => in_parentheses_only_group(&FormatImplicitConcatenatedString::new(item))
+                        .fmt(f),
+                }
             }
-            .fmt(f),
         }
     }
 }
@@ -48,7 +71,7 @@ impl NeedsParentheses for ExprStringLiteral {
     ) -> OptionalParentheses {
         if self.value.is_implicit_concatenated() {
             OptionalParentheses::Multiline
-        } else if StringLike::String(self).is_multiline(context.source()) {
+        } else if StringLike::from(self).is_multiline(context.source()) {
             OptionalParentheses::Never
         } else {
             OptionalParentheses::BestFit
