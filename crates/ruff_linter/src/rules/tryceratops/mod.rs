70e5c4a8ba679e80554c02bce7435dde085eb5bf
Zanie Blue <contact@zanie.dev>
2024-10-08 11:41:27 -0500

    Recode `TRY302` to `TRY203` (#13502)
    Closes https://github.com/astral-sh/ruff/issues/13492

--- crates/ruff_linter/src/rules/tryceratops/mod.rs
+++ crates/ruff_linter/src/rules/tryceratops/mod.rs
@@ -19,9 +19,9 @@ mod tests {
     #[test_case(Rule::RaiseVanillaArgs, Path::new("TRY003.py"))]
     #[test_case(Rule::TypeCheckWithoutTypeError, Path::new("TRY004.py"))]
     #[test_case(Rule::VerboseRaise, Path::new("TRY201.py"))]
+    #[test_case(Rule::UselessTryExcept, Path::new("TRY203.py"))]
     #[test_case(Rule::TryConsiderElse, Path::new("TRY300.py"))]
     #[test_case(Rule::RaiseWithinTry, Path::new("TRY301.py"))]
-    #[test_case(Rule::UselessTryExcept, Path::new("TRY302.py"))]
     #[test_case(Rule::ErrorInsteadOfException, Path::new("TRY400.py"))]
     #[test_case(Rule::VerboseLogMessage, Path::new("TRY401.py"))]
     fn rules(rule_code: Rule, path: &Path) -> Result<()> {
