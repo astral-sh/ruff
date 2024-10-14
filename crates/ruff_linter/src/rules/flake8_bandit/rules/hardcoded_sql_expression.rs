91138d8ab48d7f4916b11f2a8706982db6f4b8dc
DataEnggNerd <solomon.santhosh@gmail.com>
2024-10-14 23:35:08 +0530

    reverting to original changes

--- crates/ruff_linter/src/rules/flake8_bandit/rules/hardcoded_sql_expression.rs
+++ crates/ruff_linter/src/rules/flake8_bandit/rules/hardcoded_sql_expression.rs
@@ -2,9 +2,7 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
-use ruff_python_ast::str::raw_contents;
 use ruff_python_ast::{self as ast, Expr, Operator};
-use ruff_source_file::Locator;
 use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
@@ -12,7 +10,7 @@ use crate::checkers::ast::Checker;
 // Implementation of regex from bandit
 static SQL_REGEX: Lazy<Regex> = Lazy::new(|| {
     Regex::new(
-        r"(?i)
+        r"(?ixs)
         (
             select\s+.*from\s |
             delete\s+from\s  |
@@ -96,7 +94,7 @@ pub(crate) fn hardcoded_sql_expression(checker: &mut Checker, expr: &Expr) {
             };
             string.value.to_str().escape_default().to_string()
         }
-        Expr::FString(f_string) => concatenated_f_string(f_string, checker.locator()),
+        // Expr::FString(f_string) => concatenated_f_string(f_string, checker.locator()),
         _ => return,
     };
 
@@ -117,12 +115,13 @@ pub(crate) fn hardcoded_sql_expression(checker: &mut Checker, expr: &Expr) {
 /// ```
 ///
 /// becomes `foobar {x}baz`.
-fn concatenated_f_string(expr: &ast::ExprFString, locator: &Locator) -> String {
-    expr.value
-        .iter()
-        .filter_map(|part| raw_contents(locator.slice(part)))
-        .collect()
-}
+// fn concatenated_f_string(expr: &ast::ExprFString, locator: &Locator) -> String {
+//     expr.value
+//         .iter()
+//         .filter_map(|part| raw_contents(locator.slice(part)))
+//         .map(|s| s.escape_default().to_string())
+//         .collect()
+// }
 
 /// Returns `Some(true)` if an expression appears to be an explicit string concatenation,
 /// `Some(false)` if it's _not_ an explicit concatenation, and `None` if it's ambiguous.
