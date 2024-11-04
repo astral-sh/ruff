14c4842823a3915f90ec8df9f19c40c86d435fda
dylwil3 <dylwil3@gmail.com>
2024-11-04 16:49:13 -0600

    update fixture

--- crates/ruff_linter/resources/test/fixtures/refurb/FURB157.py
+++ crates/ruff_linter/resources/test/fixtures/refurb/FURB157.py
@@ -15,3 +15,29 @@ decimal.Decimal("0")
 Decimal(0)
 Decimal("Infinity")
 decimal.Decimal(0)
+
+# Handle Python's Decimal parsing
+# See https://github.com/astral-sh/ruff/issues/13807
+
+# Errors
+Decimal("1_000")
+Decimal("__1____000") 
+Decimal("2e4")
+Decimal("2e+4")
+Decimal("2E4")
+
+# Ok
+Decimal("2e-4")
+Decimal("2E-4")
+Decimal("_1.234__")
+# Ok: even though this is equal to `Decimal(123)`,
+# we assume that a developer would
+# only write it this way if they meant to.
+Decimal("١٢٣") 
+# Ok: due to floating point errors
+# this is not equal to `Decimal(1.2)`.
+Decimal("1.2") 
+# Ok: This is an error of type `decimal.InvalidOperation`,
+# whereas `Decimal(2e4e4)` is a SyntaxError, so
+# we leave it as is.
+Decimal("2e4e4")
