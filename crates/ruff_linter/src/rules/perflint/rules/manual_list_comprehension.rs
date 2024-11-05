9aa5627019a53c3f1afbca60397cd7bc1c8297bd
Sebastian <storo3@outlook.com>
2024-11-05 14:38:20 -0500

    uncomment LineRanges import

--- crates/ruff_linter/src/rules/perflint/rules/manual_list_comprehension.rs
+++ crates/ruff_linter/src/rules/perflint/rules/manual_list_comprehension.rs
@@ -7,7 +7,7 @@ use ruff_python_ast::comparable::ComparableExpr;
 use ruff_python_ast::helpers::any_over_expr;
 use ruff_python_semantic::{analyze::typing::is_list, Binding};
 use ruff_python_trivia::PythonWhitespace;
-// use ruff_source_file::LineRanges;
+use ruff_source_file::LineRanges;
 use ruff_text_size::{Ranged, TextRange};
 
 use crate::checkers::ast::Checker;
