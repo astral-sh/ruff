10605d38876a0b1f22cddaa154199bb5c32c5dcc
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-11-07 10:58:18 +0000

    [`flake8-pyi`] Remove docstring fix (`PYI021`)

--- crates/ruff_linter/src/rules/flake8_pyi/rules/docstring_in_stubs.rs
+++ crates/ruff_linter/src/rules/flake8_pyi/rules/docstring_in_stubs.rs
@@ -1,8 +1,10 @@
 use ruff_python_ast::ExprStringLiteral;
 
-use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_text_size::Ranged;
+
+use ruff_python_semantic::Definition;
 
 use crate::checkers::ast::Checker;
 
@@ -29,18 +31,41 @@ use crate::checkers::ast::Checker;
 #[violation]
 pub struct DocstringInStub;
 
-impl Violation for DocstringInStub {
+impl AlwaysFixableViolation for DocstringInStub {
     #[derive_message_formats]
     fn message(&self) -> String {
         "Docstrings should not be included in stubs".to_string()
     }
+
+    fn fix_title(&self) -> String {
+        "Remove docstring".to_string()
+    }
 }
 
 /// PYI021
-pub(crate) fn docstring_in_stubs(checker: &mut Checker, docstring: Option<&ExprStringLiteral>) {
-    if let Some(docstr) = docstring {
-        checker
-            .diagnostics
-            .push(Diagnostic::new(DocstringInStub, docstr.range()));
-    }
+pub(crate) fn docstring_in_stubs(
+    checker: &mut Checker,
+    definition: &Definition,
+    maybe_docstring: Option<&ExprStringLiteral>,
+) {
+    let Some(docstring) = maybe_docstring else {
+        return;
+    };
+    let statements = match definition {
+        Definition::Module(module) => module.python_ast,
+        Definition::Member(member) => member.body(),
+    };
+
+    let range = docstring.range();
+
+    let edit = if statements.len() == 1 {
+        Edit::range_replacement("pass".to_string(), range)
+    } else {
+        Edit::deletion(range.start(), range.end())
+    };
+
+    let fix = Fix::unsafe_edit(edit);
+    let diagnostic = Diagnostic::new(DocstringInStub, range).with_fix(fix);
+
+    checker.diagnostics.push(diagnostic);
 }
