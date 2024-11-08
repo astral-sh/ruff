272d24bf3e4dabe59d2ec49f505e8fa4e00ba798
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-11-07 22:45:19 -0500

    [`flake8-pyi`] Add a fix for `duplicate-literal-member` (#14188)
    ## Summary
    
    Closes https://github.com/astral-sh/ruff/issues/14187.

--- crates/ruff_linter/src/rules/flake8_pyi/rules/duplicate_literal_member.rs
+++ crates/ruff_linter/src/rules/flake8_pyi/rules/duplicate_literal_member.rs
@@ -2,12 +2,12 @@ use std::collections::HashSet;
 
 use rustc_hash::FxHashSet;
 
-use ruff_diagnostics::{Diagnostic, FixAvailability, Violation};
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::comparable::ComparableExpr;
-use ruff_python_ast::Expr;
+use ruff_python_ast::{self as ast, Expr, ExprContext};
 use ruff_python_semantic::analyze::typing::traverse_literal;
-use ruff_text_size::Ranged;
+use ruff_text_size::{Ranged, TextRange};
 
 use crate::checkers::ast::Checker;
 
@@ -27,6 +27,10 @@ use crate::checkers::ast::Checker;
 /// foo: Literal["a", "b"]
 /// ```
 ///
+/// ## Fix safety
+/// This rule's fix is marked as safe; however, the fix will flatten nested
+/// literals into a single top-level literal.
+///
 /// ## References
 /// - [Python documentation: `typing.Literal`](https://docs.python.org/3/library/typing.html#typing.Literal)
 #[violation]
@@ -34,24 +38,29 @@ pub struct DuplicateLiteralMember {
     duplicate_name: String,
 }
 
-impl Violation for DuplicateLiteralMember {
-    const FIX_AVAILABILITY: FixAvailability = FixAvailability::Sometimes;
-
+impl AlwaysFixableViolation for DuplicateLiteralMember {
     #[derive_message_formats]
     fn message(&self) -> String {
         format!("Duplicate literal member `{}`", self.duplicate_name)
+    }
+
+    fn fix_title(&self) -> String {
+        "Remove duplicates".to_string()
     }
 }
 
 /// PYI062
 pub(crate) fn duplicate_literal_member<'a>(checker: &mut Checker, expr: &'a Expr) {
     let mut seen_nodes: HashSet<ComparableExpr<'_>, _> = FxHashSet::default();
+    let mut unique_nodes: Vec<&Expr> = Vec::new();
     let mut diagnostics: Vec<Diagnostic> = Vec::new();
 
     // Adds a member to `literal_exprs` if it is a `Literal` annotation
     let mut check_for_duplicate_members = |expr: &'a Expr, _: &'a Expr| {
         // If we've already seen this literal member, raise a violation.
-        if !seen_nodes.insert(expr.into()) {
+        if seen_nodes.insert(expr.into()) {
+            unique_nodes.push(expr);
+        } else {
             diagnostics.push(Diagnostic::new(
                 DuplicateLiteralMember {
                     duplicate_name: checker.generator().expr(expr),
@@ -61,7 +70,36 @@ pub(crate) fn duplicate_literal_member<'a>(checker: &mut Checker, expr: &'a Expr
         }
     };
 
-    // Traverse the literal, collect all diagnostic members
+    // Traverse the literal, collect all diagnostic members.
     traverse_literal(&mut check_for_duplicate_members, checker.semantic(), expr);
+
+    // If there's at least one diagnostic, create a fix to remove the duplicate members.
+    if !diagnostics.is_empty() {
+        if let Expr::Subscript(subscript) = expr {
+            let subscript = Expr::Subscript(ast::ExprSubscript {
+                slice: Box::new(if let [elt] = unique_nodes.as_slice() {
+                    (*elt).clone()
+                } else {
+                    Expr::Tuple(ast::ExprTuple {
+                        elts: unique_nodes.into_iter().cloned().collect(),
+                        range: TextRange::default(),
+                        ctx: ExprContext::Load,
+                        parenthesized: false,
+                    })
+                }),
+                value: subscript.value.clone(),
+                range: TextRange::default(),
+                ctx: ExprContext::Load,
+            });
+            let fix = Fix::safe_edit(Edit::range_replacement(
+                checker.generator().expr(&subscript),
+                expr.range(),
+            ));
+            for diagnostic in &mut diagnostics {
+                diagnostic.set_fix(fix.clone());
+            }
+        }
+    }
+
     checker.diagnostics.append(&mut diagnostics);
 }
