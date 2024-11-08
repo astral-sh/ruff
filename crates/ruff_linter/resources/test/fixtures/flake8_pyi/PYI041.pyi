727a95373bd1e3cf6770695754b06313c924536e
Simon Brugman <sfbbrugman@gmail.com>
2024-11-08 23:52:21 +0100

    [`flake8-pyi`] Implement autofix and fix false negatives for nested unions for `redundant-numeric-union` (`PYI041`)

--- crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI041.pyi
+++ crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI041.pyi
@@ -33,7 +33,21 @@ def f3(arg1: int, *args: Union[int | int | float]) -> None: ...  # PYI041
 async def f4(**kwargs: int | int | float) -> None: ...  # PYI041
 
 
+def f4(arg1: int, *args: Union[int, int, float]) -> None: ...  # PYI041
+
+
+def f5(arg1: int, *args: Union[Union[int, int, float]]) -> None: ...  # PYI041
+
+
 class Foo:
     def good(self, arg: int) -> None: ...
 
     def bad(self, arg: int | float | complex) -> None: ...  # PYI041
+
+    def bad2(self, arg: int | Union[float, complex]) -> None: ...  # PYI041
+
+    def bad3(self, arg: Union[Union[float, complex], int]) -> None: ...  # PYI041
+
+    def bad4(self, arg: Union[float | complex, int]) -> None: ...  # PYI041
+
+    def bad5(self, arg: int | (float | complex)) -> None: ...  # PYI041
