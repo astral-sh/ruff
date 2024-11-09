a659381c5cadbd7d4ce76a1d45a65679f61b557c
Simon Brugman <sfbbrugman@gmail.com>
2024-11-09 13:23:33 +0100

    Add autofix for `duplicate-union-member` (`PYI016`)

--- crates/ruff_linter/src/rules/flake8_pyi/rules/duplicate_union_member.rs
+++ crates/ruff_linter/src/rules/flake8_pyi/rules/duplicate_union_member.rs
@@ -2,12 +2,12 @@ use std::collections::HashSet;
 
 use rustc_hash::FxHashSet;
 
-use ruff_diagnostics::{Diagnostic, Edit, Fix, FixAvailability, Violation};
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::comparable::ComparableExpr;
-use ruff_python_ast::{self as ast, Expr};
+use ruff_python_ast::{self as ast, Expr, ExprContext};
 use ruff_python_semantic::analyze::typing::traverse_union;
-use ruff_text_size::Ranged;
+use ruff_text_size::{Ranged, TextRange};
 
 use crate::checkers::ast::Checker;
 
@@ -27,6 +27,11 @@ use crate::checkers::ast::Checker;
 /// foo: str
 /// ```
 ///
+/// ## Fix safety
+/// This rule's fix is marked as safe; however, for duplicate members
+/// in non-PEP604 unions (i.e. `typing.Union`), the fix will flatten
+/// nested unions type expressions into a single top-level union.
+///
 /// ## References
 /// - [Python documentation: `typing.Union`](https://docs.python.org/3/library/typing.html#typing.Union)
 #[violation]
@@ -34,31 +39,29 @@ pub struct DuplicateUnionMember {
     duplicate_name: String,
 }
 
-impl Violation for DuplicateUnionMember {
-    const FIX_AVAILABILITY: FixAvailability = FixAvailability::Sometimes;
-
+impl AlwaysFixableViolation for DuplicateUnionMember {
     #[derive_message_formats]
     fn message(&self) -> String {
         format!("Duplicate union member `{}`", self.duplicate_name)
     }
 
-    fn fix_title(&self) -> Option<String> {
-        Some(format!(
-            "Remove duplicate union member `{}`",
-            self.duplicate_name
-        ))
+    fn fix_title(&self) -> String {
+        format!("Remove duplicate union member `{}`", self.duplicate_name)
     }
 }
 
 /// PYI016
 pub(crate) fn duplicate_union_member<'a>(checker: &mut Checker, expr: &'a Expr) {
     let mut seen_nodes: HashSet<ComparableExpr<'_>, _> = FxHashSet::default();
+    let mut unique_nodes: Vec<&Expr> = Vec::new();
     let mut diagnostics: Vec<Diagnostic> = Vec::new();
 
     // Adds a member to `literal_exprs` if it is a `Literal` annotation
     let mut check_for_duplicate_members = |expr: &'a Expr, parent: &'a Expr| {
         // If we've already seen this union member, raise a violation.
-        if !seen_nodes.insert(expr.into()) {
+        if seen_nodes.insert(expr.into()) {
+            unique_nodes.push(expr);
+        } else {
             let mut diagnostic = Diagnostic::new(
                 DuplicateUnionMember {
                     duplicate_name: checker.generator().expr(expr),
@@ -68,7 +71,7 @@ pub(crate) fn duplicate_union_member<'a>(checker: &mut Checker, expr: &'a Expr) 
             // Delete the "|" character as well as the duplicate value by reconstructing the
             // parent without the duplicate.
 
-            // If the parent node is not a `BinOp` we will not perform a fix
+            // If the parent node is not a PEP604-style union (`a | b`) we will not perform a fix.
             if let Expr::BinOp(ast::ExprBinOp { left, right, .. }) = parent {
                 // Replace the parent with its non-duplicate child.
                 let child = if expr == left.as_ref() { right } else { left };
@@ -83,6 +86,37 @@ pub(crate) fn duplicate_union_member<'a>(checker: &mut Checker, expr: &'a Expr) 
 
     // Traverse the union, collect all diagnostic members
     traverse_union(&mut check_for_duplicate_members, checker.semantic(), expr);
+
+    // If any [`Violation`] has no [`Fix`].
+    if diagnostics.iter().any(|f| f.fix.is_none()) {
+        // Flatten the union with only unique elements.
+        if let Expr::Subscript(subscript) = expr {
+            let subscript = Expr::Subscript(ast::ExprSubscript {
+                slice: Box::new(if let [elt] = unique_nodes.as_slice() {
+                    (*elt).clone()
+                } else {
+                    Expr::Tuple(ast::ExprTuple {
+                        elts: unique_nodes.into_iter().cloned().collect(),
+                        range: TextRange::default(),
+                        ctx: ExprContext::Load,
+                        parenthesized: false,
+                    })
+                }),
+                value: subscript.value.clone(),
+                range: TextRange::default(),
+                ctx: ExprContext::Load,
+            });
+            let fix = Fix::safe_edit(Edit::range_replacement(
+                checker.generator().expr(&subscript),
+                expr.range(),
+            ));
+            for diagnostic in &mut diagnostics {
+                if diagnostic.fix.is_none() {
+                    diagnostic.set_fix(fix.clone());
+                }
+            }
+        }
+    }
 
     // Add all diagnostics to the checker
     checker.diagnostics.append(&mut diagnostics);
