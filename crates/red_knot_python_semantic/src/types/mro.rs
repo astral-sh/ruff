d3f1c8e536ccfa1948d442bc162e21b1c14ea93c
Alex Waygood <Alex.Waygood@Gmail.com>
2024-11-09 09:10:00 +0000

    [red-knot] Add `Type` constructors for `Instance`, `ClassLiteral` and `SubclassOf` variants (#14215)
    ## Summary
    
    Reduces some repetetiveness and verbosity at callsites. Addresses
    @carljm's review comments at
    https://github.com/astral-sh/ruff/pull/14155/files#r1833252458
    
    ## Test Plan
    
    `cargo test -p red_knot_python_semantic`

--- crates/red_knot_python_semantic/src/types/mro.rs
+++ crates/red_knot_python_semantic/src/types/mro.rs
@@ -406,7 +406,7 @@ impl<'db> From<ClassBase<'db>> for Type<'db> {
             ClassBase::Any => Type::Any,
             ClassBase::Todo => Type::Todo,
             ClassBase::Unknown => Type::Unknown,
-            ClassBase::Class(class) => Type::ClassLiteral(ClassLiteralType { class }),
+            ClassBase::Class(class) => Type::class_literal(class),
         }
     }
 }
