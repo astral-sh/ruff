a020eedc6aaeb2590ca8cfbcd3feb1e6f3197493
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-11-10 17:51:37 -0500

    Nits

--- crates/ruff_linter/src/rules/pylint/rules/shallow_copy_environ.rs
+++ crates/ruff_linter/src/rules/pylint/rules/shallow_copy_environ.rs
@@ -10,34 +10,38 @@ use crate::checkers::ast::Checker;
 /// Check for shallow `os.environ` copies.
 ///
 /// ## Why is this bad?
-/// `os.environ` is not a dict object but proxy object, so shallow copy has still
-/// effects on original object. See [Python issue 15373: `copy.copy()` does not properly copy `os.environment`](https://bugs.python.org/issue15373) for reference.
+/// `os.environ` is not a `dict` object, but rather, a proxy object. As such, mutating a shallow
+/// copy of `os.environ` will also mutate the original object.
+///
+/// See: [#15373] for more information.
 ///
 /// ## Example
 /// ```python
 /// import copy
 /// import os
 ///
-/// copied_env = copy.copy(os.environ)
+/// env = copy.copy(os.environ)
 /// ```
 ///
 /// Use instead:
 /// ```python
 /// import os
 ///
-/// copied_env = os.environ.copy()
+/// env = os.environ.copy()
 /// ```
 ///
 /// ## References
 /// - [Python documentation: `copy` — Shallow and deep copy operations](https://docs.python.org/3/library/copy.html)
 /// - [Python documentation: `os.environ`](https://docs.python.org/3/library/os.html#os.environ)
+///
+/// [#15373]: https://bugs.python.org/issue15373
 #[violation]
 pub struct ShallowCopyEnviron;
 
 impl AlwaysFixableViolation for ShallowCopyEnviron {
     #[derive_message_formats]
     fn message(&self) -> String {
-        "Using `copy.copy(os.environ)`. Use `os.environ.copy()` instead.".to_string()
+        "Shallow copy of `os.environ` via `copy.copy(os.environ)`".to_string()
     }
 
     fn fix_title(&self) -> String {
@@ -61,13 +65,17 @@ pub(crate) fn shallow_copy_environ(checker: &mut Checker, call: &ast::ExprCall) 
         return;
     }
 
-    let Some(first_arg) = call.arguments.args.first() else {
+    if !call.arguments.keywords.is_empty() {
+        return;
+    }
+
+    let [arg] = call.arguments.args.as_ref() else {
         return;
     };
 
     if !checker
         .semantic()
-        .resolve_qualified_name(first_arg)
+        .resolve_qualified_name(arg)
         .is_some_and(|qualified_name| matches!(qualified_name.segments(), ["os", "environ"]))
     {
         return;
@@ -75,7 +83,7 @@ pub(crate) fn shallow_copy_environ(checker: &mut Checker, call: &ast::ExprCall) 
 
     let mut diagnostic = Diagnostic::new(ShallowCopyEnviron, call.range());
     diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-        "os.environ.copy()".to_string(),
+        format!("{}.copy()", checker.locator().slice(arg)),
         call.range(),
     )));
     checker.diagnostics.push(diagnostic);
