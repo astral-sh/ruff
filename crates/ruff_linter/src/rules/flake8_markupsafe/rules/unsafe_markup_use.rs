4bfbe7c5500a1e555956701aacbcf79311206743
Daverball <dave@daverball.com>
2024-11-11 12:22:40 +0100

    Fixes formatting

--- crates/ruff_linter/src/rules/flake8_markupsafe/rules/unsafe_markup_use.rs
+++ crates/ruff_linter/src/rules/flake8_markupsafe/rules/unsafe_markup_use.rs
@@ -114,5 +114,5 @@ fn is_unsafe_call(call: &ExprCall) -> bool {
     // unlikely that someone will actually use a keyword argument here
     // TODO: Eventually we may want to allow dynamic values, as long as they
     //       have a __html__ attribute, since that is part of the API
-	matches!(&*call.arguments.args, [first] if !first.is_string_literal_expr() && !first.is_bytes_literal_expr())
+    matches!(&*call.arguments.args, [first] if !first.is_string_literal_expr() && !first.is_bytes_literal_expr())
 }
