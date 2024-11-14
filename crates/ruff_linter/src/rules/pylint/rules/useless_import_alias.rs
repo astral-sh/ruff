5717d3bdedc772a440e6acd939b5faf0bb102a42
dylwil3 <dylwil3@gmail.com>
2024-11-13 23:50:54 -0600

    allow clippy violation so that message preview in docs looks nicer

--- crates/ruff_linter/src/rules/pylint/rules/useless_import_alias.rs
+++ crates/ruff_linter/src/rules/pylint/rules/useless_import_alias.rs
@@ -37,6 +37,7 @@ impl Violation for UselessImportAlias {
 
     #[derive_message_formats]
     fn message(&self) -> String {
+        #[allow(clippy::if_not_else)]
         if !self.required_import_conflict {
             "Import alias does not rename original package".to_string()
         } else {
@@ -45,10 +46,10 @@ impl Violation for UselessImportAlias {
     }
 
     fn fix_title(&self) -> Option<String> {
-        if !self.required_import_conflict {
-            Some("Remove import alias".to_string())
-        } else {
+        if self.required_import_conflict {
             Some("Change required import or disable rule.".to_string())
+        } else {
+            Some("Remove import alias".to_string())
         }
     }
 }
