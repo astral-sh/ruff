516ea80d1b3d26045832e46229f17d1e2a068ba8
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-11-16 13:10:09 -0500

    Nits

--- crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI061.pyi
+++ crates/ruff_linter/resources/test/fixtures/flake8_pyi/PYI061.pyi
@@ -20,12 +20,12 @@ def func6(arg1: Literal[
     "hello",
     None  # Comment 1
     , "world"
-    ]): ...
+]): ...
 
 
 def func7(arg1: Literal[
     None  # Comment 1
-    ]): ...
+]): ...
 
 
 # OK
@@ -33,20 +33,5 @@ def good_func(arg1: Literal[int] | None): ...
 
 
 # From flake8-pyi
-Literal[None]  # Y061 None inside "Literal[]" expression. Replace with "None"
-Literal[True, None]  # Y061 None inside "Literal[]" expression. Replace with "Literal[True] | None"
-
-###
-# The following rules here are slightly subtle,
-# but make sense when it comes to giving the best suggestions to users of flake8-pyi.
-###
-
-# If Y061 and Y062 both apply, but all the duplicate members are None,
-# only emit Y061...
-Literal[None, None]  # Y061 None inside "Literal[]" expression. Replace with "None"
-Literal[1, None, "foo", None]  # Y061 None inside "Literal[]" expression. Replace with "Literal[1, 'foo'] | None"
-
-# ... but if Y061 and Y062 both apply
-# and there are no None members in the Literal[] slice,
-# only emit Y062:
-Literal[None, True, None, True]  # Y062 Duplicate "Literal[]" member "True"
+Literal[None]  # PYI061 None inside "Literal[]" expression. Replace with "None"
+Literal[True, None]  # PYI061 None inside "Literal[]" expression. Replace with "Literal[True] | None"
