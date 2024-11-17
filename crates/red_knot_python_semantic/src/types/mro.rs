35a2e51b60b5fcb0a5bcf22ec575e856a86171f3
Glyphack <sh.hooshyari@gmail.com>
2024-11-17 11:35:47 +0100

    Merge cases

--- crates/red_knot_python_semantic/src/types/mro.rs
+++ crates/red_knot_python_semantic/src/types/mro.rs
@@ -371,8 +371,9 @@ impl<'db> ClassBase<'db> {
             | Type::ModuleLiteral(_)
             | Type::SubclassOf(_) => None,
             Type::KnownInstance(known_instance) => match known_instance {
-                KnownInstanceType::Literal | KnownInstanceType::Optional => None,
-                KnownInstanceType::TypeVar(_) => None,
+                KnownInstanceType::TypeVar(_)
+                | KnownInstanceType::Literal
+                | KnownInstanceType::Optional => None,
             },
         }
     }
