8b925ea626f659311b9777ddbfe578adc7ea5035
Dylan <53534755+dylwil3@users.noreply.github.com>
2024-11-18 07:39:16 -0600

    [`pycodestyle`] Stabilize behavior to ignore stub files in `ambiguous-variable-name (E741)` (#14405)

--- crates/ruff_linter/src/rules/pycodestyle/mod.rs
+++ crates/ruff_linter/src/rules/pycodestyle/mod.rs
@@ -26,8 +26,8 @@ mod tests {
     #[test_case(Rule::AmbiguousClassName, Path::new("E742.py"))]
     #[test_case(Rule::AmbiguousFunctionName, Path::new("E743.py"))]
     #[test_case(Rule::AmbiguousVariableName, Path::new("E741.py"))]
-    // E741 has different behaviour for `.pyi` files in preview mode;
-    // this test case checks it still has the old behaviour in stable mode
+    // E741 is disapplied for `.pyi` files (see #13119 for rationale);
+    // this fixture tests that we emit no errors there
     #[test_case(Rule::AmbiguousVariableName, Path::new("E741.pyi"))]
     #[test_case(Rule::LambdaAssignment, Path::new("E731.py"))]
     #[test_case(Rule::BareExcept, Path::new("E722.py"))]
--- crates/ruff_linter/src/rules/pycodestyle/rules/ambiguous_variable_name.rs
+++ crates/ruff_linter/src/rules/pycodestyle/rules/ambiguous_variable_name.rs
@@ -9,6 +9,12 @@ use crate::rules::pycodestyle::helpers::is_ambiguous_name;
 /// ## What it does
 /// Checks for the use of the characters 'l', 'O', or 'I' as variable names.
 ///
+/// Note: This rule is automatically disabled for all stub files
+/// (files with `.pyi` extensions). The rule has little relevance for authors
+/// of stubs: a well-written stub should aim to faithfully represent the
+/// interface of the equivalent .py file as it exists at runtime, including any
+/// ambiguously named variables in the runtime module.
+///
 /// ## Why is this bad?
 /// In some fonts, these characters are indistinguishable from the
 /// numerals one and zero. When tempted to use 'l', use 'L' instead.
@@ -27,14 +33,6 @@ use crate::rules::pycodestyle::helpers::is_ambiguous_name;
 /// i = 42
 /// ```
 ///
-/// ## Preview mode behavior for stub files
-/// In [preview] mode, this rule is automatically disabled for all stub files
-/// (files with `.pyi` extensions). The rule has little relevance for authors
-/// of stubs: a well-written stub should aim to faithfully represent the
-/// interface of the equivalent .py file as it exists at runtime, including any
-/// ambiguously named variables in the runtime module.
-/// [preview]: https://docs.astral.sh/ruff/preview/
-
 #[violation]
 pub struct AmbiguousVariableName(pub String);
 
@@ -48,7 +46,7 @@ impl Violation for AmbiguousVariableName {
 
 /// E741
 pub(crate) fn ambiguous_variable_name(checker: &mut Checker, name: &str, range: TextRange) {
-    if checker.settings.preview.is_enabled() && checker.source_type.is_stub() {
+    if checker.source_type.is_stub() {
         return;
     }
     if is_ambiguous_name(name) {
