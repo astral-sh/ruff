10b2cacecdd22dc9c714dc6922fb28032ea0ec17
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-26 11:45:53 -0500

    switch to snapshot test

---
+++ crates/ruff_linter/resources/test/fixtures/pyflakes/F821_30.py
@@ -0,0 +1,12 @@
+"""Regression test for #13824.
+
+Don't report an error when the function being annotated has the
+`@no_type_check` decorator.
+"""
+
+from typing import no_type_check
+
+
+@no_type_check
+def f(arg: "A") -> "R":
+    pass
