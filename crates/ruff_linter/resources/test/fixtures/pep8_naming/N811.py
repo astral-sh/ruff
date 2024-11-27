cfc70bb152cd289a55ff637061aabf4b82674e75
snowdrop4 <82846066+snowdrop4@users.noreply.github.com>
2024-11-27 13:58:46 +0000

    change ordering of test file

--- crates/ruff_linter/resources/test/fixtures/pep8_naming/N811.py
+++ crates/ruff_linter/resources/test/fixtures/pep8_naming/N811.py
@@ -1,7 +1,7 @@
-import mod.CON as c
 import mod.CONST as const
 from mod import CONSTANT as constant
 from mod import ANOTHER_CONSTANT as another_constant
+import mod.CON as c
 
 import django.db.models.Q as Query1
 from django.db.models import Q as Query2
