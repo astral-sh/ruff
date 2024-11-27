d56c079ada3b4b092c6d51848dff83a577df656b
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-27 17:46:31 -0500

    update tests

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF055.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF055.py
@@ -5,14 +5,35 @@ s = "str"
 # this should be replaced with s.replace("abc", "")
 re.sub("abc", "", s)
 
-# this should be replaced with s.startswith("abc")
-re.match("abc", s)
+
+# this example, adapted from https://docs.python.org/3/library/re.html#re.sub,
+# should *not* be replaced because repl is a function, not a string
+def dashrepl(matchobj):
+    if matchobj.group(0) == "-":
+        return " "
+    else:
+        return "-"
+
+
+re.sub("-", dashrepl, "pro----gram-files")
+
+# this one should be replaced with s.startswith("abc") because the Match is
+# used in an if context for its truth value
+if re.match("abc", s):
+    pass
+if m := re.match("abc", s):  # this should *not* be replaced
+    pass
+re.match("abc", s)  # this should not be replaced because match returns a Match
 
 # this should be replaced with "abc" in s
-re.search("abc", s)
+if re.search("abc", s):
+    pass
+re.search("abc", s)  # this should not be replaced
 
 # this should be replaced with "abc" == s
-re.fullmatch("abc", s)
+if re.fullmatch("abc", s):
+    pass
+re.fullmatch("abc", s)  # this should not be replaced
 
 # this should be replaced with s.split("abc")
 re.split("abc", s)
