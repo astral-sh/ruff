1732d6b4e448ce76745b739a6206af846bfcd834
snowdrop4 <82846066+snowdrop4@users.noreply.github.com>
2024-11-27 13:57:44 +0000

    remove comments from test files

--- crates/ruff_linter/resources/test/fixtures/pep8_naming/N811.py
+++ crates/ruff_linter/resources/test/fixtures/pep8_naming/N811.py
@@ -1,9 +1,7 @@
-# Error
 import mod.CON as c
 import mod.CONST as const
 from mod import CONSTANT as constant
 from mod import ANOTHER_CONSTANT as another_constant
 
-# Ok
 import django.db.models.Q as Query1
 from django.db.models import Q as Query2
