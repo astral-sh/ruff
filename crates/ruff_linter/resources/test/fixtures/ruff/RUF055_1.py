1817fb67aa03ef053cb8e870879053727db4aa71
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-28 16:52:52 -0500

    test repl resolution

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF055_1.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF055_1.py
@@ -1,4 +1,5 @@
-"Test that RUF055 can follow a chain of str assignments"
+"""Test that RUF055 can follow a chain of str assignments for both the pattern
+and the replacement argument to re.sub"""
 
 import re
 
@@ -10,3 +11,6 @@ re.sub(pat3, "", haystack)
 
 if pat4 := pat3:
     re.sub(pat4, "", haystack)
+
+repl = "new"
+re.sub(r"abc", repl, haystack)
