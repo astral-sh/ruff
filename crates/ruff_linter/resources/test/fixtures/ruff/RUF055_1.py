8d2539a23647a496f1f82faee42ad72e0d56b8d8
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-28 16:34:25 -0500

    add test, include named expr assignment

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF055_1.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF055_1.py
@@ -1,3 +1,5 @@
+"Test that RUF055 can follow a chain of str assignments"
+
 import re
 
 pat1 = "needle"
@@ -5,3 +7,6 @@ pat2 = pat1
 pat3 = pat2
 
 re.sub(pat3, "", haystack)
+
+if pat4 := pat3:
+    re.sub(pat4, "", haystack)
