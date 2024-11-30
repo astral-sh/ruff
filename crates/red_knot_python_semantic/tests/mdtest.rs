c3efac9d5ef441fdaa3573bf8c10884d6ffe100d
Micha Reiser <micha@reiser.io>
2024-11-30 11:56:06 +0100

    Use Utf8Path, move test_name function to the end

--- crates/red_knot_python_semantic/tests/mdtest.rs
+++ crates/red_knot_python_semantic/tests/mdtest.rs
@@ -1,13 +1,31 @@
-use std::ffi::OsStr;
+use camino::Utf8Path;
+use dir_test::{dir_test, Fixture};
 use std::path::Path;
 
-use dir_test::{dir_test, Fixture};
+/// See `crates/red_knot_test/README.md` for documentation on these tests.
+#[dir_test(
+    dir: "$CARGO_MANIFEST_DIR/resources/mdtest",
+    glob: "**/*.md"
+)]
+#[allow(clippy::needless_pass_by_value)]
+fn mdtest(fixture: Fixture<&str>) {
+    let fixture_path = Utf8Path::new(fixture.path());
+    let crate_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let workspace_root = crate_dir.ancestors().nth(2).unwrap();
+
+    let long_title = fixture_path.strip_prefix(workspace_root).unwrap();
+    let short_title = fixture_path.file_name().unwrap();
+
+    let test_name = test_name("mdtest", fixture_path);
+
+    red_knot_test::run(fixture_path, long_title.as_str(), short_title, &test_name);
+}
 
 /// Constructs the test name used for individual markdown files
 ///
 /// This code is copied from <https://github.com/fe-lang/dir-test/blob/1c0f41c480a3490bc2653a043ff6e3f8085a1f47/macros/src/lib.rs#L104-L138>
 /// and should be updated if they diverge
-fn test_name(test_func_name: &str, fixture_path: &Path) -> String {
+fn test_name(test_func_name: &str, fixture_path: &Utf8Path) -> String {
     assert!(fixture_path.is_file());
 
     let dir_path = format!("{}/resources/mdtest", std::env!("CARGO_MANIFEST_DIR"));
@@ -19,8 +37,7 @@ fn test_name(test_func_name: &str, fixture_path: &Path) -> String {
 
     for component in rel_path.parent().unwrap().components() {
         let component = component
-            .as_os_str()
-            .to_string_lossy()
+            .as_str()
             .replace(|c: char| c.is_ascii_punctuation(), "_");
         test_name.push_str(&component);
         test_name.push('_');
@@ -30,32 +47,8 @@ fn test_name(test_func_name: &str, fixture_path: &Path) -> String {
         &rel_path
             .file_stem()
             .unwrap()
-            .to_string_lossy()
             .replace(|c: char| c.is_ascii_punctuation(), "_"),
     );
 
     test_name
-}
-
-/// See `crates/red_knot_test/README.md` for documentation on these tests.
-#[dir_test(
-    dir: "$CARGO_MANIFEST_DIR/resources/mdtest",
-    glob: "**/*.md"
-)]
-#[allow(clippy::needless_pass_by_value)]
-fn mdtest(fixture: Fixture<&str>) {
-    let fixture_path = Path::new(fixture.path());
-    let crate_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
-    let workspace_root = crate_dir.parent().and_then(Path::parent).unwrap();
-
-    let long_title = fixture_path
-        .strip_prefix(workspace_root)
-        .unwrap()
-        .to_str()
-        .unwrap();
-    let short_title = fixture_path.file_name().and_then(OsStr::to_str).unwrap();
-
-    let test_name = test_name("mdtest", fixture_path);
-
-    red_knot_test::run(fixture_path, long_title, short_title, &test_name);
 }
