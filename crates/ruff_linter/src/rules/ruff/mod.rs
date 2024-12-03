62e358e9298e8f5899c189a674ba2891143155f3
Brent Westbrook <36778786+ntBre@users.noreply.github.com>
2024-12-03 10:17:20 -0500

    [`ruff`] Extend unnecessary-regular-expression to non-literal strings (`RUF055`) (#14679)
    Co-authored-by: Alex Waygood <alex.waygood@gmail.com>

--- crates/ruff_linter/src/rules/ruff/mod.rs
+++ crates/ruff_linter/src/rules/ruff/mod.rs
@@ -411,7 +411,8 @@ mod tests {
     #[test_case(Rule::MapIntVersionParsing, Path::new("RUF048_1.py"))]
     #[test_case(Rule::UnrawRePattern, Path::new("RUF039.py"))]
     #[test_case(Rule::UnrawRePattern, Path::new("RUF039_concat.py"))]
-    #[test_case(Rule::UnnecessaryRegularExpression, Path::new("RUF055.py"))]
+    #[test_case(Rule::UnnecessaryRegularExpression, Path::new("RUF055_0.py"))]
+    #[test_case(Rule::UnnecessaryRegularExpression, Path::new("RUF055_1.py"))]
     fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!(
             "preview__{}_{}",
