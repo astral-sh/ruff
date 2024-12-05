fda8b1f884c8598960b2e3f0c729e17ac62bfe35
InSync <insyncwithfoo@gmail.com>
2024-12-05 16:30:06 +0700

    [`ruff`] Unnecessary cast to `int` (`RUF046`) (#14697)
    ## Summary
    
    Resolves #11412.
    
    ## Test Plan
    
    `cargo nextest run` and `cargo insta test`.
    
    ---------
    
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_linter/src/checkers/ast/analyze/expression.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/expression.rs
@@ -1093,6 +1093,9 @@ pub(crate) fn expression(expr: &Expr, checker: &mut Checker) {
             if checker.enabled(Rule::Airflow3Removal) {
                 airflow::rules::removed_in_3(checker, expr);
             }
+            if checker.enabled(Rule::UnnecessaryCastToInt) {
+                ruff::rules::unnecessary_cast_to_int(checker, call);
+            }
         }
         Expr::Dict(dict) => {
             if checker.any_enabled(&[
