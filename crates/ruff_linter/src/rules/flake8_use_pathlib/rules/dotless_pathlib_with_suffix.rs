43c746c11ad09dc88ab10e68fa86c0305731e0a0
InSync <insyncwithfoo@gmail.com>
2024-12-06 18:52:59 +0700

    Per review

--- crates/ruff_linter/src/rules/flake8_use_pathlib/rules/dotless_pathlib_with_suffix.rs
+++ crates/ruff_linter/src/rules/flake8_use_pathlib/rules/dotless_pathlib_with_suffix.rs
@@ -7,22 +7,12 @@ use ruff_python_semantic::SemanticModel;
 use ruff_text_size::Ranged;
 
 /// ## What it does
-/// Checks for `Path.with_suffix()` calls where
+/// Checks for `pathlib.Path.with_suffix()` calls where
 /// the given suffix does not have a leading dot.
 ///
 /// ## Why is this bad?
 /// `Path.with_suffix()` will raise an error at runtime
 /// if the given suffix is not prefixed with a dot.
-///
-/// ## Known problems
-/// This rule is prone to false positives and negatives
-/// due to type inference limitations, as it will only detect
-/// paths that are either instantiated (`p = Path(...)`)
-/// or annotated (`def f(p: Path)`) as such.
-///
-/// ## Fix safety
-/// The fix is marked as unsafe, since it changes runtime behaviour
-/// (`.with_suffix()` call no longer raises an exception).
 ///
 /// ## Examples
 ///
@@ -35,6 +25,21 @@ use ruff_text_size::Ranged;
 /// ```python
 /// path.with_suffix(".py")
 /// ```
+///
+/// ## Known problems
+/// This rule is prone to false negatives due to type inference limitations,
+/// as it will only detect paths that are either instantiated (`p = Path(...)`)
+/// or annotated (`def f(p: Path)`) as such.
+///
+/// ## Fix safety
+/// The fix for this rule adds a leading period to the string passed
+/// to the `with_suffix()` call. This fix is marked as unsafe, as it
+/// changes runtime behaviour: the call would previously always have
+/// raised an exception, but no longer will.
+///
+/// Moreover, it's impossible to determine if this is the correct fix
+/// for a given situation (it's possible that the string was correct
+/// but was being passed to the wrong method entirely, for example).
 #[derive(ViolationMetadata)]
 pub(crate) struct DotlessPathlibWithSuffix;
 
