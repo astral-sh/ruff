d3e2fadc487ce76d35ad3b2eb74c3e2b1a962f18
dylwil3 <dylwil3@gmail.com>
2024-12-06 12:32:02 -0600

    add tests to fixture

--- crates/ruff_linter/resources/test/fixtures/pylint/boolean_chained_comparison.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/boolean_chained_comparison.py
@@ -130,3 +130,17 @@ a < b and (b < c)
 
 # should error and fix
 a<b<c and c<d
+
+# more involved examples (all should error and fix)
+a < ( # sneaky comment
+	b
+  # more comments 
+) and b < c
+
+(
+    a
+    <b
+    # hmmm...
+    <c
+    and ((c<d))
+)
