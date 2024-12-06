f7d10b66553cc1b6d66c9ad270013e2a5bd1f95d
David Peter <mail@david-peter.de>
2024-12-06 21:07:30 +0100

    Add comment

--- crates/red_knot_python_semantic/src/semantic_index/use_def/symbol_state.rs
+++ crates/red_knot_python_semantic/src/semantic_index/use_def/symbol_state.rs
@@ -156,6 +156,7 @@ pub(super) struct SymbolBindings {
     /// binding in `live_bindings`.
     constraints: Constraints,
 
+    /// For each live binding, which [`ScopedConstraintId`] were active *at the time of the binding*?
     constraints_active_at_binding: Constraints,
 
     /// Could the symbol be unbound at this point?
