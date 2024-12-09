0d259af3993d877096f5f27b68b26fd3ff3f7406
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-12-09 17:53:29 +0000

    Add a `utf8_code_other` case

--- crates/ruff_linter/src/rules/pyupgrade/mod.rs
+++ crates/ruff_linter/src/rules/pyupgrade/mod.rs
@@ -82,6 +82,7 @@ mod tests {
     #[test_case(Rule::UTF8EncodingDeclaration, Path::new("UP009_utf8_other.py"))]
     #[test_case(Rule::UTF8EncodingDeclaration, Path::new("UP009_utf8_utf8.py"))]
     #[test_case(Rule::UTF8EncodingDeclaration, Path::new("UP009_utf8_utf8_other.py"))]
+    #[test_case(Rule::UTF8EncodingDeclaration, Path::new("UP009_utf8_code_other.py"))]
     #[test_case(
         Rule::UTF8EncodingDeclaration,
         Path::new("UP009_hashbang_utf8_other.py")
