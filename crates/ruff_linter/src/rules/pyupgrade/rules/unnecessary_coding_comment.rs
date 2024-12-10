a31affd0923fefe09461405e64980d4cea4e5f32
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-12-10 20:02:40 +0000

    Simplify

--- crates/ruff_linter/src/rules/pyupgrade/rules/unnecessary_coding_comment.rs
+++ crates/ruff_linter/src/rules/pyupgrade/rules/unnecessary_coding_comment.rs
@@ -63,7 +63,20 @@ pub(crate) fn unnecessary_coding_comment(
     locator: &Locator,
     comment_ranges: &CommentRanges,
 ) {
-    let mut coding_comments = coding_comments_on_first_three_lines(locator, comment_ranges);
+    let first_line_trimmed = locator.full_line_str(0.into()).trim();
+
+    if !first_line_trimmed.is_empty() && !first_line_trimmed.starts_with('#') {
+        return;
+    }
+
+    // The coding comment must be on one of the first two lines.
+    // Since each comment spans at least one line,
+    // we only need to check the first two comments,
+    // plus a third to make sure it would not become a new coding comment.
+    let mut coding_comments = comment_ranges
+        .iter()
+        .take(3)
+        .map(|comment_range| coding_comment(locator, *comment_range));
 
     let first = coding_comments.next().flatten();
     let second = coding_comments.next().flatten();
@@ -79,50 +92,25 @@ pub(crate) fn unnecessary_coding_comment(
     }
 }
 
-fn coding_comments_on_first_three_lines<'a>(
-    locator: &'a Locator<'a>,
-    comment_ranges: &'a CommentRanges,
-) -> impl Iterator<Item = Option<CodingComment>> + use<'a> {
-    let first_line_trimmed = locator.full_line_str(0.into()).trim();
+fn coding_comment(locator: &Locator, self_range: TextRange) -> Option<CodingComment> {
+    let line_range = locator.full_line_range(self_range.start());
 
-    let seen_code = !first_line_trimmed.is_empty() && !first_line_trimmed.starts_with('#');
+    // If leading content is not whitespace then it's not a valid coding comment e.g.
+    // ```
+    // print(x) # coding=utf8
+    // ```
+    let before_hash_sign = locator.slice(TextRange::new(line_range.start(), self_range.start()));
 
-    // The coding comment must be on one of the first two lines.
-    // Since each comment spans at least one line,
-    // we only need to check the first two comments,
-    // plus a third to make sure it would not become a new coding comment.
-    comment_ranges.iter().take(3).map(move |comment_range| {
-        if seen_code {
-            return None;
-        }
+    if !before_hash_sign.trim().is_empty() {
+        return None;
+    }
 
-        let line_range = locator.full_line_range(comment_range.start());
-        let line_index = locator.count_lines(TextRange::up_to(line_range.start()));
+    let line_index = locator.count_lines(TextRange::up_to(line_range.start()));
 
-        if line_index > 2 {
-            return None;
-        }
+    if line_index > 2 {
+        return None;
+    }
 
-        // If leading content is not whitespace then it's not a valid coding comment e.g.
-        // ```
-        // print(x) # coding=utf8
-        // ```
-        let before_hash_sign =
-            locator.slice(TextRange::new(line_range.start(), comment_range.start()));
-
-        if !before_hash_sign.trim().is_empty() {
-            return None;
-        }
-
-        coding_comment(locator, *comment_range, line_range)
-    })
-}
-
-fn coding_comment(
-    locator: &Locator,
-    self_range: TextRange,
-    line_range: TextRange,
-) -> Option<CodingComment> {
     let part_of_interest = CODING_COMMENT_REGEX.captures(locator.slice(line_range))?;
     let coding_name = part_of_interest.name("name")?.as_str();
 
