6f8d8fa36b7c75122cbd87b71eb0add493457b6a
InSync <insyncwithfoo@gmail.com>
2024-12-11 18:12:23 +0700

    [`ruff`] `if k in d: del d[k]` (`RUF051`) (#14553)
    ## Summary
    
    Resolves #7537.
    
    ## Test Plan
    
    `cargo nextest run` and `cargo insta test`.
    
    ---------
    
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_linter/src/rules/ruff/mod.rs
+++ crates/ruff_linter/src/rules/ruff/mod.rs
@@ -71,6 +71,7 @@ mod tests {
     #[test_case(Rule::InvalidAssertMessageLiteralArgument, Path::new("RUF040.py"))]
     #[test_case(Rule::UnnecessaryNestedLiteral, Path::new("RUF041.py"))]
     #[test_case(Rule::UnnecessaryNestedLiteral, Path::new("RUF041.pyi"))]
+    #[test_case(Rule::IfKeyInDictDel, Path::new("RUF051.py"))]
     #[test_case(Rule::UsedDummyVariable, Path::new("RUF052.py"))]
     fn rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!("{}_{}", rule_code.noqa_code(), path.to_string_lossy());
