51e4f655e4be6a9ac751ed1cae9a276be0cbb10e
Sergey Mezentsev <thebits@yandex.ru>
2024-12-12 15:38:20 +0300

    Format code

--- crates/ruff_linter/src/rules/flake8_use_pathlib/rules/invalid_pathlib_with_suffix.rs
+++ crates/ruff_linter/src/rules/flake8_use_pathlib/rules/invalid_pathlib_with_suffix.rs
@@ -60,9 +60,11 @@ impl Violation for InvalidPathlibWithSuffix {
 
     #[derive_message_formats]
     fn message(&self) -> String {
-        if !self.single_dot {"Dotless suffix passed to `.with_suffix()`".to_string()} else {
+        if !self.single_dot {
+            "Dotless suffix passed to `.with_suffix()`".to_string()
+        } else {
             "Invalid suffix passed to `.with_suffix()`".to_string()
-         }
+        }
     }
 
     fn fix_title(&self) -> Option<String> {
