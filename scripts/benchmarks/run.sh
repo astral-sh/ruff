91135da2589f749e278a54d4e30a3d40bdd8a18e
Micha Reiser <micha@reiser.io>
2024-12-23 10:03:13 +0100

    Update benchmark scripts, use uv

--- scripts/benchmarks/run.sh
+++ scripts/benchmarks/run.sh
@@ -4,5 +4,5 @@
 # Benchmark Ruff on the CPython codebase.
 ###
 
-cargo build --release && hyperfine --ignore-failure --warmup 10 \
-  "./target/release/ruff ./crates/ruff_linter/resources/test/cpython/ --no-cache"
+cargo build --release && hyperfine --warmup 10 \
+  "./target/release/ruff check ./crates/ruff_linter/resources/test/cpython/ --no-cache --exit-zero"
