5947f64f2bf488ca4f40ac3ea3ff1828b0cd27c6
David Salvisberg <dave@daverball.com>
2025-01-03 14:25:51 +0100

    Update crates/ruff_linter/src/checkers/ast/mod.rs
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- crates/ruff_linter/src/checkers/ast/mod.rs
+++ crates/ruff_linter/src/checkers/ast/mod.rs
@@ -2324,6 +2324,7 @@ impl<'a> Checker<'a> {
                     let parsed_expr = parsed_annotation.expression();
                     self.visit_expr(parsed_expr);
                     if self.semantic.in_type_alias_value() {
+                        // stub files are covered by PYI020
                         if !self.source_type.is_stub() && self.enabled(Rule::QuotedTypeAlias) {
                             flake8_type_checking::rules::quoted_type_alias(
                                 self,
