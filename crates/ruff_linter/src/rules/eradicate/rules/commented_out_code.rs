e6e44d89670a3dc0c617669faa1fa66e7312725b
InSync <insyncwithfoo@gmail.com>
2025-01-08 03:38:57 +0700

    Clippy 2

--- crates/ruff_linter/src/rules/eradicate/rules/commented_out_code.rs
+++ crates/ruff_linter/src/rules/eradicate/rules/commented_out_code.rs
@@ -90,7 +90,7 @@ where
     let line_end = locator.full_line_end(script_start.end());
     let rest = locator.after(line_end);
     let mut end_offset = None;
-    let mut lines = UniversalNewlineIterator::with_offset(rest, line_end);
+    let lines = UniversalNewlineIterator::with_offset(rest, line_end);
 
     for line in lines {
         let Some(content) = script_line_content(&line) else {
