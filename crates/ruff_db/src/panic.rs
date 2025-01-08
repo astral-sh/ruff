0e66889a1c9eeb895a37ad1b99b2ae402e047229
Douglas Creager <dcreager@dcreager.net>
2025-01-08 11:08:32 -0500

    Document (and detect) that we don't work with competing panic hooks

--- crates/ruff_db/src/panic.rs
+++ crates/ruff_db/src/panic.rs
@@ -20,6 +20,7 @@ impl std::fmt::Display for PanicError {
 
 thread_local! {
     static CAPTURE_PANIC_INFO: Cell<bool> = const { Cell::new(false) };
+    static OUR_HOOK_RAN: Cell<bool> = const { Cell::new(false) };
     static LAST_PANIC: Cell<Option<PanicError>> = const { Cell::new(None) };
 }
 
@@ -28,6 +29,7 @@ fn install_hook() {
     ONCE.get_or_init(|| {
         let prev = std::panic::take_hook();
         std::panic::set_hook(Box::new(move |info| {
+            OUR_HOOK_RAN.with(|cell| cell.set(true));
             let should_capture = CAPTURE_PANIC_INFO.with(Cell::get);
             if !should_capture {
                 return (*prev)(info);
@@ -54,18 +56,24 @@ fn install_hook() {
 /// still use the previous hook (typically the default hook, which prints out panic information to
 /// stderr).
 ///
-/// Note that we are careful to install our custom hook only once, and we do not restore the
-/// previous hook (since can always retain the previous hook's behavior by not calling this
+/// We assume that there is nothing else running in this process that needs to install a competing
+/// panic hook.  We are careful to install our custom hook only once, and we do not ever restore
+/// the previous hook (since you can always retain the previous hook's behavior by not calling this
 /// wrapper).
 pub fn catch_unwind<F, R>(f: F) -> Result<R, PanicError>
 where
     F: FnOnce() -> R + std::panic::UnwindSafe,
 {
     install_hook();
-    let prev_should_capture =
-        CAPTURE_PANIC_INFO.with(|should_capture| should_capture.replace(true));
-    let result = std::panic::catch_unwind(f)
-        .map_err(|_| LAST_PANIC.with(std::cell::Cell::take).unwrap_or_default());
-    CAPTURE_PANIC_INFO.with(|should_capture| should_capture.set(prev_should_capture));
+    OUR_HOOK_RAN.with(|cell| cell.set(false));
+    let prev_should_capture = CAPTURE_PANIC_INFO.with(|cell| cell.replace(true));
+    let result = std::panic::catch_unwind(f).map_err(|_| {
+        let our_hook_ran = OUR_HOOK_RAN.with(Cell::get);
+        if !our_hook_ran {
+            panic!("detected a competing panic hook");
+        }
+        LAST_PANIC.with(Cell::take).unwrap_or_default()
+    });
+    CAPTURE_PANIC_INFO.with(|cell| cell.set(prev_should_capture));
     result
 }
