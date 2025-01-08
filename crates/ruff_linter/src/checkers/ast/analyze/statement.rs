aa62f803f62618419c4cccd28b2660f6466d8af6
InSync <insyncwithfoo@gmail.com>
2025-01-08 15:27:46 +0700

    [`flake8-pytest-style`] Stabilize "Detect more `pytest.mark.parametrize` calls" (`PT006`) (#15327)
    Co-authored-by: Micha Reiser <micha@reiser.io>
    Resolves #15324. Stabilizes the behavior changes introduced in #14515.

--- crates/ruff_linter/src/checkers/ast/analyze/statement.rs
+++ crates/ruff_linter/src/checkers/ast/analyze/statement.rs
@@ -309,21 +309,7 @@ pub(crate) fn statement(stmt: &Stmt, checker: &mut Checker) {
                     body,
                 );
             }
-            // In preview mode, calls are analyzed. To avoid duplicate diagnostics,
-            // skip analyzing the decorators.
-            if !checker.settings.preview.is_enabled()
-                && checker.any_enabled(&[
-                    Rule::PytestParametrizeNamesWrongType,
-                    Rule::PytestParametrizeValuesWrongType,
-                    Rule::PytestDuplicateParametrizeTestCases,
-                ])
-            {
-                for decorator in decorator_list {
-                    if let Some(call) = decorator.expression.as_call_expr() {
-                        flake8_pytest_style::rules::parametrize(checker, call);
-                    }
-                }
-            }
+
             if checker.any_enabled(&[
                 Rule::PytestIncorrectMarkParenthesesStyle,
                 Rule::PytestUseFixturesWithoutParameters,
