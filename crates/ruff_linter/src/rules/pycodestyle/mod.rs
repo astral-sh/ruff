b0905c4b043189879af78afb757270c4e0397e25
Dylan <53534755+dylwil3@users.noreply.github.com>
2025-01-09 10:50:39 -0600

    [`pycodestyle`] Handle each cell separately for `too-many-newlines-at-end-of-file` (`W391`) (#15308)
    Jupyter notebooks are converted into source files by joining with
    newlines, which confuses the check [too-many-newlines-at-end-of-file
    (W391)](https://docs.astral.sh/ruff/rules/too-many-newlines-at-end-of-file/#too-many-newlines-at-end-of-file-w391).
    This PR introduces logic to apply the check cell-wise (and, in
    particular, correctly handles empty cells.)
    
    Closes #13763

--- crates/ruff_linter/src/rules/pycodestyle/mod.rs
+++ crates/ruff_linter/src/rules/pycodestyle/mod.rs
@@ -79,6 +79,7 @@ mod tests {
     #[test_case(Rule::TooManyNewlinesAtEndOfFile, Path::new("W391_2.py"))]
     #[test_case(Rule::TooManyNewlinesAtEndOfFile, Path::new("W391_3.py"))]
     #[test_case(Rule::TooManyNewlinesAtEndOfFile, Path::new("W391_4.py"))]
+    #[test_case(Rule::TooManyNewlinesAtEndOfFile, Path::new("W391.ipynb"))]
     fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!(
             "preview__{}_{}",
