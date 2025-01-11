c120a99de5a0429879df4160652f5743ccd76253
Nikolas Hearp <154913660+njhearp@users.noreply.github.com>
2025-01-11 17:32:49 -0500

    Use match instead of contains
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_linter/src/rules/ruff/rules/unit_test_super.rs
+++ crates/ruff_linter/src/rules/ruff/rules/unit_test_super.rs
@@ -63,7 +63,7 @@ pub(crate) fn unit_test_super(checker: &mut Checker, class: &StmtClassDef) {
         if let Stmt::FunctionDef(function_def) = stmt {
             let body = &function_def.body;
             let name = function_def.name.as_str();
-            if unit_test_methods.contains(&name) && !has_super_call(body, name) {
+            if matches!(name, "setUp" | "tearDown" | "setUpClass" | "tearDownClass) && !has_super_call(body, name) {
                 checker
                     .diagnostics
                     .push(Diagnostic::new(UnitTestSuper, stmt.identifier()));
