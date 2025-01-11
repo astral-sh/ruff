cc1eb7b23c0ac37cd2bdc0e8a8103b0e5124bb9e
Kasperi Apell <apkaspell@gmail.com>
2025-01-11 12:31:35 +0200

    Refactor to account for subexpressions otherwise following bugbear

--- crates/ruff_linter/src/rules/flake8_bugbear/rules/return_in_generator.rs
+++ crates/ruff_linter/src/rules/flake8_bugbear/rules/return_in_generator.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use ruff_diagnostics::Diagnostic;
 use ruff_diagnostics::Violation;
 use ruff_macros::{derive_message_formats, ViolationMetadata};
@@ -97,7 +95,7 @@ pub(crate) fn return_in_generator(checker: &mut Checker, function_def: &ast::Stm
     }
 
     let mut visitor = ReturnInGeneratorVisitor::default();
-    ast::statement_visitor::StatementVisitor::visit_body(&mut visitor, &function_def.body);
+    visitor.visit_body(&function_def.body);
 
     if visitor.has_yield {
         if let Some(return_) = visitor.return_ {
@@ -108,20 +106,13 @@ pub(crate) fn return_in_generator(checker: &mut Checker, function_def: &ast::Stm
     }
 }
 
-enum BindState {
-    Stored,
-    Reassigned,
-}
-
 #[derive(Default)]
 struct ReturnInGeneratorVisitor {
     return_: Option<TextRange>,
     has_yield: bool,
-    yield_expr_names: HashMap<String, BindState>,
-    yield_on_last_visit: bool,
 }
 
-impl ast::statement_visitor::StatementVisitor<'_> for ReturnInGeneratorVisitor {
+impl Visitor<'_> for ReturnInGeneratorVisitor {
     fn visit_stmt(&mut self, stmt: &Stmt) {
         match stmt {
             Stmt::Expr(ast::StmtExpr { value, .. }) => match **value {
@@ -132,131 +123,26 @@ impl ast::statement_visitor::StatementVisitor<'_> for ReturnInGeneratorVisitor {
                     self.visit_expr(value);
                 }
             },
+            Stmt::Assign(_) | Stmt::AnnAssign(_) | Stmt::AugAssign(_) => {}
             Stmt::FunctionDef(_) => {
                 // Do not recurse into nested functions; they're evaluated separately.
             }
-            Stmt::Assign(ast::StmtAssign { targets, value, .. }) => {
-                for target in targets {
-                    self.discover_yield_assignments(target, value);
+            Stmt::Return(ast::StmtReturn { value, range }) => {
+                if value.is_some() {
+                    self.return_ = Some(*range);
                 }
             }
-            Stmt::AnnAssign(ast::StmtAnnAssign {
-                target,
-                value: Some(value),
-                ..
-            }) => {
-                self.yield_on_last_visit = false;
-                self.visit_expr(value);
-                self.evaluate_target(target);
-            }
-            Stmt::Return(ast::StmtReturn {
-                value: Some(value),
-                range,
-            }) => {
-                if let Expr::Name(ast::ExprName { ref id, .. }) = **value {
-                    if !matches!(
-                        self.yield_expr_names.get(id.as_str()),
-                        Some(BindState::Reassigned) | None
-                    ) {
-                        return;
-                    }
-                }
-                self.return_ = Some(*range);
-            }
-            _ => ast::statement_visitor::walk_stmt(self, stmt),
+            _ => ast::visitor::walk_stmt(self, stmt),
         }
     }
-}
 
-impl Visitor<'_> for ReturnInGeneratorVisitor {
     fn visit_expr(&mut self, expr: &Expr) {
         match expr {
             Expr::Yield(_) | Expr::YieldFrom(_) => {
                 self.has_yield = true;
-                self.yield_on_last_visit = true;
             }
             Expr::Lambda(_) | Expr::Call(_) => {}
             _ => ast::visitor::walk_expr(self, expr),
-        }
-    }
-}
-
-impl ReturnInGeneratorVisitor {
-    /// Determine if a target is bound to a yield or a yield from expression and,
-    /// if so, track that target
-    fn evaluate_target(&mut self, target: &Expr) {
-        if let Expr::Name(ast::ExprName { ref id, .. }) = *target {
-            if self.yield_on_last_visit {
-                match self.yield_expr_names.get(id.as_str()) {
-                    Some(BindState::Reassigned) => {}
-                    _ => {
-                        self.yield_expr_names
-                            .insert(id.to_string(), BindState::Stored);
-                    }
-                }
-            } else {
-                if let Some(BindState::Stored) = self.yield_expr_names.get(id.as_str()) {
-                    self.yield_expr_names
-                        .insert(id.to_string(), BindState::Reassigned);
-                }
-            }
-        }
-    }
-
-    /// Given a target and a value, track any identifiers that are bound to
-    /// yield or yield from expressions
-    fn discover_yield_assignments(&mut self, target: &Expr, value: &Expr) {
-        match target {
-            Expr::Name(_) => {
-                self.yield_on_last_visit = false;
-                self.visit_expr(value);
-                self.evaluate_target(target);
-            }
-            Expr::Tuple(ast::ExprTuple { elts: tar_elts, .. })
-            | Expr::List(ast::ExprList { elts: tar_elts, .. }) => match value {
-                Expr::Tuple(ast::ExprTuple { elts: val_elts, .. })
-                | Expr::List(ast::ExprList { elts: val_elts, .. })
-                | Expr::Set(ast::ExprSet { elts: val_elts, .. }) => {
-                    self.discover_yield_container_assignments(tar_elts, val_elts);
-                }
-                Expr::Yield(_) | Expr::YieldFrom(_) => {
-                    self.has_yield = true;
-                    self.yield_on_last_visit = true;
-                    self.evaluate_target(target);
-                }
-                _ => {}
-            },
-            _ => {}
-        }
-    }
-
-    fn discover_yield_container_assignments(&mut self, targets: &[Expr], values: &[Expr]) {
-        for (target, value) in targets.iter().zip(values) {
-            match target {
-                Expr::Tuple(ast::ExprTuple { elts: tar_elts, .. })
-                | Expr::List(ast::ExprList { elts: tar_elts, .. })
-                | Expr::Set(ast::ExprSet { elts: tar_elts, .. }) => {
-                    match value {
-                        Expr::Tuple(ast::ExprTuple { elts: val_elts, .. })
-                        | Expr::List(ast::ExprList { elts: val_elts, .. })
-                        | Expr::Set(ast::ExprSet { elts: val_elts, .. }) => {
-                            self.discover_yield_container_assignments(tar_elts, val_elts);
-                        }
-                        Expr::Yield(_) | Expr::YieldFrom(_) => {
-                            self.has_yield = true;
-                            self.yield_on_last_visit = true;
-                            self.evaluate_target(target);
-                        }
-                        _ => {}
-                    };
-                }
-                Expr::Name(_) => {
-                    self.yield_on_last_visit = false;
-                    self.visit_expr(value);
-                    self.evaluate_target(target);
-                }
-                _ => {}
-            }
         }
     }
 }
