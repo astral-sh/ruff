6f35a4d8d56ce8680dd6ccec66af8f3145acf877
InSync <insyncwithfoo@gmail.com>
2025-01-13 15:51:02 +0700

    [`fastapi`] Handle parameters with `Depends` correctly (`FAST003`) (#15364)
    Co-authored-by: Charlie Marsh <charlie.r.marsh@gmail.com>
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_linter/resources/test/fixtures/fastapi/FAST003.py
+++ crates/ruff_linter/resources/test/fixtures/fastapi/FAST003.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path
+from fastapi import Depends, FastAPI, Path
 
 app = FastAPI()
 
@@ -144,3 +144,37 @@ async def read_thing(query: str):
 @app.get("/things/{thing_id=}")
 async def read_thing(query: str):
     return {"query": query}
+
+
+# https://github.com/astral-sh/ruff/issues/13657
+def takes_thing_id(thing_id): ...
+def something_else(lorem): ...
+
+from foo import unknown_imported
+unknown_not_function = unknown_imported()
+
+
+### Errors
+@app.get("/things/{thing_id}")
+async def single(other: Annotated[str, Depends(something_else)]): ...
+@app.get("/things/{thing_id}")
+async def default(other: str = Depends(something_else)): ...
+
+
+### No errors
+# A parameter with multiple `Depends()` has undefined behaviour.
+# https://github.com/astral-sh/ruff/pull/15364#discussion_r1912551710
+@app.get("/things/{thing_id}")
+async def single(other: Annotated[str, Depends(takes_thing_id)]): ...
+@app.get("/things/{thing_id}")
+async def double(other: Annotated[str, Depends(something_else), Depends(takes_thing_id)]): ...
+@app.get("/things/{thing_id}")
+async def double(other: Annotated[str, Depends(takes_thing_id), Depends(something_else)]): ...
+@app.get("/things/{thing_id}")
+async def default(other: str = Depends(takes_thing_id)): ...
+@app.get("/things/{thing_id}")
+async def unknown_1(other: str = Depends(unknown_unresolved)): ...
+@app.get("/things/{thing_id}")
+async def unknown_2(other: str = Depends(unknown_not_function)): ...
+@app.get("/things/{thing_id}")
+async def unknown_3(other: str = Depends(unknown_imported)): ...
