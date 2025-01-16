153e1eab04f3e24336986783211ce8f26f9dc5ac
dylwil3 <dylwil3@gmail.com>
2025-01-16 13:58:19 -0600

    more test cases

--- crates/ruff_linter/resources/test/fixtures/pylint/redefined_slots_in_subclass.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/redefined_slots_in_subclass.py
@@ -4,3 +4,14 @@ class Base:
 
 class Subclass(Base):
     __slots__ = ("a", "d")  # [redefined-slots-in-subclass]
+
+class Grandparent:
+    __slots__ = ("a", "b")
+
+
+class Parent(Grandparent):
+    pass
+
+
+class Child(Parent):
+    __slots__ = ("c", "a")
