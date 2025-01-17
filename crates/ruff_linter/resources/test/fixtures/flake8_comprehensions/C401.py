1ba8e6187564722f9b3d2b7b63d4ce954a72a228
wooly18 <97658409+wooly18@users.noreply.github.com>
2025-01-18 01:08:22 +0800

    [`flake8-comprehensions`] strip parentheses around generators in `unnecessary-generator-set` (`C401`) (#15553)
    <!--
    Thank you for contributing to Ruff! To help us out with reviewing,
    please consider the following:
    
    - Does this pull request include a summary of the change? (See below.)
    - Does this pull request include a descriptive title?
    - Does this pull request include references to any relevant issues?
    -->
    
    ## Summary
    
    Fixes parentheses not being stripped in C401. Pretty much the same as
    #11607 which fixed it for C400.
    
    ## Test Plan
    `cargo nextest run`

--- crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C401.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C401.py
@@ -1,4 +1,4 @@
-# Cannot conbime with C416. Should use set comprehension here.
+# Cannot combine with C416. Should use set comprehension here.
 even_nums = set(2 * x for x in range(3))
 odd_nums = set(
     2 * x + 1 for x in range(3)
@@ -21,6 +21,10 @@ print(f"Hello {set(a for a in range(3))} World")
 print(f"{set(a for a in 'abc') - set(a for a in 'ab')}")
 print(f"{ set(a for a in 'abc') - set(a for a in 'ab') }")
 
+# Strip parentheses from inner generators.
+set((2 * x for x in range(3)))
+set(((2 * x for x in range(3))))
+set((((2 * x for x in range(3)))))
 
 # Not built-in set.
 def set(*args, **kwargs):
