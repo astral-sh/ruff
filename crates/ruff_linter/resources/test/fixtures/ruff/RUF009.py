4cfa3555199f9377b29c490cbb703beb6b1eb3b4
InSync <insyncwithfoo@gmail.com>
2025-01-20 21:44:47 +0700

    [`ruff`] Exempt `NewType` calls where the original type is immutable (`RUF009`) (#15588)
    ## Summary
    
    Resolves #6447.
    
    ## Test Plan
    
    `cargo nextest run` and `cargo insta test`.

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF009.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF009.py
@@ -69,3 +69,31 @@ class IntConversionDescriptor:
 @dataclass
 class InventoryItem:
     quantity_on_hand: IntConversionDescriptor = IntConversionDescriptor(default=100)
+
+
+# Regression tests for:
+# https://github.com/astral-sh/ruff/issues/6447
+from typing import NewType
+
+ListOfStrings = NewType("ListOfStrs", list[str])
+StringsToInts = NewType("IntsToStrings", dict[str, int])
+
+SpecialString = NewType(name="SpecialString", tp=str)
+NegativeInteger = NewType("NegInt", tp=int)
+
+Invalid1 = NewType(*Foo)
+Invalid2 = NewType("Invalid2", name=Foo)
+Invalid3 = NewType("Invalid3", name=Foo, lorem="ipsum")
+
+@dataclass
+class DataclassWithNewTypeFields:
+    # Errors
+    a: ListOfStrings = ListOfStrings([])
+    b: StringsToInts = StringsToInts()
+    c: Invalid1 = Invalid1()
+    d: Invalid2 = Invalid2()
+    e: Invalid3 = Invalid3()
+
+    # No errors
+    e: SpecialString = SpecialString("Lorem ipsum")
+    f: NegativeInteger = NegativeInteger(-110)
