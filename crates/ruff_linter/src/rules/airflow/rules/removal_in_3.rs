fbfb23ba142b81a17ec0d91c02ef203c2479c21b
Wei Lee <weilee.rx@gmail.com>
2025-01-24 18:09:20 +0800

    feat(AIR302): add is_execute_method_inherits_from_airflow_operator for checking removed context key in execute method

--- crates/ruff_linter/src/rules/airflow/rules/removal_in_3.rs
+++ crates/ruff_linter/src/rules/airflow/rules/removal_in_3.rs
@@ -154,7 +154,9 @@ const REMOVED_CONTEXT_KEYS: [&str; 12] = [
 ///     pass
 /// ```
 fn check_function_parameters(checker: &mut Checker, function_def: &StmtFunctionDef) {
-    if !is_airflow_task(function_def, checker.semantic()) {
+    if !is_airflow_task(function_def, checker.semantic())
+        && !is_execute_method_inherits_from_airflow_operator(function_def, checker.semantic())
+    {
         return;
     }
 
@@ -1075,4 +1077,40 @@ fn is_airflow_task(function_def: &StmtFunctionDef, semantic: &SemanticModel) -> 
                 matches!(qualified_name.segments(), ["airflow", "decorators", "task"])
             })
     })
+}
+
+/// Check it's "execute" method inherits from Airflow base operator
+///
+/// For example:
+///
+/// ```python
+/// from airflow.models.baseoperator import BaseOperator
+///
+/// class CustomOperator(BaseOperator):
+///     def execute(self):
+///         pass
+/// ```
+fn is_execute_method_inherits_from_airflow_operator(
+    function_def: &StmtFunctionDef,
+    semantic: &SemanticModel,
+) -> bool {
+    if function_def.name.as_str() != "execute" {
+        return false;
+    }
+
+    let ScopeKind::Class(class_def) = semantic.current_scope().kind else {
+        return false;
+    };
+
+    if !class_def.bases().iter().any(|class_base| {
+        semantic
+            .resolve_qualified_name(class_base)
+            .is_some_and(|qualified_name| {
+                matches!(qualified_name.segments(), ["airflow", .., "BaseOperator"])
+            })
+    }) {
+        return false;
+    };
+
+    true
 }
