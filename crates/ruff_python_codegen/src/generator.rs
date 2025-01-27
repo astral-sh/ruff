c2ad78a5faeb18dc57f4d7e785e9ea3894f62505
Brent Westbrook <brentrwestbrook@gmail.com>
2025-01-27 16:13:31 -0500

    accept new bytes round trip

--- crates/ruff_python_codegen/src/generator.rs
+++ crates/ruff_python_codegen/src/generator.rs
@@ -1743,7 +1743,7 @@ class Foo:
         assert_round_trip!(r"'hello'");
         assert_round_trip!(r"u'hello'");
         assert_round_trip!(r"r'hello'");
-        assert_eq!(round_trip(r"b'hello'"), r#"b"hello""#);
+        assert_round_trip!(r"b'hello'");
         assert_eq!(round_trip(r#"("abc" "def" "ghi")"#), r#""abc" "def" "ghi""#);
         assert_eq!(round_trip(r#""he\"llo""#), r#"'he"llo'"#);
         assert_eq!(round_trip(r#"f"abc{'def'}{1}""#), r#"f"abc{'def'}{1}""#);
