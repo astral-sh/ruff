bbccf23cef10319ee6279008f9a7173187b4a103
Brent Westbrook <brentrwestbrook@gmail.com>
2025-01-30 09:51:23 -0500

    add a test with type_params

--- crates/ruff_linter/resources/test/fixtures/pyflakes/F401_34.py
+++ crates/ruff_linter/resources/test/fixtures/pyflakes/F401_34.py
@@ -2,10 +2,19 @@
 
 from typing import Union
 
-from typing_extensions import TypeAliasType
+from typing_extensions import TypeAliasType, TypeVar
 
 
 Json = TypeAliasType(
     'Json',
     'Union[dict[str, Json], list[Json], str, int, float, bool, None]',
+)
+
+
+T = TypeVar("T")
+V = TypeVar("V")
+Json = TypeAliasType(
+    'Json',
+    'Union[dict[str, Json], list[Json], str, int, float, bool, T, V, None]',
+    type_params=(T, V)
 )
