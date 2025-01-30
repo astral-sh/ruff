358d581abf1c2594ec687a4c3b3d8d08b66cd717
Brent Westbrook <brentrwestbrook@gmail.com>
2025-01-30 09:42:44 -0500

    add test case

---
+++ crates/ruff_linter/resources/test/fixtures/pyflakes/F401_34.py
@@ -0,0 +1,11 @@
+"""Regression test for https://github.com/astral-sh/ruff/issues/15812"""
+
+from typing import Union
+
+from typing_extensions import TypeAliasType
+
+
+Json = TypeAliasType(
+    'Json',
+    'Union[dict[str, Json], list[Json], str, int, float, bool, None]',
+)
