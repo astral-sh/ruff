20b309ea62ec293dbf14ab77de013e090d8a38f7
dylwil3 <dylwil3@gmail.com>
2025-01-30 16:09:53 -0600

    add fixtures

--- crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C411.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_comprehensions/C411.py
@@ -1,2 +1,8 @@
 x = [1, 2, 3]
 list([i for i in x])
+
+# Skip when too many positional arguments
+# or keyword argument present.
+# See https://github.com/astral-sh/ruff/issues/15810
+list([x for x in "XYZ"],[])
+list([x for x in "XYZ"],foo=[])
