93bcdd3e189a35bf972f448acab8d509e4922664
Brent Westbrook <brentrwestbrook@gmail.com>
2025-01-30 12:07:16 -0500

    combine tests into F401_34
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- crates/ruff_linter/resources/test/fixtures/pyflakes/F401_34.py
+++ crates/ruff_linter/resources/test/fixtures/pyflakes/F401_34.py
@@ -1,10 +1,67 @@
-"""Regression test for https://github.com/astral-sh/ruff/issues/15812"""
+"""Regression tests for https://github.com/astral-sh/ruff/issues/15812"""
 
-from typing import Union
 
-from typing_extensions import TypeAliasType
+def f():
+    from typing import Union
 
-Json = TypeAliasType(
-    "Json",
-    "Union[dict[str, Json], list[Json], str, int, float, bool, None]",
-)
+    from typing_extensions import TypeAliasType
+
+    Json = TypeAliasType(
+        "Json",
+        "Union[dict[str, Json], list[Json], str, int, float, bool, None]",
+    )
+
+
+def f():
+    from typing import Union
+
+    from typing_extensions import TypeAliasType, TypeVar
+
+    T = TypeVar("T")
+    V = TypeVar("V")
+    Json = TypeAliasType(
+        "Json",
+        "Union[dict[str, Json], list[Json], str, int, float, bool, T, V, None]",
+        type_params=(T, V),
+    )
+
+
+def f():
+    from typing import Union
+
+    from typing_extensions import TypeAliasType
+
+    Json = TypeAliasType(
+        value="Union[dict[str, Json], list[Json], str, int, float, bool, None]",
+        name="Json",
+    )
+
+
+# strictly speaking it's a false positive to emit F401 for both of these, but
+# we can't really be expected to understand that the strings here are type
+# expressions (and type checkers probably wouldn't understand them as type
+# expressions either!)
+def f():
+    from typing import Union
+
+    from typing_extensions import TypeAliasType
+
+    args = [
+        "Json",
+        "Union[dict[str, Json], list[Json], str, int, float, bool, None]",
+    ]
+
+    Json = TypeAliasType(*args)
+
+
+def f():
+    from typing import Union
+
+    from typing_extensions import TypeAliasType
+
+    kwargs = {
+        "name": "Json",
+        "value": "Union[dict[str, Json], list[Json], str, int, float, bool, None]",
+    }
+
+    Json = TypeAliasType(**kwargs)
