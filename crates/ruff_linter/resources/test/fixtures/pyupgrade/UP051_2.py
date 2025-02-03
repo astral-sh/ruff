0ff60ec0ea9f210d62bcfa11c9a2d321fd8fa5d2
Brent Westbrook <brentrwestbrook@gmail.com>
2025-02-03 17:43:01 -0500

    add inner annotations, constraint, and paramspec

--- crates/ruff_linter/resources/test/fixtures/pyupgrade/UP051_2.py
+++ crates/ruff_linter/resources/test/fixtures/pyupgrade/UP051_2.py
@@ -1,5 +1,18 @@
-# bounds
-class Foo[_T: str]: ...
+# bound
+class Foo[_T: str]:
+    var: _T
+
+
+# constraint
+class Foo[_T: (str, bytes)]:
+    var: _T
+
 
 # tuple
-class Foo[*_Ts]: ...
+class Foo[*_Ts]:
+    var: tuple[*_Ts]
+
+
+# paramspec
+class C[**_P]:
+    var: _P
