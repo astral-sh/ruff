a199aecd67953cea7ae4864c35470655ec7638d1
dylwil3 <dylwil3@gmail.com>
2025-02-03 19:21:16 -0600

    add test fixtures

--- crates/ruff_linter/src/rules/ruff/mod.rs
+++ crates/ruff_linter/src/rules/ruff/mod.rs
@@ -30,6 +30,7 @@ mod tests {
     #[test_case(Rule::ZipInsteadOfPairwise, Path::new("RUF007.py"))]
     #[test_case(Rule::MutableDataclassDefault, Path::new("RUF008.py"))]
     #[test_case(Rule::MutableDataclassDefault, Path::new("RUF008_attrs.py"))]
+    #[test_case(Rule::MutableDataclassDefault, Path::new("RUF008_deferred.py"))]
     #[test_case(Rule::FunctionCallInDataclassDefaultArgument, Path::new("RUF009.py"))]
     #[test_case(
         Rule::FunctionCallInDataclassDefaultArgument,
@@ -41,6 +42,7 @@ mod tests {
     )]
     #[test_case(Rule::ExplicitFStringTypeConversion, Path::new("RUF010.py"))]
     #[test_case(Rule::MutableClassDefault, Path::new("RUF012.py"))]
+    #[test_case(Rule::MutableClassDefault, Path::new("RUF012_deferred.py"))]
     #[test_case(Rule::ImplicitOptional, Path::new("RUF013_0.py"))]
     #[test_case(Rule::ImplicitOptional, Path::new("RUF013_1.py"))]
     #[test_case(Rule::ImplicitOptional, Path::new("RUF013_2.py"))]
