7db5a924af51cba6fe49ef88cad43d325d8943e2
InSync <insyncwithfoo@gmail.com>
2025-02-07 15:58:05 +0700

    [`flake8-comprehensions`] Detect overshadowed `list`/`set`/`dict`, ignore variadics and named expressions (`C417`) (#15955)
    ## Summary
    
    Part of #15809 and #15876.
    
    This change brings several bugfixes:
    
    * The nested `map()` call in `list(map(lambda x: x, []))` where `list`
    is overshadowed is now correctly reported.
    * The call will no longer reported if:
    	* Any arguments given to `map()` are variadic.
    	* Any of the iterables contain a named expression.
    
    ## Test Plan
    
    `cargo nextest run` and `cargo insta test`.
    
    ---------
    
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_linter/src/rules/flake8_comprehensions/fixes.rs
+++ crates/ruff_linter/src/rules/flake8_comprehensions/fixes.rs
@@ -11,7 +11,7 @@ use libcst_native::{
 };
 
 use ruff_diagnostics::{Edit, Fix};
-use ruff_python_ast::{self as ast, Expr};
+use ruff_python_ast::{self as ast, Expr, ExprCall};
 use ruff_python_codegen::Stylist;
 use ruff_python_semantic::SemanticModel;
 use ruff_text_size::{Ranged, TextRange};
@@ -649,13 +649,13 @@ pub(crate) fn fix_unnecessary_comprehension(
 
 /// (C417) Convert `map(lambda x: x * 2, bar)` to `(x * 2 for x in bar)`.
 pub(crate) fn fix_unnecessary_map(
-    expr: &Expr,
+    call_ast_node: &ExprCall,
     parent: Option<&Expr>,
     object_type: ObjectType,
     locator: &Locator,
     stylist: &Stylist,
 ) -> Result<Edit> {
-    let module_text = locator.slice(expr);
+    let module_text = locator.slice(call_ast_node);
     let mut tree = match_expression(module_text)?;
     let call = match_call_mut(&mut tree)?;
 
@@ -800,7 +800,7 @@ pub(crate) fn fix_unnecessary_map(
         }
     }
 
-    Ok(Edit::range_replacement(content, expr.range()))
+    Ok(Edit::range_replacement(content, call_ast_node.range()))
 }
 
 /// (C419) Convert `[i for i in a]` into `i for i in a`
