c9a0f869a1e05f92bb70fee154a5aca81e376a2b
Brent Westbrook <brentrwestbrook@gmail.com>
2025-02-07 09:06:02 -0500

    more test cases, second failing

--- crates/ruff_linter/resources/test/fixtures/pydocstyle/D417.py
+++ crates/ruff_linter/resources/test/fixtures/pydocstyle/D417.py
@@ -193,3 +193,28 @@ def send(payload: str, attributes: dict[str, Any]) -> None:
         attributes:
             Additional attributes to be sent alongside the message.
     """
+
+
+# undocumented argument with the same name as a section
+def should_fail(payload, Args):
+    """
+    Send a message.
+
+    Args:
+        payload:
+            The message payload.
+    """
+
+
+# documented argument with the same name as a section
+def should_not_fail(payload, Args):
+    """
+    Send a message.
+
+    Args:
+        payload:
+            The message payload.
+
+        Args:
+            The other arguments.
+    """
