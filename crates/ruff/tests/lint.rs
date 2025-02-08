77217ded8380e5c3ede0704cd1a470b03159f3dd
Brent Westbrook <brentrwestbrook@gmail.com>
2025-02-08 12:22:37 -0500

    separate a005_module_shadowing integration tests

--- crates/ruff/tests/lint.rs
+++ crates/ruff/tests/lint.rs
@@ -2272,38 +2272,36 @@ class Foo[_T, __T]:
     );
 }
 
-#[test]
-fn a005_module_shadowing_strict() -> Result<()> {
+/// construct a directory tree with this structure:
+/// .
+/// ├── abc
+/// │   └── __init__.py
+/// ├── collections
+/// │   ├── __init__.py
+/// │   ├── abc
+/// │   │   └── __init__.py
+/// │   └── foobar
+/// │       └── __init__.py
+/// ├── foobar
+/// │   ├── __init__.py
+/// │   ├── abc
+/// │   │   └── __init__.py
+/// │   └── collections
+/// │       ├── __init__.py
+/// │       ├── abc
+/// │       │   └── __init__.py
+/// │       └── foobar
+/// │           └── __init__.py
+/// ├── ruff.toml
+/// └── urlparse
+///     └── __init__.py
+fn create_a005_module_structure(tempdir: &TempDir) -> Result<()> {
     fn create_module(path: &Path) -> Result<()> {
         fs::create_dir(path)?;
         fs::File::create(path.join("__init__.py"))?;
         Ok(())
     }
-    // construct a directory tree with this structure:
-    // .
-    // ├── abc
-    // │   └── __init__.py
-    // ├── collections
-    // │   ├── __init__.py
-    // │   ├── abc
-    // │   │   └── __init__.py
-    // │   └── foobar
-    // │       └── __init__.py
-    // ├── foobar
-    // │   ├── __init__.py
-    // │   ├── abc
-    // │   │   └── __init__.py
-    // │   └── collections
-    // │       ├── __init__.py
-    // │       ├── abc
-    // │       │   └── __init__.py
-    // │       └── foobar
-    // │           └── __init__.py
-    // ├── ruff.toml
-    // └── urlparse
-    //     └── __init__.py
 
-    let tempdir = TempDir::new()?;
     let foobar = tempdir.path().join("foobar");
     create_module(&foobar)?;
     for base in [&tempdir.path().into(), &foobar] {
@@ -2316,6 +2314,82 @@ fn a005_module_shadowing_strict() -> Result<()> {
     create_module(&tempdir.path().join("urlparse"))?;
     // also create a ruff.toml to mark the project root
     fs::File::create(tempdir.path().join("ruff.toml"))?;
+
+    Ok(())
+}
+
+/// Test A005 with `builtins-strict-checking = true`
+#[test]
+fn a005_module_shadowing_strict() -> Result<()> {
+    let tempdir = TempDir::new()?;
+    create_a005_module_structure(&tempdir)?;
+
+    insta::with_settings!({
+        filters => vec![(r"\\", "/")]
+    }, {
+        assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
+            .args(STDIN_BASE_OPTIONS)
+            .arg("--config")
+            .arg(r#"lint.flake8-builtins.builtins-strict-checking = true"#)
+            .args(["--select", "A005"])
+            .current_dir(tempdir.path()),
+            @r"
+        success: false
+        exit_code: 1
+        ----- stdout -----
+        abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
+        collections/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
+        collections/abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
+        foobar/abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
+        foobar/collections/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
+        foobar/collections/abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
+        Found 6 errors.
+
+        ----- stderr -----
+        ");
+    });
+
+    Ok(())
+}
+
+/// Test A005 with `builtins-strict-checking = false`
+#[test]
+fn a005_module_shadowing_non_strict() -> Result<()> {
+    let tempdir = TempDir::new()?;
+    create_a005_module_structure(&tempdir)?;
+
+    insta::with_settings!({
+        filters => vec![(r"\\", "/")]
+    }, {
+        assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
+            .args(STDIN_BASE_OPTIONS)
+            .arg("--config")
+            .arg(r#"lint.flake8-builtins.builtins-strict-checking = false"#)
+            .args(["--select", "A005"])
+            .current_dir(tempdir.path()),
+            @r"
+        success: false
+        exit_code: 1
+        ----- stdout -----
+        abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
+        collections/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
+        Found 2 errors.
+
+        ----- stderr -----
+        ");
+
+    });
+
+    Ok(())
+}
+
+/// Test A005 with `builtins-strict-checking` unset
+/// TODO(brent) This should currently match the strict version, but after the next minor
+/// release it will match the non-strict version directly above
+#[test]
+fn a005_module_shadowing_strict_default() -> Result<()> {
+    let tempdir = TempDir::new()?;
+    create_a005_module_structure(&tempdir)?;
 
     insta::with_settings!({
         filters => vec![(r"\\", "/")]
@@ -2335,48 +2409,6 @@ fn a005_module_shadowing_strict() -> Result<()> {
         foobar/collections/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
         foobar/collections/abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
         Found 6 errors.
-
-        ----- stderr -----
-        ");
-
-        assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
-            .args(STDIN_BASE_OPTIONS)
-            .arg("--config")
-            .arg(r#"lint.flake8-builtins.builtins-strict-checking = false"#)
-            .args(["--select", "A005"])
-            .current_dir(tempdir.path()),
-            @r"
-        success: false
-        exit_code: 1
-        ----- stdout -----
-        abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
-        collections/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        collections/abc/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        collections/foobar/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        Found 4 errors.
-
-        ----- stderr -----
-        ");
-
-        // TODO(brent) Default should currently match the strict version, but after the next minor
-        // release it will match the non-strict version directly above
-        assert_cmd_snapshot!(Command::new(get_cargo_bin(BIN_NAME))
-            .args(STDIN_BASE_OPTIONS)
-            .args(["--select", "A005"])
-            .current_dir(tempdir.path()),
-            @r"
-        success: false
-        exit_code: 1
-        ----- stdout -----
-        abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
-        collections/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        collections/abc/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        collections/foobar/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        foobar/abc/__init__.py:1:1: A005 Module `abc` shadows a Python standard-library module
-        foobar/collections/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        foobar/collections/abc/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        foobar/collections/foobar/__init__.py:1:1: A005 Module `collections` shadows a Python standard-library module
-        Found 8 errors.
 
         ----- stderr -----
         ");
