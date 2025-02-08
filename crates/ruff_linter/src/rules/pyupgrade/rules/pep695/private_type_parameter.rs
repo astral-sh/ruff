b664a9f03fce3ec94ab53dc75c4aab6d16d00642
Alex Waygood <alex.waygood@gmail.com>
2025-02-08 11:18:31 +0000

    .

--- crates/ruff_linter/src/rules/pyupgrade/rules/pep695/private_type_parameter.rs
+++ crates/ruff_linter/src/rules/pyupgrade/rules/pep695/private_type_parameter.rs
@@ -1,13 +1,14 @@
-use crate::{
-    checkers::ast::Checker,
-    renamer::{Renamer, ShadowedKind},
-};
 use ruff_diagnostics::{Applicability, Diagnostic, Fix, FixAvailability, Violation};
 use ruff_macros::{derive_message_formats, ViolationMetadata};
 use ruff_python_ast::Stmt;
 use ruff_python_semantic::Binding;
 use ruff_python_stdlib::identifiers::is_identifier;
 use ruff_text_size::Ranged;
+
+use crate::{
+    checkers::ast::Checker,
+    renamer::{Renamer, ShadowedKind},
+};
 
 /// ## What it does
 ///
