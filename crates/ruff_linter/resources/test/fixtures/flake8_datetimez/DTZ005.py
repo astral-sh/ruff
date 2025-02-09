f51460cf58a2a56b17d1beeb6788a92358f36ade
InSyncWithFoo <insyncwithfoo@gmail.com>
2025-02-09 00:40:19 +0000

    [`flake8-datetime`] Ignore `.replace()` calls while looking for `.astimezone`

--- crates/ruff_linter/resources/test/fixtures/flake8_datetimez/DTZ005.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_datetimez/DTZ005.py
@@ -19,3 +19,23 @@ datetime.now()
 
 # uses `astimezone` method
 datetime.now().astimezone()
+datetime.now().astimezone
+
+
+# https://github.com/astral-sh/ruff/issues/15998
+
+## Errors
+datetime.now().replace.astimezone()
+datetime.now().replace[0].astimezone()
+datetime.now()().astimezone()
+datetime.now().replace(datetime.now()).astimezone()
+
+datetime.now().replace(0).replace(
+    datetime.now().replace()
+).astimezone
+
+## No errors
+datetime.now().replace(microsecond=0).astimezone()
+datetime.now().replace(0).astimezone()
+datetime.now().replace(0).astimezone
+datetime.now().replace(0).replace(1).astimezone
--- crates/ruff_linter/src/rules/flake8_datetimez/rules/helpers.rs
+++ crates/ruff_linter/src/rules/flake8_datetimez/rules/helpers.rs
@@ -1,4 +1,4 @@
-use ruff_python_ast::{Expr, ExprAttribute};
+use ruff_python_ast::{AnyNodeRef, Expr, ExprAttribute, ExprCall};
 
 use crate::checkers::ast::Checker;
 
@@ -8,10 +8,45 @@ pub(super) enum DatetimeModuleAntipattern {
     NonePassedToTzArgument,
 }
 
-/// Check if the parent expression is a call to `astimezone`.
+/// Check if the current expression is followed by
+/// a chain of `.replace()` calls followed by `.astimezone`.
+///
 /// This assumes that the current expression is a `datetime.datetime` object.
 pub(super) fn parent_expr_is_astimezone(checker: &Checker) -> bool {
-    checker.semantic().current_expression_parent().is_some_and(|parent| {
-        matches!(parent, Expr::Attribute(ExprAttribute { attr, .. }) if attr.as_str() == "astimezone")
-    })
+    let semantic = checker.semantic();
+    let mut last = None;
+
+    for (index, expr) in semantic.current_expressions().enumerate() {
+        if index == 0 {
+            // datetime.now(...).replace(...).astimezone
+            // ^^^^^^^^^^^^^^^^^
+            continue;
+        }
+
+        if index % 2 == 1 {
+            // datetime.now(...).replace(...).astimezone
+            //                   ^^^^^^^      ^^^^^^^^^^
+            let Expr::Attribute(ExprAttribute { attr, .. }) = expr else {
+                return false;
+            };
+
+            match attr.as_str() {
+                "replace" => last = Some(AnyNodeRef::from(expr)),
+                "astimezone" => return true,
+                _ => return false,
+            }
+        } else {
+            // datetime.now(...).replace(...).astimezone
+            //                          ^^^^^
+            let Expr::Call(ExprCall { func, .. }) = expr else {
+                return false;
+            };
+
+            if !last.is_some_and(|it| it.ptr_eq(AnyNodeRef::from(&**func))) {
+                return false;
+            }
+        }
+    }
+
+    false
 }
