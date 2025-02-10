f367aa8367c96764cc543c73784d531b7439cc4b
InSync <insyncwithfoo@gmail.com>
2025-02-10 07:23:48 +0700

    [`ruff`] Indented form feeds (`RUF054`) (#16049)
    ## Summary
    
    Resolves #12321.
    
    The physical-line-based `RUF054` checks for form feed characters that
    are preceded by only tabs and spaces, but not any other characters,
    including form feeds.
    
    ## Test Plan
    
    `cargo nextest run` and `cargo insta test`.

---
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF054.py
@@ -0,0 +1,32 @@
+############# Warning ############
+# This file contains form feeds. #
+############# Warning ############
+
+
+# Errors
+
+ 
+
+		
+
+def _():
+		pass
+
+if False:
+    print('F')
+    print('T')
+
+
+# No errors
+
+
+
+
+
+  
+
+def _():
+    pass
+
+def f():
+	pass 
