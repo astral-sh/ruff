9a357636500801bfdcd9bb3f13d24d0aca5b44c0
Brent Westbrook <brentrwestbrook@gmail.com>
2025-02-13 18:11:22 -0500

    update docs

--- crates/ruff_python_parser/src/python_version.rs
+++ crates/ruff_python_parser/src/python_version.rs
@@ -9,6 +9,10 @@ mod pyversion {
 
     use pep440_rs::{Operator, Version as Pep440Version, Version, VersionSpecifiers};
 
+    /// Representation of supported Python versions.
+    ///
+    /// Unlike [`PythonVersion`](super::PythonVersion), this is deserialized from versions like
+    /// `py39` rather than dotted versions like `3.9`.
     #[derive(Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq, Default, EnumIter)]
     #[cfg_attr(feature = "ruff_macros", derive(ruff_macros::CacheKey))]
     #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
@@ -111,8 +115,8 @@ mod pyversion {
 
 /// Representation of a Python version.
 ///
-/// Unlike the `TargetVersion` enums in the CLI crates,
-/// this does not necessarily represent a Python version that we actually support.
+/// Unlike the [`PyVersion`], this does not necessarily represent a Python version that we actually
+/// support.
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[cfg_attr(feature = "wasm-bindgen", wasm_bindgen::prelude::wasm_bindgen)]
 pub struct PythonVersion {
