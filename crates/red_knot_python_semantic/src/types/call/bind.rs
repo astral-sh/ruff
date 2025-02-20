8adb5541543d330f8add29ba256bea61cf64039b
Andrew Gallant <andrew@astral.sh>
2025-02-20 08:04:40 -0500

    red_knot_python_semantic: avoid adding `callable_ty` to `CallBindingError`
    This is a small tweak to avoid adding the callable `Type` on the error
    value itself. Namely, it's always available regardless of the error, and
    it's easy to pass it down explicitly to the diagnostic generating code.
    
    It's likely that the other `CallBindingError` variants will also want
    the callable `Type` to improve diagnostics too. This way, we don't have
    to duplicate the `Type` on each variant. It's just available to all of
    them.
    
    Ref https://github.com/astral-sh/ruff/pull/16239#discussion_r1962352646

--- crates/red_knot_python_semantic/src/types/call/bind.rs
+++ crates/red_knot_python_semantic/src/types/call/bind.rs
@@ -78,7 +78,6 @@ pub(crate) fn bind_call<'db>(
         if let Some(expected_ty) = parameter.annotated_type() {
             if !argument_ty.is_assignable_to(db, expected_ty) {
                 errors.push(CallBindingError::InvalidArgumentType {
-                    callable_ty,
                     parameter: ParameterContext::new(parameter, index, positional),
                     argument_index: get_argument_index(argument_index, num_synthetic_args),
                     expected_ty,
@@ -205,7 +204,7 @@ impl<'db> CallBinding<'db> {
     pub(crate) fn report_diagnostics(&self, context: &InferContext<'db>, node: ast::AnyNodeRef) {
         let callable_name = self.callable_name(context.db());
         for error in &self.errors {
-            error.report_diagnostic(context, node, callable_name);
+            error.report_diagnostic(context, node, self.callable_ty, callable_name);
         }
     }
 
@@ -272,7 +271,6 @@ pub(crate) enum CallBindingError<'db> {
     /// The type of an argument is not assignable to the annotated type of its corresponding
     /// parameter.
     InvalidArgumentType {
-        callable_ty: Type<'db>,
         parameter: ParameterContext,
         argument_index: Option<usize>,
         expected_ty: Type<'db>,
@@ -303,11 +301,11 @@ impl<'db> CallBindingError<'db> {
         &self,
         context: &InferContext<'db>,
         node: ast::AnyNodeRef,
+        callable_ty: Type<'db>,
         callable_name: Option<&str>,
     ) {
         match self {
             Self::InvalidArgumentType {
-                callable_ty,
                 parameter,
                 argument_index,
                 expected_ty,
