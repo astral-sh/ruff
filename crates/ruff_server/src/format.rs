34345de972e81152f5d8856b79bb4234b488900d
Brent Westbrook <brentrwestbrook@gmail.com>
2025-02-21 09:27:58 -0500

    test script formatting in the server

--- crates/ruff_server/src/format.rs
+++ crates/ruff_server/src/format.rs
@@ -62,3 +62,146 @@ pub(crate) fn format_range(
         Err(err) => Err(err.into()),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use insta::assert_snapshot;
+    use ruff_linter::settings::types::{CompiledPerFileTargetVersionList, PerFileTargetVersion};
+    use ruff_python_ast::{PySourceType, PythonVersion};
+    use ruff_text_size::{TextRange, TextSize};
+    use ruff_workspace::FormatterSettings;
+
+    use crate::format::{format, format_range};
+    use crate::TextDocument;
+
+    #[test]
+    fn format_per_file_version() {
+        let document = TextDocument::new(r#"
+with open("a_really_long_foo") as foo, open("a_really_long_bar") as bar, open("a_really_long_baz") as baz:
+    pass
+"#.to_string(), 0);
+        let per_file_target_version =
+            CompiledPerFileTargetVersionList::resolve(vec![PerFileTargetVersion::new(
+                "test.py".to_string(),
+                PythonVersion::PY310,
+                Some(Path::new(".")),
+            )])
+            .unwrap();
+        let result = format(
+            &document,
+            PySourceType::Python,
+            &FormatterSettings {
+                unresolved_target_version: PythonVersion::PY38,
+                per_file_target_version,
+                ..Default::default()
+            },
+            Some(Path::new("test.py")),
+        )
+        .expect("Expected no errors when formatting")
+        .expect("Expected formatting changes");
+
+        assert_snapshot!(result, @r#"
+        with (
+            open("a_really_long_foo") as foo,
+            open("a_really_long_bar") as bar,
+            open("a_really_long_baz") as baz,
+        ):
+            pass
+        "#);
+
+        // same as above but without the per_file_target_version override
+        let result = format(
+            &document,
+            PySourceType::Python,
+            &FormatterSettings {
+                unresolved_target_version: PythonVersion::PY38,
+                ..Default::default()
+            },
+            Some(Path::new("test.py")),
+        )
+        .expect("Expected no errors when formatting")
+        .expect("Expected formatting changes");
+
+        assert_snapshot!(result, @r#"
+        with open("a_really_long_foo") as foo, open("a_really_long_bar") as bar, open(
+            "a_really_long_baz"
+        ) as baz:
+            pass
+        "#);
+    }
+
+    #[test]
+    fn format_per_file_version_range() -> anyhow::Result<()> {
+        // prepare a document with formatting changes before and after the intended range (the
+        // context manager)
+        let document = TextDocument::new(r#"
+def fn(x: str) -> Foo | Bar: return foobar(x)
+
+with open("a_really_long_foo") as foo, open("a_really_long_bar") as bar, open("a_really_long_baz") as baz:
+    pass
+
+sys.exit(
+1
+)
+"#.to_string(), 0);
+
+        let start = document.contents().find("with").unwrap();
+        let end = document.contents().find("pass").unwrap() + "pass".len();
+        let range = TextRange::new(TextSize::try_from(start)?, TextSize::try_from(end)?);
+
+        let per_file_target_version =
+            CompiledPerFileTargetVersionList::resolve(vec![PerFileTargetVersion::new(
+                "test.py".to_string(),
+                PythonVersion::PY310,
+                Some(Path::new(".")),
+            )])
+            .unwrap();
+        let result = format_range(
+            &document,
+            PySourceType::Python,
+            &FormatterSettings {
+                unresolved_target_version: PythonVersion::PY38,
+                per_file_target_version,
+                ..Default::default()
+            },
+            range,
+            Some(Path::new("test.py")),
+        )
+        .expect("Expected no errors when formatting")
+        .expect("Expected formatting changes");
+
+        assert_snapshot!(result.as_code(), @r#"
+        with (
+            open("a_really_long_foo") as foo,
+            open("a_really_long_bar") as bar,
+            open("a_really_long_baz") as baz,
+        ):
+            pass
+        "#);
+
+        // same as above but without the per_file_target_version override
+        let result = format_range(
+            &document,
+            PySourceType::Python,
+            &FormatterSettings {
+                unresolved_target_version: PythonVersion::PY38,
+                ..Default::default()
+            },
+            range,
+            Some(Path::new("test.py")),
+        )
+        .expect("Expected no errors when formatting")
+        .expect("Expected formatting changes");
+
+        assert_snapshot!(result.as_code(), @r#"
+        with open("a_really_long_foo") as foo, open("a_really_long_bar") as bar, open(
+            "a_really_long_baz"
+        ) as baz:
+            pass
+        "#);
+
+        Ok(())
+    }
+}
