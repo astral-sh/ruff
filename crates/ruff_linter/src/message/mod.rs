a2f70cd1fea629122ed65b777a1f8273744f4ba9
Brent Westbrook <brentrwestbrook@gmail.com>
2025-02-24 12:11:08 -0500

    remove unused match
    I also remove this in #16308, so even with additional variants we don't need the
    `match`. It was needed in an earlier draft of this prototype to emit a different
    syntax error as a ruff rule diagnostic

--- crates/ruff_linter/src/message/mod.rs
+++ crates/ruff_linter/src/message/mod.rs
@@ -16,7 +16,7 @@ pub use pylint::PylintEmitter;
 pub use rdjson::RdjsonEmitter;
 use ruff_diagnostics::{Diagnostic, DiagnosticKind, Fix};
 use ruff_notebook::NotebookIndex;
-use ruff_python_parser::{ParseError, SyntaxError, SyntaxErrorKind};
+use ruff_python_parser::{ParseError, SyntaxError};
 use ruff_source_file::{SourceFile, SourceLocation};
 use ruff_text_size::{Ranged, TextLen, TextRange, TextSize};
 pub use sarif::SarifEmitter;
@@ -123,13 +123,11 @@ impl Message {
 
     /// Create a [`Message`] from the given [`SyntaxError`].
     pub fn from_syntax_error(syntax_error: &SyntaxError, file: SourceFile) -> Message {
-        match syntax_error.kind {
-            SyntaxErrorKind::MatchBeforePy310 => Message::SyntaxError(SyntaxErrorMessage {
-                message: format!("SyntaxError: {syntax_error}"),
-                range: syntax_error.range,
-                file,
-            }),
-        }
+        Message::SyntaxError(SyntaxErrorMessage {
+            message: format!("SyntaxError: {syntax_error}"),
+            range: syntax_error.range,
+            file,
+        })
     }
 
     pub const fn as_diagnostic_message(&self) -> Option<&DiagnosticMessage> {
