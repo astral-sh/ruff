3f1c678d1463e9fb4e8808a6f3ade1f71c2f5055
Douglas Creager <dcreager@dcreager.net>
2025-02-24 14:03:51 -0500

    Implement all operations iteratively instead of recursively

--- crates/ruff_index/src/list.rs
+++ crates/ruff_index/src/list.rs
@@ -71,6 +71,30 @@ impl<'a, I: Idx, K, V> Iterator for ListIterator<'a, I, K, V> {
 #[derive(Debug, Eq, PartialEq)]
 pub struct ListBuilder<I, K, V> {
     storage: ListStorage<I, K, V>,
+
+    /// Scratch space that lets us implement our list operations iteratively instead of
+    /// recursively.
+    ///
+    /// The cons-list representation that we use for alists is very common in functional
+    /// programming, and the simplest implementations of most of the operations are defined
+    /// recursively on that data structure. However, they are not _tail_ recursive, which means
+    /// that the call stack grows linearly with the size of the input, which can be a problem for
+    /// large lists.
+    ///
+    /// You can often rework those recursive implementations into iterative ones using an
+    /// _accumulator_, but that comes at the cost of reversing the list. If we didn't care about
+    /// ordering, that wouldn't be a problem. Since we want our lists to be sorted, we can't rely
+    /// on that on its own.
+    ///
+    /// The next standard trick is to use an accumulator, and use a fix-up step at the end to
+    /// reverse the (reversed) result in the accumulator, restoring the correct order.
+    ///
+    /// So, that's what we do! However, as one last optimization, we don't build up alist cells in
+    /// our accumulator, since that would add wasteful cruft to our list storage. Instead, we use a
+    /// normal Vec as our accumulator, holding the key/value pairs that should be stitched onto the
+    /// beginning of whatever result list we are creating. For our fix-up step, we can consume a
+    /// Vec in reverse order by `pop`ping the elements off one by one.
+    scratch: Vec<(K, V)>,
 }
 
 impl<I: Idx, K, V> Default for ListBuilder<I, K, V> {
@@ -79,6 +103,7 @@ impl<I: Idx, K, V> Default for ListBuilder<I, K, V> {
             storage: ListStorage {
                 cells: IndexVec::default(),
             },
+            scratch: Vec::default(),
         }
     }
 }
@@ -108,57 +133,91 @@ impl<I: Idx, K: Clone + Ord, V: Clone> ListBuilder<I, K, V> {
     /// Inserts a new key/value pair into an existing list.  If there is already an entry with an
     /// equal key, its value is overwritten.
     pub fn insert(&mut self, list: Option<I>, key: K, value: V) -> Option<I> {
-        let Some(curr_id) = list else {
-            // First entry in the list
-            return self.add_cell(key, value, None);
+        // Iterate through the input list, looking for the position where the key should be
+        // inserted. We will need to create new list cells for any elements that appear before the
+        // new key. Stash those away in our scratch accumulator as we step through the input. The
+        // result of the loop is that "tail" of the result list, which we will stitch the new key
+        // (and any preceding keys) onto.
+        let mut curr = list;
+        let mut result = loop {
+            let Some(curr_id) = curr else {
+                // First entry in the list
+                break None;
+            };
+
+            let ListCell(curr_key, curr_value, tail) = &self.storage.cells[curr_id];
+            match key.cmp(curr_key) {
+                // We found an existing entry in the input list with the desired key, which we need
+                // to overwrite.
+                Ordering::Equal => break *tail,
+                // The input list does not already contain this key, and this is where we should
+                // add it. Break out of the loop and start the stitch-up process.
+                Ordering::Less => break curr,
+                // If this key is in the list, it's further along. We'll need to create a new cell
+                // for this entry into the result list, so add its contents to the scratch
+                // accumulator.
+                Ordering::Greater => {
+                    let new_key = curr_key.clone();
+                    let new_value = curr_value.clone();
+                    self.scratch.push((new_key, new_value));
+                    curr = *tail;
+                }
+            }
         };
 
-        let ListCell(curr_key, curr_value, tail) = &self.storage.cells[curr_id];
-        match key.cmp(curr_key) {
-            // Existing entry with equal key; overwrite the value
-            Ordering::Equal => self.add_cell(key, value, *tail),
-            // List does not already contain this key, and this is where we should add it.
-            Ordering::Less => self.add_cell(key, value, list),
-            // If this key is in the list, it's further along.
-            Ordering::Greater => {
-                let new_key = curr_key.clone();
-                let new_value = curr_value.clone();
-                let new_tail = self.insert(*tail, key, value);
-                self.add_cell(new_key, new_value, new_tail)
-            }
+        // We found where the new key should be added (either because the list didn't already
+        // contain it, or because it needs to be overwritten). Add the new key first, and then add
+        // new cells for all of the existing entries that are smaller than the new key.
+        result = self.add_cell(key, value, result);
+        while let Some((key, value)) = self.scratch.pop() {
+            result = self.add_cell(key, value, result);
         }
+        result
     }
 
     /// Inserts a new key/value pair into an existing list.  If there is already an entry with an
     /// equal key, the original value is retained, and the new value is thrown away.
     pub fn insert_if_needed(&mut self, list: Option<I>, key: K, value: V) -> Option<I> {
-        let Some(curr_id) = list else {
-            // First entry in the list
-            return self.add_cell(key, value, None);
+        // Iterate through the input list, looking for the position where the key should be
+        // inserted. We will need to create new list cells for any elements that appear before the
+        // new key. Stash those away in our scratch accumulator as we step through the input. The
+        // result of the loop is that "tail" of the result list, which we will stitch the new key
+        // (and any preceding keys) onto.
+        let mut curr = list;
+        let mut result = loop {
+            let Some(curr_id) = curr else {
+                // First entry in the list
+                break None;
+            };
+
+            let ListCell(curr_key, curr_value, tail) = &self.storage.cells[curr_id];
+            match key.cmp(curr_key) {
+                // We found an existing entry in the input list with the desired key, which means
+                // we can return the original input as-is.
+                Ordering::Equal => return list,
+                // The input list does not already contain this key, and this is where we should
+                // add it. Break out of the loop and start the stitch-up process.
+                Ordering::Less => break curr,
+                // If this key is in the list, it's further along. We'll need to create a new cell
+                // for this entry into the result list, so add its contents to the scratch
+                // accumulator.
+                Ordering::Greater => {
+                    let new_key = curr_key.clone();
+                    let new_value = curr_value.clone();
+                    self.scratch.push((new_key, new_value));
+                    curr = *tail;
+                }
+            }
         };
 
-        let ListCell(curr_key, _, tail) = &self.storage.cells[curr_id];
-        match key.cmp(curr_key) {
-            // Existing entry with equal key; retain the previous value.
-            Ordering::Equal => list,
-            // List does not already contain this key, and this is where we should add it.
-            Ordering::Less => self.add_cell(key, value, list),
-            // If this key is in the list, it's further along.
-            Ordering::Greater => {
-                let tail = *tail;
-                let new_tail = self.insert_if_needed(tail, key, value);
-                if new_tail == tail {
-                    // The element was already in the list, so we don't need to stitch up a new
-                    // one.
-                    return list;
-                }
-                // Reborrow to satisfy the borrow checker
-                let ListCell(curr_key, curr_value, _) = &self.storage.cells[curr_id];
-                let new_key = curr_key.clone();
-                let new_value = curr_value.clone();
-                self.add_cell(new_key, new_value, new_tail)
-            }
+        // The input did not already contain the key, and we found where it should be added. Add
+        // the new key first, and then add new cells for all of the existing entries that are
+        // smaller than the new key.
+        result = self.add_cell(key, value, result);
+        while let Some((key, value)) = self.scratch.pop() {
+            result = self.add_cell(key, value, result);
         }
+        result
     }
 }
 
@@ -166,31 +225,39 @@ impl<I: Idx, K: Clone + Ord, V: Clone> ListBuilder<I, K, V> {
     /// Returns the intersection of two lists. The result will contain an entry for any key that
     /// appears in both lists. The corresponding values will be combined using the `combine`
     /// function.
-    pub fn intersect<F>(&mut self, a: Option<I>, b: Option<I>, mut combine: F) -> Option<I>
+    pub fn intersect<F>(&mut self, mut a: Option<I>, mut b: Option<I>, mut combine: F) -> Option<I>
     where
         F: FnMut(&V, &V) -> V,
     {
-        let (Some(a_id), Some(b_id)) = (a, b) else {
-            // a ∩ ∅ == ∅
-            // ∅ ∩ a == ∅
-            return None;
-        };
-
-        let ListCell(a_key, a_value, a_tail) = &self.storage.cells[a_id];
-        let ListCell(b_key, b_value, b_tail) = &self.storage.cells[b_id];
-        match a_key.cmp(b_key) {
-            // Both lists contain this key; combine their values
-            Ordering::Equal => {
-                let new_key = a_key.clone();
-                let new_value = combine(a_value, b_value);
-                let new_tail = self.intersect(*a_tail, *b_tail, combine);
-                self.add_cell(new_key, new_value, new_tail)
+        // Zip through the lists, building up the keys/values of the new entries into our scratch
+        // vector. Continue until we run out of elements in either list. (Any remaining elements in
+        // the other list cannot possibly be in the intersection.)
+        while let (Some(a_id), Some(b_id)) = (a, b) {
+            let ListCell(a_key, a_value, a_tail) = &self.storage.cells[a_id];
+            let ListCell(b_key, b_value, b_tail) = &self.storage.cells[b_id];
+            match a_key.cmp(b_key) {
+                // Both lists contain this key; combine their values
+                Ordering::Equal => {
+                    let new_key = a_key.clone();
+                    let new_value = combine(a_value, b_value);
+                    self.scratch.push((new_key, new_value));
+                    a = *a_tail;
+                    b = *b_tail;
+                }
+                // a's key is only present in a, so it's not included in the result.
+                Ordering::Less => a = *a_tail,
+                // b's key is only present in b, so it's not included in the result.
+                Ordering::Greater => b = *b_tail,
             }
-            // a's key is only present in a, so it's not included in the result.
-            Ordering::Less => self.intersect(*a_tail, b, combine),
-            // b's key is only present in b, so it's not included in the result.
-            Ordering::Greater => self.intersect(a, *b_tail, combine),
         }
+
+        // Once the iteration loop terminates, we stitch the new entries back together into proper
+        // alist cells.
+        let mut result = None;
+        while let Some((key, value)) = self.scratch.pop() {
+            result = self.add_cell(key, value, result);
+        }
+        result
     }
 }
 
@@ -198,37 +265,54 @@ impl<I: Idx, K: Clone + Ord, V: Clone> ListBuilder<I, K, V> {
     /// Returns the union of two lists. The result will contain an entry for any key that appears
     /// in either list. For keys that appear in both lists, the corresponding values will be
     /// combined using the `combine` function.
-    pub fn union<F>(&mut self, a: Option<I>, b: Option<I>, mut combine: F) -> Option<I>
+    pub fn union<F>(&mut self, mut a: Option<I>, mut b: Option<I>, mut combine: F) -> Option<I>
     where
         F: FnMut(&V, &V) -> V,
     {
-        let (a_id, b_id) = match (a, b) {
-            (None, other) | (other, None) => return other,
-            (Some(a_id), Some(b_id)) => (a_id, b_id),
+        // Zip through the lists, building up the keys/values of the new entries into our scratch
+        // vector. Continue until we run out of elements in either list. (Any remaining elements in
+        // the other list cannot possibly be in the intersection.)
+        let mut result = loop {
+            let (a_id, b_id) = match (a, b) {
+                // If we run out of elements in one of the lists, the non-empty list will appear in
+                // the output unchanged.
+                (None, other) | (other, None) => break other,
+                (Some(a_id), Some(b_id)) => (a_id, b_id),
+            };
+
+            let ListCell(a_key, a_value, a_tail) = &self.storage.cells[a_id];
+            let ListCell(b_key, b_value, b_tail) = &self.storage.cells[b_id];
+            match a_key.cmp(b_key) {
+                // Both lists contain this key; combine their values
+                Ordering::Equal => {
+                    let new_key = a_key.clone();
+                    let new_value = combine(a_value, b_value);
+                    self.scratch.push((new_key, new_value));
+                    a = *a_tail;
+                    b = *b_tail;
+                }
+                // a's key is lower, so it goes into the result next
+                Ordering::Less => {
+                    let new_key = a_key.clone();
+                    let new_value = a_value.clone();
+                    self.scratch.push((new_key, new_value));
+                    a = *a_tail;
+                }
+                // b's key is lower, so it goes into the result next
+                Ordering::Greater => {
+                    let new_key = b_key.clone();
+                    let new_value = b_value.clone();
+                    self.scratch.push((new_key, new_value));
+                    b = *b_tail;
+                }
+            }
         };
 
-        let ListCell(a_key, a_value, a_tail) = &self.storage.cells[a_id];
-        let ListCell(b_key, b_value, b_tail) = &self.storage.cells[b_id];
-        let (new_key, new_value, new_tail) = match a_key.cmp(b_key) {
-            // Both lists contain this key; combine their values
-            Ordering::Equal => (
-                a_key.clone(),
-                combine(a_value, b_value),
-                self.union(*a_tail, *b_tail, combine),
-            ),
-            // a's key is lower, so it goes into the result next
-            Ordering::Less => (
-                a_key.clone(),
-                a_value.clone(),
-                self.union(*a_tail, b, combine),
-            ),
-            // b's key is lower, so it goes into the result next
-            Ordering::Greater => (
-                b_key.clone(),
-                b_value.clone(),
-                self.union(a, *b_tail, combine),
-            ),
-        };
-        self.add_cell(new_key, new_value, new_tail)
+        // Once the iteration loop terminates, we stitch the new entries back together into proper
+        // alist cells.
+        while let Some((key, value)) = self.scratch.pop() {
+            result = self.add_cell(key, value, result);
+        }
+        result
     }
 }
