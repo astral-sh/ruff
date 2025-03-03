0e865f52961dd10373221c57a0a51e4a31ec9f26
dylwil3 <dylwil3@gmail.com>
2025-03-03 17:04:17 -0600

    revert "ascii whitespace only in regex" and try again
    Turns out non-ascii whitespace is allowed anyway!
    
    This reverts commit 367d3a63fea6bebae30c7dd9dfe9dec0cbcc611e.

--- crates/ruff_linter/src/noqa.rs
+++ crates/ruff_linter/src/noqa.rs
@@ -311,12 +311,11 @@ pub(crate) struct ParsedNoqa<'a> {
 
 /// Marks the beginning of an in-line `noqa` directive
 static NOQA_DIRECTIVE_PREFIX: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r"#[\t\v\f\r ]*(?i)noqa").unwrap());
+    LazyLock::new(|| Regex::new(r"#\s*(?i)noqa").unwrap());
 
 /// Marks the beginning of a file-level exemption comment
-static FILE_EXEMPTION_PREFIX: LazyLock<Regex> = LazyLock::new(|| {
-    Regex::new(r"#[\t\v\f\r ]*(?:ruff|flake8)[\t\v\f\r ]*:[\t\v\f\r ]*(?i)noqa").unwrap()
-});
+static FILE_EXEMPTION_PREFIX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"#\s*(?:ruff|flake8)\s*:\s*(?i)noqa").unwrap());
 
 /// Parses in-line `noqa` comment, e.g. `# noqa: F401`
 pub(crate) fn parse_inline_noqa(
