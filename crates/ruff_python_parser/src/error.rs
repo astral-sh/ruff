5686e97a73e50b6be8beeff5a1be9d299e0ad0df
Brent Westbrook <brentrwestbrook@gmail.com>
2025-03-04 12:29:21 -0500

    document variant

--- crates/ruff_python_parser/src/error.rs
+++ crates/ruff_python_parser/src/error.rs
@@ -449,6 +449,34 @@ pub enum UnsupportedSyntaxErrorKind {
     Match,
     Walrus,
     ExceptStar,
+    /// Represents the use of a [type parameter list] before Python 3.12.
+    ///
+    /// ## Examples
+    ///
+    /// Before Python 3.12, generic parameters had to be declared separately using a class like
+    /// [`typing.TypeVar`], which could then be used in a function or class definition:
+    ///
+    /// ```python
+    /// from typing import Generic, TypeVar
+    ///
+    /// T = TypeVar("T")
+    ///
+    /// def f(t: T): ...
+    /// class C(Generic[T]): ...
+    /// ```
+    ///
+    /// [PEP 695], included in Python 3.12, introduced the new type parameter syntax, which allows
+    /// these to be written more compactly and without a separate type variable:
+    ///
+    /// ```python
+    /// def f[T](t: T): ...
+    /// class C[T]: ...
+    /// ```
+    ///
+    /// [type parameter list]:
+    /// https://docs.python.org/3/reference/compound_stmts.html#type-parameter-lists
+    /// [PEP 695]: https://peps.python.org/pep-0695/
+    /// [`typing.TypeVar`]: https://docs.python.org/3/library/typing.html#typevar
     TypeParams,
 }
 
