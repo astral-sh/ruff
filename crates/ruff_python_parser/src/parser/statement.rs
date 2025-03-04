99931646b58595d0653ec3f451d8eabe1b2692ce
Brent Westbrook <brentrwestbrook@gmail.com>
2025-03-04 12:07:38 -0500

    move tests to right after parsing `type`

--- crates/ruff_python_parser/src/parser/statement.rs
+++ crates/ruff_python_parser/src/parser/statement.rs
@@ -891,6 +891,14 @@ impl<'src> Parser<'src> {
         let type_range = self.current_token_range();
         self.bump(TokenKind::Type);
 
+        // test_ok type_stmt_py312
+        // # parse_options: {"target-version": "3.12"}
+        // type x = int
+
+        // test_err type_stmt_py311
+        // # parse_options: {"target-version": "3.11"}
+        // type x = int
+
         let mut name = Expr::Name(self.parse_name());
         helpers::set_expr_ctx(&mut name, ExprContext::Store);
 
@@ -909,14 +917,6 @@ impl<'src> Parser<'src> {
         // type x = yield from y
         // type x = x := 1
         let value = self.parse_conditional_expression_or_higher();
-
-        // test_ok type_stmt_py312
-        // # parse_options: {"target-version": "3.12"}
-        // type x = int
-
-        // test_err type_stmt_py311
-        // # parse_options: {"target-version": "3.11"}
-        // type x = int
 
         self.add_unsupported_syntax_error(UnsupportedSyntaxErrorKind::TypeStmt, type_range);
 
