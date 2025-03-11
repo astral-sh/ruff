61d31c6816cd2d71377586b98c07a3d08a4861ed
Brent Westbrook <brentrwestbrook@gmail.com>
2025-03-11 16:59:45 -0400

    [`ruff`] Stabilize `unnecessary-nested-literal` (`RUF041`)
    Summary
    --
    
    Stabilizes RUF041. The tests are already in the right place, and the docs look
    good.
    
    Test Plan
    --
    
    0 issues, 1 [PR] fixing nested literals and unions the day after the rule was
    added. No changes since then
    
    I wonder if the fix in that PR could be relevant for
    https://github.com/astral-sh/ruff/pull/16639, where I noticed a potential issue
    with `Union`. It could be unrelated, though.
    
    [PR]: https://github.com/astral-sh/ruff/pull/14641

--- crates/ruff_linter/src/codes.rs
+++ crates/ruff_linter/src/codes.rs
@@ -998,7 +998,7 @@ pub fn code_to_rule(linter: Linter, code: &str) -> Option<(RuleGroup, Rule)> {
         (Ruff, "038") => (RuleGroup::Preview, rules::ruff::rules::RedundantBoolLiteral),
         (Ruff, "039") => (RuleGroup::Preview, rules::ruff::rules::UnrawRePattern),
         (Ruff, "040") => (RuleGroup::Preview, rules::ruff::rules::InvalidAssertMessageLiteralArgument),
-        (Ruff, "041") => (RuleGroup::Preview, rules::ruff::rules::UnnecessaryNestedLiteral),
+        (Ruff, "041") => (RuleGroup::Stable, rules::ruff::rules::UnnecessaryNestedLiteral),
         (Ruff, "043") => (RuleGroup::Preview, rules::ruff::rules::PytestRaisesAmbiguousPattern),
         (Ruff, "045") => (RuleGroup::Preview, rules::ruff::rules::ImplicitClassVarInDataclass),
         (Ruff, "046") => (RuleGroup::Preview, rules::ruff::rules::UnnecessaryCastToInt),
