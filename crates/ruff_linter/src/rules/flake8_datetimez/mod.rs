5cae8706d2ab297b767bec17e06d1dc687480e30
Brent Westbrook <brentrwestbrook@gmail.com>
2025-03-11 12:29:53 -0400

    [`flake8-datetimez`] Stabilize `datetime-min-max` (`DTZ901`)
    Summary
    --
    
    Stabilizes DTZ901, renames the rule function to match the rule name, removes the
    `preview_rules` test, and handles some nits in the docs (mention `min` first to
    match the rule name too).
    
    Test Plan
    --
    
    1 closed issue on 2024-11-12, 4 days after the rule was added. No issues since

--- crates/ruff_linter/src/rules/flake8_datetimez/mod.rs
+++ crates/ruff_linter/src/rules/flake8_datetimez/mod.rs
@@ -9,7 +9,6 @@ mod tests {
     use test_case::test_case;
 
     use crate::registry::Rule;
-    use crate::settings::types::PreviewMode;
     use crate::test::test_path;
     use crate::{assert_messages, settings};
 
@@ -22,25 +21,12 @@ mod tests {
     #[test_case(Rule::CallDatetimeStrptimeWithoutZone, Path::new("DTZ007.py"))]
     #[test_case(Rule::CallDateToday, Path::new("DTZ011.py"))]
     #[test_case(Rule::CallDateFromtimestamp, Path::new("DTZ012.py"))]
+    #[test_case(Rule::DatetimeMinMax, Path::new("DTZ901.py"))]
     fn rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!("{}_{}", rule_code.noqa_code(), path.to_string_lossy());
         let diagnostics = test_path(
             Path::new("flake8_datetimez").join(path).as_path(),
             &settings::LinterSettings::for_rule(rule_code),
-        )?;
-        assert_messages!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::DatetimeMinMax, Path::new("DTZ901.py"))]
-    fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!("{}_{}", rule_code.noqa_code(), path.to_string_lossy());
-        let diagnostics = test_path(
-            Path::new("flake8_datetimez").join(path).as_path(),
-            &settings::LinterSettings {
-                preview: PreviewMode::Enabled,
-                ..settings::LinterSettings::for_rule(rule_code)
-            },
         )?;
         assert_messages!(snapshot, diagnostics);
         Ok(())
