2ae5344708e3579e06ce2da159ac9b38741b5e54
David Salvisberg <david.salvisberg@seantis.ch>
2025-03-11 13:19:18 +0100

    [`flake8-bandit`] Move `unsafe-markup-use` from `RUF035` to `S704` (#15957)
    ## Summary
    
    `RUF035` has been backported into bandit as `S704` in this
    [PR](https://github.com/PyCQA/bandit/pull/1225)
    
    This moves the rule and its corresponding setting to the `flake8-bandit`
    category
    
    ## Test Plan
    
    `cargo nextest run`
    
    ---------
    
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_linter/resources/test/fixtures/flake8_bandit/S704_extend_markup_names.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_bandit/S704_extend_markup_names.py
@@ -2,5 +2,5 @@ from markupsafe import Markup
 from webhelpers.html import literal
 
 content = "<script>alert('Hello, world!')</script>"
-Markup(f"unsafe {content}")  # RUF035
-literal(f"unsafe {content}")  # RUF035
+Markup(f"unsafe {content}")  # S704
+literal(f"unsafe {content}")  # S704
