092520ad496c13081df93ea093115a9b0d7424ee
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-03-13 07:43:34 +0530

    Add todo for overloaded callables

--- crates/red_knot_python_semantic/src/types.rs
+++ crates/red_knot_python_semantic/src/types.rs
@@ -4305,6 +4305,7 @@ impl<'db> FunctionType<'db> {
     /// Returns `None` if the function is overloaded. This powers the `CallableTypeFromFunction`
     /// special form from the `knot_extensions` module.
     pub(crate) fn into_callable_type(self, db: &'db dyn Db) -> Option<Type<'db>> {
+        // TODO: Add support for overloaded callables; return `Type`, not `Option<Type>`.
         Some(Type::Callable(CallableType::General(
             GeneralCallableType::new(db, self.signature(db).as_single()?.clone()),
         )))
