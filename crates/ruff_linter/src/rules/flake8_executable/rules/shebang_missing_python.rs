51c20e79e9790c6a80f88e085adb4988ef2e4e54
dylwil3 <dylwil3@gmail.com>
2025-03-19 10:24:50 -0500

    update documentation for rule

--- crates/ruff_linter/src/rules/flake8_executable/rules/shebang_missing_python.rs
+++ crates/ruff_linter/src/rules/flake8_executable/rules/shebang_missing_python.rs
@@ -6,17 +6,17 @@ use ruff_macros::{derive_message_formats, ViolationMetadata};
 use crate::comments::shebang::ShebangDirective;
 
 /// ## What it does
-/// Checks for a shebang directive in `.py` files that does not contain `python`.
+/// Checks for a shebang directive in `.py` files that does not contain `python`,
+/// `pytest`, or `uv run`.
 ///
 /// ## Why is this bad?
 /// In Python, a shebang (also known as a hashbang) is the first line of a
-/// script, which specifies the interpreter that should be used to run the
+/// script, which specifies the command that should be used to run the
 /// script.
 ///
-/// For Python scripts, the shebang must contain `python` to indicate that the
-/// script should be executed as a Python script. If the shebang does not
-/// contain `python`, then the file will be executed with the default
-/// interpreter, which is likely a mistake.
+/// For Python scripts, if the shebang does not include a command that explicitly
+/// or implicitly specifies an interpreter, then the file will be executed with
+/// the default interpreter, which is likely a mistake.
 ///
 /// ## Example
 /// ```python
