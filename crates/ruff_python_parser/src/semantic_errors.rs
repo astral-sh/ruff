1a63839bf7f12a70d261a40a106721d722727064
Brent Westbrook <brentrwestbrook@gmail.com>
2025-03-21 11:08:50 -0400

    add duplicates with different stars, also shows diagnostic range

--- crates/ruff_python_parser/src/semantic_errors.rs
+++ crates/ruff_python_parser/src/semantic_errors.rs
@@ -98,6 +98,8 @@ impl SemanticSyntaxChecker {
                 // class C[T, T]: ...
                 // type Alias[T, U: str, V: (str, bytes), *Ts, **P, T = default] = ...
                 // def f[T, T, T](): ...  # two errors
+                // def f[T, *T](): ...    # star is still duplicate
+                // def f[T, **T](): ...   # as is double star
                 Self::add_error(
                     ctx,
                     SemanticSyntaxErrorKind::DuplicateTypeParameter,
