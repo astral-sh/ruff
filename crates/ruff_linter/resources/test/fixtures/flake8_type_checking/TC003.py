33e693e5497bec37551d0e7d8e53d8fc1a7e40cb
David Salvisberg <david.salvisberg@seantis.ch>
2025-03-26 13:59:31 +0100

    [`flake8-type-checking`] Treat type statement value as typing references

--- crates/ruff_linter/resources/test/fixtures/flake8_type_checking/TC003.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_type_checking/TC003.py
@@ -14,3 +14,27 @@ def f():
     import os
 
     print(os)
+
+
+def f():
+    import pathlib
+
+    type Paths = list[pathlib.Path]
+
+
+def f():
+    import pathlib
+
+    type Paths = list[pathlib.Path]
+
+    print(Paths)
+
+
+def f():
+    import pathlib
+
+    type Paths = list[pathlib.Path]
+    type PathsMapping = dict[str, Paths]
+
+    # FIXME: false positive for indirect runtime use of Paths
+    print(PathsMapping)
