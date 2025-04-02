5cc83056e74b67c10fa889ae722947cfc5b99dcc
Max Mynter <maxmynter@me.com>
2025-04-02 11:57:19 -0400

    (tests) Test comment preceeding codes

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF102.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF102.py
@@ -1,9 +1,8 @@
 # Invalid code
-import os  # noqa: XYZ111
+import os  # noqa: INVALID123
 # Valid noqa
 import sys  # noqa: E402
+from functools import cache  # Preceeding comment # noqa: F401, INVALID456
+from itertools import product  # Preceeding comment # noqa: INVALID789
 # Mixed valid and invalid
 from typing import List  # noqa: F401, INVALID123
-
-# Multiple invalid codes
-# noqa: XYZ222, XYZ333
