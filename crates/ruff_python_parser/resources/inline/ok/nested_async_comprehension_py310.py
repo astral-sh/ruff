d49c50c536646e8bba076dc63cc533334b6a7b54
Brent Westbrook <brentrwestbrook@gmail.com>
2025-04-02 17:18:00 -0400

    add a test case for exit_stmt and some commentary

--- crates/ruff_python_parser/resources/inline/ok/nested_async_comprehension_py310.py
+++ crates/ruff_python_parser/resources/inline/ok/nested_async_comprehension_py310.py
@@ -1,6 +1,11 @@
 # parse_options: {"target-version": "3.10"}
 # if all the comprehensions are async, it should be okay
 async def test(): return [[x async for x in elements(n)] async for n in range(3)]
+# this case fails if exit_expr doesn't run
 async def f():
     [_ for n in range(3)]
+    [_ async for n in range(3)]
+# and this fails without exit_stmt
+async def f():
+    def g(): ...
     [_ async for n in range(3)]
