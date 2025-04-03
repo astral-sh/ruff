d401a5440e8a7c1162b042bfcc809ee6d973659e
Dylan <dylwil3@gmail.com>
2025-04-03 08:30:29 -0500

    Control flow: `return` and `raise` (#17121)
    We add support for `return` and `raise` statements in the control flow
    graph: we simply add an edge to the terminal block, push the statements
    to the current block, and proceed.
    
    This implementation will have to be modified somewhat once we add
    support for `try` statements - then we will need to check whether to
    _defer_ the jump. But for now this will do!
    
    Also in this PR: We fix the `unreachable` diagnostic range so that it
    lumps together consecutive unreachable blocks.

--- crates/ruff_linter/resources/test/fixtures/pylint/unreachable.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/unreachable.py
@@ -12,3 +12,19 @@ def no_control_flow_reachable():
     del c
     def foo():
         return
+
+def after_return():
+    return 1
+    print("unreachable")
+    print("and this")
+
+def after_raise():
+    raise ValueError
+    print("unreachable")
+    print("and this")
+
+def multiple_returns():
+    return 1
+    print("unreachable")
+    return 2
+    print("unreachable range should include above return")
