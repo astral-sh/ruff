f006d294f3919f19ee99494b04851d8207bb9930
cake-monotone <cake.monotone@gmail.com>
2025-04-05 01:24:55 +0900

    improve `display` for BoundSuper

--- crates/red_knot_python_semantic/src/types/display.rs
+++ crates/red_knot_python_semantic/src/types/display.rs
@@ -171,17 +171,12 @@ impl Display for DisplayRepresentation<'_> {
             Type::AlwaysTruthy => f.write_str("AlwaysTruthy"),
             Type::AlwaysFalsy => f.write_str("AlwaysFalsy"),
             Type::BoundSuper(bound_super) => {
-                f.write_str("<super: ")?;
-                Type::class_literal(*bound_super.pivot_class(self.db))
-                    .display(self.db)
-                    .fmt(f)?;
-                f.write_str(", ")?;
-                bound_super
-                    .owner(self.db)
-                    .into_type()
-                    .display(self.db)
-                    .fmt(f)?;
-                f.write_str(">")
+                write!(
+                    f,
+                    "<super: {pivot}, {owner}>",
+                    pivot = Type::class_literal(*bound_super.pivot_class(self.db)).display(self.db),
+                    owner = bound_super.owner(self.db).into_type().display(self.db)
+                )
             }
         }
     }
