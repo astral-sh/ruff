ff9efba45ef17e66f4c1b20d0d1392c2832f6f3d
David Peter <mail@david-peter.de>
2025-04-15 09:55:35 +0200

    Change display name of decorator callable

--- crates/red_knot_python_semantic/src/types/display.rs
+++ crates/red_knot_python_semantic/src/types/display.rs
@@ -165,7 +165,9 @@ impl Display for DisplayRepresentation<'_> {
                 };
                 write!(f, "<wrapper-descriptor `{method}` of `{object}` objects>")
             }
-            Type::DataclassDecorator(_) => f.write_str("<function dataclasses.dataclass>"),
+            Type::DataclassDecorator(_) => {
+                f.write_str("<decorator produced by dataclasses.dataclass>")
+            }
             Type::Union(union) => union.display(self.db).fmt(f),
             Type::Intersection(intersection) => intersection.display(self.db).fmt(f),
             Type::IntLiteral(n) => n.fmt(f),
