9192e6ec744944a932d8852da301b136b960e794
weartist <weartistt@gmail.com>
2025-04-16 10:13:17 +0800

    remove unnecessary comment

--- crates/ruff_linter/src/rules/pyflakes/rules/unused_variable.rs
+++ crates/ruff_linter/src/rules/pyflakes/rules/unused_variable.rs
@@ -42,9 +42,6 @@ use crate::fix::edits::delete_stmt;
 ///
 /// This rule's fix is marked as unsafe because removing an unused variable assignment may:
 /// - Delete comments that are attached to the assignment.
-/// - Remove code with side effects (for example, if the right-hand side of the assignment is a function call).
-///
-/// Please review changes carefully when applying automatic fixes.
 ///
 /// ## Options
 /// - `lint.dummy-variable-rgx`
