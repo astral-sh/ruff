dc27878e5e8b70d76a8950f9c96f364898936602
Douglas Creager <dcreager@dcreager.net>
2025-04-17 14:27:29 -0400

    fix todo type

--- crates/red_knot_python_semantic/src/types/infer.rs
+++ crates/red_knot_python_semantic/src/types/infer.rs
@@ -7189,7 +7189,7 @@ impl<'db> TypeInferenceBuilder<'db> {
             }
             _ => {
                 self.infer_type_expression(slice);
-                todo_type!("generics")
+                todo_type!("unsupported subscript form")
             }
         }
     }
