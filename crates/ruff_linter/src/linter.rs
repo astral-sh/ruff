7d82da608e53852262bc04c521bed8e810c889ea
Brent Westbrook <brentrwestbrook@gmail.com>
2025-04-21 14:32:20 -0400

    default to latest for import checking

--- crates/ruff_linter/src/linter.rs
+++ crates/ruff_linter/src/linter.rs
@@ -215,7 +215,7 @@ pub fn check_path(
                     package,
                     source_type,
                     cell_offsets,
-                    target_version,
+                    target_version.unwrap_or_else(PythonVersion::latest),
                 );
 
                 diagnostics.extend(import_diagnostics);
