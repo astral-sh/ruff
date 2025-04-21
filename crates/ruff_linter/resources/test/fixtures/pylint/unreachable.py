8d67bf3c193d2118c7cb5eab28650a26da90a3b4
dylwil3 <dylwil3@gmail.com>
2025-04-21 14:26:33 -0500

    add test fixtures

--- crates/ruff_linter/resources/test/fixtures/pylint/unreachable.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/unreachable.py
@@ -58,3 +58,15 @@ def empty_iter():
         print("unreachable")
     else:
         print("reachable")
+
+def simple_switch(cond):
+    if cond:
+        return "reachable"
+    print("reachable")
+
+def simple_switch(cond):
+    if cond:
+        return "reachable"
+    else:
+        return "reachable"
+    print("unreachable")
--- crates/ruff_python_semantic/src/cfg/mod.rs
+++ crates/ruff_python_semantic/src/cfg/mod.rs
@@ -18,6 +18,7 @@ mod tests {
     #[test_case("no_flow.py")]
     #[test_case("jumps.py")]
     #[test_case("loops.py")]
+    #[test_case("switch.py")]
     fn control_flow_graph(filename: &str) {
         let path = PathBuf::from("resources/test/fixtures/cfg").join(filename);
         let source = fs::read_to_string(path).expect("failed to read file");
