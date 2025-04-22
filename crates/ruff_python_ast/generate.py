5f2134e37eee3af765b1b8a8f5261f12dfb2a76a
Glyphack <sh.hooshyari@gmail.com>
2025-04-22 21:04:09 +0200

    Remove `visit_annotation` from generator

--- crates/ruff_python_ast/generate.py
+++ crates/ruff_python_ast/generate.py
@@ -817,8 +817,6 @@ def write_source_order(out: list[str], ast: Ast) -> None:
 
             for field in node.fields_in_source_order():
                 visitor = type_to_visitor_function[field.parsed_ty.inner]
-                if field.is_annotation:
-                    visitor = annotation_visitor_function
 
                 if field.parsed_ty.optional:
                     body += f"""
