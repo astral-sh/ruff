d1a6fefcfb64c0f3ac049b473aa2d1051c22b3e2
dylwil3 <dylwil3@gmail.com>
2025-04-28 06:32:05 -0500

    add newlines after functions

--- crates/ruff_linter/src/preview.rs
+++ crates/ruff_linter/src/preview.rs
@@ -22,47 +22,61 @@ pub(crate) const fn is_shell_injection_only_trusted_input_enabled(
 ) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_suspicious_function_reference_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_bool_subtype_of_annotation_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_comprehension_with_min_max_sum_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_check_comprehensions_in_tuple_call_enabled(
     settings: &LinterSettings,
 ) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_bad_version_info_in_non_stub_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_fix_future_annotations_in_stub_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_only_add_return_none_at_end_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_simplify_ternary_to_binary_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_fix_manual_dict_comprehension_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_fix_manual_list_comprehension_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_dunder_init_fix_unused_import_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_defer_optional_to_up045_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_unicode_to_unicode_confusables_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
+
 pub(crate) const fn is_support_slices_in_literal_concatenation_enabled(
     settings: &LinterSettings,
 ) -> bool {
