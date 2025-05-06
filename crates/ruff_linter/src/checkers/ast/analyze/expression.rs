mtshiba/ruff-fork

Add new rule InEmptyCollection

crates/ruff_linter/src/checkers/ast/analyze/expression.rs
@@ -1491,6 +1491,9 @@ pub(crate) fn expression(expr: &Expr, checker: &Checker) {
             if checker.enabled(Rule::LiteralMembership) {
                 pylint::rules::literal_membership(checker, compare);
             }
+            if checker.enabled(Rule::InEmptyCollection) {
+                pylint::rules::in_empty_collection(checker, compare);
+            }
             if checker.enabled(Rule::ComparisonOfConstant) {
                 pylint::rules::comparison_of_constant(checker, left, ops, comparators);
             }

astral-sh/ruff

crates/ruff_linter/src/checkers/ast/analyze/expression.rs
@@ -1503,6 +1503,9 @@ pub(crate) fn expression(expr: &Expr, checker: &Checker) {
             if checker.enabled(Rule::NanComparison) {
                 pylint::rules::nan_comparison(checker, left, comparators);
             }
+            if checker.enabled(Rule::InEmptyCollection) {
+                ruff::rules::in_empty_collection(checker, compare);
+            }
             if checker.enabled(Rule::InDictKeys) {
                 flake8_simplify::rules::key_in_dict_compare(checker, compare);
             }
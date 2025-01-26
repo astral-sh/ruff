afe3f20c3afb32ab0c05898147c022f23f6de21d
Marcus Näslund <naslundx@gmail.com>
2025-01-26 20:08:38 +0100

    Add new rule InEmptyCollection

---
+++ crates/ruff_linter/src/rules/ruff/rules/in_empty_collection.rs
@@ -0,0 +1,56 @@
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, ViolationMetadata};
+use ruff_python_ast::{self as ast, CmpOp, Expr};
+use ruff_text_size::Ranged;
+
+use crate::checkers::ast::Checker;
+
+/// ## What it does
+/// Checks for membership tests on empty collections (such as `list`, `tuple`, `set` or `dict`).
+///
+/// ## Why is this bad?
+/// If the collection is always empty, the check is unnecessary, and can be removed.
+
+#[derive(ViolationMetadata)]
+pub(crate) struct InEmptyCollection;
+
+impl Violation for InEmptyCollection {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        "Unnecessary membership test on empty collection".to_string()
+    }
+}
+
+/// RUF060
+pub(crate) fn in_empty_collection(checker: &Checker, compare: &ast::ExprCompare) {
+    let [op] = &*compare.ops else {
+        return;
+    };
+
+    if !matches!(op, CmpOp::In | CmpOp::NotIn) {
+        return;
+    }
+
+    let [right] = &*compare.comparators else {
+        return;
+    };
+
+    let semantic = checker.semantic();
+
+    let is_empty_collection = match right {
+        Expr::List(ast::ExprList { elts, .. }) => elts.is_empty(),
+        Expr::Tuple(ast::ExprTuple { elts, .. }) => elts.is_empty(),
+        Expr::Set(ast::ExprSet { elts, .. }) => elts.is_empty(),
+        Expr::Call(ast::ExprCall {
+            func,
+            arguments,
+            range: _,
+        }) => semantic.match_builtin_expr(func, "set") && arguments.is_empty(),
+        Expr::Dict(ast::ExprDict { range: _, items }) => items.is_empty(),
+        _ => false,
+    };
+
+    if is_empty_collection {
+        checker.report_diagnostic(Diagnostic::new(InEmptyCollection, compare.range()));
+    }
+}
