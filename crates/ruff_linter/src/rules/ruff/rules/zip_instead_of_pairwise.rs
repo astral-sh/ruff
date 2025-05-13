3ad5bd8ec283409966b2c8593697806564c97ee9
vasco <vasco.schiavo@protonmail.com>
2025-05-13 22:30:56 +0200

    correction

--- crates/ruff_linter/src/rules/ruff/rules/zip_instead_of_pairwise.rs
+++ crates/ruff_linter/src/rules/ruff/rules/zip_instead_of_pairwise.rs
@@ -34,8 +34,8 @@ use crate::{checkers::ast::Checker, importer::ImportRequest};
 /// The fix is always marked unsafe because it assumes that slicing an object
 /// (e.g., `obj[1:]`) produces a value with the same type and iteration behavior
 /// as the original object, which is not guaranteed for user-defined types that
-/// override `__getitem__` without handling slices properly. Moreover, the fix could deletes
-/// comments.
+/// override `__getitem__` without properly handling slices. Moreover, the fix
+/// could delete comments.
 ///
 /// ## References
 /// - [Python documentation: `itertools.pairwise`](https://docs.python.org/3/library/itertools.html#itertools.pairwise)
