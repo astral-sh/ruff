0bcfb14fb1e3c5f64f9a781a0ae3a2f581f98dbe
Wei Lee <weilee.rx@gmail.com>
2025-05-14 21:26:00 +0800

    feat(AIR302): add autofix to the cases that only paths have been changed

--- crates/ruff_linter/resources/test/fixtures/airflow/AIR302_hive.py
+++ crates/ruff_linter/resources/test/fixtures/airflow/AIR302_hive.py
@@ -14,24 +14,24 @@ from airflow.operators.hive_operator import HiveOperator
 from airflow.operators.hive_stats_operator import HiveStatsCollectionOperator
 from airflow.operators.hive_to_mysql import (
     HiveToMySqlOperator,
-    HiveToMySqlTransfer,
+
 )
 from airflow.operators.hive_to_samba_operator import HiveToSambaOperator
-from airflow.operators.mssql_to_hive import (
-    MsSqlToHiveOperator,
-    MsSqlToHiveTransfer,
-)
-from airflow.operators.mysql_to_hive import (
-    MySqlToHiveOperator,
-    MySqlToHiveTransfer,
-)
-from airflow.operators.s3_to_hive_operator import (
-    S3ToHiveOperator,
-    S3ToHiveTransfer,
-)
-from airflow.sensors.hive_partition_sensor import HivePartitionSensor
-from airflow.sensors.metastore_partition_sensor import MetastorePartitionSensor
-from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 closest_ds_partition()
 max_partition()
@@ -46,21 +46,62 @@ HiveOperator()
 HiveStatsCollectionOperator()
 
 HiveToMySqlOperator()
-HiveToMySqlTransfer()
+
 
 HiveToSambaOperator()
 
-MsSqlToHiveOperator()
-MsSqlToHiveTransfer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+
+HiveToMySqlTransfer()
+
+from airflow.operators.mysql_to_hive import MySqlToHiveOperator
 
 MySqlToHiveOperator()
+
+from airflow.operators.mysql_to_hive import MySqlToHiveTransfer
+
 MySqlToHiveTransfer()
 
+from airflow.operators.mssql_to_hive import MsSqlToHiveOperator
+
+MsSqlToHiveOperator()
+
+from airflow.operators.mssql_to_hive import MsSqlToHiveTransfer
+
+MsSqlToHiveTransfer()
+
+from airflow.operators.s3_to_hive_operator import S3ToHiveOperator
+
 S3ToHiveOperator()
+
+from airflow.operators.s3_to_hive_operator import S3ToHiveTransfer
+
 S3ToHiveTransfer()
+
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
 
 HivePartitionSensor()
 
+from airflow.sensors.metastore_partition_sensor import MetastorePartitionSensor
+
 MetastorePartitionSensor()
 
+from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
 NamedHivePartitionSensor()
--- crates/ruff_linter/resources/test/fixtures/airflow/AIR302_standard.py
+++ crates/ruff_linter/resources/test/fixtures/airflow/AIR302_standard.py
@@ -5,10 +5,10 @@ from airflow.operators.dagrun_operator import (
     TriggerDagRunLink,
     TriggerDagRunOperator,
 )
-from airflow.operators.dummy import (
-    DummyOperator,
-    EmptyOperator,
-)
+
+
+
+
 from airflow.operators.latest_only_operator import LatestOnlyOperator
 from airflow.operators.python_operator import (
     BranchPythonOperator,
@@ -19,15 +19,15 @@ from airflow.operators.python_operator import (
 from airflow.sensors.external_task_sensor import (
     ExternalTaskMarker,
     ExternalTaskSensor,
-    ExternalTaskSensorLink,
+
 )
 
 BashOperator()
 
 TriggerDagRunLink()
 TriggerDagRunOperator()
-DummyOperator()
-EmptyOperator()
+
+
 
 LatestOnlyOperator()
 
@@ -38,15 +38,15 @@ ShortCircuitOperator()
 
 ExternalTaskMarker()
 ExternalTaskSensor()
-ExternalTaskSensorLink()
 
-from airflow.operators.dummy_operator import (
-    DummyOperator,
-    EmptyOperator,
-)
 
-DummyOperator()
-EmptyOperator()
+
+
+
+
+
+
+
 
 from airflow.hooks.subprocess import SubprocessResult
 SubprocessResult()
@@ -60,3 +60,13 @@ from airflow.sensors.external_task import ExternalTaskSensorLink
 ExternalTaskSensorLink()
 from airflow.sensors.time_delta import WaitSensor
 WaitSensor()
+from airflow.operators.dummy import DummyOperator
+DummyOperator()
+from airflow.operators.dummy import EmptyOperator
+EmptyOperator()
+from airflow.operators.dummy_operator import DummyOperator
+DummyOperator()
+from airflow.operators.dummy_operator import EmptyOperator
+EmptyOperator()
+from airflow.sensors.external_task_sensor import ExternalTaskSensorLink
+ExternalTaskSensorLink()
