34d57ad9c512eddc675b6f34aadfaf1b7492cce2
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2025-05-14 22:56:38 -0400

    update salsa

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -9057,7 +9057,7 @@ impl<'db> BoundSuperType<'db> {
 // Make sure that the `Type` enum does not grow unexpectedly.
 #[cfg(not(debug_assertions))]
 #[cfg(target_pointer_width = "64")]
-static_assertions::assert_eq_size!(Type, [u8; 16]);
+static_assertions::assert_eq_size!(Type, [u8; 24]);
 
 #[cfg(test)]
 pub(crate) mod tests {
