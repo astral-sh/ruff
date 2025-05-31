d2bfbf9d81c2553408a113f7a8b9d81f929b7ca5
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-05-31 10:29:13 +0200

    Ignore __init__.py files in PLC0414

--- crates/ruff_linter/src/rules/pylint/mod.rs
+++ crates/ruff_linter/src/rules/pylint/mod.rs
@@ -168,6 +168,7 @@ mod tests {
     )]
     #[test_case(Rule::UselessElseOnLoop, Path::new("useless_else_on_loop.py"))]
     #[test_case(Rule::UselessImportAlias, Path::new("import_aliasing.py"))]
+    #[test_case(Rule::UselessImportAlias, Path::new("import_aliasing_2/__init__.py"))]
     #[test_case(Rule::UselessReturn, Path::new("useless_return.py"))]
     #[test_case(Rule::UselessWithLock, Path::new("useless_with_lock.py"))]
     #[test_case(Rule::UnreachableCode, Path::new("unreachable.py"))]
