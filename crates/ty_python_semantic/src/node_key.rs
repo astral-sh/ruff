8491a39919d2f4925f39dfce70b28df4e96230c0
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2025-06-05 13:37:05 -0400

    use `NodeIndex` in `NodeKey`

--- crates/ty_python_semantic/src/node_key.rs
+++ crates/ty_python_semantic/src/node_key.rs
@@ -1,21 +1,14 @@
-use ruff_python_ast::AnyNodeRef;
+use ruff_python_ast::HasNodeIndex;
 
 /// Compact key for a node for use in a hash map.
-///
-/// Stores the memory address of the node, because using the range and the kind
-/// of the node is not enough to uniquely identify them in ASTs resulting from
-/// invalid syntax. For example, parsing the input `for` results in a `StmtFor`
-/// AST node where both the `target` and the `iter` field are `ExprName` nodes
-/// with the same (empty) range `3..3`.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub(super) struct NodeKey(usize);
 
 impl NodeKey {
-    pub(super) fn from_node<'a, N>(node: N) -> Self
+    pub(super) fn from_node<N>(node: N) -> Self
     where
-        N: Into<AnyNodeRef<'a>>,
+        N: HasNodeIndex,
     {
-        let node = node.into();
-        NodeKey(node.as_ptr().as_ptr() as usize)
+        NodeKey(node.node_index().as_usize())
     }
 }
