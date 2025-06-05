6913d994d9d6a3ecfcaffe2f76580fe0e9c1c033
Glyphack <sh.hooshyari@gmail.com>
2025-06-05 23:43:06 +0200

    Handle type var in SuperOwnerKind::try_from_type

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -10132,6 +10132,15 @@ impl<'db> SuperOwnerKind<'db> {
             Type::SpecialForm(special_form) => {
                 SuperOwnerKind::try_from_type(db, special_form.instance_fallback(db))
             }
+            // I'm not sure about this, I thought if a type var is passed, then
+            // the best option we have is to treat is as an instance of the
+            // constraint if there is one.
+            Type::TypeVar(typevar) => match typevar.bound_or_constraints(db) {
+                Some(TypeVarBoundOrConstraints::UpperBound(upper_bound)) => {
+                    SuperOwnerKind::try_from_type(db, upper_bound)
+                }
+                _ => None,
+            },
             _ => None,
         }
     }
