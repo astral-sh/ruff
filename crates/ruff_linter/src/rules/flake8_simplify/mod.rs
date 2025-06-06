eb5abda8acb33af661b3adae6f68a9837cb4df80
Dylan <dylwil3@gmail.com>
2025-06-06 16:46:43 -0500

    [`flake8-simplify`] Stabilize further simplification to binary expressions in autofix for `if-else-block-instead-of-if-exp` (`SIM108`) (#18506)

--- crates/ruff_linter/src/rules/flake8_simplify/mod.rs
+++ crates/ruff_linter/src/rules/flake8_simplify/mod.rs
@@ -58,7 +58,6 @@ mod tests {
         Ok(())
     }
 
-    #[test_case(Rule::IfElseBlockInsteadOfIfExp, Path::new("SIM108.py"))]
     #[test_case(Rule::MultipleWithStatements, Path::new("SIM117.py"))]
     fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!(
