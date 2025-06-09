886fc1a5126d3e368059406eb75d8b2f774cc518
Dylan <dylwil3@gmail.com>
2025-06-09 12:47:00 -0500

    [`pylint`] Stabilize `unnecessary-lambda` (`PLW0108`) (#18553)

--- crates/ruff_linter/src/codes.rs
+++ crates/ruff_linter/src/codes.rs
@@ -280,7 +280,7 @@ pub fn code_to_rule(linter: Linter, code: &str) -> Option<(RuleGroup, Rule)> {
         (Pylint, "R6301") => (RuleGroup::Preview, rules::pylint::rules::NoSelfUse),
         #[cfg(any(feature = "test-rules", test))]
         (Pylint, "W0101") => (RuleGroup::Preview, rules::pylint::rules::UnreachableCode),
-        (Pylint, "W0108") => (RuleGroup::Preview, rules::pylint::rules::UnnecessaryLambda),
+        (Pylint, "W0108") => (RuleGroup::Stable, rules::pylint::rules::UnnecessaryLambda),
         (Pylint, "W0177") => (RuleGroup::Preview, rules::pylint::rules::NanComparison),
         (Pylint, "W0120") => (RuleGroup::Stable, rules::pylint::rules::UselessElseOnLoop),
         (Pylint, "W0127") => (RuleGroup::Stable, rules::pylint::rules::SelfAssigningVariable),
