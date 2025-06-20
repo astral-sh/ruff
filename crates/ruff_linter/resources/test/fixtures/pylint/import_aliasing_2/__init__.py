d8d691bdb2cd4839152d9f0294ccdfb8ef889cfa
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-06-20 18:35:06 +0200

    Remove useless comments

--- crates/ruff_linter/resources/test/fixtures/pylint/import_aliasing_2/__init__.py
+++ crates/ruff_linter/resources/test/fixtures/pylint/import_aliasing_2/__init__.py
@@ -1,7 +1,3 @@
-# pylint: disable=unused-import, missing-docstring, invalid-name, reimported, import-error, wrong-import-order, no-name-in-module, shadowed-import
-# Functional tests for import aliasing
-# 1. useless-import-alias
-
 import collections as collections
 from collections import OrderedDict as OrderedDict
 from . import foo as foo
