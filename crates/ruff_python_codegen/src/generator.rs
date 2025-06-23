369223f2fb340f8b341b7013f228e77246442fb9
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-06-22 17:37:00 -0700

    Make test work

--- crates/ruff_python_codegen/src/generator.rs
+++ crates/ruff_python_codegen/src/generator.rs
@@ -1936,7 +1936,12 @@ class Foo:
         assert_round_trip!(r#"f"{ chr(65)  =   :#x}""#);
         assert_round_trip!(r#"f"{  ( chr(65)  ) = }""#);
         assert_round_trip!(r#"f"{a=!r:0.05f}""#);
-        assert_round_trip!(r#"f"{1=\n}""#); // https://github.com/astral-sh/ruff/issues/18742
+        assert_round_trip!(r#"
+f"{1=
+}"
+"#
+            .trim()
+            .replace('\n', LineEnding::default().as_str())); // https://github.com/astral-sh/ruff/issues/18742
     }
 
     #[test]
