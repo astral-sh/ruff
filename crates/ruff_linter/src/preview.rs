55251bd15a229c1d5ecf317d72c7a23f10b10bd2
Robsdedude <rouven.bauer@neo4j.com>
2025-06-24 22:12:50 +0200

    Link PR from preview check function

--- crates/ruff_linter/src/preview.rs
+++ crates/ruff_linter/src/preview.rs
@@ -90,6 +90,7 @@ pub(crate) const fn is_ignore_init_files_in_useless_alias_enabled(
     settings.preview.is_enabled()
 }
 
+// https://github.com/astral-sh/ruff/pull/18572
 pub(crate) const fn is_optional_as_none_in_union_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
