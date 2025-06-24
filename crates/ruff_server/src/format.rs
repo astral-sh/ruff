66fc7c8fc0dbc5f95c75feaec957b03ead6473be
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-06-24 17:54:28 +0530

    Consider virtual path for various server actions (#18910)
    ## Summary
    
    Ref:
    https://github.com/astral-sh/ruff/issues/14820#issuecomment-2996690681
    
    This PR fixes a bug where virtual paths or any paths that doesn't exists
    on the file system weren't being considered for checking inclusion /
    exclusion. This was because the logic used `file_path` which returns
    `None` for those path. This PR fixes that by using the
    `virtual_file_path` method that returns a `Path` corresponding to the
    actual file on disk or any kind of virtual path.
    
    This should ideally just fix the above linked issue by way of excluding
    the documents representing the interactive window because they aren't in
    the inclusion set. It failed only on Windows previously because the file
    path construction would fail and then Ruff would default to including
    all the files.
    
    ## Test Plan
    
    On my machine, the `.interactive` paths are always excluded so I'm using
    the inclusion set instead:
    
    ```json
    {
      "ruff.nativeServer": "on",
      "ruff.path": ["/Users/dhruv/work/astral/ruff/target/debug/ruff"],
      "ruff.configuration": {
        "extend-include": ["*.interactive"]
      }
    }
    ```
    
    The diagnostics are shown for both the file paths and the interactive
    window:
    
    <img width="1727" alt="Screenshot 2025-06-24 at 14 56 40"
    src="https://github.com/user-attachments/assets/d36af96a-777e-4367-8acf-4d9c9014d025"
    />
    
    And, the logs:
    
    ```
    2025-06-24 14:56:26.478275000 DEBUG notification{method="notebookDocument/didChange"}: Included path via `extend-include`: /Interactive-1.interactive
    ```
    
    And, when using `ruff.exclude` via:
    
    ```json
    {
    	"ruff.exclude": ["*.interactive"]
    }
    ```
    
    With logs:
    
    ```
    2025-06-24 14:58:41.117743000 DEBUG notification{method="notebookDocument/didChange"}: Ignored path via `exclude`: /Interactive-1.interactive
    ```

--- crates/ruff_server/src/format.rs
+++ crates/ruff_server/src/format.rs
@@ -12,10 +12,10 @@ pub(crate) fn format(
     document: &TextDocument,
     source_type: PySourceType,
     formatter_settings: &FormatterSettings,
-    path: Option<&Path>,
+    path: &Path,
 ) -> crate::Result<Option<String>> {
     let format_options =
-        formatter_settings.to_format_options(source_type, document.contents(), path);
+        formatter_settings.to_format_options(source_type, document.contents(), Some(path));
     match format_module_source(document.contents(), format_options) {
         Ok(formatted) => {
             let formatted = formatted.into_code();
@@ -40,10 +40,10 @@ pub(crate) fn format_range(
     source_type: PySourceType,
     formatter_settings: &FormatterSettings,
     range: TextRange,
-    path: Option<&Path>,
+    path: &Path,
 ) -> crate::Result<Option<PrintedRange>> {
     let format_options =
-        formatter_settings.to_format_options(source_type, document.contents(), path);
+        formatter_settings.to_format_options(source_type, document.contents(), Some(path));
 
     match ruff_python_formatter::format_range(document.contents(), range, format_options) {
         Ok(formatted) => {
@@ -97,7 +97,7 @@ with open("a_really_long_foo") as foo, open("a_really_long_bar") as bar, open("a
                 per_file_target_version,
                 ..Default::default()
             },
-            Some(Path::new("test.py")),
+            Path::new("test.py"),
         )
         .expect("Expected no errors when formatting")
         .expect("Expected formatting changes");
@@ -119,7 +119,7 @@ with open("a_really_long_foo") as foo, open("a_really_long_bar") as bar, open("a
                 unresolved_target_version: PythonVersion::PY38,
                 ..Default::default()
             },
-            Some(Path::new("test.py")),
+            Path::new("test.py"),
         )
         .expect("Expected no errors when formatting")
         .expect("Expected formatting changes");
@@ -167,7 +167,7 @@ sys.exit(
                 ..Default::default()
             },
             range,
-            Some(Path::new("test.py")),
+            Path::new("test.py"),
         )
         .expect("Expected no errors when formatting")
         .expect("Expected formatting changes");
@@ -190,7 +190,7 @@ sys.exit(
                 ..Default::default()
             },
             range,
-            Some(Path::new("test.py")),
+            Path::new("test.py"),
         )
         .expect("Expected no errors when formatting")
         .expect("Expected formatting changes");
