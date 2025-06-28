f403f11f189f7fcc9a5004cd0c83f72c9dbde5f4
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-06-27 22:49:45 -0700

    Update test

--- crates/ruff_linter/resources/test/fixtures/flake8_bandit/S412.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_bandit/S412.py
@@ -1,1 +1,4 @@
 from twisted.web.twcgi import CGIScript  # S412
+import twisted.web.twcgi.CGIScript  # S412
+from wsgiref.handlers import CGIHandler  # S412
+import wsgiref.handlers.CGIHandler  # S412
--- crates/ruff_linter/resources/test/fixtures/flake8_bandit/S412.pyi
+++ crates/ruff_linter/resources/test/fixtures/flake8_bandit/S412.pyi
@@ -1,1 +1,4 @@
 from twisted.web.twcgi import CGIScript
+import twisted.web.twcgi.CGIScript
+from wsgiref.handlers import CGIHandler
+import wsgiref.handlers.CGIHandler
