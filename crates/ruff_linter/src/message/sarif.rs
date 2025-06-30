faccadc15d5ab1b7ec9172f56eff2b81fa7464bf
Brent Westbrook <brentrwestbrook@gmail.com>
2025-06-30 15:47:44 -0400

    fix wasm

--- crates/ruff_linter/src/message/sarif.rs
+++ crates/ruff_linter/src/message/sarif.rs
@@ -144,9 +144,9 @@ impl<'a> SarifResult<'a> {
     #[cfg(target_arch = "wasm32")]
     #[expect(clippy::unnecessary_wraps)]
     fn from_message(message: &'a Diagnostic) -> Result<Self> {
-        let start_location = message.compute_start_location();
-        let end_location = message.compute_end_location();
-        let path = normalize_path(&*message.filename());
+        let start_location = message.expect_ruff_start_location();
+        let end_location = message.expect_ruff_end_location();
+        let path = normalize_path(&*message.expect_ruff_filename());
         Ok(Self {
             code: message.secondary_code(),
             level: "error".to_string(),
