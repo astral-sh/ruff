2ccd10ef30ae871fc6b19abacb651e7c49232199
soundsonacid <soundsonacid@gmail.com>
2025-07-08 19:15:06 -0500

    (chore): pr review

--- crates/ruff_linter/resources/test/fixtures/flake8_type_checking/whitespace.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_type_checking/whitespace.py
@@ -1,4 +1,5 @@
 # Regression test for: https://github.com/astral-sh/ruff/issues/19175
+# there is a (potentially invisible) unicode formfeed character (000C) between `TYPE_CHECKING` and the backslash
 from typing import TYPE_CHECKING\
 
 if TYPE_CHECKING: import builtins
