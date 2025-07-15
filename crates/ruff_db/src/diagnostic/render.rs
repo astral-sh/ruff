e9cac3684afbbb23b22f047e0545cee5cfc77a54
Brent Westbrook <36778786+ntBre@users.noreply.github.com>
2025-07-15 10:14:49 -0400

    Move Pylint rendering to `ruff_db` (#19340)
    Summary
    --
    
    This is a very simple output format, the only decision is what to do if
    the file
    is missing from the diagnostic. For now, I opted to `unwrap_or_default`
    both the
    path and the `OneIndexed` row number, giving `:1: main diagnostic
    message` in
    the test without a file.
    
    Another quirk here is that the path is relativized. I just pasted in the
    `relativize_path` and `get_cwd` implementations from `ruff_linter::fs`
    for now,
    but maybe there's a better place for them.
    
    I didn't see any details about why this needs to be relativized in the
    original
    [issue](https://github.com/astral-sh/ruff/issues/1953),
    [PR](https://github.com/astral-sh/ruff/pull/1995), or in the pylint
    
    [docs](https://flake8.pycqa.org/en/latest/internal/formatters.html#pylint-formatter),
    but it did change the results of the CLI integration test when I tried
    deleting
    it. I haven't been able to reproduce that in the CLI, though, so it may
    only
    happen with `Command::current_dir`.
    
    Test Plan
    --
    
    Tests ported from `ruff_linter` and a new test for the case with no file
    
    ---------
    
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_db/src/diagnostic/render.rs
+++ crates/ruff_db/src/diagnostic/render.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use std::path::Path;
 
 use ruff_annotate_snippets::{
     Annotation as AnnotateAnnotation, Level as AnnotateLevel, Message as AnnotateMessage,
@@ -22,12 +23,14 @@ use super::{
 };
 
 use azure::AzureRenderer;
+use pylint::PylintRenderer;
 
 mod azure;
 #[cfg(feature = "serde")]
 mod json;
 #[cfg(feature = "serde")]
 mod json_lines;
+mod pylint;
 #[cfg(feature = "serde")]
 mod rdjson;
 
@@ -189,6 +192,9 @@ impl std::fmt::Display for DisplayDiagnostics<'_> {
             #[cfg(feature = "serde")]
             DiagnosticFormat::Rdjson => {
                 rdjson::RdjsonRenderer::new(self.resolver).render(f, self.diagnostics)?;
+            }
+            DiagnosticFormat::Pylint => {
+                PylintRenderer::new(self.resolver).render(f, self.diagnostics)?;
             }
         }
 
@@ -711,6 +717,9 @@ pub trait FileResolver {
 
     /// Returns whether the file given is a Jupyter notebook.
     fn is_notebook(&self, file: &UnifiedFile) -> bool;
+
+    /// Returns the current working directory.
+    fn current_directory(&self) -> &Path;
 }
 
 impl<T> FileResolver for T
@@ -746,6 +755,10 @@ where
             UnifiedFile::Ruff(_) => unimplemented!("Expected an interned ty file"),
         }
     }
+
+    fn current_directory(&self) -> &Path {
+        self.system().current_directory().as_std_path()
+    }
 }
 
 impl FileResolver for &dyn Db {
@@ -777,6 +790,10 @@ impl FileResolver for &dyn Db {
             UnifiedFile::Ty(file) => self.input(*file).text.as_notebook().is_some(),
             UnifiedFile::Ruff(_) => unimplemented!("Expected an interned ty file"),
         }
+    }
+
+    fn current_directory(&self) -> &Path {
+        self.system().current_directory().as_std_path()
     }
 }
 
