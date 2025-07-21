76185257bc55b8438526956e537c4131c1a7fb8f
Brent Westbrook <brentrwestbrook@gmail.com>
2025-07-21 10:50:43 -0400

    Revert "try using SmallVec for subdiagnostics"
    This reverts commit 7c0e0ff1c9d6e0e1b2808da35bb203290825f238.

--- crates/ruff_db/src/diagnostic/mod.rs
+++ crates/ruff_db/src/diagnostic/mod.rs
@@ -5,7 +5,6 @@ use ruff_source_file::{LineColumn, SourceCode, SourceFile};
 
 use ruff_annotate_snippets::Level as AnnotateLevel;
 use ruff_text_size::{Ranged, TextRange, TextSize};
-use smallvec::SmallVec;
 
 pub use self::render::{DisplayDiagnostic, DisplayDiagnostics, FileResolver, Input};
 use crate::{Db, files::File};
@@ -62,7 +61,7 @@ impl Diagnostic {
             severity,
             message: message.into_diagnostic_message(),
             annotations: vec![],
-            subs: SmallVec::new(),
+            subs: vec![],
             fix: None,
             parent: None,
             noqa_offset: None,
@@ -480,7 +479,7 @@ struct DiagnosticInner {
     severity: Severity,
     message: DiagnosticMessage,
     annotations: Vec<Annotation>,
-    subs: SmallVec<[SubDiagnostic; 1]>,
+    subs: Vec<SubDiagnostic>,
     fix: Option<Fix>,
     parent: Option<TextSize>,
     noqa_offset: Option<TextSize>,
