88de5727dfab24ce223024de683b283b70ef62e7
Douglas Creager <dcreager@dcreager.net>
2025-07-21 14:16:27 -0400

    [ty] Garbage-collect reachability constraints (#19414)
    This is a follow-on to #19410 that further reduces the memory usage of
    our reachability constraints. When finishing the building of a use-def
    map, we walk through all of the "final" states and mark only those
    reachability constraints as "used". We then throw away the interior TDD
    nodes of any reachability constraints that weren't marked as used.
    
    (This helps because we build up quite a few intermediate TDD nodes when
    constructing complex reachability constraints. These nodes can never be
    accessed if they were _only_ used as an intermediate TDD node. The
    marking step ensures that we keep any nodes that ended up being referred
    to in some accessible use-def map state.)

--- crates/ty_python_semantic/src/semantic_index/use_def/place_state.rs
+++ crates/ty_python_semantic/src/semantic_index/use_def/place_state.rs
@@ -172,6 +172,13 @@ impl Declarations {
             }
         }
     }
+
+    pub(super) fn finish(&mut self, reachability_constraints: &mut ReachabilityConstraintsBuilder) {
+        self.live_declarations.shrink_to_fit();
+        for declaration in &self.live_declarations {
+            reachability_constraints.mark_used(declaration.reachability_constraint);
+        }
+    }
 }
 
 /// A snapshot of a place state that can be used to resolve a reference in a nested eager scope.
@@ -183,6 +190,17 @@ impl Declarations {
 pub(super) enum EagerSnapshot {
     Constraint(ScopedNarrowingConstraint),
     Bindings(Bindings),
+}
+
+impl EagerSnapshot {
+    pub(super) fn finish(&mut self, reachability_constraints: &mut ReachabilityConstraintsBuilder) {
+        match self {
+            EagerSnapshot::Constraint(_) => {}
+            EagerSnapshot::Bindings(bindings) => {
+                bindings.finish(reachability_constraints);
+            }
+        }
+    }
 }
 
 /// Live bindings for a single place at some point in control flow. Each live binding comes
@@ -202,6 +220,13 @@ impl Bindings {
     pub(super) fn unbound_narrowing_constraint(&self) -> ScopedNarrowingConstraint {
         self.unbound_narrowing_constraint
             .unwrap_or(self.live_bindings[0].narrowing_constraint)
+    }
+
+    pub(super) fn finish(&mut self, reachability_constraints: &mut ReachabilityConstraintsBuilder) {
+        self.live_bindings.shrink_to_fit();
+        for binding in &self.live_bindings {
+            reachability_constraints.mark_used(binding.reachability_constraint);
+        }
     }
 }
 
@@ -421,6 +446,11 @@ impl PlaceState {
 
     pub(super) fn declarations(&self) -> &Declarations {
         &self.declarations
+    }
+
+    pub(super) fn finish(&mut self, reachability_constraints: &mut ReachabilityConstraintsBuilder) {
+        self.declarations.finish(reachability_constraints);
+        self.bindings.finish(reachability_constraints);
     }
 }
 
