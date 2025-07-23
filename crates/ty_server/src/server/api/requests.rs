c98194448eb6e54de681f644a19054b39b595779
UnboundVariable <unbound@gmail.com>
2025-07-23 09:58:53 -0700

    [ty] Added support for "document highlights" language server feature.

--- crates/ty_server/src/server/api/requests.rs
+++ crates/ty_server/src/server/api/requests.rs
@@ -1,11 +1,12 @@
 mod completion;
 mod diagnostic;
+mod doc_highlights;
 mod goto_declaration;
 mod goto_definition;
+mod goto_references;
 mod goto_type_definition;
 mod hover;
 mod inlay_hints;
-mod references;
 mod semantic_tokens;
 mod semantic_tokens_range;
 mod shutdown;
@@ -14,12 +15,13 @@ mod workspace_diagnostic;
 
 pub(super) use completion::CompletionRequestHandler;
 pub(super) use diagnostic::DocumentDiagnosticRequestHandler;
+pub(super) use doc_highlights::DocumentHighlightRequestHandler;
 pub(super) use goto_declaration::GotoDeclarationRequestHandler;
 pub(super) use goto_definition::GotoDefinitionRequestHandler;
+pub(super) use goto_references::ReferencesRequestHandler;
 pub(super) use goto_type_definition::GotoTypeDefinitionRequestHandler;
 pub(super) use hover::HoverRequestHandler;
 pub(super) use inlay_hints::InlayHintRequestHandler;
-pub(super) use references::ReferencesRequestHandler;
 pub(super) use semantic_tokens::SemanticTokensRequestHandler;
 pub(super) use semantic_tokens_range::SemanticTokensRangeRequestHandler;
 pub(super) use shutdown::ShutdownHandler;
