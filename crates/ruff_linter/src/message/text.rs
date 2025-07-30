8979271ea8e2f7d3cd594cab7f2d237e38718b38
Brent Westbrook <36778786+ntBre@users.noreply.github.com>
2025-07-30 11:00:36 -0400

    Always expand tabs to four spaces in diagnostics (#19618)
    ## Summary
    
    I was a bit stuck on some snapshot differences I was seeing in #19415,
    but @BurntSushi pointed out that `annotate-snippets` already normalizes
    tabs on its own, which was very helpful! Instead of applying this change
    directly to the other branch, I wanted to try applying it in
    `ruff_linter` first. This should very slightly reduce the number of
    changes in #19415 proper.
    
    It looks like `annotate-snippets` always expands a tab to four spaces,
    whereas I think we were aligning to tab stops:
    
    ```diff
      6 | spam(ham[1], { eggs: 2})
      7 | #: E201:1:6
    - 8 | spam(   ham[1], {eggs: 2})
    -   |      ^^^ E201
    + 8 | spam(    ham[1], {eggs: 2})
    +   |      ^^^^ E201
    ```
    
    ```diff
    61 | #: E203:2:15 E702:2:16
     62 | if x == 4:
    -63 |     print(x, y) ; x, y = y, x
    -   |                ^ E203
    +63 |     print(x, y)    ; x, y = y, x
    +   |                ^^^^ E203
    ```
    
    ```diff
     E27.py:15:6: E271 [*] Multiple spaces after keyword
        |
    -13 | True        and False
    +13 | True        and    False
     14 | #: E271
     15 | a and  b
        |      ^^ E271
    ```
    
    I don't think this is too bad and has the major benefit of allowing us
    to pass the non-tab-expanded range to `annotate-snippets` in #19415,
    where it's also displayed in the header. Ruff doesn't have this problem
    currently because it uses its own concise diagnostic output as the
    header for full diagnostics, where the pre-expansion range is used
    directly.
    
    ## Test Plan
    
    Existing tests with a few snapshot updates

--- crates/ruff_linter/src/message/text.rs
+++ crates/ruff_linter/src/message/text.rs
@@ -13,7 +13,6 @@ use ruff_notebook::NotebookIndex;
 use ruff_source_file::OneIndexed;
 use ruff_text_size::{TextLen, TextRange, TextSize};
 
-use crate::line_width::{IndentWidth, LineWidthBuilder};
 use crate::message::diff::Diff;
 use crate::message::{Emitter, EmitterContext};
 use crate::settings::types::UnsafeFixes;
@@ -229,7 +228,7 @@ impl Display for MessageCodeFrame<'_> {
         let start_offset = source_code.line_start(start_index);
         let end_offset = source_code.line_end(end_index);
 
-        let source = replace_whitespace_and_unprintable(
+        let source = replace_unprintable(
             source_code.slice(TextRange::new(start_offset, end_offset)),
             self.message.expect_range() - start_offset,
         )
@@ -272,16 +271,20 @@ impl Display for MessageCodeFrame<'_> {
 }
 
 /// Given some source code and an annotation range, this routine replaces
-/// tabs with ASCII whitespace, and unprintable characters with printable
-/// representations of them.
+///  unprintable characters with printable representations of them.
 ///
 /// The source code returned has an annotation that is updated to reflect
 /// changes made to the source code (if any).
-fn replace_whitespace_and_unprintable(source: &str, annotation_range: TextRange) -> SourceCode {
+///
+/// We don't need to normalize whitespace, such as converting tabs to spaces,
+/// because `annotate-snippets` handles that internally. Similarly, it's safe to
+/// modify the annotation ranges by inserting 3-byte Unicode replacements
+/// because `annotate-snippets` will account for their actual width when
+/// rendering and displaying the column to the user.
+fn replace_unprintable(source: &str, annotation_range: TextRange) -> SourceCode {
     let mut result = String::new();
     let mut last_end = 0;
     let mut range = annotation_range;
-    let mut line_width = LineWidthBuilder::new(IndentWidth::default());
 
     // Updates the range given by the caller whenever a single byte (at
     // `index` in `source`) is replaced with `len` bytes.
@@ -310,19 +313,7 @@ fn replace_whitespace_and_unprintable(source: &str, annotation_range: TextRange)
     };
 
     for (index, c) in source.char_indices() {
-        let old_width = line_width.get();
-        line_width = line_width.add_char(c);
-
-        if matches!(c, '\t') {
-            let tab_width = u32::try_from(line_width.get() - old_width)
-                .expect("small width because of tab size");
-            result.push_str(&source[last_end..index]);
-            for _ in 0..tab_width {
-                result.push(' ');
-            }
-            last_end = index + 1;
-            update_range(index, tab_width);
-        } else if let Some(printable) = unprintable_replacement(c) {
+        if let Some(printable) = unprintable_replacement(c) {
             result.push_str(&source[last_end..index]);
             result.push(printable);
             last_end = index + 1;
