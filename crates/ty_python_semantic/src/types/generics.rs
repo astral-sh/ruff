46c3d1a89c3f6812ecf592129adf0f1b96f64b4c
Alex Waygood <alex.waygood@gmail.com>
2025-08-01 20:22:42 +0100

    remove unused methods

--- crates/ty_python_semantic/src/types/generics.rs
+++ crates/ty_python_semantic/src/types/generics.rs
@@ -676,6 +676,9 @@ impl<'db> Specialization<'db> {
         for ty in self.types(db) {
             ty.find_legacy_typevars(db, typevars);
         }
+        if let Some(t) = self.tuple_inner(db) {
+            t.find_legacy_typevars(db, typevars);
+        }
     }
 }
 
