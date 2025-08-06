122b6bb4cf51f5449546050ce3c39cc7d6e82579
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-08-06 21:42:43 +0530

    Avoid multiple keys in test are the order is arbitrary

--- crates/ty_server/tests/e2e/initialize.rs
+++ crates/ty_server/tests/e2e/initialize.rs
@@ -392,13 +392,7 @@ fn unknown_initialization_options() -> Result<()> {
     let mut server = TestServerBuilder::new()?
         .with_workspace(workspace_root, None)?
         .with_initialization_options(
-            ClientOptions::default().with_unknown(
-                [
-                    ("foo".to_string(), Value::Null),
-                    ("bar".to_string(), Value::Null),
-                ]
-                .into(),
-            ),
+            ClientOptions::default().with_unknown([("bar".to_string(), Value::Null)].into()),
         )
         .build()?
         .wait_until_workspaces_are_initialized()?;
@@ -408,7 +402,7 @@ fn unknown_initialization_options() -> Result<()> {
     insta::assert_json_snapshot!(show_message_params, @r#"
     {
       "type": 2,
-      "message": "Received unknown options during initialization: 'bar', 'foo'. Refer to the logs for more details"
+      "message": "Received unknown options during initialization: 'bar'. Refer to the logs for more details"
     }
     "#);
 
@@ -423,15 +417,7 @@ fn unknown_options_in_workspace_configuration() -> Result<()> {
     let mut server = TestServerBuilder::new()?
         .with_workspace(
             workspace_root,
-            Some(
-                ClientOptions::default().with_unknown(
-                    [
-                        ("foo".to_string(), Value::Null),
-                        ("bar".to_string(), Value::Null),
-                    ]
-                    .into(),
-                ),
-            ),
+            Some(ClientOptions::default().with_unknown([("bar".to_string(), Value::Null)].into())),
         )?
         .build()?
         .wait_until_workspaces_are_initialized()?;
@@ -441,7 +427,7 @@ fn unknown_options_in_workspace_configuration() -> Result<()> {
     insta::assert_json_snapshot!(show_message_params, @r#"
     {
       "type": 2,
-      "message": "Received unknown options for workspace `file://<temp_dir>/foo`: 'bar', 'foo'. Refer to the logs for more details."
+      "message": "Received unknown options for workspace `file://<temp_dir>/foo`: 'bar'. Refer to the logs for more details."
     }
     "#);
 
