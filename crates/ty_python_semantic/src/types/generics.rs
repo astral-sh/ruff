99a057d84f3b38b70d097963b169821eb9a9bd8c
Alex Waygood <alex.waygood@gmail.com>
2025-08-07 09:41:51 +0100

    remove unnecessary call to `find_legacy_typevars`

--- crates/ty_python_semantic/src/types/generics.rs
+++ crates/ty_python_semantic/src/types/generics.rs
@@ -681,9 +681,8 @@ impl<'db> Specialization<'db> {
         for ty in self.types(db) {
             ty.find_legacy_typevars(db, typevars);
         }
-        if let Some(t) = self.tuple_inner(db) {
-            t.find_legacy_typevars(db, typevars);
-        }
+        // A tuple's specialization will include all of its element types, so we don't need to also
+        // look in `self.tuple`.
     }
 }
 
