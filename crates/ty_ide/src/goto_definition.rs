4be6fc097918d2213b03fe3c12058c6ea4294027
Aria Desires <aria.desires@gmail.com>
2025-08-08 16:46:28 -0400

    [ty] fix goto-definition on imports (#19834)
    The stub mapper wasn't being passed into this codepath. It is now being
    used. A previously messed up test result I intentionally checked in was
    subsequently fixed.

--- crates/ty_ide/src/goto_definition.rs
+++ crates/ty_ide/src/goto_definition.rs
@@ -76,13 +76,14 @@ def my_function(): ...
             )
             .build();
 
-        assert_snapshot!(test.goto_definition(), @r"
+        assert_snapshot!(test.goto_definition(), @r#"
         info[goto-definition]: Definition
-         --> mymodule.pyi:1:1
+         --> mymodule.py:1:1
           |
         1 |
           | ^
-        2 | def my_function(): ...
+        2 | def my_function():
+        3 |     return "hello"
           |
         info: Source
          --> main.py:2:6
@@ -90,7 +91,7 @@ def my_function(): ...
         2 | from mymodule import my_function
           |      ^^^^^^^^
           |
-        ");
+        "#);
     }
 
     /// goto-definition on a module ref should go to the .py not the .pyi
