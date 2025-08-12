d6bd0b27db278ab7199bec1a0b662645e432e148
Andrew Gallant <andrew@astral.sh>
2025-08-12 14:14:31 -0400

    [ty] Add "list modules" implementation
    The actual implementation wasn't too bad. It's not long
    but pretty fiddly. I copied over the tests from the existing
    module resolver and adapted them to work with this API. Then
    I added a number of my own tests as well.

--- crates/ty_python_semantic/src/module_resolver/mod.rs
+++ crates/ty_python_semantic/src/module_resolver/mod.rs
@@ -1,5 +1,6 @@
 use std::iter::FusedIterator;
 
+pub use list::list_modules;
 pub(crate) use module::KnownModule;
 pub use module::Module;
 pub use path::SearchPathValidationError;
@@ -12,6 +13,7 @@ use crate::Db;
 use crate::module_resolver::resolver::{ModuleResolveMode, search_paths};
 use resolver::SearchPathIterator;
 
+mod list;
 mod module;
 mod path;
 mod resolver;
