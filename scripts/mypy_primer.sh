355d6d5a3af3091234a75e8c72fdb90dcbd51cbd
Alex Waygood <Alex.Waygood@Gmail.com>
2025-08-18 13:23:23 +0100

    Update mypy_primer.sh

--- scripts/mypy_primer.sh
+++ scripts/mypy_primer.sh
@@ -20,7 +20,7 @@ cd ..
 echo "Project selector: ${PRIMER_SELECTOR}"
 # Allow the exit code to be 0 or 1, only fail for actual mypy_primer crashes/bugs
 uvx \
-  --from="git+https://github.com/hauntsaninja/mypy_primer@5c73ab6e233315008fa25f87b4005c1e37683022" \
+  --from="git+https://github.com/hauntsaninja/mypy_primer@a3798a3d7b8470603e650179b0f82deb2154364d" \
   mypy_primer \
   --repo ruff \
   --type-checker ty \
