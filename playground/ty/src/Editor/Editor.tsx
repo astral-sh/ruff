a62b5ec35641821fc83f58b261a9ec08fee5c4fe
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-08-22 15:38:44 +0100

    Add inlay hint kind in wasm and playground

--- playground/ty/src/Editor/Editor.tsx
+++ playground/ty/src/Editor/Editor.tsx
@@ -27,6 +27,7 @@ import {
   type FileHandle,
   DocumentHighlight,
   DocumentHighlightKind,
+  InlayHintKind,
 } from "ty_wasm";
 import { FileId, ReadonlyFiles } from "../Playground";
 import { isPythonFile } from "./Files";
@@ -404,6 +405,15 @@ class PlaygroundServer
       return undefined;
     }
 
+    function mapInlayHintKind(kind: InlayHintKind): languages.InlayHintKind {
+      switch (kind) {
+        case InlayHintKind.Type:
+          return languages.InlayHintKind.Type;
+        case InlayHintKind.Parameter:
+          return languages.InlayHintKind.Parameter;
+      }
+    }
+
     return {
       dispose: () => {},
       hints: inlayHints.map((hint) => ({
@@ -412,6 +422,7 @@ class PlaygroundServer
           lineNumber: hint.position.line,
           column: hint.position.column,
         },
+        kind: mapInlayHintKind(hint.kind),
       })),
     };
   }
