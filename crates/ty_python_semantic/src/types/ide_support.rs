2cf0c73049a69e4836eb9c33b756c3fece66625f
Carl Meyer <carl@astral.sh>
2025-08-22 18:41:11 -0700

    working except for alias directly in union

--- crates/ty_python_semantic/src/types/ide_support.rs
+++ crates/ty_python_semantic/src/types/ide_support.rs
@@ -12,7 +12,7 @@ use crate::semantic_index::{
 };
 use crate::types::call::{CallArguments, MatchedArgument};
 use crate::types::signatures::Signature;
-use crate::types::{ClassBase, ClassLiteral, DynamicType, KnownClass, KnownInstanceType, Type};
+use crate::types::{ClassBase, ClassLiteral, KnownClass, KnownInstanceType, Type};
 use crate::{Db, HasType, NameKind, SemanticModel};
 use ruff_db::files::{File, FileRange};
 use ruff_db::parsed::parsed_module;
@@ -226,7 +226,6 @@ impl<'db> AllMembers<'db> {
                             Type::KnownInstance(
                                 KnownInstanceType::TypeVar(_) | KnownInstanceType::TypeAliasType(_),
                             ) => continue,
-                            Type::Dynamic(DynamicType::TodoTypeAlias) => continue,
                             _ => {}
                         }
                     }
--- crates/ty_python_semantic/src/types/infer.rs
+++ crates/ty_python_semantic/src/types/infer.rs
@@ -128,8 +128,8 @@ use crate::types::unpacker::{UnpackResult, Unpacker};
 use crate::types::{
     CallDunderError, CallableType, ClassLiteral, ClassType, DataclassParams, DynamicType,
     IntersectionBuilder, IntersectionType, KnownClass, KnownInstanceType, LintDiagnosticGuard,
-    MemberLookupPolicy, MetaclassCandidate, PEP695TypeAliasType, Parameter, ParameterForm,
-    Parameters, SpecialFormType, SubclassOfType, Truthiness, Type, TypeAliasType,
+    MemberLookupPolicy, MetaclassCandidate, PEP613TypeAlias, PEP695TypeAlias, Parameter,
+    ParameterForm, Parameters, SpecialFormType, SubclassOfType, Truthiness, Type, TypeAliasType,
     TypeAndQualifiers, TypeIsType, TypeQualifiers, TypeVarBoundOrConstraintsEvaluation,
     TypeVarDefaultEvaluation, TypeVarInstance, TypeVarKind, UnionBuilder, UnionType, binding_type,
     todo_type,
@@ -1852,6 +1852,9 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
             DefinitionKind::TypeVar(typevar) => {
                 self.infer_typevar_deferred(typevar.node(self.module()));
             }
+            DefinitionKind::AnnotatedAssignment(annotated_assignment) => {
+                self.infer_annotated_assignment_deferred(annotated_assignment);
+            }
             _ => {}
         }
     }
@@ -3227,7 +3230,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
             .to_scope_id(self.db(), self.file());
 
         let type_alias_ty = Type::KnownInstance(KnownInstanceType::TypeAliasType(
-            TypeAliasType::PEP695(PEP695TypeAliasType::new(
+            TypeAliasType::PEP695(PEP695TypeAlias::new(
                 self.db(),
                 &type_alias.name.as_name_expr().unwrap().id,
                 rhs_scope,
@@ -4720,6 +4723,34 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
             declared.inner = Type::BooleanLiteral(true);
         }
 
+        // Handle PEP 613 type aliases.
+        if matches!(
+            declared.inner,
+            Type::SpecialForm(SpecialFormType::TypeAlias)
+        ) {
+            let ty = if let Some(name_expr) = target.as_name_expr() {
+                if value.is_some() {
+                    self.deferred.insert(definition);
+                    Type::KnownInstance(KnownInstanceType::TypeAlias(TypeAliasType::PEP613(
+                        PEP613TypeAlias::new(self.db(), name_expr.id.clone(), definition),
+                    )))
+                } else {
+                    // TODO diagnostic: no RHS
+                    Type::unknown()
+                }
+            } else {
+                // TODO diagnostic: target is not a name
+                Type::unknown()
+            };
+            // TODO diagnostic if qualifiers are not empty
+            self.add_declaration_with_binding(
+                target.into(),
+                definition,
+                &DeclaredAndInferredType::AreTheSame(ty.into()),
+            );
+            return;
+        }
+
         // Handle various singletons.
         if let Some(name_expr) = target.as_name_expr() {
             if let Some(special_form) =
@@ -4788,6 +4819,16 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
 
             self.store_expression_type(target, declared.inner_type());
         }
+    }
+
+    fn infer_annotated_assignment_deferred(
+        &mut self,
+        assignment: &'db AnnotatedAssignmentDefinitionKind,
+    ) {
+        // Annotated assignments defer the entire value expression in case of a PEP 613 type alias.
+        // SAFETY `infer_annotated_assignment_definition` does not defer if there is no RHS
+        let value_node = assignment.value(&self.module()).unwrap();
+        self.infer_type_expression(value_node);
     }
 
     fn infer_augmented_assignment_statement(&mut self, assignment: &ast::StmtAugAssign) {
@@ -7582,8 +7623,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
                 todo @ Type::Dynamic(
                     DynamicType::Todo(_)
                     | DynamicType::TodoPEP695ParamSpec
-                    | DynamicType::TodoUnpack
-                    | DynamicType::TodoTypeAlias,
+                    | DynamicType::TodoUnpack,
                 ),
                 _,
                 _,
@@ -7593,8 +7633,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
                 todo @ Type::Dynamic(
                     DynamicType::Todo(_)
                     | DynamicType::TodoPEP695ParamSpec
-                    | DynamicType::TodoUnpack
-                    | DynamicType::TodoTypeAlias,
+                    | DynamicType::TodoUnpack,
                 ),
                 _,
             ) => Some(todo),
@@ -10559,6 +10598,10 @@ impl<'db> TypeInferenceBuilder<'db, '_> {
                 KnownInstanceType::TypeAliasType(_) => {
                     self.infer_type_expression(&subscript.slice);
                     todo_type!("Generic PEP-695 type alias")
+                }
+                KnownInstanceType::TypeAlias(_) => {
+                    self.infer_type_expression(&subscript.slice);
+                    todo_type!("Generic PEP-613 type alias")
                 }
             },
             Type::Dynamic(DynamicType::Todo(_)) => {
