9cea75293469530afa6416d70bce9b933b8eea9b
Andrew Gallant <andrew@astral.sh>
2025-08-26 14:55:19 -0400

    [ty] Require that we can find a root when listing sub-modules
    This is similar to a change made in the "list top-level modules"
    implementation that had been masked by poor Salsa failure modes.
    Basically, if we can't find a root here, it *must* be a bug. And if we
    just silently skip over it, we risk voiding Salsa's purity contract,
    leading to more difficult to debug panics.
    
    This did cause one test to fail, but only because the test wasn't
    properly setting up roots.

--- crates/ty_ide/src/lib.rs
+++ crates/ty_ide/src/lib.rs
@@ -286,10 +286,12 @@ impl HasNavigationTargets for TypeDefinition<'_> {
 
 #[cfg(test)]
 mod tests {
+    use camino::Utf8Component;
     use insta::internals::SettingsBindDropGuard;
+
     use ruff_db::Db;
     use ruff_db::diagnostic::{Diagnostic, DiagnosticFormat, DisplayDiagnosticConfig};
-    use ruff_db::files::{File, system_path_to_file};
+    use ruff_db::files::{File, FileRootKind, system_path_to_file};
     use ruff_db::system::{DbWithWritableSystem, SystemPath, SystemPathBuf};
     use ruff_python_trivia::textwrap::dedent;
     use ruff_text_size::TextSize;
@@ -377,6 +379,19 @@ mod tests {
             {
                 db.write_file(path, contents)
                     .expect("write to memory file system to be successful");
+
+                // Add a root for the top-most component.
+                let top = path.components().find_map(|c| match c {
+                    Utf8Component::Normal(c) => Some(c),
+                    _ => None,
+                });
+                if let Some(top) = top {
+                    let top = SystemPath::new(top);
+                    if db.system().is_directory(top) {
+                        db.files()
+                            .try_add_root(&db, top, FileRootKind::LibrarySearchPath);
+                    }
+                }
 
                 let file = system_path_to_file(&db, path).expect("newly written file to existing");
 
