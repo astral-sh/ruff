1bb5c03f6c639e040c930a51792706ddd3946fba
chiri <chirizxc@proton.me>
2025-08-26 20:56:13 +0300

    simplify and update documentation

--- crates/ruff_linter/src/rules/flake8_use_pathlib/rules/os_path_abspath.rs
+++ crates/ruff_linter/src/rules/flake8_use_pathlib/rules/os_path_abspath.rs
@@ -1,4 +1,4 @@
-use ruff_diagnostics::{Applicability, Edit, Fix};
+use ruff_diagnostics::{Edit, Fix};
 use ruff_macros::{ViolationMetadata, derive_message_formats};
 use ruff_python_ast::ExprCall;
 use ruff_text_size::Ranged;
@@ -40,17 +40,16 @@ use crate::{FixAvailability, Violation};
 /// especially on older versions of Python.
 ///
 /// ## Fix Safety
-/// This rule's fix is marked as unsafe if the replacement would remove comments attached to the original expression.
-///
-/// `Path.resolve()` resolves symlinks, while `os.path.abspath()` does not.
-/// If preserving symlinks is important, you may need to use `Path.absolute()`.
-/// The suggested autofix is marked as unsafe because it can change behavior.
+/// This rule's fix is always marked as unsafe because `Path.resolve()` resolves symlinks, while `os.path.abspath()` does not.
+/// If resolving symlinks is important, you may need to use `Path.absolute()`.
+/// However, `Path.absolute()` also does not remove any `..` components in a path, unlike `os.path.abspath()` and `Path.resolve()`, so if that specific combination of behaviors is required, there's no existing `pathlib` alternative.
+/// See CPython issue [#69200](https://github.com/python/cpython/issues/69200).
 ///
 /// ## References
 /// - [Python documentation: `Path.resolve`](https://docs.python.org/3/library/pathlib.html#pathlib.Path.resolve)
 /// - [Python documentation: `os.path.abspath`](https://docs.python.org/3/library/os.path.html#os.path.abspath)
 /// - [PEP 428 – The pathlib module – object-oriented filesystem paths](https://peps.python.org/pep-0428/)
-/// - [Correspondence between `os` and `pathlib`](https://docs.python.org/3/library/pathlib.html#correspondence-to-tools-in-the-os-module)
+/// - [Correspondence between `os` and `pathlib`](https://docs.python.org/3/library/pathlib.html#corresponding-tools)
 /// - [Why you should be using pathlib](https://treyhunner.com/2018/12/why-you-should-be-using-pathlib/)
 /// - [No really, pathlib is great](https://treyhunner.com/2019/01/no-really-pathlib-is-great/)
 #[derive(ViolationMetadata)]
@@ -108,10 +107,9 @@ pub(crate) fn os_path_abspath(checker: &Checker, call: &ExprCall, segments: &[&s
             format!("{binding}({arg_code}).resolve()")
         };
 
-        Ok(Fix::applicable_edits(
+        Ok(Fix::unsafe_edits(
             Edit::range_replacement(replacement, range),
             [import_edit],
-            Applicability::Unsafe,
         ))
     });
 }
