2355e5bfac39b51fb42f17a4515e92bb87d8019d
Douglas Creager <dcreager@dcreager.net>
2025-08-27 09:15:22 -0400

    use constraint sets for lhs contrained typevars

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -1563,7 +1563,10 @@ impl<'db> Type<'db> {
                     .constraints(db)
                     .when_some_and(|constraints| {
                         constraints.iter().when_all(db, |constraint| {
-                            self.has_relation_to_impl(db, *constraint, relation, visitor)
+                            C::constrain_typevar(db, bound_typevar, *constraint, *constraint)
+                                .implies(db, || {
+                                    self.has_relation_to_impl(db, *constraint, relation, visitor)
+                                })
                         })
                     })
                     .is_never_satisfied() =>
@@ -1577,7 +1580,10 @@ impl<'db> Type<'db> {
                     .constraints(db)
                     .when_some_and(|constraints| {
                         constraints.iter().when_all(db, |constraint| {
-                            self.has_relation_to_impl(db, *constraint, relation, visitor)
+                            C::constrain_typevar(db, bound_typevar, *constraint, *constraint)
+                                .implies(db, || {
+                                    self.has_relation_to_impl(db, *constraint, relation, visitor)
+                                })
                         })
                     })
             }
