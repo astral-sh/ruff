87fd37cc8c1e64b2f9534b0656c3092d23245ff8
Takayuki Maeda <takoyaki0316@gmail.com>
2025-08-28 11:17:54 +0900

    use separated functions

--- crates/ruff_linter/resources/test/fixtures/pyflakes/F401_35.py
+++ crates/ruff_linter/resources/test/fixtures/pyflakes/F401_35.py
@@ -6,7 +6,11 @@ Test: allowed-unused-imports-top-level-module
 
 def f():
     import hvplot
+def f():
     import hvplot.pandas
+def f():
     import hvplot.pandas.plots
+def f():
     from hvplot.pandas import scatter_matrix
+def f():
     from hvplot.pandas.plots import scatter_matrix
