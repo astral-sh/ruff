04f46a2bf0297a6ef92ba8d42f3fcdc85993c904
typeshedbot <>
2025-09-01 00:44:47 +0000

    Sync Windows docstrings

--- crates/ty_vendored/vendor/typeshed/stdlib/tkinter/ttk.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/tkinter/ttk.pyi
@@ -244,7 +244,9 @@ LAYOUTS
             /,
             *,
             padding: _Padding = ...,
-        ) -> None: ...
+        ) -> None:
+            """Create a new element in the current theme of given etype.
+"""
         @overload
         def element_create(
             self,
