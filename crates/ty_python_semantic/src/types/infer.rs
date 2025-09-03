baf30b469d8e35b5d2028f111390f26bfa19e9e9
David Peter <mail@david-peter.de>
2025-09-03 16:46:22 +0200

    Minor adjustments to special_first_method_parameter_type

--- crates/ty_python_semantic/src/types/infer.rs
+++ crates/ty_python_semantic/src/types/infer.rs
@@ -2991,7 +2991,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
         } else {
             let ty = if let Some(default_ty) = default_ty {
                 UnionType::from_elements(self.db(), [Type::unknown(), default_ty])
-            } else if let Some(ty) = self.special_first_method_argument(parameter) {
+            } else if let Some(ty) = self.special_first_method_parameter_type(parameter) {
                 ty
             } else {
                 Type::unknown()
@@ -3001,27 +3001,30 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
     }
 
     /// Special case for unannotated `cls` and `self` arguments to class methods and instance methods.
-    fn special_first_method_argument(&mut self, parameter: &ast::Parameter) -> Option<Type<'db>> {
-        let current_scope_id = self.scope().file_scope_id(self.db());
-        let current_scope = self.index.scope(current_scope_id);
-        let module = &parsed_module(self.db(), self.scope().file(self.db())).load(self.db());
-        let method = current_scope.node().as_function(module)?;
+    fn special_first_method_parameter_type(
+        &mut self,
+        parameter: &ast::Parameter,
+    ) -> Option<Type<'db>> {
+        let current_scope = self.scope().scope(self.db());
+        let module = parsed_module(self.db(), self.scope().file(self.db())).load(self.db());
+        let method = current_scope.node().as_function(&module)?;
 
         let parent_scope_id = current_scope.parent()?;
         let parent_scope = self.index.scope(parent_scope_id);
-        parent_scope.node().as_class(module)?;
+        parent_scope.node().as_class(&module)?;
 
-        let definition = self.index.expect_single_definition(method);
-        let DefinitionKind::Function(func_def) = definition.kind(self.db()) else {
+        let method_definition = self.index.expect_single_definition(method);
+        let DefinitionKind::Function(func_def) = method_definition.kind(self.db()) else {
             return None;
         };
-        let func_type = infer_definition_types(self.db(), definition)
-            .declaration_type(definition)
+
+        let func_type = infer_definition_types(self.db(), method_definition)
+            .declaration_type(method_definition)
             .inner_type()
             .into_function_literal()?;
 
         if func_def
-            .node(module)
+            .node(&module)
             .parameters
             .index(parameter.name())
             .is_some_and(|index| index != 0)
@@ -3035,13 +3038,10 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
         } else if func_type.is_staticmethod(self.db()) {
             return None;
         }
-        let previous_typevar_binding_context = self.typevar_binding_context.replace(definition);
-        let t = Type::SpecialForm(SpecialFormType::TypingSelf)
-            .in_type_expression(self.db(), self.scope(), self.typevar_binding_context)
-            .ok();
 
-        self.typevar_binding_context = previous_typevar_binding_context;
-        t
+        Type::SpecialForm(SpecialFormType::TypingSelf)
+            .in_type_expression(self.db(), self.scope(), Some(method_definition))
+            .ok()
     }
 
     /// Set initial declared/inferred types for a `*args` variadic positional parameter.
