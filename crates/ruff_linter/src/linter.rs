4c3e1930f6543ad4dea3083dfe4a17349eb2fe76
Bhuminjay Soni <Soni5Happy@gmail.com>
2025-09-03 19:43:05 +0530

    [syntax-errors] Detect `yield from` inside async function (#20051)
    <!--
    Thank you for contributing to Ruff/ty! To help us out with reviewing,
    please consider the following:
    
    - Does this pull request include a summary of the change? (See below.)
    - Does this pull request include a descriptive title? (Please prefix
    with `[ty]` for ty pull
      requests.)
    - Does this pull request include references to any relevant issues?
    -->
    
    ## Summary
    
    This PR implements
    https://docs.astral.sh/ruff/rules/yield-from-in-async-function/ as a
    syntax semantic error
    
    ## Test Plan
    
    <!-- How was it tested? -->
    I have written a simple inline test as directed in
    [https://github.com/astral-sh/ruff/issues/17412](https://github.com/astral-sh/ruff/issues/17412)
    
    ---------
    
    Signed-off-by: 11happy <soni5happy@gmail.com>
    Co-authored-by: Alex Waygood <alex.waygood@gmail.com>
    Co-authored-by: Brent Westbrook <36778786+ntBre@users.noreply.github.com>

--- crates/ruff_linter/src/linter.rs
+++ crates/ruff_linter/src/linter.rs
@@ -1231,6 +1231,10 @@ mod tests {
     )]
     #[test_case(Rule::AwaitOutsideAsync, Path::new("await_outside_async_function.py"))]
     #[test_case(Rule::AwaitOutsideAsync, Path::new("async_comprehension.py"))]
+    #[test_case(
+        Rule::YieldFromInAsyncFunction,
+        Path::new("yield_from_in_async_function.py")
+    )]
     fn test_syntax_errors(rule: Rule, path: &Path) -> Result<()> {
         let snapshot = path.to_string_lossy().to_string();
         let path = Path::new("resources/test/fixtures/syntax_errors").join(path);
