c4b6581cfe718bd5bfafa82a260b8f29603e1cda
Brent Westbrook <brentrwestbrook@gmail.com>
2025-09-04 09:45:00 -0400

    Stabilize `unused-unpacked-variable` (`RUF059`)
    The tests looked good. For the docs, I added a `## See also` section pointing to
    the closely-related F841 (unused-variable) and the corresponding section to F841
    pointing back to RUF059. It seems like you'd probably want both of these active
    or at least to know about the other when reading the docs.

--- crates/ruff_linter/src/codes.rs
+++ crates/ruff_linter/src/codes.rs
@@ -1046,7 +1046,7 @@ pub fn code_to_rule(linter: Linter, code: &str) -> Option<(RuleGroup, Rule)> {
         (Ruff, "056") => (RuleGroup::Preview, rules::ruff::rules::FalsyDictGetFallback),
         (Ruff, "057") => (RuleGroup::Stable, rules::ruff::rules::UnnecessaryRound),
         (Ruff, "058") => (RuleGroup::Stable, rules::ruff::rules::StarmapZip),
-        (Ruff, "059") => (RuleGroup::Preview, rules::ruff::rules::UnusedUnpackedVariable),
+        (Ruff, "059") => (RuleGroup::Stable, rules::ruff::rules::UnusedUnpackedVariable),
         (Ruff, "060") => (RuleGroup::Preview, rules::ruff::rules::InEmptyCollection),
         (Ruff, "061") => (RuleGroup::Preview, rules::ruff::rules::LegacyFormPytestRaises),
         (Ruff, "063") => (RuleGroup::Preview, rules::ruff::rules::AccessAnnotationsFromClassDict),
