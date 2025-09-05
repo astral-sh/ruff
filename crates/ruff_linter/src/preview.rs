4bda9dad6826319e8bc271bf08dee8e23de27b7b
Dylan <dylwil3@gmail.com>
2025-09-05 15:27:17 -0500

    [`pylint`] Stabilize adding U+061C to `bidirectional-unicode` (`PLE2502`) (#20276)
    Introduced in #20106. Removed gating. Updated tests. No documentation to
    update.

--- crates/ruff_linter/src/preview.rs
+++ crates/ruff_linter/src/preview.rs
@@ -249,11 +249,6 @@ pub(crate) const fn is_maxsplit_without_separator_fix_enabled(settings: &LinterS
     settings.preview.is_enabled()
 }
 
-// https://github.com/astral-sh/ruff/pull/20106
-pub(crate) const fn is_bidi_forbid_arabic_letter_mark_enabled(settings: &LinterSettings) -> bool {
-    settings.preview.is_enabled()
-}
-
 // https://github.com/astral-sh/ruff/pull/20027
 pub(crate) const fn is_unnecessary_default_type_args_stubs_enabled(
     settings: &LinterSettings,
