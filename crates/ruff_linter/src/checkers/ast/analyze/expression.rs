astral-sh/ruff

commit 5eb5ec987db9c40ceb62a53c943adc5177fd6672

[`flake8-bugbear`] Support non-context-manager calls in `B017` (#19063)
## Summary

Fixes #19050

---------

Co-authored-by: Brent Westbrook <brentrwestbrook@gmail.com>

crates/ruff_linter/src/checkers/ast/analyze/expression.rs
@@ -7,7 +7,9 @@ use ruff_python_semantic::analyze::typing;
 use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
-use crate::preview::is_optional_as_none_in_union_enabled;
+use crate::preview::{
+    is_assert_raises_exception_call_enabled, is_optional_as_none_in_union_enabled,
+};
 use crate::registry::Rule;
 use crate::rules::{
     airflow, flake8_2020, flake8_async, flake8_bandit, flake8_boolean_trap, flake8_bugbear,
@@ -1235,6 +1237,11 @@ pub(crate) fn expression(expr: &Expr, checker: &Checker) {
             }
             if checker.is_rule_enabled(Rule::NonOctalPermissions) {
                 ruff::rules::non_octal_permissions(checker, call);
+            }
+            if checker.is_rule_enabled(Rule::AssertRaisesException)
+                && is_assert_raises_exception_call_enabled(checker.settings())
+            {
+                flake8_bugbear::rules::assert_raises_exception_call(checker, call);
             }
         }
         Expr::Dict(dict) => {

crates/ruff_linter/src/preview.rs
@@ -125,3 +125,8 @@ pub(crate) const fn is_safe_super_call_with_parameters_fix_enabled(
 ) -> bool {
     settings.preview.is_enabled()
 }
+
+// https://github.com/astral-sh/ruff/pull/19063
+pub(crate) const fn is_assert_raises_exception_call_enabled(settings: &LinterSettings) -> bool {
+    settings.preview.is_enabled()
+}

crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
@@ -16,11 +16,14 @@ mod tests {
     use crate::settings::LinterSettings;
     use crate::test::test_path;
 
+    use crate::settings::types::PreviewMode;
+
     use ruff_python_ast::PythonVersion;
 
     #[test_case(Rule::AbstractBaseClassWithoutAbstractMethod, Path::new("B024.py"))]
     #[test_case(Rule::AssertFalse, Path::new("B011.py"))]
-    #[test_case(Rule::AssertRaisesException, Path::new("B017.py"))]
+    #[test_case(Rule::AssertRaisesException, Path::new("B017_0.py"))]
+    #[test_case(Rule::AssertRaisesException, Path::new("B017_1.py"))]
     #[test_case(Rule::AssignmentToOsEnviron, Path::new("B003.py"))]
     #[test_case(Rule::CachedInstanceMethod, Path::new("B019.py"))]
     #[test_case(Rule::ClassAsDataStructure, Path::new("class_as_data_structure.py"))]
@@ -169,6 +172,25 @@ mod tests {
                     extend_immutable_calls: vec!["fastapi.Query".to_string()],
                 },
                 ..LinterSettings::for_rule(Rule::MutableContextvarDefault)
+            },
+        )?;
+        assert_diagnostics!(snapshot, diagnostics);
+        Ok(())
+    }
+
+    #[test_case(Rule::AssertRaisesException, Path::new("B017_0.py"))]
+    #[test_case(Rule::AssertRaisesException, Path::new("B017_1.py"))]
+    fn rules_preview(rule_code: Rule, path: &Path) -> Result<()> {
+        let snapshot = format!(
+            "preview__{}_{}",
+            rule_code.noqa_code(),
+            path.to_string_lossy()
+        );
+        let diagnostics = test_path(
+            Path::new("flake8_bugbear").join(path).as_path(),
+            &LinterSettings {
+                preview: PreviewMode::Enabled,
+                ..LinterSettings::for_rule(rule_code)
             },
         )?;
         assert_diagnostics!(snapshot, diagnostics);

crates/ruff_linter/src/rules/flake8_bugbear/snapshots/ruff_linter__rules__flake8_bugbear__tests__B017_B017_1.py.snap
@@ -1,0 +1,4 @@
+---
+source: crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
+---
+

crates/ruff_linter/src/rules/flake8_bugbear/snapshots/ruff_linter__rules__flake8_bugbear__tests__preview__B017_B017_0.py.snap
@@ -1,0 +1,45 @@
+---
+source: crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
+---
+B017_0.py:23:14: B017 Do not assert blind exception: `Exception`
+   |
+21 | class Foobar(unittest.TestCase):
+22 |     def evil_raises(self) -> None:
+23 |         with self.assertRaises(Exception):
+   |              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ B017
+24 |             raise Exception("Evil I say!")
+   |
+
+B017_0.py:27:14: B017 Do not assert blind exception: `BaseException`
+   |
+26 |     def also_evil_raises(self) -> None:
+27 |         with self.assertRaises(BaseException):
+   |              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ B017
+28 |             raise Exception("Evil I say!")
+   |
+
+B017_0.py:45:10: B017 Do not assert blind exception: `Exception`
+   |
+44 | def test_pytest_raises():
+45 |     with pytest.raises(Exception):
+   |          ^^^^^^^^^^^^^^^^^^^^^^^^ B017
+46 |         raise ValueError("Hello")
+   |
+
+B017_0.py:48:10: B017 Do not assert blind exception: `Exception`
+   |
+46 |         raise ValueError("Hello")
+47 |
+48 |     with pytest.raises(Exception), pytest.raises(ValueError):
+   |          ^^^^^^^^^^^^^^^^^^^^^^^^ B017
+49 |         raise ValueError("Hello")
+   |
+
+B017_0.py:57:36: B017 Do not assert blind exception: `Exception`
+   |
+55 |         raise ValueError("This is also fine")
+56 |
+57 |     with contextlib.nullcontext(), pytest.raises(Exception):
+   |                                    ^^^^^^^^^^^^^^^^^^^^^^^^ B017
+58 |         raise ValueError("Multiple context managers")
+   |

crates/ruff_linter/src/rules/flake8_bugbear/snapshots/ruff_linter__rules__flake8_bugbear__tests__preview__B017_B017_1.py.snap
@@ -1,0 +1,37 @@
+---
+source: crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
+---
+B017_1.py:20:9: B017 Do not assert blind exception: `Exception`
+   |
+18 | class Foobar(unittest.TestCase):
+19 |     def call_form_raises(self) -> None:
+20 |         self.assertRaises(Exception, something_else)
+   |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ B017
+21 |         self.assertRaises(BaseException, something_else)
+   |
+
+B017_1.py:21:9: B017 Do not assert blind exception: `BaseException`
+   |
+19 |     def call_form_raises(self) -> None:
+20 |         self.assertRaises(Exception, something_else)
+21 |         self.assertRaises(BaseException, something_else)
+   |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ B017
+   |
+
+B017_1.py:25:5: B017 Do not assert blind exception: `Exception`
+   |
+24 | def test_pytest_call_form() -> None:
+25 |     pytest.raises(Exception, something_else)
+   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ B017
+26 |     pytest.raises(BaseException, something_else)
+   |
+
+B017_1.py:26:5: B017 Do not assert blind exception: `BaseException`
+   |
+24 | def test_pytest_call_form() -> None:
+25 |     pytest.raises(Exception, something_else)
+26 |     pytest.raises(BaseException, something_else)
+   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ B017
+27 |
+28 |     pytest.raises(Exception, something_else, match="hello")
+   |

crates/ruff_linter/resources/test/fixtures/flake8_bugbear/B017_0.py
@@ -1,10 +1,10 @@
 """
 Should emit:
-B017 - on lines 23 and 41
+B017 - on lines 24, 28, 46, 49, 52, and 58
 """
 import asyncio
 import unittest
-import pytest
+import pytest, contextlib
 
 CONSTANT = True
 

crates/ruff_linter/resources/test/fixtures/flake8_bugbear/B017_1.py
@@ -1,0 +1,28 @@
+"""
+Should emit:
+B017 - on lines 20, 21, 25, and 26
+"""
+import unittest
+import pytest
+
+
+def something_else() -> None:
+    for i in (1, 2, 3):
+        print(i)
+
+
+class Foo:
+    pass
+
+
+class Foobar(unittest.TestCase):
+    def call_form_raises(self) -> None:
+        self.assertRaises(Exception, something_else)
+        self.assertRaises(BaseException, something_else)
+
+
+def test_pytest_call_form() -> None:
+    pytest.raises(Exception, something_else)
+    pytest.raises(BaseException, something_else)
+
+    pytest.raises(Exception, something_else, match="hello")

commit ad0583a934983d04f1e4cf886d5c017f9b345ddd

[`flake8-bugbear`] Stabilize support for non-context-manager calls in `assert-raises-exception` (`B017`) (#20274)
Introduced in #19063. Removed gating, updated tests. Not documented so
docs are the same.

crates/ruff_linter/src/checkers/ast/analyze/expression.rs
@@ -7,9 +7,7 @@ use ruff_python_semantic::analyze::typing;
 use ruff_text_size::Ranged;
 
 use crate::checkers::ast::Checker;
-use crate::preview::{
-    is_assert_raises_exception_call_enabled, is_optional_as_none_in_union_enabled,
-};
+use crate::preview::is_optional_as_none_in_union_enabled;
 use crate::registry::Rule;
 use crate::rules::{
     airflow, flake8_2020, flake8_async, flake8_bandit, flake8_boolean_trap, flake8_bugbear,
@@ -1292,9 +1290,7 @@ pub(crate) fn expression(expr: &Expr, checker: &Checker) {
             if checker.is_rule_enabled(Rule::NonOctalPermissions) {
                 ruff::rules::non_octal_permissions(checker, call);
             }
-            if checker.is_rule_enabled(Rule::AssertRaisesException)
-                && is_assert_raises_exception_call_enabled(checker.settings())
-            {
+            if checker.is_rule_enabled(Rule::AssertRaisesException) {
                 flake8_bugbear::rules::assert_raises_exception_call(checker, call);
             }
         }

crates/ruff_linter/src/preview.rs
@@ -212,11 +212,6 @@ pub(crate) const fn is_safe_super_call_with_parameters_fix_enabled(
     settings.preview.is_enabled()
 }
 
-// https://github.com/astral-sh/ruff/pull/19063
-pub(crate) const fn is_assert_raises_exception_call_enabled(settings: &LinterSettings) -> bool {
-    settings.preview.is_enabled()
-}
-
 // https://github.com/astral-sh/ruff/pull/19100
 pub(crate) const fn is_add_future_annotations_imports_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()

crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
@@ -16,8 +16,6 @@ mod tests {
     use crate::settings::LinterSettings;
     use crate::test::test_path;
 
-    use crate::settings::types::PreviewMode;
-
     use ruff_python_ast::PythonVersion;
 
     #[test_case(Rule::AbstractBaseClassWithoutAbstractMethod, Path::new("B024.py"))]
@@ -172,25 +170,6 @@ mod tests {
                     extend_immutable_calls: vec!["fastapi.Query".to_string()],
                 },
                 ..LinterSettings::for_rule(Rule::MutableContextvarDefault)
-            },
-        )?;
-        assert_diagnostics!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::AssertRaisesException, Path::new("B017_0.py"))]
-    #[test_case(Rule::AssertRaisesException, Path::new("B017_1.py"))]
-    fn rules_preview(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!(
-            "preview__{}_{}",
-            rule_code.noqa_code(),
-            path.to_string_lossy()
-        );
-        let diagnostics = test_path(
-            Path::new("flake8_bugbear").join(path).as_path(),
-            &LinterSettings {
-                preview: PreviewMode::Enabled,
-                ..LinterSettings::for_rule(rule_code)
             },
         )?;
         assert_diagnostics!(snapshot, diagnostics);

crates/ruff_linter/src/rules/flake8_bugbear/snapshots/ruff_linter__rules__flake8_bugbear__tests__B017_B017_1.py.snap
@@ -1,4 +1,41 @@
 ---
 source: crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
 ---
+B017 Do not assert blind exception: `Exception`
+  --> B017_1.py:20:9
+   |
+18 | class Foobar(unittest.TestCase):
+19 |     def call_form_raises(self) -> None:
+20 |         self.assertRaises(Exception, something_else)
+   |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+21 |         self.assertRaises(BaseException, something_else)
+   |
 
+B017 Do not assert blind exception: `BaseException`
+  --> B017_1.py:21:9
+   |
+19 |     def call_form_raises(self) -> None:
+20 |         self.assertRaises(Exception, something_else)
+21 |         self.assertRaises(BaseException, something_else)
+   |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+   |
+
+B017 Do not assert blind exception: `Exception`
+  --> B017_1.py:25:5
+   |
+24 | def test_pytest_call_form() -> None:
+25 |     pytest.raises(Exception, something_else)
+   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+26 |     pytest.raises(BaseException, something_else)
+   |
+
+B017 Do not assert blind exception: `BaseException`
+  --> B017_1.py:26:5
+   |
+24 | def test_pytest_call_form() -> None:
+25 |     pytest.raises(Exception, something_else)
+26 |     pytest.raises(BaseException, something_else)
+   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+27 |
+28 |     pytest.raises(Exception, something_else, match="hello")
+   |

crates/ruff_linter/src/rules/flake8_bugbear/snapshots/ruff_linter__rules__flake8_bugbear__tests__preview__B017_B017_0.py.snap
@@ -1,79 +1,0 @@
----
-source: crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
----
-B017 Do not assert blind exception: `Exception`
-  --> B017_0.py:23:14
-   |
-21 | class Foobar(unittest.TestCase):
-22 |     def evil_raises(self) -> None:
-23 |         with self.assertRaises(Exception):
-   |              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-24 |             raise Exception("Evil I say!")
-   |
-
-B017 Do not assert blind exception: `BaseException`
-  --> B017_0.py:27:14
-   |
-26 |     def also_evil_raises(self) -> None:
-27 |         with self.assertRaises(BaseException):
-   |              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-28 |             raise Exception("Evil I say!")
-   |
-
-B017 Do not assert blind exception: `Exception`
-  --> B017_0.py:45:10
-   |
-44 | def test_pytest_raises():
-45 |     with pytest.raises(Exception):
-   |          ^^^^^^^^^^^^^^^^^^^^^^^^
-46 |         raise ValueError("Hello")
-   |
-
-B017 Do not assert blind exception: `Exception`
-  --> B017_0.py:48:10
-   |
-46 |         raise ValueError("Hello")
-47 |
-48 |     with pytest.raises(Exception), pytest.raises(ValueError):
-   |          ^^^^^^^^^^^^^^^^^^^^^^^^
-49 |         raise ValueError("Hello")
-   |
-
-B017 Do not assert blind exception: `Exception`
-  --> B017_0.py:57:36
-   |
-55 |         raise ValueError("This is also fine")
-56 |
-57 |     with contextlib.nullcontext(), pytest.raises(Exception):
-   |                                    ^^^^^^^^^^^^^^^^^^^^^^^^
-58 |         raise ValueError("Multiple context managers")
-   |
-
-B017 Do not assert blind exception: `Exception`
-  --> B017_0.py:62:10
-   |
-61 | def test_pytest_raises_keyword():
-62 |     with pytest.raises(expected_exception=Exception):
-   |          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-63 |         raise ValueError("Should be flagged")
-   |
-
-B017 Do not assert blind exception: `Exception`
-  --> B017_0.py:68:18
-   |
-66 |     class TestKwargs(unittest.TestCase):
-67 |         def test_method(self):
-68 |             with self.assertRaises(exception=Exception):
-   |                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-69 |                 raise ValueError("Should be flagged")
-   |
-
-B017 Do not assert blind exception: `BaseException`
-  --> B017_0.py:71:18
-   |
-69 |                 raise ValueError("Should be flagged")
-70 |
-71 |             with self.assertRaises(exception=BaseException):
-   |                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-72 |                 raise ValueError("Should be flagged")
-   |

crates/ruff_linter/src/rules/flake8_bugbear/snapshots/ruff_linter__rules__flake8_bugbear__tests__preview__B017_B017_1.py.snap
@@ -1,41 +1,0 @@
----
-source: crates/ruff_linter/src/rules/flake8_bugbear/mod.rs
----
-B017 Do not assert blind exception: `Exception`
-  --> B017_1.py:20:9
-   |
-18 | class Foobar(unittest.TestCase):
-19 |     def call_form_raises(self) -> None:
-20 |         self.assertRaises(Exception, something_else)
-   |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-21 |         self.assertRaises(BaseException, something_else)
-   |
-
-B017 Do not assert blind exception: `BaseException`
-  --> B017_1.py:21:9
-   |
-19 |     def call_form_raises(self) -> None:
-20 |         self.assertRaises(Exception, something_else)
-21 |         self.assertRaises(BaseException, something_else)
-   |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-   |
-
-B017 Do not assert blind exception: `Exception`
-  --> B017_1.py:25:5
-   |
-24 | def test_pytest_call_form() -> None:
-25 |     pytest.raises(Exception, something_else)
-   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-26 |     pytest.raises(BaseException, something_else)
-   |
-
-B017 Do not assert blind exception: `BaseException`
-  --> B017_1.py:26:5
-   |
-24 | def test_pytest_call_form() -> None:
-25 |     pytest.raises(Exception, something_else)
-26 |     pytest.raises(BaseException, something_else)
-   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-27 |
-28 |     pytest.raises(Exception, something_else, match="hello")
-   |