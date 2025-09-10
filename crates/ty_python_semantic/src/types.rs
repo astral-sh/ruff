cbaac56cf1e6b8dad7492625f8b7d88c9029f21a
David Peter <mail@david-peter.de>
2025-09-10 10:56:38 +0200

    [ty] Use 'unknown' specialization for upper bound on Self

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -5657,9 +5657,7 @@ impl<'db> Type<'db> {
                             ],
                         });
                     };
-                    let instance = Type::ClassLiteral(class).to_instance(db).expect(
-                        "nearest_enclosing_class must return type that can be instantiated",
-                    );
+                    let instance = Type::instance(db, class.unknown_specialization(db));
                     let class_definition = class.definition(db);
                     let typevar = TypeVarInstance::new(
                         db,
