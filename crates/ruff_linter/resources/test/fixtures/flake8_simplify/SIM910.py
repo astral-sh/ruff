2e0dcee342c524651e671ad3ab78ee015c96b3b7
Dan <dparizher@gmail.com>
2025-09-11 18:38:01 -0400

    apply preview gate

--- crates/ruff_linter/resources/test/fixtures/flake8_simplify/SIM910.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_simplify/SIM910.py
@@ -56,7 +56,3 @@ def foo():
     dict = {"Tom": 23, "Maria": 23, "Dog": 11}
     age = dict.get("Cat", None)
 
-# https://github.com/astral-sh/ruff/issues/20341
-ages = {"Tom": 23, "Maria": 23, "Dog": 11}
-key_source = {"Thomas": "Tom"}
-age = ages.get(key_source.get("Thomas", "Tom"), None)
