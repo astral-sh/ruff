d91bb5d1402a06ec5725c8a931d628248c192216
dylwil3 <dylwil3@gmail.com>
2025-09-12 13:33:44 -0500

    add link to PR in preview module

--- crates/ruff_linter/src/preview.rs
+++ crates/ruff_linter/src/preview.rs
@@ -236,7 +236,7 @@ pub(crate) const fn is_a003_class_scope_shadowing_expansion_enabled(
     settings.preview.is_enabled()
 }
 
-// TODO
+// https://github.com/astral-sh/ruff/pull/20200
 pub(crate) const fn is_refined_submodule_import_match_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
