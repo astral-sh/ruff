037ac58937c035a972000456d0358520d9c1c1f6
Brent Westbrook <brentrwestbrook@gmail.com>
2025-09-12 10:05:16 -0400

    test on both preview and stable

--- crates/ruff_linter/src/rules/flake8_simplify/mod.rs
+++ crates/ruff_linter/src/rules/flake8_simplify/mod.rs
@@ -60,7 +60,7 @@ mod tests {
     }
 
     #[test_case(Rule::SplitStaticString, Path::new("SIM905.py"))]
-    #[test_case(Rule::DictGetWithNoneDefault, Path::new("SIM910_preview.py"))]
+    #[test_case(Rule::DictGetWithNoneDefault, Path::new("SIM910.py"))]
     fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!(
             "preview__{}_{}",
