7c26318626c76f1c84ed1ef38df7db90d91e8af2
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2025-09-16 17:16:47 -0400

    add cycle handling to `ClassType::into_callable`

--- crates/ty_python_semantic/src/types/class.rs
+++ crates/ty_python_semantic/src/types/class.rs
@@ -1048,7 +1048,7 @@ impl<'db> ClassType<'db> {
 
     /// Return a callable type (or union of callable types) that represents the callable
     /// constructor signature of this class.
-    #[salsa::tracked(heap_size=ruff_memory_usage::heap_size)]
+    #[salsa::tracked(cycle_fn=into_callable_cycle_recover, cycle_initial=into_callable_cycle_initial, heap_size=ruff_memory_usage::heap_size)]
     pub(super) fn into_callable(self, db: &'db dyn Db) -> Type<'db> {
         let self_ty = Type::from(self);
         let metaclass_dunder_call_function_symbol = self_ty
@@ -1206,6 +1206,20 @@ impl<'db> ClassType<'db> {
     pub(super) fn header_span(self, db: &'db dyn Db) -> Span {
         self.class_literal(db).0.header_span(db)
     }
+}
+
+#[allow(clippy::trivially_copy_pass_by_ref)]
+fn into_callable_cycle_recover<'db>(
+    _db: &'db dyn Db,
+    _value: &Type<'db>,
+    _count: u32,
+    _self: ClassType<'db>,
+) -> salsa::CycleRecoveryAction<Type<'db>> {
+    salsa::CycleRecoveryAction::Iterate
+}
+
+fn into_callable_cycle_initial<'db>(_db: &'db dyn Db, _self: ClassType<'db>) -> Type<'db> {
+    Type::Never
 }
 
 impl<'db> From<GenericAlias<'db>> for ClassType<'db> {
