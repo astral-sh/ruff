3879e432f5605475b6c2970714aba8cfca8c797e
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-09-16 13:00:38 +0200

    Clippy

--- crates/ruff_python_codegen/src/generator.rs
+++ crates/ruff_python_codegen/src/generator.rs
@@ -106,6 +106,7 @@ impl<'a> Generator<'a> {
     }
 
     /// Force quote style for generated source code
+    #[must_use]
     pub fn with_quote(mut self, quote: Quote) -> Self {
         self.quote = Some(quote);
         self
