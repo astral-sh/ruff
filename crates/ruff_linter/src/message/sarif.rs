e28f06c302036658b2782d86f218d8171e435438
Micha Reiser <micha@reiser.io>
2025-09-18 09:17:01 +0200

    Omit fix if source file is missing

--- crates/ruff_linter/src/message/sarif.rs
+++ crates/ruff_linter/src/message/sarif.rs
@@ -2,11 +2,12 @@ use std::collections::HashSet;
 use std::io::Write;
 
 use anyhow::Result;
+use log::warn;
 use serde::{Serialize, Serializer};
 use serde_json::json;
 
 use ruff_db::diagnostic::{Diagnostic, SecondaryCode};
-use ruff_source_file::OneIndexed;
+use ruff_source_file::{OneIndexed, SourceFile};
 use ruff_text_size::{Ranged, TextRange};
 
 use crate::VERSION;
@@ -242,67 +243,70 @@ struct SarifRegion {
 }
 
 impl<'a> SarifResult<'a> {
-    fn range_to_sarif_region(diagnostic: &Diagnostic, range: TextRange) -> SarifRegion {
-        if let Some(source_file) = diagnostic.ruff_source_file() {
-            let source_code = source_file.to_source_code();
-            let start_location = source_code.line_column(range.start());
-            let end_location = source_code.line_column(range.end());
+    fn range_to_sarif_region(source_file: &SourceFile, range: TextRange) -> SarifRegion {
+        let source_code = source_file.to_source_code();
+        let start_location = source_code.line_column(range.start());
+        let end_location = source_code.line_column(range.end());
 
-            SarifRegion {
-                start_line: start_location.line,
-                start_column: start_location.column,
-                end_line: end_location.line,
-                end_column: end_location.column,
-            }
-        } else {
-            SarifRegion {
-                start_line: OneIndexed::MIN,
-                start_column: OneIndexed::MIN,
-                end_line: OneIndexed::MIN,
-                end_column: OneIndexed::MIN,
-            }
+        SarifRegion {
+            start_line: start_location.line,
+            start_column: start_location.column,
+            end_line: end_location.line,
+            end_column: end_location.column,
         }
     }
 
-    fn fixes(diagnostic: &'a Diagnostic, uri: &str) -> Vec<SarifFix> {
-        diagnostic
-            .fix()
-            .map(|fix| {
-                let fix_description = diagnostic
-                    .first_help_text()
-                    .map(std::string::ToString::to_string);
+    fn fix(diagnostic: &'a Diagnostic, uri: &str) -> Option<SarifFix> {
+        let Some(fix) = diagnostic.fix() else {
+            return None;
+        };
 
-                let replacements: Vec<SarifReplacement> = fix
-                    .edits()
-                    .iter()
-                    .map(|edit| {
-                        let range = edit.range();
-                        let deleted_region = Self::range_to_sarif_region(diagnostic, range);
-                        SarifReplacement {
-                            deleted_region,
-                            inserted_content: edit.content().map(|content| InsertedContent {
-                                text: content.to_string(),
-                            }),
-                        }
-                    })
-                    .collect();
+        let Some(source_file) = diagnostic.ruff_source_file() else {
+            debug_assert!(
+                false,
+                "Omitting the fix for diagnostic with id `{}` because the source file is missing. This is a bug in Ruff, please report an issue.",
+                diagnostic.id()
+            );
 
-                let artifact_changes = vec![SarifArtifactChange {
-                    artifact_location: SarifArtifactLocation {
-                        uri: uri.to_string(),
-                    },
-                    replacements,
-                }];
+            warn!(
+                "Omitting the fix for diagnostic with id `{}` because the source file is missing. This is a bug in Ruff, please report an issue.",
+                diagnostic.id()
+            );
+            return None;
+        };
 
-                SarifFix {
-                    description: RuleDescription {
-                        text: fix_description,
-                    },
-                    artifact_changes,
+        let fix_description = diagnostic
+            .first_help_text()
+            .map(std::string::ToString::to_string);
+
+        let replacements: Vec<SarifReplacement> = fix
+            .edits()
+            .iter()
+            .map(|edit| {
+                let range = edit.range();
+                let deleted_region = Self::range_to_sarif_region(source_file, range);
+                SarifReplacement {
+                    deleted_region,
+                    inserted_content: edit.content().map(|content| InsertedContent {
+                        text: content.to_string(),
+                    }),
                 }
             })
-            .into_iter()
-            .collect()
+            .collect();
+
+        let artifact_changes = vec![SarifArtifactChange {
+            artifact_location: SarifArtifactLocation {
+                uri: uri.to_string(),
+            },
+            replacements,
+        }];
+
+        Some(SarifFix {
+            description: RuleDescription {
+                text: fix_description,
+            },
+            artifact_changes,
+        })
     }
 
     #[allow(clippy::unnecessary_wraps)]
@@ -334,7 +338,7 @@ impl<'a> SarifResult<'a> {
             message: SarifMessage {
                 text: diagnostic.body().to_string(),
             },
-            fixes: Self::fixes(diagnostic, &uri),
+            fixes: Self::fix(diagnostic, &uri).into_iter().collect(),
             locations: vec![SarifLocation {
                 physical_location: SarifPhysicalLocation {
                     artifact_location: SarifArtifactLocation { uri },
