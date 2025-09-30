2f8a9120cee72ce309815a09e26fa9c412f89dff
Igor Drokin <drokinii1017@gmail.com>
2025-09-30 21:55:23 +0300

    feat(UP010): use secondary annotation to underlying certain unused imports

--- crates/ruff_linter/src/rules/pyupgrade/rules/unnecessary_future_import.rs
+++ crates/ruff_linter/src/rules/pyupgrade/rules/unnecessary_future_import.rs
@@ -1,13 +1,11 @@
-use std::collections::{BTreeSet, HashMap};
-
 use itertools::{Itertools, chain};
-use ruff_python_semantic::NodeId;
-
 use ruff_macros::{ViolationMetadata, derive_message_formats};
 use ruff_python_ast::name::{QualifiedName, QualifiedNameBuilder};
 use ruff_python_ast::{self as ast, Alias, Stmt, StmtRef};
+use ruff_python_semantic::NodeId;
 use ruff_python_semantic::{NameImport, Scope};
 use ruff_text_size::{Ranged, TextRange};
+use std::collections::{BTreeSet, HashMap};
 
 use crate::checkers::ast::Checker;
 use crate::fix;
@@ -38,8 +36,8 @@ use crate::{AlwaysFixableViolation, Applicability, Fix};
 ///
 /// ## Preview
 ///
-/// When [preview] is enabled, this rule creates separate diagnostics
-/// for each unused import instead of grouping them together.
+/// When [preview] is enabled, this rule underlines each unused import individually
+/// instead of grouping them together if there are two or more within an import statement.
 ///
 /// ## Fix safety
 /// This fix is marked unsafe if applying it would delete a comment.
@@ -138,6 +136,7 @@ pub(crate) fn is_import_required_by_isort(
 /// UP010
 pub(crate) fn unnecessary_future_import(checker: &Checker, scope: &Scope) {
     let mut unused_imports: HashMap<NodeId, Vec<&Alias>> = HashMap::new();
+    let mut import_counts: HashMap<NodeId, usize> = HashMap::new();
     for future_name in chain(PY33_PLUS_REMOVE_FUTURES, PY37_PLUS_REMOVE_FUTURES).unique() {
         for binding_id in scope.get_all(future_name) {
             let binding = checker.semantic().binding(binding_id);
@@ -148,6 +147,10 @@ pub(crate) fn unnecessary_future_import(checker: &Checker, scope: &Scope) {
 
                 let stmt = checker.semantic().statement(node_id);
                 if let Stmt::ImportFrom(ast::StmtImportFrom { names, .. }) = stmt {
+                    import_counts
+                        .entry(node_id)
+                        .and_modify(|c| *c += names.len())
+                        .or_insert(names.len());
                     let Some(alias) = names
                         .iter()
                         .find(|alias| alias.name.as_str() == binding.name(checker.source()))
@@ -171,27 +174,20 @@ pub(crate) fn unnecessary_future_import(checker: &Checker, scope: &Scope) {
             }
         }
     }
-    if is_separate_unused_import_diag_enabled(checker.settings()) {
-        for (node_id, unused_aliases) in unused_imports {
-            for unused_alias in unused_aliases {
-                create_diagnostic(checker, &[unused_alias], unused_alias.range(), node_id);
-            }
-        }
-    } else {
-        for (node_id, unused_aliases) in unused_imports {
-            create_diagnostic(
-                checker,
-                unused_aliases.as_slice(),
-                checker.semantic().statement(node_id).range(),
-                node_id,
-            );
-        }
+    for (node_id, unused_aliases) in unused_imports {
+        create_diagnostic(
+            checker,
+            unused_aliases.as_slice(),
+            *import_counts.get(&node_id).unwrap_or(&unused_aliases.len()),
+            checker.semantic().statement(node_id).range(),
+            node_id,
+        );
     }
 }
-
 fn create_diagnostic(
     checker: &Checker,
     unused_aliases: &[&Alias],
+    import_counts: usize,
     range: TextRange,
     node_id: NodeId,
 ) {
@@ -205,6 +201,15 @@ fn create_diagnostic(
         },
         range,
     );
+
+    if is_separate_unused_import_diag_enabled(checker.settings()) && import_counts > 1 {
+        for unused_alias in unused_aliases {
+            diagnostic.secondary_annotation(
+                format!("Unused import `{}`", unused_alias.name),
+                unused_alias.range(),
+            );
+        }
+    }
 
     diagnostic.try_set_fix(|| {
         let statement = checker.semantic().statement(node_id);
