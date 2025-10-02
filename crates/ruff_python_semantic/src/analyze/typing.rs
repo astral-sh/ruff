021910aefaa47d05428ddb3c8c6106b997f233da
Liam <liam@scalzulli.com>
2025-10-02 17:07:08 -0400

    Inline `has_pep_585_generic`

--- crates/ruff_python_semantic/src/analyze/typing.rs
+++ crates/ruff_python_semantic/src/analyze/typing.rs
@@ -8,11 +8,10 @@ use ruff_python_ast::{
     StmtAssign,
 };
 use ruff_python_stdlib::typing::{
-    as_pep_585_generic, has_pep_585_generic, is_immutable_generic_type,
-    is_immutable_non_generic_type, is_immutable_return_type, is_literal_member,
-    is_mutable_return_type, is_pep_593_generic_member, is_pep_593_generic_type,
-    is_standard_library_generic, is_standard_library_generic_member, is_standard_library_literal,
-    is_typed_dict, is_typed_dict_member,
+    as_pep_585_generic, is_immutable_generic_type, is_immutable_non_generic_type,
+    is_immutable_return_type, is_literal_member, is_mutable_return_type, is_pep_593_generic_member,
+    is_pep_593_generic_type, is_standard_library_generic, is_standard_library_generic_member,
+    is_standard_library_literal, is_typed_dict, is_typed_dict_member,
 };
 use ruff_text_size::Ranged;
 use smallvec::{SmallVec, smallvec};
@@ -156,7 +155,8 @@ pub fn is_pep585_generic(
     semantic
         .resolve_qualified_name(expr)
         .is_some_and(|qualified_name| match qualified_name.segments() {
-            [module, member] if has_pep_585_generic(module, member) => true,
+            ["", "dict" | "frozenset" | "list" | "set" | "tuple" | "type"]
+            | ["collections", "deque" | "defaultdict"] => true,
             [
                 "collections",
                 "abc",
