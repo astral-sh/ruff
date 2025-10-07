82279b09c562644458aaec01be15fc49677eaa39
Douglas Creager <dcreager@dcreager.net>
2025-10-07 10:58:46 -0400

    inference heuristic

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -943,6 +943,17 @@ impl<'db> Type<'db> {
         self.apply_type_mapping_impl(db, &TypeMapping::Materialize(materialization_kind), visitor)
     }
 
+    pub(crate) const fn is_type_var(self) -> bool {
+        matches!(self, Type::TypeVar(_))
+    }
+
+    pub(crate) const fn into_type_var(self) -> Option<BoundTypeVarInstance<'db>> {
+        match self {
+            Type::TypeVar(bound_typevar) => Some(bound_typevar),
+            _ => None,
+        }
+    }
+
     pub(crate) const fn into_class_literal(self) -> Option<ClassLiteral<'db>> {
         match self {
             Type::ClassLiteral(class_type) => Some(class_type),
