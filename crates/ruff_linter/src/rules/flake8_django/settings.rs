b93c132e668d23529e3dda296335f4f734f108ca
Jonas Vacek <jvacek@users.noreply.github.com>
2025-10-07 10:04:33 +0200

    cargo clippy --fix

--- crates/ruff_linter/src/rules/flake8_django/settings.rs
+++ crates/ruff_linter/src/rules/flake8_django/settings.rs
@@ -4,17 +4,9 @@ use crate::display_settings;
 use ruff_macros::CacheKey;
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Clone, CacheKey)]
+#[derive(Debug, Clone, CacheKey, Default)]
 pub struct Settings {
     pub additional_path_functions: Vec<String>,
-}
-
-impl Default for Settings {
-    fn default() -> Self {
-        Self {
-            additional_path_functions: vec![],
-        }
-    }
 }
 
 impl Display for Settings {
