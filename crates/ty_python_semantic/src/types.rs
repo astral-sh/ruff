b22124715ddcf55ce48e5689f8d084e94cf8df80
David Peter <mail@david-peter.de>
2025-10-13 14:21:02 +0200

    [ty] Treat functions, methods, and dynamic types as function-like Callables

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -1402,7 +1402,10 @@ impl<'db> Type<'db> {
         match self {
             Type::Callable(_) => Some(self),
 
-            Type::Dynamic(_) => Some(CallableType::single(db, Signature::dynamic(self))),
+            Type::Dynamic(_) => Some(CallableType::single_function_like(
+                db,
+                Signature::dynamic(self),
+            )),
 
             Type::FunctionLiteral(function_literal) => {
                 Some(Type::Callable(function_literal.into_callable_type(db)))
@@ -9771,7 +9774,7 @@ impl<'db> BoundMethodType<'db> {
                     .iter()
                     .map(|signature| signature.bind_self(db, Some(self_instance))),
             ),
-            false,
+            true,
         )
     }
 
@@ -9866,6 +9869,16 @@ impl<'db> CallableType<'db> {
             db,
             CallableSignature::single(signature),
             false,
+        ))
+    }
+
+    /// Create a callable type with a single non-overloaded signature. Mark the callable
+    /// as function-like.
+    pub(crate) fn single_function_like(db: &'db dyn Db, signature: Signature<'db>) -> Type<'db> {
+        Type::Callable(CallableType::new(
+            db,
+            CallableSignature::single(signature),
+            true,
         ))
     }
 
