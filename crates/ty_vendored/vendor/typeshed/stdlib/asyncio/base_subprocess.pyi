990f6881a8134fb0f3cedcf4af771001b2299278
typeshedbot <>
2025-10-15 15:27:51 +0000

    Sync Linux docstrings

--- crates/ty_vendored/vendor/typeshed/stdlib/asyncio/base_subprocess.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/asyncio/base_subprocess.pyi
@@ -52,7 +52,11 @@ class BaseSubprocessTransport(transports.SubprocessTransport):
     def _pipe_connection_lost(self, fd: int, exc: BaseException | None) -> None: ...  # undocumented
     def _pipe_data_received(self, fd: int, data: bytes) -> None: ...  # undocumented
     def _process_exited(self, returncode: int) -> None: ...  # undocumented
-    async def _wait(self) -> int: ...  # undocumented
+    async def _wait(self) -> int:  # undocumented
+        """Wait until the process exit and return the process return code.
+
+This method is a coroutine.
+"""
     def _try_finish(self) -> None: ...  # undocumented
     def _call_connection_lost(self, exc: BaseException | None) -> None: ...  # undocumented
     def __del__(self) -> None: ...
