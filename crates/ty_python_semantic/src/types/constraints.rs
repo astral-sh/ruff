362a1a2c52cdd7eb6fa3e6698a9449c11c83bef3
Douglas Creager <dcreager@dcreager.net>
2025-10-21 12:49:15 -0400

    doc implies 2

--- crates/ty_python_semantic/src/types/constraints.rs
+++ crates/ty_python_semantic/src/types/constraints.rs
@@ -1236,6 +1236,11 @@ impl<'db> ConstraintAssignment<'db> {
         *self = self.negated();
     }
 
+    /// Returns whether this constraint implies contains another — i.e., whether every type that
+    /// satisfies this constraint also satisfies `other`.
+    ///
+    /// This is used (among other places) to simplify how we display constraint sets, by removing
+    /// redundant constraints from a clause.
     fn implies(self, db: &'db dyn Db, other: Self) -> bool {
         match (self, other) {
             // For two positive constraints, one range has to fully contain the other; the smaller
