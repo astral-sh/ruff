c7aa20a0ab7dd16b876f03a53c2210aca9c3c13f
Jack O'Connor <oconnor663@gmail.com>
2025-10-23 11:00:50 -0700

    NewType and NewTypeBase structs, sans methods

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -8011,6 +8011,43 @@ impl<'db> FieldInstance<'db> {
     }
 }
 
+/// A `typing.NewType` declaration, either from the perspective of the
+/// identity-function-that-acts-like-a-subclass-in-type-expressions returned by the call to
+/// `typing.NewType`, or from the perspective of instances of that subclass. For example:
+///
+/// ```py
+/// import typing
+/// Foo = typing.NewType("Foo", int)
+/// x = Foo(42)
+/// ```
+///
+/// The revealed types there are:
+/// - `typing.NewType`: `Type::ClassLiteral(ClassLiteral)` with `KnownClass::NewType`.
+/// - `Foo`: `Type::KnownInstance(KnownInstanceType::NewType(NewType { .. }))`
+/// - `x`: `Type::NewTypeInstance(NewType { .. })`
+#[salsa::interned(debug, heap_size=ruff_memory_usage::heap_size)]
+#[derive(PartialOrd, Ord)]
+pub struct NewType<'db> {
+    /// The name of this NewType (e.g. `"Foo"`)
+    #[returns(ref)]
+    pub name: ast::name::Name,
+
+    /// The binding where this NewType is first created.
+    pub definition: Definition<'db>,
+
+    // The base class of this NewType (e.g. `int`), which could be a (specialized) class type or
+    // could be another NewType.
+    pub base: NewTypeBase<'db>,
+}
+
+impl get_size2::GetSize for NewType<'_> {}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, get_size2::GetSize)]
+pub enum NewTypeBase<'db> {
+    ClassType(ClassType<'db>),
+    NewType(NewType<'db>),
+}
+
 /// Whether this typevar was created via the legacy `TypeVar` constructor, using PEP 695 syntax,
 /// or an implicit typevar like `Self` was used.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, get_size2::GetSize)]
