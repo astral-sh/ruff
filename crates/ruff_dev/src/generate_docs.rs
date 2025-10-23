39048136925f42b248fd58d6b3877800f2b61393
Brent Westbrook <36778786+ntBre@users.noreply.github.com>
2025-10-23 10:14:40 -0400

    improve rule issue link
    Co-authored-by: GiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>

--- crates/ruff_dev/src/generate_docs.rs
+++ crates/ruff_dev/src/generate_docs.rs
@@ -35,7 +35,7 @@ pub(crate) fn main(args: &Args) -> Result<()> {
                 &mut output,
                 r#"<small>
 Added in <a href="https://github.com/astral-sh/ruff/releases/tag/{version}">{version}</a> ·
-<a href="https://github.com/astral-sh/ruff/issues?q=sort%3Aupdated-desc%20is%3Aissue%20is%3Aopen%20{encoded_name}" target="_blank">Related issues</a> ·
+<a href="https://github.com/astral-sh/ruff/issues?q=sort%3Aupdated-desc%20is%3Aissue%20is%3Aopen%20(%27{encoded_name}%27%20OR%20{rule_code})" target="_blank">Related issues</a> ·
 <a href="https://github.com/astral-sh/ruff/blob/main/{file}#L{line}" target="_blank">View source</a>
 </small>
 
@@ -44,12 +44,12 @@ Added in <a href="https://github.com/astral-sh/ruff/releases/tag/{version}">{ver
                 encoded_name =
                     url::form_urlencoded::byte_serialize(rule.name().as_str().as_bytes())
                         .collect::<String>(),
+                rule_code = rule.noqa_code(),
                 file =
                     url::form_urlencoded::byte_serialize(rule.file().replace('\\', "/").as_bytes())
                         .collect::<String>(),
                 line = rule.line(),
             );
-
             let (linter, _) = Linter::parse_code(&rule.noqa_code().to_string()).unwrap();
             if linter.url().is_some() {
                 let common_prefix: String = match linter.common_prefix() {
