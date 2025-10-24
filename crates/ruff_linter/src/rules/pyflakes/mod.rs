ecc0bef6f122f41cc704ca8bcac0e6c9a6ec3bb2
dylwil3 <dylwil3@gmail.com>
2025-10-24 13:53:47 -0500

    add fixture with TYPE_CHECKING

--- crates/ruff_linter/src/rules/pyflakes/mod.rs
+++ crates/ruff_linter/src/rules/pyflakes/mod.rs
@@ -548,6 +548,18 @@ mod tests {
         ",
         "f401_different_branch"
     )]
+    #[test_case(
+        r"
+        import mlflow.pyfunc.loaders.chat_agent
+        import mlflow.pyfunc.loaders.chat_model
+        import mlflow.pyfunc.loaders.code_model
+        from mlflow.utils.pydantic_utils import IS_PYDANTIC_V2_OR_NEWER
+
+        if IS_PYDANTIC_V2_OR_NEWER:
+            import mlflow.pyfunc.loaders.responses_agent
+        ",
+        "f401_type_checking"
+    )]
     fn f401_preview_refined_submodule_handling(contents: &str, snapshot: &str) {
         let diagnostics = test_contents(
             &SourceKind::Python(dedent(contents).to_string()),
