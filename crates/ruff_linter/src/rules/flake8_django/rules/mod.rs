a7ad21caa5b9cecc6ca9d42b40edddf593bf00a2
Jonas Vacek <jvacek@users.noreply.github.com>
2025-10-29 19:55:24 +0100

    implement DJ014

--- crates/ruff_linter/src/rules/flake8_django/rules/mod.rs
+++ crates/ruff_linter/src/rules/flake8_django/rules/mod.rs
@@ -5,6 +5,7 @@ pub(crate) use model_without_dunder_str::*;
 pub(crate) use non_leading_receiver_decorator::*;
 pub(crate) use nullable_model_string_field::*;
 pub(crate) use unordered_body_content_in_model::*;
+pub(crate) use url_path_without_trailing_slash::*;
 
 mod all_with_model_form;
 mod exclude_with_model_form;
@@ -13,3 +14,4 @@ mod model_without_dunder_str;
 mod non_leading_receiver_decorator;
 mod nullable_model_string_field;
 mod unordered_body_content_in_model;
+mod url_path_without_trailing_slash;
