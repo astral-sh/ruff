3328deb5af04b9493369c78e4e3ee0e990794353
Peter Law <PeterJCLaw@gmail.com>
2025-10-30 21:21:52 +0000

    Update block output

--- demo-proj/block.rs
+++ demo-proj/block.rs
@@ -182,3 +182,187 @@ Block {
   ],
   trailer: None
 }
+Block {
+  nested: false,
+  imports: [
+    Import(StmtImport {
+      node_index: NodeIndex(None),
+      range: 0..9,
+      names: [Alias {
+        range: 7..9,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("os"),
+          range: 7..9,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }]
+    }),
+    Import(StmtImport {
+      node_index: NodeIndex(None),
+      range: 11..21,
+      names: [Alias {
+        range: 18..21,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("bar"),
+          range: 18..21,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }]
+    }),
+    Import(StmtImport {
+      node_index: NodeIndex(None),
+      range: 22..32,
+      names: [Alias {
+        range: 29..32,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("foo"),
+          range: 29..32,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }]
+    })
+  ],
+  trailer: Some(FunctionDef)
+}
+Block {
+  nested: false,
+  imports: [
+    Import(StmtImport {
+      node_index: NodeIndex(None),
+      range: 56..67,
+      names: [Alias {
+        range: 63..67,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("last"),
+          range: 63..67,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }]
+    }),
+    Import(StmtImport {
+      node_index: NodeIndex(None),
+      range: 68..79,
+      names: [Alias {
+        range: 75..79,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("late"),
+          range: 75..79,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }]
+    }),
+    ImportFrom(StmtImportFrom {
+      node_index: NodeIndex(None),
+      range: 91..120,
+      module: Some(Identifier {
+        id: Name("late_paren1"),
+        range: 96..107,
+        node_index: NodeIndex(None)
+      }),
+      names: [Alias {
+        range: 115..120,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("value"),
+          range: 115..120,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }],
+      level: 0
+    }),
+    ImportFrom(StmtImportFrom {
+      node_index: NodeIndex(None),
+      range: 132..161,
+      module: Some(Identifier {
+        id: Name("late_paren2"),
+        range: 137..148,
+        node_index: NodeIndex(None)
+      }),
+      names: [Alias {
+        range: 156..161,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("value"),
+          range: 156..161,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }],
+      level: 0
+    }),
+    ImportFrom(StmtImportFrom {
+      node_index: NodeIndex(None),
+      range: 173..202,
+      module: Some(Identifier {
+        id: Name("late_paren3"),
+        range: 178..189,
+        node_index: NodeIndex(None)
+      }),
+      names: [Alias {
+        range: 197..202,
+        node_index: NodeIndex(None),
+        name: Identifier {
+          id: Name("value"),
+          range: 197..202,
+          node_index: NodeIndex(None)
+        },
+        asname: None
+      }],
+      level: 0
+    }),
+    ImportFrom(StmtImportFrom {
+      node_index: NodeIndex(None),
+      range: 214..288,
+      module: Some(Identifier {
+        id: Name("late_paren4"),
+        range: 219..230,
+        node_index: NodeIndex(None)
+      }),
+      names: [
+        Alias {
+          range: 244..250,
+          node_index: NodeIndex(None),
+          name: Identifier {
+            id: Name("value1"),
+            range: 244..250,
+            node_index: NodeIndex(None)
+          },
+          asname: None
+        },
+        Alias {
+          range: 256..262,
+          node_index: NodeIndex(None),
+          name: Identifier {
+            id: Name("value2"),
+            range: 256..262,
+            node_index: NodeIndex(None)
+          },
+          asname: None
+        },
+        Alias {
+          range: 279..285,
+          node_index: NodeIndex(None),
+          name: Identifier {
+            id: Name("value3"),
+            range: 279..285,
+            node_index: NodeIndex(None)
+          },
+          asname: None
+        }
+      ],
+      level: 0
+    })
+  ],
+  trailer: None
+}
