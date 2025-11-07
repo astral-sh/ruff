276f1d0d88d7815f70fabb712af44bb4de85d9a7
Brent Westbrook <36778786+ntBre@users.noreply.github.com>
2025-11-07 12:47:21 -0500

    Remove duplicate preview tests for `FURB101` and `FURB103` (#21303)
    Summary
    --
    
    These rules are themselves in preview, so we don't need the additional
    preview checks on the fixes or the separate preview tests. This has
    confused me in a couple of reviews of changes to the fixes.
    
    Test Plan
    --
    
    Existing tests, with the fixes previously only shown in the preview
    tests now in the "non-preview" tests.

--- crates/ruff_linter/src/rules/refurb/mod.rs
+++ crates/ruff_linter/src/rules/refurb/mod.rs
@@ -12,7 +12,6 @@ mod tests {
     use test_case::test_case;
 
     use crate::registry::Rule;
-    use crate::settings::types::PreviewMode;
     use crate::test::test_path;
     use crate::{assert_diagnostics, settings};
 
@@ -58,25 +57,6 @@ mod tests {
         let diagnostics = test_path(
             Path::new("refurb").join(path).as_path(),
             &settings::LinterSettings::for_rule(rule_code),
-        )?;
-        assert_diagnostics!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::ReadWholeFile, Path::new("FURB101.py"))]
-    #[test_case(Rule::WriteWholeFile, Path::new("FURB103.py"))]
-    fn preview_rules(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!(
-            "preview_{}_{}",
-            rule_code.noqa_code(),
-            path.to_string_lossy()
-        );
-        let diagnostics = test_path(
-            Path::new("refurb").join(path).as_path(),
-            &settings::LinterSettings {
-                preview: PreviewMode::Enabled,
-                ..settings::LinterSettings::for_rule(rule_code)
-            },
         )?;
         assert_diagnostics!(snapshot, diagnostics);
         Ok(())
