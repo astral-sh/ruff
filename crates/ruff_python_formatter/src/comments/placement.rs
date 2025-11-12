ecccfd2a560a07e3e65136868ddb83a68714613d
Brent Westbrook <brentrwestbrook@gmail.com>
2025-11-12 15:01:39 -0500

    dangling operator comment
    fix leading comments

--- crates/ruff_python_formatter/src/comments/placement.rs
+++ crates/ruff_python_formatter/src/comments/placement.rs
@@ -1861,11 +1861,17 @@ fn handle_unary_op_comment<'a>(
             | SimpleTokenKind::Plus
             | SimpleTokenKind::Minus
     )));
-    let up_to = tokenizer
+    let lparen_start = tokenizer
         .find(|token| token.kind == SimpleTokenKind::LParen)
-        .map_or(unary_op.operand.start(), |lparen| lparen.start());
-    if comment.end() < up_to {
-        CommentPlacement::leading(&*unary_op.operand, comment)
+        .map(|lparen| lparen.start());
+    let up_to = lparen_start.unwrap_or(unary_op.operand.start());
+    if lparen_start.is_none()
+        && comment.end() < unary_op.operand.start()
+        && comment.line_position().is_end_of_line()
+    {
+        CommentPlacement::dangling(unary_op, comment)
+    } else if comment.end() < up_to {
+        CommentPlacement::leading(unary_op, comment)
     } else {
         CommentPlacement::Default(comment)
     }
