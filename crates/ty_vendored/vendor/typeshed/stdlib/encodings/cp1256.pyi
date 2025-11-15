f0d074240862b0053c72257312eb6aeb75d7f323
typeshedbot <>
2025-11-15 00:30:18 +0000

    Sync typeshed. Source commit: https://github.com/python/typeshed/commit/f8cdc0bd526301e873cd952eb0d457bdf2554e57

--- crates/ty_vendored/vendor/typeshed/stdlib/encodings/cp1256.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/encodings/cp1256.pyi
@@ -1,5 +1,3 @@
-"""Python Character Mapping Codec cp1256 generated from 'MAPPINGS/VENDORS/MICSFT/WINDOWS/CP1256.TXT' with gencodec.py."""
-
 import codecs
 from _codecs import _EncodingMap
 from _typeshed import ReadableBuffer
--- crates/ty_vendored/vendor/typeshed/stdlib/stringprep.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/stringprep.pyi
@@ -1,9 +1,3 @@
-"""Library that exposes various tables found in the StringPrep RFC 3454.
-
-There are two kinds of tables: sets, for which a member test is provided,
-and mappings, for which a mapping function is provided.
-"""
-
 from typing import Final
 
 b1_set: Final[set[int]]
