4afb67b61ee7bded417bcadda83a5e7a341d7153
Micha Reiser <micha@reiser.io>
2025-11-17 09:43:07 +0100

    Discard changes to crates/ruff_server/src/session/index.rs

--- crates/ruff_server/src/session/index.rs
+++ crates/ruff_server/src/session/index.rs
@@ -430,11 +430,6 @@ impl WorkspaceSettingsIndex {
             anyhow!("Failed to convert workspace URL to file path: {workspace_url}")
         })?;
 
-        tracing::debug!(
-            "options for workspace {workspace_url}: {workspace_options:#?}",
-            workspace_options = workspace.options()
-        );
-
         let client_settings = if let Some(workspace_options) = workspace.options() {
             let options = workspace_options.clone().combine(global.options().clone());
             let settings = match options.into_settings() {
@@ -451,8 +446,6 @@ impl WorkspaceSettingsIndex {
         } else {
             global.to_settings_arc()
         };
-
-        tracing::debug!("Resolved client settings: {client_settings:#?}");
 
         let workspace_settings_index = ruff_settings::RuffSettingsIndex::new(
             client,
