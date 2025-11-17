65f1a20e381427ad57dfccaeec9cf0da92f535ae
Alex Waygood <alex.waygood@gmail.com>
2025-11-17 18:54:43 +0000

    remove done TODO

--- crates/ty_python_semantic/src/types/infer/builder.rs
+++ crates/ty_python_semantic/src/types/infer/builder.rs
@@ -548,7 +548,6 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
             "Inferring deferred types should not add more deferred definitions"
         );
 
-        // TODO: Only call this function when diagnostics are enabled.
         if self.db().should_check_file(self.file()) {
             self.check_class_definitions();
             self.check_overloaded_functions(node);
