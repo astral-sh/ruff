d5a95ec8246aae0656c3be6b5b23741e84fa8a86
David Peter <sharkdp@users.noreply.github.com>
2025-11-18 09:06:05 +0100

    [ty] Implicit type aliases: Add support for `Callable` (#21496)
    ## Summary
    
    Add support for `Callable` special forms in implicit type aliases.
    
    ## Typing conformance
    
    Four new tests are passing
    
    ## Ecosystem impact
    
    * All of the `invalid-type-form` errors are from libraries that use
    `mypy_extensions` and do something like `Callable[[NamedArg("x", str)],
    int]`.
    * A handful of new false positives because we do not support generic
    specializations of implicit type aliases, yet. But other
    * Everything else looks like true positives or known limitations
    
    ## Test Plan
    
    New Markdown tests.

--- crates/ty_python_semantic/src/types/class_base.rs
+++ crates/ty_python_semantic/src/types/class_base.rs
@@ -174,6 +174,7 @@ impl<'db> ClassBase<'db> {
                 | KnownInstanceType::Deprecated(_)
                 | KnownInstanceType::Field(_)
                 | KnownInstanceType::ConstraintSet(_)
+                | KnownInstanceType::Callable(_)
                 | KnownInstanceType::UnionType(_)
                 | KnownInstanceType::Literal(_)
                 // A class inheriting from a newtype would make intuitive sense, but newtype
