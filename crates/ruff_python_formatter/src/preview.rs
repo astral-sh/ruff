1b586430402d6fd660102284829a411440966476
Brent Westbrook <brentrwestbrook@gmail.com>
2025-11-20 15:12:22 -0500

    wip: parenthesize long lambda bodies

--- crates/ruff_python_formatter/src/preview.rs
+++ crates/ruff_python_formatter/src/preview.rs
@@ -54,6 +54,13 @@ pub(crate) const fn is_avoid_parens_for_long_as_captures_enabled(
 }
 
 /// Returns `true` if the
+/// [`parenthesize_lambda_bodies`](https://github.com/astral-sh/ruff/pull/21385) preview style is
+/// enabled.
+pub(crate) const fn is_parenthesize_lambda_bodies_enabled(context: &PyFormatContext) -> bool {
+    context.is_preview()
+}
+
+/// Returns `true` if the
 /// [`force_single_line_lambda_parameters`](https://github.com/astral-sh/ruff/pull/21385) preview
 /// style is enabled.
 pub(crate) const fn is_force_single_line_lambda_parameters_enabled(
