2eaf6e87f689070893df4ef0bbf32156482f307d
Vasilios Syrakis <cetanu@gmail.com>
2025-11-29 00:22:52 +1100

    Adding the junit option
    This uses existing code from ruff and "just works".
    The test cases output are okay, but they are missing
    bits of data that would be useful, and would normally
    be present in the default output format.
    
    I'm not sure what approach I will take from here, but
    I think, if it's possible, it would be good to have the
    entire text message that would normally appear, in the
    failure message of the junit test cases. Any decent junit
    report parser should display these well, in places such as
    the results of a CI job.

--- crates/ty_project/src/metadata/options.rs
+++ crates/ty_project/src/metadata/options.rs
@@ -1173,6 +1173,9 @@ pub enum OutputFormat {
     ///
     /// [GitHub Actions]: https://docs.github.com/en/actions/reference/workflows-and-actions/workflow-commands#setting-an-error-message
     Github,
+    /// Print diagnostics as a JUnit-style XML report, commonly used by test &
+    /// Continuous-Integration tools
+    Junit,
 }
 
 impl OutputFormat {
@@ -1193,6 +1196,7 @@ impl From<OutputFormat> for DiagnosticFormat {
             OutputFormat::Concise => Self::Concise,
             OutputFormat::Gitlab => Self::Gitlab,
             OutputFormat::Github => Self::Github,
+            OutputFormat::Junit => Self::Junit,
         }
     }
 }
@@ -1228,7 +1232,7 @@ pub struct TerminalOptions {
     #[serde(skip_serializing_if = "Option::is_none")]
     #[option(
         default = r#"full"#,
-        value_type = "full | concise",
+        value_type = "full | concise | github | gitlab | junit",
         example = r#"
             output-format = "concise"
         "#
