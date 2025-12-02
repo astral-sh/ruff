ce230191c16ef2611c79541900e20c554ca0ac72
Brent Westbrook <brentrwestbrook@gmail.com>
2025-12-02 13:12:39 -0500

    add failing ruff tests

--- crates/ruff_linter/resources/test/fixtures/pyflakes/F704.py
+++ crates/ruff_linter/resources/test/fixtures/pyflakes/F704.py
@@ -17,3 +17,20 @@ def _():
 
     # Valid yield scope
     yield 3
+
+
+# await is valid in any generator, sync or async
+(await cor async for cor in f())  # ok
+(await cor for cor in f())  # ok
+
+# but not in comprehensions
+[await cor async for cor in f()]  # F704
+{await cor async for cor in f()}  # F704
+{await cor: 1 async for cor in f()}  # F704
+[await cor for cor in f()]  # F704
+{await cor for cor in f()}  # F704
+{await cor: 1 for cor in f()}  # F704
+
+# or in the iterator of an async generator, which is evaluated in the parent
+# scope
+(cor async for cor in await f())  # F704
