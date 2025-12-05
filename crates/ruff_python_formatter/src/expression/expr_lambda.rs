fe255d1ad27ebd8ff94e365bcb5f5aebae297c7d
Brent Westbrook <brentrwestbrook@gmail.com>
2025-12-05 13:39:37 -0500

    leading_comments in block

--- crates/ruff_python_formatter/src/expression/expr_lambda.rs
+++ crates/ruff_python_formatter/src/expression/expr_lambda.rs
@@ -3,7 +3,7 @@ use ruff_python_ast::{AnyNodeRef, Expr, ExprLambda};
 use ruff_text_size::Ranged;
 
 use crate::builders::parenthesize_if_expands;
-use crate::comments::{dangling_comments, trailing_comments};
+use crate::comments::{dangling_comments, leading_comments, trailing_comments};
 use crate::expression::parentheses::{NeedsParentheses, OptionalParentheses, Parentheses};
 use crate::expression::{CallChainLayout, has_own_parentheses};
 use crate::other::parameters::ParametersParentheses;
@@ -95,7 +95,7 @@ impl FormatNodeRule<ExprLambda> for FormatExprLambda {
                         token("("),
                         trailing_comments(dangling_end_of_line),
                         block_indent(&format_args!(
-                            dangling_comments(dangling_own_line),
+                            leading_comments(dangling_own_line),
                             body.format().with_options(Parentheses::Never)
                         )),
                         token(")")
