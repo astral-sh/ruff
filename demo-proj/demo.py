cb4619d79e421ab820516f4e42692ff95ebe2b51
Peter Law <PeterJCLaw@gmail.com>
2025-12-13 23:24:23 +0000

    More demo details

--- demo-proj/demo.py
+++ demo-proj/demo.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+"""Demo file for testing isort float-to-top and E402 fix behaviours"""
+
+from __future__ import annotations
+
 import bar
 import foo
 import os
@@ -8,22 +14,22 @@ def foo():
 
 import last
 
-import late  # comment
+import late  # comment-late
 
-from late_paren1 import (  # comment
+from late_paren1 import (  # comment-late_paren1
     value
 )
 
 from late_paren2 import (
-    value  # comment
+    value  # comment-late_paren2
 )
 
 from late_paren3 import (
     value
-)  # comment
+)  # comment-late_paren3
 
 from late_paren4 import (
     value1,
-    value2,  # comment
+    value2,  # comment-late_paren4
     value3,
 )
