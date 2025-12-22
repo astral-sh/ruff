95c182d682d478928f6aa3aa61f092e02e436de7
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-12-22 12:58:25 +0000

    Fix tests

--- crates/ty_ide/src/inlay_hints.rs
+++ crates/ty_ide/src/inlay_hints.rs
@@ -1661,6 +1661,7 @@ mod tests {
         ---------------------------------------------
         info[inlay-hint-edit]: File after edits
         info: Source
+        from typing import Literal
 
         def i(x: int, /) -> int:
             return x
