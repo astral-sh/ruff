58b59bb62271554e7a4fb663ef208a822a470a1f
typeshedbot <>
2025-12-30 16:54:36 +0000

    Sync Linux docstrings

--- crates/ty_vendored/vendor/typeshed/stdlib/multiprocessing/forkserver.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/multiprocessing/forkserver.pyi
@@ -10,10 +10,29 @@ MAXFDS_TO_SEND: Final = 256
 SIGNED_STRUCT: Final[Struct]
 
 class ForkServer:
-    def set_forkserver_preload(self, modules_names: list[str]) -> None: ...
-    def get_inherited_fds(self) -> list[int] | None: ...
-    def connect_to_new_process(self, fds: Sequence[int]) -> tuple[int, int]: ...
-    def ensure_running(self) -> None: ...
+    def set_forkserver_preload(self, modules_names: list[str]) -> None:
+        """Set list of module names to try to load in forkserver process."""
+    def get_inherited_fds(self) -> list[int] | None:
+        """Return list of fds inherited from parent process.
+
+This returns None if the current process was not started by fork
+server.
+"""
+    def connect_to_new_process(self, fds: Sequence[int]) -> tuple[int, int]:
+        """Request forkserver to create a child process.
+
+Returns a pair of fds (status_r, data_w).  The calling process can read
+the child process's pid and (eventually) its returncode from status_r.
+The calling process should write to data_w the pickled preparation and
+process data.
+"""
+    def ensure_running(self) -> None:
+        """Make sure that a fork server is running.
+
+This can be called from any process.  Note that usually a child
+process will just reuse the forkserver started by its parent, so
+ensure_running() will do nothing.
+"""
 
 if sys.version_info >= (3, 14):
     def main(
@@ -24,7 +43,8 @@ if sys.version_info >= (3, 14):
         sys_path: list[str] | None = None,
         *,
         authkey_r: int | None = None,
-    ) -> None: ...
+    ) -> None:
+        """Run forkserver."""
 
 else:
     def main(
@@ -33,13 +53,36 @@ else:
         preload: Sequence[str],
         main_path: str | None = None,
         sys_path: Unused = None,
-    ) -> None: ...
+    ) -> None:
+        """Run forkserver."""
 
 def read_signed(fd: int) -> Any: ...
 def write_signed(fd: int, n: int) -> None: ...
 
 _forkserver: ForkServer
 ensure_running = _forkserver.ensure_running
+"""Make sure that a fork server is running.
+
+This can be called from any process.  Note that usually a child
+process will just reuse the forkserver started by its parent, so
+ensure_running() will do nothing.
+"""
+
 get_inherited_fds = _forkserver.get_inherited_fds
+"""Return list of fds inherited from parent process.
+
+This returns None if the current process was not started by fork
+server.
+"""
+
 connect_to_new_process = _forkserver.connect_to_new_process
+"""Request forkserver to create a child process.
+
+Returns a pair of fds (status_r, data_w).  The calling process can read
+the child process's pid and (eventually) its returncode from status_r.
+The calling process should write to data_w the pickled preparation and
+process data.
+"""
+
 set_forkserver_preload = _forkserver.set_forkserver_preload
+"""Set list of module names to try to load in forkserver process."""
