dfd02426a29f85c66d7fce30daad1ead3c248581
Brent Westbrook <brentrwestbrook@gmail.com>
2025-12-31 17:35:52 -0500

    test TypeAlias too

--- crates/ruff_linter/resources/test/fixtures/pyupgrade/UP045_py39.py
+++ crates/ruff_linter/resources/test/fixtures/pyupgrade/UP045_py39.py
@@ -4,10 +4,12 @@ Regression test for https://github.com/astral-sh/ruff/issues/20096
 
 from __future__ import annotations
 
-from typing import Optional, cast
+from typing import Optional, cast, TypeAlias
 
 
 x: Optional[str]             # UP045
 x: "Optional[str]"           # UP045
 cast("Optional[str]", None)  # UP045
 cast(Optional[str], None)    # okay, str | None is a runtime error
+x: TypeAlias = "Optional[str]"  # UP045
+x: TypeAlias = Optional[str]  # okay
