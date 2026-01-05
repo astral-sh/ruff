607c3b2609f847d01950683d158230c61b4f6c89
Alex Waygood <alex.waygood@gmail.com>
2026-01-05 14:41:23 +0000

    [ty] Improve tracebacks when installing dependencies fails in ty_benchmark

--- scripts/ty_benchmark/src/benchmark/test_lsp_diagnostics.py
+++ scripts/ty_benchmark/src/benchmark/test_lsp_diagnostics.py
@@ -42,7 +42,9 @@ def project_setup(
         cwd = Path(tempdir)
         project.clone(cwd)
 
-        venv = Venv.create(cwd, project.python_version)
+        venv = Venv.create(
+            project=project.name, parent=cwd, python_version=project.python_version
+        )
         venv.install(project.install_arguments)
 
         yield project, venv
