5d97d9ad3115c1cce35e55e3d314ae6a63d3be6d
Jack O'Connor <oconnor663@gmail.com>
2026-01-05 10:42:06 -0800

    clippy fix

--- crates/ty_python_semantic/src/types/diagnostic.rs
+++ crates/ty_python_semantic/src/types/diagnostic.rs
@@ -2174,9 +2174,9 @@ declare_lint! {
     ///
     /// ## Why is this bad?
     /// `TypedDict` class bodies aren't allowed to contain any other types of statements. For
-    /// example, method definitions and field values aren't allowed. None of these will be available
-    /// on "instances of the TypedDict" at runtime (as `dict` is the runtime class of all
-    /// "TypedDict instances").
+    /// example, method definitions and field values aren't allowed. None of these will be
+    /// available on "instances of the `TypedDict`" at runtime (as `dict` is the runtime class of
+    /// all "`TypedDict` instances").
     ///
     /// ## Example
     /// ```python
