2309d367909516af64b1d70f440fd5ac576b4a0d
Dan Jones <hello@danjones.dev>
2026-01-08 20:07:56 +0100

    fix: rename mistyped / misnamed symbols

--- crates/ruff_linter/src/rules/flake8_annotation_complexity/rules/complex_annotation.rs
+++ crates/ruff_linter/src/rules/flake8_annotation_complexity/rules/complex_annotation.rs
@@ -3,7 +3,6 @@ use ruff_macros::{ViolationMetadata, derive_message_formats};
 use ruff_python_ast::{Expr, StmtFunctionDef};
 use ruff_python_parser::parse_expression;
 use ruff_text_size::Ranged;
-use similar::DiffableStr;
 
 use crate::checkers::ast::Checker;
 
@@ -14,22 +13,22 @@ use crate::checkers::ast::Checker;
 /// TODO
 #[derive(ViolationMetadata)]
 #[violation_metadata(preview_since = "v0.14.9")]
-pub(crate) struct ComplexArumentAnnotation {
-    argument_name: String,
+pub(crate) struct ComplexAnnotation {
+    symbol_name: String,
     complexity_value: isize,
     max_complexity_value: isize,
 }
 
-impl Violation for ComplexArumentAnnotation {
+impl Violation for ComplexAnnotation {
     #[derive_message_formats]
     fn message(&self) -> String {
         let Self {
-            argument_name,
+            symbol_name,
             complexity_value,
             max_complexity_value,
         } = self;
         format!(
-            "Type annotation for argument `{argument_name}` is too complex ({complexity_value} > {max_complexity_value})"
+            "Type annotation for `{symbol_name}` is too complex ({complexity_value} > {max_complexity_value})"
         )
     }
 }
@@ -66,14 +65,14 @@ fn get_annoation_complexity(expr: &Expr) -> isize {
 const MAX_COMPLEXITY_VALUE: isize = 3;
 
 /// TAE002
-pub(crate) fn complex_argument_annotation(checker: &Checker, function_def: &StmtFunctionDef) {
+pub(crate) fn complex_annotation(checker: &Checker, function_def: &StmtFunctionDef) {
     for arg in function_def.parameters.iter_non_variadic_params() {
         if let Some(type_annotation) = arg.annotation() {
             let annoation_complexity = get_annoation_complexity(type_annotation);
             if annoation_complexity > MAX_COMPLEXITY_VALUE {
                 checker.report_diagnostic(
-                    ComplexArumentAnnotation {
-                        argument_name: arg.name().to_string(),
+                    ComplexAnnotation {
+                        symbol_name: arg.name().to_string(),
                         complexity_value: annoation_complexity,
                         max_complexity_value: MAX_COMPLEXITY_VALUE,
                     },
