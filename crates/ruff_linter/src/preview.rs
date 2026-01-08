f146668539bfc5a1aba9710afe27cb777e9b6b33
Amethyst Reese <amethyst@n7.gg>
2026-01-08 09:38:03 -0800

    Test ungating range suppressions

--- crates/ruff_linter/src/preview.rs
+++ crates/ruff_linter/src/preview.rs
@@ -294,7 +294,8 @@ pub(crate) const fn is_s310_resolve_string_literal_bindings_enabled(
 
 // https://github.com/astral-sh/ruff/pull/21623
 pub(crate) const fn is_range_suppressions_enabled(settings: &LinterSettings) -> bool {
-    settings.preview.is_enabled()
+    // settings.preview.is_enabled()
+    true
 }
 
 // https://github.com/astral-sh/ruff/pull/22419
