adfbbae2564bcd2f24c3eb774b97df69b4ef3e21
dylwil3 <dylwil3@gmail.com>
2026-01-12 08:55:42 -0600

    naive attempt

--- crates/ruff_python_formatter/src/preview.rs
+++ crates/ruff_python_formatter/src/preview.rs
@@ -66,3 +66,10 @@ pub(crate) const fn is_parenthesize_lambda_bodies_enabled(context: &PyFormatCont
 pub(crate) const fn is_fluent_layout_split_first_call_enabled(context: &PyFormatContext) -> bool {
     context.is_preview()
 }
+
+/// Returns `true` if the
+/// [`never_break_literal_subscripts`]() preview
+/// style is enabled.
+pub(crate) const fn is_never_break_literal_subscripts_enabled(context: &PyFormatContext) -> bool {
+    context.is_preview()
+}
