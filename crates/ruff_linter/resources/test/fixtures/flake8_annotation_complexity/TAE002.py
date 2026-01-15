a00d7985fc3cd7a9711f8b708f973f44b2c342e4
Dan Jones <hello@danjones.dev>
2026-01-16 00:02:00 +0100

    feat: handle return type annotations

--- crates/ruff_linter/resources/test/fixtures/flake8_annotation_complexity/TAE002.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_annotation_complexity/TAE002.py
@@ -8,12 +8,22 @@ def okay_fn(
     a: int, b: str, c: list[str, int, 0], d: Annotated[list[str], "yes"]
 ) -> list[int]: ...
 
-
 # Error
 def complex_argument(
-    a: list[dict[str, list[int]]], d: Annotated[dict[str, list[list[int]]], "yes"]
+    a: list[dict[str | int, list[int]]], d: Annotated[dict[str, list[list[int] | str]], 'yes']
 ) -> list[int]: ...
 
 
 # Error
-def complex_return(a: int) -> dict[str, list[dict[str, int]]]: ...
+def complex_return(a: int) -> dict[str, list[dict[str, int | None]]]: ...
+
+# Error
+variable_with_complex_ann: dict[str, dict[str, dict[str, dict[str, str]]]] = {}
+
+def variable_with_complex_ann_in_fn() -> None:
+    var: dict[str, dict[str, dict[str, dict[str, str]]]] = {}
+    print(var)
+
+
+class ClassWithComplexMemberFunction:
+    def __init__(self, bad_arg: dict[str, dict[str, dict[str, dict[str, str]]]]) -> None: ...
--- crates/ruff_linter/resources/test/fixtures/flake8_annotation_complexity/TAE002_quoted.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_annotation_complexity/TAE002_quoted.py
@@ -10,9 +10,20 @@ def okay_fn(
 
 # Error
 def complex_argument(
-    a: "list[dict[str, list[int]]]", d: "Annotated[dict[str, list[list[int]]], 'yes']"
+    a: "list[dict[str | int, list[int]]]", d: "Annotated[dict[str, list[list[int] | str]], 'yes']"
 ) -> list[int]: ...
 
 
 # Error
-def complex_return(a: int) -> "dict[str, list[dict[str, int]]]": ...
+def complex_return(a: int) -> "dict[str, list[dict[str, dict[str, str] | int | None]]]": ...
+
+# Error
+variable_with_complex_ann: "dict[str, dict[str, dict[str, dict[str, str]]]]" = {}
+
+def variable_with_complex_ann_in_fn() -> None:
+    var: "dict[str, dict[str, dict[str, dict[str, str]]]]" = {}
+    print(var)
+
+
+class ClassWithComplexMemberFunction:
+    def __init__(self, bad_arg: "dict[str, dict[str, dict[str, dict[str, str]]]]") -> None: ...
