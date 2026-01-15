c3a8e913cd198b1110be6dacebbd48bcbb7f646f
typeshedbot <>
2026-01-15 00:33:45 +0000

    Sync Linux docstrings

--- crates/ty_vendored/vendor/typeshed/stdlib/encodings/base64_codec.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/encodings/base64_codec.pyi
@@ -1,3 +1,9 @@
+"""Python 'base64_codec' Codec - base64 content transfer encoding.
+
+This codec de/encodes from bytes to bytes.
+
+Written by Marc-Andre Lemburg (mal@lemburg.com).
+"""
 import codecs
 from _typeshed import ReadableBuffer
 from typing import ClassVar
