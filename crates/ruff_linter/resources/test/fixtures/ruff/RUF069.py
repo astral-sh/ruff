a4e240c92f4ace259de8575a3a65c66c026e4b6c
Leandro Braga <18340809+leandrobbraga@users.noreply.github.com>
2026-01-14 21:01:17 -0300

    [`ruff`] add test cases to RUF069
    - non-literal-string
    - mult-line without comments

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF069.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF069.py
@@ -12,6 +12,7 @@ __all__ = "A" + "B"
 __all__: list[str] = ["A", "B"]
 __all__: typing.Any = ("A", "B")
 __all__ = ["A", "B"]
+__all__ = [A, "A", "B"]
 __all__ += ["A", "B"]
 __all__.extend(["A", "B"])
 
@@ -20,6 +21,12 @@ __all__: list[str] = ["A", "B", "A"]
 __all__: typing.Any = ("A", "B", "B")
 __all__ = ["A", "B", "A"]
 __all__ = ["A", "A", "B", "B"]
+__all__ = [
+    "A",
+    "A",
+    "B",
+    "B"
+]
 __all__ += ["B", "B"]
 __all__.extend(["B", "B"])
 
