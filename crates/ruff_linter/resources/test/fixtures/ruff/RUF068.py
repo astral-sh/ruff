b1df60a994ee283e80f4e378859ee90633cc16f2
Brent Westbrook <brentrwestbrook@gmail.com>
2026-01-16 14:47:33 -0500

    update test for `continue` behavior

--- crates/ruff_linter/resources/test/fixtures/ruff/RUF068.py
+++ crates/ruff_linter/resources/test/fixtures/ruff/RUF068.py
@@ -12,7 +12,7 @@ __all__ = "A" + "B"
 __all__: list[str] = ["A", "B"]
 __all__: typing.Any = ("A", "B")
 __all__ = ["A", "B"]
-__all__ = [A, "A", "B"]
+__all__ = [A, "B", "B"]
 __all__ += ["A", "B"]
 __all__.extend(["A", "B"])
 
