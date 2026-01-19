bb641fbe7b970e426b1a31987dffef6a21a099de
K900 <me@0upti.me>
2026-01-19 16:24:37 +0300

    Skip EXE0xx tests on WSL
    The rule itself is always ignored on WSL, which makes the tests fail
    due to false negatives when run in a WSL environment.

--- crates/ruff_linter/src/rules/flake8_executable/mod.rs
+++ crates/ruff_linter/src/rules/flake8_executable/mod.rs
@@ -32,6 +32,12 @@ mod tests {
     #[test_case(Path::new("EXE005_2.py"))]
     #[test_case(Path::new("EXE005_3.py"))]
     fn rules(path: &Path) -> Result<()> {
+        if is_wsl::is_wsl() {
+            // these rules are always ignored on WSL, so skip testing them in a WSL environment
+            // see https://github.com/astral-sh/ruff/pull/21724 for latest discussion
+            return Ok(());
+        }
+
         let snapshot = path.to_string_lossy().into_owned();
         let diagnostics = test_path(
             Path::new("flake8_executable").join(path).as_path(),
