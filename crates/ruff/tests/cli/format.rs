04d6636ab6daa588127b527ebb304fc67ee30642
Amethyst Reese <amethyst@n7.gg>
2026-01-20 13:13:09 -0800

    Fix markdown formatting on stdin, add stdin test

--- crates/ruff/tests/cli/format.rs
+++ crates/ruff/tests/cli/format.rs
@@ -2478,3 +2478,92 @@ fn markdown_formatting_preview_enabled() -> Result<()> {
     "#);
     Ok(())
 }
+
+#[test]
+fn markdown_formatting_stdin() -> Result<()> {
+    let test = CliTest::new()?;
+
+    let fixtures = Path::new("resources").join("test").join("fixtures");
+    let unformatted = fs::read(fixtures.join("unformatted.md")).unwrap();
+    assert_cmd_snapshot!(test.format_command()
+        .args(["--isolated", "--preview", "--stdin-filename", "unformatted.md"])
+        .arg("-")
+        .pass_stdin(unformatted), @r#"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    This is a markdown document with some fenced code blocks with unformatted code.
+
+    Unlabeled Python code:
+
+    ```
+    print( "hello" )
+    ```
+
+    Unlabeled Rust code:
+
+    ```
+    fn thing() {
+        println!( "hello" );
+    }
+    ```
+
+    Labeled Python code:
+
+    ```py
+    print("hello")
+
+
+    def foo():
+        pass
+    ```
+
+    Labeled Python stub:
+
+    ```pyi
+    print("hello")
+
+    def foo():
+        pass
+    ```
+
+    Labeled Rust code:
+    ```rust
+    fn thing() {
+        println!( "hello" );
+    }
+    ```
+
+    Indented code blocks should also work:
+
+    * List item
+
+      ```py
+      print("hello")
+      ```
+
+    Block quoted code blocks may not be supported:
+
+    > Quoted text
+    >
+    > ```py
+    > print( "hello" )
+    > ```
+
+
+    Blacken-docs supports ignore directives:
+
+    <!-- blacken-docs:off -->
+    ```py
+    print("hello")
+    ```
+    <!-- blacken-docs:on -->
+
+    ```py
+    print("hello")
+    ```
+
+    ----- stderr -----
+    "#);
+    Ok(())
+}
