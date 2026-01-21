ce8fadf676e018364ade28a4b750e50e810e1397
Shunsuke Shibayama <sbym1346@gmail.com>
2026-01-21 16:38:37 +0900

    Update ty_walltime.rs

--- crates/ruff_benchmark/benches/ty_walltime.rs
+++ crates/ruff_benchmark/benches/ty_walltime.rs
@@ -189,7 +189,7 @@ static PYDANTIC: Benchmark = Benchmark::new(
         max_dep_date: "2025-06-17",
         python_version: PythonVersion::PY39,
     },
-    3200,
+    4635,
 );
 
 static SYMPY: Benchmark = Benchmark::new(
