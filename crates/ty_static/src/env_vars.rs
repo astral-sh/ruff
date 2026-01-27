e43efd09b8dc4d6a2257ff794217e03f11018aad
konsti <konstin@mailbox.org>
2026-01-27 15:31:20 +0100

    Fix `TY_LOG` typo (#22885)

--- crates/ty_static/src/env_vars.rs
+++ crates/ty_static/src/env_vars.rs
@@ -10,7 +10,7 @@ impl EnvVars {
     ///
     /// For example:
     ///
-    /// - `TY_LOG=uv=debug` is the equivalent of `-vv` to the command line
+    /// - `TY_LOG=ty=debug` is the equivalent of `-vv` to the command line
     /// - `TY_LOG=trace` will enable all trace-level logging.
     ///
     /// See the [tracing documentation](https://docs.rs/tracing-subscriber/latest/tracing_subscriber/filter/struct.EnvFilter.html#example-syntax)
