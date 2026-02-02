74009d355c8eb88ee15d59c3d63359f4f37d793f
jkhall81 <jason.kei.hall@gmail.com>
2026-02-01 17:17:33 -0700

    pyupgrade: tighten diagnostic range for UP024 tuples (#19696

--- crates/ruff_linter/src/preview.rs
+++ crates/ruff_linter/src/preview.rs
@@ -272,3 +272,8 @@ pub(crate) const fn is_mutable_default_in_dataclass_field_enabled(
 ) -> bool {
     settings.preview.is_enabled()
 }
+
+// https://github.com/astral-sh/ruff/pull/23013
+pub(crate) const fn is_up024_precise_highlighting_enabled(settings: &LinterSettings) -> bool {
+    settings.preview.is_enabled()
+}
