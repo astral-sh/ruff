6b02f75945529c227d249e8b5a5f2f38224e4632
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-04 10:25:53 +0000

    Replace `is_wsl` check with file-system heuristic.
    - Remove all checks from EXE001. All files are always executable on NTFS
    filesystems, therefore the EXE001 lint will never fail. Running any logic
    here simply slows down performance for ALL unix users.
    - Add a helper function to identify whether files are executable by default.
        1. Using a `OnceLock` to avoid repeated checks improves performance
        vs previous repeated is_wsl checks for ALL users
        2. If `pyproject.toml` exists and is not executable we know that this
        filesystem supports `is_executable`.
        3. If no `pyproject.toml` is available we create a `NamedTempFile` in
        the project root and check whether `is_executable`.
    - If files are executable by default EXE002 lint is not viable. Replace
    `is_wsl` with `executable_by_default`
    - Remove dependency on `is_wsl` crate. No longer needed.
    - Update documentation to reflect improved logic and link to WSL
    documentation regarding filesystem recommendations.

--- crates/ruff_linter/src/rules/flake8_executable/helpers.rs
+++ crates/ruff_linter/src/rules/flake8_executable/helpers.rs
@@ -2,11 +2,33 @@
 
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
+use std::sync::OnceLock;
 
 use anyhow::Result;
+use tempfile::NamedTempFile;
+
+use crate::settings::LinterSettings;
 
 pub(super) fn is_executable(filepath: &Path) -> Result<bool> {
     let metadata = filepath.metadata()?;
     let permissions = metadata.permissions();
     Ok(permissions.mode() & 0o111 != 0)
+}
+
+// Some file systems do not support executable bits. Instead, everything is executable.
+// See #3110, #5445, #10084, #12941
+//
+// Benchmarking shows better performance vs previous, incorrect approach of checking `is_wsl()`
+// as long as we use a `OnceLock` and a simple test first (filemode of pyproject.toml).
+static EXECUTABLE_BY_DEFAULT: OnceLock<bool> = OnceLock::new();
+
+pub(super) fn executable_by_default(settings: &LinterSettings) -> bool {
+    *EXECUTABLE_BY_DEFAULT.get_or_init(|| {
+        is_executable(&settings.project_root.join("pyproject.toml")).unwrap_or(true)
+            // if pyproject.toml is executable or doesn't exist, run a slower check too:
+            && NamedTempFile::new_in(&settings.project_root)
+                .map_err(std::convert::Into::into)
+                .and_then(|tmpfile| is_executable(tmpfile.path()))
+                .unwrap_or(false) // Assume a normal filesystem in case of read-only, IOError, ...
+    })
 }
