fe1f4373dcb438e106fde42725089dbafedc686b
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-04 14:20:10 +0000

    Add tests for case where `pyproject.toml` exists.

--- crates/ruff_linter/src/rules/flake8_executable/mod.rs
+++ crates/ruff_linter/src/rules/flake8_executable/mod.rs
@@ -11,7 +11,7 @@ mod tests {
     use test_case::test_case;
 
     use crate::registry::Rule;
-    use crate::test::test_path;
+    use crate::test::{test_path, test_resource_path};
     use crate::{assert_diagnostics, settings};
 
     #[cfg_attr(
@@ -45,19 +45,72 @@ mod tests {
     #[test_case(Path::new("EXE005_1.py"))]
     #[test_case(Path::new("EXE005_2.py"))]
     #[test_case(Path::new("EXE005_3.py"))]
-    fn rules(path: &Path) -> Result<()> {
+    fn rules_no_pyproject_toml(path: &Path) -> Result<()> {
         let snapshot = path.to_string_lossy().into_owned();
-        let diagnostics = test_path(
-            Path::new("flake8_executable").join(path).as_path(),
-            &settings::LinterSettings::for_rules(vec![
+        let settings = settings::LinterSettings::for_rules(vec![
                 Rule::ShebangNotExecutable,
                 Rule::ShebangMissingExecutableFile,
                 Rule::ShebangLeadingWhitespace,
                 Rule::ShebangNotFirstLine,
                 Rule::ShebangMissingPython,
-            ]),
+            ]);
+        assert!(!&settings.project_root.join("pyproject.toml").exists(), "unexpected pyproject.toml found: {}", &settings.project_root.join("pyproject.toml").to_string_lossy());
+        let diagnostics = test_path(
+            Path::new("flake8_executable").join(path).as_path(),
+            &settings,
         )?;
         assert_diagnostics!(snapshot, diagnostics);
         Ok(())
     }
+
+    #[cfg_attr(
+        all(unix, not(test_environment = "ntfs")),
+        test_case(Path::new("EXE001_1.py"))
+    )]
+    #[cfg_attr(
+        any(windows, test_environment = "ntfs"),
+        test_case(Path::new("EXE001_1_ntfs.py"))
+    )]
+    #[test_case(Path::new("EXE001_2.py"))]
+    #[test_case(Path::new("EXE001_3.py"))]
+    #[cfg_attr(
+        all(unix, not(test_environment = "ntfs")),
+        test_case(Path::new("EXE002_1.py"))
+    )]
+    #[cfg_attr(
+        any(windows, test_environment = "ntfs"),
+        test_case(Path::new("EXE002_1_ntfs.py"))
+    )]
+    #[test_case(Path::new("EXE002_2.py"))]
+    #[test_case(Path::new("EXE002_3.py"))]
+    #[test_case(Path::new("EXE003.py"))]
+    #[test_case(Path::new("EXE003_uv.py"))]
+    #[test_case(Path::new("EXE003_uv_tool.py"))]
+    #[test_case(Path::new("EXE003_uvx.py"))]
+    #[test_case(Path::new("EXE004_1.py"))]
+    #[test_case(Path::new("EXE004_2.py"))]
+    #[test_case(Path::new("EXE004_3.py"))]
+    #[test_case(Path::new("EXE004_4.py"))]
+    #[test_case(Path::new("EXE005_1.py"))]
+    #[test_case(Path::new("EXE005_2.py"))]
+    #[test_case(Path::new("EXE005_3.py"))]
+    fn rules_with_pyproject_toml(path: &Path) -> Result<()> {
+        let snapshot = path.to_string_lossy().into_owned();
+        let mut settings = settings::LinterSettings::for_rules(vec![
+                Rule::ShebangNotExecutable,
+                Rule::ShebangMissingExecutableFile,
+                Rule::ShebangLeadingWhitespace,
+                Rule::ShebangNotFirstLine,
+                Rule::ShebangMissingPython,
+            ]);
+        settings.project_root = test_resource_path("fixtures").join("flake8_executable");
+        assert!(&settings.project_root.join("pyproject.toml").exists(), "{} not found", &settings.project_root.join("pyproject.toml").to_string_lossy());
+        let diagnostics = test_path(
+            Path::new("flake8_executable").join(path).as_path(),
+            &settings,
+        )?;
+        assert_diagnostics!(snapshot, diagnostics);
+        Ok(())
+    }
+
 }
