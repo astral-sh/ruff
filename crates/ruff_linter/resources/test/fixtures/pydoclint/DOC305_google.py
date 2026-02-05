b1613107981e52b736b594445cfe7c4a4e5ad391
Aleksandr Berkhmiller <aberkhmiller@ptsecurity.com>
2026-02-05 11:44:38 +0700

    Implement DOC301, DOC302, DOC304, DOC305, DOC306

---
+++ crates/ruff_linter/resources/test/fixtures/pydoclint/DOC305_google.py
@@ -0,0 +1,44 @@
+
+# DOC305
+class Foo:
+    """
+    Docstring of the class
+
+    Raises:
+        ValueError
+    """
+
+    def __init__(self, a: int, b: int) -> None:
+        """
+        Initialize class.
+
+        Args:
+            a (int): Some integer.
+            b (int): Another integer.
+
+        Raises:
+            ValueError
+        """
+        self.a = a
+        self.b = b
+        raise ValueError
+
+
+# OK
+class Foo:
+    """Docstring of the class"""
+
+    def __init__(self, a: int, b: int) -> None:
+        """
+        Initialize class.
+
+        Args:
+            a (int): Some integer.
+            b (int): Another integer.
+
+        Raises:
+            ValueError
+        """
+        self.a = a
+        self.b = b
+        raise ValueError
