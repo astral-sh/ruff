3600c3b776e2320ff6039e05d5354ced0b41d5c8
bxff <51504045+bxff@users.noreply.github.com>
2026-02-08 04:53:34 +0530

    Add PT006 grouped tuple regression test

--- crates/ruff_linter/resources/test/fixtures/flake8_pytest_style/PT006.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_pytest_style/PT006.py
@@ -2,100 +2,82 @@ import pytest
 
 
 @pytest.mark.parametrize("param", [1, 2, 3])
-def test_always_ok(param):
-    ...
+def test_always_ok(param): ...
 
 
 @pytest.mark.parametrize("param1,param2", [(1, 2), (3, 4)])
-def test_csv(param1, param2):
-    ...
+def test_csv(param1, param2): ...
 
 
 @pytest.mark.parametrize("   param1,   ,    param2   , ", [(1, 2), (3, 4)])
-def test_csv_with_whitespace(param1, param2):
-    ...
+def test_csv_with_whitespace(param1, param2): ...
 
 
 @pytest.mark.parametrize("param1,param2", [(1, 2), (3, 4)])
-def test_csv_bad_quotes(param1, param2):
-    ...
+def test_csv_bad_quotes(param1, param2): ...
 
 
 @pytest.mark.parametrize(("param1", "param2"), [(1, 2), (3, 4)])
-def test_tuple(param1, param2):
-    ...
+def test_tuple(param1, param2): ...
 
 
 @pytest.mark.parametrize(("param1",), [1, 2, 3])
-def test_tuple_one_elem(param1, param2):
-    ...
+def test_tuple_one_elem(param1, param2): ...
 
 
 @pytest.mark.parametrize(["param1", "param2"], [(1, 2), (3, 4)])
-def test_list(param1, param2):
-    ...
+def test_list(param1, param2): ...
 
 
 @pytest.mark.parametrize(["param1"], [1, 2, 3])
-def test_list_one_elem(param1, param2):
-    ...
+def test_list_one_elem(param1, param2): ...
 
 
 @pytest.mark.parametrize([some_expr, another_expr], [1, 2, 3])
-def test_list_expressions(param1, param2):
-    ...
+def test_list_expressions(param1, param2): ...
 
 
 @pytest.mark.parametrize([some_expr, "param2"], [1, 2, 3])
-def test_list_mixed_expr_literal(param1, param2):
-    ...
+def test_list_mixed_expr_literal(param1, param2): ...
 
 
-@pytest.mark.parametrize(("param1, " "param2, " "param3"), [(1, 2, 3), (4, 5, 6)])
-def test_implicit_str_concat_with_parens(param1, param2, param3):
-    ...
+@pytest.mark.parametrize(("param1, param2, param3"), [(1, 2, 3), (4, 5, 6)])
+def test_implicit_str_concat_with_parens(param1, param2, param3): ...
 
 
-@pytest.mark.parametrize("param1, " "param2, " "param3", [(1, 2, 3), (4, 5, 6)])
-def test_implicit_str_concat_no_parens(param1, param2, param3):
-    ...
+@pytest.mark.parametrize("param1, param2, param3", [(1, 2, 3), (4, 5, 6)])
+def test_implicit_str_concat_no_parens(param1, param2, param3): ...
 
 
-@pytest.mark.parametrize((("param1, " "param2, " "param3")), [(1, 2, 3), (4, 5, 6)])
-def test_implicit_str_concat_with_multi_parens(param1, param2, param3):
-    ...
+@pytest.mark.parametrize(("param1, param2, param3"), [(1, 2, 3), (4, 5, 6)])
+def test_implicit_str_concat_with_multi_parens(param1, param2, param3): ...
 
 
 @pytest.mark.parametrize(("param1,param2"), [(1, 2), (3, 4)])
-def test_csv_with_parens(param1, param2):
-    ...
+def test_csv_with_parens(param1, param2): ...
 
 
 parametrize = pytest.mark.parametrize(("param1,param2"), [(1, 2), (3, 4)])
 
+
 @parametrize
-def test_not_decorator(param1, param2):
-    ...
+def test_not_decorator(param1, param2): ...
 
 
 @pytest.mark.parametrize(argnames=("param1,param2"), argvalues=[(1, 2), (3, 4)])
-def test_keyword_arguments(param1, param2):
-    ...
+def test_keyword_arguments(param1, param2): ...
 
 
 @pytest.mark.parametrize(("param",), [(1,), (2,)])
-def test_single_element_tuple(param):
-    ...
+def test_single_element_tuple(param): ...
 
 
 @pytest.mark.parametrize(("param",), [[1], [2]])
-def test_single_element_list(param):
-    ...
+def test_single_element_list(param): ...
 
 
 @pytest.mark.parametrize(("param",), [[1], [2]])
-def test_single_element_list(param):
-    ...
+def test_single_element_list(param): ...
 
 
 # Unsafe fix
@@ -106,8 +88,8 @@ def test_single_element_list(param):
     ),
     [[1], [2]],
 )
-def test_comment_in_argnames(param):
-    ...
+def test_comment_in_argnames(param): ...
+
 
 # Unsafe fix
 @pytest.mark.parametrize(
@@ -120,8 +102,7 @@ def test_comment_in_argnames(param):
         (2,),
     ],
 )
-def test_comment_in_argvalues(param):
-    ...
+def test_comment_in_argvalues(param): ...
 
 
 # Safe fix
@@ -133,8 +114,7 @@ def test_comment_in_argvalues(param):
         (2,),
     ],
 )
-def test_comment_between_argvalues_items(param):
-    ...
+def test_comment_between_argvalues_items(param): ...
 
 
 # A fix should be suggested for `argnames`, but not for `argvalues`.
@@ -167,8 +147,7 @@ def test_invalid_argvalues(param):
         ((1,),),
     ],
 )
-def test_single_element_nested_empty_tuple(param):
-    ...
+def test_single_element_nested_empty_tuple(param): ...
 
 
 @pytest.mark.parametrize(
@@ -178,8 +157,7 @@ def test_single_element_nested_empty_tuple(param):
         ((3, 4),),
     ],
 )
-def test_single_element_nested_multi_tuple(param):
-    ...
+def test_single_element_nested_multi_tuple(param): ...
 
 
 @pytest.mark.parametrize(
@@ -188,5 +166,13 @@ def test_single_element_nested_multi_tuple(param):
         (((1,),),),
     ],
 )
-def test_single_element_deeply_nested_tuple(param):
-    ...
+def test_single_element_deeply_nested_tuple(param): ...
+
+
+@pytest.mark.parametrize(
+    ["param"],
+    [
+        (((1,)),),
+    ],
+)
+def test_single_element_grouped_tuple(param): ...
