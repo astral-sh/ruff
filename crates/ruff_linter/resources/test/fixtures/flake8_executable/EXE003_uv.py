54bb9d8ff1794ea2775e25f9c17eea43faf57a3d
Amethyst Reese <amethyst@n7.gg>
2026-02-13 14:36:11 -0800

    more test cases

--- crates/ruff_linter/resources/test/fixtures/flake8_executable/EXE003_uv.py
+++ crates/ruff_linter/resources/test/fixtures/flake8_executable/EXE003_uv.py
@@ -4,11 +4,17 @@ print("hello world")
 #!/usr/bin/env uv --offline run
 print("offline")
 
-#!/usr/bin/env uvx
-print("uvx")
+#!/usr/bin/env uv --color=auto run
+print("color")
+
+#!/usr/bin/env uv --quiet run --script
+print("quiet run script")
 
 #!/usr/bin/env uv tool run
 print("uv tool")
+
+#!/usr/bin/env uvx
+print("uvx")
 
 #!/usr/bin/env uvx --quiet
 print("uvx quiet")
