bd539232cec6c904ac5b3a1d050b113f80320737
Alex Waygood <alex.waygood@gmail.com>
2026-02-16 01:37:00 +0000

    run prek again...

--- scripts/conformance.py
+++ scripts/conformance.py
@@ -506,7 +506,9 @@ def group_diagnostics_by_key(
     }
 
     for diag in chain(old, new):
-        diag.tag = tagged_lines.get((diag.location.path.name, diag.location.positions.begin.line))
+        diag.tag = tagged_lines.get(
+            (diag.location.path.name, diag.location.positions.begin.line)
+        )
 
     diagnostics = [
         *old,
