b76b453f8cac252c4bbb536f6ed316c66385e8e9
Brent Westbrook <brentrwestbrook@gmail.com>
2026-02-26 10:46:02 -0500

    use `scope` field

--- crates/ruff_workspace/src/options.rs
+++ crates/ruff_workspace/src/options.rs
@@ -2530,14 +2530,13 @@ pub struct IsortOptions {
     #[option(
         default = r#"{}"#,
         value_type = r#"dict["future" | "standard-library" | "third-party" | "first-party" | "local-folder" | str, str]"#,
+        scope = "import-heading",
         example = r#"
-            import-heading = {
-                future = "Future imports",
-                standard-library = "Standard library imports",
-                third-party = "Third party imports",
-                first-party = "First party imports",
-                local-folder = "Local folder imports",
-            }
+            future = "Future imports"
+            standard-library = "Standard library imports"
+            third-party = "Third party imports"
+            first-party = "First party imports"
+            local-folder = "Local folder imports"
         "#
     )]
     pub import_heading: Option<FxHashMap<ImportSection, String>>,
