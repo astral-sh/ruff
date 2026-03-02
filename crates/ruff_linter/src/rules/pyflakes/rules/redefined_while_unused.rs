29d45213bb26b699d6d21ffb05281b5af6b5fdcb
chiri <chirizxc@proton.me>
2026-03-02 23:10:22 +0300

    fix mkdocs

--- crates/ruff_linter/src/rules/pyflakes/rules/redefined_while_unused.rs
+++ crates/ruff_linter/src/rules/pyflakes/rules/redefined_while_unused.rs
@@ -13,6 +13,14 @@ use crate::fix::edits;
 use crate::preview::is_f811_shadowing_in_type_checking_enabled;
 use crate::{Fix, FixAvailability, Violation};
 
+/// ## What it does
+/// Checks for variable definitions that redefine (or "shadow") unused
+/// variables.
+///
+/// ## Why is this bad?
+/// Redefinitions of unused names are unnecessary and often indicative of a
+/// mistake.
+///
 /// ## Example
 ///
 /// ```python
