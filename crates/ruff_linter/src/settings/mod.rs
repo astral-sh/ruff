0736524a7075d7ea7bc161d03d0fea2baea7d168
Brent Westbrook <brentrwestbrook@gmail.com>
2026-03-10 11:53:51 -0400

    drop SIM102
    this came up both internally and in the discussion, and it feels similar in
    spirit to SIM114

--- crates/ruff_linter/src/settings/mod.rs
+++ crates/ruff_linter/src/settings/mod.rs
@@ -700,7 +700,6 @@ pub const PREVIEW_DEFAULT_SELECTORS: &[RuleSelector] = &[
     RuleSelector::rule(RuleCodePrefix::Flake8Bandit(codes::Flake8Bandit::_110)), // S110
     RuleSelector::rule(RuleCodePrefix::Flake8Bandit(codes::Flake8Bandit::_112)), // S112
     RuleSelector::rule(RuleCodePrefix::Flake8Simplify(codes::Flake8Simplify::_101)), // SIM101
-    RuleSelector::rule(RuleCodePrefix::Flake8Simplify(codes::Flake8Simplify::_102)), // SIM102
     RuleSelector::rule(RuleCodePrefix::Flake8Simplify(codes::Flake8Simplify::_103)), // SIM103
     RuleSelector::rule(RuleCodePrefix::Flake8Simplify(codes::Flake8Simplify::_107)), // SIM107
     RuleSelector::rule(RuleCodePrefix::Flake8Simplify(codes::Flake8Simplify::_113)), // SIM113
