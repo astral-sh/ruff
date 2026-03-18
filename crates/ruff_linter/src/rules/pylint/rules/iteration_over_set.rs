b5665bd18eecab4d3b5ab1256b36904cd99a4c57
Hunter Hogan <hunterhogan@users.noreply.github.com>
2026-03-18 15:12:07 -0500

    [`pylint`] Improve phrasing (`PLC0208`) (#24033)
    Since I already put a lot of thought into the grammar and style, at
    least we can benefit from those changes.
    
    Preemptive response:
    I use this linter and follow its advice because I'm not an expert
    programmer. If you were to give me two similar Python functions, I would
    probably not be able to say which function was better.
    
    On the other hand, I have exceptional training and experience as an
    editor and a writer. I can say whether changing one word in a draft does
    or does not affect the grammar, the style, the voice, or more than one.
    
    I'm gifting to Astral the benefit of my professional knowledge by
    editing this text. I will not, however, attempt to explain how to write
    gooder and more clearer and conciser itself, which required thousands of
    hours to learn, in this Pull Request.
    
    ... or in future Pull Requests, if I were to choose to gift my knowledge
    again.
    
    <!--
    Thank you for contributing to Ruff/ty! To help us out with reviewing,
    please consider the following:
    
    - Does this pull request include a summary of the change? (See below.)
    - Does this pull request include a descriptive title? (Please prefix
    with `[ty]` for ty pull
      requests.)
    - Does this pull request include references to any relevant issues?
    -->
    
    ## Summary
    
    <!-- What's the purpose of the change? What does it do, and why? -->
    
    ## Test Plan
    
    <!-- How was it tested? -->

--- crates/ruff_linter/src/rules/pylint/rules/iteration_over_set.rs
+++ crates/ruff_linter/src/rules/pylint/rules/iteration_over_set.rs
@@ -9,12 +9,11 @@ use crate::checkers::ast::Checker;
 use crate::{AlwaysFixableViolation, Edit, Fix};
 
 /// ## What it does
-/// Checks for iteration over a `set` literal where each element in the set is
-/// itself a literal value.
+/// Checks for iteration over a `set` literal in which each element is a literal.
 ///
 /// ## Why is this bad?
 /// Iterating over a `set` is less efficient than iterating over a sequence
-/// type, like `list` or `tuple`.
+/// type, such as `list` or `tuple`.
 ///
 /// ## Example
 /// ```python
