astral-sh/ruff

commit 5c59f8a46965cac3470f09972196c8620faa4626

[`pyupgrade`] Ignore strings with string-only escapes (`UP012`) (#16058)
## Summary

Resolves #12753.

After this change, `UP012` will no longer report strings containing any
of the following:

* Name escapes (`\N{NAME}`)
* Short (`\u0000`) and long (`\U00000000`) Unicode escapes
* Octal escapes (`\0`, `\00`, `\000`) where the codepoint value is
greater than 255 (377<sub>8</sub>)

## Test Plan

`cargo nextest run` and `cargo insta test`.

crates/ruff_linter/src/rules/pyupgrade/rules/unnecessary_encode_utf8.rs
@@ -2,8 +2,9 @@ use std::fmt::Write as _;
 
 use ruff_macros::{ViolationMetadata, derive_message_formats};
 use ruff_python_ast::token::{TokenKind, Tokens};
-use ruff_python_ast::{self as ast, Arguments, Expr, Keyword};
-use ruff_text_size::{Ranged, TextRange};
+use ruff_python_ast::{self as ast, Arguments, Expr, Keyword, StringLiteral, StringLiteralValue};
+use ruff_python_trivia::Cursor;
+use ruff_text_size::{Ranged, TextLen, TextRange};
 
 use crate::Locator;
 use crate::checkers::ast::Checker;
@@ -158,6 +159,10 @@ pub(crate) fn unnecessary_encode_utf8(checker: &Checker, call: &ast::ExprCall) {
     };
     match variable {
         Expr::StringLiteral(ast::ExprStringLiteral { value: literal, .. }) => {
+            if string_contains_string_only_escapes(literal, checker.locator()) {
+                return;
+            }
+
             // Ex) `"str".encode()`, `"str".encode("utf-8")`
             if let Some(encoding_arg) = match_encoding_arg(&call.arguments) {
                 if literal.to_str().is_ascii() {
@@ -258,4 +263,83 @@ pub(crate) fn unnecessary_encode_utf8(checker: &Checker, call: &ast::ExprCall) {
         }
         _ => {}
     }
+}
+/// In a string, there are two kinds of escape sequences: "single" and "multi".
+///
+/// A "single" escape sequence is formed if a backslash is followed by
+/// a newline, another backslash, `'`, `"`, `a`, `b`, `f`, `n`, `t`, or `v`.
+/// A "multi" escape sequence is formed if a backslash is followed by
+/// `x` and 2 hex digits, `N` and a Unicode character name enclosed in a pair of braces,
+/// `u` and 4 hex digits, `U` and 8 hex digits, or 1 to 3 oct digits.
+///
+/// Out of the aforementioned, `u`, `U` and `N` are only valid in a string.
+/// However, an octal escape `\ooo` where `ooo` is greater than 377 base 8
+/// currently raises a `SyntaxWarning` (will eventually be a `SyntaxError`)
+/// in both strings and bytes and thus is not considered `bytes`-compatible.
+///
+/// An unrecognized escape sequence is ignored, resulting in both
+/// the backslash and the following character being part of the string.
+///
+/// Reference: [Lexical analysis &sect; 2.4.1.1. Escape sequences][escape-sequences]
+///
+/// [escape-sequences]: https://docs.python.org/3/reference/lexical_analysis.html#escape-sequences
+fn string_contains_string_only_escapes(string: &StringLiteralValue, locator: &Locator) -> bool {
+    for literal in string {
+        let flags = literal.flags;
+
+        if flags.prefix().is_raw() {
+            continue;
+        }
+
+        if literal.content_range().len() > literal.as_str().text_len()
+            && literal_contains_string_only_escapes(literal, locator)
+        {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn literal_contains_string_only_escapes(literal: &StringLiteral, locator: &Locator) -> bool {
+    let inner_in_source = locator.slice(literal.content_range());
+
+    let mut cursor = Cursor::new(inner_in_source);
+
+    while let Some(backslash_offset) = memchr::memchr(b'\\', cursor.as_bytes()) {
+        cursor.skip_bytes(backslash_offset + "\\".len());
+
+        let Some(escaped) = cursor.bump() else {
+            continue;
+        };
+
+        match escaped {
+            'N' | 'u' | 'U' => return true,
+            'x' => {
+                cursor.skip_bytes(2);
+            }
+            '0'..='7' => {
+                let (second, third) = (cursor.first(), cursor.second());
+
+                let octal_codepoint = match (is_octal_digit(second), is_octal_digit(third)) {
+                    (false, _) => escaped.to_string(),
+                    (true, false) => format!("{escaped}{second}"),
+                    (true, true) => format!("{escaped}{second}{third}"),
+                };
+
+                if octal_codepoint.parse::<u8>().is_err() {
+                    return true;
+                }
+
+                cursor.skip_bytes(octal_codepoint.len());
+            }
+            _ => {}
+        }
+    }
+
+    false
+}
+
+const fn is_octal_digit(char: char) -> bool {
+    matches!(char, '0'..='7')
 }

crates/ruff_linter/resources/test/fixtures/pyupgrade/UP012.py
@@ -88,3 +88,44 @@ x: '"foo".encode("utf-8")'
 # AttributeError for t-strings so skip lint
 (t"foo{bar}").encode("utf-8")
 (t"foo{bar}").encode(encoding="utf-8")
+
+
+# https://github.com/astral-sh/ruff/issues/12753
+
+## Errors
+("a" "b").encode()
+
+'''\
+'''.encode()
+
+'\x20\\'.encode()
+'\0\b0'.encode()
+'\01\fc'.encode()
+'\143\\'.encode()
+
+("a" "\b").encode()
+("\a" "b").encode()
+("\a" r"\b").encode()
+(r"\a" "\b").encode()
+
+'\"'.encode()
+"\'".encode()
+
+'\\\\\\\\ '.encode()  # 4 backslashes
+'\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+
+'\\a'.encode()
+'a\\\b'.encode()
+
+'\\ u0000 '.encode()
+
+
+## No errors
+"\N{DIGIT ONE}".encode()
+"\u0031".encode()
+"\U00000031".encode()
+
+'\477'.encode()
+
+"\
+" "\u0001".encode()

crates/ruff_linter/src/rules/pyupgrade/snapshots/ruff_linter__rules__pyupgrade__tests__UP012.py.snap
@@ -563,3 +563,333 @@ help: Rewrite as bytes literal
 87 |
 88 | # AttributeError for t-strings so skip lint
 89 | (t"foo{bar}").encode("utf-8")
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+  --> UP012.py:96:1
+   |
+95 | ## Errors
+96 | ("a" "b").encode()
+   | ^^^^^^^^^^^^^^^^^^
+97 |
+98 | '''\
+   |
+help: Rewrite as bytes literal
+93 | # https://github.com/astral-sh/ruff/issues/12753
+94 |
+95 | ## Errors
+   - ("a" "b").encode()
+96 + (b"a" b"b")
+97 |
+98 | '''\
+99 | '''.encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:98:1
+    |
+ 96 |   ("a" "b").encode()
+ 97 |
+ 98 | / '''\
+ 99 | | '''.encode()
+    | |____________^
+100 |
+101 |   '\x20\\'.encode()
+    |
+help: Rewrite as bytes literal
+95  | ## Errors
+96  | ("a" "b").encode()
+97  |
+    - '''\
+    - '''.encode()
+98  + b'''\
+99  + '''
+100 |
+101 | '\x20\\'.encode()
+102 | '\0\b0'.encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:101:1
+    |
+ 99 | '''.encode()
+100 |
+101 | '\x20\\'.encode()
+    | ^^^^^^^^^^^^^^^^^
+102 | '\0\b0'.encode()
+103 | '\01\fc'.encode()
+    |
+help: Rewrite as bytes literal
+98  | '''\
+99  | '''.encode()
+100 |
+    - '\x20\\'.encode()
+101 + b'\x20\\'
+102 | '\0\b0'.encode()
+103 | '\01\fc'.encode()
+104 | '\143\\'.encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:102:1
+    |
+101 | '\x20\\'.encode()
+102 | '\0\b0'.encode()
+    | ^^^^^^^^^^^^^^^^
+103 | '\01\fc'.encode()
+104 | '\143\\'.encode()
+    |
+help: Rewrite as bytes literal
+99  | '''.encode()
+100 |
+101 | '\x20\\'.encode()
+    - '\0\b0'.encode()
+102 + b'\0\b0'
+103 | '\01\fc'.encode()
+104 | '\143\\'.encode()
+105 |
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:103:1
+    |
+101 | '\x20\\'.encode()
+102 | '\0\b0'.encode()
+103 | '\01\fc'.encode()
+    | ^^^^^^^^^^^^^^^^^
+104 | '\143\\'.encode()
+    |
+help: Rewrite as bytes literal
+100 |
+101 | '\x20\\'.encode()
+102 | '\0\b0'.encode()
+    - '\01\fc'.encode()
+103 + b'\01\fc'
+104 | '\143\\'.encode()
+105 |
+106 | ("a" "\b").encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:104:1
+    |
+102 | '\0\b0'.encode()
+103 | '\01\fc'.encode()
+104 | '\143\\'.encode()
+    | ^^^^^^^^^^^^^^^^^
+105 |
+106 | ("a" "\b").encode()
+    |
+help: Rewrite as bytes literal
+101 | '\x20\\'.encode()
+102 | '\0\b0'.encode()
+103 | '\01\fc'.encode()
+    - '\143\\'.encode()
+104 + b'\143\\'
+105 |
+106 | ("a" "\b").encode()
+107 | ("\a" "b").encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:106:1
+    |
+104 | '\143\\'.encode()
+105 |
+106 | ("a" "\b").encode()
+    | ^^^^^^^^^^^^^^^^^^^
+107 | ("\a" "b").encode()
+108 | ("\a" r"\b").encode()
+    |
+help: Rewrite as bytes literal
+103 | '\01\fc'.encode()
+104 | '\143\\'.encode()
+105 |
+    - ("a" "\b").encode()
+106 + (b"a" b"\b")
+107 | ("\a" "b").encode()
+108 | ("\a" r"\b").encode()
+109 | (r"\a" "\b").encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:107:1
+    |
+106 | ("a" "\b").encode()
+107 | ("\a" "b").encode()
+    | ^^^^^^^^^^^^^^^^^^^
+108 | ("\a" r"\b").encode()
+109 | (r"\a" "\b").encode()
+    |
+help: Rewrite as bytes literal
+104 | '\143\\'.encode()
+105 |
+106 | ("a" "\b").encode()
+    - ("\a" "b").encode()
+107 + (b"\a" b"b")
+108 | ("\a" r"\b").encode()
+109 | (r"\a" "\b").encode()
+110 |
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:108:1
+    |
+106 | ("a" "\b").encode()
+107 | ("\a" "b").encode()
+108 | ("\a" r"\b").encode()
+    | ^^^^^^^^^^^^^^^^^^^^^
+109 | (r"\a" "\b").encode()
+    |
+help: Rewrite as bytes literal
+105 |
+106 | ("a" "\b").encode()
+107 | ("\a" "b").encode()
+    - ("\a" r"\b").encode()
+108 + (b"\a" br"\b")
+109 | (r"\a" "\b").encode()
+110 |
+111 | '\"'.encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:109:1
+    |
+107 | ("\a" "b").encode()
+108 | ("\a" r"\b").encode()
+109 | (r"\a" "\b").encode()
+    | ^^^^^^^^^^^^^^^^^^^^^
+110 |
+111 | '\"'.encode()
+    |
+help: Rewrite as bytes literal
+106 | ("a" "\b").encode()
+107 | ("\a" "b").encode()
+108 | ("\a" r"\b").encode()
+    - (r"\a" "\b").encode()
+109 + (br"\a" b"\b")
+110 |
+111 | '\"'.encode()
+112 | "\'".encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:111:1
+    |
+109 | (r"\a" "\b").encode()
+110 |
+111 | '\"'.encode()
+    | ^^^^^^^^^^^^^
+112 | "\'".encode()
+    |
+help: Rewrite as bytes literal
+108 | ("\a" r"\b").encode()
+109 | (r"\a" "\b").encode()
+110 |
+    - '\"'.encode()
+111 + b'\"'
+112 | "\'".encode()
+113 |
+114 | '\\\\\\\\ '.encode()  # 4 backslashes
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:112:1
+    |
+111 | '\"'.encode()
+112 | "\'".encode()
+    | ^^^^^^^^^^^^^
+113 |
+114 | '\\\\\\\\ '.encode()  # 4 backslashes
+    |
+help: Rewrite as bytes literal
+109 | (r"\a" "\b").encode()
+110 |
+111 | '\"'.encode()
+    - "\'".encode()
+112 + b"\'"
+113 |
+114 | '\\\\\\\\ '.encode()  # 4 backslashes
+115 | '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:114:1
+    |
+112 | "\'".encode()
+113 |
+114 | '\\\\\\\\ '.encode()  # 4 backslashes
+    | ^^^^^^^^^^^^^^^^^^^^
+115 | '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+    |
+help: Rewrite as bytes literal
+111 | '\"'.encode()
+112 | "\'".encode()
+113 |
+    - '\\\\\\\\ '.encode()  # 4 backslashes
+114 + b'\\\\\\\\ '  # 4 backslashes
+115 | '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+116 |
+117 | '\\a'.encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:115:1
+    |
+114 | '\\\\\\\\ '.encode()  # 4 backslashes
+115 | '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+    | ^^^^^^^^^^^^^^^^^^^^
+116 |
+117 | '\\a'.encode()
+    |
+help: Rewrite as bytes literal
+112 | "\'".encode()
+113 |
+114 | '\\\\\\\\ '.encode()  # 4 backslashes
+    - '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+115 + b'\\\\\\\  '  # `\ ` is invalid but only causes a SyntaxWarning
+116 |
+117 | '\\a'.encode()
+118 | 'a\\\b'.encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:117:1
+    |
+115 | '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+116 |
+117 | '\\a'.encode()
+    | ^^^^^^^^^^^^^^
+118 | 'a\\\b'.encode()
+    |
+help: Rewrite as bytes literal
+114 | '\\\\\\\\ '.encode()  # 4 backslashes
+115 | '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+116 |
+    - '\\a'.encode()
+117 + b'\\a'
+118 | 'a\\\b'.encode()
+119 |
+120 | '\\ u0000 '.encode()
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:118:1
+    |
+117 | '\\a'.encode()
+118 | 'a\\\b'.encode()
+    | ^^^^^^^^^^^^^^^^
+119 |
+120 | '\\ u0000 '.encode()
+    |
+help: Rewrite as bytes literal
+115 | '\\\\\\\  '.encode()  # `\ ` is invalid but only causes a SyntaxWarning
+116 |
+117 | '\\a'.encode()
+    - 'a\\\b'.encode()
+118 + b'a\\\b'
+119 |
+120 | '\\ u0000 '.encode()
+121 |
+
+UP012 [*] Unnecessary call to `encode` as UTF-8
+   --> UP012.py:120:1
+    |
+118 | 'a\\\b'.encode()
+119 |
+120 | '\\ u0000 '.encode()
+    | ^^^^^^^^^^^^^^^^^^^^
+    |
+help: Rewrite as bytes literal
+117 | '\\a'.encode()
+118 | 'a\\\b'.encode()
+119 |
+    - '\\ u0000 '.encode()
+120 + b'\\ u0000 '
+121 |
+122 |
+123 | ## No errors

commit 23364ae6a52b47c855db63c203893325a9aab1fa

[`pyupgrade`] Fix panic caused by handling of octals in `UP012` (#24390)
This fixes two errors introduced by #16058 : 

- An off-by-one error caused a panic when `UP012` was run on strings
ending in an octal (and could also cause the rule to trigger when it
should not, e.g. for `"\000\N{DIGIT ONE}"`).
- When checking that an octal `\abc` was not larger than `\377`, it was
parsed using `"abc".parse::<u8>()`. But this uses base 10. We need to
use `u8::from_str_radix("abc",8)` instead.

Closes #24389

crates/ruff_linter/src/rules/pyupgrade/rules/unnecessary_encode_utf8.rs
@@ -327,11 +327,13 @@ fn literal_contains_string_only_escapes(literal: &StringLiteral, locator: &Locat
                     (true, true) => format!("{escaped}{second}{third}"),
                 };
 
-                if octal_codepoint.parse::<u8>().is_err() {
+                if u8::from_str_radix(&octal_codepoint, 8).is_err() {
                     return true;
                 }
 
-                cursor.skip_bytes(octal_codepoint.len());
+                // Cursor is currently at first octal digit, so we just
+                // skip the remaining.
+                cursor.skip_bytes(octal_codepoint.len().saturating_sub(1));
             }
             _ => {}
         }

crates/ruff_linter/resources/test/fixtures/pyupgrade/UP012.py
@@ -129,3 +129,9 @@ x: '"foo".encode("utf-8")'
 
 "\
 " "\u0001".encode()
+
+# Regression https://github.com/astral-sh/ruff/issues/24389
+# (Should not panic)
+IMR_HEADER = "$IMURAW\0".encode("ascii")
+# No error
+"\000\N{DIGIT ONE}".encode()