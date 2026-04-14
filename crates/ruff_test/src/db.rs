4a55162f892bfb2b5152751a47d75c6691114473
Brent Westbrook <brentrwestbrook@gmail.com>
2026-04-14 14:15:22 -0400

    remove ty deps

--- crates/ruff_test/src/db.rs
+++ crates/ruff_test/src/db.rs
@@ -1,29 +1,20 @@
 use ruff_db::Db as SourceDb;
-use ruff_db::files::{File, Files};
+use ruff_db::files::Files;
 use ruff_db::system::{DbWithWritableSystem, InMemorySystem, System, SystemPath, WritableSystem};
 use ruff_db::vendored::VendoredFileSystem;
-use ty_module_resolver::SearchPaths;
-use ty_python_core::program::Program;
 
 #[salsa::db]
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub(crate) struct Db {
     storage: salsa::Storage<Self>,
     files: Files,
     system: InMemorySystem,
+    vendored: VendoredFileSystem,
 }
 
 impl Db {
     pub(crate) fn setup() -> Self {
-        Self {
-            system: InMemorySystem::default(),
-            storage: salsa::Storage::new(Some(Box::new({
-                move |event| {
-                    tracing::trace!("event: {:?}", event);
-                }
-            }))),
-            files: Files::default(),
-        }
+        Self::default()
     }
 
     pub(crate) fn use_in_memory_system(&mut self) {
@@ -39,7 +30,7 @@ impl Db {
 #[salsa::db]
 impl SourceDb for Db {
     fn vendored(&self) -> &VendoredFileSystem {
-        ty_vendored::file_system()
+        &self.vendored
     }
 
     fn system(&self) -> &dyn System {
@@ -51,21 +42,7 @@ impl SourceDb for Db {
     }
 
     fn python_version(&self) -> ruff_python_ast::PythonVersion {
-        Program::get(self).python_version(self)
-    }
-}
-
-#[salsa::db]
-impl ty_module_resolver::Db for Db {
-    fn search_paths(&self) -> &SearchPaths {
-        Program::get(self).search_paths(self)
-    }
-}
-
-#[salsa::db]
-impl ty_python_core::Db for Db {
-    fn should_check_file(&self, file: File) -> bool {
-        !file.path(self).is_vendored_path()
+        ruff_python_ast::PythonVersion::latest()
     }
 }
 
