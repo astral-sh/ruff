4add3b137d780d3d8bd85ef5c94c7928810e7616
Felix Scherz <felixwscherz@gmail.com>
2026-04-21 20:09:25 +0200

    [ty] fix goto definition for generic classes (#24714)
    Hi, this fixes https://github.com/astral-sh/ty/issues/2854
    
    ## Summary
    
    Compare class literals instead of class bases for the purposes of
    skipping the MRO until the pivot class.
    Also skip `BoundSuperType` when looking up definitions since we probably
    do not want to look up definitions on the `super` class itself.
    
    ## Test Plan
    Added snapshot tests
    
    ---------
    
    Co-authored-by: Douglas Creager <dcreager@dcreager.net>

--- crates/ty_python_semantic/src/types/ide_support.rs
+++ crates/ty_python_semantic/src/types/ide_support.rs
@@ -261,6 +261,11 @@ pub fn definitions_for_attribute<'db>(
             continue;
         }
 
+        // Prevent lookup on BoundSuper proxy object
+        if matches!(ty, Type::BoundSuper(_)) {
+            continue;
+        }
+
         let meta_type = ty.to_meta_type(db);
 
         // Look up the attribute first on the meta-type, unless it's already a class-like type.
