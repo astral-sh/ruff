e3ec081e7527cdf5cb317fcc02b654ef9112f9cb
Micha Reiser <micha@reiser.io>
2026-04-24 16:38:30 -0400

    Make regression test more resilient

--- crates/ruff_server/tests/e2e/workspace.rs
+++ crates/ruff_server/tests/e2e/workspace.rs
@@ -16,12 +16,15 @@ ignore = ["F401"]
 "#,
         )?
         .with_file("systemtests/tests/common/fakes/wus.py", "import os\n")?
+        .with_file("external/Y/wus.py", "import os\n")?
         .build();
 
     server.open_text_document("systemtests/tests/common/fakes/wus.py", "import os\n", 1);
+    server.open_text_document("external/Y/wus.py", "import os\n", 1);
 
     let diagnostics =
         server.document_diagnostic_request("systemtests/tests/common/fakes/wus.py", None);
+    let external_diagnostics = server.document_diagnostic_request("external/Y/wus.py", None);
 
     assert_json_snapshot!(
         diagnostics,
@@ -29,6 +32,71 @@ ignore = ["F401"]
     {
       "kind": "full",
       "items": []
+    }
+    "#
+    );
+
+    assert_json_snapshot!(
+        external_diagnostics,
+        @r#"
+    {
+      "kind": "full",
+      "items": [
+        {
+          "range": {
+            "start": {
+              "line": 0,
+              "character": 7
+            },
+            "end": {
+              "line": 0,
+              "character": 9
+            }
+          },
+          "severity": 2,
+          "code": "F401",
+          "codeDescription": {
+            "href": "https://docs.astral.sh/ruff/rules/unused-import"
+          },
+          "source": "Ruff",
+          "message": "`os` imported but unused",
+          "tags": [
+            1
+          ],
+          "data": {
+            "code": "F401",
+            "edits": [
+              {
+                "newText": "",
+                "range": {
+                  "end": {
+                    "character": 0,
+                    "line": 1
+                  },
+                  "start": {
+                    "character": 0,
+                    "line": 0
+                  }
+                }
+              }
+            ],
+            "noqa_edit": {
+              "newText": "  # noqa: F401\n",
+              "range": {
+                "end": {
+                  "character": 0,
+                  "line": 1
+                },
+                "start": {
+                  "character": 9,
+                  "line": 0
+                }
+              }
+            },
+            "title": "Remove unused import: `os`"
+          }
+        }
+      ]
     }
     "#
     );
