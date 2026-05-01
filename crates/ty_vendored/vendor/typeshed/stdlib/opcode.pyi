75dce66af64fc15dc374867122b64fd31995f423
typeshedbot <>
2026-05-01 01:02:22 +0000

    Sync typeshed. Source commit: https://github.com/python/typeshed/commit/e4d32e01bee44241a5e7c33298c261175b9f1bdb

--- crates/ty_vendored/vendor/typeshed/stdlib/opcode.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/opcode.pyi
@@ -1,8 +1,3 @@
-"""
-opcode module - potentially shared between dis and other modules which
-operate on bytecodes (e.g. peephole optimizers).
-"""
-
 import sys
 from typing import Final, Literal
 
@@ -49,5 +44,4 @@ opmap: Final[dict[str, int]]
 HAVE_ARGUMENT: Final[int]
 EXTENDED_ARG: Final[int]
 
-def stack_effect(opcode: int, oparg: int | None = None, /, *, jump: bool | None = None) -> int:
-    """Compute the stack effect of the opcode."""
+def stack_effect(opcode: int, oparg: int | None = None, /, *, jump: bool | None = None) -> int: ...
--- crates/ty_vendored/vendor/typeshed/stdlib/posixpath.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/posixpath.pyi
@@ -1,15 +1,3 @@
-"""Common operations on Posix pathnames.
-
-Instead of importing this module directly, import os and refer to
-this module as os.path.  The "os.path" name is an alias for this
-module on Posix systems; on other systems (e.g. Windows),
-os.path provides the same operations in a manner specific to that
-platform, and is an alias to another module (e.g. ntpath).
-
-Some of this can actually be useful on non-Posix systems too, e.g.
-for manipulation of the pathname component of URLs.
-"""
-
 import sys
 from _typeshed import AnyOrLiteralStr, BytesPath, FileDescriptorOrPath, StrOrBytesPath, StrPath
 from collections.abc import Iterable
@@ -94,55 +82,35 @@ devnull: LiteralString
 
 # Overloads are necessary to work around python/mypy#17952 & python/mypy#11880
 @overload
-def abspath(path: PathLike[AnyStr]) -> AnyStr:
-    """Return an absolute path."""
-
+def abspath(path: PathLike[AnyStr]) -> AnyStr: ...
 @overload
 def abspath(path: AnyStr) -> AnyStr: ...
 @overload
-def basename(p: PathLike[AnyStr]) -> AnyStr:
-    """Returns the final component of a pathname"""
-
+def basename(p: PathLike[AnyStr]) -> AnyStr: ...
 @overload
 def basename(p: AnyOrLiteralStr) -> AnyOrLiteralStr: ...
 @overload
-def dirname(p: PathLike[AnyStr]) -> AnyStr:
-    """Returns the directory component of a pathname"""
-
+def dirname(p: PathLike[AnyStr]) -> AnyStr: ...
 @overload
 def dirname(p: AnyOrLiteralStr) -> AnyOrLiteralStr: ...
 @overload
-def expanduser(path: PathLike[AnyStr]) -> AnyStr:
-    """Expand ~ and ~user constructions.  If user or $HOME is unknown,
-    do nothing.
-    """
-
+def expanduser(path: PathLike[AnyStr]) -> AnyStr: ...
 @overload
 def expanduser(path: AnyStr) -> AnyStr: ...
 @overload
-def expandvars(path: PathLike[AnyStr]) -> AnyStr:
-    """Expand shell variables of form $var and ${var}.  Unknown variables
-    are left unchanged.
-    """
-
+def expandvars(path: PathLike[AnyStr]) -> AnyStr: ...
 @overload
 def expandvars(path: AnyStr) -> AnyStr: ...
 @overload
-def normcase(s: PathLike[AnyStr]) -> AnyStr:
-    """Normalize case of pathname.  Has no effect under Posix"""
-
+def normcase(s: PathLike[AnyStr]) -> AnyStr: ...
 @overload
 def normcase(s: AnyOrLiteralStr) -> AnyOrLiteralStr: ...
 @overload
-def normpath(path: PathLike[AnyStr]) -> AnyStr:
-    """Normalize path, eliminating double slashes, etc."""
-
+def normpath(path: PathLike[AnyStr]) -> AnyStr: ...
 @overload
 def normpath(path: AnyOrLiteralStr) -> AnyOrLiteralStr: ...
 @overload
-def commonpath(paths: Iterable[LiteralString]) -> LiteralString:
-    """Given a sequence of path names, returns the longest common sub-path."""
-
+def commonpath(paths: Iterable[LiteralString]) -> LiteralString: ...
 @overload
 def commonpath(paths: Iterable[StrPath]) -> str: ...
 @overload
@@ -152,83 +120,41 @@ def commonpath(paths: Iterable[BytesPath]) -> bytes: ...
 # but must be defined as pos-only in the stub or cross-platform code doesn't type-check,
 # as the parameter name is different in ntpath.join()
 @overload
-def join(a: LiteralString, /, *paths: LiteralString) -> LiteralString:
-    """Join two or more pathname components, inserting '/' as needed.
-    If any component is an absolute path, all previous path components
-    will be discarded.  An empty last part will result in a path that
-    ends with a separator.
-    """
-
+def join(a: LiteralString, /, *paths: LiteralString) -> LiteralString: ...
 @overload
 def join(a: StrPath, /, *paths: StrPath) -> str: ...
 @overload
 def join(a: BytesPath, /, *paths: BytesPath) -> bytes: ...
 @overload
-def realpath(filename: PathLike[AnyStr], *, strict: bool | _AllowMissingType = False) -> AnyStr:
-    """Return the canonical path of the specified filename, eliminating any
-    symbolic links encountered in the path.
-    """
-
+def realpath(filename: PathLike[AnyStr], *, strict: bool | _AllowMissingType = False) -> AnyStr: ...
 @overload
 def realpath(filename: AnyStr, *, strict: bool | _AllowMissingType = False) -> AnyStr: ...
 @overload
-def relpath(path: LiteralString, start: LiteralString | None = None) -> LiteralString:
-    """Return a relative version of a path"""
-
+def relpath(path: LiteralString, start: LiteralString | None = None) -> LiteralString: ...
 @overload
 def relpath(path: BytesPath, start: BytesPath | None = None) -> bytes: ...
 @overload
 def relpath(path: StrPath, start: StrPath | None = None) -> str: ...
 @overload
-def split(p: PathLike[AnyStr]) -> tuple[AnyStr, AnyStr]:
-    """Split a pathname.  Returns tuple "(head, tail)" where "tail" is
-    everything after the final slash.  Either part may be empty.
-    """
-
+def split(p: PathLike[AnyStr]) -> tuple[AnyStr, AnyStr]: ...
 @overload
 def split(p: AnyOrLiteralStr) -> tuple[AnyOrLiteralStr, AnyOrLiteralStr]: ...
 @overload
-def splitdrive(p: PathLike[AnyStr]) -> tuple[AnyStr, AnyStr]:
-    """Split a pathname into drive and path. On Posix, drive is always
-    empty.
-    """
-
+def splitdrive(p: PathLike[AnyStr]) -> tuple[AnyStr, AnyStr]: ...
 @overload
 def splitdrive(p: AnyOrLiteralStr) -> tuple[AnyOrLiteralStr, AnyOrLiteralStr]: ...
 @overload
-def splitext(p: PathLike[AnyStr]) -> tuple[AnyStr, AnyStr]:
-    """Split the extension from a pathname.
-
-    Extension is everything from the last dot to the end, ignoring
-    leading dots.  Returns "(root, ext)"; ext may be empty.
-    """
-
+def splitext(p: PathLike[AnyStr]) -> tuple[AnyStr, AnyStr]: ...
 @overload
 def splitext(p: AnyOrLiteralStr) -> tuple[AnyOrLiteralStr, AnyOrLiteralStr]: ...
-def isabs(s: StrOrBytesPath) -> bool:
-    """Test whether a path is absolute"""
-
-def islink(path: FileDescriptorOrPath) -> bool:
-    """Test whether a path is a symbolic link"""
-
-def ismount(path: FileDescriptorOrPath) -> bool:
-    """Test whether a path is a mount point"""
-
-def lexists(path: FileDescriptorOrPath) -> bool:
-    """Test whether a path exists.  Returns True for broken symbolic links"""
+def isabs(s: StrOrBytesPath) -> bool: ...
+def islink(path: FileDescriptorOrPath) -> bool: ...
+def ismount(path: FileDescriptorOrPath) -> bool: ...
+def lexists(path: FileDescriptorOrPath) -> bool: ...
 
 if sys.version_info >= (3, 12):
-    def isjunction(path: StrOrBytesPath) -> bool:
-        """Test whether a path is a junction
-        Junctions are not supported on the current platform
-        """
-
+    def isjunction(path: StrOrBytesPath) -> bool: ...
     @overload
-    def splitroot(p: AnyOrLiteralStr) -> tuple[AnyOrLiteralStr, AnyOrLiteralStr, AnyOrLiteralStr]:
-        """Split a pathname into drive, root and tail.
-
-        The tail contains anything after the root.
-        """
-
+    def splitroot(p: AnyOrLiteralStr) -> tuple[AnyOrLiteralStr, AnyOrLiteralStr, AnyOrLiteralStr]: ...
     @overload
     def splitroot(p: PathLike[AnyStr]) -> tuple[AnyStr, AnyStr, AnyStr]: ...
