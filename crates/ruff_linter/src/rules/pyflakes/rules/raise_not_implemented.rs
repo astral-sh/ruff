10cbb9d6e57cc17c0e40eb0042da6bce1d25a07c
Andrej730 <azhilenkov@gmail.com>
2026-05-09 00:04:33 +0500

    docs: F901 - Clarify NotImplemented is a value, not an exception
    - clarified that `NotImplemented` is a value, not an exception
    
    - changed a bit wording in a second paragaph too - "should not be raised directly" seems to imply a bit that `NotImplemented` could be raised in theory somehow.

--- crates/ruff_linter/src/rules/pyflakes/rules/raise_not_implemented.rs
+++ crates/ruff_linter/src/rules/pyflakes/rules/raise_not_implemented.rs
@@ -10,11 +10,10 @@ use crate::{Edit, Fix, FixAvailability, Violation};
 /// Checks for `raise` statements that raise `NotImplemented`.
 ///
 /// ## Why is this bad?
-/// `NotImplemented` is an exception used by binary special methods to indicate
+/// `NotImplemented` is a special value returned by binary special methods to indicate
 /// that an operation is not implemented with respect to a particular type.
 ///
-/// `NotImplemented` should not be raised directly. Instead, raise
-/// `NotImplementedError`, which is used to indicate that the method is
+/// Raise `NotImplementedError` instead, which is used to indicate that a method is
 /// abstract or not implemented in the derived class.
 ///
 /// ## Example
