284472709825651d9ce4fbaf21df455aa6201da1
GiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2026-06-04 12:44:03 -0700

    [ruff] Add max-height calc style to diagnostics list in Diagnostics.tsx

--- playground/ruff/src/Editor/Diagnostics.tsx
+++ playground/ruff/src/Editor/Diagnostics.tsx
@@ -69,7 +69,10 @@ function Items({
   const uniqueIds: Map<string, number> = new Map();
 
   return (
-    <ul className="space-y-0.5 grow overflow-y-scroll">
+    <ul
+      className="space-y-0.5 grow overflow-y-scroll"
+      style="max-height: calc(100cqh - 52px);"
+    >
       {diagnostics.map((diagnostic) => {
         const row = diagnostic.start_location.row;
         const column = diagnostic.start_location.column;
