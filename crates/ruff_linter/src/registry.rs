efb513a16ddb21864ab69494ef080f8eed5e6d21
Brent Westbrook <brentrwestbrook@gmail.com>
2026-06-04 13:13:02 -0400

    offer both codes and names as possible values

--- crates/ruff_linter/src/registry.rs
+++ crates/ruff_linter/src/registry.rs
@@ -434,10 +434,13 @@ pub mod clap_completion {
         }
 
         fn possible_values(&self) -> Option<Box<dyn Iterator<Item = PossibleValue> + '_>> {
-            Some(Box::new(Rule::iter().map(|rule| {
+            Some(Box::new(Rule::iter().flat_map(|rule| {
                 let name = rule.noqa_code().to_string();
                 let help = rule.name().as_str();
-                PossibleValue::new(name).help(help)
+                [
+                    PossibleValue::new(&name).help(help),
+                    PossibleValue::new(help).help(name),
+                ]
             })))
         }
     }
