826026d235bce487ea007eb2021e6330992af603
Shunsuke Shibayama <sbym1346@gmail.com>
2026-06-08 00:39:29 +0900

    WIP

--- crates/ty_python_semantic/src/types/type_alias.rs
+++ crates/ty_python_semantic/src/types/type_alias.rs
@@ -168,7 +168,7 @@ impl<'db> PEP695TypeAliasType<'db> {
     /// The RHS type of a PEP-695 style type alias with *no* specialization applied.
     /// Returns `Divergent` if the type alias is defined cyclically.
     #[salsa::tracked(
-        cycle_initial=|_, id, _| Type::divergent(id),
+        cycle_initial=|db, id, _| Type::recursive(db, id, None, Type::divergent(id)),
         cycle_fn=|db, cycle, previous: &Type<'db>, value: Type<'db>, _| {
             value.cycle_normalized(db, *previous, cycle)
         },
@@ -286,7 +286,7 @@ impl<'db> ManualPEP695TypeAliasType<'db> {
     /// Computed lazily from the definition to avoid including the value in the interned
     /// struct's identity. Returns `Divergent` if the type alias is defined cyclically.
     #[salsa::tracked(
-        cycle_initial=|_, id, _| Type::divergent(id),
+        cycle_initial=|db, id, _| Type::recursive(db, id, None, Type::divergent(id)),
         cycle_fn=|db, cycle, previous: &Type<'db>, value: Type<'db>, _| {
             value.cycle_normalized(db, *previous, cycle)
         },
