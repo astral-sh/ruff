43e2172d5d77dff2e0886deb89b41f6db7938359
Shunsuke Shibayama <sbym1346@gmail.com>
2026-06-08 16:07:06 +0900

    no bare Divergent

--- crates/ty_python_semantic/src/types/class/static_literal.rs
+++ crates/ty_python_semantic/src/types/class/static_literal.rs
@@ -2207,8 +2207,8 @@ impl<'db> StaticClassLiteral<'db> {
 
     #[salsa::tracked(
         cycle_fn=implicit_attribute_cycle_recover,
-        cycle_initial=|_, id, _| Member {
-            inner: Place::bound(Type::divergent(id)).into(),
+        cycle_initial=|db, id, _| Member {
+            inner: Place::bound(Type::recursive(db, id, None, Type::divergent(id))).into(),
         },
         heap_size=ruff_memory_usage::heap_size,
     )]
@@ -3155,7 +3155,8 @@ fn explicit_bases_cycle_initial<'db>(
     // Try to produce a list of `Divergent` types of the right length. However, if one or more of
     // the bases is a starred expression, we don't know how many entries that will eventually
     // expand to.
-    vec![Type::divergent(id); class_stmt.bases().len()].into_boxed_slice()
+    vec![Type::recursive(db, id, None, Type::divergent(id)); class_stmt.bases().len()]
+        .into_boxed_slice()
 }
 
 fn explicit_bases_cycle_fn<'db>(
