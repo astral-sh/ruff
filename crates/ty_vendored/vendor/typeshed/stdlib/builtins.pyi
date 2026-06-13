b3c0247c9965fb6a96e0f9c16a02d78f045be1c5
typeshedbot <>
2026-06-13 12:07:14 +0000

    Apply typeshed patches

--- crates/ty_vendored/vendor/typeshed/stdlib/builtins.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/builtins.pyi
@@ -3145,12 +3145,12 @@ class dict(MutableMapping[_KT, _VT]):
 
     # Positional-only in dict, but not in MutableMapping
     @overload  # type: ignore[override]
-    def get(self, key: _KT, default: None = None, /) -> _VT | None:
+    def get(self, key: object, default: None = None, /) -> _VT | None:
         """Return the value for key if key is in the dictionary, else default."""
     @overload
-    def get(self, key: _KT, default: _VT, /) -> _VT: ...
+    def get(self, key: object, default: _VT, /) -> _VT: ...
     @overload
-    def get(self, key: _KT, default: _T, /) -> _VT | _T: ...
+    def get(self, key: object, default: _T, /) -> _VT | _T: ...
 
     @overload
     def pop(self, key: _KT, /) -> _VT:
