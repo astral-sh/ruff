ff91f656de28687a9cef36e644ebcdd4fd316311
typeshedbot <>
2026-06-13 12:03:09 +0000

    Sync typeshed. Source commit: https://github.com/python/typeshed/commit/95aa599bbbb5bbc84f445a78a52416ed040a81fc

--- crates/ty_vendored/vendor/typeshed/stdlib/encodings/koi8_r.pyi
+++ crates/ty_vendored/vendor/typeshed/stdlib/encodings/koi8_r.pyi
@@ -1,5 +1,3 @@
-"""Python Character Mapping Codec koi8_r generated from 'MAPPINGS/VENDORS/MISC/KOI8-R.TXT' with gencodec.py."""
-
 import codecs
 from _codecs import _EncodingMap
 from _typeshed import ReadableBuffer
