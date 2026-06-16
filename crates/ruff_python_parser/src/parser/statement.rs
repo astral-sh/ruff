2965512db8829578c6b6b324b143429f46e5c95d
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-06-16 05:22:45 -0400

    Add parenthesized yield parser test

--- crates/ruff_python_parser/src/parser/statement.rs
+++ crates/ruff_python_parser/src/parser/statement.rs
@@ -358,6 +358,9 @@ impl<'src> Parser<'src> {
                 // test_err yield_after_comma
                 // def f(): 1, yield 1
 
+                // test_ok yield_after_comma_parenthesized
+                // def f(): 1, (yield 1)
+
                 // simple_stmt: `... | yield_stmt | star_expressions | ...`
                 let parsed_expr =
                     self.parse_expression_list(ExpressionContext::yield_or_starred_bitwise_or());
