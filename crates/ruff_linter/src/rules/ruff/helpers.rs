astral-sh/ruff

commit 41febb9a341e7b997c47bbd0c4feb45a264a8b8b

fix typos (#26436)
Co-authored-by: Samuel Rigaud <rigaud@gmail.com>

crates/ruff_linter/src/rules/ruff/helpers.rs
@@ -44,7 +44,7 @@ fn is_attrs_field(func: &Expr, semantic: &SemanticModel) -> bool {
 /// I.e., if `DataclassKind::Attrs` is passed in,
 /// return `true` if `func` represents a call to `attr.ib()` or `attrs.field()`;
 /// if `DataclassKind::Stdlib` is passed in,
-/// return `true` if `func` represents a call to `dataclasse.field()`.
+/// return `true` if `func` represents a call to `dataclass.field()`.
 pub(super) fn is_dataclass_field(
     func: &Expr,
     semantic: &SemanticModel,

crates/ruff_linter/src/rules/airflow/helpers.rs
@@ -23,7 +23,7 @@ pub(crate) const INTERNAL_MODULE_WARNING: &str = "This is an internal module whi
 pub(crate) enum Replacement {
     // There's no replacement or suggestion other than removal
     None,
-    // Additional information. Used when there's no direct maaping replacement.
+    // Additional information. Used when there's no direct mapping replacement.
     Message(&'static str),
     // The attribute name of a class has been changed.
     AttrName(&'static str),

crates/ruff_linter/resources/test/fixtures/pycodestyle/E30.pyi
@@ -3,7 +3,7 @@ import json
 from typing import Any, Sequence
 
 class MissingCommand(TypeError): ...
-class AnoherClass: ...
+class AnotherClass: ...
 
 def a(): ...
 

crates/ruff_linter/resources/test/fixtures/pydocstyle/D300.py
@@ -20,7 +20,7 @@ def contains_triples(t):
     '''(\'''|""")'''
 
 
-# TODO: here should raise D300 for using dobule quotes instead,
+# TODO: here should raise D300 for using double quotes instead,
 # because escaped double quote does allow us.
 def contains_triples(t):
     '''(\""")'''

crates/ruff_linter/resources/test/fixtures/pyflakes/F811_36.py
@@ -3,7 +3,7 @@ from faststream._internal.broker import BrokerUsecase
 
 if TYPE_CHECKING:
     # See: https://github.com/astral-sh/ruff/issues/22554
-    # shoud detect -------------------------vvvvvvvvvvvvv
+    # should detect -------------------------vvvvvvvvvvvvv
     from faststream._internal.broker import BrokerUsecase
     from faststream.specification.schema import Contact, License
 

crates/ruff_linter/resources/test/fixtures/pylint/unexpected_special_method_signature.py
@@ -8,7 +8,7 @@ class TestClass:
     def __bool__(self, x=1):  # additional optional args OK
         ...
 
-    def __bool__():  # ignored; should be caughty by E0211/N805
+    def __bool__():  # ignored; should be caught by E0211/N805
         ...
 
     @staticmethod

crates/ruff_linter/resources/test/fixtures/pyupgrade/UP009_code_utf8_utf8.py
@@ -1,3 +1,3 @@
-print('No coding coments here')
+print('No coding comments here')
 # -*- coding: utf-8 -*-
 # -*- coding: utf-8 -*-

crates/ruff_linter/resources/test/fixtures/pyupgrade/UP034.py
@@ -2,7 +2,7 @@
 print(("foo"))
 
 # UP034
-print(("hell((goodybe))o"))
+print(("hell((goodbye))o"))
 
 # UP034
 print((("foo")))

commit de0848cc91019790fb9a198d7067f719401dce33

Fix typo introduced by #26436 ("fix typos")
The name of the function is `dataclasses.field`, not `dataclass.field`

crates/ruff_linter/src/rules/ruff/helpers.rs
@@ -44,7 +44,7 @@ fn is_attrs_field(func: &Expr, semantic: &SemanticModel) -> bool {
 /// I.e., if `DataclassKind::Attrs` is passed in,
 /// return `true` if `func` represents a call to `attr.ib()` or `attrs.field()`;
 /// if `DataclassKind::Stdlib` is passed in,
-/// return `true` if `func` represents a call to `dataclass.field()`.
+/// return `true` if `func` represents a call to `dataclasses.field()`.
 pub(super) fn is_dataclass_field(
     func: &Expr,
     semantic: &SemanticModel,