1f2edd971bdea6d7e0ca48ad3f75b7e3e73aa99e
Andrej730 <azhilenkov@gmail.com>
2026-07-20 00:12:49 +0500

    ruff-ecosystem: bump bokeh/bokeh branch to `branch-3.10`
    `branch-3.3` got deleted and running `ruff-ecosystem` results in `Failed to clone bokeh/bokeh: fatal: Remote branch branch-3.3 not found in upstream origin`
    
    `branch-3.10` is the current default branch on https://github.com/bokeh/bokeh
    
    Changing deprecated script just for consistency.

--- scripts/check_ecosystem.py
+++ scripts/check_ecosystem.py
@@ -126,7 +126,7 @@ REPOSITORIES: list[Repository] = [
     Repository("aws", "aws-sam-cli", "develop"),
     Repository("binary-husky", "gpt_academic", "master"),
     Repository("bloomberg", "pytest-memray", "main"),
-    Repository("bokeh", "bokeh", "branch-3.3", select="ALL"),
+    Repository("bokeh", "bokeh", "branch-3.10", select="ALL"),
     # Disabled due to use of explicit `select` with `E999`, which has been removed.
     # See: https://github.com/astral-sh/ruff/pull/12129
     # Repository("demisto", "content", "master"),
