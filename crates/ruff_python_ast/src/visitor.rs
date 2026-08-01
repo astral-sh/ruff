0ed646f0accbee9251a881dd450e537186afe5d9
Andrej730 <azhilenkov@gmail.com>
2026-08-01 12:12:07 +0500

    `Visitor` trait: fix visting elif `test` expressions twice
    Once insde `Stmt::If` arm in `walk_stmt`, another time in
    `walk_elif_else_clause`.
    Allows minor performance gain and avoiding double diagnostics:
    ```python
    # ruff check --select TRY401 test.py
    
    import logging
    
    logger = logging.getLogger(__name__)
    
    def f():
        try:
            pass
        except Exception as e:
            if False:
                pass
            elif logger.exception("uh oh", e):
                pass
    ```

--- crates/ruff_python_ast/src/visitor.rs
+++ crates/ruff_python_ast/src/visitor.rs
@@ -265,9 +265,6 @@ pub fn walk_stmt<'a, V: Visitor<'a> + ?Sized>(visitor: &mut V, stmt: &'a Stmt) {
             visitor.visit_expr(test);
             visitor.visit_body(body);
             for clause in elif_else_clauses {
-                if let Some(test) = &clause.test {
-                    visitor.visit_expr(test);
-                }
                 walk_elif_else_clause(visitor, clause);
             }
         }
