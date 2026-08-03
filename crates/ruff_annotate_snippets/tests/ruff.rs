f5f7e142734bd759eab34146c82e178da2fb34a7
Ed Page <eopage@gmail.com>
2026-08-03 16:27:29 -0500

    test(annotate): Capture confusing suggestion

--- crates/ruff_annotate_snippets/tests/ruff.rs
+++ crates/ruff_annotate_snippets/tests/ruff.rs
@@ -461,3 +461,83 @@ help: remove the entry
     let renderer = renderer.decor_style(DecorStyle::Unicode);
     assert_data_eq!(renderer.render(input), expected_unicode);
 }
+
+#[test]
+fn insertion_with_trailing_whitespace() {
+    let source = "line 1\nline 2\nline 3\nline 4\nline 5\nline 6\nline 7\nline 8\nline 9\nline 10\nline 11\nline 12\nline 13\n";
+    let input = &[
+        Level::ERROR
+            .primary_title("main diagnostic message")
+            .id("test-diagnostic")
+            .element(
+                Snippet::source(source)
+                    .path("example.py")
+                    .annotation(AnnotationKind::Primary.span(7..13)),
+            ),
+        Level::HELP
+            .secondary_title("Replace three lines")
+            .element(Snippet::source(source).patch(Patch::new(7..7, "fixed "))),
+    ];
+    let expected_ascii = str![[r#"
+error[test-diagnostic]: main diagnostic message
+ --> example.py:2:1
+  |
+2 | line 2
+  | ^^^^^^
+  |
+help: Replace three lines
+  |
+2 | fixed line 2
+  | +++++
+"#]];
+
+    let renderer = Renderer::plain();
+    assert_data_eq!(renderer.render(input), expected_ascii);
+
+    let expected_unicode = str![[r#"
+error[test-diagnostic]: main diagnostic message
+  ╭▸ example.py:2:1
+  │
+2 │ line 2
+  │ ━━━━━━
+  ╰╴
+help: Replace three lines
+  ╭╴
+2 │ fixed line 2
+  ╰╴+++++
+"#]];
+    let renderer = renderer.decor_style(DecorStyle::Unicode);
+    assert_data_eq!(renderer.render(input), expected_unicode);
+}
+
+#[test]
+#[should_panic = "index out of bounds: the len is 11 but the index is 11"]
+fn multiple_insertions() {
+    let source = "line 1\nline 2\nline 3\nline 4\nline 5\nline 6\nline 7\nline 8\nline 9\nline 10\nline 11\nline 12\nline 13\n";
+    let input = &[
+        Level::ERROR
+            .primary_title("main diagnostic message")
+            .id("test-diagnostic")
+            .element(
+                Snippet::source(source)
+                    .path("example.py")
+                    .annotation(AnnotationKind::Primary.span(7..13)),
+            ),
+        Level::HELP.secondary_title("Replace three lines").element(
+            Snippet::source(source)
+                .patch(Patch::new(7..7, "fixed "))
+                .patch(Patch::new(42..42, "fixed "))
+                .patch(Patch::new(87..87, "fixed ")),
+        ),
+    ];
+    let expected_ascii = str![[r#"
+"#]];
+
+    let renderer = Renderer::plain();
+    assert_data_eq!(renderer.render(input), expected_ascii);
+
+    let expected_unicode = str![[r#"
+"#]];
+    let renderer = renderer.decor_style(DecorStyle::Unicode);
+    assert_data_eq!(renderer.render(input), expected_unicode);
+}
