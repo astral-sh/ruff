313568d021f4afd0f11b57f897420643951121c7
Brent Westbrook <brentrwestbrook@gmail.com>
2026-08-13 10:12:42 -0400

    preserve has_any_shebang check for EXE002

--- crates/ruff_linter/src/rules/flake8_executable/rules/mod.rs
+++ crates/ruff_linter/src/rules/flake8_executable/rules/mod.rs
@@ -29,9 +29,9 @@ pub(crate) fn from_tokens(
     for range in comment_ranges {
         let comment = locator.slice(range);
         if let Some(shebang) = ShebangDirective::try_extract(comment) {
-            if range.start() == TextSize::ZERO {
-                has_any_shebang = true;
+            has_any_shebang = true;
 
+            if range.start() == TextSize::ZERO {
                 shebang_missing_python(range, &shebang, context);
 
                 if context.is_rule_enabled(Rule::ShebangNotExecutable) {
