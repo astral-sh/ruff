a92d0cf7ba1e2f8df6b49330bf8f795229eeb43e
Brent Westbrook <brentrwestbrook@gmail.com>
2026-08-19 11:53:10 -0400

    this data hasn't been a tuple for a very long time

--- crates/ruff_macros/src/map_codes.rs
+++ crates/ruff_macros/src/map_codes.rs
@@ -58,8 +58,7 @@ pub(crate) fn map_codes(func: &ItemFn) -> syn::Result<TokenStream> {
         ));
     };
 
-    // Map from: linter (e.g., `Flake8Bugbear`) to rule code (e.g.,`"002"`) to rule data (e.g.,
-    // `(Rule::UnaryPrefixIncrement, vec![])`).
+    // Map from: linter (e.g., `Flake8Bugbear`) to rule code (e.g.,`"002"`) to rule data.
     let mut linter_to_rules: BTreeMap<Ident, BTreeMap<String, Rule>> = BTreeMap::new();
 
     for arm in arms {
