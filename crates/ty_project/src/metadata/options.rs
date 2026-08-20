b9378450b6c035055174deb3d00b6800055ee7eb
Glyphack <sh.hooshyari@gmail.com>
2026-08-20 08:24:15 +0200

    Include executable in tracing

--- crates/ty_project/src/metadata/options.rs
+++ crates/ty_project/src/metadata/options.rs
@@ -296,11 +296,6 @@ impl Options {
             ToProgramSettingsError::SearchPaths,
         )?;
 
-        tracing::info!(
-            "Python version: Python {python_version}, platform: {python_platform}",
-            python_version = python_version.version
-        );
-
         let python_executable = python_environment.as_ref().map(|env| {
             let sys_prefix = env.sys_prefix();
             if cfg!(windows) {
@@ -309,6 +304,11 @@ impl Options {
                 sys_prefix.join("bin/python")
             }
         });
+
+        tracing::info!(
+            "Python version: Python {python_version}, platform: {python_platform}, executable: {python_executable}",
+            python_version = python_version.version
+        );
 
         Ok((
             ProgramSettings {
