a917e1c2449cb048f080207ed66b8e320eedc010
Douglas Creager <dcreager@dcreager.net>
2026-08-26 08:59:22 -0400

    property tests

--- crates/ty_python_semantic/src/types/constraints/bitset.rs
+++ crates/ty_python_semantic/src/types/constraints/bitset.rs
@@ -43,6 +43,29 @@ impl BitSet {
         }
     }
 
+    #[cfg(test)]
+    fn contains(&self, index: usize) -> bool {
+        match self {
+            // Fast path
+            BitSet::Inline(chunk) => {
+                let Ok(index) = u32::try_from(index) else {
+                    return false;
+                };
+                let bit_mask_within_chunk = 1usize.checked_shl(index);
+                bit_mask_within_chunk
+                    .is_some_and(|bit_mask_within_chunk| (*chunk & bit_mask_within_chunk) != 0)
+            }
+            BitSet::Heap(chunks) => {
+                let chunk_index = index / CHUNK_SIZE;
+                chunks.get(chunk_index).is_some_and(|chunk| {
+                    let bit_index_within_chunk = index % CHUNK_SIZE;
+                    let bit_mask_within_chunk = 1 << bit_index_within_chunk;
+                    (*chunk & bit_mask_within_chunk) != 0
+                })
+            }
+        }
+    }
+
     fn iter_chunks(&self) -> impl Iterator<Item = usize> + '_ {
         let chunks = match self {
             BitSet::Inline(chunk) => std::slice::from_ref(chunk),
@@ -77,7 +100,14 @@ impl BitSet {
     }
 
     pub(super) fn overlaps_with(&self, other: &Self) -> bool {
-        std::iter::zip(self.iter_chunks(), other.iter_chunks()).any(|(lhs, rhs)| (lhs & rhs) != 0)
+        match (self, other) {
+            (BitSet::Inline(lhs), BitSet::Inline(rhs)) => (*lhs & *rhs) != 0,
+            (BitSet::Inline(lhs), BitSet::Heap(rhs)) => (*lhs & rhs[0]) != 0,
+            (BitSet::Heap(lhs), BitSet::Inline(rhs)) => (lhs[0] & *rhs) != 0,
+            (BitSet::Heap(lhs), BitSet::Heap(rhs)) => {
+                std::iter::zip(lhs, rhs).any(|(lhs, rhs)| (*lhs & *rhs) != 0)
+            }
+        }
     }
 }
 
@@ -89,24 +119,30 @@ impl Default for BitSet {
 
 impl BitOrAssign<&Self> for BitSet {
     fn bitor_assign(&mut self, rhs: &Self) {
-        match (self, rhs) {
-            (BitSet::Inline(lhs), BitSet::Inline(rhs)) => {
-                *lhs |= *rhs;
-            }
-            (BitSet::Inline(lhs), BitSet::Heap(rhs)) => {
-                *lhs |= rhs[0];
-            }
-            (BitSet::Heap(lhs), BitSet::Inline(rhs)) => {
-                lhs[0] |= *rhs;
-            }
-            (BitSet::Heap(lhs), BitSet::Heap(rhs)) => {
-                if lhs.len() < rhs.len() {
-                    lhs.resize(rhs.len(), 0);
-                }
-                for (lhs, rhs) in std::iter::zip(lhs, rhs) {
+        match self {
+            BitSet::Inline(lhs) => match rhs {
+                BitSet::Inline(rhs) => {
                     *lhs |= *rhs;
                 }
-            }
+                BitSet::Heap(rhs) => {
+                    let mut chunks = rhs.clone();
+                    chunks[0] |= *lhs;
+                    *self = BitSet::Heap(chunks);
+                }
+            },
+            BitSet::Heap(lhs) => match rhs {
+                BitSet::Inline(rhs) => {
+                    lhs[0] |= *rhs;
+                }
+                BitSet::Heap(rhs) => {
+                    if lhs.len() < rhs.len() {
+                        lhs.resize(rhs.len(), 0);
+                    }
+                    for (lhs, rhs) in std::iter::zip(lhs, rhs) {
+                        *lhs |= *rhs;
+                    }
+                }
+            },
         }
     }
 }
@@ -116,5 +152,141 @@ impl BitOrAssign<Option<&Self>> for BitSet {
         if let Some(rhs) = rhs {
             *self |= rhs;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+    use std::ops::BitOr;
+
+    use itertools::Itertools;
+    use quickcheck_macros::quickcheck;
+
+    use super::BitSet;
+
+    impl BitSet {
+        fn from_elements(elements: &[u16]) -> BitSet {
+            let mut bitset = BitSet::default();
+            for element in elements {
+                bitset.insert(*element as usize);
+            }
+            bitset
+        }
+
+        /// Returns whether two bitsets are _mathematically_ equal. (Our [`Eq`] impl is only used
+        /// for salsa caching, and implements the cheaper test of _structural_ equality.)
+        fn equals(a: &Self, b: &Self) -> bool {
+            // This depends on iter returning the values in sorted order
+            a.iter().eq(b.iter())
+        }
+    }
+
+    impl BitOr<&BitSet> for &BitSet {
+        type Output = BitSet;
+
+        fn bitor(self, rhs: &BitSet) -> BitSet {
+            let mut result = self.clone();
+            result |= rhs;
+            result
+        }
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn iter_returns_all_elements(elements: Vec<u16>) -> bool {
+        let expected: HashSet<_> = elements.iter().copied().map_into::<usize>().collect();
+        let bitset = BitSet::from_elements(&elements);
+        let actual: HashSet<_> = bitset.iter().collect();
+        expected == actual
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn iter_contains_no_duplicates(elements: Vec<u16>) -> bool {
+        let bitset = BitSet::from_elements(&elements);
+        let as_vec: Vec<_> = bitset.iter().collect();
+        let as_set: HashSet<_> = bitset.iter().collect();
+        as_vec.len() == as_set.len()
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn union_preserves_iter(a: Vec<u16>, b: Vec<u16>) -> bool {
+        let expected: HashSet<_> = std::iter::chain(a.iter().copied(), b.iter().copied())
+            .map_into::<usize>()
+            .collect();
+        let a = BitSet::from_elements(&a);
+        let b = BitSet::from_elements(&b);
+        let union = &a | &b;
+        let actual: HashSet<_> = union.iter().collect();
+        expected == actual
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn union_preserves_membership(a: Vec<u16>, b: Vec<u16>, member: u16) -> bool {
+        let a = BitSet::from_elements(&a);
+        let b = BitSet::from_elements(&b);
+        let union = &a | &b;
+        let member = member as usize;
+        (a.contains(member) || b.contains(member)) == (union.contains(member))
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn union_has_left_identity(elements: Vec<u16>) -> bool {
+        let bitset = BitSet::from_elements(&elements);
+        let empty = BitSet::default();
+        let union = &empty | &bitset;
+        BitSet::equals(&bitset, &union)
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn union_has_right_identity(elements: Vec<u16>) -> bool {
+        let bitset = BitSet::from_elements(&elements);
+        let empty = BitSet::default();
+        let union = &bitset | &empty;
+        BitSet::equals(&bitset, &union)
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn union_is_idempotent(elements: Vec<u16>) -> bool {
+        let bitset = BitSet::from_elements(&elements);
+        let union = &bitset | &bitset;
+        BitSet::equals(&bitset, &union)
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn union_is_commutative(a: Vec<u16>, b: Vec<u16>) -> bool {
+        let a = BitSet::from_elements(&a);
+        let b = BitSet::from_elements(&b);
+        let leftwards = &a | &b;
+        let rightwards = &b | &a;
+        BitSet::equals(&leftwards, &rightwards)
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn union_is_associative(a: Vec<u16>, b: Vec<u16>, c: Vec<u16>) -> bool {
+        let a = BitSet::from_elements(&a);
+        let b = BitSet::from_elements(&b);
+        let c = BitSet::from_elements(&c);
+        let leftwards = &(&a | &b) | &c;
+        let rightwards = &a | &(&b | &c);
+        BitSet::equals(&leftwards, &rightwards)
+    }
+
+    #[quickcheck]
+    #[expect(clippy::needless_pass_by_value)]
+    fn overlaps_with_iff_not_disjoint(a: Vec<u16>, b: Vec<u16>) -> bool {
+        let set_a: HashSet<_> = a.iter().copied().collect();
+        let set_b: HashSet<_> = b.iter().copied().collect();
+        let a = BitSet::from_elements(&a);
+        let b = BitSet::from_elements(&b);
+        a.overlaps_with(&b) != set_a.is_disjoint(&set_b)
     }
 }
