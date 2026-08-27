841bc2de8a76e6f29b69a2908f3e6825f484ce22
WhiteFox0-0 <swayammmm0@gmail.com>
2026-08-28 02:52:28 +0530

    chore: add PR number

--- crates/ruff_linter/src/preview.rs
+++ crates/ruff_linter/src/preview.rs
@@ -175,7 +175,7 @@ pub(crate) const fn is_fix_os_path_samefile_enabled(settings: &LinterSettings) -
     settings.preview.is_enabled()
 }
 
-// https://github.com/astral-sh/ruff/pull/
+// https://github.com/astral-sh/ruff/pull/28118
 pub(crate) const fn is_fix_os_path_join_enabled(settings: &LinterSettings) -> bool {
     settings.preview.is_enabled()
 }
