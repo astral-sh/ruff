6a1075c4587e07a6762556fbd5c193cc80cf9299
Brent Westbrook <brentrwestbrook@gmail.com>
2026-08-28 12:06:03 -0400

    use `TestServerBuilder::with_file` for notebook tests

--- crates/ruff_server/tests/e2e/main.rs
+++ crates/ruff_server/tests/e2e/main.rs
@@ -1006,6 +1006,7 @@ impl TestServerBuilder {
     }
 
     /// Set the initial client options for the test server
+    #[expect(dead_code)]
     pub(crate) fn with_initialization_options(mut self, options: serde_json::Value) -> Self {
         self.initialization_options = Some(options);
         self
