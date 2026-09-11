f49a8679593e2ee285f3b0bce5df6c2b18e358e7
David Salvisberg <david.salvisberg@seantis.ch>
2026-09-11 11:14:44 +0200

    Also changes `TYPING_CONTEXT` to align with previous behavior

--- crates/ruff_python_semantic/src/model.rs
+++ crates/ruff_python_semantic/src/model.rs
@@ -2911,7 +2911,7 @@ bitflags! {
 
         /// The context is in a typing-only context.
         const TYPING_CONTEXT = Self::TYPE_CHECKING_BLOCK.bits() | Self::TYPING_ONLY_ANNOTATION.bits() |
-            Self::STRING_TYPE_DEFINITION.bits() | Self::TYPE_PARAM_DEFINITION.bits();
+            Self::STRING_TYPE_DEFINITION.bits() | Self::TYPE_PARAM_DEFINITION.bits() | Self::DEFERRED_TYPE_ALIAS.bits();
 
         /// The context is in any type alias.
         const TYPE_ALIAS = Self::ANNOTATED_TYPE_ALIAS.bits() | Self::DEFERRED_TYPE_ALIAS.bits();
